44c82dc06fa986331387db0c732d217552381e2a
yshang1 <yshang1@6f19259b-4bc3-4df7-8a09-765794883524>
2006-05-30 06:04:00 +0000

    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@319 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkModulePkg/Core/Pei/PeiMain.h
+++ EdkModulePkg/Core/Pei/PeiMain.h
@@ -127,28 +127,6 @@ typedef union {
 //
 // Main PEI entry
 //
-EFI_STATUS
-EFIAPI
-PeiMain (
-  IN EFI_PEI_STARTUP_DESCRIPTOR  *PeiStartupDescriptor
-  )
-/*++
-
-Routine Description:
-
-  Main entry point to Pei Core.
-
-Arguments:
-
-  PeiStartupDescriptor - Information and services provided by SEC phase.
-
-Returns:
-
-  This function never returns
-
---*/
-;
-
 
 EFI_STATUS
 EFIAPI
@@ -160,9 +138,8 @@ PeiCore (
 
 Routine Description:
 
-  The entry routine to Pei Core, invoked by PeiMain during transition
-  from SEC to PEI. After switching stack in the PEI core, it will restart
-  with the old core data.
+  Main entry point to Pei Core. After switching stack in the PEI core, 
+  it will restart with the old core data.
 
 Arguments:
 
