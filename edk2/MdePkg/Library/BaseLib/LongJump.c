3c3ebc1ddd4f202c41ac85a2f5595fe87fc7837c
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2006-05-31 08:45:29 +0000

    Created as one wrapper for LongJump(), to added assert() function
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@341 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ edk2/MdePkg/Library/BaseLib/LongJump.c
@@ -0,0 +1,49 @@
+/** @file
+  Long Jump functions. 
+
+  Copyright (c) 2006, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+  Module Name:  LongJump.c
+
+**/
+
+VOID
+EFIAPI
+_LongJump (
+  IN BASE_LIBRARY_JUMP_BUFFER  *JumpBuffer,
+  IN      UINTN                Value
+  );
+
+/**
+  Restores the CPU context that was saved with SetJump().
+
+  Restores the CPU context from the buffer specified by JumpBuffer.
+  This function never returns to the caller.
+  Instead is resumes execution based on the state of JumpBuffer.
+
+  If JumpBuffer is NULL, then ASSERT().
+  If Value is 0, then ASSERT().
+
+  @param  JumpBuffer    A pointer to CPU context buffer.
+  @param  Value         The value to return when the SetJump() context is restored.
+
+**/
+VOID
+EFIAPI
+LongJump (
+  IN BASE_LIBRARY_JUMP_BUFFER  *JumpBuffer,
+  IN      UINTN                Value
+  )
+{
+  ASSERT (JumpBuffer != NULL);
+  ASSERT (Value != 0);
+  
+  _LongJump (JumpBuffer, Value);
+}
