493b974b1bcc0ffb9be5cb6b240ebd341ceca374
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2006-05-31 08:52:54 +0000

    Created as wrapper for SwitchStack() to add assert()
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@347 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ MdePkg/Library/BaseLib/Ipf/SwitchStack.c
@@ -0,0 +1,49 @@
+/** @file
+  Switch Stack functions.
+
+  Copyright (c) 2006, Intel Corporation<BR>
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+  Module Name:  SwitchStack.c
+
+**/
+
+/**
+  Transfers control to a function starting with a new stack.
+
+  Transfers control to the function specified by EntryPoint using the new stack
+  specified by NewStack and passing in the parameters specified by Context1 and
+  Context2. Context1 and Context2 are optional and may be NULL. The function
+  EntryPoint must never return.
+
+  If EntryPoint is NULL, then ASSERT().
+  If NewStack is NULL, then ASSERT().
+
+  @param  EntryPoint  A pointer to function to call with the new stack.
+  @param  Context1    A pointer to the context to pass into the EntryPoint
+                      function.
+  @param  Context2    A pointer to the context to pass into the EntryPoint
+                      function.
+  @param  NewStack    A pointer to the new stack to use for the EntryPoint
+                      function.
+
+**/
+VOID
+EFIAPI
+SwitchStack (
+  IN      SWITCH_STACK_ENTRY_POINT  EntryPoint,
+  IN      VOID                      *Context1,
+  IN      VOID                      *Context2,
+  IN      VOID                      *NewStack
+  )
+{
+  ASSERT (EntryPoint != NULL && NewStack != NULL);
+
+  _SwitchStack (EntryPoint, Context1, Context2, NewStack);
+}
