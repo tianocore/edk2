4985daa52cbc1544902425536203cba256e60ee2
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2006-05-31 08:56:42 +0000

    add function header
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@350 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Library/BasePrintLib/PrintLibInternal.c
+++ edk2/MdePkg/Library/BasePrintLib/PrintLibInternal.c
@@ -18,6 +18,20 @@
 
 static CONST CHAR8 mHexStr[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
+
+/**
+  Internal function that places the character into the Buffer.
+
+  Internal function that places ASCII or Unicode character into the Buffer.
+
+  @param  Buffer      Buffer to place the Unicode or ASCII string.
+  @param  Length      Count of character to be placed into Buffer.
+  @param  Character   Character to be placed into Buffer.
+  @param  Increment   Character increment in Buffer.
+
+  @return Number of characters printed.
+
+**/
 CHAR8 *
 BasePrintLibFillBuffer (
   CHAR8   *Buffer,
@@ -37,15 +51,13 @@ BasePrintLibFillBuffer (
 }
 
 /**
-  Print worker function that prints a Value as a decimal number in Buffer.
+  Internal function that convert a decimal number to a string in Buffer.
 
-  @param  Buffer Location to place the Unicode or ASCII string of Value.
-  
-  @param  Value Value to convert to a Decimal or Hexidecimal string in Buffer.
-  
-  @param  Flags Flags to use in printing string, see file header for details.
-  
-  @param  Precision Minimum number of digits to return in the ASCII string
+  Print worker function that convert a decimal number to a string in Buffer.
+
+  @param  Buffer    Location to place the Unicode or ASCII string of Value.
+  @param  Value     Value to convert to a Decimal or Hexidecimal string in Buffer.
+  @param  Radix     Radix of the value
 
   @return Number of characters printed.
 
@@ -74,6 +86,43 @@ BasePrintLibValueToString (
   return Digits;
 }
 
+/**
+  Internal function that converts a decimal value to a Null-terminated string.
+  
+  Converts the decimal number specified by Value to a Null-terminated  
+  string specified by Buffer containing at most Width characters.
+  If Width is 0 then a width of  MAXIMUM_VALUE_CHARACTERS is assumed.
+  The total number of characters placed in Buffer is returned.
+  If the conversion contains more than Width characters, then only the first
+  Width characters are returned, and the total number of characters 
+  required to perform the conversion is returned.
+  Additional conversion parameters are specified in Flags.  
+  The Flags bit LEFT_JUSTIFY is always ignored.
+  All conversions are left justified in Buffer.
+  If Width is 0, PREFIX_ZERO is ignored in Flags.
+  If COMMA_TYPE is set in Flags, then PREFIX_ZERO is ignored in Flags, and commas
+  are inserted every 3rd digit starting from the right.
+  If Value is < 0, then the fist character in Buffer is a '-'.
+  If PREFIX_ZERO is set in Flags and PREFIX_ZERO is not being ignored, 
+  then Buffer is padded with '0' characters so the combination of the optional '-' 
+  sign character, '0' characters, digit characters for Value, and the Null-terminator
+  add up to Width characters.
+
+  If Buffer is NULL, then ASSERT().
+  If unsupported bits are set in Flags, then ASSERT().
+  If Width >= MAXIMUM_VALUE_CHARACTERS, then ASSERT()
+
+  @param  Buffer    Pointer to the output buffer for the produced Null-terminated
+                    string.
+  @param  Flags     The bitmask of flags that specify left justification, zero pad,
+                    and commas.
+  @param  Value     The 64-bit signed value to convert to a string.
+  @param  Width	    The maximum number of characters to place in Buffer.
+  @param  Increment Character increment in Buffer.
+  
+  @return Total number of characters required to perform the conversion.
+
+**/
 UINTN
 BasePrintLibConvertValueToString (
   IN OUT CHAR8   *Buffer,
