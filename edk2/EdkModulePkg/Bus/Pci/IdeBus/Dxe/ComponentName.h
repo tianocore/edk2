b383582a94f88b665e8c261dc066038b485e212b
darylm503 <darylm503@6f19259b-4bc3-4df7-8a09-765794883524>
2006-06-09 23:44:26 +0000

    Update comments to conform to the new, Doxygen friendly, coding standard.  These are structural changes only.  There is NO NEW CONTENT.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@462 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/EdkModulePkg/Bus/Pci/IdeBus/Dxe/ComponentName.h
+++ edk2/EdkModulePkg/Bus/Pci/IdeBus/Dxe/ComponentName.h
@@ -1,22 +1,14 @@
-/*++
-Copyright (c) 2006, Intel Corporation                                                         
-All rights reserved. This program and the accompanying materials                          
-are licensed and made available under the terms and conditions of the BSD License         
-which accompanies this distribution.  The full text of the license may be found at        
-http://opensource.org/licenses/bsd-license.php                                            
-                                                                                          
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+/** @file
+  Copyright (c) 2006, Intel Corporation                                                         
+  All rights reserved. This program and the accompanying materials                          
+  are licensed and made available under the terms and conditions of the BSD License         
+  which accompanies this distribution.  The full text of the license may be found at        
+  http://opensource.org/licenses/bsd-license.php                                            
 
-Module Name:
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
 
-    ComponentName.h
-
-Abstract:
-
-
-Revision History
---*/
+**/
 
 #ifndef _IDE_BUS_COMPONENT_NAME_H
 #define _IDE_BUS_COMPONENT_NAME_H
@@ -38,6 +30,16 @@ extern EFI_COMPONENT_NAME_PROTOCOL  gIDEBusComponentName;
 //
 // EFI Component Name Functions
 //
+/**
+  TODO: Add function description
+
+  @param  This TODO: add argument description
+  @param  Language TODO: add argument description
+  @param  DriverName TODO: add argument description
+
+  TODO: add return values
+
+**/
 EFI_STATUS
 EFIAPI
 IDEBusComponentNameGetDriverName (
@@ -45,25 +47,20 @@ IDEBusComponentNameGetDriverName (
   IN  CHAR8                        *Language,
   OUT CHAR16                       **DriverName
   )
-/*++
+;
 
-Routine Description:
-
+/**
   TODO: Add function description
 
-Arguments:
-
-  This        - TODO: add argument description
-  Language    - TODO: add argument description
-  DriverName  - TODO: add argument description
-
-Returns:
+  @param  This TODO: add argument description
+  @param  ControllerHandle TODO: add argument description
+  @param  ChildHandle TODO: add argument description
+  @param  Language TODO: add argument description
+  @param  ControllerName TODO: add argument description
 
   TODO: add return values
 
---*/
-;
-
+**/
 EFI_STATUS
 EFIAPI
 IDEBusComponentNameGetControllerName (
@@ -73,46 +70,20 @@ IDEBusComponentNameGetControllerName (
   IN  CHAR8                                           *Language,
   OUT CHAR16                                          **ControllerName
   )
-/*++
+;
 
-Routine Description:
-
+/**
   TODO: Add function description
 
-Arguments:
-
-  This              - TODO: add argument description
-  ControllerHandle  - TODO: add argument description
-  ChildHandle       - TODO: add argument description
-  Language          - TODO: add argument description
-  ControllerName    - TODO: add argument description
-
-Returns:
+  @param  IdeBlkIoDevicePtr TODO: add argument description
 
   TODO: add return values
 
---*/
-;
-
+**/
 VOID
 AddName (
   IN  IDE_BLK_IO_DEV               *IdeBlkIoDevicePtr
   )
-/*++
-
-Routine Description:
-
-  TODO: Add function description
-
-Arguments:
-
-  IdeBlkIoDevicePtr - TODO: add argument description
-
-Returns:
-
-  TODO: add return values
-
---*/
 ;
 
 #endif
