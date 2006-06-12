4f92b864cf12d7e6550b9fe334d0c63eace94c6c
bbahnsen <bbahnsen@6f19259b-4bc3-4df7-8a09-765794883524>
2006-06-12 17:15:58 +0000

    Convert env var name to all caps.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@486 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/edksetup.sh
+++ edk2/edksetup.sh
@@ -26,8 +26,8 @@ else
 
 # These should be ok as they are.
 export CLASSPATH=$WORKSPACE/Tools/Jars/SurfaceArea.jar:$WORKSPACE/Tools/Jars/frameworktasks.jar:$WORKSPACE/Tools/Jars/cpptasks.jar:$WORKSPACE/Tools/Jars/GenBuild.jar:$XMLBEANS_HOME/lib/resolver.jar:$XMLBEANS_HOME/lib/xbean.jar:$XMLBEANS_HOME/lib/xmlpublic.jar:$XMLBEANS_HOME/lib/jsr173_1.0_api.jar:$XMLBEANS_HOME/lib/saxon8.jar:$XMLBEANS_HOME/lib/xbean_xpath.jar
-export Framework_Tools_Path=$WORKSPACE/Tools/bin
-export PATH=$Framework_Tools_Path:$ANT_HOME/bin:$JAVA_HOME/bin:$PATH
+export FRAMEWORK_TOOLS_PATH=$WORKSPACE/Tools/bin
+export PATH=$FRAMEWORK_TOOLS_PATH:$ANT_HOME/bin:$JAVA_HOME/bin:$PATH
 
 # Handle any particulars down here.
 case "`uname`" in
