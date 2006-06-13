006bcfb5185bf6506caa7a863b8ea0124ba8f5f1
bbahnsen <bbahnsen@6f19259b-4bc3-4df7-8a09-765794883524>
2006-06-13 16:06:40 +0000

    Sync SetMem64 with MASM version.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@500 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Library/BaseMemoryLibRepStr/Ia32/SetMem64.s
+++ edk2/MdePkg/Library/BaseMemoryLibRepStr/Ia32/SetMem64.s
@@ -22,19 +22,20 @@
 #------------------------------------------------------------------------------
 
     .386: 
+    #.MODEL flat,C
     .code: 
 
-.global InternalMemSetMem64
-InternalMemSetMem64:
+.global _InternalMemSetMem64
+_InternalMemSetMem64:
     push    %edi
-    movl    12(%esp),%ecx
-    movl    16(%esp),%eax
-    movl    20(%esp),%edx
-    movl    8(%esp),%edi
+    movl    12(%esp), %ecx
+    movl    16(%esp), %eax
+    movl    20(%esp), %edx
+    movl    8(%esp), %edi
 L0: 
     mov     %eax,-8(%edi,%ecx,8)
     mov     %edx,-4(%edi,%ecx,8)
     loop    L0
-    movl    %edi,%eax
+    movl    %edi, %eax
     pop     %edi
     ret
