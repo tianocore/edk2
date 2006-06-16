8d44eafc42b707df86e98f083a7b3741e2be5896
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2006-06-16 14:45:03 +0000

    Fix DxeIpl module bugs: DxeCoreFileName should be from the Dxe Core ffs file that contain DxeCore Pe Data.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@543 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/EdkModulePkg/Core/DxeIplPeim/DxeIpl.h
+++ edk2/EdkModulePkg/Core/DxeIplPeim/DxeIpl.h
@@ -102,9 +102,9 @@ DxeLoadCore (
 
 EFI_STATUS
 PeiProcessFile (
-  IN  UINT16                 SectionType,
-  IN  EFI_FFS_FILE_HEADER    *FfsFileHeader,
-  OUT VOID                   **Pe32Data
+  IN      UINT16                 SectionType,
+  IN OUT  EFI_FFS_FILE_HEADER    **RealFfsFileHeader,
+  OUT     VOID                   **Pe32Data
   );
 
 EFI_STATUS
