b2df304b07bf28a6b517805d735c79d0b4baa7c5
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2006-06-30 08:40:03 +0000

    1.Updated some functions header of BaseLib with new MWG spec 2.Rollback EnablePaging64.asm
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@669 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Library/BaseLib/Ia32/EnablePaging64.asm
+++ edk2/MdePkg/Library/BaseLib/Ia32/EnablePaging64.asm
@@ -27,12 +27,8 @@
 
 InternalX86EnablePaging64 PROC
     cli
-
-    mov     ebx, [esp + 4]              ; save CS
-    mov     eax, OFFSET cs_address
-    mov     word ptr [eax], bx          ; Update CS selector for far jump
-
-    mov     eax, cr4 
+    mov     [esp], @F                   ; offset for far retf
+    mov     eax, cr4
     or      al, (1 SHL 5)
     mov     cr4, eax                    ; enable PAE
     mov     ecx, 0c0000080h
@@ -43,28 +39,17 @@ InternalX86EnablePaging64 PROC
     bts     eax, 31
     mov     cr0, eax                    ; enable paging
     retf
-    jmp     go_to_long_mode
-go_to_long_mode:
-
-    ;
-    ; This is the next instruction after enabling paging.  Jump to long mode
-    ;
-    db      067h
-    db      0eah                        ;   Far Jump Offset:Selector to reload CS
-    dd      OFFSET in_long_mode         ;   Offset is ensuing instruction boundary
-cs_address:
-    dw      0h                          ;   CS selector will be updated at runtime 
-
-in_long_mode:                           ; now in long mode
+@@:                                     ; long mode starts here
     DB      67h, 48h
-    mov     ebx, [esp + 8]              ; mov rbx, [esp]
-    DB      67h, 48h                   
-    mov     ecx, [esp + 10h]            ; mov rcx, [esp + 8]
+    mov     ebx, [esp]                  ; mov rbx, [esp]
     DB      67h, 48h
-    mov     edx, [esp + 18h]            ; mov rdx, [esp + 10h]
+    mov     ecx, [esp + 8]              ; mov rcx, [esp + 8]
     DB      67h, 48h
-    mov     esp, [esp + 20h]            ; mov rsp, [esp + 18h]
+    mov     edx, [esp + 10h]            ; mov rdx, [esp + 10h]
+    DB      67h, 48h
+    mov     esp, [esp + 18h]            ; mov rsp, [esp + 18h]
     DB      48h
+    add     esp, -20h                   ; add rsp, -20h
     call    ebx                         ; call rbx
     jmp     $
 InternalX86EnablePaging64 ENDP
