9bc8d472a61d5dfebcff211089e2730baa387fba
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2006-07-01 03:55:08 +0000

    Put the FD creation operation in run.cmd temporary.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@705 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkNt32Pkg/run.cmd
+++ EdkNt32Pkg/run.cmd
@@ -10,8 +10,17 @@
 @REM
 
 @echo off
+
+:check_fd
+if exist %WORKSPACE%\EdkNt32Pkg\build\Debug\Fv\Fv_Recovery.fd goto start_secmain
+
+:create_fd
+if not exist Build\Debug\Fv mkdir Build\Debug\Fv
+copy Build\Debug\Msft\Fv\FV_RECOVERY.fv /B + Build\Debug\Msft\Fv\NV_STORAGE.fv /B Build\Debug\Fv\Fv_Recovery.fd /B
+
+:start_secmain
 pushd .
-cd Build\DEBUG\IA32
+cd Build\DEBUG\MSFT\IA32
 SecMain.exe
 popd
 @echo on
