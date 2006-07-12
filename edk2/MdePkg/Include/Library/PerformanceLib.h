d2f6a54ed16a8a21563de4ece5398ce1a58c3058
alfred <alfred@6f19259b-4bc3-4df7-8a09-765794883524>
2006-07-12 10:24:36 +0000

    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@905 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/Library/PerformanceLib.h
+++ edk2/MdePkg/Include/Library/PerformanceLib.h
@@ -1,7 +1,7 @@
 /** @file
   Library that provides services to measure module execution performance
 
-  Copyright (c) 2004, Intel Corporation                                                         
+  Copyright (c) 2006, Intel Corporation.
   All rights reserved. This program and the accompanying materials                          
   are licensed and made available under the terms and conditions of the BSD License         
   which accompanies this distribution.  The full text of the license may be found at        
