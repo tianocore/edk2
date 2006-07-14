8774a65da1626a7def5893419e845a4e01345796
bbahnsen <bbahnsen@6f19259b-4bc3-4df7-8a09-765794883524>
2006-07-14 09:27:09 +0000

    Add new OSS License
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@1011 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/Tools/Source/TianoTools/Strip/Strip.c
+++ edk2/Tools/Source/TianoTools/Strip/Strip.c
@@ -1,6 +1,6 @@
 /*++
 
-Copyright (c)  1999 - 2002 Intel Corporation. All rights reserved
+Copyright (c)  2004-2006 Intel Corporation. All rights reserved
 This software and associated documentation (if any) is furnished
 under a license and may only be used or copied in accordance
 with the terms of the license. Except as permitted by such
@@ -8,7 +8,6 @@ license, no part of this software or documentation may be
 reproduced, stored in a retrieval system, or transmitted in any
 form or by any means without the express written consent of
 Intel Corporation.
-
 
 Module Name:
 
