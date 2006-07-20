9f73d38da4a01da1c5ff6679565d426f1c4bbe98
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2006-07-20 04:00:59 +0000

    added some comments in BaseLib
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@1053 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Library/UefiBootServicesTableLib.h
+++ MdePkg/Include/Library/UefiBootServicesTableLib.h
@@ -1,14 +1,14 @@
 /** @file
   Library that provides a global pointer to the UEFI Boot Services Tables
 
-  Copyright (c) 2006, Intel Corporation                                                         
-  All rights reserved. This program and the accompanying materials                          
-  are licensed and made available under the terms and conditions of the BSD License         
-  which accompanies this distribution.  The full text of the license may be found at        
-  http://opensource.org/licenses/bsd-license.php                                            
+  Copyright (c) 2006, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
 
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
   Module Name:  UefiBootServicesTableLib.h
 
@@ -18,10 +18,18 @@
 #define __UEFI_BOOT_SERVICES_TABLE_LIB_H__
 
 //
-//
+// Cache the Image Handle
 //
 extern EFI_HANDLE         gImageHandle;
+
+//
+// Cache pointer to the EFI System Table
+//
 extern EFI_SYSTEM_TABLE   *gST;
+
+//
+// Cache pointer to the EFI Boot Services Table
+//
 extern EFI_BOOT_SERVICES  *gBS;
 
 #endif
