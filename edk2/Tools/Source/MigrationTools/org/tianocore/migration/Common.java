41ada623c1d1702fefdb9d9270e8038a0cb4fd39
alfred <alfred@6f19259b-4bc3-4df7-8a09-765794883524>
2006-08-16 12:09:16 +0000

    Enhancing Critic
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@1296 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/Tools/Source/MigrationTools/org/tianocore/migration/Common.java
+++ edk2/Tools/Source/MigrationTools/org/tianocore/migration/Common.java
@@ -1,3 +1,15 @@
+/** @file
+ 
+ Copyright (c) 2006, Intel Corporation
+ All rights reserved. This program and the accompanying materials
+ are licensed and made available under the terms and conditions of the BSD License
+ which accompanies this distribution.  The full text of the license may be found at
+ http://opensource.org/licenses/bsd-license.php
+ 
+ THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+ 
+ **/
 package org.tianocore.migration;
 
 import java.io.*;
@@ -5,6 +17,8 @@ import java.util.regex.*;
 import java.util.*;
 
 public class Common {
+	public static Pattern ptnseparate = Pattern.compile("(.*)\\\\([^\\\\]*)");
+	
 	public static String file2string(String filename) throws Exception {
 		BufferedReader rd = new BufferedReader(new FileReader(filename));
 		StringBuffer wholefile = new StringBuffer();
@@ -16,11 +30,8 @@ public class Common {
 	}
 
 	public static void ensureDir(String objFileWhole) {
-		Pattern ptnseparate = Pattern.compile("(.*)\\\\[^\\\\]*");
-		Matcher mtrseparate;
 		File tempdir;
-
-		mtrseparate = ptnseparate.matcher(objFileWhole);
+		Matcher mtrseparate = ptnseparate.matcher(objFileWhole);
 		if (mtrseparate.find()) {
 			tempdir = new File(mtrseparate.group(1));
 			if (!tempdir.exists()) tempdir.mkdirs();
@@ -50,6 +61,29 @@ public class Common {
 		}
 		
 		return filelist;
+	}
+
+	public static String dirCopy_(String src) throws Exception {
+		Matcher mtrseparate = Common.ptnseparate.matcher(src);
+		if (mtrseparate.find()) {
+			dirCopy(src, mtrseparate.group(1) + File.separator + "_" + mtrseparate.group(2));
+		}
+		return mtrseparate.group(1) + File.separator + "_" + mtrseparate.group(2);
+	}
+	
+	public static void dirCopy(String src, String des) throws Exception {
+		String[] list = new File(src).list();
+		File test;
+
+		for (int i = 0 ; i < list.length ; i++) {
+			test = new File(src + File.separator + list[i]);
+			if (test.isDirectory()) {
+				dirCopy(src + File.separator + list[i], des + File.separator + list[i]);
+			} else {
+				ensureDir(des + File.separator + list[i]);
+				string2file(file2string(src + File.separator + list[i]), des + File.separator + list[i]);
+			}
+		}
 	}
 	
 	public static void toDoAll(String path, ForDoAll fda) throws Exception { // filter of file type can be done in toDo
