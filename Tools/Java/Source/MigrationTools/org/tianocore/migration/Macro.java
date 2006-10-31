71f30e5184872f6c5b43dfc8738ed7a0ed1e23bd
alfred <alfred@6f19259b-4bc3-4df7-8a09-765794883524>
2006-10-31 02:27:39 +0000

    Coding Style Phase 1 Eclipse Format Comment for MigrationTool.java
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@1869 6f19259b-4bc3-4df7-8a09-765794883524

--- Tools/Java/Source/MigrationTools/org/tianocore/migration/Macro.java
+++ Tools/Java/Source/MigrationTools/org/tianocore/migration/Macro.java
@@ -12,36 +12,43 @@
  **/
 package org.tianocore.migration;
 
-import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Macro {
-    Macro(String r8, String r9) {
-        r8name = r8;
-        r9name = r9;
-    }
-    Macro(String[] linecontext) {
-        r8name = linecontext[0];
-        r9name = linecontext[1];
-    }
-    
-    public String r8name;
-    public String r9name;
+	Macro(String r8, String r9) {
+		r8name = r8;
+		r9name = r9;
+	}
 
-    public static Pattern ptntmacro = Pattern.compile("\\b\\w(\\w|\\d)*",Pattern.MULTILINE);
+	Macro(String[] linecontext) {
+		r8name = linecontext[0];
+		r9name = linecontext[1];
+	}
 
-    private static String unmacro = "VOID UINTN BOOLEAN ASSERT OPTIONAL STATIC NULL TRUE IN OUT FALSE";
+	public String r8name;
 
-    public static String register(Matcher mtr, ModuleInfo mi, Database db) {
-        String temp = null;
-        
-        temp = mtr.group();
-        mi.hashmacro.add(temp);
-        if (MigrationTool.db.hasMacro(temp)) {                    // only changed macros registered, because the database of macro has only changed ones
-            if (!unmacro.contains(temp)) {
-                mi.hashnonlocalmacro.add(temp);
-            }
-            return temp;
-        }
-        return null;
-    }
+	public String r9name;
+
+	public static Pattern ptntmacro = Pattern.compile("\\b\\w(\\w|\\d)*",
+			Pattern.MULTILINE);
+
+	private static String unmacro = "VOID UINTN BOOLEAN ASSERT OPTIONAL STATIC NULL TRUE IN OUT FALSE";
+
+	public static String register(Matcher mtr, ModuleInfo mi, Database db) {
+		String temp = null;
+
+		temp = mtr.group();
+		mi.hashmacro.add(temp);
+		if (MigrationTool.db.hasMacro(temp)) { // only changed macros
+												// registered, because the
+												// database of macro has only
+												// changed ones
+			if (!unmacro.contains(temp)) {
+				mi.hashnonlocalmacro.add(temp);
+			}
+			return temp;
+		}
+		return null;
+	}
 }
