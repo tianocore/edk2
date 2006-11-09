4a501849869146bbbd075b59cc5c06145daebd59
mdkinney <mdkinney@6f19259b-4bc3-4df7-8a09-765794883524>
2006-11-09 22:18:41 +0000

    Fix build break from CPU_STACK_ALIGNMENT update.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@1927 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkModulePkg/Core/DxeIplX64Peim/DxeLoadX64.c
+++ EdkModulePkg/Core/DxeIplX64Peim/DxeLoadX64.c
@@ -263,7 +263,7 @@ Returns:
   // for safety (PpisNeededByDxe and DxeCore).
   //
   TopOfStack = BaseOfStack + EFI_SIZE_TO_PAGES (STACK_SIZE) * EFI_PAGE_SIZE - CPU_STACK_ALIGNMENT;
-  TopOfStack = ALIGN_POINTER (TopOfStack, CPU_STACK_ALIGNMENT);
+  TopOfStack = (EFI_PHYSICAL_ADDRESS) ALIGN_POINTER (TopOfStack, CPU_STACK_ALIGNMENT);
 
   //
   // Add architecture-specifc HOBs (including the BspStore HOB)
