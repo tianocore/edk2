397e97139f6137a692aff2c3385b3b3ff91e3aa6
mdkinney <mdkinney@6f19259b-4bc3-4df7-8a09-765794883524>
2006-11-22 00:39:03 +0000

    Add 64-bit I/O port functions that ASSERT()
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@1992 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Library/BaseIoLibIntrinsic/IoLibIpf.c
+++ edk2/MdePkg/Library/BaseIoLibIntrinsic/IoLibIpf.c
@@ -107,6 +107,30 @@ IoRead32 (
 }
 
 /**
+  Reads a 64-bit I/O port.
+
+  Reads the 64-bit I/O port specified by Port. The 64-bit read value is returned.
+  This function must guarantee that all I/O read and write operations are
+  serialized.
+
+  If 64-bit I/O port operations are not supported, then ASSERT().
+
+  @param  Port  The I/O port to read.
+
+  @return The value read.
+
+**/
+UINT64
+EFIAPI
+IoRead64 (
+  IN      UINTN                     Port
+  )
+{
+  ASSERT (FALSE);
+  return 0;
+}
+
+/**
   Writes a 8-bit I/O port.
 
   Writes the 8-bit I/O port specified by Port with the value specified by Value
@@ -197,6 +221,32 @@ IoWrite32 (
   Address += PcdGet64(PcdIoBlockBaseAddressForIpf);
 
   return MmioWrite32 (Address, Data);
+}
+
+/**
+  Writes a 64-bit I/O port.
+
+  Writes the 64-bit I/O port specified by Port with the value specified by Value
+  and returns Value. This function must guarantee that all I/O read and write
+  operations are serialized.
+
+  If 64-bit I/O port operations are not supported, then ASSERT().
+
+  @param  Port  The I/O port to write.
+  @param  Value The value to write to the I/O port.
+
+  @return The value written the I/O port.
+
+**/
+UINT64
+EFIAPI
+IoWrite64 (
+  IN      UINTN                     Port,
+  IN      UINT64                    Value
+  )
+{
+  ASSERT (FALSE);
+  return 0;
 }
 
 /**
