265c22cc33668997ac811a3bad61e0e6c3177f15
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2006-12-08 03:43:57 +0000

    Handle new relocation type EFI_IMAGE_REL_BASED_DIR64  for X64.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2075 6f19259b-4bc3-4df7-8a09-765794883524

--- Tools/CCode/Source/PeCoffLoader/X64/PeCoffLoaderEx.c
+++ Tools/CCode/Source/PeCoffLoader/X64/PeCoffLoaderEx.c
@@ -1,6 +1,7 @@
-/*++
+/**@file
+  x64 Specific relocation fixups.
 
-Copyright 2005, Intel Corporation                                                         
+Copyright (c) 2005 - 2006 Intel Corporation. <BR>
 All rights reserved. This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -9,48 +10,30 @@ http://opensource.org/licenses/bsd-license.php
 THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
 WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
 
-
-Module Name:
-
-  PeCoffLoaderEx.c
-
-Abstract:
-
-  x64 Specific relocation fixups
-
-Revision History
-
---*/
+**/
 
 #include <Common/UefiBaseTypes.h>
 #include <Common/EfiImage.h>
 #include <Library/PeCoffLib.h>
 
-
-
-
-RETURN_STATUS
-PeCoffLoaderRelocateImageEx (
-  IN UINT16      *Reloc,
-  IN OUT CHAR8   *Fixup, 
-  IN OUT CHAR8   **FixupData,
-  IN UINT64      Adjust
-  )
-/*++
-
-Routine Description:
+/**
   Performs an x64 specific relocation fixup
 
-Arguments:
-  Reloc      - Pointer to the relocation record
-  Fixup      - Pointer to the address to fix up
-  FixupData  - Pointer to a buffer to log the fixups
-  Adjust     - The offset to adjust the fixup
-
-Returns:
-  None
-
---*/
+  @param Reloc        Pointer to the relocation record
+  @param Fixup        Pointer to the address to fix up
+  @param FixupData    Pointer to a buffer to log the fixups
+  @param Adjust       The offset to adjust the fixup
+  
+  @retval RETURN_SUCCESS      Success to perform relocation
+  @retval RETURN_UNSUPPORTED  Unsupported.
+**/
+RETURN_STATUS
+PeCoffLoaderRelocateImageEx (
+  IN     UINT16       *Reloc,
+  IN OUT CHAR8        *Fixup, 
+  IN OUT CHAR8        **FixupData,
+  IN     UINT64       Adjust
+  )
 {
   UINT64      *F64;
 
