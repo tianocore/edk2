0cab4aa06ab6774ff895d9465b9e2924b706a41a
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2006-12-20 03:58:26 +0000

    Add runtime checking for EdkFvbServiceLib.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@2121 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/EdkModulePkg/Library/EdkFvbServiceLib/Fvb.h
+++ edk2/EdkModulePkg/Library/EdkFvbServiceLib/Fvb.h
@@ -1,21 +1,13 @@
-/*++
-
-Copyright (c) 2006, Intel Corporation                                                         
+/**
+  The internal header file for EdkFvbServiceLib.
+  
+Copyright (c) 2006 Intel Corporation. <BR>
 All rights reserved. This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
 http://opensource.org/licenses/bsd-license.php                                            
                                                                                           
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
-
-Module Name:
-
-  Fvb.h
-
-Abstract:
-
---*/
+**/
 
 #ifndef __FVB_H__
 #define __FVB_H__
@@ -26,6 +18,7 @@ typedef struct {
   EFI_HANDLE                          Handle;
   EFI_FIRMWARE_VOLUME_BLOCK_PROTOCOL  *Fvb;
   EFI_FVB_EXTENSION_PROTOCOL          *FvbExtension;
+  BOOLEAN                             IsRuntimeAccess;
 } FVB_ENTRY;
 
 #endif
