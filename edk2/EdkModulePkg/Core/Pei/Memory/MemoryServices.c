3b7d56d1070c0c7e228c3506460d16f1656378ce
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2006-12-26 10:00:18 +0000

    Add debug info when decompress failed in DxeIpl and allocate pages failed in PeiMain.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@2140 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/EdkModulePkg/Core/Pei/Memory/MemoryServices.c
+++ edk2/EdkModulePkg/Core/Pei/Memory/MemoryServices.c
@@ -243,6 +243,9 @@ Returns:
   //
   if (Hob.HandoffInformationTable->EfiFreeMemoryTop - ((Pages * EFI_PAGE_SIZE) + sizeof (EFI_HOB_MEMORY_ALLOCATION)) < 
       Hob.HandoffInformationTable->EfiFreeMemoryBottom) {
+    DEBUG ((EFI_D_ERROR, "AllocatePages failed: No 0x%x Pages is available.\n", Pages));
+    DEBUG ((EFI_D_ERROR, "There is only left 0x%x pages memory resource to be allocated.\n", \
+    (Hob.HandoffInformationTable->EfiFreeMemoryTop - Hob.HandoffInformationTable->EfiFreeMemoryBottom)/EFI_PAGE_SIZE));
     return  EFI_OUT_OF_RESOURCES;
   } else {
     //
