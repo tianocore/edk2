d57d21b5ba5a044778a2b1b4cf7b1762eaf68992
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2007-01-18 06:48:58 +0000

    Updated the copyright year.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2265 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkModulePkg/Universal/PCD/Dxe/Service.c
+++ EdkModulePkg/Universal/PCD/Dxe/Service.c
@@ -1,7 +1,7 @@
 /** @file
 Private functions used by PCD DXE driver.
 
-Copyright (c) 2006, Intel Corporation
+Copyright (c) 2006 - 2007, Intel Corporation
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
