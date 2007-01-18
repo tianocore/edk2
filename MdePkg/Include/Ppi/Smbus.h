76dbeb3b910133575ad03583f79dcd9443af06f5
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2007-01-18 09:15:52 +0000

    1) Clean up MdePkg/Include/Common/BootScript.h and remove boot script definition which is not defined in Framework Boot Scirpt Specification V0.91. 2) Add MdePkg/Include/IndustryStandard/SmBus.h which hosts the standard definition defined in System Management Bus Spefication V2.0.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2266 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Ppi/Smbus.h
+++ MdePkg/Include/Ppi/Smbus.h
@@ -1,7 +1,7 @@
 /** @file
   This file declares Smbus PPI.
 
-  Copyright (c) 2006, Intel Corporation                                                         
+  Copyright (c) 2006 - 2007, Intel Corporation                                                         
   All rights reserved. This program and the accompanying materials                          
   are licensed and made available under the terms and conditions of the BSD License         
   which accompanies this distribution.  The full text of the license may be found at        
@@ -20,6 +20,8 @@
 
 #ifndef _PEI_SMBUS_PPI_H
 #define _PEI_SMBUS_PPI_H
+
+#include <IndustryStandard/SmBus.h>
 
 #define EFI_PEI_SMBUS_PPI_GUID \
   { \
