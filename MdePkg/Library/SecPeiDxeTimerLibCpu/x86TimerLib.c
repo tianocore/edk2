4faa50287ee70f2140ab81efbacfb0bb0ac891b8
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2007-02-02 05:36:42 +0000

    1. removed BaseTimerLibLocalApic from MdePkg and Nt32TimerLib from EdkNt32Pkg. 2. added BaseTimerlibNullTemplate in MdePkg and SecPeiDxeTimerLibCpu in MdePkg. 3. modified the related spd and fpd files
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2347 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/SecPeiDxeTimerLibCpu/x86TimerLib.c
+++ MdePkg/Library/SecPeiDxeTimerLibCpu/x86TimerLib.c
@@ -5,7 +5,7 @@
   the IA32_APIC_BASE MSR, the offsets of InitialCount, CorrentCount
   and DivideConfiguration.
 
-  Copyright (c) 2006, Intel Corporation<BR>
+  Copyright (c) 2006 - 2007, Intel Corporation<BR>
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
