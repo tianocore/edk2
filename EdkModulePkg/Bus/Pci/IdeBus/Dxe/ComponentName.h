8ba8cb28e8b79708708e0f95f164c0afded46e46
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2007-03-05 03:41:22 +0000

    Removed EFI_SIZE_REDUCTION_APPLIED and SIZE_REDUCTION_ISA_COMBINED because one PCD PcdComponentNameDisable has been introduced.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2416 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkModulePkg/Bus/Pci/IdeBus/Dxe/ComponentName.h
+++ EdkModulePkg/Bus/Pci/IdeBus/Dxe/ComponentName.h
@@ -1,30 +1,21 @@
 /** @file
-  Copyright (c) 2006, Intel Corporation                                                         
-  All rights reserved. This program and the accompanying materials                          
-  are licensed and made available under the terms and conditions of the BSD License         
-  which accompanies this distribution.  The full text of the license may be found at        
-  http://opensource.org/licenses/bsd-license.php                                            
+  Copyright (c) 2006, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
 
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
 #ifndef _IDE_BUS_COMPONENT_NAME_H
 #define _IDE_BUS_COMPONENT_NAME_H
 
-
-#ifndef EFI_SIZE_REDUCTION_APPLIED
-
 #define ADD_NAME(x) AddName ((x));
 
 extern EFI_COMPONENT_NAME_PROTOCOL  gIDEBusComponentName;
-
-#else
-
-#define ADD_NAME(x)
-
-#endif
 
 
 //
