1c2af4476a469e876b263c3963dd5b03a2d1e29b
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2007-03-05 03:41:22 +0000

    Removed EFI_SIZE_REDUCTION_APPLIED and SIZE_REDUCTION_ISA_COMBINED because one PCD PcdComponentNameDisable has been introduced.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@2416 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/EdkModulePkg/Bus/Pci/PciBus/Dxe/ComponentName.h
+++ edk2/EdkModulePkg/Bus/Pci/PciBus/Dxe/ComponentName.h
@@ -1,11 +1,11 @@
 /*++
 
-Copyright (c) 2006, Intel Corporation                                                         
-All rights reserved. This program and the accompanying materials                          
-are licensed and made available under the terms and conditions of the BSD License         
-which accompanies this distribution.  The full text of the license may be found at        
-http://opensource.org/licenses/bsd-license.php                                            
-                                                                                          
+Copyright (c) 2006, Intel Corporation
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
 THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
 WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
 
@@ -23,11 +23,7 @@ Revision History
 #ifndef _EFI_PCI_BUS_COMPONENT_NAME_H
 #define _EFI_PCI_BUS_COMPONENT_NAME_H
 
-#ifndef EFI_SIZE_REDUCTION_APPLIED
-
 extern EFI_COMPONENT_NAME_PROTOCOL  gPciBusComponentName;
-
-#endif
 
 //
 // EFI Component Name Functions
