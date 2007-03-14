675a7b0b3fb896e5fcf4128b4fea52fc7d0811a1
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2007-03-14 02:14:28 +0000

    Remove the BugBug in package header file for DXE_CORE and DXE_DRIVER which force every driver to include MdePkg/Include/Library/PeCoffLib.h.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2458 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkModulePkg/Include/EdkPeiCore.h
+++ EdkModulePkg/Include/EdkPeiCore.h
@@ -1,6 +1,6 @@
 /*++
 
-Copyright (c) 2006, Intel Corporation                                                         
+Copyright (c) 2006 - 2007, Intel Corporation                                                         
 All rights reserved. This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -26,10 +26,6 @@ Abstract:
 #ifndef __EDK_PEI_CORE_H__
 #define __EDK_PEI_CORE_H__
 
-//
-// BUGBUG: We must include this lib here due to ordering issues
-//
-#include <Library/PeCoffLib.h>
 
 #include <Guid/PeiPeCoffLoader.h>
 
