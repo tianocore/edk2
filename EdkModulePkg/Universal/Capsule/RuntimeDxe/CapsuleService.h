396514f590f2435696f0b591c36da86fa238e280
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2007-04-10 07:10:28 +0000

    removed ICC compile issue.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2553 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkModulePkg/Universal/Capsule/RuntimeDxe/CapsuleService.h
+++ EdkModulePkg/Universal/Capsule/RuntimeDxe/CapsuleService.h
@@ -1,13 +1,13 @@
 /*++
 
-Copyright (c) 2006, Intel Corporation                                                         
-All rights reserved. This program and the accompanying materials                          
-are licensed and made available under the terms and conditions of the BSD License         
-which accompanies this distribution.  The full text of the license may be found at        
-http://opensource.org/licenses/bsd-license.php                                            
-                                                                                          
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+Copyright (c) 2006, Intel Corporation
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 Module Name:
 
@@ -24,12 +24,12 @@ Abstract:
 
 #include <Common/CapsuleName.h>
 
-
+extern EFI_GUID gEfiCapsuleGuid;
 
 EFI_STATUS
 EFIAPI
 UpdateCapsule(
-  IN UEFI_CAPSULE_HEADER     **CapsuleHeaderArray,
+  IN UEFI_CAPSULE_HEADER    **CapsuleHeaderArray,
   IN UINTN                   CapsuleCount,
   IN EFI_PHYSICAL_ADDRESS    ScatterGatherList OPTIONAL
   );
