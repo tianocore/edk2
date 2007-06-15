79964ac84ea0ca6c68d0dea38245fa83ff1945d1
bxing <bxing@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-15 10:02:42 +0000

    Initial directory structure of IntelFrameworkPkg.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2657 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ IntelFrameworkPkg/Include/FrameworkSmm.h
@@ -0,0 +1,26 @@
+/** @file
+  Header file that supports Framework extension to UEFI/PI for DXE modules.
+
+  This header file must include Framework extension definitions common to DXE
+  modules.
+
+  Copyright (c) 2007, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+  Module Name:    FrameworkSmm.h
+
+**/
+
+#ifndef _FRAMEWORK_SMM_H_
+#define _FRAMEWORK_SMM_H_
+
+#include <FrameworkDxe.h>
+#include <Common/FrameworkSmmCis.h>
+
+#endif
---
+++ IntelFrameworkPkg/Library/UefiLibFramework/UefiLibFramework.h
@@ -0,0 +1,31 @@
+/** @file
+  Header file to include header files common to all source files in
+  UefiLibFramework.
+
+  Copyright (c) 2007, Intel Corporation<BR>
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+  Module Name:  UefiLibFramework.h
+
+**/
+
+#ifndef _UEFI_LIB_FRAMEWORK_H_
+#define _UEFI_LIB_FRAMEWORK_H_
+
+#include <FrameworkDxe.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/BaseLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/PcdLib.h>
+#include <Library/PrintLib.h>
+#include <Library/UefiLib.h>
+
+#endif
