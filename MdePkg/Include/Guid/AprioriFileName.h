c2f83f0ae172410cbd6d2ebf73dd776a0c2cd6e0
yshang1 <yshang1@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-19 10:29:07 +0000

    Checked in the GUIDs introduced in UEFI/PI.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2675 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ MdePkg/Include/Guid/AprioriFileName.h
@@ -0,0 +1,56 @@
+/** @file
+
+  The GUID PEI_APRIORI_FILE_NAME_GUID definition is the file
+  name of the PEI a priori file that is stored in a firmware
+  volume.
+
+  Copyright (c) 2006 - 2007, Intel Corporation                                                         
+  All rights reserved. This program and the accompanying materials                          
+  are licensed and made available under the terms and conditions of the BSD License         
+  which accompanies this distribution.  The full text of the license may be found at        
+  http://opensource.org/licenses/bsd-license.php                                            
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+
+  Module Name:  AprioriFileName.h
+
+  @par Revision Reference: PI
+  Version 1.00.
+
+**/
+
+#ifndef __PEI_APRIORI_FILE_NAME_H__
+#define __PEI_APRIORI_FILE_NAME_H__
+
+#define PEI_APRIORI_FILE_NAME_GUID \
+  { 0x1b45cc0a, 0x156a, 0x428a, { 0x62, 0XAF, 0x49, 0x86, 0x4d, 0xa0, 0xe6, 0xe6 } }
+
+
+/**
+   
+  This file must be of type EFI_FV_FILETYPE_FREEFORM and must
+  contain a single section of type EFI_SECTION_RAW. For details on
+  firmware volumes, firmware file types, and firmware file section
+  types.
+
+  @param FileNamesWithinVolume[]  An array of zero or more
+                                  EFI_GUID type entries that
+                                  match the file names of PEIM
+                                  modules in the same Firmware
+                                  Volume. The maximum number of
+                                  entries.
+
+  @param NumberOfModulesInVolume  Determined by the number of
+                                  modules in the FV.
+  
+**/
+typedef struct {
+  EFI_GUID  FileNamesWithinVolume[1];
+// Optional list of file-names
+} PEI_APRIORI_FILE_CONTENTS;
+
+extern EFI_GUID gPeiAprioriFileNameGuid;
+
+#endif
+
---
+++ MdePkg/Include/Guid/SmBios.h
@@ -0,0 +1,36 @@
+/** @file
+  GUIDs used to locate the SMBIOS tables in the EFI 1.0 system table.
+
+  This GUID in the system table is the only legal way to search for and 
+  locate the SMBIOS tables. Do not search the 0xF0000 segment to find SMBIOS
+  tables.
+
+  Copyright (c) 2006, Intel Corporation                                                         
+  All rights reserved. This program and the accompanying materials                          
+  are licensed and made available under the terms and conditions of the BSD License         
+  which accompanies this distribution.  The full text of the license may be found at        
+  http://opensource.org/licenses/bsd-license.php                                            
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+
+  Module Name:  SmBios.h
+
+  @par Revision Reference:
+  GUIDs defined in UEFI 2.0 spec.
+
+**/
+
+#ifndef __SMBIOS_GUID_H__
+#define __SMBIOS_GUID_H__
+
+#define EFI_SMBIOS_TABLE_GUID \
+  { \
+    0xeb9d2d31, 0x2d88, 0x11d3, {0x9a, 0x16, 0x0, 0x90, 0x27, 0x3f, 0xc1, 0x4d } \
+  }
+
+#define SMBIOS_TABLE_GUID EFI_SMBIOS_TABLE_GUID
+
+extern EFI_GUID       gEfiSmbiosTableGuid;
+
+#endif
