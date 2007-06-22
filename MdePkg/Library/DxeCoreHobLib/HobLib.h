738ec5655985bd75d2eb15cc161a77b46f4abeac
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-22 07:42:13 +0000

    Import DxeCoreHobLib, DxeHobLib, PeimEntryPoint and PeiServicesLib.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2714 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ MdePkg/Library/DxeCoreHobLib/HobLib.h
@@ -0,0 +1,27 @@
+/** @file
+  Internal include file of DXE Entry Point HOB Library.
+
+  Copyright (c) 2006, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+  Module Name:  HobLib.h
+
+**/
+
+#ifndef __DXE_ENTRY_POINT_HOB_LIB_H__
+#define __DXE_ENTRY_POINT_HOB_LIB_H__
+
+//
+// Include common header file for this module.
+//
+#include "CommonHeader.h"
+
+extern VOID *gHobList;
+
+#endif
