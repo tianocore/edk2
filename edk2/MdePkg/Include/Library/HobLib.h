f080d895841a46e3277b0a8abc0b38bad926b37d
yshang1 <yshang1@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-22 02:18:24 +0000

    Fixed typo in PiPeiCis.h.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@2702 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/Library/HobLib.h
+++ edk2/MdePkg/Include/Library/HobLib.h
@@ -368,4 +368,15 @@ BuildMemoryAllocationHob (
   )
 ;
 
+#define GET_HOB_TYPE(Hob)     ((Hob).Header->HobType)
+#define GET_HOB_LENGTH(Hob)   ((Hob).Header->HobLength)
+#define GET_NEXT_HOB(Hob)     ((Hob).Raw + GET_HOB_LENGTH (Hob))
+#define END_OF_HOB_LIST(Hob)  (GET_HOB_TYPE (Hob) == EFI_HOB_TYPE_END_OF_HOB_LIST)
+
+//
+// Get the data and data size field of GUID 
+//
+#define GET_GUID_HOB_DATA(GuidHob)      ((VOID *) (((UINT8 *) &((GuidHob)->Name)) + sizeof (EFI_GUID)))
+#define GET_GUID_HOB_DATA_SIZE(GuidHob) (((GuidHob)->Header).HobLength - sizeof (EFI_HOB_GUID_TYPE))
+
 #endif
