1f4e3329cd49add50a4b3055184b14b27982e9bd
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-26 08:03:14 +0000

    1. added common header files, Guid header filess, Protocol header files, PPI header files. 2. added Guids/Protocols/PPIs/Pcds definitions in .nspd, .dec files.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@2744 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ edk2/MdeModulePkg/Include/Guid/GenericPlatformVariable.h
@@ -0,0 +1,27 @@
+/*++
+
+Copyright (c) 2006 - 2007, Intel Corporation
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+Module Name:
+
+  GenericPlatformVariable.h
+
+Abstract:
+
+  The variable space Guid to pair with a Unicode string name to tag an EFI variable.
+
+--*/
+
+#ifndef __GENERIC_PLATFORM_VARIABLE_H__
+#define __GENERIC_PLATFORM_VARIABLE_H__
+
+extern EFI_GUID gEfiGenericPlatformVariableGuid;
+
+#endif
