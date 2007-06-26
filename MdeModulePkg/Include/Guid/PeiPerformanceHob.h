88f2012778d7a6d6f6d68789d90cc8cd214e6c4a
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-26 08:03:14 +0000

    1. added common header files, Guid header filess, Protocol header files, PPI header files. 2. added Guids/Protocols/PPIs/Pcds definitions in .nspd, .dec files.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2744 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ MdeModulePkg/Include/Guid/PeiPerformanceHob.h
@@ -0,0 +1,56 @@
+/*++
+
+Copyright (c) 2006, Intel Corporation                                                         
+All rights reserved. This program and the accompanying materials                          
+are licensed and made available under the terms and conditions of the BSD License         
+which accompanies this distribution.  The full text of the license may be found at        
+http://opensource.org/licenses/bsd-license.php                                            
+                                                                                          
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+
+Module Name:
+
+  PeiPerformanceHob.h
+    
+Abstract:
+
+  GUIDs used for PEI Performance HOB data structures
+
+--*/
+
+#ifndef __PEI_PERFORMANCE_HOB_H__
+#define __PEI_PERFORMANCE_HOB_H__
+
+//
+// This is the GUID of PEI performance HOB
+//
+#define PEI_PERFORMANCE_HOB_GUID \
+  { 0xec4df5af, 0x4395, 0x4cc9, { 0x94, 0xde, 0x77, 0x50, 0x6d, 0x12, 0xc7, 0xb8 } }
+
+//
+// PEI_PERFORMANCE_STRING_SIZE must be a multiple of 8.
+//
+#define PEI_PERFORMANCE_STRING_SIZE     8
+#define PEI_PERFORMANCE_STRING_LENGTH   (PEI_PERFORMANCE_STRING_SIZE - 1)
+
+typedef struct {
+  EFI_PHYSICAL_ADDRESS  Handle;
+  CHAR8                 Token[PEI_PERFORMANCE_STRING_SIZE];
+  CHAR8                 Module[PEI_PERFORMANCE_STRING_SIZE];
+  UINT64                StartTimeStamp;
+  UINT64                EndTimeStamp;
+} PEI_PERFORMANCE_LOG_ENTRY;
+
+//
+// The header must be aligned at 8 bytes.
+// 
+typedef struct {
+  UINT32                             NumberOfEntries;
+  UINT32                             Reserved;
+} PEI_PERFORMANCE_LOG_HEADER;
+
+
+extern EFI_GUID gPeiPerformanceHobGuid;
+
+#endif
---
+++ MdeModulePkg/Include/Guid/PrimaryStandardErrorDevice.h
@@ -0,0 +1,28 @@
+/*++
+
+Copyright (c) 2006, Intel Corporation
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+Module Name:
+
+  PrimaryStandardErrorDevice.h
+
+Abstract:
+
+--*/
+
+#ifndef __PRIMARY_STANDARD_ERROR_DEVICE_H__
+#define __PRIMARY_STANDARD_ERROR_DEVICE_H__
+
+#define EFI_PRIMARY_STANDARD_ERROR_DEVICE_GUID    \
+  { 0x5a68191b, 0x9b97, 0x4752, {0x99, 0x46, 0xe3, 0x6a, 0x5d, 0xa9, 0x42, 0xb1 } }
+
+extern EFI_GUID gEfiPrimaryStandardErrorDeviceGuid;
+
+#endif
