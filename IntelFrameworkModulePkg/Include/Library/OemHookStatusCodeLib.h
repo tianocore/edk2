a9102619d488c92947605380f00c10e4651f4dc6
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-26 08:14:44 +0000

    1. Create Intel Framework Module Package directory 2. Complete the public inclusion files
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2746 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ IntelFrameworkModulePkg/Include/Library/OemHookStatusCodeLib.h
@@ -0,0 +1,73 @@
+
+/** @file
+  OEM hook status code library functions with no library constructor/destructor
+
+  Copyright (c) 2006, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+  Module Name:  OemHookStatusCodeLib.h
+
+**/
+
+#ifndef __OEM_HOOK_STATUSCODE_LIB__
+#define __OEM_HOOK_STATUSCODE_LIB__
+
+/**
+
+  Initialize OEM status code device .
+
+
+  @return    Status of initialization of OEM status code device.
+ 
+**/
+EFI_STATUS
+EFIAPI
+OemHookStatusCodeInitialize (
+  VOID
+  );
+
+/**
+  Report status code to OEM device.
+ 
+  @param  CodeType      Indicates the type of status code being reported.  Type EFI_STATUS_CODE_TYPE is defined in "Related Definitions" below.
+ 
+  @param  Value         Describes the current status of a hardware or software entity.  
+                        This included information about the class and subclass that is used to classify the entity 
+                        as well as an operation.  For progress codes, the operation is the current activity. 
+                        For error codes, it is the exception.  For debug codes, it is not defined at this time. 
+                        Type EFI_STATUS_CODE_VALUE is defined in "Related Definitions" below.  
+                        Specific values are discussed in the Intel? Platform Innovation Framework for EFI Status Code Specification.
+ 
+  @param  Instance      The enumeration of a hardware or software entity within the system.  
+                        A system may contain multiple entities that match a class/subclass pairing. 
+                        The instance differentiates between them.  An instance of 0 indicates that instance information is unavailable, 
+                        not meaningful, or not relevant.  Valid instance numbers start with 1.
+
+
+  @param  CallerId      This optional parameter may be used to identify the caller. 
+                        This parameter allows the status code driver to apply different rules to different callers. 
+                        Type EFI_GUID is defined in InstallProtocolInterface() in the EFI 1.10 Specification.
+
+
+  @param  Data          This optional parameter may be used to pass additional data
+ 
+  @return               The function always return EFI_SUCCESS.
+
+**/
+EFI_STATUS
+EFIAPI
+OemHookStatusCodeReport (
+  IN EFI_STATUS_CODE_TYPE     CodeType,
+  IN EFI_STATUS_CODE_VALUE    Value,
+  IN UINT32                   Instance,
+  IN EFI_GUID                 *CallerId, OPTIONAL
+  IN EFI_STATUS_CODE_DATA     *Data      OPTIONAL
+  );
+
+#endif
