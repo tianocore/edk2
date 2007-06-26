45a20a7fd8b12de926a87f6d7b121287354d63af
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-26 08:25:56 +0000

    Initialize the include folder for NT32Pkg
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2749 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ Nt32Pkg/Include/Ppi/NtAutoscan.h
@@ -0,0 +1,66 @@
+/*++
+
+Copyright (c) 2006, Intel Corporation                                                         
+All rights reserved. This program and the accompanying materials                          
+are licensed and made available under the terms and conditions of the BSD License         
+which accompanies this distribution.  The full text of the license may be found at        
+http://opensource.org/licenses/bsd-license.php                                            
+                                                                                          
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+
+Module Name:
+
+  NtAutoscan.h
+
+Abstract:
+
+Nt Autoscan PPI
+
+--*/
+
+#ifndef __NT_PEI_AUTOSCAN_H__
+#define __NT_PEI_AUTOSCAN_H__
+
+#include <WinNtDxe.h>
+
+#define PEI_NT_AUTOSCAN_PPI_GUID \
+  { \
+    0xdce384d, 0x7c, 0x4ba5, {0x94, 0xbd, 0xf, 0x6e, 0xb6, 0x4d, 0x2a, 0xa9 } \
+  }
+
+typedef
+EFI_STATUS
+(EFIAPI *PEI_NT_AUTOSCAN) (
+  IN  UINTN                 Index,
+  OUT EFI_PHYSICAL_ADDRESS  * MemoryBase,
+  OUT UINT64                *MemorySize
+  );
+
+/*++
+
+Routine Description:
+  This service is called from Index == 0 until it returns EFI_UNSUPPORTED.
+  It allows discontiguous memory regions to be supported by the emulator.
+  It uses gSystemMemory[] and gSystemMemoryCount that were created by
+  parsing the Windows environment variable EFI_MEMORY_SIZE.
+  The size comes from the varaible and the address comes from the call to
+  WinNtOpenFile. 
+
+Arguments:
+  Index      - Which memory region to use
+  MemoryBase - Return Base address of memory region
+  MemorySize - Return size in bytes of the memory region
+
+Returns:
+  EFI_SUCCESS - If memory region was mapped
+  EFI_UNSUPPORTED - If Index is not supported
+
+--*/
+typedef struct {
+  PEI_NT_AUTOSCAN NtAutoScan;
+} PEI_NT_AUTOSCAN_PPI;
+
+extern EFI_GUID gPeiNtAutoScanPpiGuid;
+
+#endif
---
+++ Nt32Pkg/Include/WinNtPeim.h
@@ -0,0 +1,31 @@
+/*++
+
+Copyright (c) 2006, Intel Corporation                                                         
+All rights reserved. This program and the accompanying materials                          
+are licensed and made available under the terms and conditions of the BSD License         
+which accompanies this distribution.  The full text of the license may be found at        
+http://opensource.org/licenses/bsd-license.php                                            
+                                                                                          
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+
+Module Name:
+  WinNtLib.h
+
+Abstract:
+  Public include file for the WinNt Library
+
+--*/
+
+#ifndef __WIN_NT_PEIM_H__
+#define __WIN_NT_PEIM_H__
+
+//
+// This forces Windows.h WIN32 include file to be included
+//  it's needed for WinNtThunk.h
+//
+#include <Common/WinNtInclude.h>
+
+#include <Protocol/WinNtThunk.h>
+
+#endif
