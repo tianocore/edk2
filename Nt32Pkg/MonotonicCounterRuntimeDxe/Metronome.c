8a4a9d2125b28e003f0492a6450269d3b5f1578a
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-27 08:43:57 +0000

    add in Metronome.inf
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2797 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ Nt32Pkg/MonotonicCounterRuntimeDxe/Metronome.c
@@ -0,0 +1,134 @@
+/*++
+
+Copyright (c) 2006, Intel Corporation
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+Module Name:
+
+  Metronome.c
+
+Abstract:
+
+  NT Emulation Metronome Architectural Protocol Driver as defined in DXE CIS
+
+--*/
+
+//
+// Include common header file for this module.
+//
+#include "CommonHeader.h"
+
+#include "Metronome.h"
+
+//
+// Global Variables
+//
+EFI_METRONOME_ARCH_PROTOCOL mMetronome = {
+  WinNtMetronomeDriverWaitForTick,
+  TICK_PERIOD
+};
+
+//
+// Worker Functions
+//
+
+EFI_STATUS
+EFIAPI
+WinNtMetronomeDriverWaitForTick (
+  IN EFI_METRONOME_ARCH_PROTOCOL  *This,
+  IN UINT32                       TickNumber
+  )
+/*++
+
+Routine Description:
+
+  The WaitForTick() function waits for the number of ticks specified by
+  TickNumber from a known time source in the platform.  If TickNumber of
+  ticks are detected, then EFI_SUCCESS is returned.  The actual time passed
+  between entry of this function and the first tick is between 0 and
+  TickPeriod 100 nS units.  If you want to guarantee that at least TickPeriod
+  time has elapsed, wait for two ticks.  This function waits for a hardware
+  event to determine when a tick occurs.  It is possible for interrupt
+  processing, or exception processing to interrupt the execution of the
+  WaitForTick() function.  Depending on the hardware source for the ticks, it
+  is possible for a tick to be missed.  This function cannot guarantee that
+  ticks will not be missed.  If a timeout occurs waiting for the specified
+  number of ticks, then EFI_TIMEOUT is returned.
+
+Arguments:
+
+  This       - The EFI_METRONOME_ARCH_PROTOCOL instance.
+  TickNumber - Number of ticks to wait.
+
+Returns:
+
+  EFI_SUCCESS - The wait for the number of ticks specified by TickNumber
+                succeeded.
+
+--*/
+{
+  UINT64  SleepTime;
+
+  //
+  // Calculate the time to sleep.  Win API smallest unit to sleep is 1 millisec
+  // Tick Period is in 100ns units, divide by 10000 to convert to ms
+  //
+  SleepTime = DivU64x32 (MultU64x32 ((UINT64) TickNumber, TICK_PERIOD) + 9999, 10000);
+  gWinNt->Sleep ((UINT32) SleepTime);
+
+  return EFI_SUCCESS;
+}
+
+
+EFI_STATUS
+EFIAPI
+WinNtMetronomeDriverInitialize (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+/*++
+
+Routine Description:
+
+  Initialize the Metronome Architectural Protocol driver
+
+Arguments:
+
+  ImageHandle - ImageHandle of the loaded driver
+
+
+  SystemTable - Pointer to the System Table
+
+Returns:
+
+  EFI_SUCCESS           - Metronome Architectural Protocol created
+
+  EFI_OUT_OF_RESOURCES  - Not enough resources available to initialize driver.
+
+  EFI_DEVICE_ERROR      - A device error occured attempting to initialize the driver.
+
+--*/
+{
+  EFI_STATUS  Status;
+  EFI_HANDLE  Handle;
+
+
+  //
+  // Install the Metronome Architectural Protocol onto a new handle
+  //
+  Handle = NULL;
+  Status = gBS->InstallProtocolInterface (
+                  &Handle,
+                  &gEfiMetronomeArchProtocolGuid,
+                  EFI_NATIVE_INTERFACE,
+                  &mMetronome
+                  );
+
+  return Status;
+}
---
+++ Nt32Pkg/MonotonicCounterRuntimeDxe/Metronome.h
@@ -0,0 +1,89 @@
+/*++
+
+Copyright (c) 2006, Intel Corporation                                                         
+All rights reserved. This program and the accompanying materials                          
+are licensed and made available under the terms and conditions of the BSD License         
+which accompanies this distribution.  The full text of the license may be found at        
+http://opensource.org/licenses/bsd-license.php                                            
+                                                                                          
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+
+Module Name:
+
+  Metronome.h
+
+Abstract:
+
+  NT Emulation Metronome Architectural Protocol Driver as defined in DXE CIS
+
+--*/
+
+#ifndef _NT_THUNK_METRONOME_H_
+#define _NT_THUNK_METRONOME_H_
+
+
+
+//
+// Include common header file for this module.
+//
+#include "CommonHeader.h"
+
+//
+// Period of on tick in 100 nanosecond units
+//
+#define TICK_PERIOD 2000
+
+//
+// Function Prototypes
+//
+
+EFI_STATUS
+EFIAPI
+WinNtMetronomeDriverInitialize (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+/*++
+
+Routine Description:
+
+  TODO: Add function description
+
+Arguments:
+
+  ImageHandle - TODO: add argument description
+  SystemTable - TODO: add argument description
+
+Returns:
+
+  TODO: add return values
+
+--*/
+;
+
+EFI_STATUS
+EFIAPI
+WinNtMetronomeDriverWaitForTick (
+  IN EFI_METRONOME_ARCH_PROTOCOL  *This,
+  IN UINT32                       TickNumber
+  )
+/*++
+
+Routine Description:
+
+  TODO: Add function description
+
+Arguments:
+
+  This        - TODO: add argument description
+  TickNumber  - TODO: add argument description
+
+Returns:
+
+  TODO: add return values
+
+--*/
+;
+
+#endif
