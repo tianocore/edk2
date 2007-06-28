5708838bf07d0115d7f894eaca2c81c0d6e47b3f
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-28 09:00:08 +0000

    Add variablePei module into MdeModulePkg.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@2838 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ edk2/MdeModulePkg/Universal/VariablePei/Ia32/VarMachine.h
@@ -0,0 +1,19 @@
+/**@file
+  Variable Machine Type
+
+Copyright (c) 2006 - 2007 Intel Corporation. <BR>
+All rights reserved. This program and the accompanying materials                          
+are licensed and made available under the terms and conditions of the BSD License         
+which accompanies this distribution.  The full text of the license may be found at        
+http://opensource.org/licenses/bsd-license.php                                            
+                                                                                          
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+
+**/
+
+#ifndef _VAR_MACHINE_H
+#define _VAR_MACHINE_H
+
+
+#endif
