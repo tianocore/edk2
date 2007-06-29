3942e72845a5a99cb9b4e9642b00f9c31e5d5f77
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-29 08:09:31 +0000

    Add Crc32SectionExtract Module
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@2902 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Universal/FirmwareVolume/GuidedSectionExtraction/Crc32SectionExtract/Dxe/Crc32SectionExtract.h
+++ edk2/MdeModulePkg/Universal/FirmwareVolume/GuidedSectionExtraction/Crc32SectionExtract/Dxe/Crc32SectionExtract.h
@@ -27,6 +27,7 @@ Abstract:
 // The package level header files this module uses
 //
 #include <PiDxe.h>
+#include <Common/FrameworkFirmwareVolumeImageFormat.h>
 //
 // The protocols, PPI and GUID defintions for this module
 //
