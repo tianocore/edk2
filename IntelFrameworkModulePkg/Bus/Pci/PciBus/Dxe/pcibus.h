1d6d9e7ef0c3c79191f10826f984c11c3f902207
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-29 09:23:03 +0000

    Use the "PAY_LOAD" StatusCode definitions defined in IntelFrameworkPkg.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2913 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkModulePkg/Bus/Pci/PciBus/Dxe/pcibus.h
+++ IntelFrameworkModulePkg/Bus/Pci/PciBus/Dxe/pcibus.h
@@ -66,28 +66,6 @@ Revision History
 #include <IndustryStandard/Acpi.h>
 #include "ComponentName.h"
 
-///
-/// Device handle Extended Data. Used for many
-/// errors and progress codes to point to the device.
-///
-typedef struct {
-  EFI_HANDLE            Handle;
-} REPORT_STATUS_CODE_LIBRARY_DEVICE_HANDLE_EXTENDED_DATA;
-
-///
-/// Resource Allocation Failure Extended Error Data
-///
-typedef struct {
-  UINT32                             Bar;
-  UINT16                             DevicePathSize;
-  UINT16                             ReqResSize;
-  UINT16                             AllocResSize;
-  UINT8                              *DevicePath;
-  UINT8                              *ReqRes;
-  UINT8                              *AllocRes;
-} REPORT_STATUS_CODE_LIBRARY_RESOURCE_ALLOC_FAILURE_ERROR_DATA;
-
-
 //
 // Driver Produced Protocol Prototypes
 //
