c9909cfe2f745baf0b4e970f6d111f16ead5530b
yshang1 <yshang1@6f19259b-4bc3-4df7-8a09-765794883524>
2007-06-29 07:30:27 +0000

    Add EFI Option ROM definitions.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@2897 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/IndustryStandard/Pci22.h
+++ edk2/MdePkg/Include/IndustryStandard/Pci22.h
@@ -535,6 +535,40 @@ typedef struct {
 #define PCI_BAR_IDX5        0x05
 #define PCI_BAR_ALL         0xFF
 
+//
+// EFI PCI Option ROM definitions
+// 
+
+#define EFI_ROOT_BRIDGE_LIST                            'eprb'
+#define EFI_PCI_EXPANSION_ROM_HEADER_EFISIGNATURE       0x0EF1
+
+typedef struct {
+  UINT8 Register;
+  UINT8 Function;
+  UINT8 Device;
+  UINT8 Bus;
+  UINT8 Reserved[4];
+} DEFIO_PCI_ADDR;
+
+typedef struct {
+  UINT16  Signature;    // 0xaa55
+  UINT16  InitializationSize;
+  UINT32  EfiSignature; // 0x0EF1
+  UINT16  EfiSubsystem;
+  UINT16  EfiMachineType;
+  UINT16  CompressionType;
+  UINT8   Reserved[8];
+  UINT16  EfiImageHeaderOffset;
+  UINT16  PcirOffset;
+} EFI_PCI_EXPANSION_ROM_HEADER;
+
+typedef union {
+  UINT8                           *Raw;
+  PCI_EXPANSION_ROM_HEADER        *Generic;
+  EFI_PCI_EXPANSION_ROM_HEADER    *Efi;
+  EFI_LEGACY_EXPANSION_ROM_HEADER *PcAt;
+} EFI_PCI_ROM_HEADER;
+
 #pragma pack(pop)
 
 #endif
