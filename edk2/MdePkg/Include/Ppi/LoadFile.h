5f8aa24e550426a03f90004e9bafe950714cf90b
yshang1 <yshang1@6f19259b-4bc3-4df7-8a09-765794883524>
2007-07-03 02:14:17 +0000

    Update EFI_PEI_LOAD_FILE_PPI_GUID in MdePkg.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@2971 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/Ppi/LoadFile.h
+++ edk2/MdePkg/Include/Ppi/LoadFile.h
@@ -15,8 +15,8 @@
 
 **/
 
-#ifndef __FV_FILE_LOADER_PPI_H__
-#define __FV_FILE_LOADER_PPI_H__
+#ifndef __LOAD_FILE_PPI_H__
+#define __LOAD_FILE_PPI_H__
 
 #define EFI_PEI_LOAD_FILE_PPI_GUID \
   { 0xb9e0abfe, 0x5979, 0x4914, { 0x97, 0x7f, 0x6d, 0xee, 0x78, 0xc2, 0x78, 0xa6 } }
@@ -93,55 +93,6 @@ struct _EFI_PEI_LOAD_FILE_PPI {
   EFI_PEI_LOAD_FILE LoadFile;
 };
 
-
-
-#define EFI_PEI_FV_FILE_LOADER_GUID \
-  { \
-    0x7e1f0d85, 0x4ff, 0x4bb2, {0x86, 0x6a, 0x31, 0xa2, 0x99, 0x6a, 0x48, 0xa8 } \
-  }
-
-typedef struct _EFI_PEI_FV_FILE_LOADER_PPI  EFI_PEI_FV_FILE_LOADER_PPI;
-
-/**
-  Loads a PEIM into memory for subsequent execution.
-
-  @param  This           Interface pointer that implements the Load File PPI instance.
-  @param  FfsHeader      Pointer to the FFS header of the file to load.
-  @param  ImageAddress   Pointer to the address of the loaded Image
-  @param  ImageSize      Pointer to the size of the loaded image.
-  @param  EntryPoint     Pointer to the entry point of the image.
-
-  @retval EFI_SUCCESS           The image was loaded successfully.
-  @retval EFI_OUT_OF_RESOURCES  There was not enough memory.
-  @retval EFI_INVALID_PARAMETER The contents of the FFS file did not
-                                contain a valid PE/COFF image that could be loaded.
-
-**/
-typedef
-EFI_STATUS
-(EFIAPI *EFI_PEI_FV_LOAD_FILE) (
-  IN EFI_PEI_FV_FILE_LOADER_PPI                 *This,
-  IN  EFI_FFS_FILE_HEADER                       *FfsHeader,
-  OUT EFI_PHYSICAL_ADDRESS                      *ImageAddress,
-  OUT UINT64                                    *ImageSize,
-  OUT EFI_PHYSICAL_ADDRESS                      *EntryPoint
-  );
-
-/**
-  @par Ppi Description:
-  This PPI is a pointer to the Load File service. This service will be 
-  published by a PEIM.The PEI Foundation will use this service to 
-  launch the known non-XIP PE/COFF PEIM images.  This service may 
-  depend upon the presence of the EFI_PEI_PERMANENT_MEMORY_INSTALLED_PPI.
-
-  @param FvLoadFile
-  Loads a PEIM into memory for subsequent execution
-
-**/
-struct _EFI_PEI_FV_FILE_LOADER_PPI {
-  EFI_PEI_FV_LOAD_FILE  FvLoadFile;
-};
-
-extern EFI_GUID gEfiPeiFvFileLoaderPpiGuid;
+extern EFI_GUID gEfiPeiLoadFilePpiGuid;
 
 #endif
