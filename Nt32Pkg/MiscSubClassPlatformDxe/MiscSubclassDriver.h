dd356cc4b930e5c8fb1e1a8ebd902428b75edf26
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2007-07-03 06:07:51 +0000

    Enable MiscSubclassDriver.inf in Nt32Pkg
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@2989 6f19259b-4bc3-4df7-8a09-765794883524

--- Nt32Pkg/MiscSubClassPlatformDxe/MiscSubclassDriver.h
+++ Nt32Pkg/MiscSubClassPlatformDxe/MiscSubclassDriver.h
@@ -1,13 +1,13 @@
 /*++
 
-Copyright (c) 2006, Intel Corporation                                                         
-All rights reserved. This program and the accompanying materials                          
-are licensed and made available under the terms and conditions of the BSD License         
-which accompanies this distribution.  The full text of the license may be found at        
-http://opensource.org/licenses/bsd-license.php                                            
-                                                                                          
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+Copyright (c) 2006, Intel Corporation
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 Module Name:
 
@@ -30,16 +30,12 @@ Abstract:
 
 #include <MiscDevicePath.h>
 
-//
-// Autogen string file
-//
-#include <MiscSubclassStrDefs.h>
 
 //
 // Data table entry update function.
 //
-typedef 
-EFI_STATUS 
+typedef
+EFI_STATUS
 (EFIAPI EFI_MISC_SUBCLASS_DATA_FUNCTION) (
   IN UINT16                         RecordType,
   IN UINT32                         *RecordLen,
--- Nt32Pkg/MiscSubClassPlatformDxe/MiscSystemSlotDesignationData.c
+++ Nt32Pkg/MiscSubClassPlatformDxe/MiscSystemSlotDesignationData.c
@@ -1,19 +1,19 @@
 /*++
 
-Copyright (c) 2006, Intel Corporation                                                         
-All rights reserved. This program and the accompanying materials                          
-are licensed and made available under the terms and conditions of the BSD License         
-which accompanies this distribution.  The full text of the license may be found at        
-http://opensource.org/licenses/bsd-license.php                                            
-                                                                                          
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+Copyright (c) 2006, Intel Corporation
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License  
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 Module Name:
-  
+
     MiscSystemSlotDesignationData.c
-  
-Abstract: 
+
+Abstract:
 
   This driver parses the mMiscSubclassDataTable structure and reports
   any generated data to the DataHub.
