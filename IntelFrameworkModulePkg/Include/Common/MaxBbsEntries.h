72d4944464f32248e834d6a9aba8baac0b08eb14
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2007-07-03 13:42:42 +0000

    add MaxBbsEntries.h
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@3016 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ IntelFrameworkModulePkg/Include/Common/MaxBbsEntries.h
@@ -0,0 +1,20 @@
+/** @file
+  Include file for _MAX_BBS_ENTRIES_H.
+
+Copyright (c) 2007, Intel Corporation. All rights reserved. 
+This software and associated documentation (if any) is furnished
+under a license and may only be used or copied in accordance
+with the terms of the license. Except as permitted by such
+license, no part of this software or documentation may be
+reproduced, stored in a retrieval system, or transmitted in any
+form or by any means without the express written consent of
+Intel Corporation.
+
+**/
+
+#ifndef _MAX_BBS_ENTRIES_H
+#define _MAX_BBS_ENTRIES_H
+
+#define MAX_BBS_ENTRIES 0x100
+
+#endif
