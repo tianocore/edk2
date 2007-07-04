0b2f42c25e328a4261177e076521828b3668b174
bxing <bxing@6f19259b-4bc3-4df7-8a09-765794883524>
2007-07-04 12:31:57 +0000

    Fixed FirmwareFileSystem.h to make it compile independently.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@3054 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkPkg/Include/Guid/FirmwareFileSystem.h
+++ IntelFrameworkPkg/Include/Guid/FirmwareFileSystem.h
@@ -1,15 +1,15 @@
 /** @file
-  Guid used to define the Firmware File System.  See the Framework Firmware 
+  Guid used to define the Firmware File System.  See the Framework Firmware
   File System Specification for more details.
 
-  Copyright (c) 2006, Intel Corporation                                                         
-  All rights reserved. This program and the accompanying materials                          
-  are licensed and made available under the terms and conditions of the BSD License         
-  which accompanies this distribution.  The full text of the license may be found at        
-  http://opensource.org/licenses/bsd-license.php                                            
+  Copyright (c) 2006, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
 
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
   Module Name:  FirmwareFileSystem.h
 
@@ -20,6 +20,8 @@
 
 #ifndef __FIRMWARE_FILE_SYSTEM_GUID_H__
 #define __FIRMWARE_FILE_SYSTEM_GUID_H__
+
+#include <PiPei.h>
 
 //
 // GUIDs defined by the FFS specification.
