db6c5cc103afe0575d9662b0a74eabb06ecff536
yshang1 <yshang1@6f19259b-4bc3-4df7-8a09-765794883524>
2007-07-04 10:46:34 +0000

    Add library class PeCoffLoaderLib in MdeModulePkg
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@3044 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ MdeModulePkg/Include/Library/PeCoffLoaderLib.h
@@ -0,0 +1,34 @@
+/*++
+
+Copyright (c) 2006, Intel Corporation                                                         
+All rights reserved. This program and the accompanying materials                          
+are licensed and made available under the terms and conditions of the BSD License         
+which accompanies this distribution.  The full text of the license may be found at        
+http://opensource.org/licenses/bsd-license.php                                            
+                                                                                          
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+
+Module Name:
+
+  EdkPeCoffLoaderLib.h
+
+Abstract:
+  Wrap the Base PE/COFF loader with the PE COFF Protocol
+
+  
+
+--*/
+
+#ifndef __PE_COFF_LOADER_LIB_H_
+#define __PE_COFF_LOADER_LIB_H_
+
+#include <Protocol/PeiPeCoffLoader.h>
+
+EFI_PEI_PE_COFF_LOADER_PROTOCOL*
+EFIAPI
+GetPeCoffLoaderProtocol (
+  VOID
+  );
+
+#endif
