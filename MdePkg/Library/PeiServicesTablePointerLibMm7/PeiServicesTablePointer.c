9c41921b39966d76f483fa4cce7aae55e7516e50
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2007-07-19 02:59:30 +0000

    add in PeiServicesTablePointerLibMm7.inf
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@3343 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ MdePkg/Library/PeiServicesTablePointerLibMm7/PeiServicesTablePointer.c
@@ -0,0 +1,68 @@
+/** @file
+  PEI Services Table Pointer Library.
+
+  Copyright (c) 2006, Intel Corporation<BR>
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+
+  Module Name:  PeiServicesTablePointer.c
+
+**/
+
+
+#include <PiPei.h>
+
+
+#include <Library/PeiServicesTablePointerLib.h>
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+
+/**
+  The function returns the pointer to PeiServices.
+
+  The function returns the pointer to PeiServices.
+  It will ASSERT() if the pointer to PeiServices is NULL.
+
+  @retval  The pointer to PeiServices.
+
+**/
+EFI_PEI_SERVICES **
+EFIAPI
+GetPeiServicesTablePointer (
+  VOID
+  )
+{
+  EFI_PEI_SERVICES  **PeiServices;
+
+  PeiServices = (EFI_PEI_SERVICES **)(UINTN)AsmReadMm7 ();
+  ASSERT (PeiServices != NULL);
+  return PeiServices;
+}
+
+/**
+  The constructor function caches the pointer to PEI services.
+
+  The constructor function caches the pointer to PEI services.
+  It will always return EFI_SUCCESS.
+
+  @param  FfsHeader   Pointer to FFS header the loaded driver.
+  @param  PeiServices Pointer to the PEI services.
+
+  @retval EFI_SUCCESS   The constructor always returns EFI_SUCCESS.
+
+**/
+EFI_STATUS
+EFIAPI
+PeiServicesTablePointerLibConstructor (
+  IN EFI_FFS_FILE_HEADER  *FfsHeader,
+  IN EFI_PEI_SERVICES     **PeiServices
+  )
+{
+  AsmWriteMm7 ((UINT64)(UINTN)PeiServices);
+  return EFI_SUCCESS;
+}
