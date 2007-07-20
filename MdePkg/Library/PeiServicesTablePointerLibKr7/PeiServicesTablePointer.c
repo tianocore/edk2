37f259c9358cbdbcbc9d9ed97a21bef347d7cdbd
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2007-07-20 05:14:34 +0000

    fixed to pass IPF build
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@3384 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/PeiServicesTablePointerLibKr7/PeiServicesTablePointer.c
+++ MdePkg/Library/PeiServicesTablePointerLibKr7/PeiServicesTablePointer.c
@@ -1,20 +1,21 @@
 /** @file
-  PEI Services Table Pointer Library implementation for IPF that uses Kernel 
+  PEI Services Table Pointer Library implementation for IPF that uses Kernel
   Register 7 to store the pointer.
 
   Copyright (c) 2006 - 2007, Intel Corporation.<BR>
-  All rights reserved. This program and the accompanying materials                          
-  are licensed and made available under the terms and conditions of the BSD License         
-  which accompanies this distribution.  The full text of the license may be found at        
-  http://opensource.org/licenses/bsd-license.php                                            
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
 
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
-
+#include <PiPei.h>
 #include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
 
 /**
   The function returns the pointer to PeiServices.
