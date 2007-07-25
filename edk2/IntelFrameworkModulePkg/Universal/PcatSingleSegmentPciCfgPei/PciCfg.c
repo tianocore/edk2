e687e3c40af53264a986656136c4c290300746ec
yshang1 <yshang1@6f19259b-4bc3-4df7-8a09-765794883524>
2007-07-25 11:00:27 +0000

    1) Add PcatSingleSegmentPciCfg2Pei in MdeModulePkg. 2) Add PcatSingleSegmentPciCfgPei in IntelFrameworkModulePkg.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@3442 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ edk2/IntelFrameworkModulePkg/Universal/PcatSingleSegmentPciCfgPei/PciCfg.c
@@ -0,0 +1,174 @@
+/*++
+
+  Copyright (c) 2006 - 2007, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+
+
+Module Name:
+
+    PciCfg.c
+
+Abstract:
+
+    Single Segment Pci Configuration PPI
+
+Revision History
+
+--*/
+
+#include "PciCfgInternal.h"
+
+
+/**
+  PCI read operation.
+
+  @param  PeiServices    An indirect pointer to the PEI Services Table
+                         published by the PEI Foundation.
+  @param  This           Pointer to local data for the interface.
+  @param  Width          The width of the access. Enumerated in bytes.
+  @param  Address        The physical address of the access.
+  @param  Buffer         A pointer to the buffer of data.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_INVALID_PARAMETER   Unsupported width
+                                  enumeration.
+
+**/
+EFI_STATUS
+EFIAPI
+PciCfgRead (
+  IN EFI_PEI_SERVICES             **PeiServices,
+  IN EFI_PEI_PCI_CFG_PPI          *This,
+  IN EFI_PEI_PCI_CFG_PPI_WIDTH    Width,
+  IN UINT64                       Address,
+  IN OUT VOID                     *Buffer
+  )
+{
+  UINTN  PciLibAddress;
+
+  PciLibAddress = COMMON_TO_PCILIB_ADDRESS (Address);
+	switch (Width) {
+    case EfiPeiPciCfgWidthUint8:
+      * (UINT8 *) Buffer = PciRead8 (PciLibAddress);
+      break;
+
+    case EfiPeiPciCfgWidthUint16:
+      * (UINT16 *) Buffer = PciRead16 (PciLibAddress);
+      break;
+
+    case EfiPeiPciCfgWidthUint32:
+      * (UINT32 *) Buffer = PciRead32 (PciLibAddress);
+      break;
+
+    default:
+      return EFI_INVALID_PARAMETER;
+  }
+	return EFI_SUCCESS;
+}
+
+
+/**
+  PCI write operation.
+
+  @param  PeiServices    An indirect pointer to the PEI Services Table
+                         published by the PEI Foundation.
+  @param  This           Pointer to local data for the interface.
+  @param  Width          The width of the access. Enumerated in bytes.
+  @param  Address        The physical address of the access.
+  @param  Buffer         A pointer to the buffer of data.
+
+  @retval EFI_SUCCESS           The function completed successfully.
+  
+  
+  @retval EFI_INVALID_PARAMETER   Unsupported width
+                                  enumeration.
+  
+**/
+EFI_STATUS
+EFIAPI
+PciCfgWrite (
+  IN EFI_PEI_SERVICES             **PeiServices,
+  IN EFI_PEI_PCI_CFG_PPI          *This,
+  IN EFI_PEI_PCI_CFG_PPI_WIDTH    Width,
+  IN UINT64                       Address,
+  IN OUT VOID                     *Buffer
+  )
+{
+  UINTN  PciLibAddress;
+
+  PciLibAddress = COMMON_TO_PCILIB_ADDRESS (Address);
+  switch (Width) {
+    case EfiPeiPciCfgWidthUint8:
+      PciWrite8 (PciLibAddress, *(UINT8 *) Buffer);
+      break;
+
+    case EfiPeiPciCfgWidthUint16:
+      PciWrite16 (PciLibAddress, *(UINT16 *) Buffer);
+      break;
+
+    case EfiPeiPciCfgWidthUint32:
+      PciWrite32 (PciLibAddress, *(UINT32 *) Buffer);
+      break;
+
+    default:
+      return EFI_INVALID_PARAMETER;
+	}
+	return EFI_SUCCESS;
+}
+
+
+/**
+  PCI read-modify-write operation.
+
+  @param  PeiServices    An indirect pointer to the PEI Services Table
+                         published by the PEI Foundation.
+  @param  This           Pointer to local data for the interface.
+  @param  Width          The width of the access. Enumerated in bytes.
+  @param  Address        The physical address of the access.
+  @param  SetBits        Value of the bits to set.
+  @param  ClearBits      Value of the bits to clear.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_INVALID_PARAMETER   Unsupported width
+                                  enumeration.
+
+**/
+EFI_STATUS
+EFIAPI
+PciCfgModify (
+  IN EFI_PEI_SERVICES             **PeiServices,
+  IN EFI_PEI_PCI_CFG_PPI          *This,
+  IN EFI_PEI_PCI_CFG_PPI_WIDTH    Width,
+  IN UINT64                       Address,
+  IN UINTN                        SetBits,
+  IN UINTN                        ClearBits
+  )
+{
+  UINTN  PciLibAddress;
+
+  PciLibAddress = COMMON_TO_PCILIB_ADDRESS (Address);
+	switch (Width) {
+    case EfiPeiPciCfgWidthUint8:
+      PciAndThenOr8 (PciLibAddress, (UINT8)~ClearBits, (UINT8)SetBits);
+      break;
+
+    case EfiPeiPciCfgWidthUint16:
+      PciAndThenOr16 (PciLibAddress, (UINT16)~ClearBits, (UINT16)SetBits);
+      break;
+
+    case EfiPeiPciCfgWidthUint32:
+      PciAndThenOr32 (PciLibAddress, (UINT32)~ClearBits, (UINT32)SetBits);
+      break;
+
+    default:
+      return EFI_INVALID_PARAMETER;
+  }
+	return EFI_SUCCESS;
+}
---
+++ edk2/IntelFrameworkModulePkg/Universal/PcatSingleSegmentPciCfgPei/PciCfgInternal.h
@@ -0,0 +1,230 @@
+/**
+
+  Copyright (c) 2006 - 2007, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef __PCICFGPPI_INTERLNAL_H_
+#define __PCICFGPPI_INTERLNAL_H_
+
+#include <PiPei.h>
+#include <FrameworkPei.h>
+
+#include <Ppi/PciCfg2.h>
+#include <Ppi/PciCfg.h>
+
+#include <Library/BaseLib.h>
+#include <Library/PcdLib.h>
+#include <Library/DebugLib.h>
+#include <Library/PciLib.h>
+#include <Library/PeimEntryPoint.h>
+
+#include <IndustryStandard\Pci.h>
+
+#define COMMON_TO_PCILIB_ADDRESS(A) (UINTN)PCI_LIB_ADDRESS( \
+	((EFI_PEI_PCI_CFG_PPI_PCI_ADDRESS *) &A)->Bus, \
+	((EFI_PEI_PCI_CFG_PPI_PCI_ADDRESS *) &A)->Device, \
+	((EFI_PEI_PCI_CFG_PPI_PCI_ADDRESS *) &A)->Function, \
+	((EFI_PEI_PCI_CFG_PPI_PCI_ADDRESS *) &A)->Register \
+	)
+
+
+/**
+  Reads from a given location in the PCI configuration space.
+
+  @param  PeiServices     An indirect pointer to the PEI Services Table published by the PEI Foundation.
+
+  @param  This            Pointer to local data for the interface.
+
+  @param  Width           The width of the access. Enumerated in bytes.
+                          See EFI_PEI_PCI_CFG_PPI_WIDTH above.
+
+  @param  Address         The physical address of the access. The format of
+                          the address is described by EFI_PEI_PCI_CFG_PPI_PCI_ADDRESS.
+
+  @param  Buffer          A pointer to the buffer of data..
+
+
+  @retval EFI_SUCCESS           The function completed successfully.
+
+  @retval EFI_DEVICE_ERROR      There was a problem with the transaction.
+
+  @retval EFI_DEVICE_NOT_READY  The device is not capable of supporting the operation at this
+                                time.
+
+**/
+EFI_STATUS
+EFIAPI 
+PciCfg2Read (
+  IN CONST  EFI_PEI_SERVICES          **PeiServices,
+  IN CONST  EFI_PEI_PCI_CFG2_PPI      *This,
+  IN        EFI_PEI_PCI_CFG_PPI_WIDTH Width,
+  IN        UINT64                    Address,
+  IN OUT    VOID                      *Buffer
+);
+
+/**
+  Write to a given location in the PCI configuration space.
+
+  @param  PeiServices     An indirect pointer to the PEI Services Table published by the PEI Foundation.
+
+  @param  This            Pointer to local data for the interface.
+
+  @param  Width           The width of the access. Enumerated in bytes.
+                          See EFI_PEI_PCI_CFG_PPI_WIDTH above.
+
+  @param  Address         The physical address of the access. The format of
+                          the address is described by EFI_PEI_PCI_CFG_PPI_PCI_ADDRESS.
+
+  @param  Buffer          A pointer to the buffer of data..
+
+
+  @retval EFI_SUCCESS           The function completed successfully.
+
+  @retval EFI_DEVICE_ERROR      There was a problem with the transaction.
+
+  @retval EFI_DEVICE_NOT_READY  The device is not capable of supporting the operation at this
+                                time.
+
+**/
+EFI_STATUS
+EFIAPI 
+PciCfg2Write (
+  IN CONST  EFI_PEI_SERVICES          **PeiServices,
+  IN CONST  EFI_PEI_PCI_CFG2_PPI      *This,
+  IN        EFI_PEI_PCI_CFG_PPI_WIDTH Width,
+  IN        UINT64                    Address,
+  IN OUT    VOID                      *Buffer
+);
+
+
+/**
+  PCI read-modify-write operation.
+
+  @param  PeiServices     An indirect pointer to the PEI Services Table
+                          published by the PEI Foundation.
+
+  @param  This            Pointer to local data for the interface.
+
+  @param  Width           The width of the access. Enumerated in bytes. Type
+                          EFI_PEI_PCI_CFG_PPI_WIDTH is defined in Read().
+
+  @param  Address         The physical address of the access.
+
+  @param  SetBits         Points to value to bitwise-OR with the read configuration value.
+
+                          The size of the value is determined by Width.
+
+  @param  ClearBits       Points to the value to negate and bitwise-AND with the read configuration value.
+                          The size of the value is determined by Width.
+
+
+  @retval EFI_SUCCESS           The function completed successfully.
+
+  @retval EFI_DEVICE_ERROR      There was a problem with the transaction.
+
+  @retval EFI_DEVICE_NOT_READY  The device is not capable of supporting
+                                the operation at this time.
+
+**/
+EFI_STATUS
+EFIAPI 
+PciCfg2Modify (
+  IN CONST  EFI_PEI_SERVICES          **PeiServices,
+  IN CONST  EFI_PEI_PCI_CFG2_PPI      *This,
+  IN        EFI_PEI_PCI_CFG_PPI_WIDTH Width,
+  IN        UINT64                    Address,
+  IN CONST  VOID                      *SetBits,
+  IN CONST  VOID                      *ClearBits
+);
+
+
+
+/**
+  PCI read operation.
+
+  @param  PeiServices    An indirect pointer to the PEI Services Table
+                         published by the PEI Foundation.
+  @param  This           Pointer to local data for the interface.
+  @param  Width          The width of the access. Enumerated in bytes.
+  @param  Address        The physical address of the access.
+  @param  Buffer         A pointer to the buffer of data.
+
+  @retval EFI_SUCCESS           The function completed successfully.
+  @retval EFI_NOT_YET_AVAILABLE The service has not been installed.
+
+**/
+EFI_STATUS
+EFIAPI 
+PciCfgRead (
+  IN EFI_PEI_SERVICES             **PeiServices,
+  IN EFI_PEI_PCI_CFG_PPI          *This,
+  IN EFI_PEI_PCI_CFG_PPI_WIDTH    Width,
+  IN UINT64                       Address,
+  IN OUT VOID                     *Buffer
+  );
+
+/**
+  PCI write operation.
+
+  @param  PeiServices    An indirect pointer to the PEI Services Table
+                         published by the PEI Foundation.
+  @param  This           Pointer to local data for the interface.
+  @param  Width          The width of the access. Enumerated in bytes.
+  @param  Address        The physical address of the access.
+  @param  Buffer         A pointer to the buffer of data.
+
+  @retval EFI_SUCCESS           The function completed successfully.
+  @retval EFI_NOT_YET_AVAILABLE The service has not been installed.
+
+**/
+EFI_STATUS
+EFIAPI 
+PciCfgWrite (
+  IN EFI_PEI_SERVICES             **PeiServices,
+  IN EFI_PEI_PCI_CFG_PPI          *This,
+  IN EFI_PEI_PCI_CFG_PPI_WIDTH    Width,
+  IN UINT64                       Address,
+  IN OUT VOID                     *Buffer
+  );
+
+/**
+  PCI read-modify-write operation.
+
+  @param  PeiServices    An indirect pointer to the PEI Services Table
+                         published by the PEI Foundation.
+  @param  This           Pointer to local data for the interface.
+  @param  Width          The width of the access. Enumerated in bytes.
+  @param  Address        The physical address of the access.
+  @param  SetBits        Value of the bits to set.
+  @param  ClearBits      Value of the bits to clear.
+
+  @retval EFI_SUCCESS           The function completed successfully.
+
+**/
+EFI_STATUS
+EFIAPI 
+PciCfgModify (
+  IN EFI_PEI_SERVICES             **PeiServices,
+  IN EFI_PEI_PCI_CFG_PPI          *This,
+  IN EFI_PEI_PCI_CFG_PPI_WIDTH    Width,
+  IN UINT64                       Address,
+  IN UINTN                        SetBits,
+  IN UINTN                        ClearBits
+  );
+
+//
+// Global Variables
+//
+extern EFI_PEI_PCI_CFG_PPI    gPciCfgPpi;
+extern EFI_PEI_PPI_DESCRIPTOR gPciCfgPpiList;
+
+
+#endif
