6cfb80eb12e270ab70ab9abad007ef49e4653fc4
yshang1 <yshang1@6f19259b-4bc3-4df7-8a09-765794883524>
2007-07-30 08:21:39 +0000

    Update License header.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@3497 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Library/EdkFvbServiceLib/Fvb.h
+++ edk2/MdeModulePkg/Library/EdkFvbServiceLib/Fvb.h
@@ -1,10 +1,12 @@
 /**
   The internal header file for EdkFvbServiceLib.
 
-Copyright (c) 2006, Intel Corporation. <BR> All rights reserved.
-This program and the accompanying materials are licensed and
-made available under the terms and conditions of the BSD License
-which accompanies this distribution.  The full text of the license may be found at
+Copyright (c) 2006, Intel Corporation. <BR>
+
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions
+of the BSD License which accompanies this distribution.  The
+full text of the license may be found at
 http://opensource.org/licenses/bsd-license.php
 
 THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
