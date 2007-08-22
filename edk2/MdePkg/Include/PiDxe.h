aa437337edd281b4b8d39b4bec8e2fc0600ec627
yshang1 <yshang1@6f19259b-4bc3-4df7-8a09-765794883524>
2007-08-22 08:45:48 +0000

    Update the comments of PiDxe.h, PiPei.h and Uefi.h.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@3688 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/PiDxe.h
+++ edk2/MdePkg/Include/PiDxe.h
@@ -2,11 +2,6 @@
 
   Root include file for Mde Package DXE_CORE, DXE, SMM, SAL type modules.
 
-  This is the include file for any module of type base. Base modules only use 
-  types defined via this include file and can be ported easily to any 
-  environment. There are a set of base libraries in the Mde Package that can
-  be used to implement base modules.
-
 Copyright (c) 2006 - 2007, Intel Corporation
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
--- edk2/MdePkg/Include/Uefi.h
+++ edk2/MdePkg/Include/Uefi.h
@@ -2,10 +2,9 @@
 
   Root include file for Mde Package UEFI, UEFI_APPLICATION type modules.
 
-  This is the include file for any module of type base. Base modules only use 
+  This is the include file for any module of type UEFI and UEFI_APPLICATION. Uefi modules only use 
   types defined via this include file and can be ported easily to any 
-  environment. There are a set of base libraries in the Mde Package that can
-  be used to implement base modules.
+  environment. 
 
 Copyright (c) 2006 - 2007, Intel Corporation
 All rights reserved. This program and the accompanying materials
