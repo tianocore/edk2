e02bc2ae12f77988b27f48a11b59791b4f29a44d
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2007-09-04 06:10:44 +0000

    Merge the PI enabling works from the branch
    First round of PI enabling work:
    1) PiPeiCis changes (CONST, EFI_PEI_FILE_HANDLE.. etc)
    2) Make use of FirmwareVolume 2 protocol.
    3) Verified for Nt32Pkg and real platform for S3.
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@3771 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/Nt32Pkg/Sec/SecMain.c
+++ edk2/Nt32Pkg/Sec/SecMain.c
@@ -426,12 +426,12 @@ Returns:
 EFI_STATUS
 EFIAPI
 SecPeiReportStatusCode (
-  IN EFI_PEI_SERVICES           **PeiServices,
+  IN CONST EFI_PEI_SERVICES           **PeiServices,
   IN EFI_STATUS_CODE_TYPE       CodeType,
   IN EFI_STATUS_CODE_VALUE      Value,
   IN UINT32                     Instance,
-  IN EFI_GUID                   *CallerId,
-  IN EFI_STATUS_CODE_DATA       *Data OPTIONAL
+  IN CONST EFI_GUID                   *CallerId,
+  IN CONST EFI_STATUS_CODE_DATA       *Data OPTIONAL
   )
 /*++
 
