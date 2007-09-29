edae6a12ba5912b3af13e2196069761b622d6c76
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2007-09-29 11:22:13 +0000

    Update to support to produce Component Name and  & Component Name 2 protocol based on Feature flag PcdComponentNameDisable & PcdComponentName2Disable.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@3990 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/Nt32Pkg/WinNtConsoleDxe/ComponentName.c
+++ edk2/Nt32Pkg/WinNtConsoleDxe/ComponentName.c
@@ -45,6 +45,45 @@ Abstract:
 //
 // EFI Component Name Functions
 //
+/**
+  Retrieves a Unicode string that is the user readable name of the driver.
+
+  This function retrieves the user readable name of a driver in the form of a
+  Unicode string. If the driver specified by This has a user readable name in
+  the language specified by Language, then a pointer to the driver name is
+  returned in DriverName, and EFI_SUCCESS is returned. If the driver specified
+  by This does not support the language specified by Language,
+  then EFI_UNSUPPORTED is returned.
+
+  @param  This[in]              A pointer to the EFI_COMPONENT_NAME2_PROTOCOL or
+                                EFI_COMPONENT_NAME_PROTOCOL instance.
+
+  @param  Language[in]          A pointer to a Null-terminated ASCII string
+                                array indicating the language. This is the
+                                language of the driver name that the caller is
+                                requesting, and it must match one of the
+                                languages specified in SupportedLanguages. The
+                                number of languages supported by a driver is up
+                                to the driver writer. Language is specified
+                                in RFC 3066 or ISO 639-2 language code format.
+
+  @param  DriverName[out]       A pointer to the Unicode string to return.
+                                This Unicode string is the name of the
+                                driver specified by This in the language
+                                specified by Language.
+
+  @retval EFI_SUCCESS           The Unicode string for the Driver specified by
+                                This and the language specified by Language was
+                                returned in DriverName.
+
+  @retval EFI_INVALID_PARAMETER Language is NULL.
+
+  @retval EFI_INVALID_PARAMETER DriverName is NULL.
+
+  @retval EFI_UNSUPPORTED       The driver specified by This does not support
+                                the language specified by Language.
+
+**/
 EFI_STATUS
 EFIAPI
 WinNtConsoleComponentNameGetDriverName (
@@ -53,6 +92,75 @@ WinNtConsoleComponentNameGetDriverName (
   OUT CHAR16                       **DriverName
   );
 
+
+/**
+  Retrieves a Unicode string that is the user readable name of the controller
+  that is being managed by a driver.
+
+  This function retrieves the user readable name of the controller specified by
+  ControllerHandle and ChildHandle in the form of a Unicode string. If the
+  driver specified by This has a user readable name in the language specified by
+  Language, then a pointer to the controller name is returned in ControllerName,
+  and EFI_SUCCESS is returned.  If the driver specified by This is not currently
+  managing the controller specified by ControllerHandle and ChildHandle,
+  then EFI_UNSUPPORTED is returned.  If the driver specified by This does not
+  support the language specified by Language, then EFI_UNSUPPORTED is returned.
+
+  @param  This[in]              A pointer to the EFI_COMPONENT_NAME2_PROTOCOL or
+                                EFI_COMPONENT_NAME_PROTOCOL instance.
+
+  @param  ControllerHandle[in]  The handle of a controller that the driver
+                                specified by This is managing.  This handle
+                                specifies the controller whose name is to be
+                                returned.
+
+  @param  ChildHandle[in]       The handle of the child controller to retrieve
+                                the name of.  This is an optional parameter that
+                                may be NULL.  It will be NULL for device
+                                drivers.  It will also be NULL for a bus drivers
+                                that wish to retrieve the name of the bus
+                                controller.  It will not be NULL for a bus
+                                driver that wishes to retrieve the name of a
+                                child controller.
+
+  @param  Language[in]          A pointer to a Null-terminated ASCII string
+                                array indicating the language.  This is the
+                                language of the driver name that the caller is
+                                requesting, and it must match one of the
+                                languages specified in SupportedLanguages. The
+                                number of languages supported by a driver is up
+                                to the driver writer. Language is specified in
+                                RFC 3066 or ISO 639-2 language code format.
+
+  @param  ControllerName[out]   A pointer to the Unicode string to return.
+                                This Unicode string is the name of the
+                                controller specified by ControllerHandle and
+                                ChildHandle in the language specified by
+                                Language from the point of view of the driver
+                                specified by This.
+
+  @retval EFI_SUCCESS           The Unicode string for the user readable name in
+                                the language specified by Language for the
+                                driver specified by This was returned in
+                                DriverName.
+
+  @retval EFI_INVALID_PARAMETER ControllerHandle is not a valid EFI_HANDLE.
+
+  @retval EFI_INVALID_PARAMETER ChildHandle is not NULL and it is not a valid
+                                EFI_HANDLE.
+
+  @retval EFI_INVALID_PARAMETER Language is NULL.
+
+  @retval EFI_INVALID_PARAMETER ControllerName is NULL.
+
+  @retval EFI_UNSUPPORTED       The driver specified by This is not currently
+                                managing the controller specified by
+                                ControllerHandle and ChildHandle.
+
+  @retval EFI_UNSUPPORTED       The driver specified by This does not support
+                                the language specified by Language.
+
+**/
 EFI_STATUS
 EFIAPI
 WinNtConsoleComponentNameGetControllerName (
@@ -62,21 +170,71 @@ WinNtConsoleComponentNameGetControllerName (
   IN  CHAR8                                           *Language,
   OUT CHAR16                                          **ControllerName
   );
+
 
 //
 // EFI Component Name Protocol
 //
-EFI_COMPONENT_NAME_PROTOCOL     gWinNtConsoleComponentName = {
+EFI_COMPONENT_NAME_PROTOCOL  gWinNtConsoleComponentName = {
   WinNtConsoleComponentNameGetDriverName,
   WinNtConsoleComponentNameGetControllerName,
   "eng"
-};
+  };
+
+//
+// EFI Component Name 2 Protocol
+//
+EFI_COMPONENT_NAME2_PROTOCOL gWinNtConsoleComponentName2 = {
+  (EFI_COMPONENT_NAME2_GET_DRIVER_NAME) WinNtConsoleComponentNameGetDriverName,
+  (EFI_COMPONENT_NAME2_GET_CONTROLLER_NAME) WinNtConsoleComponentNameGetControllerName,
+  "en"
+  };
+
 
 static EFI_UNICODE_STRING_TABLE mWinNtConsoleDriverNameTable[] = {
-  { "eng", L"Windows Text Console Driver" },
+  { "eng;en", L"Windows Text Console Driver" },
   { NULL , NULL }
 };
 
+/**
+  Retrieves a Unicode string that is the user readable name of the driver.
+
+  This function retrieves the user readable name of a driver in the form of a
+  Unicode string. If the driver specified by This has a user readable name in
+  the language specified by Language, then a pointer to the driver name is
+  returned in DriverName, and EFI_SUCCESS is returned. If the driver specified
+  by This does not support the language specified by Language,
+  then EFI_UNSUPPORTED is returned.
+
+  @param  This[in]              A pointer to the EFI_COMPONENT_NAME2_PROTOCOL or
+                                EFI_COMPONENT_NAME_PROTOCOL instance.
+
+  @param  Language[in]          A pointer to a Null-terminated ASCII string
+                                array indicating the language. This is the
+                                language of the driver name that the caller is
+                                requesting, and it must match one of the
+                                languages specified in SupportedLanguages. The
+                                number of languages supported by a driver is up
+                                to the driver writer. Language is specified
+                                in RFC 3066 or ISO 639-2 language code format.
+
+  @param  DriverName[out]       A pointer to the Unicode string to return.
+                                This Unicode string is the name of the
+                                driver specified by This in the language
+                                specified by Language.
+
+  @retval EFI_SUCCESS           The Unicode string for the Driver specified by
+                                This and the language specified by Language was
+                                returned in DriverName.
+
+  @retval EFI_INVALID_PARAMETER Language is NULL.
+
+  @retval EFI_INVALID_PARAMETER DriverName is NULL.
+
+  @retval EFI_UNSUPPORTED       The driver specified by This does not support
+                                the language specified by Language.
+
+**/
 EFI_STATUS
 EFIAPI
 WinNtConsoleComponentNameGetDriverName (
@@ -84,41 +242,84 @@ WinNtConsoleComponentNameGetDriverName (
   IN  CHAR8                        *Language,
   OUT CHAR16                       **DriverName
   )
-/*++
-
-  Routine Description:
-    Retrieves a Unicode string that is the user readable name of the EFI Driver.
-
-  Arguments:
-    This       - A pointer to the EFI_COMPONENT_NAME_PROTOCOL instance.
-    Language   - A pointer to a three character ISO 639-2 language identifier.
-                 This is the language of the driver name that that the caller 
-                 is requesting, and it must match one of the languages specified
-                 in SupportedLanguages.  The number of languages supported by a 
-                 driver is up to the driver writer.
-    DriverName - A pointer to the Unicode string to return.  This Unicode string
-                 is the name of the driver specified by This in the language 
-                 specified by Language.
-
-  Returns:
-    EFI_SUCCESS           - The Unicode string for the Driver specified by This
-                            and the language specified by Language was returned 
-                            in DriverName.
-    EFI_INVALID_PARAMETER - Language is NULL.
-    EFI_INVALID_PARAMETER - DriverName is NULL.
-    EFI_UNSUPPORTED       - The driver specified by This does not support the 
-                            language specified by Language.
-
---*/
 {
-  return LookupUnicodeString (
-          Language,
-          gWinNtConsoleComponentName.SupportedLanguages,
-          mWinNtConsoleDriverNameTable,
-          DriverName
-          );
+  return LookupUnicodeString2 (
+           Language,
+           This->SupportedLanguages,
+           mWinNtConsoleDriverNameTable,
+           DriverName,
+           (BOOLEAN)(This == &gWinNtConsoleComponentName)
+           );
 }
 
+/**
+  Retrieves a Unicode string that is the user readable name of the controller
+  that is being managed by a driver.
+
+  This function retrieves the user readable name of the controller specified by
+  ControllerHandle and ChildHandle in the form of a Unicode string. If the
+  driver specified by This has a user readable name in the language specified by
+  Language, then a pointer to the controller name is returned in ControllerName,
+  and EFI_SUCCESS is returned.  If the driver specified by This is not currently
+  managing the controller specified by ControllerHandle and ChildHandle,
+  then EFI_UNSUPPORTED is returned.  If the driver specified by This does not
+  support the language specified by Language, then EFI_UNSUPPORTED is returned.
+
+  @param  This[in]              A pointer to the EFI_COMPONENT_NAME2_PROTOCOL or
+                                EFI_COMPONENT_NAME_PROTOCOL instance.
+
+  @param  ControllerHandle[in]  The handle of a controller that the driver
+                                specified by This is managing.  This handle
+                                specifies the controller whose name is to be
+                                returned.
+
+  @param  ChildHandle[in]       The handle of the child controller to retrieve
+                                the name of.  This is an optional parameter that
+                                may be NULL.  It will be NULL for device
+                                drivers.  It will also be NULL for a bus drivers
+                                that wish to retrieve the name of the bus
+                                controller.  It will not be NULL for a bus
+                                driver that wishes to retrieve the name of a
+                                child controller.
+
+  @param  Language[in]          A pointer to a Null-terminated ASCII string
+                                array indicating the language.  This is the
+                                language of the driver name that the caller is
+                                requesting, and it must match one of the
+                                languages specified in SupportedLanguages. The
+                                number of languages supported by a driver is up
+                                to the driver writer. Language is specified in
+                                RFC 3066 or ISO 639-2 language code format.
+
+  @param  ControllerName[out]   A pointer to the Unicode string to return.
+                                This Unicode string is the name of the
+                                controller specified by ControllerHandle and
+                                ChildHandle in the language specified by
+                                Language from the point of view of the driver
+                                specified by This.
+
+  @retval EFI_SUCCESS           The Unicode string for the user readable name in
+                                the language specified by Language for the
+                                driver specified by This was returned in
+                                DriverName.
+
+  @retval EFI_INVALID_PARAMETER ControllerHandle is not a valid EFI_HANDLE.
+
+  @retval EFI_INVALID_PARAMETER ChildHandle is not NULL and it is not a valid
+                                EFI_HANDLE.
+
+  @retval EFI_INVALID_PARAMETER Language is NULL.
+
+  @retval EFI_INVALID_PARAMETER ControllerName is NULL.
+
+  @retval EFI_UNSUPPORTED       The driver specified by This is not currently
+                                managing the controller specified by
+                                ControllerHandle and ChildHandle.
+
+  @retval EFI_UNSUPPORTED       The driver specified by This does not support
+                                the language specified by Language.
+
+**/
 EFI_STATUS
 EFIAPI
 WinNtConsoleComponentNameGetControllerName (
@@ -128,50 +329,6 @@ WinNtConsoleComponentNameGetControllerName (
   IN  CHAR8                                           *Language,
   OUT CHAR16                                          **ControllerName
   )
-/*++
-
-  Routine Description:
-    Retrieves a Unicode string that is the user readable name of the controller
-    that is being managed by an EFI Driver.
-
-  Arguments:
-    This             - A pointer to the EFI_COMPONENT_NAME_PROTOCOL instance.
-    ControllerHandle - The handle of a controller that the driver specified by 
-                       This is managing.  This handle specifies the controller 
-                       whose name is to be returned.
-    ChildHandle      - The handle of the child controller to retrieve the name 
-                       of.  This is an optional parameter that may be NULL.  It 
-                       will be NULL for device drivers.  It will also be NULL 
-                       for a bus drivers that wish to retrieve the name of the 
-                       bus controller.  It will not be NULL for a bus driver 
-                       that wishes to retrieve the name of a child controller.
-    Language         - A pointer to a three character ISO 639-2 language 
-                       identifier.  This is the language of the controller name 
-                       that that the caller is requesting, and it must match one
-                       of the languages specified in SupportedLanguages.  The 
-                       number of languages supported by a driver is up to the 
-                       driver writer.
-    ControllerName   - A pointer to the Unicode string to return.  This Unicode
-                       string is the name of the controller specified by 
-                       ControllerHandle and ChildHandle in the language specified
-                       by Language from the point of view of the driver specified
-                       by This. 
-
-  Returns:
-    EFI_SUCCESS           - The Unicode string for the user readable name in the 
-                            language specified by Language for the driver 
-                            specified by This was returned in DriverName.
-    EFI_INVALID_PARAMETER - ControllerHandle is not a valid EFI_HANDLE.
-    EFI_INVALID_PARAMETER - ChildHandle is not NULL and it is not a valid EFI_HANDLE.
-    EFI_INVALID_PARAMETER - Language is NULL.
-    EFI_INVALID_PARAMETER - ControllerName is NULL.
-    EFI_UNSUPPORTED       - The driver specified by This is not currently managing 
-                            the controller specified by ControllerHandle and 
-                            ChildHandle.
-    EFI_UNSUPPORTED       - The driver specified by This does not support the 
-                            language specified by Language.
-
---*/
 {
   EFI_STATUS                      Status;
   EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL    *SimpleTextOut;
@@ -211,10 +368,11 @@ WinNtConsoleComponentNameGetControllerName (
 
   Private = WIN_NT_SIMPLE_TEXT_OUT_PRIVATE_DATA_FROM_THIS (SimpleTextOut);
 
-  return LookupUnicodeString (
-          Language,
-          gWinNtConsoleComponentName.SupportedLanguages,
-          Private->ControllerNameTable,
-          ControllerName
-          );
+  return LookupUnicodeString2 (
+           Language,
+           This->SupportedLanguages,
+           Private->ControllerNameTable,
+           ControllerName,
+           (BOOLEAN)(This == &gWinNtConsoleComponentName)
+           );
 }
