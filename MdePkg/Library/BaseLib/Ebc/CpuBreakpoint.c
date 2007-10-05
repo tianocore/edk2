3e0ada4914d9f5f749cc90f47d8c2ae37d67c07d
vprabhal <vprabhal@6f19259b-4bc3-4df7-8a09-765794883524>
2007-10-05 18:51:24 +0000

    moved CpuSleep and CpuFlushTlb to the CpuLib to use PalCall in those functions, it is creating circular dependency if those functions are in BaseLib and the PalCallStatic is replaced with PalCall
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@4023 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/BaseLib/Ebc/CpuBreakpoint.c
+++ MdePkg/Library/BaseLib/Ebc/CpuBreakpoint.c
@@ -136,33 +136,3 @@ CpuPause (
 {
 }
 
-/**
-  Flushes all the Translation Lookaside Buffers(TLB) entries in a CPU.
-
-  Flushes all the Translation Lookaside Buffers(TLB) entries in a CPU.
-
-**/
-VOID
-EFIAPI
-CpuFlushTlb (
-  VOID
-  )
-{
-  ASSERT (FALSE);
-}
-
-/**
-  Places the CPU in a sleep state until an interrupt is received.
-
-  Places the CPU in a sleep state until an interrupt is received. If interrupts
-  are disabled prior to calling this function, then the CPU will be placed in a
-  sleep state indefinitely.
-
-**/
-VOID
-EFIAPI
-CpuSleep (
-  VOID
-  )
-{
-}
--- MdePkg/Library/BaseLib/Ipf/CpuBreakpointMsc.c
+++ MdePkg/Library/BaseLib/Ipf/CpuBreakpointMsc.c
@@ -102,19 +102,3 @@ EnableDisableInterrupts (
   DisableInterrupts ();
 }
 
-/**
-  Places the CPU in a sleep state until an interrupt is received.
-
-  Places the CPU in a sleep state until an interrupt is received. If interrupts
-  are disabled prior to calling this function, then the CPU will be placed in a
-  sleep state indefinitely.
-
-**/
-VOID
-EFIAPI
-CpuSleep (
-  VOID
-  )
-{
-  PalCallStatic (NULL, 29, 0, 0, 0);
-}
