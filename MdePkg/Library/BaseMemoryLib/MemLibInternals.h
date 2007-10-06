18be21dc5e39c7e8293f6795266662cf77c0b713
mdkinney <mdkinney@6f19259b-4bc3-4df7-8a09-765794883524>
2007-10-06 22:00:36 +0000

    Make header consistent with other files.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@4034 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/BaseMemoryLib/MemLibInternals.h
+++ MdePkg/Library/BaseMemoryLib/MemLibInternals.h
@@ -9,7 +9,7 @@
     PeiMemoryLib
     DxeMemoryLib
 
-  <BR>Copyright (c) 2006, Intel Corporation<BR>
+  Copyright (c) 2006, Intel Corporation<BR>
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
