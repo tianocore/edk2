631f060bb7fb457ff8720a690f4bdd378328cb2d
mdkinney <mdkinney@6f19259b-4bc3-4df7-8a09-765794883524>
2007-10-06 23:23:49 +0000

    Add BaseMemoryLib optimized for DXE
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@4036 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ MdePkg/Library/BaseMemoryLibOptDxe/CompareMemWrapper.c
@@ -0,0 +1,70 @@
+/** @file
+  CompareMem() implementation.
+
+  Copyright (c) 2006, Intel Corporation<BR>
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+  The following BaseMemoryLib instances share the same version of this file:
+
+    BaseMemoryLib
+    BaseMemoryLibMmx
+    BaseMemoryLibSse2
+    BaseMemoryLibRepStr
+    PeiMemoryLib
+    DxeMemoryLib
+
+**/
+
+//
+// Include common header file for this module.
+//
+
+
+#include "MemLibInternals.h"
+
+/**
+  Compares the contents of two buffers.
+
+  This function compares Length bytes of SourceBuffer to Length bytes of DestinationBuffer.
+  If all Length bytes of the two buffers are identical, then 0 is returned.  Otherwise, the
+  value returned is the first mismatched byte in SourceBuffer subtracted from the first
+  mismatched byte in DestinationBuffer.
+  If Length > 0 and DestinationBuffer is NULL and Length > 0, then ASSERT().
+  If Length > 0 and SourceBuffer is NULL and Length > 0, then ASSERT().
+  If Length is greater than (MAX_ADDRESS - DestinationBuffer + 1), then ASSERT(). 
+  If Length is greater than (MAX_ADDRESS - SourceBuffer + 1), then ASSERT(). 
+
+
+  @param  DestinationBuffer Pointer to the destination buffer to compare.
+  @param  SourceBuffer      Pointer to the source buffer to compare.
+  @param  Length            Number of bytes to compare.
+
+  @return 0                 All Length bytes of the two buffers are identical.
+  @retval Non-zero          The first mismatched byte in SourceBuffer subtracted from the first
+                            mismatched byte in DestinationBuffer.
+
+**/
+INTN
+EFIAPI
+CompareMem (
+  IN CONST VOID  *DestinationBuffer,
+  IN CONST VOID  *SourceBuffer,
+  IN UINTN       Length
+  )
+{
+  if (Length == 0) {
+    return 0;
+  }
+  ASSERT (DestinationBuffer != NULL);
+  ASSERT (SourceBuffer != NULL);
+  ASSERT ((Length - 1) <= (MAX_ADDRESS - (UINTN)DestinationBuffer));
+  ASSERT ((Length - 1) <= (MAX_ADDRESS - (UINTN)SourceBuffer));
+
+  return InternalMemCompareMem (DestinationBuffer, SourceBuffer, Length);
+}
