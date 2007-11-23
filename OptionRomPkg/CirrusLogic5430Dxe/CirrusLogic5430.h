94b73c2482a415da50c5b13c42753ac6f1548c07
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2007-11-23 06:19:16 +0000

    Save original PCI attributes in start() function and restore it in Stop().
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@4324 6f19259b-4bc3-4df7-8a09-765794883524

--- OptionRomPkg/CirrusLogic5430Dxe/CirrusLogic5430.h
+++ OptionRomPkg/CirrusLogic5430Dxe/CirrusLogic5430.h
@@ -1,14 +1,14 @@
 /** @file
   Cirrus Logic 5430 Controller Driver
 
-  Copyright (c) 2006, Intel Corporation                                                         
-  All rights reserved. This program and the accompanying materials                          
-  are licensed and made available under the terms and conditions of the BSD License         
-  which accompanies this distribution.  The full text of the license may be found at        
-  http://opensource.org/licenses/bsd-license.php                                            
+  Copyright (c) 2006, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
 
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
@@ -65,6 +65,7 @@ typedef struct {
   UINT64                                Signature;
   EFI_HANDLE                            Handle;
   EFI_PCI_IO_PROTOCOL                   *PciIo;
+  UINT64                                OriginalPciAttributes;
   EFI_UGA_DRAW_PROTOCOL                 UgaDraw;
   EFI_GRAPHICS_OUTPUT_PROTOCOL          GraphicsOutput;
   UINTN                                 CurrentMode;
