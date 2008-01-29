4214a3551d118b6aa522b6443e702289fa8321db
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2008-01-29 08:47:42 +0000

    Sync the bug that list node is free before it is removed from the list. That made the list invalid.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@4641 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Universal/Network/Tcp4Dxe/Tcp4Misc.c
+++ edk2/MdeModulePkg/Universal/Network/Tcp4Dxe/Tcp4Misc.c
@@ -425,8 +425,6 @@ TcpCloneTcb (
   )
 {
   TCP_CB               *Clone;
-  TCP4_SERVICE_DATA  *TcpService;
-  EFI_IP4_PROTOCOL   *Ip4;
 
   Clone = NetAllocatePool (sizeof (TCP_CB));
 
@@ -454,34 +452,6 @@ TcpCloneTcb (
   }
 
   ((TCP4_PROTO_DATA *) (Clone->Sk->ProtoReserved))->TcpPcb = Clone;
-
-  TcpService = ((TCP4_PROTO_DATA *) (Clone->Sk->ProtoReserved))->TcpService;
-
-  NetListInsertTail (&TcpService->SocketList, &Clone->Sk->Link);
-
-  //
-  // Open the device path on the handle where service binding resides on.
-  //
-  gBS->OpenProtocol (
-         TcpService->ControllerHandle,
-         &gEfiDevicePathProtocolGuid,
-         (VOID **) &Clone->Sk->ParentDevicePath,
-         TcpService->DriverBindingHandle,
-         Clone->Sk->SockHandle,
-         EFI_OPEN_PROTOCOL_BY_CHILD_CONTROLLER
-         );
-
-  //
-  // Open the ip protocol by child controller.
-  //
-  gBS->OpenProtocol (
-         TcpService->IpIo->ChildHandle,
-         &gEfiIp4ProtocolGuid,
-         (VOID **) &Ip4,
-         TcpService->DriverBindingHandle,
-         Clone->Sk->SockHandle,
-         EFI_OPEN_PROTOCOL_BY_CHILD_CONTROLLER
-         );
 
   return Clone;
 }
