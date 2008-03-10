25ca163d0cff76660887a4e973c4aec8c0b6f594
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2008-03-10 05:46:29 +0000

    Fix GCC building warning.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@4809 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Include/Library/GenericBdsLib.h
+++ MdeModulePkg/Include/Library/GenericBdsLib.h
@@ -536,7 +536,7 @@ BdsLibUpdateFvFileDevicePath (
 
 EFI_STATUS
 BdsLibConnectUsbDevByShortFormDP (
-  IN CHAR8                      HostControllerPI,
+  IN UINT8                      HostControllerPI,
   IN EFI_DEVICE_PATH_PROTOCOL   *RemainingDevicePath
   );
   
