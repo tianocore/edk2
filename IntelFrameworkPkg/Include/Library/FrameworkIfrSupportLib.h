cf7e50f8f9d88a77495f76f98cb775de9a0f6afe
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2008-03-19 03:26:51 +0000

    Modify all file header to follow doxygen format
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@4907 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkPkg/Include/Library/FrameworkIfrSupportLib.h
+++ IntelFrameworkPkg/Include/Library/FrameworkIfrSupportLib.h
@@ -1,4 +1,4 @@
-/*++
+/** @file
 
 Copyright (c) 2006, Intel Corporation                                                         
 All rights reserved. This program and the accompanying materials                          
@@ -17,7 +17,7 @@ Abstract:
 
   The file contain all library function for Ifr Operations.
 
---*/
+**/
 
 #ifndef _IFRSUPPORTLIBRARY_H
 #define _IFRSUPPORTLIBRARY_H
