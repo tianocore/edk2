96226baa2884ba0776dec3431f84b20cb3062915
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2008-04-08 03:01:25 +0000

    Update all files in DxeIpl Peim to follow doxygen style header.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@4998 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Core/DxeIplPeim/DxeIpl.h
+++ MdeModulePkg/Core/DxeIplPeim/DxeIpl.h
@@ -1,6 +1,7 @@
-/**@file
+/** @file
+  Header file for DxeIpl PEIM.
 
-Copyright (c) 2006 - 2007, Intel Corporation
+Copyright (c) 2006 - 2008, Intel Corporation. <BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
--- MdeModulePkg/Core/DxeIplPeim/Ipf/ImageRead.c
+++ MdeModulePkg/Core/DxeIplPeim/Ipf/ImageRead.c
@@ -1,6 +1,7 @@
-/*++
+/** @file
+  This module loads an image to memory for IPF Cpu architecture.
 
-Copyright (c) 2006, Intel Corporation
+Copyright (c) 2006 - 2008, Intel Corporation. <BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -9,13 +10,7 @@ http://opensource.org/licenses/bsd-license.php
 THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
 WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
-Module Name:
-
-  ImageRead.c
-
-Abstract:
-
---*/
+**/
 
 #include "DxeIpl.h"
 
