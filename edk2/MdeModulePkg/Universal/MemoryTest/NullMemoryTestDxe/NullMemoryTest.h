6f0428fa357161691c25a71ec46b592527625f4f
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2008-04-08 03:08:15 +0000

    Update all files to follow doxygen style file header.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@4999 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Universal/MemoryTest/NullMemoryTestDxe/NullMemoryTest.h
+++ edk2/MdeModulePkg/Universal/MemoryTest/NullMemoryTestDxe/NullMemoryTest.h
@@ -1,22 +1,16 @@
-/*++
-
-Copyright (c) 2006, Intel Corporation                                                         
-All rights reserved. This program and the accompanying materials                          
-are licensed and made available under the terms and conditions of the BSD License         
-which accompanies this distribution.  The full text of the license may be found at        
-http://opensource.org/licenses/bsd-license.php                                            
-                                                                                          
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
-
-Module Name:
-  
-    NullMemoryTest.h
-  
-Abstract:
+/** @file
   The generic memory test driver definition
 
---*/
+Copyright (c) 2006 - 2008, Intel Corporation. <BR>
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
 
 #ifndef _NULL_MEMORY_TEST_H
 #define _NULL_MEMORY_TEST_H
