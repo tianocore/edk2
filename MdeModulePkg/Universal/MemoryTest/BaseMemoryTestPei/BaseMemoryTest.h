7902102145f42143a062be1fe9f9d870686f96f9
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2008-04-08 03:08:39 +0000

    Update all files to follow doxygen style file header.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@5000 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/MemoryTest/BaseMemoryTestPei/BaseMemoryTest.h
+++ MdeModulePkg/Universal/MemoryTest/BaseMemoryTestPei/BaseMemoryTest.h
@@ -1,23 +1,16 @@
-/*++
-
-Copyright (c) 2006 - 2007, Intel Corporation                                                         
-All rights reserved. This program and the accompanying materials                          
-are licensed and made available under the terms and conditions of the BSD License         
-which accompanies this distribution.  The full text of the license may be found at        
-http://opensource.org/licenses/bsd-license.php                                            
-                                                                                          
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
-
-Module Name:
-
-  BaseMemoryTest.h
-   
-Abstract:
-
+/** @file
   Tiano PEIM to provide a PEI memory test service.
 
---*/
+Copyright (c) 2006 - 2008, Intel Corporation. <BR>
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
 
 #ifndef _PEI_BASE_MEMORY_TEST_H_
 #define _PEI_BASE_MEMORY_TEST_H_
