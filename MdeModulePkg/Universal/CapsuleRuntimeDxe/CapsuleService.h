13d40eddeb047d5bd2711e9f070025dbce34db1e
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2008-04-09 05:50:10 +0000

    Update all files to follow doxygen style file header.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@5025 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/CapsuleRuntimeDxe/CapsuleService.h
+++ MdeModulePkg/Universal/CapsuleRuntimeDxe/CapsuleService.h
@@ -1,6 +1,7 @@
-/*++
+/** @file
+  Capsule Runtime Service
 
-Copyright (c) 2006, Intel Corporation
+Copyright (c) 2006 - 2008, Intel Corporation. <BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -9,15 +10,7 @@ http://opensource.org/licenses/bsd-license.php
 THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
 WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
-Module Name:
-
-  CapsuleService.h
-
-Abstract:
-
-  Capsule Runtime Service
-
---*/
+**/
 
 #ifndef  _CAPSULE_RUNTIME_H_
 #define  _CAPSULE_RUNTIME_H_
