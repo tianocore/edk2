3322734908c318d7b904cde69067012b43b7c10d
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2008-04-09 05:46:57 +0000

    Update to follow doxygen style file header.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@5024 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Include/MdeModuleHii.h
+++ edk2/MdeModulePkg/Include/MdeModuleHii.h
@@ -1,8 +1,7 @@
-/* @file
+/** @file
+  EDK II implementation specific HII relative definition.
 
-  EDK II specific HII relative definition.
-
-Copyright (c) 2006 - 2007, Intel Corporation
+Copyright (c) 2006 - 2008, Intel Corporation. <BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
