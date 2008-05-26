078572d1ee2d2e61639ae3f67c87a7ac1cf76533
jljusten <jljusten@6f19259b-4bc3-4df7-8a09-765794883524>
2008-05-26 21:55:35 +0000

    edk2/EdkCompatibilityPkg/Foundation/Include/(Ia32|X64)/EfiBind.h:   Merge from MdePkg/Include/(Ia32|x64)/ProcessorBind.h   for GCC support.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@5293 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/EdkCompatibilityPkg/Foundation/Include/X64/EfiBind.h
+++ edk2/EdkCompatibilityPkg/Foundation/Include/X64/EfiBind.h
@@ -248,7 +248,12 @@ typedef int64_t   intn_t;
 
 #endif
 
-
+//
+// For symbol name in GNU assembly code, an extra "_" is necessary
+//
+#if __GNUC__
+  #define ASM_PFX(name) _##name    
+#endif
 
 #endif
 
