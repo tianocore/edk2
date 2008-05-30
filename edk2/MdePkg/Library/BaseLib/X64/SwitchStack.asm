3f210a2098cd88a1ba77e165b4779cb2da5f8ee2
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2008-05-30 02:03:15 +0000

    1. Add comments about reserve space on space according to x64 calling convention 2. Fix comments in SwitchStack as for the x64 calling convention.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@5315 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Library/BaseLib/X64/SwitchStack.asm
+++ edk2/MdePkg/Library/BaseLib/X64/SwitchStack.asm
@@ -1,6 +1,6 @@
 ;------------------------------------------------------------------------------
 ;
-; Copyright (c) 2006, Intel Corporation
+; Copyright (c) 2006 - 2008, Intel Corporation
 ; All rights reserved. This program and the accompanying materials
 ; are licensed and made available under the terms and conditions of the BSD License
 ; which accompanies this distribution.  The full text of the license may be found at
@@ -22,12 +22,12 @@
 ;------------------------------------------------------------------------------
 ; Routine Description:
 ;
-;   Routine for switching stacks with 1 parameter
+;   Routine for switching stacks with 2 parameters
 ;
 ; Arguments:
 ;
 ;   (rcx) EntryPoint    - Entry point with new stack.
-;   (rdx) Context       - Parameter for entry point.
+;   (rdx) Context1      - Parameter1 for entry point.
 ;   (r8)  Context2      - Parameter2 for entry point.
 ;   (r9)  NewStack      - Pointer to new stack.
 ;
@@ -40,6 +40,10 @@ InternalSwitchStack PROC
     mov     rax, rcx
     mov     rcx, rdx
     mov     rdx, r8
+    ;
+    ; Reserve space for register parameters (rcx, rdx, r8 & r9) on the stack,
+    ; in case the callee wishes to spill them.
+    ;
     lea     rsp, [r9 - 20h]
     call    rax
 InternalSwitchStack ENDP
