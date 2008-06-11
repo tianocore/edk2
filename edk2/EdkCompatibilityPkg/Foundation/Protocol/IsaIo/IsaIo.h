50b31dab69ecbd0e477d4ff4c935e95c99ff1a74
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2008-06-11 05:26:24 +0000

    remove reference to the obsolete EFI_LIGHT_ISA_IO_PROTOCOL. This protocol is introduced for size reduction. But later this method is obsoleted as EDK II use feature flags to conditional mask off some code. As EDK II core code does not produce this protocol so we remove all reference to this protocol.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@5340 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/EdkCompatibilityPkg/Foundation/Protocol/IsaIo/IsaIo.h
+++ edk2/EdkCompatibilityPkg/Foundation/Protocol/IsaIo/IsaIo.h
@@ -1,6 +1,6 @@
 /*++
 
-Copyright (c) 2004 - 2006, Intel Corporation                                                         
+Copyright (c) 2004 - 2008, Intel Corporation                                                         
 All rights reserved. This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -15,8 +15,7 @@ Module Name:
     
 Abstract:
 
-    EFI_ISA_IO_PROTOCOL or EFI_LIGHT_ISA_IO_PROTOCOL 
-    based on macro SIZE_REDUCTION_ISA_COMBINED.
+    EFI_ISA_IO_PROTOCOL
 
 Revision History
 
@@ -75,8 +74,6 @@ typedef enum {
   EfiIsaIoOperationSlaveWrite,
   EfiIsaIoOperationMaximum
 } EFI_ISA_IO_PROTOCOL_OPERATION;
-
-#ifndef SIZE_REDUCTION_ISA_COMBINED
 
 //
 // Specific for ISA I/O Protocol
@@ -185,178 +182,5 @@ struct _EFI_ISA_IO_PROTOCOL {
 
 extern EFI_GUID gEfiIsaIoProtocolGuid;
 
-#else
-
-//
-// Specific for Light ISA I/O Protocol
-//
-
-#define EFI_INTERFACE_DEFINITION_FOR_ISA_IO EFI_LIGHT_ISA_IO_PROTOCOL
-#define EFI_ISA_IO_PROTOCOL_VERSION         &gEfiLightIsaIoProtocolGuid
-#define EFI_ISA_IO_OPERATION_TOKEN          EfiIsaIoOperationSlaveWrite  
-
-#define ADD_SERIAL_NAME(x, y)
-
-//
-// Global ID for the Light ISA I/O Protocol
-//
-
-#define EFI_LIGHT_ISA_IO_PROTOCOL_GUID \
-  { 0x7cc7ed80, 0x9a68, 0x4781, {0x80, 0xe4, 0xda, 0x16, 0x99, 0x10, 0x5a, 0xfe} }
-
-EFI_FORWARD_DECLARATION (EFI_LIGHT_ISA_IO_PROTOCOL);
-
-
-typedef
-EFI_STATUS
-(EFIAPI *EFI_LIGHT_ISA_IO_PROTOCOL_IO_MEM) (
-  IN     EFI_LIGHT_ISA_IO_PROTOCOL    *This,
-  IN     EFI_ISA_IO_PROTOCOL_WIDTH    Width,
-  IN     UINT32                       Offset,
-  IN     UINTN                        Count,
-  IN OUT VOID                         *Buffer
-  )
-/*++
-
-Routine Description:
-
-  Performs an ISA I/O Read/Write Cycle
-  EFI_LIGHT_ISA_IO_PROTOCOL doesn't verfiy access for I/O operation.
-
-Arguments:
-
-  This                  - A pointer to the EFI_ISA_IO_PROTOCOL or EFI_LIGHT_ISA_IO_PROTOCOL instance.
-  Width                 - Signifies the width of the I/O operation.
-  Offset                - The offset in ISA I/O space to start the I/O operation.  
-  Count                 - The number of I/O operations to perform. 
-  Buffer                - The source/destination buffer
-
-Returns:
-
-  EFI_SUCCESS           - The data was read from or written to the device sucessfully.
-  EFI_UNSUPPORTED       - The Offset is not valid for this device.
-  EFI_INVALID_PARAMETER - Width or Count, or both, were invalid.
-  EFI_OUT_OF_RESOURCES  - The request could not be completed due to a lack of resources.
-
---*/  
-;
-
-typedef struct {
-  EFI_LIGHT_ISA_IO_PROTOCOL_IO_MEM  Read;
-  EFI_LIGHT_ISA_IO_PROTOCOL_IO_MEM  Write;
-} EFI_LIGHT_ISA_IO_PROTOCOL_ACCESS;
-
-
-typedef
-EFI_STATUS
-(EFIAPI *EFI_LIGHT_ISA_IO_PROTOCOL_MAP) (
-  IN     EFI_LIGHT_ISA_IO_PROTOCOL      *This,
-  IN     EFI_ISA_IO_PROTOCOL_OPERATION  Operation,
-  IN     UINT8                          ChannelNumber      OPTIONAL,
-  IN     UINT32                         ChannelAttributes,
-  IN     VOID                           *HostAddress,
-  IN OUT UINTN                          *NumberOfBytes,
-  OUT    EFI_PHYSICAL_ADDRESS           *DeviceAddress,
-  OUT    VOID                           **Mapping
-  )
-/*++
-
-Routine Description:
-
-  Maps a memory region for DMA, EFI_LIGHT_ISA_IO_PROTOCOL only supports 
-  Slave read/write operation to save code size.
-
-Arguments:
-
-  This                  - A pointer to the EFI_LIGHT_ISA_IO_PROTOCOL instance.
-  Operation             - Indicates the type of DMA (slave or bus master), and if 
-                          the DMA operation is going to read or write to system memory. 
-  ChannelNumber         - The slave channel number to use for this DMA operation. 
-                          If Operation and ChannelAttributes shows that this device 
-                          performs bus mastering DMA, then this field is ignored.  
-                          The legal range for this field is 0..7.  
-  ChannelAttributes     - The attributes of the DMA channel to use for this DMA operation
-  HostAddress           - The system memory address to map to the device.  
-  NumberOfBytes         - On input the number of bytes to map.  On output the number 
-                          of bytes that were mapped.
-  DeviceAddress         - The resulting map address for the bus master device to use 
-                          to access the hosts HostAddress.  
-  Mapping               - A resulting value to pass to EFI_ISA_IO.Unmap().
-
-Returns:
-
-  EFI_SUCCESS           - The range was mapped for the returned NumberOfBytes.
-  EFI_INVALID_PARAMETER - The Operation or HostAddress is undefined.
-  EFI_UNSUPPORTED       - The HostAddress can not be mapped as a common buffer.
-  EFI_DEVICE_ERROR      - The system hardware could not map the requested address.
-  EFI_OUT_OF_RESOURCES  - The memory pages could not be allocated.
-
---*/
-;
-
-typedef
-EFI_STATUS
-(EFIAPI *EFI_LIGHT_ISA_IO_PROTOCOL_UNMAP) (
-  IN EFI_LIGHT_ISA_IO_PROTOCOL     *This,
-  IN  VOID                         *Mapping
-  )
-/*++
-
-Routine Description:
-
-  Unmaps a memory region for DMA
-
-Arguments:
-
-  This             - A pointer to the EFI_ISA_IO_PROTOCOL or EFI_LIGHT_ISA_IO_PROTOCOL instance.
-  Mapping          - The mapping value returned from EFI_ISA_IO.Map().
-
-Returns:
-
-  EFI_SUCCESS      - The range was unmapped.
-  EFI_DEVICE_ERROR - The data was not committed to the target system memory.
-
---*/  
-;	
-
-typedef
-EFI_STATUS
-(EFIAPI *EFI_LIGHT_ISA_IO_PROTOCOL_FLUSH) (
-  IN EFI_LIGHT_ISA_IO_PROTOCOL     *This
-  )
-/*++
-
-Routine Description:
-
-  Flushes a DMA buffer
-
-Arguments:
-
-  This             - A pointer to the EFI_ISA_IO_PROTOCOL or EFI_LIGHT_ISA_IO_PROTOCOL instance.
-
-Returns:
-
-  EFI_SUCCESS      - The buffers were flushed.
-  EFI_DEVICE_ERROR - The buffers were not flushed due to a hardware error.
-
---*/  
-;
-
-//
-// Interface structure for the Light ISA I/O Protocol
-//
-struct _EFI_LIGHT_ISA_IO_PROTOCOL {
-  EFI_LIGHT_ISA_IO_PROTOCOL_ACCESS     Io;
-  EFI_LIGHT_ISA_IO_PROTOCOL_MAP        Map;
-  EFI_LIGHT_ISA_IO_PROTOCOL_UNMAP      Unmap;
-  EFI_LIGHT_ISA_IO_PROTOCOL_FLUSH      Flush;
-  EFI_ISA_ACPI_RESOURCE_LIST           *ResourceList;
-  UINT32                               RomSize;
-  VOID                                 *RomImage;
-};
-
-extern EFI_GUID gEfiLightIsaIoProtocolGuid;
-
-#endif
 
 #endif
