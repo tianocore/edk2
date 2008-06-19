452125274125a9b2b833e8b35922d1857b9b5445
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2008-06-19 11:44:04 +0000

    Remove unrecognized chars in comment in source file.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@5356 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkCompatibilityPkg/Foundation/Library/Dxe/UefiEfiIfrSupportLib/UefiIfrLibrary.h
+++ EdkCompatibilityPkg/Foundation/Library/Dxe/UefiEfiIfrSupportLib/UefiIfrLibrary.h
@@ -911,7 +911,7 @@ GetSupportedLanguages (
 
 Routine Description:
   This function returns the list of supported languages, in the format specified
-  in UEFI specification Appendix M.
+  in UEFI specification Appendix M.
 
 Arguments:
   HiiHandle  - The HII package list handle.
