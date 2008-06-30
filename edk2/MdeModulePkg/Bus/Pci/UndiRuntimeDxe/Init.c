3bfc6cb735a4613f34433850cd66da6274d3ff51
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2008-06-30 02:19:41 +0000

    Boolean values and variable type BOOLEAN should not use explicit comparisons to TRUE or FALSE
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@5377 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Bus/Pci/UndiRuntimeDxe/Init.c
+++ edk2/MdeModulePkg/Bus/Pci/UndiRuntimeDxe/Init.c
@@ -1,7 +1,7 @@
 /** @file
   Initialization functions for EFI UNDI32 driver.
 
-Copyright (c) 2006 - 2007, Intel Corporation
+Copyright (c) 2006 - 2008, Intel Corporation
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -514,7 +514,7 @@ UndiErrorDeletePxe:
   }
 
 UndiErrorDeleteDevice:
-  if (PciAttributesSaved == TRUE) {
+  if (PciAttributesSaved) {
     //
     // Restore original PCI attributes
     //
