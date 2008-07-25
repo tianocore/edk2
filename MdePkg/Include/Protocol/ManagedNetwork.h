1a09f6f6ac691a8dea2e601335d77b652b052404
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2008-07-25 10:41:27 +0000

    Update CopyRight to 2008
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@5565 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Protocol/ManagedNetwork.h
+++ MdePkg/Include/Protocol/ManagedNetwork.h
@@ -2,7 +2,7 @@
   EFI_MANAGED_NETWORK_SERVICE_BINDING_PROTOCOL as defined in UEFI 2.0.
   EFI_MANAGED_NETWORK_PROTOCOL as defined in UEFI 2.0.
 
-  Copyright (c) 2006, Intel Corporation
+  Copyright (c) 2006 - 2008, Intel Corporation
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
