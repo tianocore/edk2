6f75430207571189afb86728b6ffc2265decc749
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2008-08-14 02:54:46 +0000

    Use doxygen comment style for document entity such as struct, enum, variable that use /// but not //
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@5643 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/Guid/AprioriFileName.h
+++ edk2/MdePkg/Include/Guid/AprioriFileName.h
@@ -25,27 +25,19 @@
   { 0x1b45cc0a, 0x156a, 0x428a, { 0x62, 0XAF, 0x49, 0x86, 0x4d, 0xa0, 0xe6, 0xe6 } }
 
 
-/**
-   
-  This file must be of type EFI_FV_FILETYPE_FREEFORM and must
-  contain a single section of type EFI_SECTION_RAW. For details on
-  firmware volumes, firmware file types, and firmware file section
-  types.
 
-  @param FileNamesWithinVolume[]  An array of zero or more
-                                  EFI_GUID type entries that
-                                  match the file names of PEIM
-                                  modules in the same Firmware
-                                  Volume. The maximum number of
-                                  entries.
-
-  @param NumberOfModulesInVolume  Determined by the number of
-                                  modules in the FV.
-  
-**/
+///   
+///  This file must be of type EFI_FV_FILETYPE_FREEFORM and must
+///  contain a single section of type EFI_SECTION_RAW. For details on
+///  firmware volumes, firmware file types, and firmware file section
+///  types.
+///
 typedef struct {
+  ///
+  /// An array of zero or more EFI_GUID type entries that match the file names of PEIM
+  /// modules in the same Firmware Volume. The maximum number of entries. 
+  ///
   EFI_GUID  FileNamesWithinVolume[1];
-// Optional list of file-names
 } PEI_APRIORI_FILE_CONTENTS;
 
 extern EFI_GUID gPeiAprioriFileNameGuid;
