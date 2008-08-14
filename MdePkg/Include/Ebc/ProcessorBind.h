d484d1206719ccd807ad4bc557c452fac91de3df
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2008-08-14 02:53:43 +0000

    Use doxygen comment style for document entity such as struct, enum, variable that use /// but not //
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@5642 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Ebc/ProcessorBind.h
+++ MdePkg/Include/Ebc/ProcessorBind.h
@@ -18,9 +18,9 @@
 #ifndef __PROCESSOR_BIND_H__
 #define __PROCESSOR_BIND_H__
 
-//
-// Define the processor type so other code can make processor based choices
-//
+///
+/// Define the processor type so other code can make processor based choices
+///
 #define MDE_CPU_EBC
 
 //
@@ -41,42 +41,42 @@ typedef unsigned int          UINT32;
 typedef __int64               INT64;
 typedef unsigned __int64      UINT64;
 
-//
-// "long" type scales to the processor native size with EBC compiler
-//
+///
+/// "long" type scales to the processor native size with EBC compiler
+///
 typedef long                  INTN;
 typedef unsigned long         UINTN;
 
 #define UINT8_MAX 0xff
 
-//
-// Scalable macro to set the most significant bit in a natural number
-//
+///
+/// Scalable macro to set the most significant bit in a natural number
+///
 #define MAX_BIT     (1ULL << (sizeof (INTN) * 8 - 1)) 
 #define MAX_2_BITS  (3ULL << (sizeof (INTN) * 8 - 2))
 
-//
-// Maximum legal EBC address
-//
+///
+/// Maximum legal EBC address
+///
 #define MAX_ADDRESS   ((UINTN) ~0)
 
-//
-// The stack alignment required for EBC
-//
+///
+/// The stack alignment required for EBC
+///
 #define CPU_STACK_ALIGNMENT   sizeof(UINTN)
 
-//
-// Modifier to ensure that all protocol member functions and EFI intrinsics
-// use the correct C calling convention. All protocol member functions and
-// EFI intrinsics are required to modify thier member functions with EFIAPI.
-//
+///
+/// Modifier to ensure that all protocol member functions and EFI intrinsics
+/// use the correct C calling convention. All protocol member functions and
+/// EFI intrinsics are required to modify thier member functions with EFIAPI.
+///
 #define EFIAPI    
 
-//
-// The Microsoft* C compiler can removed references to unreferenced data items
-//  if the /OPT:REF linker option is used. We defined a macro as this is a 
-//  a non standard extension. Currently not supported by the EBC compiler
-//
+///
+/// The Microsoft* C compiler can removed references to unreferenced data items
+///  if the /OPT:REF linker option is used. We defined a macro as this is a 
+///  a non standard extension. Currently not supported by the EBC compiler
+///
 #define GLOBAL_REMOVE_IF_UNREFERENCED
 
 #define FUNCTION_ENTRY_POINT(p) (p)
