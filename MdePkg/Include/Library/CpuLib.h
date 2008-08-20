a8ff971524f55118babf06e19f21a357a1e09952
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2008-08-20 09:15:34 +0000

    Cleanup Cpu library class and CpuLib instance and BaseLib instance; and add comments for the confused logic in IfrSupportLib instance.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@5694 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Library/CpuLib.h
+++ MdePkg/Include/Library/CpuLib.h
@@ -1,7 +1,7 @@
 /** @file
   Library that provides processor specific library services
 
-  Copyright (c) 2006, Intel Corporation                                                         
+  Copyright (c) 2006 - 2008, Intel Corporation                                                         
   All rights reserved. This program and the accompanying materials                          
   are licensed and made available under the terms and conditions of the BSD License         
   which accompanies this distribution.  The full text of the license may be found at        
@@ -28,21 +28,6 @@ EFIAPI
 CpuSleep (
   VOID
   );
-
-
-/**
-  Requests CPU to pause for a short period of time.
-
-  Requests CPU to pause for a short period of time. Typically used in MP
-  systems to prevent memory starvation while waiting for a spin lock.
-
-**/
-VOID
-EFIAPI
-CpuPause (
-  VOID
-  );
-
 
 /**
   Flushes all the Translation Lookaside Buffers(TLB) entries in a CPU.
