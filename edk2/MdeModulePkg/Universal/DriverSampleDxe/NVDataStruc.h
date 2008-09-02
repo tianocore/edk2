4b47f16469d51a087e96a70595e9f38a324eafd6
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2008-09-02 01:28:17 +0000

    Merged in the bug fixes from EDK I.
    *** Press F9 in Uefi64 FrontPage shouldnot enter sub-menu 
    ** [FT] Some UI error on multiple platforms on framework_20080811
    ** [FT] UI test of How tall are you(Hex) on framework_20080811
    
    
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@5770 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Universal/DriverSampleDxe/NVDataStruc.h
+++ edk2/MdeModulePkg/Universal/DriverSampleDxe/NVDataStruc.h
@@ -1,6 +1,6 @@
 /** @file
 
-Copyright (c) 2007, Intel Corporation
+Copyright (c) 2007 - 2008, Intel Corporation
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -41,7 +41,7 @@ Revision History:
 typedef struct {
   UINT16  WhatIsThePassword[20];
   UINT16  WhatIsThePassword2[20];
-  UINT16  MyStringData[20];
+  UINT16  MyStringData[40];
   UINT16  PasswordClearText[20];
   UINT16  SomethingHiddenForHtml;
   UINT8   HowOldAreYouInYearsManual;
