9eb7382f73a1b000aca55cd0dc5b61998ec62f52
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2008-09-04 07:38:10 +0000

    1. remove EFI_EVENT_ alias, replace them with EVT_ 2. remove definitions of EFI_DESCRIPTION_STRING and EFI_DESCRIPTION_STRING_BUNDLE.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@5812 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/UefiLib/UefiNotTiano.c
+++ MdePkg/Library/UefiLib/UefiNotTiano.c
@@ -26,7 +26,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 /**
   An empty function to pass error checking of CreateEventEx ().
 
-  This empty function ensures that EFI_EVENT_NOTIFY_SIGNAL_ALL is error
+  This empty function ensures that EVT_NOTIFY_SIGNAL_ALL is error
   checked correctly since it is now mapped into CreateEventEx() in UEFI 2.0.
  
   @param  Event                 Event whose notification function is being invoked.
