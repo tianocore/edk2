5a58200d19b6d9c544d671cf05d6fc54d4744315
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2008-09-19 09:26:18 +0000

    Correct CompareGuid function
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@5933 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkCompatibilityPkg/Compatibility/FrameworkHiiToUefiHiiThunk/Utility.c
+++ EdkCompatibilityPkg/Compatibility/FrameworkHiiToUefiHiiThunk/Utility.c
@@ -344,7 +344,7 @@ GetAttributesOfFirstFormSet (
 
         case EFI_IFR_GUID_OP:
           Class = (EFI_IFR_GUID_CLASS*) OpCode;
-          if (CompareGuid ((EFI_GUID *)(VOID *)&Class->Guid, &gTianoHiiIfrGuid) == 0) {
+          if (CompareGuid ((EFI_GUID *)(VOID *)&Class->Guid, &gTianoHiiIfrGuid)) {
             Class = (EFI_IFR_GUID_CLASS *) OpCode;
 
             switch (Class->ExtendOpCode) {
@@ -497,7 +497,7 @@ CreateQuestionIdMap (
        
         case EFI_IFR_GUID_OP:
           OptionMap = (EFI_IFR_GUID_OPTIONKEY *) OpCode;
-          if (CompareGuid ((EFI_GUID *)(VOID *)&OptionMap->Guid, &gFrameworkHiiCompatbilityGuid) == 0) {
+          if (CompareGuid ((EFI_GUID *)(VOID *)&OptionMap->Guid, &gFrameworkHiiCompatbilityGuid)) {
             if (OptionMap->ExtendOpCode == EFI_IFR_EXTEND_OP_OPTIONKEY) {
               OneOfOptinMapEntryListHead = GetOneOfOptionMapEntryListHead (ThunkContext, OptionMap->QuestionId);
               if (OneOfOptinMapEntryListHead == NULL) {
@@ -525,7 +525,7 @@ CreateQuestionIdMap (
               
               InsertTailList (OneOfOptinMapEntryListHead, &OneOfOptionMapEntry->Link);
             }
-         } else if (CompareGuid ((EFI_GUID *)(VOID *)&OptionMap->Guid, &gTianoHiiIfrGuid) == 0) {
+         } else if (CompareGuid ((EFI_GUID *)(VOID *)&OptionMap->Guid, &gTianoHiiIfrGuid)) {
             Class = (EFI_IFR_GUID_CLASS *) OpCode;
 
             switch (Class->ExtendOpCode) {
