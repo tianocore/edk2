08d0c1591120531e9f90ece8e5841e93554c9a99
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2008-10-09 18:23:56 +0000

    1, Fix doxygen comment for structure and macro 2, Add missing macro - EfiBltVideoToVideo
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@6094 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/Protocol/EdidActive.h
+++ edk2/MdePkg/Include/Protocol/EdidActive.h
@@ -28,22 +28,23 @@
   EDID information retrieved from the EFI_EDID_OVERRIDE_PROTOCOL if an override is
   available, or an identical copy of the EDID information from the
   EFI_EDID_DISCOVERED_PROTOCOL if no overrides are available.
-
-  @param SizeOfEdid 
-  The size, in bytes, of the Edid buffer. 0 if no EDID information
-  is available from the video output device. Otherwise, it must be a
-  minimum of 128 bytes.
-  
-  @param Edid 
-  A pointer to a read-only array of bytes that contains the EDID
-  information for an active video output device. This pointer is
-  NULL if no EDID information is available for the video output
-  device. The minimum size of a valid Edid buffer is 128 bytes.
-  EDID information is defined in the E-DID EEPROM
-  specification published by VESA (www.vesa.org).
 **/
 typedef struct {
+  ///
+  /// The size, in bytes, of the Edid buffer. 0 if no EDID information
+  /// is available from the video output device. Otherwise, it must be a
+  /// minimum of 128 bytes.
+  ///
   UINT32   SizeOfEdid;
+
+  ///
+  /// A pointer to a read-only array of bytes that contains the EDID
+  /// information for an active video output device. This pointer is
+  /// NULL if no EDID information is available for the video output
+  /// device. The minimum size of a valid Edid buffer is 128 bytes.
+  /// EDID information is defined in the E-DID EEPROM
+  /// specification published by VESA (www.vesa.org).
+  ///
   UINT8    *Edid;
 } EFI_EDID_ACTIVE_PROTOCOL;
 
