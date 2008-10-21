842a119dba6f81ac1ce2bc14dacc9848ef766557
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2008-10-21 18:39:04 +0000

    fix no ascii char
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@6180 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Protocol/DevicePath.h
+++ MdePkg/Include/Protocol/DevicePath.h
@@ -49,16 +49,16 @@
   that make up the Device Path.
 **/
 typedef struct {
-  UINT8 Type;       ///< 0x01 - Hardware Device Path
-                    ///< 0x02 – ACPI Device Path
-                    ///< 0x03 – Messaging Device Path
-                    ///< 0x04 – Media Device Path
-                    ///< 0x05 – BIOS Boot Specification Device Path
-                    ///< 0x7F – End of Hardware Device Path
+  UINT8 Type;       ///< 0x01 Hardware Device Path
+                    ///< 0x02 ACPI Device Path
+                    ///< 0x03 Messaging Device Path
+                    ///< 0x04 Media Device Path
+                    ///< 0x05 BIOS Boot Specification Device Path
+                    ///< 0x7F End of Hardware Device Path
                     
   UINT8 SubType;    ///< Varies by Type
-                    ///< 0xFF – End Entire Device Path, or
-                    ///< 0x01 – End This Instance of a Device Path and start a new
+                    ///< 0xFF End Entire Device Path, or
+                    ///< 0x01 End This Instance of a Device Path and start a new
                     ///< Device Path
                     
   UINT8 Length[2];  ///< Specific Device Path data. Type and Sub-Type define
