6a24dc00f177fd9ccf52f6f9e5af0609989818fe
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2008-10-31 07:59:47 +0000

    Fix a prototype bug.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@6354 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Library/BaseIoLibIntrinsic/IoLibIpf.c
+++ edk2/MdePkg/Library/BaseIoLibIntrinsic/IoLibIpf.c
@@ -256,7 +256,7 @@ IoWrite64 (
 UINT8
 EFIAPI
 MmioRead8 (
-  IN  UINT64                 Address
+  IN  UINTN                  Address
   )
 {
   UINT8            Data;
@@ -285,7 +285,7 @@ MmioRead8 (
 UINT16
 EFIAPI
 MmioRead16 (
-  IN  UINT64                 Address
+  IN  UINTN                  Address
   )
 {
   UINT16           Data;
@@ -319,7 +319,7 @@ MmioRead16 (
 UINT32
 EFIAPI
 MmioRead32 (
-  IN  UINT64                 Address
+  IN  UINTN                  Address
   )
 {
   UINT32           Data;
@@ -353,7 +353,7 @@ MmioRead32 (
 UINT64
 EFIAPI
 MmioRead64 (
-  IN  UINT64                 Address
+  IN  UINTN                  Address
   )
 {
   UINT64           Data;
@@ -391,7 +391,7 @@ MmioRead64 (
 UINT8
 EFIAPI
 MmioWrite8 (
-  IN  UINT64                 Address,
+  IN  UINTN                  Address,
   IN  UINT8                  Data
   )
 {
@@ -422,7 +422,7 @@ MmioWrite8 (
 UINT16
 EFIAPI
 MmioWrite16 (
-  IN  UINT64                 Address,
+  IN  UINTN                  Address,
   IN  UINT16                 Data
   )
 {
@@ -458,7 +458,7 @@ MmioWrite16 (
 UINT32
 EFIAPI
 MmioWrite32 (
-  IN  UINT64                 Address,
+  IN  UINTN                  Address,
   IN  UINT32                 Data
   )
 {
@@ -494,7 +494,7 @@ MmioWrite32 (
 UINT64
 EFIAPI
 MmioWrite64 (
-  IN  UINT64                 Address,
+  IN  UINTN                  Address,
   IN  UINT64                 Data
   )
 {
