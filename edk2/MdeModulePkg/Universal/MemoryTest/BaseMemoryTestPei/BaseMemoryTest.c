af58e6035a1bcd056593adbc5b71c0b4af6ffd67
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2008-10-31 04:43:59 +0000

    Use #include "XXX.h" for module internal header files.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@6344 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Universal/MemoryTest/BaseMemoryTestPei/BaseMemoryTest.c
+++ edk2/MdeModulePkg/Universal/MemoryTest/BaseMemoryTestPei/BaseMemoryTest.c
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
-#include <BaseMemoryTest.h>
+#include "BaseMemoryTest.h"
 #include <Library/PeiServicesLib.h>
 
 PEI_BASE_MEMORY_TEST_PPI mPeiBaseMemoryTestPpi = { BaseMemoryTest };
