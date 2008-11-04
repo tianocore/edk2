063d7784e28503840ebd17c9b050b24e37839e9d
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2008-11-04 14:17:07 +0000

    remove DEBUG_DEPEX as it does not pass design review yet.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@6375 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/Library/DebugLib.h
+++ edk2/MdePkg/Include/Library/DebugLib.h
@@ -35,7 +35,6 @@
 #define DEBUG_POOL      0x00000010  // Alloc & Free's
 #define DEBUG_PAGE      0x00000020  // Alloc & Free's
 #define DEBUG_INFO      0x00000040  // Verbose
-#define DEBUG_DEPEX     0x00000080  // PEI or DXE Dependency Evaluation
 #define DEBUG_VARIABLE  0x00000100  // Variable
 #define DEBUG_BM        0x00000400  // Boot Manager
 #define DEBUG_BLKIO     0x00001000  // BlkIo Driver
@@ -55,7 +54,6 @@
 #define EFI_D_POOL      DEBUG_POOL
 #define EFI_D_PAGE      DEBUG_PAGE
 #define EFI_D_INFO      DEBUG_INFO
-#define EFI_D_DEPEX     DEBUG_DEPEX
 #define EFI_D_VARIABLE  DEBUG_VARIABLE
 #define EFI_D_BM        DEBUG_BM
 #define EFI_D_BLKIO     DEBUG_BLKIO
