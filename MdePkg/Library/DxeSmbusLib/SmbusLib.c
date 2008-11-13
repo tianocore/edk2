484c77852ea2ba6ccc8e5c3b3ec4548588dd7f01
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2008-11-13 08:30:16 +0000

    Update minor comments for Smbus, PcdLib, PeiServicesLib library instance. And change UefiRuntimeLib instance type to UEFI_DRIVER according to MdeLib spec.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@6495 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/DxeSmbusLib/SmbusLib.c
+++ MdePkg/Library/DxeSmbusLib/SmbusLib.c
@@ -1,5 +1,5 @@
 /** @file
-Implementation of SmBusLib class library for PEI phase.
+Implementation of SmBusLib class library for DXE phase.
 
 Copyright (c) 2006, Intel Corporation<BR>
 All rights reserved. This program and the accompanying materials
