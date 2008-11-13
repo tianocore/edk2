ae50ee26b51677062427832aecfea53db14bf322
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2008-11-13 09:18:40 +0000

    Rename the reference library class of ScsiLib to UefiScsiLib.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@6523 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Bus/Scsi/ScsiDiskDxe/ScsiDisk.h
+++ MdeModulePkg/Bus/Scsi/ScsiDiskDxe/ScsiDisk.h
@@ -29,7 +29,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #include <Library/UefiDriverEntryPoint.h>
 #include <Library/UefiLib.h>
 #include <Library/BaseMemoryLib.h>
-#include <Library/ScsiLib.h>
+#include <Library/UefiScsiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 
 #include <IndustryStandard/Scsi.h>
