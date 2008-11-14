7ebaab5eec2630caf612889979441389bf059a9d
xli24 <xli24@6f19259b-4bc3-4df7-8a09-765794883524>
2008-11-14 07:58:59 +0000

    Files of PeiPalLib uses the wrong license information in the file header. Update them.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@6546 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Library/PeiPalLib/PeiPalLib.c
+++ edk2/MdePkg/Library/PeiPalLib/PeiPalLib.c
@@ -1,16 +1,15 @@
 /** @file
   PAL Call Services Function.
 
-  Copyright (c) 2006 - 2008 Intel Corporation. All rights reserved
-  This software and associated documentation (if any) is furnished
-  under a license and may only be used or copied in accordance
-  with the terms of the license. Except as permitted by such
-  license, no part of this software or documentation may be
-  reproduced, stored in a retrieval system, or transmitted in any
-  form or by any means without the express written consent of
-  Intel Corporation.
+  Copyright (c) 2006 -2008, Intel Corporation All rights
+  reserved. This program and the accompanying materials are
+  licensed and made available under the terms and conditions of
+  the BSD License which accompanies this distribution.  The full
+  text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
 
-  Module Name:  PalCallLib.c
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
