a6c42ad9b42c81e9d5a42e6d93514b63e3834d9a
qwang12 <qwang12@6f19259b-4bc3-4df7-8a09-765794883524>
2008-11-18 08:05:32 +0000

    ICC Cleanup: add (UINT8 *) typecast.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@6586 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkCompatibilityPkg/Sample/Platform/Generic/RuntimeDxe/StatusCode/Lib/BsSerialStatusCode/BsSerialStatusCode.c
+++ EdkCompatibilityPkg/Sample/Platform/Generic/RuntimeDxe/StatusCode/Lib/BsSerialStatusCode/BsSerialStatusCode.c
@@ -1,6 +1,6 @@
 /*++
 
-Copyright (c) 2004 - 2007, Intel Corporation                                                         
+Copyright (c) 2004 - 2008, Intel Corporation                                                         
 All rights reserved. This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -2317,7 +2317,7 @@ Returns:
     //
     // Callout to platform Lib function to do print.
     //
-    DebugSerialPrint (Buffer);
+    DebugSerialPrint ((UINT8 *) Buffer);
   }
   //
   // Debug code to display human readable code information.
@@ -2369,7 +2369,7 @@ Returns:
         Instance
         );
 
-      DebugSerialPrint (Buffer);
+      DebugSerialPrint ((UINT8 *) Buffer);
     }
   }
 #endif
