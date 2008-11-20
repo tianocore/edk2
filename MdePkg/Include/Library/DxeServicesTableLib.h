f9eb929db4057864af5a4e6eb7f4d77251e9e29e
gikidy <gikidy@6f19259b-4bc3-4df7-8a09-765794883524>
2008-11-20 04:55:02 +0000

    Synchronize the MdePkg\Include\Library\HobLib.h, DxeServicesTableLib.h with the MDE_Library_Spec.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@6644 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Library/DxeServicesTableLib.h
+++ MdePkg/Include/Library/DxeServicesTableLib.h
@@ -1,7 +1,16 @@
 /** @file
   Provides a service to retrieve a pointer to the DXE Services Table.
   Only available to DXE module types.
-
+  
+  This library does not contain any functions or macros.  It simply exports a global 
+  pointer to the DXE Services Table as defined in the Platform Initialization Driver 
+  Execution Environment Core Interface Specification.  The library constructor must 
+  initialize this global pointer to the DX Services Table, so it is available at the
+  module¡¯s entry point.  Since there is overhead in looking up the pointer to the DXE 
+  Services Table, only those modules that actually require access to the DXE Services 
+  Table should use this library.  This will typically be DXE Drivers that require GCD 
+  or Dispatcher services.
+   
 Copyright (c) 2006 - 2008, Intel Corporation
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
