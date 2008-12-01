891193ee4f57db77dd7b7a3dab47798febe92176
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2008-12-01 07:34:29 +0000

    Fix the issue that wrong doxygen header comments.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@6788 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/BaseLib/Ipf/Ia64gen.h
+++ MdePkg/Library/BaseLib/Ipf/Ia64gen.h
@@ -1,14 +1,15 @@
-/** @file Register Definition for IPF.
- 
- 
- Copyright (c) 2006, Intel Corporation<BR>
- All rights reserved. This program and the accompanying materials
- are licensed and made available under the terms and conditions of the BSD License
- which accompanies this distribution.  The full text of the license may be found at
- http://opensource.org/licenses/bsd-license.php
+/** @file 
 
- THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
- WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+    Register Definition for IPF.
+ 
+Copyright (c) 2006, Intel Corporation<BR>
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
  
 **/
 #ifndef _IA64GEN_H_
