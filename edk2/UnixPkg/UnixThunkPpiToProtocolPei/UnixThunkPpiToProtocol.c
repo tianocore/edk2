4c54da92dfe7031aacaa94934619b817927d37c3
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2008-12-03 06:20:14 +0000

    Fix entry point prototype.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@6813 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/UnixPkg/UnixThunkPpiToProtocolPei/UnixThunkPpiToProtocol.c
+++ edk2/UnixPkg/UnixThunkPpiToProtocolPei/UnixThunkPpiToProtocol.c
@@ -32,8 +32,8 @@ Revision History
 EFI_STATUS
 EFIAPI
 PeimInitializeUnixStuff (
-  IN EFI_FFS_FILE_HEADER       *FfsHeader,
-  IN EFI_PEI_SERVICES          **PeiServices
+  IN       EFI_PEI_FILE_HANDLE       FileHandle,
+  IN CONST EFI_PEI_SERVICES          **PeiServices
   )
 /*++
 
