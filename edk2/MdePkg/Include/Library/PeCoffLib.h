38c38cdfeefce5fb688e28636f9218db4878be14
gikidy <gikidy@6f19259b-4bc3-4df7-8a09-765794883524>
2008-12-04 02:03:14 +0000

    Follow the Mde_Lib_Spec to fix the function PeCoffLoaderUnloadImage() IN/OUT issue about definition and implementation.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@6825 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/Library/PeCoffLib.h
+++ edk2/MdePkg/Include/Library/PeCoffLib.h
@@ -360,6 +360,6 @@ PeCoffLoaderRelocateImageForRuntime (
 RETURN_STATUS
 EFIAPI
 PeCoffLoaderUnloadImage (
-  IN PE_COFF_LOADER_IMAGE_CONTEXT  *ImageContext
+  IN OUT PE_COFF_LOADER_IMAGE_CONTEXT  *ImageContext
   );
 #endif
