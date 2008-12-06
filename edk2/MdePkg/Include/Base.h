d51d9cf43994666b73fc9c6ee7ae8ad6ae3d6c8b
mdkinney <mdkinney@6f19259b-4bc3-4df7-8a09-765794883524>
2008-12-06 01:19:56 +0000

    Change _CR() to BASE_CR() to follow naming convention
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@6886 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/Base.h
+++ edk2/MdePkg/Include/Base.h
@@ -274,7 +274,7 @@ typedef CHAR8 *VA_LIST;
   @return  A pointer to the structure from one of it's elements.
   
 **/
-#define _CR(Record, TYPE, Field)  ((TYPE *) ((CHAR8 *) (Record) - (CHAR8 *) &(((TYPE *) 0)->Field)))
+#define BASE_CR(Record, TYPE, Field)  ((TYPE *) ((CHAR8 *) (Record) - (CHAR8 *) &(((TYPE *) 0)->Field)))
 
 /**
   Rounds a value up to the next boundary using a specified alignment.  
