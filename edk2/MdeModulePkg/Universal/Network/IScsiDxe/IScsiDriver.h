1008e232063d73812567e4c697a984352a9b5d4c
ywu21 <ywu21@6f19259b-4bc3-4df7-8a09-765794883524>
2008-12-07 04:45:45 +0000

    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@6892 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Universal/Network/IScsiDxe/IScsiDriver.h
+++ edk2/MdeModulePkg/Universal/Network/IScsiDxe/IScsiDriver.h
@@ -40,6 +40,8 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 extern EFI_COMPONENT_NAME2_PROTOCOL       gIScsiComponentName2;
 extern EFI_COMPONENT_NAME_PROTOCOL        gIScsiComponentName;
 
+extern EFI_ISCSI_INITIATOR_NAME_PROTOCOL  gIScsiInitiatorName;
+
 
 extern EFI_GUID                           mIScsiPrivateGuid;
 
@@ -256,6 +258,81 @@ IScsiComponentNameGetControllerName (
   IN  EFI_HANDLE                    ChildHandle        OPTIONAL,
   IN  CHAR8                         *Language,
   OUT CHAR16                        **ControllerName
+  );
+  
+//
+// EFI IScsi Initiator Name Protocol for IScsi driver.
+//
+/**
+  Retrieves the current set value of iSCSI Initiator Name. 
+
+  @param  This[in]              Pointer to the EFI_ISCSI_INITIATOR_NAME_PROTOCOL instance.
+
+  @param  BufferSize[in][out]   Size of the buffer in bytes pointed to by Buffer / Actual
+                                size of the variable data buffer.
+
+  @param  Buffer[out]           Pointer to the buffer for data to be read.
+
+  @retval EFI_SUCCESS           Data was successfully retrieved into the provided 
+                                buffer and the BufferSize was sufficient to handle the
+                                iSCSI initiator name.
+  @retval EFI_BUFFER_TOO_SMALL  BufferSize is too small for the result. BufferSize will
+                                be updated with the size required to complete the request.
+                                Buffer will not be affected.
+
+  @retval EFI_INVALID_PARAMETER BufferSize is NULL. BufferSize and Buffer will not be
+                                affected.
+
+  @retval EFI_INVALID_PARAMETER Buffer is NULL. BufferSize and Buffer will not be
+                                affected.
+
+  @retval EFI_DEVICE_ERROR      The iSCSI initiator name could not be retrieved due to
+                                a hardware error.
+
+**/
+EFI_STATUS
+EFIAPI
+IScsiGetInitiatorName (
+  IN     EFI_ISCSI_INITIATOR_NAME_PROTOCOL  *This,
+  IN OUT UINTN                              *BufferSize,
+  OUT    VOID                               *Buffer
+  );
+
+/**
+  Sets the iSCSI Initiator Name. 
+
+  @param  This[in]              Pointer to the EFI_ISCSI_INITIATOR_NAME_PROTOCOL instance.
+
+  @param  BufferSize[in][out]   Size of the buffer in bytes pointed to by Buffer.
+
+  @param  Buffer[out]           Pointer to the buffer for data to be written.
+  
+  @retval EFI_SUCCESS           Data was successfully stored by the protocol.
+
+  @retval EFI_UNSUPPORTED       Platform policies do not allow for data to be written.
+
+  @retval EFI_INVALID_PARAMETER BufferSize exceeds the maximum allowed limit.
+                                BufferSize will be updated with the maximum size
+                                required to complete the request.
+
+  @retval EFI_INVALID_PARAMETER Buffersize is NULL. BufferSize and Buffer will not be
+                                affected.
+
+  @retval EFI_INVALID_PARAMETER Buffer is NULL. BufferSize and Buffer will not be affected.
+
+  @retval EFI_DEVICE_ERROR      The data could not be stored due to a hardware error.
+
+  @retval EFI_OUT_OF_RESOURCES  Not enough storage is available to hold the data.
+
+  @retval EFI_PROTOCOL_ERROR    Input iSCSI initiator name does not adhere to RFC 3720.
+
+**/
+EFI_STATUS
+EFIAPI
+IScsiSetInitiatorName (
+  IN     EFI_ISCSI_INITIATOR_NAME_PROTOCOL  *This,
+  IN OUT UINTN                              *BufferSize,
+  IN     VOID                               *Buffer
   );
 
 #endif
