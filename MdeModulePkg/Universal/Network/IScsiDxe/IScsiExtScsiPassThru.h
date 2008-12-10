7a457422df9029083a0cb64f253c82a2d1aa8f21
ywu21 <ywu21@6f19259b-4bc3-4df7-8a09-765794883524>
2008-12-10 03:22:05 +0000

    update file header
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@6954 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/Network/IScsiDxe/IScsiExtScsiPassThru.h
+++ MdeModulePkg/Universal/Network/IScsiDxe/IScsiExtScsiPassThru.h
@@ -1,7 +1,7 @@
 /** @file
-  The header file of IScsiExtScsiPassThru.c
+  The header file of IScsiExtScsiPassThru.c.
 
-Copyright (c) 2004 - 2008, Intel Corporation
+Copyright (c) 2004 - 2008, Intel Corporation.<BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -9,13 +9,6 @@ http://opensource.org/licenses/bsd-license.php
 
 THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
 WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
-Module Name:
-
-  IScsiExtScsiPassThru.h
-
-Abstract:
-  The header file of IScsiExtScsiPassThru.c
 
 **/
 
