04ec589b5f75955675de47224851ef5b10c314df
ywu21 <ywu21@6f19259b-4bc3-4df7-8a09-765794883524>
2008-12-10 02:30:31 +0000

    update file  header update function header update enum variable definition
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@6946 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Universal/Network/IScsiDxe/IScsiDhcp.h
+++ edk2/MdeModulePkg/Universal/Network/IScsiDxe/IScsiDhcp.h
@@ -1,7 +1,7 @@
 /** @file
-  Data structure for DHCP support
+  The header file of IScsiDhcp.
 
-Copyright (c) 2004 - 2008, Intel Corporation
+Copyright (c) 2004 - 2008, Intel Corporation.<BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -9,13 +9,6 @@ http://opensource.org/licenses/bsd-license.php
 
 THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
 WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
-Module Name:
-
-  IScsiDhcp.h
-
-Abstract:
-  The header file of IScsiDhcp.c
 
 **/
 
@@ -33,14 +26,14 @@ Abstract:
 #define ISCSI_ROOT_PATH_ID              "iscsi:"
 #define ISCSI_ROOT_PATH_FIELD_DELIMITER ':'
 
-enum {
+typedef enum {
   RP_FIELD_IDX_SERVERNAME = 0,
   RP_FIELD_IDX_PROTOCOL,
   RP_FIELD_IDX_PORT,
   RP_FIELD_IDX_LUN,
   RP_FIELD_IDX_TARGETNAME,
   RP_FIELD_IDX_MAX
-};
+} RP_FIELD_IDX;
 
 typedef struct _ISCSI_ROOT_PATH_FIELD {
   CHAR8 *Str;
@@ -50,20 +43,13 @@ typedef struct _ISCSI_ROOT_PATH_FIELD {
 /**
   Parse the DHCP ACK to get the address configuration and DNS information.
   
-  @param  Image[in]             The handle of the driver image.
-
-  @param  Controller[in]        The handle of the controller;
-
-  @param  ConfigData[in]        The session configuration data.
+  @param[in]   Image            The handle of the driver image.
+  @param[in]   Controller       The handle of the controller;
+  @param[in]   ConfigData       The session configuration data.
 
   @retval EFI_SUCCESS           The DNS information is got from the DHCP ACK.
-
-  @retval EFI_NO_MAPPING        DHCP failed to acquire address and other information.
-
-  @retval EFI_INVALID_PARAMETER The DHCP ACK's DNS option is mal-formatted.
-
-  @retval EFI_DEVICE_ERROR      Some unexpected error happened.
-
+  @retval EFI_OUT_OF_RESOURCES  Failed to allocate memory.
+  @retval Others                Some unexpected error happened.
 **/
 EFI_STATUS
 IScsiDoDhcp (
