a0c0a4982664113e5402cb4f3a3c582398c8fe05
ywu21 <ywu21@6f19259b-4bc3-4df7-8a09-765794883524>
2008-12-10 03:27:58 +0000

    update file header delete IN from IScsiPublishIbft (   IN VOID   );
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@6956 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Universal/Network/IScsiDxe/IScsiIbft.h
+++ edk2/MdeModulePkg/Universal/Network/IScsiDxe/IScsiIbft.h
@@ -1,7 +1,7 @@
 /** @file
-  Some extra definitions for iBFT
+  Some extra definitions for iBFT.
 
-Copyright (c) 2004 - 2008, Intel Corporation
+Copyright (c) 2004 - 2008, Intel Corporation.<BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -9,14 +9,6 @@ http://opensource.org/licenses/bsd-license.php
 
 THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
 WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
-Module Name:
-
-  IScsiIbft.h
-
-Abstract:
-
-  Some extra definitions for iBFT.
 
 **/
 
@@ -44,7 +36,7 @@ Abstract:
 **/
 VOID
 IScsiPublishIbft (
-  IN VOID
+  VOID
   );
 
 #endif
