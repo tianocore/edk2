d312c2f53709349591f36fb1a57387a7e6f5df86
tding1 <tding1@6f19259b-4bc3-4df7-8a09-765794883524>
2008-12-15 09:16:03 +0000

    clean coding style issue
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@7032 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/IntelFrameworkPkg/Include/Protocol/CpuIo.h
+++ edk2/IntelFrameworkPkg/Include/Protocol/CpuIo.h
@@ -78,11 +78,11 @@ typedef enum {
 typedef
 EFI_STATUS
 (EFIAPI *EFI_CPU_IO_PROTOCOL_IO_MEM)(
-  IN EFI_CPU_IO_PROTOCOL                *This,
-  IN  EFI_CPU_IO_PROTOCOL_WIDTH         Width,
-  IN  UINT64                            Address,
-  IN  UINTN                             Count,
-  IN  OUT VOID                          *Buffer
+  IN     EFI_CPU_IO_PROTOCOL               *This,
+  IN     EFI_CPU_IO_PROTOCOL_WIDTH         Width,
+  IN     UINT64                            Address,
+  IN     UINTN                             Count,
+  IN OUT VOID                              *Buffer
   );
 
 //
