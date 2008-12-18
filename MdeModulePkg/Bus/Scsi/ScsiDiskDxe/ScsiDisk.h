94e0fd072b030c94393cacbcc649bf6d8bd1dc49
jji4 <jji4@6f19259b-4bc3-4df7-8a09-765794883524>
2008-12-18 05:30:57 +0000

    Library class of MemoryAllocationLib has been added.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@7073 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Bus/Scsi/ScsiDiskDxe/ScsiDisk.h
+++ MdeModulePkg/Bus/Scsi/ScsiDiskDxe/ScsiDisk.h
@@ -29,6 +29,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #include <Library/UefiDriverEntryPoint.h>
 #include <Library/UefiLib.h>
 #include <Library/BaseMemoryLib.h>
+#include <Library/MemoryAllocationLib.h>
 #include <Library/UefiScsiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 
