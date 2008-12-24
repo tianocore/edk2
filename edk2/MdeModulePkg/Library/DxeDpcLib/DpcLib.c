74b69267a83d5a571ffd439a0328e8f5245d6487
ywu21 <ywu21@6f19259b-4bc3-4df7-8a09-765794883524>
2008-12-24 13:50:38 +0000

    Pure UEFI code should not include “PiDxe.h” & “PiPei.h”
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@7115 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Library/DxeDpcLib/DpcLib.c
+++ edk2/MdeModulePkg/Library/DxeDpcLib/DpcLib.c
@@ -1,6 +1,7 @@
 /** @file
-
-Copyright (c) 2007, Intel Corporation
+  Help functions to access UDP service.
+  
+Copyright (c) 2005 - 2007, Intel Corporation.<BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -8,13 +9,6 @@ http://opensource.org/licenses/bsd-license.php
 
 THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
 WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
-Module Name:
-
-  DpcLib.c
-
-Abstract:
-
 **/
 
 #include <PiDxe.h>
