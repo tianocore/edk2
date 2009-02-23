73bb16ac15603e89f7f7774db3fa874d34ef19f3
jljusten <jljusten@6f19259b-4bc3-4df7-8a09-765794883524>
2009-02-23 23:14:09 +0000

    Cleanup and remove DuetPkg references.  Allow library to be used with any driver type (increased scope from DXE_DRIVER to BASE).
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@7621 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/PcAtChipsetPkg/Library/SerialIoLib/SerialPortLib.c
+++ edk2/PcAtChipsetPkg/Library/SerialIoLib/SerialPortLib.c
@@ -1,7 +1,7 @@
 /** @file
-  UART Serial Port library instance with empty functions.
+  UART Serial Port library functions
 
-  Copyright (c) 2006 - 2008, Intel Corporation
+  Copyright (c) 2006 - 2009, Intel Corporation
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -13,9 +13,8 @@
 **/
 
 #include <Base.h>
-
-#include <Library/SerialPortLib.h>
 #include <Library/IoLib.h>
+#include <Library/SerialPortLib.h>
 
 //---------------------------------------------
 // UART Register Offsets
