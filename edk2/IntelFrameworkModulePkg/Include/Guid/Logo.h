d98f3f62f37c69125f5dd6eabe2bda937ffb7e61
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2009-03-02 06:17:04 +0000

    remove Bmp.h and ShellFile.h From MdeModulePkg to IntelFrameworkPkg
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@7761 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ edk2/IntelFrameworkModulePkg/Include/Guid/Logo.h
@@ -0,0 +1,23 @@
+/** @file
+  This file defines the File GUID for default BMP logo file.
+
+Copyright (c) 2006 - 2008, Intel Corporation. <BR>
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef _LOGO_GUID_H__
+#define _LOGO_GUID_H__
+
+#define EFI_DEFAULT_BMP_LOGO_GUID \
+  {0x7BB28B99,0x61BB,0x11d5,{0x9A,0x5D,0x00,0x90,0x27,0x3F,0xC1,0x4D}}
+
+extern EFI_GUID gEfiDefaultBmpLogoGuid;
+
+#endif
