247d3676c763548f99afdc7f285249fd9cf73be0
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2009-03-06 09:15:17 +0000

    Correct comments for new null instance.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@7832 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Library/PeCoffExtraActionLib.h
+++ MdePkg/Include/Library/PeCoffExtraActionLib.h
@@ -2,7 +2,7 @@
   Provides services to perform additional actions to relocate and unload
   PE/Coff image for some environment specific purpose say for souce level debug.
 
-  Copyright (c) 2006 - 2008, Intel Corporation<BR>
+  Copyright (c) 2009, Intel Corporation<BR>
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
