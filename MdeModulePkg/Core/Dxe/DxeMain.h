4941d8817da63dc42d8d2dfcedf35ecc56e07b1f
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2009-03-10 07:57:15 +0000

    Retire TCG platform protocol, which will be replaced by PI Security Architecture protocol.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@7848 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Core/Dxe/DxeMain.h
+++ MdeModulePkg/Core/Dxe/DxeMain.h
@@ -48,7 +48,6 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #include <Protocol/Capsule.h>
 #include <Protocol/BusSpecificDriverOverride.h>
 #include <Protocol/TcgService.h>
-#include <Protocol/TcgPlatform.h>
 #include <Guid/MemoryTypeInformation.h>
 #include <Guid/FirmwareFileSystem2.h>
 #include <Guid/HobList.h>
