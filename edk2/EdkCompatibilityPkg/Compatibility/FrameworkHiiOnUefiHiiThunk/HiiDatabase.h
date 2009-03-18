631ac7ad79f34eec3d35c52b065fbdcbce69b2b8
jji4 <jji4@6f19259b-4bc3-4df7-8a09-765794883524>
2009-03-18 06:15:07 +0000

    Fix build break. ISO_639_2_ENTRY_SIZE has been moved to be local.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@7902 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/EdkCompatibilityPkg/Compatibility/FrameworkHiiOnUefiHiiThunk/HiiDatabase.h
+++ edk2/EdkCompatibilityPkg/Compatibility/FrameworkHiiOnUefiHiiThunk/HiiDatabase.h
@@ -69,6 +69,12 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #define FRAMEWORK_RESERVED_VARSTORE_ID 0x0001
 #define FRAMEWORK_RESERVED_VARSTORE_NAME L"Setup"
 
+///
+/// The size of a 3 character ISO639 language code.
+///
+#define ISO_639_2_ENTRY_SIZE            3
+
+
 
 #pragma pack (1)
 typedef struct {
