1d972a710e90f4d8f39b5eaf82a33f992b7771c9
xli24 <xli24@6f19259b-4bc3-4df7-8a09-765794883524>
2009-03-23 13:16:32 +0000

    Fix the issue that VariablePei hangs on Richford when variable region is wiped out.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@7927 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/IntelFrameworkModulePkg/Universal/VariablePei/Variable.c
+++ edk2/IntelFrameworkModulePkg/Universal/VariablePei/Variable.c
@@ -1,7 +1,7 @@
 /** @file
   Framework PEIM to provide the Variable functionality
   
-Copyright (c) 2006 - 2008 Intel Corporation. <BR>
+Copyright (c) 2006 - 2009 Intel Corporation. <BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -426,6 +426,14 @@ FindVariable (
       Variable = IndexTable->StartPtr;
     } else {
       VariableBase = (UINT8 *) (UINTN) PcdGet32 (PcdFlashNvStorageVariableBase);
+
+      //
+      // Check if FV header is valid.
+      //
+      if (((EFI_FIRMWARE_VOLUME_HEADER *) VariableBase)->Signature != EFI_FVH_SIGNATURE) {
+        return EFI_UNSUPPORTED;
+      }
+
       VariableStoreHeader = (VARIABLE_STORE_HEADER *) (VariableBase + \
                             ((EFI_FIRMWARE_VOLUME_HEADER *) (VariableBase)) -> HeaderLength);
 
