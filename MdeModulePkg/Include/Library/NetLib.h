8d7e5af111456e900d18d9b063860923b3eeea28
niry <niry@6f19259b-4bc3-4df7-8a09-765794883524>
2009-03-25 05:42:36 +0000

    Fixing coding style issue.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@7943 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Include/Library/NetLib.h
+++ MdeModulePkg/Include/Library/NetLib.h
@@ -233,7 +233,7 @@ Ip4IsUnicast (
   IN IP4_ADDR               NetMask
   );
 
-extern IP4_ADDR gIp4AllMasks [IP4_MASK_NUM];
+extern IP4_ADDR gIp4AllMasks[IP4_MASK_NUM];
 
 
 extern EFI_IPv4_ADDRESS  mZeroIp4Addr;
