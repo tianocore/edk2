285ef631c4a70c3b825e0dee07df32637b943ce2
gikidy <gikidy@6f19259b-4bc3-4df7-8a09-765794883524>
2009-04-09 07:54:37 +0000

    Useless definition removed and typo corrected.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@8045 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/Uefi/UefiSpec.h
+++ edk2/MdePkg/Include/Uefi/UefiSpec.h
@@ -2010,7 +2010,7 @@ typedef union {
     ///
     /// The SysReq key must be pressed (1) or must not be pressed (0).
     ///
-    UINT32  SysReqPessed    : 1;
+    UINT32  SysReqPressed    : 1;
     UINT32  Reserved        : 16;
     ///
     /// Specifies the actual number of entries in EFI_KEY_OPTION.Keys, from 0-3. If
@@ -2048,14 +2048,6 @@ typedef struct {
   ///
   //EFI_INPUT_KEY      Keys[];
 } EFI_KEY_OPTION;
-
-#define EFI_KEY_OPTION_SHIFT     0x00000001
-#define EFI_KEY_OPTION_CONTROL   0x00000002
-#define EFI_KEY_OPTION_ALT       0x00000004
-#define EFI_KEY_OPTION_LOGO      0x00000008
-#define EFI_KEY_OPTION_MENU      0x00000010
-#define EFI_KEY_OPTION_SYSREQ    0x00000020
-#define EFI_KEY_CODE_COUNT       0x00000300
 
 //
 // EFI File location to boot from on removable media devices
