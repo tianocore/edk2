7547649f22102726e555be072e8975a199b027bf
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2009-04-22 02:06:58 +0000

    Update DxeCore to support to install gEfiHiiPackageListProtocolGuid in LoadImage() if it detects the existence of HII resource on the PE image.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8142 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Core/Dxe/Image/Image.c
+++ MdeModulePkg/Core/Dxe/Image/Image.c
@@ -641,6 +641,14 @@ CoreUnloadAndCloseImage (
                &Image->Info
                );
 
+    if (Image->ImageContext.HiiResourceData != 0) {
+      Status = CoreUninstallProtocolInterface (
+                 Image->Handle,
+                 &gEfiHiiPackageListProtocolGuid,
+                 (VOID *) (UINTN) Image->ImageContext.HiiResourceData
+                 );
+    }
+
   }
 
   if (Image->RuntimeData != NULL) {
@@ -901,6 +909,21 @@ CoreLoadImageCommon (
             );
   if (EFI_ERROR (Status)) {
     goto Done;
+  }
+
+  //
+  // Install HII Package List Protocol onto the image handle
+  //
+  if (Image->ImageContext.HiiResourceData != 0) {
+    Status = CoreInstallProtocolInterface (
+               &Image->Handle,
+               &gEfiHiiPackageListProtocolGuid,
+               EFI_NATIVE_INTERFACE,
+               (VOID *) (UINTN) Image->ImageContext.HiiResourceData
+               );
+    if (EFI_ERROR (Status)) {
+      goto Done;
+    }
   }
 
   //
