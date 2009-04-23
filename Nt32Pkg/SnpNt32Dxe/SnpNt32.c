4324075fce7f9fa66b8aa437dd39878730c5d09f
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2009-04-23 07:19:23 +0000

    fixed one bug when copy MAC address.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8156 6f19259b-4bc3-4df7-8a09-765794883524

--- Nt32Pkg/SnpNt32Dxe/SnpNt32.c
+++ Nt32Pkg/SnpNt32Dxe/SnpNt32.c
@@ -1,6 +1,6 @@
 /** @file
 
-Copyright (c) 2006 - 2007, Intel Corporation
+Copyright (c) 2006 - 2009, Intel Corporation
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -1284,7 +1284,7 @@ SnpNt32InitializeInstanceData (
   CopyMem (
     &Node.MacAddr.MacAddress,
     &Instance->Mode.CurrentAddress,
-    sizeof (EFI_MAC_ADDRESS)
+    NET_ETHER_ADDR_LEN
     );
 
   Node.MacAddr.IfType = Instance->Mode.IfType;
