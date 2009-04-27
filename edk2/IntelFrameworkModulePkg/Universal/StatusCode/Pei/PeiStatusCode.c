9709af123d4a9552395b3874b6011f9d9f0006b3
xli24 <xli24@6f19259b-4bc3-4df7-8a09-765794883524>
2009-04-27 08:47:07 +0000

    Code Scrub for Status Code PEIM.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@8186 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/IntelFrameworkModulePkg/Universal/StatusCode/Pei/PeiStatusCode.c
+++ edk2/IntelFrameworkModulePkg/Universal/StatusCode/Pei/PeiStatusCode.c
@@ -1,7 +1,7 @@
 /** @file
-  Generic PeiStatusCode Module.
+  Status code PEIM which produces Status Code PPI.
 
-  Copyright (c) 2006, Intel Corporation
+  Copyright (c) 2006 - 2009, Intel Corporation
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -10,13 +10,11 @@
   THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
   WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
-  Module Name:  PeiStatusCode.c
-
 **/
 
 #include "PeiStatusCode.h"
 
-EFI_PEI_PROGRESS_CODE_PPI     mStatusCodePpi           = {
+EFI_PEI_PROGRESS_CODE_PPI     mStatusCodePpi = {
   ReportDispatcher
   };
 
@@ -27,40 +25,38 @@ EFI_PEI_PPI_DESCRIPTOR        mStatusCodePpiDescriptor = {
   };
 
 /**
-  Report status code to all supported device.
+  Publishes an interface that allows PEIMs to report status codes.
 
+  This function implements EFI_PEI_PROGRESS_CODE_PPI.ReportStatusCode().
+  It publishes an interface that allows PEIMs to report status codes.
 
-  @param  PeiServices
+  @param  PeiServices      An indirect pointer to the EFI_PEI_SERVICES table published by the PEI Foundation.
+  @param  CodeType         Indicates the type of status code being reported.
+  @param  Value            Describes the current status of a hardware or
+                           software entity. This includes information about the class and
+                           subclass that is used to classify the entity as well as an operation.
+                           For progress codes, the operation is the current activity.
+                           For error codes, it is the exception.For debug codes,it is not defined at this time.
+  @param  Instance         The enumeration of a hardware or software entity within
+                           the system. A system may contain multiple entities that match a class/subclass
+                           pairing. The instance differentiates between them. An instance of 0 indicates
+                           that instance information is unavailable, not meaningful, or not relevant.
+                           Valid instance numbers start with 1.
+  @param  CallerId         This optional parameter may be used to identify the caller.
+                           This parameter allows the status code driver to apply different rules to
+                           different callers.
+  @param  Data             This optional parameter may be used to pass additional data.
 
-  @param  CodeType      Indicates the type of status code being reported.
-                        The type EFI_STATUS_CODE_TYPE is defined in "Related Definitions" below.
-  @param  Value         Describes the current status of a hardware or software entity.
-                        This includes information about the class and subclass that is used to classify the entity
-                        as well as an operation.  For progress codes, the operation is the current activity.
-                        For error codes, it is the exception.  For debug codes, it is not defined at this time.
-                        Type EFI_STATUS_CODE_VALUE is defined in "Related Definitions" below.
-                        Specific values are discussed in the Intel? Platform Innovation Framework for EFI Status Code Specification.
-  @param  Instance      The enumeration of a hardware or software entity within the system.
-                        A system may contain multiple entities that match a class/subclass pairing.
-                        The instance differentiates between them.  An instance of 0 indicates that instance
-                        information is unavailable, not meaningful, or not relevant.  Valid instance numbers start with 1.
-  @param  CallerId      This optional parameter may be used to identify the caller.
-                        This parameter allows the status code driver to apply different rules to different callers.
-  @param  Data          This optional parameter may be used to pass additional data.
-                        Type EFI_STATUS_CODE_DATA is defined in "Related Definitions" below.
-                        The contents of this data type may have additional GUID-specific data.  The standard GUIDs and
-                        their associated data structures are defined in the Intel? Platform Innovation Framework for EFI Status Codes Specification.
-
-  @return               Always return EFI_SUCCESS.
+  @retval EFI_SUCCESS      The function completed successfully.
 
 **/
 EFI_STATUS
 EFIAPI
 ReportDispatcher (
   IN CONST EFI_PEI_SERVICES         **PeiServices,
-  IN EFI_STATUS_CODE_TYPE     CodeType,
-  IN EFI_STATUS_CODE_VALUE    Value,
-  IN UINT32                   Instance,
+  IN EFI_STATUS_CODE_TYPE           CodeType,
+  IN EFI_STATUS_CODE_VALUE          Value,
+  IN UINT32                         Instance,
   IN CONST EFI_GUID                 *CallerId OPTIONAL,
   IN CONST EFI_STATUS_CODE_DATA     *Data OPTIONAL
   )
@@ -82,6 +78,9 @@ ReportDispatcher (
       );
   }
   if (FeaturePcdGet (PcdStatusCodeUseOEM)) {
+    //
+    // Call OEM hook status code library API to report status code to OEM device
+    //
     OemHookStatusCodeReport (
       CodeType,
       Value,
@@ -95,13 +94,16 @@ ReportDispatcher (
 }
 
 /**
-  Initialize PEI status codes and publish the status code
-  PPI.
+  Entry point of Status Code PEIM.
+  
+  This function is the entry point of this Status Code PEIM.
+  It initializes supported status code devices according to PCD settings,
+  and installs Status Code PPI.
 
   @param  FileHandle  Handle of the file being invoked.
   @param  PeiServices Describes the list of possible PEI Services.
 
-  @return                  The function always returns success.
+  @retval EFI_SUCESS  The entry point of DXE IPL PEIM executes successfully.
 
 **/
 EFI_STATUS
@@ -133,9 +135,9 @@ PeiStatusCodeDriverEntry (
   }
 
   //
-  // Install PeiStatusCodePpi.
-  // PeiServices use this Ppi to output status code.
-  // use library
+  // Install Status Code PPI.
+  // It serves the PEI Service ReportStatusCode.
+  //
   Status = PeiServicesInstallPpi (&mStatusCodePpiDescriptor);
   ASSERT_EFI_ERROR (Status);
 
