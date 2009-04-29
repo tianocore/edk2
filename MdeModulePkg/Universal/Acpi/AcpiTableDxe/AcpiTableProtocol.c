6198c34621b38815bbfb116f8fcf656a54470920
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2009-04-29 08:57:19 +0000

    fixed 2 k8 issues.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8204 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/Acpi/AcpiTableDxe/AcpiTableProtocol.c
+++ MdeModulePkg/Universal/Acpi/AcpiTableDxe/AcpiTableProtocol.c
@@ -1,7 +1,7 @@
 /** @file
   ACPI Table Protocol Implementation
 
-  Copyright (c) 2006, 2008, 2009, Intel Corporation<BR>
+  Copyright (c) 2006 - 2009, Intel Corporation<BR>
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -613,45 +613,46 @@ AddTableToList (
           &Buffer64,
           sizeof (UINT64)
           );
+
+        //
+        // RSDP OEM information is updated to match the FADT OEM information
+        //
+        CopyMem (
+          &AcpiTableInstance->Rsdp3->OemId,
+          &AcpiTableInstance->Fadt3->Header.OemId,
+          6
+          );
+        
+        //
+        // RSDT OEM information is updated to match FADT OEM information.
+        //
+        CopyMem (
+          &AcpiTableInstance->Rsdt3->OemId,
+          &AcpiTableInstance->Fadt3->Header.OemId,
+          6
+          );
+        CopyMem (
+          &AcpiTableInstance->Rsdt3->OemTableId,
+          &AcpiTableInstance->Fadt3->Header.OemTableId,
+          sizeof (UINT64)
+          );
+        AcpiTableInstance->Rsdt3->OemRevision = AcpiTableInstance->Fadt3->Header.OemRevision;
+        
+        //
+        // XSDT OEM information is updated to match FADT OEM information.
+        //
+        CopyMem (
+          &AcpiTableInstance->Xsdt->OemId,
+          &AcpiTableInstance->Fadt3->Header.OemId,
+          6
+          );
+        CopyMem (
+          &AcpiTableInstance->Xsdt->OemTableId,
+          &AcpiTableInstance->Fadt3->Header.OemTableId,
+          sizeof (UINT64)
+          );
+        AcpiTableInstance->Xsdt->OemRevision = AcpiTableInstance->Fadt3->Header.OemRevision;
       }
-      //
-      // RSDP OEM information is updated to match the FADT OEM information
-      //
-      CopyMem (
-        &AcpiTableInstance->Rsdp3->OemId,
-        &AcpiTableInstance->Fadt3->Header.OemId,
-        6
-        );
-
-      //
-      // RSDT OEM information is updated to match FADT OEM information.
-      //
-      CopyMem (
-        &AcpiTableInstance->Rsdt3->OemId,
-        &AcpiTableInstance->Fadt3->Header.OemId,
-        6
-        );
-      CopyMem (
-        &AcpiTableInstance->Rsdt3->OemTableId,
-        &AcpiTableInstance->Fadt3->Header.OemTableId,
-        sizeof (UINT64)
-        );
-      AcpiTableInstance->Rsdt3->OemRevision = AcpiTableInstance->Fadt3->Header.OemRevision;
-
-      //
-      // XSDT OEM information is updated to match FADT OEM information.
-      //
-      CopyMem (
-        &AcpiTableInstance->Xsdt->OemId,
-        &AcpiTableInstance->Fadt3->Header.OemId,
-        6
-        );
-      CopyMem (
-        &AcpiTableInstance->Xsdt->OemTableId,
-        &AcpiTableInstance->Fadt3->Header.OemTableId,
-        sizeof (UINT64)
-        );
-      AcpiTableInstance->Xsdt->OemRevision = AcpiTableInstance->Fadt3->Header.OemRevision;
     }    
     //
     // Checksum the table
@@ -1142,6 +1143,10 @@ DeleteTable (
   // Init locals
   //
   RemoveFromRsdt        = TRUE;
+  //
+  // Check for Table->Table
+  //
+  ASSERT (Table->Table != NULL);
   CurrentTableSignature = ((EFI_ACPI_COMMON_HEADER *) Table->Table)->Signature;
 
   //
