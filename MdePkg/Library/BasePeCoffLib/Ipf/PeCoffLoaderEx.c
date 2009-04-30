821b090646b61b332ae22d126c452e8b1b4e83a1
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2009-04-30 03:29:49 +0000

    Update IPF version BasePeCoffLib only supports IFP and EBC PeImage, doesn't support IA32 and X64 PeImage
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8208 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/BasePeCoffLib/Ipf/PeCoffLoaderEx.c
+++ MdePkg/Library/BasePeCoffLib/Ipf/PeCoffLoaderEx.c
@@ -236,8 +236,7 @@ PeCoffLoaderImageFormatSupported (
   IN  UINT16  Machine
   )
 {
-  if ((Machine == IMAGE_FILE_MACHINE_IA64) || (Machine == IMAGE_FILE_MACHINE_I386) ||
-      (Machine ==  IMAGE_FILE_MACHINE_EBC) || (Machine == IMAGE_FILE_MACHINE_X64)) {
+  if ((Machine == IMAGE_FILE_MACHINE_IA64) || (Machine == IMAGE_FILE_MACHINE_EBC)) {
     return TRUE;
   }
 
