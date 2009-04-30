44c4024739e4ccd087770b0875b3f7d9f11d8376
gikidy <gikidy@6f19259b-4bc3-4df7-8a09-765794883524>
2009-04-30 05:36:29 +0000

    Replace references to RFC 3066 with RFC 4646.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8213 6f19259b-4bc3-4df7-8a09-765794883524

--- Nt32Pkg/WinNtBusDriverDxe/ComponentName.c
+++ Nt32Pkg/WinNtBusDriverDxe/ComponentName.c
@@ -57,7 +57,7 @@ Abstract:
                                 languages specified in SupportedLanguages. The
                                 number of languages supported by a driver is up
                                 to the driver writer. Language is specified
-                                in RFC 3066 or ISO 639-2 language code format.
+                                in RFC 4646 or ISO 639-2 language code format.
 
   @param  DriverName[out]       A pointer to the Unicode string to return.
                                 This Unicode string is the name of the
@@ -122,7 +122,7 @@ WinNtBusDriverComponentNameGetDriverName (
                                 languages specified in SupportedLanguages. The
                                 number of languages supported by a driver is up
                                 to the driver writer. Language is specified in
-                                RFC 3066 or ISO 639-2 language code format.
+                                RFC 4646 or ISO 639-2 language code format.
 
   @param  ControllerName[out]   A pointer to the Unicode string to return.
                                 This Unicode string is the name of the
@@ -208,7 +208,7 @@ GLOBAL_REMOVE_IF_UNREFERENCED EFI_UNICODE_STRING_TABLE mWinNtBusDriverNameTable[
                                 languages specified in SupportedLanguages. The
                                 number of languages supported by a driver is up
                                 to the driver writer. Language is specified
-                                in RFC 3066 or ISO 639-2 language code format.
+                                in RFC 4646 or ISO 639-2 language code format.
 
   @param  DriverName[out]       A pointer to the Unicode string to return.
                                 This Unicode string is the name of the
@@ -281,7 +281,7 @@ WinNtBusDriverComponentNameGetDriverName (
                                 languages specified in SupportedLanguages. The
                                 number of languages supported by a driver is up
                                 to the driver writer. Language is specified in
-                                RFC 3066 or ISO 639-2 language code format.
+                                RFC 4646 or ISO 639-2 language code format.
 
   @param  ControllerName[out]   A pointer to the Unicode string to return.
                                 This Unicode string is the name of the
--- Nt32Pkg/WinNtSerialIoDxe/ComponentName.c
+++ Nt32Pkg/WinNtSerialIoDxe/ComponentName.c
@@ -42,7 +42,7 @@ Abstract:
                                 languages specified in SupportedLanguages. The
                                 number of languages supported by a driver is up
                                 to the driver writer. Language is specified
-                                in RFC 3066 or ISO 639-2 language code format.
+                                in RFC 4646 or ISO 639-2 language code format.
 
   @param  DriverName[out]       A pointer to the Unicode string to return.
                                 This Unicode string is the name of the
@@ -107,7 +107,7 @@ WinNtSerialIoComponentNameGetDriverName (
                                 languages specified in SupportedLanguages. The
                                 number of languages supported by a driver is up
                                 to the driver writer. Language is specified in
-                                RFC 3066 or ISO 639-2 language code format.
+                                RFC 4646 or ISO 639-2 language code format.
 
   @param  ControllerName[out]   A pointer to the Unicode string to return.
                                 This Unicode string is the name of the
@@ -193,7 +193,7 @@ GLOBAL_REMOVE_IF_UNREFERENCED EFI_UNICODE_STRING_TABLE mWinNtSerialIoDriverNameT
                                 languages specified in SupportedLanguages. The
                                 number of languages supported by a driver is up
                                 to the driver writer. Language is specified
-                                in RFC 3066 or ISO 639-2 language code format.
+                                in RFC 4646 or ISO 639-2 language code format.
 
   @param  DriverName[out]       A pointer to the Unicode string to return.
                                 This Unicode string is the name of the
@@ -266,7 +266,7 @@ WinNtSerialIoComponentNameGetDriverName (
                                 languages specified in SupportedLanguages. The
                                 number of languages supported by a driver is up
                                 to the driver writer. Language is specified in
-                                RFC 3066 or ISO 639-2 language code format.
+                                RFC 4646 or ISO 639-2 language code format.
 
   @param  ControllerName[out]   A pointer to the Unicode string to return.
                                 This Unicode string is the name of the
