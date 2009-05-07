85a9032180adb8c489c44ab408dcc539d95d24ec
xli24 <xli24@6f19259b-4bc3-4df7-8a09-765794883524>
2009-05-07 08:51:16 +0000

    Refine code for OemHookStatusCodeLibNull.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8255 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkModulePkg/Library/OemHookStatusCodeLibNull/OemHookStatusCodeLibNull.c
+++ IntelFrameworkModulePkg/Library/OemHookStatusCodeLibNull/OemHookStatusCodeLibNull.c
@@ -1,7 +1,7 @@
 /** @file
-  OEM hook status code library functions with no library constructor/destructor
+  Null instance of OEM Hook Status Code Library with empty functions.
 
-  Copyright (c) 2006, Intel Corporation
+  Copyright (c) 2006 - 2009, Intel Corporation
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -14,10 +14,9 @@
 
 
 /**
-
   Initialize OEM status code device .
 
-  @return     Always return EFI_SUCCESS.
+  @retval  EFI_SUCCESS   Always return EFI_SUCCESS.
 
 **/
 EFI_STATUS
@@ -32,29 +31,20 @@ OemHookStatusCodeInitialize (
 /**
   Report status code to OEM device.
  
-  @param  CodeType      Indicates the type of status code being reported.  Type EFI_STATUS_CODE_TYPE is defined in "Related Definitions" below.
- 
+  @param  CodeType      Indicates the type of status code being reported.
   @param  Value         Describes the current status of a hardware or software entity.  
                         This included information about the class and subclass that is used to classify the entity 
                         as well as an operation.  For progress codes, the operation is the current activity. 
                         For error codes, it is the exception.  For debug codes, it is not defined at this time. 
-                        Type EFI_STATUS_CODE_VALUE is defined in "Related Definitions" below.  
-                        Specific values are discussed in the Intel? Platform Innovation Framework for EFI Status Code Specification.
- 
   @param  Instance      The enumeration of a hardware or software entity within the system.  
                         A system may contain multiple entities that match a class/subclass pairing. 
                         The instance differentiates between them.  An instance of 0 indicates that instance information is unavailable, 
                         not meaningful, or not relevant.  Valid instance numbers start with 1.
-
-
   @param  CallerId      This optional parameter may be used to identify the caller. 
                         This parameter allows the status code driver to apply different rules to different callers. 
-                        Type EFI_GUID is defined in InstallProtocolInterface() in the UEFI 2.0 Specification.
-
-
   @param  Data          This optional parameter may be used to pass additional data
  
-  @return               The function always return EFI_SUCCESS.
+  @retval EFI_SUCCESS   Always return EFI_SUCCESS.
 
 **/
 EFI_STATUS
