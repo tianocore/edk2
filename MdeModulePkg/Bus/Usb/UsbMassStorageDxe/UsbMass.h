776a705ef181555725c619a715274b66d84a4c19
mdkinney <mdkinney@6f19259b-4bc3-4df7-8a09-765794883524>
2009-05-20 20:04:23 +0000

    Update USB Mass Storage driver to declare its dependency on the BaseLib
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8353 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Bus/Usb/UsbMassStorageDxe/UsbMass.h
+++ MdeModulePkg/Bus/Usb/UsbMassStorageDxe/UsbMass.h
@@ -23,6 +23,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #include <Protocol/UsbIo.h>
 #include <Protocol/DevicePath.h>
 
+#include <Library/BaseLib.h>
 #include <Library/DebugLib.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/UefiDriverEntryPoint.h>
