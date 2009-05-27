b6207b05f97b5fba0a5dd07602da68784c6f982b
jljusten <jljusten@6f19259b-4bc3-4df7-8a09-765794883524>
2009-05-27 21:09:47 +0000

    Add CPU DXE driver for IA32 & X64 processor architectures.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@8395 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ edk2/UefiCpuPkg/CpuDxe/CpuDxe.h
@@ -0,0 +1,140 @@
+/** @file
+  CPU DXE Module.
+
+  Copyright (c) 2008 - 2009, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef _CPU_DXE_H
+#define _CPU_DXE_H
+
+#include <PiDxe.h>
+
+#include <Protocol/Cpu.h>
+
+#include <Library/UefiDriverEntryPoint.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/DxeServicesTableLib.h>
+#include <Library/BaseLib.h>
+#include <Library/CpuLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/DebugLib.h>
+#include <Library/MtrrLib.h>
+
+//
+//
+//
+#define INTERRUPT_VECTOR_NUMBER   256
+
+#define EFI_MEMORY_CACHETYPE_MASK     (EFI_MEMORY_UC  | \
+                                       EFI_MEMORY_WC  | \
+                                       EFI_MEMORY_WT  | \
+                                       EFI_MEMORY_WB  | \
+                                       EFI_MEMORY_UCE   \
+                                       )
+
+
+//
+// Function declarations
+//
+EFI_STATUS
+EFIAPI
+CpuFlushCpuDataCache (
+  IN EFI_CPU_ARCH_PROTOCOL     *This,
+  IN EFI_PHYSICAL_ADDRESS      Start,
+  IN UINT64                    Length,
+  IN EFI_CPU_FLUSH_TYPE        FlushType
+  );
+
+EFI_STATUS
+EFIAPI
+CpuEnableInterrupt (
+  IN EFI_CPU_ARCH_PROTOCOL     *This
+  );
+
+EFI_STATUS
+EFIAPI
+CpuDisableInterrupt (
+  IN EFI_CPU_ARCH_PROTOCOL     *This
+  );
+
+EFI_STATUS
+EFIAPI
+CpuGetInterruptState (
+  IN  EFI_CPU_ARCH_PROTOCOL     *This,
+  OUT BOOLEAN                   *State
+  );
+
+EFI_STATUS
+EFIAPI
+CpuInit (
+  IN EFI_CPU_ARCH_PROTOCOL     *This,
+  IN EFI_CPU_INIT_TYPE         InitType
+  );
+
+EFI_STATUS
+EFIAPI
+CpuRegisterInterruptHandler (
+  IN EFI_CPU_ARCH_PROTOCOL         *This,
+  IN EFI_EXCEPTION_TYPE            InterruptType,
+  IN EFI_CPU_INTERRUPT_HANDLER     InterruptHandler
+  );
+
+EFI_STATUS
+EFIAPI
+CpuGetTimerValue (
+  IN  EFI_CPU_ARCH_PROTOCOL       *This,
+  IN  UINT32                      TimerIndex,
+  OUT UINT64                      *TimerValue,
+  OUT UINT64                      *TimerPeriod OPTIONAL
+  );
+
+EFI_STATUS
+EFIAPI
+CpuSetMemoryAttributes (
+  IN EFI_CPU_ARCH_PROTOCOL      *This,
+  IN EFI_PHYSICAL_ADDRESS       BaseAddress,
+  IN UINT64                     Length,
+  IN UINT64                     Attributes
+  );
+
+VOID
+EFIAPI
+AsmIdtVector00 (
+  VOID
+  );
+
+VOID
+EFIAPI
+InitializeExternalVectorTablePtr (
+  EFI_CPU_INTERRUPT_HANDLER  *VectorTable
+  );
+
+VOID
+InitGlobalDescriptorTable (
+  VOID
+  );
+
+VOID
+EFIAPI
+SetCodeSelector (
+  UINT16 Selector
+  );
+
+VOID
+EFIAPI
+SetDataSelectors (
+  UINT16 Selector
+  );
+
+
+#endif
+
---
+++ edk2/UefiCpuPkg/CpuDxe/CpuGdt.c
@@ -0,0 +1,200 @@
+/** @file
+  C based implemention of IA32 interrupt handling only
+  requiring a minimal assembly interrupt entry point.
+
+  Copyright (c) 2006 - 2009, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#include "CpuDxe.h"
+
+
+//
+// Local structure definitions
+//
+
+#pragma pack (1)
+
+//
+// Global Descriptor Entry structures
+//
+
+typedef
+struct _GDT_ENTRY {
+  UINT16 limit15_0;
+  UINT16 base15_0;
+  UINT8  base23_16;
+  UINT8  type;
+  UINT8  limit19_16_and_flags;
+  UINT8  base31_24;
+} GDT_ENTRY;
+
+typedef
+struct _GDT_ENTRIES {
+  GDT_ENTRY Null;
+  GDT_ENTRY Linear;
+  GDT_ENTRY LinearCode;
+  GDT_ENTRY SysData;
+  GDT_ENTRY SysCode;
+  GDT_ENTRY LinearCode64;
+  GDT_ENTRY Spare4;
+  GDT_ENTRY Spare5;
+} GDT_ENTRIES;
+
+#define NULL_SEL          OFFSET_OF (GDT_ENTRIES, Null)
+#define LINEAR_SEL        OFFSET_OF (GDT_ENTRIES, Linear)
+#define LINEAR_CODE_SEL   OFFSET_OF (GDT_ENTRIES, LinearCode)
+#define SYS_DATA_SEL      OFFSET_OF (GDT_ENTRIES, SysData)
+#define SYS_CODE_SEL      OFFSET_OF (GDT_ENTRIES, SysCode)
+#define LINEAR_CODE64_SEL OFFSET_OF (GDT_ENTRIES, LinearCode64)
+#define SPARE4_SEL        OFFSET_OF (GDT_ENTRIES, Spare4)
+#define SPARE5_SEL        OFFSET_OF (GDT_ENTRIES, Spare5)
+
+#if defined (MDE_CPU_IA32)
+#define CPU_CODE_SEL LINEAR_CODE_SEL
+#define CPU_DATA_SEL LINEAR_SEL
+#elif defined (MDE_CPU_X64)
+#define CPU_CODE_SEL LINEAR_CODE64_SEL
+#define CPU_DATA_SEL LINEAR_SEL
+#else
+#error CPU type not supported for CPU GDT initialization!
+#endif
+
+//
+// Global descriptor table (GDT) Template
+//
+STATIC GDT_ENTRIES GdtTemplate = {
+  //
+  // NULL_SEL
+  //
+  {
+    0x0,            // limit 15:0
+    0x0,            // base 15:0
+    0x0,            // base 23:16
+    0x0,            // type
+    0x0,            // limit 19:16, flags
+    0x0,            // base 31:24
+  },
+  //
+  // LINEAR_SEL
+  //
+  {
+    0x0FFFF,        // limit 0xFFFFF
+    0x0,            // base 0
+    0x0,
+    0x092,          // present, ring 0, data, expand-up, writable
+    0x0CF,          // page-granular, 32-bit
+    0x0,
+  },
+  //
+  // LINEAR_CODE_SEL
+  //
+  {
+    0x0FFFF,        // limit 0xFFFFF
+    0x0,            // base 0
+    0x0,
+    0x09A,          // present, ring 0, data, expand-up, writable
+    0x0CF,          // page-granular, 32-bit
+    0x0,
+  },
+  //
+  // SYS_DATA_SEL
+  //
+  {
+    0x0FFFF,        // limit 0xFFFFF
+    0x0,            // base 0
+    0x0,
+    0x092,          // present, ring 0, data, expand-up, writable
+    0x0CF,          // page-granular, 32-bit
+    0x0,
+  },
+  //
+  // SYS_CODE_SEL
+  //
+  {
+    0x0FFFF,        // limit 0xFFFFF
+    0x0,            // base 0
+    0x0,
+    0x09A,          // present, ring 0, data, expand-up, writable
+    0x0CF,          // page-granular, 32-bit
+    0x0,
+  },
+  //
+  // LINEAR_CODE64_SEL
+  //
+  {
+    0x0FFFF,        // limit 0xFFFFF
+    0x0,            // base 0
+    0x0,
+    0x09B,          // present, ring 0, code, expand-up, writable
+    0x0AF,          // LimitHigh (CS.L=1, CS.D=0)
+    0x0,            // base (high)
+  },
+  //
+  // SPARE4_SEL
+  //
+  {
+    0x0,            // limit 0
+    0x0,            // base 0
+    0x0,
+    0x0,            // present, ring 0, data, expand-up, writable
+    0x0,            // page-granular, 32-bit
+    0x0,
+  },
+  //
+  // SPARE5_SEL
+  //
+  {
+    0x0,            // limit 0
+    0x0,            // base 0
+    0x0,
+    0x0,            // present, ring 0, data, expand-up, writable
+    0x0,            // page-granular, 32-bit
+    0x0,
+  },
+};
+
+/**
+  Initialize Global Descriptor Table
+
+**/
+VOID
+InitGlobalDescriptorTable (
+  )
+{
+  GDT_ENTRIES *gdt;
+  IA32_DESCRIPTOR gdtPtr;
+
+  //
+  // Allocate Runtime Data for the GDT
+  //
+  gdt = AllocateRuntimePool (sizeof (GdtTemplate) + 8);
+  ASSERT (gdt != NULL);
+  gdt = ALIGN_POINTER (gdt, 8);
+
+  //
+  // Initialize all GDT entries
+  //
+  CopyMem (gdt, &GdtTemplate, sizeof (GdtTemplate));
+
+  //
+  // Write GDT register
+  //
+  gdtPtr.Base = (UINT32)(UINTN)(VOID*) gdt;
+  gdtPtr.Limit = sizeof (GdtTemplate) - 1;
+  AsmWriteGdtr (&gdtPtr);
+
+  //
+  // Update selector (segment) registers base on new GDT
+  //
+  SetCodeSelector ((UINT16)CPU_CODE_SEL);
+  SetDataSelectors ((UINT16)CPU_DATA_SEL);
+}
+
