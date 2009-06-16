9f6f953491116b312e4243b1cd7b97f40f8a7dd7
rsun3 <rsun3@6f19259b-4bc3-4df7-8a09-765794883524>
2009-06-16 08:33:16 +0000

    Update comments.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8574 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkPkg/Include/Framework/SmmCis.h
+++ IntelFrameworkPkg/Include/Framework/SmmCis.h
@@ -1,7 +1,8 @@
 /** @file
-  Include file matches things in the Smm CIS spec.
+  Include file for definitions in the Intel Platform Innovation Framework for EFI
+  System Management Mode Core Interface Specification (SMM CIS) version 0.90.
 
-  Copyright (c) 2007, Intel Corporation
+  Copyright (c) 2007 - 2009, Intel Corporation
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -9,11 +10,6 @@
 
   THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
   WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
-  Module Name:  FrameworkSmmCis.h
-
-  @par Revision Reference:
-  VOLUME 4: Platform Initialization Specification,System Management Mode Core Interface, Version 1.1.
 
 **/
 
