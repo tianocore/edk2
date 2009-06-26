5ba364466dabf4e4d8809218197b5bd0761db881
darylm503 <darylm503@6f19259b-4bc3-4df7-8a09-765794883524>
2009-06-26 19:44:34 +0000

    Update Copyright. Delete erroneous "Module Name" line.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8673 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkPkg/Include/FrameworkDxe.h
+++ IntelFrameworkPkg/Include/FrameworkDxe.h
@@ -4,7 +4,7 @@
   This header file must include Framework extension definitions common to DXE
   modules.
 
-  Copyright (c) 2007, Intel Corporation
+  Copyright (c) 2007-2009, Intel Corporation
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -12,9 +12,6 @@
 
   THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
   WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
-  Module Name:    FrameworkDxe.h
-
 **/
 
 #ifndef _FRAMEWORK_DXE_H_
