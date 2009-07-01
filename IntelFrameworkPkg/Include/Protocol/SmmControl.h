053d977438b4db210d1e545825737730523ea505
lgr <lgr@6f19259b-4bc3-4df7-8a09-765794883524>
2009-07-01 15:57:13 +0000

    Per code review, Update Doxygen comments - add /// comments from spec to data and protocol struct. Add some missing retval's to func comments
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8704 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkPkg/Include/Protocol/SmmControl.h
+++ IntelFrameworkPkg/Include/Protocol/SmmControl.h
@@ -9,7 +9,7 @@
   event from a platform chipset agent is an optional capability for both IA-32 and Itanium based
   systems.
 
-  Copyright (c) 2007, Intel Corporation
+  Copyright (c) 2007,2009 Intel Corporation
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -41,7 +41,15 @@ typedef struct _EFI_SMM_CONTROL_PROTOCOL              EFI_SMM_CONTROL_PROTOCOL;
 // SMM Access specification Data Structures
 //
 typedef struct {
+	/// 
+	///  Describes the I/O location of the particular port that engendered the synchronous
+	///  SMI. For example, this location can include but is not limited to the traditional 
+	///  PCAT* APM port of 0B2h.
+	///
   UINT8 SmiTriggerRegister;
+	///
+	///  Describes the value that was written to the respective activation port.
+	///
   UINT8 SmiDataRegister;
 } EFI_SMM_CONTROL_REGISTER;
 
@@ -100,6 +108,8 @@ EFI_STATUS
   @param  SmiRegister           Pointer to the SMI register description structure
 
   @retval EFI_SUCCESS           The register structure has been returned.
+	@retval	EFI_DEVICE_ERROR	 The source could not be cleared.
+	@retval	EFI_INVALID_PARAMETER The service did not support the Periodic input argument.
 
 **/
 typedef
@@ -125,12 +135,45 @@ EFI_STATUS
   @param MinimumTriggerPeriod
   Minimum interval at which the platform can set the period.
 
+	@retval	EFI_SUCCESS The register structure has been returned.
 **/
 
+//
+// SMM Control Protocol
+//
+/**
+	This protocol is used initiate SMI/PMI activations. 
+	This protocol could be published by either of the following:
+		- A processor driver to abstract the SMI/PMI IPI
+		- The driver that abstracts the ASIC that is supporting the APM port, such as the ICH in an Intel® chipset
+	Because of the possibility of performing SMI or PMI IPI transactions, the ability to generate this
+  
+	The EFI_SMM_CONTROL_PROTOCOL is used by the platform chipset or processor driver. This
+  protocol is useable both in boot services and runtime. The runtime aspect is so that an
+  implementation of EFI_SMM_BASE_PROTOCOL.Communicate() can layer upon this service
+  and provide an SMI callback from a general EFI runtime driver.
+  The purpose of this protocol is to provide an abstraction to the platform hardware that generates an
+  SMI or PMI. There are often I/O ports that, when accessed, will engender the
+**/
 struct _EFI_SMM_CONTROL_PROTOCOL {
+	///
+	///  Initiates the SMI/PMI activation.
+	///
   EFI_SMM_ACTIVATE          Trigger;
+	///
+	///  Quiesces the SMI/PMI activation.
+	///
   EFI_SMM_DEACTIVATE        Clear;
+	///
+	/// Provides data on the register used as the source of the SMI.
+	///
   EFI_SMM_GET_REGISTER_INFO GetRegisterInfo;
+	///
+	/// Minimum interval at which the platform can set the period. A maximum is not
+	/// specified in that the SMM infrastructure code can emulate a maximum interval that is
+	/// greater than the hardware capabilities by using software emulation in the SMM
+	/// infrastructure code.
+	///
   UINTN                     MinimumTriggerPeriod;
 };
 
