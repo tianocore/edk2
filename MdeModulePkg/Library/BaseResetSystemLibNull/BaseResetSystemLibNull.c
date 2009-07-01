064d5de3784253c623a1941c6afaf02d0dde3fbc
mdkinney <mdkinney@6f19259b-4bc3-4df7-8a09-765794883524>
2009-07-01 00:03:38 +0000

    Add Null implementation of the Reset System Library that generated an ASSERT() on all reset APIs.
    This library can be used as a template to implement a Reset System Library for a specific platform.
    
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8693 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Library/BaseResetSystemLibNull/BaseResetSystemLibNull.c
+++ MdeModulePkg/Library/BaseResetSystemLibNull/BaseResetSystemLibNull.c
@@ -1,25 +1,26 @@
 /** @file
-  System reset Library Services.  This library class defines a set of
-  methods that reset the whole system.
+  Null Reset System Library instance that only generates ASSERT() conditions.
 
-  Copyright (c) 2005 - 2007, Intel Corporation
+  Copyright (c) 2006 - 2009, Intel Corporation<BR>
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
   http://opensource.org/licenses/bsd-license.php
-  
+
   THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
   WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
-#ifndef __RESET_SYSTEM_LIB_H__
-#define __RESET_SYSTEM_LIB_H__
+#include <Base.h>
+
+#include <Library/ResetSystemLib.h>
+#include <Library/DebugLib.h>
 
 /**
   This function causes a system-wide reset (cold reset), in which
   all circuitry within the system returns to its initial state. This type of reset 
-  is asynchronous to system operation and operates without regard to 
+  is asynchronous to system operation and operates without regard to
   cycle boundaries.
 
   If this function returns, it means that the system does not support cold reset. 
@@ -28,7 +29,10 @@ VOID
 EFIAPI
 ResetCold (
   VOID
-  );
+  )
+{
+  ASSERT (FALSE);
+}
 
 /**
   This function causes a system-wide initialization (warm reset), in which all processors 
@@ -40,29 +44,36 @@ VOID
 EFIAPI
 ResetWarm (
   VOID
-  );
+  )
+{
+  ASSERT (FALSE);
+}
 
 /**
   This function causes the system to enter a power state equivalent 
   to the ACPI G2/S5 or G3 states.
-  
+
   If this function returns, it means that the system does not support shut down reset.
 **/
 VOID
 EFIAPI
 ResetShutdown (
   VOID
-  );
+  )
+{
+  ASSERT (FALSE);
+}
 
 /**
   This function causes the system to enter S3 and then wake up immediately.
-  
+
   If this function returns, it means that the system does not support S3 feature.
 **/
 VOID
 EFIAPI
 EnterS3WithImmediateWake (
   VOID
-  );
-  
-#endif
+  )
+{
+  ASSERT (FALSE);
+}
