8c2884df8b6f1d674a67965ccccd275fb961060a
rsun3 <rsun3@6f19259b-4bc3-4df7-8a09-765794883524>
2009-07-03 02:42:12 +0000

    Change naming of bit value definitions of AuthenticationStatus for Section Extraction PPI to match PEI CIS spec. Also do some coding style clean up.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@8731 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/IntelFrameworkPkg/Include/Ppi/SectionExtraction.h
+++ edk2/IntelFrameworkPkg/Include/Ppi/SectionExtraction.h
@@ -1,9 +1,10 @@
 /** @file
   This file declares Section Extraction PPI.
-  This PPI supports encapsulating sections, such as GUIDed sections used to authenticate the file
-  encapsulation of other domain-specific wrapping.
 
-  Copyright (c) 2006, Intel Corporation
+  This PPI is defined in PEI CIS version 0.91. It supports encapsulating sections,
+  such as GUIDed sections used to authenticate the file encapsulation of other domain-specific wrapping.
+
+  Copyright (c) 2006-2009, Intel Corporation
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -12,16 +13,10 @@
   THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
   WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
-  Module Name:  SectionExtraction.h
-
-  @par Revision Reference:
-  This PPI is defined in PEI CIS.
-  Version 0.91.
-
 **/
 
-#ifndef __SECTION_EXTRACTION_PPI_H__
-#define __SECTION_EXTRACTION_PPI_H__
+#ifndef __SECTION_EXTRACTION_H__
+#define __SECTION_EXTRACTION_H__
 
 #include <PiPei.h>
 
@@ -35,37 +30,35 @@ typedef struct _EFI_PEI_SECTION_EXTRACTION_PPI EFI_PEI_SECTION_EXTRACTION_PPI;
 //
 // Bit values for AuthenticationStatus
 //
-#define EFI_PEI_AUTH_STATUS_PLATFORM_OVERRIDE 0x01
-#define EFI_PEI_AUTH_STATUS_IMAGE_SIGNED      0x02
-#define EFI_PEI_AUTH_STATUS_NOT_TESTED        0x04
-#define EFI_PEI_AUTH_STATUS_TEST_FAILED       0x08
+#define EFI_AUTH_STATUS_PLATFORM_OVERRIDE 0x01
+#define EFI_AUTH_STATUS_IMAGE_SIGNED      0x02
+#define EFI_AUTH_STATUS_NOT_TESTED        0x04
+#define EFI_AUTH_STATUS_TEST_FAILED       0x08
 
 /**
   The function is used to retrieve a section from within a section file.
   It will retrieve both encapsulation sections and leaf sections in their entirety,
   exclusive of the section header.
 
-  @param  PeiServices    Pointer to the PEI Services Table.
-  @param  This           Indicates the calling context
-  @param  SectionType    Pointer to an EFI_SECTION_TYPE. If SectionType == NULL,
-                         the contents of the entire section are returned in Buffer. If SectionType
-                         is not NULL, only the requested section is returned.
-  @param  SectionDefinitionGuid
-                         Pointer to an EFI_GUID.
-                         If SectionType == EFI_SECTION_GUID_DEFINED, SectionDefinitionGuid
-                         indicates for which section GUID to search.
-                         If SectionType != EFI_SECTION_GUID_DEFINED, SectionDefinitionGuid
-                         is unused and is ignored.
-  @param  SectionInstance If SectionType is not NULL, indicates which
-                         instance of the requested section type to return.
-  @param  Buffer         Pointer to a pointer to a buffer in which the section
-                         contents are returned.
-  @param  BufferSize     A pointer to a caller-allocated UINT32.On input, *BufferSize
-                         indicates the size in bytes of the memory region pointed to by Buffer.On output,
-                         *BufferSize contains the number of bytes required to read the section.
-  @param  AuthenticationStatus
-                         A pointer to a caller-allocated UINT32 in
-                         which any metadata from encapsulating GUID-defined sections is returned.
+  @param PeiServices            Pointer to the PEI Services Table.
+  @param This                   Indicates the calling context
+  @param SectionType            Pointer to an EFI_SECTION_TYPE. If SectionType == NULL,
+                                the contents of the entire section are returned in Buffer. If SectionType
+                                is not NULL, only the requested section is returned.
+  @param SectionDefinitionGuid  Pointer to an EFI_GUID.
+                                If SectionType == EFI_SECTION_GUID_DEFINED, SectionDefinitionGuid
+                                indicates for which section GUID to search.
+                                If SectionType != EFI_SECTION_GUID_DEFINED, SectionDefinitionGuid
+                                is unused and is ignored.
+  @param SectionInstance        If SectionType is not NULL, indicates which
+                                instance of the requested section type to return.
+  @param Buffer                 Pointer to a pointer to a buffer in which the section
+                                contents are returned.
+  @param BufferSize             A pointer to a caller-allocated UINT32.On input, *BufferSize
+                                indicates the size in bytes of the memory region pointed to by Buffer.On output,
+                                *BufferSize contains the number of bytes required to read the section.
+  @param AuthenticationStatus   A pointer to a caller-allocated UINT32 in
+                                which any metadata from encapsulating GUID-defined sections is returned.
 
   @retval EFI_SUCCESS           The section was successfully processed and the section
                                 contents were returned in Buffer.
@@ -105,3 +98,4 @@ struct _EFI_PEI_SECTION_EXTRACTION_PPI {
 extern EFI_GUID gEfiPeiSectionExtractionPpiGuid;
 
 #endif
+
