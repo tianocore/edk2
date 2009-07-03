3e5386266a7199fde23b7bdb467b3d83845a2ff3
mdkinney <mdkinney@6f19259b-4bc3-4df7-8a09-765794883524>
2009-07-03 00:52:28 +0000

    Update comments
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@8729 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/IntelFrameworkModulePkg/Include/Guid/StatusCodeDataTypeDebug.h
+++ edk2/IntelFrameworkModulePkg/Include/Guid/StatusCodeDataTypeDebug.h
@@ -1,6 +1,6 @@
 /** @file
-  GUID used to pass DEBUG() macro information through the Status Code Protocol
-  and Status Code PPI
+  This file defines the GUID and data structure used to pass DEBUG() macro 
+  information to the Status Code Protocol and Status Code PPI.
 
   Copyright (c) 2007 - 2009, Intel Corporation
   All rights reserved. This program and the accompanying materials
@@ -16,22 +16,32 @@
 #ifndef _STATUS_CODE_DATA_TYPE_DEBUG_H_
 #define _STATUS_CODE_DATA_TYPE_DEBUG_H_
 
-//
-// Debug DataType defintions.
-//
-
+///
+/// Global ID used to identify a structure of type EFI_DEBUG_INFO
+///
 #define EFI_STATUS_CODE_DATA_TYPE_DEBUG_GUID \
-  { 0x9A4E9246, 0xD553, 0x11D5, { 0x87, 0xE2, 0x00, 0x06, 0x29, 0x45, 0xC3, 0xb9 } }
+  { \
+    0x9A4E9246, 0xD553, 0x11D5, { 0x87, 0xE2, 0x00, 0x06, 0x29, 0x45, 0xC3, 0xb9 } \
+  }
 
+///
+/// The maximum size of an EFI_DEBUG_INO structure
+///
 #define EFI_STATUS_CODE_DATA_MAX_SIZE 200
 
+///
+/// This structire contains the ErrorLevel passed into the DEBUG() macro followed 
+/// by a 96-byte buffer that contains the variable argument list passed to the 
+//  DEBUG() macro that has been converted to a BASE_LIST.  The 96-byte buffer is 
+/// followed by a Null terminated ASCII string that is the Format string passed 
+/// to the DEBUG() macro.  The maximum size of this structure is defined by 
+/// EFI_STATUS_CODE_DATAS_MAX_SIZE.
+///
 typedef struct {
+  ///
+  /// The debug error level passed into a DEBUG() macro.
+  ///
   UINT32  ErrorLevel;
-  //
-  // 12 * sizeof (UINT64) area to store variable arguments for format in DEBUG string
-  //
-  // ascii DEBUG () Format string
-  //
 } EFI_DEBUG_INFO;
 
 extern EFI_GUID gEfiStatusCodeDataTypeDebugGuid;
