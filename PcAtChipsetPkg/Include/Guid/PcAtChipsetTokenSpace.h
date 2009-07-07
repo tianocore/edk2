af6f4f4b27bc1d2e6200dbeff38a8f2f21d3dc4a
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2009-07-07 06:38:18 +0000

    Add declaration header file for token space.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8781 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ PcAtChipsetPkg/Include/Guid/PcAtChipsetTokenSpace.h
@@ -0,0 +1,25 @@
+/** @file
+  GUID for PcAtChipsetPkg PCD Token Space 
+
+  Copyright (c) 2009, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef _PCATCHIPSET_TOKEN_SPACE_GUID_H_
+#define _PCATCHIPSET_TOKEN_SPACE_GUID_H_
+
+#define PCATCHIPSET_TOKEN_SPACE_GUID \
+  { \
+    0x326ae723, 0xae32, 0x4589, { 0x98, 0xb8, 0xca, 0xc2, 0x3c, 0xdc, 0xc1, 0xb1 } \
+  }
+
+extern EFI_GUID gPcAtChipsetPkgTokenSpaceGuid;
+
+#endif
