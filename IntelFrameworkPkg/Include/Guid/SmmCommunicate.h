ee896758ccc6bfd9990e82f35dd31168e8f4d6c6
xli24 <xli24@6f19259b-4bc3-4df7-8a09-765794883524>
2009-07-08 02:44:46 +0000

    Add comments to describe inconsistency between code and Framework spec.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8791 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkPkg/Include/Guid/SmmCommunicate.h
+++ IntelFrameworkPkg/Include/Guid/SmmCommunicate.h
@@ -2,7 +2,7 @@
   Definitions EFI_SMM_COMMUNICATE_HEADER used by EFI_SMM_BASE_PROTOCOL.Communicate()
   functions
 
-  Copyright (c) 2007, Intel Corporation
+  Copyright (c) 2007 - 2009, Intel Corporation
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -23,9 +23,10 @@
 
 #include <PiPei.h>
 
-//******************************************************
-// EFI_SMM_COMMUNICATE_HEADER
-//******************************************************
+///
+/// Inconsistent with specification here: 
+/// GUID definition format has been changed, because the GUID format in Framework spec is incorrect.
+///
 #define SMM_COMMUNICATE_HEADER_GUID \
   { \
     0xf328e36c, 0x23b6, 0x4a95, {0x85, 0x4b, 0x32, 0xe1, 0x95, 0x34, 0xcd, 0x75 } \
