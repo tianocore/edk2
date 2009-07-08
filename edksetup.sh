2b1473c1ba8138de830c53a3e9bbaab5ad3a472e
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2009-07-08 05:54:53 +0000

    add more description in edksetup.sh
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8806 6f19259b-4bc3-4df7-8a09-765794883524

--- edksetup.sh
+++ edksetup.sh
@@ -7,13 +7,23 @@
 # 
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
 # WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
+#
+# In *inux environment, the build tools's source is required and need to be compiled
+# firstly, please reference https://edk2.tianocore.org/unix-getting-started.html to 
+# to get how to setup build tool.
+#
+# After build tool is downloaded and compiled, a soft symbol linker need to be created
+# at <workspace>/Conf. For example: ln -s /work/BaseTools /work/edk2/Conf/BaseToolsSource.
+#
 # Setup the environment for unix-like systems running a bash-like shell.
 # This file must be "sourced" not merely executed. For example: ". edksetup.sh"
-
+#
 # CYGWIN users: Your path and filename related environment variables should be
 # set up in the unix style.  This script will make the necessary conversions to
 # windows style.
+#
+# Please reference edk2 user manual for more detail descriptions at https://edk2.tianocore.org/files/documents/64/494/EDKII_UserManual.pdf
+#
 
 if [ \
      -z "$1" -o \
