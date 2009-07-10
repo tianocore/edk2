d405baa0b6566b08ec648f1fef1791c622c15b33
xli24 <xli24@6f19259b-4bc3-4df7-8a09-765794883524>
2009-07-10 03:53:02 +0000

    Refine comments.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8858 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkPkg/Include/Ppi/DeviceRecoveryModule.h
+++ IntelFrameworkPkg/Include/Ppi/DeviceRecoveryModule.h
@@ -99,7 +99,7 @@ EFI_STATUS
 
   @param[in, out] PeiServices     General-purpose services that are available to every PEIM
   @param[in]      This            Indicates the EFI_PEI_DEVICE_RECOVERY_MODULE_PPI instance.
-  @param[in]      CapsuleInstance Specifies for which capsule instance to retrieve the information.
+  @param[in]      CapsuleInstance Specifies which capsule instance to retrieve.
                                   This parameter must be between one and the value returned by
                                   GetNumberRecoveryCapsules() in NumberRecoveryCapsules.
   @param[out]     Buffer          Specifies a caller-allocated buffer in which the requested
