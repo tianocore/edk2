21d2b17f9864a9891c19299d58d56bcdcaf273a7
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2009-07-13 05:53:48 +0000

    updated parameters name to match spec.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@8920 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkPkg/Include/Protocol/IncompatiblePciDeviceSupport.h
+++ IntelFrameworkPkg/Include/Protocol/IncompatiblePciDeviceSupport.h
@@ -44,9 +44,9 @@ typedef struct _EFI_INCOMPATIBLE_PCI_DEVICE_SUPPORT_PROTOCOL EFI_INCOMPATIBLE_PC
   resource configuration requirements for an incompatible PCI device.
 
   @param  This                  Pointer to the EFI_INCOMPATIBLE_PCI_DEVICE_SUPPORT_PROTOCOL instance.
-  @param  VendorID              A unique ID to identify the manufacturer of the PCI device.
-  @param  DeviceID              A unique ID to identify the particular PCI device.
-  @param  RevisionID            A PCI device-specific revision identifier.
+  @param  VendorId              A unique ID to identify the manufacturer of the PCI device.
+  @param  DeviceId              A unique ID to identify the particular PCI device.
+  @param  RevisionId            A PCI device-specific revision identifier.
   @param  SubsystemVendorId     Specifies the subsystem vendor ID.
   @param  SubsystemDeviceId     Specifies the subsystem device ID.
   @param  Configuration         A list of ACPI resource descriptors that detail
@@ -58,12 +58,12 @@ typedef struct _EFI_INCOMPATIBLE_PCI_DEVICE_SUPPORT_PROTOCOL EFI_INCOMPATIBLE_PC
 typedef
 EFI_STATUS
 (EFIAPI *EFI_INCOMPATIBLE_PCI_DEVICE_SUPPORT_CHECK_DEVICE)(
-  IN EFI_INCOMPATIBLE_PCI_DEVICE_SUPPORT_PROTOCOL   *This,
+  IN  EFI_INCOMPATIBLE_PCI_DEVICE_SUPPORT_PROTOCOL  *This,
   IN  UINTN                                         VendorId,
   IN  UINTN                                         DeviceId,
-  IN  UINTN                                         Revision,
-  IN  UINTN                                         SubVendorId,
-  IN  UINTN                                         SubDeviceId,
+  IN  UINTN                                         RevisionId,
+  IN  UINTN                                         SubsystemVendorId,
+  IN  UINTN                                         SubsystemDeviceId,
   OUT VOID                                          **Configuration
 );
 
