d6435e84883b521455879959641265bb090f559e
darylm503 <darylm503@6f19259b-4bc3-4df7-8a09-765794883524>
2009-07-15 22:34:41 +0000

    Clean up PERF macro usage.  The macros were being used with 0 as the first parameter.  This parameter is actually a void*.  All of these instances were corrected to use NULL as the first parameter.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@8955 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Core/Dxe/DxeMain/DxeMain.c
+++ edk2/MdeModulePkg/Core/Dxe/DxeMain/DxeMain.c
@@ -258,8 +258,8 @@ DxeMain (
   // Call constructor for all libraries
   //
   ProcessLibraryConstructorList (gDxeCoreImageHandle, gDxeCoreST);
-  PERF_END   (0,"PEI", NULL, 0) ;
-  PERF_START (0,"DXE", NULL, 0) ;
+  PERF_END   (NULL,"PEI", NULL, 0) ;
+  PERF_START (NULL,"DXE", NULL, 0) ;
 
   //
   // Initialize the Global Coherency Domain Services
@@ -360,16 +360,16 @@ DxeMain (
   //
   // Initialize the DXE Dispatcher
   //
-  PERF_START (0,"CoreInitializeDispatcher", "DxeMain", 0) ;
+  PERF_START (NULL,"CoreInitializeDispatcher", "DxeMain", 0) ;
   CoreInitializeDispatcher ();
-  PERF_END (0,"CoreInitializeDispatcher", "DxeMain", 0) ;
+  PERF_END (NULL,"CoreInitializeDispatcher", "DxeMain", 0) ;
 
   //
   // Invoke the DXE Dispatcher
   //
-  PERF_START (0, "CoreDispatcher", "DxeMain", 0);
+  PERF_START (NULL, "CoreDispatcher", "DxeMain", 0);
   CoreDispatcher ();
-  PERF_END (0, "CoreDispatcher", "DxeMain", 0);
+  PERF_END (NULL, "CoreDispatcher", "DxeMain", 0);
 
   //
   // Display Architectural protocols that were not loaded if this is DEBUG build
