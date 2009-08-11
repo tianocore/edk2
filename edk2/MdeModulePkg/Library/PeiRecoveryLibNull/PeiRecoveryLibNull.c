57b26d9bea8c8bd7a6a123397afd4e7c30356224
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2009-08-11 15:51:48 +0000

    Update the comments for S3Lib and RecoveryLib class and instance to state that they are deprecated.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@9047 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Library/PeiRecoveryLibNull/PeiRecoveryLibNull.c
+++ edk2/MdeModulePkg/Library/PeiRecoveryLibNull/PeiRecoveryLibNull.c
@@ -1,5 +1,9 @@
 /** @file
-  Null Recovery Library instance does nothing and returns unsupport status.
+  Null Recovery Library instance does nothing and returns unsupported status.
+
+  This library instance is no longer used and module using this library
+  class should update to directly locate EFI_PEI_RECOVERY_MODULE_PPI defined
+  in PI 1.2 specification. 
 
 Copyright (c) 2006 - 2008, Intel Corporation. <BR>
 All rights reserved. This program and the accompanying materials
