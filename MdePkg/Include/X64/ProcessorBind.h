d4cf6b6c976d07af769ad3bfeeccbcf833e72c32
eric_tian <eric_tian@6f19259b-4bc3-4df7-8a09-765794883524>
2009-08-14 09:39:55 +0000

    redefine the ASM_PFX to let it can work on Apple/NetBSD and other Unix* platform.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9073 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/X64/ProcessorBind.h
+++ MdePkg/Include/X64/ProcessorBind.h
@@ -400,11 +400,18 @@ typedef INT64   INTN;
 // For symbol name in GNU assembly code, an extra "_" is necessary
 //
 #if defined(__GNUC__)
-  #if defined(linux)
-    #define ASM_PFX(name) name
-  #else
-    #define ASM_PFX(name) _##name
-  #endif
+  ///
+  /// Private worker functions for ASM_PFX()
+  ///
+  #define _CONCATENATE(a, b)  __CONCATENATE(a, b)
+  #define __CONCATENATE(a, b) a ## b
+
+  ///
+  /// The __USER_LABEL_PREFIX__ macro predefined by GNUC represents the prefix
+  /// on symbols in assembly language.
+  ///
+  #define ASM_PFX(name) _CONCATENATE (__USER_LABEL_PREFIX__, name)
+
   ///
   /// For GNU assembly code, .global or .globl can declare global symbols.
   /// Define this macro to unify the usage.
