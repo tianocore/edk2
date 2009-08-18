990e25aa3597f1f48ec5920b03a276b94c5db8ec
mdkinney <mdkinney@6f19259b-4bc3-4df7-8a09-765794883524>
2009-08-18 21:24:08 +0000

    Add Add ARM support Add C inline assembly files for IA32 and X64 GCC builds.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9114 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ MdePkg/Library/BaseCpuLib/Arm/CpuSleep.asm
@@ -0,0 +1,39 @@
+;------------------------------------------------------------------------------ 
+;
+; CpuSleep() for ARM
+;
+; Copyright (c) 2006 - 2009, Intel Corporation<BR>
+; Portions copyright (c) 2008-2009 Apple Inc.<BR>
+; All rights reserved. This program and the accompanying materials
+; are licensed and made available under the terms and conditions of the BSD License
+; which accompanies this distribution.  The full text of the license may be found at
+; http://opensource.org/licenses/bsd-license.php
+;
+; THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+; WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+;
+;------------------------------------------------------------------------------
+
+  EXPORT CpuSleep
+  AREA cpu_sleep, CODE, READONLY
+
+;/**
+;  Places the CPU in a sleep state until an interrupt is received.
+;
+;  Places the CPU in a sleep state until an interrupt is received. If interrupts
+;  are disabled prior to calling this function, then the CPU will be placed in a
+;  sleep state indefinitely.
+;
+;**/
+;VOID
+;EFIAPI
+;CpuSleep (
+;  VOID
+;  );
+;
+CpuSleep
+    MOV r0,#0
+    MCR p15,0,r0,c7,c0,4   ;Wait for Interrupt instruction
+    BX LR
+
+  END
