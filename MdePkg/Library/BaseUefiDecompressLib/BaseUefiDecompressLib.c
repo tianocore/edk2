acbcf0425f4fa835d3455df86c6e505b5095cd51
mdkinney <mdkinney@6f19259b-4bc3-4df7-8a09-765794883524>
2009-08-24 23:39:38 +0000

    Fix file headers
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9189 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/BaseUefiDecompressLib/BaseUefiDecompressLib.c
+++ MdePkg/Library/BaseUefiDecompressLib/BaseUefiDecompressLib.c
@@ -2,7 +2,7 @@
   UEFI Decompress Library implementation refer to UEFI specification.
 
   Copyright (c) 2006 - 2008, Intel Corporation<BR>
-  Portions Copyright (c) 2008-2009 Apple Inc.<BR>
+  Portions copyright (c) 2008-2009 Apple Inc. All rights reserved.<BR>
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
