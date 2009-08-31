33bb6326576a3f4f722b60d8a8d996af882009d1
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2009-08-31 14:14:44 +0000

    Update BdsDxe to use HiiLib and UefiHiiServicesLib APIs to eliminate the symbol collision.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9210 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkModulePkg/Universal/BdsDxe/Bds.h
+++ IntelFrameworkModulePkg/Universal/BdsDxe/Bds.h
@@ -1,7 +1,7 @@
 /** @file
   Head file for BDS Architectural Protocol implementation
 
-Copyright (c) 2004 - 2008, Intel Corporation. <BR>
+Copyright (c) 2004 - 2009, Intel Corporation. <BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -61,6 +61,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #include <Library/HiiLib.h>
 #include <Library/DevicePathLib.h>
 #include <Library/PcdLib.h>
+#include <Library/UefiHiiServicesLib.h>
 
 #include <Library/GenericBdsLib.h>
 #include <Library/PlatformBdsLib.h>
