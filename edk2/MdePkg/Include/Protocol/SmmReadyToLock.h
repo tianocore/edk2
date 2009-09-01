eb142d15130dd7ac8a0baa7abce5f63e9aa1658b
rsun3 <rsun3@6f19259b-4bc3-4df7-8a09-765794883524>
2009-09-01 07:19:16 +0000

    Add into MdePkg definitions for SMM Ready To Lock protocol as defined in the PI 1.2 specification.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@9216 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ edk2/MdePkg/Include/Protocol/SmmReadyToLock.h
@@ -0,0 +1,29 @@
+/** @file
+  SMM Ready To Lock protocol as defined in the PI 1.2 specification.
+
+  This SMM protocol indicates that SMM is about to be locked.
+  This protocol is a mandatory protocol published by the SMM Foundation code when the system is 
+  preparing to lock SMM. 
+
+  Copyright (c) 2009, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef _SMM_READY_TO_LOCK_H_
+#define _SMM_READY_TO_LOCK_H_
+
+#define EFI_SMM_READY_TO_LOCK_PROTOCOL_GUID \
+  { \
+    0x47b7fa8c, 0xf4bd, 0x4af6, { 0x82, 0x00, 0x33, 0x30, 0x86, 0xf0, 0xd2, 0xc8 } \
+  }
+
+extern EFI_GUID gEfiSmmReadyToLockProtocolGuid;
+
+#endif
