3ca4da8d46fd7b1e8ee21181a61d80e96c44d9ae
eric_tian <eric_tian@6f19259b-4bc3-4df7-8a09-765794883524>
2009-09-03 09:21:05 +0000

    specify the spec revision number followed by protocol
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@9237 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/IntelFrameworkPkg/Include/Protocol/SmmBase.h
+++ edk2/IntelFrameworkPkg/Include/Protocol/SmmBase.h
@@ -19,18 +19,18 @@
   workarounds for the century rollover in CMOS should provide commensurate services throughout
   preboot and OS runtime.
 
+  Copyright (c) 2007 - 2009, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
   @par Revision Reference:
   This Protocol is defined in Framework of EFI SMM Core Interface Spec
   Version 0.9.
-
-Copyright (c) 2007 - 2009, Intel Corporation
-All rights reserved. This program and the accompanying materials
-are licensed and made available under the terms and conditions of the BSD License
-which accompanies this distribution.  The full text of the license may be found at
-http://opensource.org/licenses/bsd-license.php
-
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
--- edk2/IntelFrameworkPkg/Include/Protocol/SmmPowerButtonDispatch.h
+++ edk2/IntelFrameworkPkg/Include/Protocol/SmmPowerButtonDispatch.h
@@ -1,18 +1,18 @@
 /** @file
   Provides the parent dispatch service for the power button SMI source generator.
 
+  Copyright (c) 2007 - 2009, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
   @par Revision Reference:
   This Protocol is defined in Framework of EFI SMM Core Interface Spec
   Version 0.9.
-
-Copyright (c) 2007, Intel Corporation
-All rights reserved. This program and the accompanying materials
-are licensed and made available under the terms and conditions of the BSD License
-which accompanies this distribution.  The full text of the license may be found at
-http://opensource.org/licenses/bsd-license.php
-
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
