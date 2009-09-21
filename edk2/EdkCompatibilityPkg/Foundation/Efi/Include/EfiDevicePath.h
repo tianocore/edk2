69b75b2344a32fd3cd8c5db19183319aee0abfae
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2009-09-21 14:19:20 +0000

    Update MEDIA_RELATIVE_OFFSET_RANGE_DEVICE_PATH in ECP to align with MdePkg definition for ECP shell to work correctly on EDKII platforms.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@9291 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/EdkCompatibilityPkg/Foundation/Efi/Include/EfiDevicePath.h
+++ edk2/EdkCompatibilityPkg/Foundation/Efi/Include/EfiDevicePath.h
@@ -1,6 +1,6 @@
 /*++
  
-Copyright (c) 2004 - 2007, Intel Corporation                                                         
+Copyright (c) 2004 - 2009, Intel Corporation                                                         
 All rights reserved. This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -432,6 +432,7 @@ typedef struct {
 #define MEDIA_RELATIVE_OFFSET_RANGE_DP 0x08
 typedef struct {
   EFI_DEVICE_PATH_PROTOCOL  Header;
+  UINT32                    Reserved;
   UINT64                    StartingOffset;
   UINT64                    EndingOffset;
 } MEDIA_RELATIVE_OFFSET_RANGE_DEVICE_PATH;
