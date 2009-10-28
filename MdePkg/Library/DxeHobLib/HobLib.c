b6b55af34cf6e1ebcd2dacdd73252aa5f16bf41e
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2009-10-28 07:53:32 +0000

    UEFI Capsule HOB updating includes: 1. add BuildCapsuleHob() in HobLib.h 2. add related APIs implementation in hob library instances.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9378 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/DxeHobLib/HobLib.c
+++ MdePkg/Library/DxeHobLib/HobLib.c
@@ -1,7 +1,7 @@
 /** @file
   HOB Library implemenation for Dxe Phase.
 
-Copyright (c) 2006 - 2008, Intel Corporation<BR>
+Copyright (c) 2006 - 2009, Intel Corporation<BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -555,6 +555,31 @@ BuildMemoryAllocationHob (
   IN EFI_PHYSICAL_ADDRESS        BaseAddress,
   IN UINT64                      Length,
   IN EFI_MEMORY_TYPE             MemoryType
+  )
+{
+  //
+  // PEI HOB is read only for DXE phase
+  //
+  ASSERT (FALSE);
+}
+
+/**
+  Builds an UEFI Capsule HOB.
+
+  This function builds an UEFI Capsule HOB.
+  It can only be invoked during PEI phase;
+  for DXE phase, it will ASSERT() since PEI HOB is read-only for DXE phase.
+  If there is no additional space for HOB creation, then ASSERT().
+
+  @param  BaseAddress   The physical memory-mapped base address of an UEFI capsule.
+  @param  Length        The length of the contiguous memory in bytes.
+
+**/
+VOID
+EFIAPI
+BuildCapsuleHob (
+  IN EFI_PHYSICAL_ADDRESS        BaseAddress,
+  IN UINT64                      Length
   )
 {
   //
