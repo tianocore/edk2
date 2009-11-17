5a9403b8f38db18924e73d73b0d1f54b4d10cdf6
xli24 <xli24@6f19259b-4bc3-4df7-8a09-765794883524>
2009-11-17 15:00:28 +0000

    Following 3 updates are for spec update published in PI 1.2. 1. Rename EFI_PEI_NEXT_VARIABLE_NAME2 to EFI_PEI_GET_NEXT_VARIABLE_NAME2, as PI 1.2 specifies. 2. Add return status description for PEI Service FfsGetVolumeInfo. 3. Update parameter description for EFI_PEI_READ_ONLY_VARIABLE2_PPI.NextVariableName().
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9438 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/Variable/Pei/Variable.c
+++ MdeModulePkg/Universal/Variable/Pei/Variable.c
@@ -3,7 +3,7 @@
   Implement ReadOnly Variable Services required by PEIM and install
   PEI ReadOnly Varaiable2 PPI. These services operates the non volatile storage space.
 
-Copyright (c) 2006 - 2008 Intel Corporation. <BR>
+Copyright (c) 2006 - 2009 Intel Corporation. <BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -592,7 +592,7 @@ PeiGetVariable (
   @param  VariableNameSize  On entry, points to the size of the buffer pointed to by VariableName.
   @param  VariableName      On entry, a pointer to a null-terminated string that is the variable's name.
                             On return, points to the next variable's null-terminated name string.
-  @param  VariableGuid      On entry, a pointer to an UEFI _GUID that is the variable's GUID. 
+  @param  VendorGuid        On entry, a pointer to an EFI_GUID that is the variable's GUID. 
                             On return, a pointer to the next variable's GUID.
 
   @retval EFI_SUCCESS           The variable was read successfully.
