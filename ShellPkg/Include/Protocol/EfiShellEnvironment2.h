b594c4665866584649ee58cc8414cc1d5cd21e8d
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2009-11-18 15:40:46 +0000

    Fix incorrect copyright format
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9448 6f19259b-4bc3-4df7-8a09-765794883524

--- ShellPkg/Include/Protocol/EfiShellEnvironment2.h
+++ ShellPkg/Include/Protocol/EfiShellEnvironment2.h
@@ -1,7 +1,7 @@
 /** @file
   Defines for EFI shell environment 2 ported to EDK II build environment. (no spec)
 
-Copyright (c) 2005 - 2009, 2009 Intel Corporation
+Copyright (c) 2005 - 2009, Intel Corporation
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
