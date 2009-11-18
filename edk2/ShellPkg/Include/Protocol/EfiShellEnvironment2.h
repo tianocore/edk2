bc1fc3cb15eb294d947e290dc19b2e2af085285b
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2009-11-18 05:59:10 +0000

    Fix GUID format
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@9444 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/ShellPkg/Include/Protocol/EfiShellEnvironment2.h
+++ edk2/ShellPkg/Include/Protocol/EfiShellEnvironment2.h
@@ -1,7 +1,7 @@
 /** @file
   Defines for EFI shell environment 2 ported to EDK II build environment. (no spec)
 
-Copyright (c) 2005, 2009 Intel Corporation
+Copyright (c) 2005 - 2009, 2009 Intel Corporation
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -105,7 +105,7 @@ typedef struct {
 **/
 #define SHELL_ENVIRONMENT_PROTOCOL_GUID \
   { \
-    0x47c7b221, 0xc42a, 0x11d2, 0x8e, 0x57, 0x0, 0xa0, 0xc9, 0x69, 0x72, 0x3b \
+    0x47c7b221, 0xc42a, 0x11d2, {0x8e, 0x57, 0x0, 0xa0, 0xc9, 0x69, 0x72, 0x3b} \
   }
 
 /**
