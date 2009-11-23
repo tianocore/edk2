1e4506f2ccff06967d7b46862c96bb6fa822b1e7
jcarsey <jcarsey@6f19259b-4bc3-4df7-8a09-765794883524>
2009-11-23 22:21:58 +0000

    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9469 6f19259b-4bc3-4df7-8a09-765794883524

--- ShellPkg/Application/ShellLibTestApp/sa3.c
+++ ShellPkg/Application/ShellLibTestApp/sa3.c
@@ -84,8 +84,8 @@ UefiMain (
     Print(L"param checking skipped.\r\n");
   }
 
-  return (EFI_SUCCESS);
-/*
+//  return (EFI_SUCCESS);
+
 
   ASSERT(ShellGetExecutionBreakFlag() == FALSE);
   ASSERT(StrCmp(ShellGetCurrentDir(NULL), L"f10:\\") == 0);
@@ -298,7 +298,6 @@ UefiMain (
   Print(L"1\r\n2\r\n3\r\n4\r\n5\r\n6\r\n7\r\n8\r\n9\r\n10\r\n11\r\n12\r\n13\r\n14\r\n15\r\n16\r\n17\r\n18\r\n19\r\n20\r\n21\r\n22\r\n23\r\n24\r\n25\r\n26\r\n27\r\n28\r\n29\r\n30\r\n31\r\n32\r\n33\r\n34\r\n35\r\n36\r\n37\r\n38\r\n39\r\n40\r\n41\r\n42\r\n43\r\n44\r\n45\r\n46\r\n47\r\n48\r\n49\r\n50\r\n51\r\n52\r\n53\r\n54\r\n55\r\n56\r\n57\r\n58\r\n59\r\n60\r\n");
 
   return EFI_SUCCESS;
-*/
 }
 
 
