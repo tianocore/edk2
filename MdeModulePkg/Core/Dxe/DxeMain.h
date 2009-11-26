7748df3da985214b8c6e39ea983018c212198d54
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2009-11-26 01:08:36 +0000

    Apply GetFileBufferByFilePath API of DxeServicesLib to replace local CoreOpenImageFile function.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9487 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Core/Dxe/DxeMain.h
+++ MdeModulePkg/Core/Dxe/DxeMain.h
@@ -2,7 +2,7 @@
   The internal header file includes the common header files, defines
   internal structure and functions used by DxeCore module.
 
-Copyright (c) 2006 - 2008, Intel Corporation. <BR>
+Copyright (c) 2006 - 2009, Intel Corporation. <BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -79,6 +79,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/ReportStatusCodeLib.h>
 #include <Library/TimerLib.h>
+#include <Library/DxeServicesLib.h>
 
 //
 // attributes for reserved memory before it is promoted to system memory
