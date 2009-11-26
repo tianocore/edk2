e337926f6de425eca20af4517d333869cc49d182
jchen20 <jchen20@6f19259b-4bc3-4df7-8a09-765794883524>
2009-11-26 05:24:31 +0000

    Add PI complied S3 implementation. Move the Opcode definition and EFI_BOOT_SCRIPT_WIDTH definition from protocol directory to Pi directory since the S3 related Library need these definitions.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@9490 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/Protocol/S3SaveState.h
+++ edk2/MdePkg/Include/Protocol/S3SaveState.h
@@ -26,45 +26,6 @@
 #define EFI_S3_SAVE_STATE_PROTOCOL_GUID \
     { 0xe857caf6, 0xc046, 0x45dc, { 0xbe, 0x3f, 0xee, 0x7, 0x65, 0xfb, 0xa8, 0x87 }}
 
-//*******************************************
-// EFI Boot Script Opcode definitions
-//*******************************************
-#define EFI_BOOT_SCRIPT_IO_WRITE_OPCODE                 0x00
-#define EFI_BOOT_SCRIPT_IO_READ_WRITE_OPCODE            0x01
-#define EFI_BOOT_SCRIPT_MEM_WRITE_OPCODE                0x02
-#define EFI_BOOT_SCRIPT_MEM_READ_WRITE_OPCODE           0x03
-#define EFI_BOOT_SCRIPT_PCI_CONFIG_WRITE_OPCODE         0x04
-#define EFI_BOOT_SCRIPT_PCI_CONFIG_READ_WRITE_OPCODE    0x05
-#define EFI_BOOT_SCRIPT_SMBUS_EXECUTE_OPCODE            0x06
-#define EFI_BOOT_SCRIPT_STALL_OPCODE                    0x07
-#define EFI_BOOT_SCRIPT_DISPATCH_OPCODE                 0x08
-#define EFI_BOOT_SCRIPT_DISPATCH_2_OPCODE               0x09
-#define EFI_BOOT_SCRIPT_INFORMATION_OPCODE              0x0A
-#define EFI_BOOT_SCRIPT_PCI_CONFIG2_WRITE_OPCODE        0x0B
-#define EFI_BOOT_SCRIPT_PCI_CONFIG2_READ_WRITE_OPCODE   0x0C
-#define EFI_BOOT_SCRIPT_IO_POLL_OPCODE                  0x0D
-#define EFI_BOOT_SCRIPT_MEM_POLL_OPCODE                 0x0E
-#define EFI_BOOT_SCRIPT_PCI_CONFIG_POLL_OPCODE          0x0F
-#define EFI_BOOT_SCRIPT_PCI_CONFIG2_POLL_OPCODE         0x10
-
-//*******************************************
-// EFI_BOOT_SCRIPT_WIDTH
-//*******************************************
-typedef enum {
-  EfiBootScriptWidthUint8,
-  EfiBootScriptWidthUint16,
-  EfiBootScriptWidthUint32,
-  EfiBootScriptWidthUint64,
-  EfiBootScriptWidthFifoUint8,
-  EfiBootScriptWidthFifoUint16,
-  EfiBootScriptWidthFifoUint32,
-  EfiBootScriptWidthFifoUint64,
-  EfiBootScriptWidthFillUint8,
-  EfiBootScriptWidthFillUint16,
-  EfiBootScriptWidthFillUint32,
-  EfiBootScriptWidthFillUint64,
-  EfiBootScriptWidthMaximum
-} EFI_BOOT_SCRIPT_WIDTH;
 
 typedef VOID *EFI_S3_BOOT_SCRIPT_POSITION;
 
