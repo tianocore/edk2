c6204371fd7306b2fff2f1cb1d6fd9ec24edcd1b
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2009-12-04 03:10:28 +0000

    Prompt the PI specification version to 1.20 and UEFI specification version to 2.30, for PEI_SERVICES_TABLE, DXE_SERVICES_TABLE, EFI_BOOT_SERVICES_TABLE, EFI_RUNTIME_SERVICES_TABLE.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9513 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Pi/PiDxeCis.h
+++ MdePkg/Include/Pi/PiDxeCis.h
@@ -655,7 +655,7 @@ EFI_STATUS
 // DXE Services Table
 //
 #define DXE_SERVICES_SIGNATURE  0x565245535f455844ULL
-#define DXE_SERVICES_REVISION   ((1<<16) | (00))
+#define DXE_SERVICES_REVISION   ((1<<16) | (20))
 
 typedef struct {
   ///
