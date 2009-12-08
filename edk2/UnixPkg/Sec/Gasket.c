8e3a9ab4b82fb6214855f7b83bbbd0da6252f285
AJFISH <AJFISH@6f19259b-4bc3-4df7-8a09-765794883524>
2009-12-08 06:14:12 +0000

    Fix for the Linux Build. #ifdef __APPLE__ around the files only needed to work around Mac OS X 16 byte IA-32 calling conventions issue. All the calls to this code is #ifdef __APPLE__, so no reason to build it on Linux or other gcc flavors.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@9543 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/UnixPkg/Sec/Gasket.c
+++ edk2/UnixPkg/Sec/Gasket.c
@@ -1,3 +1,18 @@
+/** @file
+
+  Copyright (c) 2008-2009, Apple Inc. All rights reserved.
+  
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifdef __APPLE__
 
 #include "SecMain.h"
 #include "Gasket.h"
@@ -415,4 +430,6 @@ GasketUgaBlt (
 {
   return GasketUintn10Args (UgaBlt, (UINTN)UgaIo, (UINTN)BltBuffer, BltOperation, SourceX, SourceY, DestinationX, DestinationY, Width, Height, Delta);
 }
+
+#endif
 
