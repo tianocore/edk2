5e973c9630be45e950aa38c629a34033c85f9637
klu2 <klu2@6f19259b-4bc3-4df7-8a09-765794883524>
2009-12-23 15:35:51 +0000

    Correct license's header.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9594 6f19259b-4bc3-4df7-8a09-765794883524

--- Nt32Pkg/MiscSubClassPlatformDxe/MiscBootInformationFunction.c
+++ Nt32Pkg/MiscSubClassPlatformDxe/MiscBootInformationFunction.c
@@ -1,25 +1,17 @@
-/*++
-
-Copyright (c) 2009, Intel Corporation. All rights reserved. <BR> 
-This software and associated documentation (if any) is furnished
-under a license and may only be used or copied in accordance
-with the terms of the license. Except as permitted by such
-license, no part of this software or documentation may be
-reproduced, stored in a retrieval system, or transmitted in any
-form or by any means without the express written consent of
-Intel Corporation.
-
-
-Module Name:
-
-  MiscBootInformationFunction.c
-  
-Abstract: 
-
+/** @file
   boot information boot time changes.
   SMBIOS type 32.
+  
+Copyright (c) 2009, Intel Corporation
+All rights reserved. This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
 
---*/
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
 
 #include "MiscSubclassDriver.h"
 
