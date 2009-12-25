654d3d37df9b40132c359247e2876d70f13fcf98
xli24 <xli24@6f19259b-4bc3-4df7-8a09-765794883524>
2009-12-25 06:49:52 +0000

    Move Status Code Handler modules for PI implementation to MdeModulePkg.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@9608 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ edk2/MdeModulePkg/Universal/StatusCodeHandler/Pei/MemoryStausCodeWorker.c
@@ -0,0 +1,153 @@
+/** @file
+  PEI memory status code worker.
+
+  Copyright (c) 2006 - 2009, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#include "StatusCodeHandlerPei.h"
+
+/**
+  Worker function to create one memory status code GUID'ed HOB,
+  using PacketIndex to identify the packet.
+
+  @param   PacketIndex    Index of records packet.
+
+  @return  Pointer to the memory status code packet.
+
+**/
+MEMORY_STATUSCODE_PACKET_HEADER *
+CreateMemoryStatusCodePacket (
+  UINT16 PacketIndex
+  )
+{
+  MEMORY_STATUSCODE_PACKET_HEADER *PacketHeader;
+
+  //
+  // Build GUID'ed HOB with PCD defined size.
+  //
+  PacketHeader = BuildGuidHob (
+                   &gMemoryStatusCodeRecordGuid,
+                   PcdGet16 (PcdStatusCodeMemorySize) * 1024 + sizeof (MEMORY_STATUSCODE_PACKET_HEADER)
+                   );
+  ASSERT (PacketHeader != NULL);
+
+  PacketHeader->MaxRecordsNumber = (PcdGet16 (PcdStatusCodeMemorySize) * 1024) / sizeof (MEMORY_STATUSCODE_RECORD);
+  PacketHeader->PacketIndex      = PacketIndex;
+  PacketHeader->RecordIndex      = 0;
+
+  return PacketHeader;
+}
+
+/**
+  Create the first memory status code GUID'ed HOB as initialization for memory status code worker.
+
+  @retval EFI_SUCCESS  The GUID'ed HOB is created successfully.
+
+**/
+EFI_STATUS
+MemoryStatusCodeInitializeWorker (
+  VOID
+  )
+{
+  //
+  // Create first memory status code GUID'ed HOB.
+  //
+  CreateMemoryStatusCodePacket (0);
+
+  return EFI_SUCCESS;
+}
+
+
+/**
+  Report status code into GUID'ed HOB.
+
+  This function reports status code into GUID'ed HOB. If not all packets are full, then
+  write status code into available entry. Otherwise, create a new packet for it.
+
+  @param  PeiServices      An indirect pointer to the EFI_PEI_SERVICES table published by the PEI Foundation.
+  @param  CodeType         Indicates the type of status code being reported.
+  @param  Value            Describes the current status of a hardware or
+                           software entity. This includes information about the class and
+                           subclass that is used to classify the entity as well as an operation.
+                           For progress codes, the operation is the current activity.
+                           For error codes, it is the exception.For debug codes,it is not defined at this time.
+  @param  Instance         The enumeration of a hardware or software entity within
+                           the system. A system may contain multiple entities that match a class/subclass
+                           pairing. The instance differentiates between them. An instance of 0 indicates
+                           that instance information is unavailable, not meaningful, or not relevant.
+                           Valid instance numbers start with 1.
+  @param  CallerId         This optional parameter may be used to identify the caller.
+                           This parameter allows the status code driver to apply different rules to
+                           different callers.
+  @param  Data             This optional parameter may be used to pass additional data.
+
+  @retval EFI_SUCCESS      The function always return EFI_SUCCESS.
+
+**/
+EFI_STATUS
+MemoryStatusCodeReportWorker (
+  IN CONST  EFI_PEI_SERVICES    **PeiServices,
+  IN EFI_STATUS_CODE_TYPE       CodeType,
+  IN EFI_STATUS_CODE_VALUE      Value,
+  IN UINT32                     Instance,
+  IN CONST EFI_GUID             *CallerId,
+  IN CONST EFI_STATUS_CODE_DATA *Data OPTIONAL
+  )
+{
+
+  EFI_PEI_HOB_POINTERS              Hob;
+  MEMORY_STATUSCODE_PACKET_HEADER   *PacketHeader;
+  MEMORY_STATUSCODE_RECORD          *Record;
+  UINT16                            PacketIndex;
+
+  Record      = NULL;
+  PacketIndex = 0;
+
+  //
+  // Journal GUID'ed HOBs to find empty record entry. if found, then save status code in it.
+  // otherwise, create a new GUID'ed HOB.
+  //
+  Hob.Raw = GetFirstGuidHob (&gMemoryStatusCodeRecordGuid);
+  while (Hob.Raw != NULL) {
+    PacketHeader = (MEMORY_STATUSCODE_PACKET_HEADER *) GET_GUID_HOB_DATA (Hob.Guid);
+
+    //
+    // Check whether pccket is full or not.
+    //
+    if (PacketHeader->RecordIndex < PacketHeader->MaxRecordsNumber) {
+      Record = (MEMORY_STATUSCODE_RECORD *) (PacketHeader + 1);
+      Record = &Record[PacketHeader->RecordIndex++];
+      break;
+    }
+    //
+    // Cache number of found packet in PacketIndex.
+    //
+    PacketIndex++;
+
+    Hob.Raw = GetNextGuidHob (&gMemoryStatusCodeRecordGuid, Hob.Raw);
+  }
+
+  if (Record == NULL) {
+    //
+    // No available entry found, so create new packet.
+    //
+    PacketHeader = CreateMemoryStatusCodePacket (PacketIndex);
+
+    Record = (MEMORY_STATUSCODE_RECORD *) (PacketHeader + 1);
+    Record = &Record[PacketHeader->RecordIndex++];
+  }
+
+  Record->CodeType = CodeType;
+  Record->Instance = Instance;
+  Record->Value    = Value;
+
+  return EFI_SUCCESS;
+}
---
+++ edk2/MdeModulePkg/Universal/StatusCodeHandler/RuntimeDxe/StatusCodeHandlerRuntimeDxe.c
@@ -0,0 +1,222 @@
+/** @file
+  Status Code Handler Driver which produces general handlers and hook them
+  onto the DXE status code router.
+
+  Copyright (c) 2006 - 2009, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#include "StatusCodeHandlerRuntimeDxe.h"
+
+EFI_EVENT                 mVirtualAddressChangeEvent = NULL;
+EFI_EVENT                 mExitBootServicesEvent     = NULL;
+EFI_RSC_HANDLER_PROTOCOL  *mRscHandlerProtocol       = NULL;
+
+/**
+  Unregister status code callback functions only available at boot time from
+  report status code router when exiting boot services.
+
+  @param  Event         Event whose notification function is being invoked.
+  @param  Context       Pointer to the notification function's context, which is
+                        always zero in current implementation.
+
+**/
+VOID
+EFIAPI
+UnregisterBootTimeHandlers (
+  IN EFI_EVENT        Event,
+  IN VOID             *Context
+  )
+{
+  if (FeaturePcdGet (PcdStatusCodeUseSerial)) {
+    mRscHandlerProtocol->Unregister (SerialStatusCodeReportWorker);
+  }
+}
+
+/**
+  Virtual address change notification call back. It converts global pointer
+  to virtual address.
+
+  @param  Event         Event whose notification function is being invoked.
+  @param  Context       Pointer to the notification function's context, which is
+                        always zero in current implementation.
+
+**/
+VOID
+EFIAPI
+VirtualAddressChangeCallBack (
+  IN EFI_EVENT        Event,
+  IN VOID             *Context
+  )
+{
+  //
+  // Convert memory status code table to virtual address;
+  //
+  EfiConvertPointer (
+    0,
+    (VOID **) &mRtMemoryStatusCodeTable
+    );
+}
+
+/**
+  Dispatch initialization request to sub status code devices based on 
+  customized feature flags.
+ 
+**/
+VOID
+InitializationDispatcherWorker (
+  VOID
+  )
+{
+  EFI_PEI_HOB_POINTERS              Hob;
+  EFI_STATUS                        Status;
+  MEMORY_STATUSCODE_PACKET_HEADER   *PacketHeader;
+  MEMORY_STATUSCODE_RECORD          *Record;
+  UINTN                             ExpectedPacketIndex;
+  UINTN                             Index;
+  VOID                              *HobStart;
+
+  //
+  // If enable UseSerial, then initialize serial port.
+  // if enable UseRuntimeMemory, then initialize runtime memory status code worker.
+  //
+  if (FeaturePcdGet (PcdStatusCodeUseSerial)) {
+    //
+    // Call Serial Port Lib API to initialize serial port.
+    //
+    Status = SerialPortInitialize ();
+    ASSERT_EFI_ERROR (Status);
+  }
+  if (FeaturePcdGet (PcdStatusCodeUseMemory)) {
+    Status = RtMemoryStatusCodeInitializeWorker ();
+    ASSERT_EFI_ERROR (Status);
+  }
+
+  //
+  // Replay Status code which saved in GUID'ed HOB to all supported devices. 
+  //
+  if (FeaturePcdGet (PcdStatusCodeReplayIn)) {
+    // 
+    // Journal GUID'ed HOBs to find all record entry, if found, 
+    // then output record to support replay device.
+    //
+    ExpectedPacketIndex = 0;
+    Hob.Raw   = GetFirstGuidHob (&gMemoryStatusCodeRecordGuid);
+    HobStart  = Hob.Raw;
+    while (Hob.Raw != NULL) {
+      PacketHeader = (MEMORY_STATUSCODE_PACKET_HEADER *) GET_GUID_HOB_DATA (Hob.Guid);
+      if (PacketHeader->PacketIndex == ExpectedPacketIndex) {
+        Record = (MEMORY_STATUSCODE_RECORD *) (PacketHeader + 1);
+        for (Index = 0; Index < PacketHeader->RecordIndex; Index++) {
+          //
+          // Dispatch records to devices based on feature flag.
+          //
+          if (FeaturePcdGet (PcdStatusCodeUseSerial)) {
+            SerialStatusCodeReportWorker (
+              Record[Index].CodeType,
+              Record[Index].Value,
+              Record[Index].Instance,
+              NULL,
+              NULL
+              );
+          }
+          if (FeaturePcdGet (PcdStatusCodeUseMemory)) {
+            RtMemoryStatusCodeReportWorker (
+              Record[Index].CodeType,
+              Record[Index].Value,
+              Record[Index].Instance,
+              NULL,
+              NULL
+              );
+          }
+        }
+        ExpectedPacketIndex++;
+  
+        //
+        // See whether there is gap of packet or not
+        //
+        if (HobStart != NULL) {
+          HobStart  = NULL;
+          Hob.Raw   = HobStart;
+          continue;
+        }
+      } else if (HobStart != NULL) {
+        //
+        // Cache the found packet for improve the performance
+        //
+        HobStart = Hob.Raw;
+      }
+  
+      Hob.Raw = GetNextGuidHob (&gMemoryStatusCodeRecordGuid, Hob.Raw);
+    }
+  }
+}
+
+/**
+  Entry point of DXE Status Code Driver.
+
+  This function is the entry point of this DXE Status Code Driver.
+  It initializes registers status code handlers, and registers event for EVT_SIGNAL_VIRTUAL_ADDRESS_CHANGE.
+
+  @param  ImageHandle       The firmware allocated handle for the EFI image.
+  @param  SystemTable       A pointer to the EFI System Table.
+  
+  @retval EFI_SUCCESS       The entry point is executed successfully.
+
+**/
+EFI_STATUS
+EFIAPI
+StatusCodeHandlerRuntimeDxeEntry (
+  IN EFI_HANDLE         ImageHandle,
+  IN EFI_SYSTEM_TABLE   *SystemTable
+  )
+{
+  EFI_STATUS                Status;
+
+  Status = gBS->LocateProtocol (
+                  &gEfiRscHandlerProtocolGuid,
+                  NULL,
+                  (VOID **) &mRscHandlerProtocol
+                  );
+  ASSERT_EFI_ERROR (Status);
+
+  //
+  // Dispatch initialization request to supported devices
+  //
+  InitializationDispatcherWorker ();
+
+  if (FeaturePcdGet (PcdStatusCodeUseSerial)) {
+    mRscHandlerProtocol->Register (SerialStatusCodeReportWorker, TPL_HIGH_LEVEL);
+  }
+  if (FeaturePcdGet (PcdStatusCodeUseMemory)) {
+    mRscHandlerProtocol->Register (RtMemoryStatusCodeReportWorker, TPL_HIGH_LEVEL);
+  }
+
+  Status = gBS->CreateEventEx (
+                  EVT_NOTIFY_SIGNAL,
+                  TPL_NOTIFY,
+                  UnregisterBootTimeHandlers,
+                  NULL,
+                  &gEfiEventExitBootServicesGuid,
+                  &mExitBootServicesEvent
+                  );
+
+  Status = gBS->CreateEventEx (
+                  EVT_NOTIFY_SIGNAL,
+                  TPL_NOTIFY,
+                  VirtualAddressChangeCallBack,
+                  NULL,
+                  &gEfiEventVirtualAddressChangeGuid,
+                  &mVirtualAddressChangeEvent
+                  );
+  ASSERT_EFI_ERROR (Status);
+
+  return EFI_SUCCESS;
+}
