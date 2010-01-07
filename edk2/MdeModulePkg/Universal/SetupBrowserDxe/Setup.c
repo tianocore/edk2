ee52c959af88ad592d663a2f543bd11031620d71
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2010-01-07 09:36:09 +0000

    Correct copyright year to 2010
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@9687 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdeModulePkg/Universal/SetupBrowserDxe/Setup.c
+++ edk2/MdeModulePkg/Universal/SetupBrowserDxe/Setup.c
@@ -1,7 +1,7 @@
 /** @file
 Entry and initialization module for the browser.
 
-Copyright (c) 2007 - 2009, Intel Corporation
+Copyright (c) 2007 - 2010, Intel Corporation
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
--- edk2/MdeModulePkg/Universal/SetupBrowserDxe/Setup.h
+++ edk2/MdeModulePkg/Universal/SetupBrowserDxe/Setup.h
@@ -1,7 +1,7 @@
 /** @file
 Private MACRO, structure and function definitions for Setup Browser module.
 
-Copyright (c) 2007 - 2009, Intel Corporation
+Copyright (c) 2007 - 2010, Intel Corporation
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
