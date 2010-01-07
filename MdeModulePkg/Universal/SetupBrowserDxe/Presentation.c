e55e1d13bf2b7362af13ce9c9f2882061f28896e
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2010-01-07 09:36:09 +0000

    Correct copyright year to 2010
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9687 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/SetupBrowserDxe/Presentation.c
+++ MdeModulePkg/Universal/SetupBrowserDxe/Presentation.c
@@ -1,7 +1,7 @@
 /** @file
 Utility functions for UI presentation.
 
-Copyright (c) 2004 - 2009, Intel Corporation
+Copyright (c) 2004 - 2010, Intel Corporation
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
