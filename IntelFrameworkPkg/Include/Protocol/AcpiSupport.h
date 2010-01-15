bde677688761fd476eb357139ce2385c7406417b
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2010-01-15 03:00:06 +0000

    Remove the duplicated definitions between Framework AcpiSupport and PI AcpiSystemDescriptionTable.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@9772 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkPkg/Include/Protocol/AcpiSupport.h
+++ IntelFrameworkPkg/Include/Protocol/AcpiSupport.h
@@ -26,6 +26,8 @@
 #ifndef _ACPI_SUPPORT_PROTOCOL_H_
 #define _ACPI_SUPPORT_PROTOCOL_H_
 
+#include <Protocol/AcpiSystemDescriptionTable.h>
+
 typedef struct _EFI_ACPI_SUPPORT_PROTOCOL EFI_ACPI_SUPPORT_PROTOCOL;
 
 //
@@ -36,32 +38,6 @@ typedef struct _EFI_ACPI_SUPPORT_PROTOCOL EFI_ACPI_SUPPORT_PROTOCOL;
     0xdbff9d55, 0x89b7, 0x46da, {0xbd, 0xdf, 0x67, 0x7d, 0x3d, 0xc0, 0x24, 0x1d } \
   }
 
-//
-// Protocol Data Definitions
-//
-//
-// ACPI Version bitmap definition:
-//
-// EFI_ACPI_TABLE_VERSION_1_0B - ACPI Version 1.0b
-// EFI_ACPI_TABLE_VERSION_2_0 - ACPI Version 2.0
-// EFI_ACPI_TABLE_VERSION_3_0 - ACPI Version 3.0
-// EFI_ACPI_TABLE_VERSION_NONE - No ACPI Versions.  This might be used
-//  to create memory-based operation regions or other information
-//  that is not part of the ACPI "tree" but must still be found
-//  in ACPI memory space and/or managed by the core ACPI driver.
-//
-// Note that EFI provides discrete GUIDs for each supported version of ACPI.
-// It is expected that each EFI GUIDed
-// version of ACPI will also have a corresponding bitmap
-// definition.  This bitmap definition enables maintenance of separate ACPI trees
-// for each distinctly different version of ACPI.
-//
-#define EFI_ACPI_TABLE_VERSION      UINT32
-
-#define EFI_ACPI_TABLE_VERSION_NONE (1 << 0)
-#define EFI_ACPI_TABLE_VERSION_1_0B (1 << 1)
-#define EFI_ACPI_TABLE_VERSION_2_0  (1 << 2)
-#define EFI_ACPI_TABLE_VERSION_3_0  (1 << 3)
 
 //
 // Protocol Member Functions
