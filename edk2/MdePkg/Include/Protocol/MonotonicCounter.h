aceceda71f11df3adf07a76e73aaafe88cbd28be
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2010-01-25 07:49:05 +0000

    Fix typo in Monotonic Counter GUID macro name
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@9806 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/Protocol/MonotonicCounter.h
+++ edk2/MdePkg/Include/Protocol/MonotonicCounter.h
@@ -3,7 +3,7 @@
 
   This code provides the services required to access the systems monotonic counter
 
-  Copyright (c) 2006 - 2008, Intel Corporation                                                         
+  Copyright (c) 2006 - 2010, Intel Corporation                                                         
   All rights reserved. This program and the accompanying materials                          
   are licensed and made available under the terms and conditions of the BSD License         
   which accompanies this distribution.  The full text of the license may be found at        
@@ -20,7 +20,7 @@
 ///
 /// Global ID for the Monotonic Counter Architectural Protocol
 ///
-#define EFI_MONTONIC_COUNTER_ARCH_PROTOCOL_GUID \
+#define EFI_MONOTONIC_COUNTER_ARCH_PROTOCOL_GUID \
   {0x1da97072, 0xbddc, 0x4b30, {0x99, 0xf1, 0x72, 0xa0, 0xb5, 0x6f, 0xff, 0x2a} }
   
 extern EFI_GUID gEfiMonotonicCounterArchProtocolGuid;
