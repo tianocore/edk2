fe92ab29bff9548a59011249e161127f7eeafa79
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2010-02-26 05:38:04 +0000

    import Debug Agent Library and null instance in MdeModulePkg.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10099 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ MdeModulePkg/Library/DebugAgentLibNull/DebugAgentLibNull.c
@@ -0,0 +1,51 @@
+/** @file
+  Debug Agent library implementition with empty functions.
+
+  Copyright (c) 2010, Intel Corporation
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+/**
+  Initialize debug agent.
+
+  This function is used to set up debug enviroment. It may enable interrupts.
+
+  @param[in] InitFlag   Init flag is used to decide initialize process.
+  @param[in] Context    Context needed according to InitFlag, it was optional.
+
+**/
+VOID
+EFIAPI
+InitializeDebugAgent (
+  IN UINT32                InitFlag,
+  IN VOID                  *Context  OPTIONAL
+  )
+{
+
+}
+
+/**
+  Enable/Disable the interrupt of debug timer.
+
+  If EnableStatus is TRUE, enable the interrupt of debug timer.
+  If EnableStatus is FALSE, disable the interrupt of debug timer.
+
+  @param[in] EnableStatus    Enable/Disable.
+
+**/
+VOID
+EFIAPI
+SetDebugTimerInterrupt (
+  IN BOOLEAN                EnableStatus
+  )
+{
+
+}
+
