c49a3c0f2ffa034ef3c774d6f84fbecd6722e7d2
qhuang8 <qhuang8@6f19259b-4bc3-4df7-8a09-765794883524>
2010-02-26 11:42:57 +0000

    Remove notice as it gets to publish in newest PI spec.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@10114 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/MdePkg/Include/Protocol/LegacyRegion2.h
+++ edk2/MdePkg/Include/Protocol/LegacyRegion2.h
@@ -2,7 +2,7 @@
   The Legacy Region Protocol controls the read, write and boot-lock attributes for
   the region 0xC0000 to 0xFFFFF.
 
-  Copyright (c) 2009, Intel Corporation
+  Copyright (c) 2009 - 2010, Intel Corporation
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -14,8 +14,6 @@
   @par Revision Reference:
   This Protocol is defined in UEFI Platform Initialization Specification 1.2 
   Volume 5: Standards
-
-  @attention Some definitions in this file do not match the PI 1.2 Specification.
 
 **/
 
