f7dab9fa6dd87a7a82e534e7efe76747064f3c66
xli24 <xli24@6f19259b-4bc3-4df7-8a09-765794883524>
2010-02-26 08:56:29 +0000

    Refine parameter description according to PI 1.2 errata A.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10109 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/Variable/Pei/Variable.c
+++ MdeModulePkg/Universal/Variable/Pei/Variable.c
@@ -3,7 +3,7 @@
   Implement ReadOnly Variable Services required by PEIM and install
   PEI ReadOnly Varaiable2 PPI. These services operates the non volatile storage space.
 
-Copyright (c) 2006 - 2009 Intel Corporation. <BR>
+Copyright (c) 2006 - 2010 Intel Corporation. <BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -590,6 +590,7 @@ PeiGetVariable (
   @param  This              A pointer to this instance of the EFI_PEI_READ_ONLY_VARIABLE2_PPI.
 
   @param  VariableNameSize  On entry, points to the size of the buffer pointed to by VariableName.
+                            On return, the size of the variable name buffer.
   @param  VariableName      On entry, a pointer to a null-terminated string that is the variable's name.
                             On return, points to the next variable's null-terminated name string.
   @param  VariableGuid      On entry, a pointer to an EFI_GUID that is the variable's GUID. 
