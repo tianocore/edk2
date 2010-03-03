f7c4d743c95c8025e26e78ea15fbd3c456826826
xli24 <xli24@6f19259b-4bc3-4df7-8a09-765794883524>
2010-03-03 03:21:43 +0000

    Remove over-specified dependencies on IntelFrameworkPkg and IntelFrameworkModulePkg.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10159 6f19259b-4bc3-4df7-8a09-765794883524

--- DuetPkg/EfiLdr/EfiLdr.h
+++ DuetPkg/EfiLdr/EfiLdr.h
@@ -1,6 +1,6 @@
 /*++
 
-Copyright (c) 2006, Intel Corporation                                                         
+Copyright (c) 2006 - 2010, Intel Corporation                                                         
 All rights reserved. This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -21,7 +21,6 @@ Revision History:
 #ifndef _DUET_EFI_LOADER_H_
 #define _DUET_EFI_LOADER_H_
 
-#include "FrameworkDxe.h"
 #include "Uefi.h"
 #include "EfiLdrHandoff.h"
 
