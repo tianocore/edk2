29b5e55d2d90980418499152c8ff8625dc51b270
jyao1 <jyao1@6f19259b-4bc3-4df7-8a09-765794883524>
2010-03-05 03:52:12 +0000

    Add SmmCore SmmServicesTable library. This library should be used and only used by SmmCore, (compare with SmmServicesTable Lib is used and only used by SmmDriver)
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@10202 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ edk2/MdePkg/Library/SmmCoreSmmServicesTableLib/SmmCoreSmmServicesTableLib.c
@@ -0,0 +1,40 @@
+/** @file
+  SMM Core SMM Services Table Library.
+
+  Copyright (c) 2010, Intel Corporation<BR>
+  All rights reserved. This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#include <PiSmm.h>
+#include <Library/SmmServicesTableLib.h>
+#include <Library/DebugLib.h>
+
+EFI_SMM_SYSTEM_TABLE2   *gSmst             = NULL;
+extern EFI_SMM_SYSTEM_TABLE2  gSmmCoreSmst;
+
+/**
+  The constructor function caches the pointer of SMM Services Table.
+
+  @param  ImageHandle   The firmware allocated handle for the EFI image.
+  @param  SystemTable   A pointer to the EFI System Table.
+
+  @retval EFI_SUCCESS   The constructor always returns EFI_SUCCESS.
+
+**/
+EFI_STATUS
+EFIAPI
+SmmCoreSmmServicesTableLibConstructor (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  gSmst = &gSmmCoreSmst;
+  return EFI_SUCCESS;
+}
