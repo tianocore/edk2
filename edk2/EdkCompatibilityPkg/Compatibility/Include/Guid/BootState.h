85d996344b6353f4c53f5af7c87c5c00031d92d9
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2010-03-10 02:17:49 +0000

    Add description for the added BootState guid.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk@10220 6f19259b-4bc3-4df7-8a09-765794883524

--- edk2/EdkCompatibilityPkg/Compatibility/Include/Guid/BootState.h
+++ edk2/EdkCompatibilityPkg/Compatibility/Include/Guid/BootState.h
@@ -1,6 +1,12 @@
 /** @file
-  GUID for use conveying the boot-state to PEI.
+  GUID and Name is used to configure PcdBootState to DynamicHii PCD, which can
+  make EDKII core work with EDK implementation.
 
+  EDK implementation may depend on this guid variable to check boot state. But 
+  EDKII core uses the dynamic PcdBootState to save boot state. To be compatible to EDK implementation, 
+  BootStateGuid and BootStateName will be used to configure PcdBootState to DynamicHii PCD to
+  save boot state as variable.
+  
   Copyright (c) 2010, Intel Corporation
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
@@ -13,6 +19,8 @@
 **/
 #ifndef _BOOT_STATE_H_
 #define _BOOT_STATE_H_
+
+#define BOOT_STATE_VARIABLE_NAME  L"BootState"
 
 #define  EFI_BOOT_STATE_VARIABLE_GUID  \
   { 0x60b5e939, 0xfcf, 0x4227, { 0xba, 0x83, 0x6b, 0xbe, 0xd4, 0x5b, 0xc0, 0xe3 } }
