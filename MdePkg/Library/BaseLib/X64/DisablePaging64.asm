bc810c8977e97c63f081f35d3ca1b2767282c1a3
geekboy15a <geekboy15a@6f19259b-4bc3-4df7-8a09-765794883524>
2010-04-16 22:34:22 +0000

    Fixed GCC 4.4 build issue.  Also fixed issue where upper 32-bits may be lost during AND operation.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10376 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/BaseLib/X64/DisablePaging64.asm
+++ MdePkg/Library/BaseLib/X64/DisablePaging64.asm
@@ -41,7 +41,7 @@ InternalX86DisablePaging64    PROC
     lea     rax, mTransitionEnd         ; rax <- end of transition code
     sub     rax, rsi                    ; rax <- The size of transition piece code 
     add     rax, 4                      ; Round RAX up to the next 4 byte boundary
-    and     rax, 0fffffffch
+    and     al, 0fch
     sub     rdi, rax                    ; rdi <- Use stack to hold transition code
     mov     r10d, edi                   ; r10 <- The start address of transicition code below 4G
     push    rcx                         ; save rcx to stack
