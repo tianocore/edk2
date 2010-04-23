4dd6f84007005ca39eb931f6e1a61f5b5b0b9ae5
hhtian <hhtian@6f19259b-4bc3-4df7-8a09-765794883524>
2010-04-23 15:37:19 +0000

    Update the copyright notice format
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10402 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/BaseIoLibIntrinsic/IoLibGcc.c
+++ MdePkg/Library/BaseIoLibIntrinsic/IoLibGcc.c
@@ -10,8 +10,8 @@
   We don't advocate putting compiler specifics in libraries or drivers but there
   is no other way to make this work.
 
-  Copyright (c) 2006 - 2008, Intel Corporation<BR>
-  All rights reserved. This program and the accompanying materials
+  Copyright (c) 2006 - 2008, Intel Corporation. All rights reserved.<BR>
+  This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
   http://opensource.org/licenses/bsd-license.php
--- MdePkg/Library/BaseIoLibIntrinsic/IoLibIcc.c
+++ MdePkg/Library/BaseIoLibIntrinsic/IoLibIcc.c
@@ -2,8 +2,8 @@
   I/O Library. This file has compiler specifics for ICC as there
   is no ANSI C standard for doing IO.
 
-  Copyright (c) 2006 - 2008, Intel Corporation<BR> All rights
-  reserved. This program and the accompanying materials are
+  Copyright (c) 2006 - 2008, Intel Corporation. All rights reserved.<BR>
+  This program and the accompanying materials are
   licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
   http://opensource.org/licenses/bsd-license.php
