1815f5bbc3ef65a4bb2ac2e04da818e4b63e3c62
hhtian <hhtian@6f19259b-4bc3-4df7-8a09-765794883524>
2010-04-23 15:36:12 +0000

    Update the copyright notice format
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10401 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/BaseDebugLibSerialPort/DebugLib.c
+++ MdePkg/Library/BaseDebugLibSerialPort/DebugLib.c
@@ -2,8 +2,8 @@
   Base Debug library instance base on Serial Port library.
   It uses PrintLib to send debug messages to serial port device.
 
-  Copyright (c) 2006 - 2008, Intel Corporation<BR>
-  All rights reserved. This program and the accompanying materials                          
+  Copyright (c) 2006 - 2008, Intel Corporation. All rights reserved.<BR>
+  This program and the accompanying materials                          
   are licensed and made available under the terms and conditions of the BSD License         
   which accompanies this distribution.  The full text of the license may be found at        
   http://opensource.org/licenses/bsd-license.php                                            
