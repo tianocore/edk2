f57387d54bd8f2c0261834b2ad16155f1db63291
hhtian <hhtian@6f19259b-4bc3-4df7-8a09-765794883524>
2010-04-28 12:14:56 +0000

    Update the copyright notice format
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10435 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkCompatibilityPkg/Foundation/Cpu/Pentium/CpuIA32Lib/X64/Cpu.asm
+++ EdkCompatibilityPkg/Foundation/Cpu/Pentium/CpuIA32Lib/X64/Cpu.asm
@@ -2,8 +2,8 @@ TITLE   Cpu.asm: Assembly code for the x64 resources
 
 ;------------------------------------------------------------------------------
 ;*
-;*   Copyright (c) 2005 - 2007, Intel Corporation                                                         
-;*   All rights reserved. This program and the accompanying materials                          
+;*   Copyright (c) 2005 - 2007, Intel Corporation. All rights reserved.<BR>
+;*   This program and the accompanying materials                          
 ;*   are licensed and made available under the terms and conditions of the BSD License         
 ;*   which accompanies this distribution.  The full text of the license may be found at        
 ;*   http://opensource.org/licenses/bsd-license.php                                            
--- EdkCompatibilityPkg/Foundation/Guid/EventLegacyBios/EventLegacyBios.h
+++ EdkCompatibilityPkg/Foundation/Guid/EventLegacyBios/EventLegacyBios.h
@@ -1,8 +1,8 @@
 /** @file
 	GUID is the name of events used with CreateEventEx in order to be notified when the EFI boot manager is about to boot a legacy boot option.  Events of this type are notificated just before Int19h is invoked. 
 
-	Copyright (c) 2006, Intel Corporation                                                         
-	All rights reserved. This program and the accompanying materials                          
+	Copyright (c) 2006, Intel Corporation. All rights reserved.<BR>
+  This program and the accompanying materials                          
 	are licensed and made available under the terms and conditions of the BSD License         
 	which accompanies this distribution.  The full text of the license may be found at        
 	http://opensource.org/licenses/bsd-license.php                                            
