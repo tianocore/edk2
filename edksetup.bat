24542fb2118e3921502c70996a30a8fe7cbc2d29
hhtian <hhtian@6f19259b-4bc3-4df7-8a09-765794883524>
2010-04-29 15:17:20 +0000

    Update the copyright notice format
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10448 6f19259b-4bc3-4df7-8a09-765794883524

--- edksetup.bat
+++ edksetup.bat
@@ -1,8 +1,8 @@
 @REM @file
 @REM   Windows batch file to setup a WORKSPACE environment
 @REM
-@REM Copyright (c) 2006 - 2008, Intel Corporation
-@REM All rights reserved. This program and the accompanying materials
+@REM Copyright (c) 2006 - 2008, Intel Corporation. All rights reserved.<BR>
+@REM This program and the accompanying materials
 @REM are licensed and made available under the terms and conditions of the BSD License
 @REM which accompanies this distribution.  The full text of the license may be found at
 @REM http://opensource.org/licenses/bsd-license.php
