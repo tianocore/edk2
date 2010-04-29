3d70643bbd4d6be9b3607b470e7c9aeb700d4ef4
hhtian <hhtian@6f19259b-4bc3-4df7-8a09-765794883524>
2010-04-29 12:46:45 +0000

    Update the copyright notice format
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10447 6f19259b-4bc3-4df7-8a09-765794883524

--- Omap35xxPkg/Library/SerialPortLib/SerialPortLib.c
+++ Omap35xxPkg/Library/SerialPortLib/SerialPortLib.c
@@ -2,9 +2,9 @@
   Serial I/O Port library functions with no library constructor/destructor
 
 
-  Copyright (c) 2008-2009, Apple Inc. All rights reserved.
+  Copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
   
-  All rights reserved. This program and the accompanying materials
+  This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
   http://opensource.org/licenses/bsd-license.php
