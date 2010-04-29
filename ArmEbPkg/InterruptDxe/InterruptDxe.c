cf748a1a6895a3418e2dea91bd0cd0e45f72c679
hhtian <hhtian@6f19259b-4bc3-4df7-8a09-765794883524>
2010-04-29 11:32:42 +0000

    Update the copyright notice format
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10443 6f19259b-4bc3-4df7-8a09-765794883524

--- ArmEbPkg/InterruptDxe/InterruptDxe.c
+++ ArmEbPkg/InterruptDxe/InterruptDxe.c
@@ -1,19 +1,8 @@
-/** @file
-  Portions copyright (c) 2010, Apple Inc. All rights reserved.
-  
-  All rights reserved. This program and the accompanying materials
-  are licensed and made available under the terms and conditions of the BSD License
-  which accompanies this distribution.  The full text of the license may be found at
-  http://opensource.org/licenses/bsd-license.php
-
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
-**/
 /*++
 
-Copyright (c) 2009, Hewlett-Packard Company                                                         
-All rights reserved. This program and the accompanying materials                          
+Copyright (c) 2009, Hewlett-Packard Company. All rights reserved.<BR>
+Portions copyright (c) 2010, Apple Inc. All rights reserved.<BR>                                                         
+This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
 http://opensource.org/licenses/bsd-license.php                                            
--- ArmEbPkg/Sec/Sec.c
+++ ArmEbPkg/Sec/Sec.c
@@ -1,9 +1,9 @@
 /** @file
   C Entry point for the SEC. First C code after the reset vector.
 
-  Copyright (c) 2008-2009, Apple Inc. All rights reserved.
+  Copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
   
-  All rights reserved. This program and the accompanying materials
+  This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
   http://opensource.org/licenses/bsd-license.php
