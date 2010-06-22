e4a34497b48b6721d9f08aa406919d09ef8754ab
MyronPorter <MyronPorter@6f19259b-4bc3-4df7-8a09-765794883524>
2010-06-22 22:59:38 +0000

    Minor grammatical work--mostly adding periods.  Items with ONLY period added did not have the heading date changed, but Items with content changes had heading copyright dates updated.  Sending  separately a list of files missing Doxygen @param and @return information. (PENDING)
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10593 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/BaseLib/Ia32/MultU64x64.c
+++ MdePkg/Library/BaseLib/Ia32/MultU64x64.c
@@ -1,11 +1,11 @@
 /** @file
   Calculate the product of a 64-bit integer and another 64-bit integer
 
-  Copyright (c) 2006 - 2008, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
-  http://opensource.org/licenses/bsd-license.php
+  http://opensource.org/licenses/bsd-license.php.
 
   THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
   WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
@@ -16,10 +16,10 @@
 
 
 /**
-  Multiples a 64-bit unsigned integer by a 64-bit unsigned integer
+  Multiplies a 64-bit unsigned integer by a 64-bit unsigned integer
   and generates a 64-bit unsigned result.
 
-  This function multiples the 64-bit unsigned value Multiplicand by the 64-bit
+  This function multiplies the 64-bit unsigned value Multiplicand by the 64-bit
   unsigned value Multiplier and generates a 64-bit unsigned result. This 64-
   bit unsigned result is returned.
 
