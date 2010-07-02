7ee525b2c1f8d3d6da5f788364fe5be36a979c9c
xdu2 <xdu2@6f19259b-4bc3-4df7-8a09-765794883524>
2010-07-02 03:27:51 +0000

    Report correct MediaPresentSupported value from Nt32 SNP mode data.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10620 6f19259b-4bc3-4df7-8a09-765794883524

--- Nt32Pkg/SnpNt32Dxe/SnpNt32.c
+++ Nt32Pkg/SnpNt32Dxe/SnpNt32.c
@@ -1,6 +1,6 @@
 /** @file
 
-Copyright (c) 2006 - 2009, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -440,7 +440,7 @@ SNPNT32_INSTANCE_DATA gSnpNt32InstanceTemplate = {
     NET_IFTYPE_ETHERNET,                  //  IfType
     FALSE,                                //  MacAddressChangeable
     FALSE,                                //  MultipleTxSupported
-    FALSE,                                //  MediaPresentSupported
+    TRUE,                                 //  MediaPresentSupported
     TRUE                                  //  MediaPresent
   },
   {
