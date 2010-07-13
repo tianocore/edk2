05399810c5c015c3d83381518989dac238c5d55a
rsun3 <rsun3@6f19259b-4bc3-4df7-8a09-765794883524>
2010-07-13 02:13:55 +0000

    Return from ProgramBar() after VF BARs are programmed.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10645 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Bus/Pci/PciBusDxe/PciResourceSupport.c
+++ MdeModulePkg/Bus/Pci/PciBusDxe/PciResourceSupport.c
@@ -1,7 +1,7 @@
 /** @file
   PCI resouces support functions implemntation for PCI Bus module.
 
-Copyright (c) 2006 - 2009, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -1291,6 +1291,7 @@ ProgramBar (
   //
   if (Node->Virtual) {
     ProgramVfBar (Base, Node);
+    return;
   }
 
   Address = 0;
