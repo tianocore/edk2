2496b465121c77e4b6f931fcc0089ae397b11d08
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2010-07-19 05:33:56 +0000

    1.  Update FFS_FIXED_CHECKSUM to 0xAA to match PI FV image generated by EDKII build tool. 2.  Clean up EfiScriptLib by removing unused EfiDriverLib from its INF file.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10662 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkCompatibilityPkg/Foundation/Framework/Include/EfiFirmwareFileSystem.h
+++ EdkCompatibilityPkg/Foundation/Framework/Include/EfiFirmwareFileSystem.h
@@ -1,6 +1,6 @@
 /*++
 
-Copyright (c) 2004 - 2007, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2010, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -62,10 +62,9 @@ Abstract:
 //
 // FFS_FIXED_CHECKSUM is the default checksum value used when the
 // FFS_ATTRIB_CHECKSUM attribute bit is clear
-// note this is NOT an architecturally defined value, but is in this file for
-// implementation convenience
+// This value is defined in PI 1.2.
 //
-#define FFS_FIXED_CHECKSUM  0x5A
+#define FFS_FIXED_CHECKSUM  0xAA
 
 
 //
