d4b6dd9300902dc071d01482d0573a046478bb0e
hhtian <hhtian@6f19259b-4bc3-4df7-8a09-765794883524>
2010-07-24 11:41:06 +0000

    minor format update
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@10691 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/BasePeCoffLib/BasePeCoffLibInternals.h
+++ MdePkg/Library/BasePeCoffLib/BasePeCoffLibInternals.h
@@ -1,7 +1,7 @@
 /** @file
   Declaration of internal functions in PE/COFF Lib.
 
-  Copyright (c) 2006 -2010, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
