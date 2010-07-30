7138606427e2678fd829caf426262f5c90603311
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2010-07-30 08:47:45 +0000

    sync patch 10719 from main trunk. r10719 - Correct the check for macro definition __EDKII_GLUE_PEI_HOB_LIB__.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/branches/UDK2010@10732 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkCompatibilityPkg/Foundation/Library/EdkIIGlueLib/Include/Common/EdkIIGlueDependencies.h
+++ EdkCompatibilityPkg/Foundation/Library/EdkIIGlueLib/Include/Common/EdkIIGlueDependencies.h
@@ -1,7 +1,7 @@
 /*++
 
-Copyright (c) 2004 - 2010, Intel Corporation                                                         
-All rights reserved. This program and the accompanying materials                          
+Copyright (c) 2004 - 2010, Intel Corporation. All rights reserved.<BR>
+This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
 http://opensource.org/licenses/bsd-license.php                                            
@@ -631,7 +631,7 @@ Abstract:
   #error EdkIIGluePciLibPciExpress and EdkIIGluePciLibCf8: can only be mutual exclusively used.
 #endif
 
-#if defined (__EDKII_GLUE_DXE_HOB_LIB__) && (__EDKII_GLUE_PEI_HOB_LIB__)
+#if defined(__EDKII_GLUE_DXE_HOB_LIB__) && defined(__EDKII_GLUE_PEI_HOB_LIB__)
   #error EdkIIGlueDxeHobLib and EdkIIGluePeiHobLib: can only be mutual exclusively used.
 #endif
 
