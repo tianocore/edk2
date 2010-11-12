be3b67bca5dfd62cea1148470554f6628efb9b7b
li-elvin <li-elvin@6f19259b-4bc3-4df7-8a09-765794883524>
2010-11-12 07:45:08 +0000

    Fix the issue that the high 16-bit of EAX may contain invalid data, which cause bad stack pointer, the change will ensure the high 16-bit of EAX is cleared.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@11046 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkCompatibilityPkg/Foundation/Library/EdkIIGlueLib/Library/BaseLib/Ia32/Thunk16.asm
+++ EdkCompatibilityPkg/Foundation/Library/EdkIIGlueLib/Library/BaseLib/Ia32/Thunk16.asm
@@ -1,4 +1,4 @@
-; Copyright (c) 2004, Intel Corporation. All rights reserved.<BR>
+; Copyright (c) 2004 - 2010, Intel Corporation. All rights reserved.<BR>
 ; This program and the accompanying materials                          
 ; are licensed and made available under the terms and conditions of the BSD License         
 ; which accompanies this distribution.  The full text of the license may be found at        
@@ -108,7 +108,7 @@ _ThunkAttr  DD      ?
     or      al, 2
     out     92h, al                     ; deactivate A20M#
 @2:
-    mov     eax, ss
+    mov     ax, ss
     DB      67h
     lea     bp, [esp + sizeof (IA32_REGS)]
     ;
