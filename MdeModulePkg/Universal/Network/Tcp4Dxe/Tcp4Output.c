8363c46f4c64943e6c54b53e6ba18628d3c171ed
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2010-11-17 03:28:28 +0000

    sync patch r10831 from main trunk. The patch acknowledges the TCP zero window probe message, either the format with 1 byte new data, or no new data. It also increases exponentially the interval between successive probes when performing TCP zero window probe.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/branches/UDK2008@11070 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/Network/Tcp4Dxe/Tcp4Output.c
+++ MdeModulePkg/Universal/Network/Tcp4Dxe/Tcp4Output.c
@@ -1,7 +1,7 @@
 /** @file
   TCP output process routines.
     
-Copyright (c) 2005 - 2009, Intel Corporation<BR>
+Copyright (c) 2005 - 2010, Intel Corporation<BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -267,7 +267,9 @@ SetPersistTimer:
       Tcb)
       );
 
-    TcpSetProbeTimer (Tcb);
+    if (!Tcb->ProbeTimerOn) {
+      TcpSetProbeTimer (Tcb);
+    }
   }
 
   return 0;
--- MdeModulePkg/Universal/Network/Tcp4Dxe/Tcp4Proto.h
+++ MdeModulePkg/Universal/Network/Tcp4Dxe/Tcp4Proto.h
@@ -1,7 +1,7 @@
 /** @file
   Tcp Protocol header file.
 
-Copyright (c) 2005 - 2006, Intel Corporation<BR>
+Copyright (c) 2005 - 2010, Intel Corporation<BR>
 All rights reserved. This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -212,6 +212,7 @@ struct _TCP_CB {
   INT32             NextExpire;  ///< Count down offset for the nearest timer
   UINT32            Idle;        ///< How long the connection is in idle
   UINT32            ProbeTime;   ///< The time out value for current window prober
+  BOOLEAN           ProbeTimerOn;///< If TRUE, the probe time is on.
 
   //
   // RFC1323 defined variables, about window scale,
