dad8dea7c8d75c7b69c0386d78f2b59f02bda2a2
mdkinney <mdkinney@6f19259b-4bc3-4df7-8a09-765794883524>
2011-01-06 02:47:38 +0000

    Fix Copyright year to 2011
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@11229 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Library/DebugLib.h
+++ MdePkg/Include/Library/DebugLib.h
@@ -8,7 +8,7 @@
   of size reduction when compiler optimization is disabled. If MDEPKG_NDEBUG is
   defined, then debug and assert related macros wrapped by it are the NULL implementations.
 
-Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2011, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials are licensed and made available under 
 the terms and conditions of the BSD License that accompanies this distribution.  
 The full text of the license may be found at
