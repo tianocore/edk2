858939cbdaa973cae59277be3b682fda115d7ff7
erictian <erictian@6f19259b-4bc3-4df7-8a09-765794883524>
2011-01-26 04:24:54 +0000

    fix arm gcc build error
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@11272 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/Variable/RuntimeDxe/VariableDxe.c
+++ MdeModulePkg/Universal/Variable/RuntimeDxe/VariableDxe.c
@@ -3,7 +3,7 @@
   Implement all four UEFI Runtime Variable services for the nonvolatile
   and volatile storage space and install variable architecture protocol.
   
-Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2011, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -17,7 +17,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #include "Variable.h"
 
 extern VARIABLE_STORE_HEADER   *mNvVariableCache;
-VARIABLE_INFO_ENTRY            *gVariableInfo;
+extern VARIABLE_INFO_ENTRY     *gVariableInfo;
 EFI_HANDLE                     mHandle                    = NULL;
 EFI_EVENT                      mVirtualAddressChangeEvent = NULL;
 EFI_EVENT                      mFtwRegistration           = NULL;
