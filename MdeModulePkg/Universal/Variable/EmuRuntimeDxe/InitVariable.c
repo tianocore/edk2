b7bc11a4ff619ad03fce5b76b9c957563b16d2d8
erictian <erictian@6f19259b-4bc3-4df7-8a09-765794883524>
2011-02-12 03:29:24 +0000

    correct Intel's copyright date as the changes checked-in at r11273 & r11272 are contributed by third party.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@11311 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/Variable/EmuRuntimeDxe/InitVariable.c
+++ MdeModulePkg/Universal/Variable/EmuRuntimeDxe/InitVariable.c
@@ -3,7 +3,7 @@
   Implment all four UEFI runtime variable services and 
   install variable architeture protocol.
   
-Copyright (c) 2006 - 2011, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
