5788e95470ef53ff4b47e59ed6b097818077513e
hhtian <hhtian@6f19259b-4bc3-4df7-8a09-765794883524>
2011-02-15 12:13:00 +0000

    update copyright format
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@11314 6f19259b-4bc3-4df7-8a09-765794883524

--- BaseTools/Bin/Win32/ImportTool.bat
+++ BaseTools/Bin/Win32/ImportTool.bat
@@ -1,6 +1,6 @@
 @REM
-@REM Copyright (c) 2008, Intel Corporation
-@REM All rights reserved. This program and the accompanying materials
+@REM Copyright (c) 2008, Intel Corporation. All rights reserved.<BR>
+@REM This program and the accompanying materials
 @REM are licensed and made available under the terms and conditions of the BSD License
 @REM which accompanies this distribution.  The full text of the license may be found at
 @REM http://opensource.org/licenses/bsd-license.php
