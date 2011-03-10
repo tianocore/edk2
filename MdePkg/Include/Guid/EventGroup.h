d12c6a54856a00474c77576cc7ffc167839145ca
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2011-03-10 01:20:15 +0000

    Updated the GUID name to follow PI Spec, 1.2 Errata B.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@11358 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Guid/EventGroup.h
+++ MdePkg/Include/Guid/EventGroup.h
@@ -1,7 +1,7 @@
 /** @file
   GUIDs for gBS->CreateEventEx Event Groups. Defined in UEFI spec 2.0 and PI 1.2.
 
-Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2011, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials are licensed and made available under 
 the terms and conditions of the BSD License that accompanies this distribution.  
 The full text of the license may be found at
@@ -39,7 +39,7 @@ extern EFI_GUID gEfiEventMemoryMapChangeGuid;
 
 extern EFI_GUID gEfiEventReadyToBootGuid;
 
-#define EFI_EVENT_GROUP_DXE_DISPATCH \
+#define EFI_EVENT_GROUP_DXE_DISPATCH_GUID \
   { 0x7081e22f, 0xcac6, 0x4053, { 0x94, 0x68, 0x67, 0x57, 0x82, 0xcf, 0x88, 0xe5 }}
 
 extern EFI_GUID gEfiEventDxeDispatchGuid;
