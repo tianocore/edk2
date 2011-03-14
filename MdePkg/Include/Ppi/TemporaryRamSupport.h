1ff0b1eb6d02d32fe67f2901f00a46b946c550b5
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2011-03-14 01:45:11 +0000

    Changed TEMPORARY_RAM_SUPPORT_PPI to EFI_PEI_TEMPORARY_RAM_SUPPORT_PPI per PI Spec, 1.2 Errata B. This was one incompatible change.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@11384 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Ppi/TemporaryRamSupport.h
+++ MdePkg/Include/Ppi/TemporaryRamSupport.h
@@ -2,7 +2,7 @@
   This file declares Temporary RAM Support PPI.
   This Ppi provides the service that migrates temporary RAM into permanent memory.
 
-  Copyright (c) 2006 - 2008, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2011, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials                          
   are licensed and made available under the terms and conditions of the BSD License         
   which accompanies this distribution.  The full text of the license may be found at        
@@ -19,12 +19,16 @@
 #ifndef __TEMPORARY_RAM_SUPPORT_H__
 #define __TEMPORARY_RAM_SUPPORT_H__
 
-#define TEMPORARY_RAM_SUPPORT_PPI_GUID \
+///
+/// Note: The GUID name EFI_PEI_TEMPORARY_RAM_SUPPORT_PPI_GUID is different from the current
+///       PI 1.2 spec.
+///
+#define EFI_PEI_TEMPORARY_RAM_SUPPORT_PPI_GUID  \
   { 0xdbe23aa9, 0xa345, 0x4b97, {0x85, 0xb6, 0xb2, 0x26, 0xf1, 0x61, 0x73, 0x89} }
 
 
 /**
-  This service of the TEMPORARY_RAM_SUPPORT_PPI that migrates temporary RAM into
+  This service of the EFI_PEI_TEMPORARY_RAM_SUPPORT_PPI that migrates temporary RAM into
   permanent memory.
 
   @param PeiServices            Pointer to the PEI Services Table.
@@ -50,10 +54,12 @@ EFI_STATUS
 
 ///
 /// This service abstracts the ability to migrate contents of the platform early memory store.
+/// Note: The name EFI_PEI_TEMPORARY_RAM_SUPPORT_PPI is different from the current PI 1.2 spec.
+///       This PPI was optional.
 ///
 typedef struct {
   TEMPORARY_RAM_MIGRATION   TemporaryRamMigration;
-} TEMPORARY_RAM_SUPPORT_PPI;
+} EFI_PEI_TEMPORARY_RAM_SUPPORT_PPI;
 
 extern EFI_GUID gEfiTemporaryRamSupportPpiGuid;
 
