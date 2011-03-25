7d271d7b01299f4a66a963996e40d34d4fc08b0b
jcarsey <jcarsey@6f19259b-4bc3-4df7-8a09-765794883524>
2011-03-25 21:01:56 +0000

    move include file line under #define protection.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@11428 6f19259b-4bc3-4df7-8a09-765794883524

--- ShellPkg/Include/Protocol/EfiShellParameters.h
+++ ShellPkg/Include/Protocol/EfiShellParameters.h
@@ -1,7 +1,7 @@
 /** @file
   EFI Shell protocol as defined in the UEFI Shell 2.0 specification.
 
-  Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2011, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -12,10 +12,10 @@
 
 **/
 
-#include <ShellBase.h>
-
 #ifndef __EFI_SHELL_PARAMETERS_PROTOCOL__
 #define __EFI_SHELL_PARAMETERS_PROTOCOL__
+
+#include <ShellBase.h>
 
 #define EFI_SHELL_PARAMETERS_PROTOCOL_GUID \
   { \
