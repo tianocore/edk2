c5fab60fb91d2ce45bae5226520a7026a82a63f4
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2011-04-13 06:48:03 +0000

    sync patch r10900 from main trunk. Add ACPI4.0 header file.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/branches/UDK2010@11533 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/IndustryStandard/Acpi.h
+++ MdePkg/Include/IndustryStandard/Acpi.h
@@ -2,7 +2,7 @@
   This file contains the latest ACPI definitions that are
   consumed by drivers that do not care about ACPI versions.
 
-  Copyright (c) 2006 - 2008, Intel Corporation
+  Copyright (c) 2006 - 2010, Intel Corporation
   All rights reserved. This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -16,6 +16,6 @@
 #ifndef _ACPI_H_
 #define _ACPI_H_
 
-#include <IndustryStandard/Acpi30.h>
+#include <IndustryStandard/Acpi40.h>
 
 #endif
