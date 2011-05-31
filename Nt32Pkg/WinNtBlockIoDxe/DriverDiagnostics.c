283d361e98f2e7da579b022426d3dfb4fc1e33a8
niruiyu <niruiyu@6f19259b-4bc3-4df7-8a09-765794883524>
2011-05-31 05:56:20 +0000

    Fix the bug that WinNtBlockIo fails to properly allocate a buffer.
    Signed-off-by: niruiyu
    Reviewed-by: ydong10
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@11717 6f19259b-4bc3-4df7-8a09-765794883524

--- Nt32Pkg/WinNtBlockIoDxe/DriverDiagnostics.c
+++ Nt32Pkg/WinNtBlockIoDxe/DriverDiagnostics.c
@@ -1,6 +1,6 @@
 /**@file
 
-Copyright (c) 2006 - 2007, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2011, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -179,7 +179,7 @@ WinNtBlockIoDriverDiagnosticsRunDiagnostics (
   if (DiagnosticType != EfiDriverDiagnosticTypeStandard) {
     *ErrorType  = &gEfiBlockIoProtocolGuid;
     *BufferSize = 0x60;
-    Buffer = AllocatePool ((UINTN) (*BufferSize));
+    *Buffer = AllocatePool ((UINTN) (*BufferSize));
     CopyMem (*Buffer, L"Windows Block I/O Driver Diagnostics Failed\n", *BufferSize);
     return EFI_DEVICE_ERROR;
   }
