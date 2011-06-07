98208ad363dc301e900ba36c3f5434f94c666721
hhtian <hhtian@6f19259b-4bc3-4df7-8a09-765794883524>
2011-06-07 08:07:29 +0000

    update copyright line format
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@11757 6f19259b-4bc3-4df7-8a09-765794883524

--- OptionRomPkg/Include/Library/BltLib.h
+++ OptionRomPkg/Include/Library/BltLib.h
@@ -1,8 +1,8 @@
 /** @file
   Library for performing video blt operations
 
-  Copyright (c) 2009 - 2011, Intel Corporation<BR>
-  All rights reserved. This program and the accompanying materials
+  Copyright (c) 2009 - 2011, Intel Corporation. All rights reserved.<BR>
+  This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
   http://opensource.org/licenses/bsd-license.php
