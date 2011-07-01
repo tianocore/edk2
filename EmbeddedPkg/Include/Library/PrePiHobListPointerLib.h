32f718cf06733443157b5aea509c508e2eba1529
oliviermartin <oliviermartin@6f19259b-4bc3-4df7-8a09-765794883524>
2011-07-01 10:59:06 +0000

    EmbeddedPkg/PrePiHobListPointerLib: New interface to abstract the HobListPointer storage
    This interface allows to use different mechanism to store the HobList Pointer.
    
    
    EmbeddedPkg/PrePiHobListPointerLib: Implementation of PrePiHobListPointerLib using PCD
    
    This library uses a PCD to store the location of the HobList Pointer.
    
    
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@11947 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ EmbeddedPkg/Include/Library/PrePiHobListPointerLib.h
@@ -0,0 +1,44 @@
+/** @file
+*
+*  Copyright (c) 2011, ARM Limited. All rights reserved.
+*  
+*  This program and the accompanying materials                          
+*  are licensed and made available under the terms and conditions of the BSD License         
+*  which accompanies this distribution.  The full text of the license may be found at        
+*  http://opensource.org/licenses/bsd-license.php                                            
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+*
+**/
+
+#ifndef __PREPI_HOBLIST_POINTER_LIB_H__
+#define __PREPI_HOBLIST_POINTER_LIB_H__
+
+/**
+  Returns the pointer to the HOB list.
+
+  This function returns the pointer to first HOB in the list.
+
+  @return The pointer to the HOB list.
+
+**/
+VOID *
+EFIAPI
+PrePeiGetHobList (
+  VOID
+  );
+
+/**
+  Updates the pointer to the HOB list.
+
+  @param  HobList       Hob list pointer to store
+  
+**/
+EFI_STATUS
+EFIAPI
+PrePeiSetHobList (
+  IN  VOID      *HobList
+  );
+
+#endif
