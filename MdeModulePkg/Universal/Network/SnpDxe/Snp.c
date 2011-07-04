f3de0a607dd36d045ef8d1782db5e9c296cf6b43
xdu2 <xdu2@6f19259b-4bc3-4df7-8a09-765794883524>
2011-07-04 07:33:34 +0000

    MdeModulePkg: Add unload support for SnpDxe and fix NULL ImageHandle in EfiLibInstallDriverBindingComponentName2() in driver entry point.
    Signed-off-by: xdu2
    Reviewed-by: vanjeff
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@11967 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/Network/SnpDxe/Snp.c
+++ MdeModulePkg/Universal/Network/SnpDxe/Snp.c
@@ -1,7 +1,7 @@
 /** @file
   Implementation of driver entry point and driver binding protocol.
 
-Copyright (c) 2004 - 2010, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2011, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials are licensed
 and made available under the terms and conditions of the BSD License which
 accompanies this distribution. The full text of the license may be found at
@@ -1014,7 +1014,7 @@ InitializeSnpNiiDriver (
            ImageHandle,
            SystemTable,
            &mSimpleNetworkDriverBinding,
-           NULL,
+           ImageHandle,
            &gSimpleNetworkComponentName,
            &gSimpleNetworkComponentName2
            );
