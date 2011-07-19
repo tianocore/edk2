664e368fd99c1d18141380c6195d3d9961f72bb0
jljusten <jljusten@6f19259b-4bc3-4df7-8a09-765794883524>
2011-07-19 20:47:45 +0000

    IntelFrameworkModulePkg: Add ExitPmAuth protocol definition
    Signed-off-by: jljusten
    Reviewed-by: geekboy15a
    Reviewed-by: jyao1
    Reviewed-by: lgao4
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@12030 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ IntelFrameworkModulePkg/Include/Protocol/ExitPmAuth.h
@@ -0,0 +1,25 @@
+/** @file
+  Defines the ExitPmAuth protocol.
+
+Copyright (c) 2010, Intel Corporation. All rights reserved.<BR>
+
+This program and the accompanying materials
+are licensed and made available under the terms and conditions
+of the BSD License which accompanies this distribution.  The
+full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef _EXIT_PM_AUTH_PROTOCOL_H_
+#define _EXIT_PM_AUTH_PROTOCOL_H_
+
+#define EXIT_PM_AUTH_PROTOCOL_GUID \
+  { 0xd088a413, 0xa70, 0x4217, { 0xba, 0x55, 0x9a, 0x3c, 0xb6, 0x5c, 0x41, 0xb3 }}
+
+extern EFI_GUID gExitPmAuthProtocolGuid;
+
+#endif // #ifndef _EXIT_PM_AUTH_PROTOCOL_H_
