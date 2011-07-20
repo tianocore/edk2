f7c8bd9f9d22d3124ef30737dae2fbe49fdacd41
andrewfish <andrewfish@6f19259b-4bc3-4df7-8a09-765794883524>
2011-07-20 17:47:44 +0000

    MdePkg: Update CpuSleep to use ARMv7 instruction.
    The code was using ARMv6 co-processor reg, that is a no-op on ARMv7. WFI is the instruction on ARMv7.
    
    signed-off-by: andrewfish
    reviewed-by: mdkinney
    
    
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@12035 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/BaseCpuLib/Arm/CpuSleep.asm
+++ MdePkg/Library/BaseCpuLib/Arm/CpuSleep.asm
@@ -1,9 +1,15 @@
 ;------------------------------------------------------------------------------ 
 ;
-; CpuSleep() for ARM
+; CpuSleep() for ARMv7
+;
+; ARMv6 versions was:
+;    MOV r0,#0
+;    MCR p15,0,r0,c7,c0,4   ;Wait for Interrupt instruction
+;
+; But this is a no-op on ARMv7
 ;
 ; Copyright (c) 2006 - 2009, Intel Corporation. All rights reserved.<BR>
-; Portions copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
+; Portions copyright (c) 2008 - 2011, Apple Inc. All rights reserved.<BR>
 ; This program and the accompanying materials
 ; are licensed and made available under the terms and conditions of the BSD License
 ; which accompanies this distribution.  The full text of the license may be found at
@@ -32,8 +38,7 @@
 ;  );
 ;
 CpuSleep
-    MOV r0,#0
-    MCR p15,0,r0,c7,c0,4   ;Wait for Interrupt instruction
+    WFI
     BX LR
 
   END
