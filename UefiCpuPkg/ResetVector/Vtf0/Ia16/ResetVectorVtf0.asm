8332983e2e334c414eeca19b5742a9ec21194fb7
rsun3 <rsun3@6f19259b-4bc3-4df7-8a09-765794883524>
2011-08-04 03:25:14 +0000

    UefiCpuPkg: Replace the un-necessary WBINVD instruction at the reset vector with two NOPs in VTF0.
    Signed-off-by: rsun3
    Reviewed-by: mdkinney
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@12085 6f19259b-4bc3-4df7-8a09-765794883524

--- UefiCpuPkg/ResetVector/Vtf0/Ia16/ResetVectorVtf0.asm
+++ UefiCpuPkg/ResetVector/Vtf0/Ia16/ResetVectorVtf0.asm
@@ -2,7 +2,7 @@
 ; @file
 ; First code exectuted by processor after resetting.
 ;
-; Copyright (c) 2008 - 2009, Intel Corporation. All rights reserved.<BR>
+; Copyright (c) 2008 - 2011, Intel Corporation. All rights reserved.<BR>
 ; This program and the accompanying materials
 ; are licensed and made available under the terms and conditions of the BSD License
 ; which accompanies this distribution.  The full text of the license may be found at
@@ -48,7 +48,8 @@ resetVector:
 ;
 ; This is where the processor will begin execution
 ;
-    wbinvd
+    nop
+    nop
     jmp     short EarlyBspInitReal16
 
 ALIGN   16
