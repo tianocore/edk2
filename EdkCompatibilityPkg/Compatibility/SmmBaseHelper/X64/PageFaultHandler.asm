743ee63f93bf9aa2e25909f759ca698a01df3019
rsun3 <rsun3@6f19259b-4bc3-4df7-8a09-765794883524>
2011-09-28 09:00:06 +0000

    EdkCompatibilityPkg SmmBaseHelper: Fix PageFaultHandlerHook assembly code to use AL register for return value from PageFaultHandler().
    Signed-off-by: rsun3
    Reviewed-by: vanjeff
    
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@12466 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkCompatibilityPkg/Compatibility/SmmBaseHelper/X64/PageFaultHandler.asm
+++ EdkCompatibilityPkg/Compatibility/SmmBaseHelper/X64/PageFaultHandler.asm
@@ -1,6 +1,6 @@
 ;------------------------------------------------------------------------------
 ;
-; Copyright (c) 2010, Intel Corporation. All rights reserved.<BR>
+; Copyright (c) 2010 - 2011, Intel Corporation. All rights reserved.<BR>
 ; This program and the accompanying materials
 ; are licensed and made available under the terms and conditions of the BSD License
 ; which accompanies this distribution.  The full text of the license may be found at
@@ -35,7 +35,7 @@ PageFaultHandlerHook PROC
     add     rsp, -20h
     call    PageFaultHandler
     add     rsp, 20h
-    test    rax, rax
+    test    al, al
     pop     r11
     pop     r10
     pop     r9
