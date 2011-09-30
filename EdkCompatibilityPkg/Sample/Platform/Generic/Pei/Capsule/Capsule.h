ec2b081a9cb8bb63d4bc6f429c359ca960148a34
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2011-09-30 05:57:44 +0000

    1. Move Capsule variable name definition to Capsule variable GUID header file. 2. Align EFI_CAPSULE_LONG_MODE_BUFFER structure to MdeModulePkg definition.
    Signed-off-by: lgao4
    Reviewed-by: hhtian
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@12492 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkCompatibilityPkg/Sample/Platform/Generic/Pei/Capsule/Capsule.h
+++ EdkCompatibilityPkg/Sample/Platform/Generic/Pei/Capsule/Capsule.h
@@ -1,6 +1,6 @@
 /*++        
 
-Copyright (c) 2004 - 2010, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2011, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -21,20 +21,6 @@ Abstract:
 #define _CAPSULE_PEIM_H
 
 #include "Tiano.h"
-
-//
-// If capsule data is passed via a variable, then this name should be used.
-//
-#define EFI_CAPSULE_VARIABLE_NAME L"CapsuleUpdateData"
-
-//
-// The variable describes the long mode buffer used by IA32 Capsule PEIM
-// to call X64 CapsuleCoalesce code to handle >4GB capsule blocks.
-//
-#define EFI_CAPSULE_LONG_MODE_BUFFER_NAME L"CapsuleLongModeBuffer"
-typedef struct {
-  UINT64   Base;
-  UINT64   Length;
-} EFI_CAPSULE_LONG_MODE_BUFFER;
+#include EFI_GUID_DEFINITION (Capsule)
 
 #endif
