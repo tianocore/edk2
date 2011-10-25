9c2454094486a3326f5cdc30fbdcf1b1b09311a0
lgao4 <lgao4@6f19259b-4bc3-4df7-8a09-765794883524>
2011-10-25 08:19:59 +0000

    Correct CpuPause.s to ICC11. Signed-off-by: lgao4
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@12565 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkCompatibilityPkg/Foundation/Library/EdkIIGlueLib/Library/BaseLib/Ipf/CpuPause.s
+++ EdkCompatibilityPkg/Foundation/Library/EdkIIGlueLib/Library/BaseLib/Ipf/CpuPause.s
@@ -1,4 +1,4 @@
-/// Copyright (c) 2004, Intel Corporation. All rights reserved.<BR>
+/// Copyright (c) 2004 - 2011, Intel Corporation. All rights reserved.<BR>
 /// This program and the accompanying materials                          
 /// are licensed and made available under the terms and conditions of the BSD License         
 /// which accompanies this distribution.  The full text of the license may be found at        
@@ -21,6 +21,6 @@
 .proc   CpuPause
 .type   CpuPause, @function
 CpuPause::
-        hint                0 //@pause
+        hint.i              @pause
         br.ret.sptk.many    b0
 .endp
