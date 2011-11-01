aa26b8f6c86af5c179ef63ed5e0f0d31d4dbb39c
oliviermartin <oliviermartin@6f19259b-4bc3-4df7-8a09-765794883524>
2011-11-01 23:45:09 +0000

    ArmPlatformPkg/PrePi: Remove duplicate call of BuildMemoryTypeInformationHob()
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@12645 6f19259b-4bc3-4df7-8a09-765794883524

--- ArmPlatformPkg/PrePi/PrePi.c
+++ ArmPlatformPkg/PrePi/PrePi.c
@@ -140,8 +140,6 @@ PrePiMain (
   Status = PlatformPeim ();
   ASSERT_EFI_ERROR (Status);
 
-  BuildMemoryTypeInformationHob ();
-
   // Now, the HOB List has been initialized, we can register performance information
   PERF_START (NULL, "PEI", NULL, StartTimeStamp);
 
