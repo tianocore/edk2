e02c57b63bfb48327c966adbcb2d25435a5cadcf
erictian <erictian@6f19259b-4bc3-4df7-8a09-765794883524>
2011-11-17 06:43:01 +0000

    PcAtChipsetPkg: Do right sign extension to avoid a wrong daylight value saving into "RTC" variable
    signed-off-by: erictian
    reviewed-by: niruiyu
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@12732 6f19259b-4bc3-4df7-8a09-765794883524

--- PcAtChipsetPkg/PcatRealTimeClockRuntimeDxe/PcRtc.c
+++ PcAtChipsetPkg/PcatRealTimeClockRuntimeDxe/PcRtc.c
@@ -1,7 +1,7 @@
 /** @file
   RTC Architectural Protocol GUID as defined in DxeCis 0.96.
 
-Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2011, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -414,7 +414,7 @@ PcRtcSetTime (
   Global->Daylight      = Time->Daylight;
 
   TimerVar = Time->Daylight;
-  TimerVar = (UINT32) ((TimerVar << 16) | Time->TimeZone);
+  TimerVar = (UINT32) ((TimerVar << 16) | (UINT16)(Time->TimeZone));
   Status =  EfiSetVariable (
               L"RTC",
               &gEfiCallerIdGuid,
