993b8cddddce6d22404b3bedd023c96120a0c641
lzeng14 <lzeng14@6f19259b-4bc3-4df7-8a09-765794883524>
2011-12-02 01:08:05 +0000

    Free memory allocated by ReadSection.
    Signed-off-by: lzeng14
    Reviewed-by: ZhangCaoIntel
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@12811 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/Acpi/AcpiPlatformDxe/AcpiPlatform.c
+++ MdeModulePkg/Universal/Acpi/AcpiPlatformDxe/AcpiPlatform.c
@@ -1,7 +1,7 @@
 /** @file
   Sample ACPI Platform Driver
 
-  Copyright (c) 2008 - 2010, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2008 - 2011, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -237,6 +237,12 @@ AcpiPlatformEntryPoint (
                             TableSize,
                             &TableHandle
                             );
+
+      //
+      // Free memory allocated by ReadSection
+      //
+      gBS->FreePool (CurrentTable);
+
       if (EFI_ERROR(Status)) {
         return EFI_ABORTED;
       }
