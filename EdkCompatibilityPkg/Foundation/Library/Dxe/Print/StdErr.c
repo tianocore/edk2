490173cecf92b9abfb9f3c386759699642ea2300
ydong10 <ydong10@6f19259b-4bc3-4df7-8a09-765794883524>
2012-05-04 05:59:26 +0000

    Correct an error in Aprint function.
    Signed-off-by: Eric Dong <eric.dong@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@13274 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkCompatibilityPkg/Foundation/Library/Dxe/Print/StdErr.c
+++ EdkCompatibilityPkg/Foundation/Library/Dxe/Print/StdErr.c
@@ -1,6 +1,6 @@
 /*++
 
-Copyright (c) 2004, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2012, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -176,7 +176,7 @@ Returns:
     return 0;
   }
 
-  for (Index = 0; Index < EFI_DRIVER_LIB_MAX_PRINT_BUFFER; Index++) {
+  for (Index = 0; Index <= MaxIndex; Index++) {
     UnicodeFormat[Index] = (CHAR16) Format[Index];
   }
 
