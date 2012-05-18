34250fe62a0cd7d1944b96f15410b9d27e56b04f
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2012-05-18 04:36:07 +0000

    Sync patch r13307 from mail trunk. The code line where judges if offset exceed the FTW work space boundary, in some special situations where there are so many records, the offset may happen to be just equal to FtwWorkSpaceSize. And then FtwHead will be updated to be outside of work space wrongly, and cause the system to hang in this driver.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/branches/UDK2010.SR1@13330 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/FaultTolerantWriteDxe/FtwMisc.c
+++ MdeModulePkg/Universal/FaultTolerantWriteDxe/FtwMisc.c
@@ -2,7 +2,7 @@
 
   Internal generic functions to operate flash block.
 
-Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2012, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -777,7 +777,7 @@ FtwGetLastWriteHeader (
     //
     // If Offset exceed the FTW work space boudary, return error.
     //
-    if (Offset > FtwWorkSpaceSize) {
+    if (Offset >= FtwWorkSpaceSize) {
       *FtwWriteHeader = FtwHeader;
       return EFI_ABORTED;
     }
