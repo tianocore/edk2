fc80ee69cba877e2d8efed767e23ab36fc36d91c
erictian <erictian@6f19259b-4bc3-4df7-8a09-765794883524>
2012-05-29 06:59:26 +0000

    MdeModulePkg/Ata: Use a new Pcd named "PcdAtaSmartEnable" to control if enabling S.M.A.R.T feature by default.
    Signed-off-by: Feng Tian <feng.tian@intel.com>
    Reviewed-by: Sun Rui <rui.sun@intel.com>
    
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@13369 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Bus/Ata/AtaAtapiPassThru/IdeMode.c
+++ MdeModulePkg/Bus/Ata/AtaAtapiPassThru/IdeMode.c
@@ -1,7 +1,7 @@
 /** @file
   Header file for AHCI mode of ATA host controller.
 
-  Copyright (c) 2010 - 2011, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2010 - 2012, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -2667,7 +2667,7 @@ DetectAndConfigIdeDevice (
     //
     // If the device is a hard disk, then try to enable S.M.A.R.T feature
     //
-    if (DeviceType == EfiIdeHarddisk) {
+    if ((DeviceType == EfiIdeHarddisk) && PcdGetBool (PcdAtaSmartEnable)) {
       IdeAtaSmartSupport (
         Instance,
         IdeChannel,
