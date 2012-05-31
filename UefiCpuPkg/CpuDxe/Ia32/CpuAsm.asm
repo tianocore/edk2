42855482f574461f72bf6332e4ea8dec7926ee35
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2012-05-31 09:06:33 +0000

    Sync patch r13388 from main trunk. Remove CPU dead loop code from IA32 assembly codes.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/branches/UDK2010.SR1@13407 6f19259b-4bc3-4df7-8a09-765794883524

--- UefiCpuPkg/CpuDxe/Ia32/CpuAsm.asm
+++ UefiCpuPkg/CpuDxe/Ia32/CpuAsm.asm
@@ -1,7 +1,7 @@
       TITLE   CpuAsm.asm:
 ;------------------------------------------------------------------------------
 ;*
-;*   Copyright (c) 2006 - 2011, Intel Corporation. All rights reserved.<BR>
+;*   Copyright (c) 2006 - 2012, Intel Corporation. All rights reserved.<BR>
 ;*   This program and the accompanying materials
 ;*   are licensed and made available under the terms and conditions of the BSD License
 ;*   which accompanies this distribution.  The full text of the license may be found at
@@ -148,13 +148,6 @@ HasErrorCode:
     ; Put Vector Number on stack and restore ECX
     ;
     xchg    ecx, [esp]
-
-    ;
-    ; Fall through to join main routine code
-    ; at ErrorCodeAndVectorOnStack
-    ;
-@@:
-    jmp @B
 
 ErrorCodeAndVectorOnStack:
     push    ebp
