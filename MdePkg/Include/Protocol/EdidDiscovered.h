0af22e2359274f0ac5c1d8db7f6c6a90b615e8bb
niruiyu <niruiyu@6f19259b-4bc3-4df7-8a09-765794883524>
2012-07-24 07:03:08 +0000

    Correct a typo in EFI_EDID_ACTIVE_PROTOCOL and EFI_EDID_DISCOVERED_PROTOCOL comments.
    Signed-off-by: Ruiyu Ni<ruiyu.ni@intel.com>
    Reviewed-by: Elvin Li<elvin.li@intel.com>
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/branches/UDK2010.SR1@13550 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Protocol/EdidDiscovered.h
+++ MdePkg/Include/Protocol/EdidDiscovered.h
@@ -4,7 +4,7 @@
   This protocol is placed on the video output device child handle. It represents
   the EDID information being used for the output device represented by the child handle.
 
-  Copyright (c) 2006 - 2008, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2012, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials                          
   are licensed and made available under the terms and conditions of the BSD License         
   which accompanies this distribution.  The full text of the license may be found at        
@@ -39,7 +39,7 @@ typedef struct {
   /// information for an active video output device. This pointer is
   /// NULL if no EDID information is available for the video output
   /// device. The minimum size of a valid Edid buffer is 128 bytes.
-  /// EDID information is defined in the E-DID EEPROM
+  /// EDID information is defined in the E-EDID EEPROM
   /// specification published by VESA (www.vesa.org).
   ///  
   UINT8    *Edid;
