c3fae5ad754cb234d71f14c7bc259d3573d43e1b
niruiyu <niruiyu@6f19259b-4bc3-4df7-8a09-765794883524>
2012-07-24 07:14:09 +0000

    Roll back the last check in (typo correction in EdidActive.h and EdidDiscovered.h) which should be checked into trunk.
    Signed-off-by: Ruiyu Ni<ruiyu.ni@intel.com>
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/branches/UDK2010.SR1@13551 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Protocol/EdidActive.h
+++ MdePkg/Include/Protocol/EdidActive.h
@@ -3,7 +3,7 @@
 
   Placed on the video output device child handle that is actively displaying output.
 
-  Copyright (c) 2006 - 2012, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2008, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials                          
   are licensed and made available under the terms and conditions of the BSD License         
   which accompanies this distribution.  The full text of the license may be found at        
@@ -41,7 +41,7 @@ typedef struct {
   /// information for an active video output device. This pointer is
   /// NULL if no EDID information is available for the video output
   /// device. The minimum size of a valid Edid buffer is 128 bytes.
-  /// EDID information is defined in the E-EDID EEPROM
+  /// EDID information is defined in the E-DID EEPROM
   /// specification published by VESA (www.vesa.org).
   ///
   UINT8    *Edid;
