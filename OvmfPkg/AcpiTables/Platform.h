7c9ff57b11a04132a8892574a6539ae62c11be62
jljusten <jljusten@6f19259b-4bc3-4df7-8a09-765794883524>
2012-08-13 15:38:16 +0000

    OvmfPkg: sync FADT with QEMU: correct SMI_CMD
    - Qemu's PIIX emulation actually supports SMM and ACPI_ENABLE /
      ACPI_DISABLE.
    
    - After enabling SMI_CMD (SMI_CMD_IO_PORT), further values to be written
      there must be synchronized with qemu: PSTATE_CNT, CST_CNT.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@13611 6f19259b-4bc3-4df7-8a09-765794883524

--- OvmfPkg/AcpiTables/Platform.h
+++ OvmfPkg/AcpiTables/Platform.h
@@ -29,9 +29,9 @@
 
 #define INT_MODEL       0x01
 #define SCI_INT_VECTOR  0x0009
-#define SMI_CMD_IO_PORT 0 // If SMM was supported, then this would be 0xB2
-#define ACPI_ENABLE     0
-#define ACPI_DISABLE    0
+#define SMI_CMD_IO_PORT 0xB2
+#define ACPI_ENABLE     0xF1
+#define ACPI_DISABLE    0xF0
 #define S4BIOS_REQ      0x00
 #define PM1a_EVT_BLK    0x0000b000
 #define PM1b_EVT_BLK    0x00000000
