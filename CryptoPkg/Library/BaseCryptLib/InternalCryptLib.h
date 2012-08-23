efad60c5845b4fb095d42940ba986ec5202b6e80
tye1 <tye1@6f19259b-4bc3-4df7-8a09-765794883524>
2012-08-23 03:31:00 +0000

    Fix PeiCryptLib build issue.
    Signed-off-by: Ye Ting <ting.ye@intel.com>
    Reviewed-by: Yao Jiewen <jiewen.yao@intel.com>
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@13670 6f19259b-4bc3-4df7-8a09-765794883524

--- CryptoPkg/Library/BaseCryptLib/InternalCryptLib.h
+++ CryptoPkg/Library/BaseCryptLib/InternalCryptLib.h
@@ -30,25 +30,5 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #define OPENSSL_SYSNAME_UWIN
 #endif
 
-/**
-  Pop single certificate from STACK_OF(X509).
-
-  If X509Stack, Cert, or CertSize is NULL, then return FALSE.
-
-  @param[in]  X509Stack       Pointer to a X509 stack object.
-  @param[out] Cert            Pointer to a X509 certificate.
-  @param[out] CertSize        Length of output X509 certificate in bytes.
-                                 
-  @retval     TRUE            The X509 stack pop succeeded.
-  @retval     FALSE           The pop operation failed.
-
-**/
-BOOLEAN
-X509PopCertificate (
-  IN  VOID  *X509Stack,
-  OUT UINT8 **Cert,
-  OUT UINTN *CertSize
-  );
-
 #endif
 
