dbcecd5da2a76f069f769feb8d875149078c0ab1
erictian <erictian@6f19259b-4bc3-4df7-8a09-765794883524>
2012-08-29 09:23:34 +0000

    Should pass in bytes of string buffer to UnicodeSPrint() rather than Unicode string length.
    Signed-off-by: Tian Feng <feng.tian@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>
    
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@13691 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Library/UefiDebugLibConOut/DebugLib.c
+++ MdePkg/Library/UefiDebugLibConOut/DebugLib.c
@@ -1,7 +1,7 @@
 /** @file
   UEFI Debug Library that sends messages to the Console Output Device in the EFI System Table.
 
-  Copyright (c) 2006 - 2011, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2012, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials                          
   are licensed and made available under the terms and conditions of the BSD License         
   which accompanies this distribution.  The full text of the license may be found at        
@@ -120,7 +120,7 @@ DebugAssert (
   //
   UnicodeSPrintAsciiFormat (
     Buffer, 
-    MAX_DEBUG_MESSAGE_LENGTH, 
+    sizeof (Buffer), 
     "ASSERT %a(%d): %a\n", 
     FileName, 
     LineNumber, 
