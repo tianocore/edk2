f7a5d58c57726da6adb47caacc3e213c0a92e465
tye1 <tye1@6f19259b-4bc3-4df7-8a09-765794883524>
2012-10-29 05:20:18 +0000

    Fix issue that ifconfig –c cannot clear IP address details.
    Signed-off-by: Ye Ting <ting.ye@intel.com>
    Reviewed-by: Fan Jeff <jeff.fan@intel.com>
    Reviewed-by: Fu Siyuan <siyuan.fu@intel.com>
    
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@13885 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/Network/Ip4ConfigDxe/Ip4ConfigNv.c
+++ MdeModulePkg/Universal/Network/Ip4ConfigDxe/Ip4ConfigNv.c
@@ -1,7 +1,7 @@
 /** @file
   Helper functions for configuring or getting the parameters relating to Ip4.
 
-Copyright (c) 2009 - 2011, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2009 - 2012, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -215,7 +215,7 @@ Ip4ConfigConvertIfrNvDataToDeviceConfigData (
     //
     ZeroMem (&Ip4ConfigInstance->Ip4ConfigCallbackInfo, sizeof (IP4_SETTING_INFO));
 
-    Status = EfiNicIp4ConfigSetInfo (Ip4ConfigInstance, NULL, TRUE);
+    Status = EfiNicIp4ConfigSetInfo (Ip4ConfigInstance, NULL, FALSE);
     if (Status == EFI_NOT_FOUND) {
       return EFI_SUCCESS;
     }
@@ -629,7 +629,7 @@ Ip4DeviceRouteConfig (
         FreePool (NicInfo);
       } else {
         ZeroMem (&Ip4ConfigInstance->Ip4ConfigCallbackInfo, sizeof (IP4_SETTING_INFO));
-        Status = EfiNicIp4ConfigSetInfo (Ip4ConfigInstance, NULL, TRUE);
+        Status = EfiNicIp4ConfigSetInfo (Ip4ConfigInstance, NULL, FALSE);
       }
     }
 
