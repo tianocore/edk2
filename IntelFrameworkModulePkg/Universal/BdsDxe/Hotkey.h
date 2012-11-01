49e46fe812bc4e66d3e1a18b73837e09b5be2d67
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2012-11-01 03:02:00 +0000

    Sync patches r13861, r13869 and r13870 from main trunk. Remove the KeyOrder variable dependency from BDS.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/branches/UDK2010.SR1@13906 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkModulePkg/Universal/BdsDxe/Hotkey.h
+++ IntelFrameworkModulePkg/Universal/BdsDxe/Hotkey.h
@@ -116,43 +116,6 @@ typedef struct {
 
 #define BDS_HOTKEY_OPTION_FROM_LINK(a) CR (a, BDS_HOTKEY_OPTION, Link, BDS_HOTKEY_OPTION_SIGNATURE)
 
-#define VAR_KEY_ORDER       L"KeyOrder"
-
-/**
-
-  Create Key#### for the given hotkey.
-
-
-  @param KeyOption       - The Hot Key Option to be added.
-  @param KeyOptionNumber - The key option number for Key#### (optional).
-
-  @retval  EFI_SUCCESS            Register hotkey successfully.
-  @retval  EFI_INVALID_PARAMETER  The hotkey option is invalid.
-
-**/
-EFI_STATUS
-RegisterHotkey (
-  IN EFI_KEY_OPTION     *KeyOption,
-  OUT UINT16            *KeyOptionNumber
-  );
-
-/**
-
-  Delete Key#### for the given Key Option number.
-
-
-  @param KeyOptionNumber - Key option number for Key####
-
-  @retval  EFI_SUCCESS            Unregister hotkey successfully.
-  @retval  EFI_NOT_FOUND          No Key#### is found for the given Key Option number.
-
-**/
-EFI_STATUS
-UnregisterHotkey (
-  IN UINT16             KeyOptionNumber
-  );
-
-
 /**
 
   Process all the "Key####" variables, associate Hotkeys with corresponding Boot Options.
