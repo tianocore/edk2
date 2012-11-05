7f3b78220a046fb9e412e1bec3dd240bfaccb65d
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2012-11-05 03:07:53 +0000

    Sync patches r13908 and r13926 from main trunk. Add Physical Presence request UI detection logic. Refine UIConfirm ReadKey logic.
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/branches/UDK2010.SR1@13927 6f19259b-4bc3-4df7-8a09-765794883524

--- SecurityPkg/Include/Library/TcgPhysicalPresenceLib.h
+++ SecurityPkg/Include/Library/TcgPhysicalPresenceLib.h
@@ -2,7 +2,7 @@
   Ihis library is intended to be used by BDS modules.
   This library will lock TPM after executing TPM request.
 
-Copyright (c) 2011, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2011 - 2012, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials 
 are licensed and made available under the terms and conditions of the BSD License 
 which accompanies this distribution.  The full text of the license may be found at 
@@ -32,6 +32,22 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 VOID
 EFIAPI
 TcgPhysicalPresenceLibProcessRequest (
+  VOID
+  );
+
+/**
+  Check if the pending TPM request needs user input to confirm.
+
+  The TPM request may come from OS. This API will check if TPM request exists and need user
+  input to confirmation.
+  
+  @retval    TRUE        TPM needs input to confirm user physical presence.
+  @retval    FALSE       TPM doesn't need input to confirm user physical presence.
+
+**/
+BOOLEAN
+EFIAPI
+TcgPhysicalPresenceLibNeedUserConfirm(
   VOID
   );
 
