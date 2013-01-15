d2b9b337374b3641226f833919323f016cb83314
ydong10 <ydong10@6f19259b-4bc3-4df7-8a09-765794883524>
2013-01-15 06:38:45 +0000

    Follow UEFI spec, clean the CRC32 field before calculate CRC for system table.
    Signed-off-by: Eric Dong <eric.dong@intel.com>
    Reviewed-by: Ni Ruiyu <ruiyu.ni@intel.com>
    
    
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@14049 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkModulePkg/Universal/BdsDxe/BdsEntry.c
+++ IntelFrameworkModulePkg/Universal/BdsDxe/BdsEntry.c
@@ -5,7 +5,7 @@
   After DxeCore finish DXE phase, gEfiBdsArchProtocolGuid->BdsEntry will be invoked
   to enter BDS phase.
 
-Copyright (c) 2004 - 2012, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2013, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -486,6 +486,7 @@ BdsEntry (
   //
   // Fixup Tasble CRC after we updated Firmware Vendor and Revision
   //
+  gST->Hdr.CRC32 = 0;
   gBS->CalculateCrc32 ((VOID *)gST, sizeof(EFI_SYSTEM_TABLE), &gST->Hdr.CRC32);
 
   //
