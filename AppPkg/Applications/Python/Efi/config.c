ffc6107d40f73196ea3db160e51f57805adbcebc
darylm503 <darylm503@6f19259b-4bc3-4df7-8a09-765794883524>
2013-01-18 20:36:27 +0000

    AppPkg/Applications/Python: Add support for the pyexpat module.
    Add required flags to [BuildOptions] in PythonCore.inf.
    Add initialization routine to Efi/config.c.
    Alter #include targets if building for UEFI in expat_external.h and xmlparse.c.
    Add expat modifications to PyMod-2.7.2/Modules/expat.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Submitted-by:   Duane Voth <duanev@gmail.com>
    Signed-off-by:  daryl.mcdaniel@intel.com
    Reviewed-by:    duanev@gmail.com
    Reviewed-by:    daryl.mcdaniel@intel.com
    
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@14071 6f19259b-4bc3-4df7-8a09-765794883524

--- AppPkg/Applications/Python/Efi/config.c
+++ AppPkg/Applications/Python/Efi/config.c
@@ -17,45 +17,58 @@
 #include "Python.h"
 
 extern void initarray(void);
+extern void init_ast(void);
 extern void initbinascii(void);
+extern void init_bisect(void);
 extern void initcmath(void);
+extern void init_codecs(void);
+extern void init_collections(void);
+extern void initcPickle(void);
+extern void initcStringIO(void);
+extern void init_csv(void);
+extern void init_ctypes(void);
+extern void initdatetime(void);
+extern void initedk2(void);
 extern void initerrno(void);
+extern void init_functools(void);
 extern void initfuture_builtins(void);
 extern void initgc(void);
+extern void init_heapq(void);
+extern void init_hotshot(void);
+extern void initimp(void);
+extern void init_io(void);
+extern void inititertools(void);
+extern void init_json(void);
+extern void init_lsprof(void);
 extern void initmath(void);
 extern void init_md5(void);
-extern void initedk2(void);
+extern void initmmap(void);
 extern void initoperator(void);
-extern void initsignal(void);
+extern void initparser(void);
+extern void initpyexpat(void);
+extern void init_random(void);
+extern void initselect(void);
 extern void init_sha(void);
 extern void init_sha256(void);
 extern void init_sha512(void);
+extern void initsignal(void);
+extern void init_socket(void);
+extern void init_sre(void);
 extern void initstrop(void);
-extern void inittime(void);
+extern void init_struct(void);
+extern void init_subprocess(void);
+extern void init_symtable(void);
 extern void initthread(void);
-extern void initcStringIO(void);
-extern void initcPickle(void);
-extern void init_codecs(void);
+extern void inittime(void);
+extern void initunicodedata(void);
 extern void init_weakref(void);
-extern void init_hotshot(void);
+extern void init_winreg(void);
 extern void initxxsubtype(void);
 extern void initzipimport(void);
-extern void init_random(void);
-extern void inititertools(void);
-extern void init_collections(void);
-extern void init_heapq(void);
-extern void init_bisect(void);
-extern void init_symtable(void);
-extern void initmmap(void);
-extern void init_csv(void);
-extern void init_sre(void);
-extern void initparser(void);
-extern void init_winreg(void);
-extern void init_struct(void);
-extern void initdatetime(void);
-extern void init_functools(void);
-extern void init_json(void);
 extern void initzlib(void);
+
+extern void PyMarshal_Init(void);
+extern void _PyWarnings_Init(void);
 
 extern void init_multibytecodec(void);
 extern void init_codecs_cn(void);
@@ -64,26 +77,13 @@ extern void init_codecs_iso2022(void);
 extern void init_codecs_jp(void);
 extern void init_codecs_kr(void);
 extern void init_codecs_tw(void);
-extern void init_subprocess(void);
-extern void init_lsprof(void);
-extern void init_ast(void);
-extern void init_io(void);
-extern void _PyWarnings_Init(void);
-
-extern void init_socket(void);
-extern void initselect(void);
-
-extern void PyMarshal_Init(void);
-extern void initimp(void);
-extern void initunicodedata(void);
-extern void init_ctypes(void);
 
 struct _inittab _PyImport_Inittab[] = {
 
     //{"_ast", init_ast},
-    //{"_bisect", init_bisect},                   /* A fast version of bisect.py */
+    //{"_bisect", init_bisect},               /* A fast version of bisect.py */
     //{"_csv", init_csv},
-    //{"_heapq", init_heapq},                     /* A fast version of heapq.py */
+    //{"_heapq", init_heapq},                 /* A fast version of heapq.py */
     //{"_io", init_io},
     //{"_json", init_json},
     //{"_md5", init_md5},
@@ -99,9 +99,10 @@ struct _inittab _PyImport_Inittab[] = {
     //{"datetime", initdatetime},
     //{"future_builtins", initfuture_builtins},
     //{"parser", initparser},
+    //{"pyexpat", initpyexpat},
     //{"select", initselect},
     //{"signal", initsignal},
-    //{"strop", initstrop},                       /* redefines some string operations that are 100-1000 times faster */
+    //{"strop", initstrop},                   /* redefines some string operations that are 100-1000 times faster */
     //{"unicodedata", initunicodedata},
     //{"xxsubtype", initxxsubtype},
     //{"zipimport", initzipimport},
