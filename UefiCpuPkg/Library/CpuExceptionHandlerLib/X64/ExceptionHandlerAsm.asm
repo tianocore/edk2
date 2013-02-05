782e407aade3b8eab2a81ac3e2fd161c608c942c
vanjeff <vanjeff@6f19259b-4bc3-4df7-8a09-765794883524>
2013-02-05 01:35:29 +0000

    Fix build issue on DDK3790 tool chain.
    Signed-off-by: Jeff Fan <jeff.fan@intel.com>
    Reviewed-by: Feng Tian <feng.tian@intel.com>
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@14122 6f19259b-4bc3-4df7-8a09-765794883524

--- UefiCpuPkg/Library/CpuExceptionHandlerLib/X64/ExceptionHandlerAsm.asm
+++ UefiCpuPkg/Library/CpuExceptionHandlerLib/X64/ExceptionHandlerAsm.asm
@@ -1,5 +1,5 @@
 ;------------------------------------------------------------------------------ ;
-; Copyright (c) 2012, Intel Corporation. All rights reserved.<BR>
+; Copyright (c) 2012 - 2013, Intel Corporation. All rights reserved.<BR>
 ; This program and the accompanying materials
 ; are licensed and made available under the terms and conditions of the BSD License
 ; which accompanies this distribution.  The full text of the license may be found at
@@ -23,7 +23,7 @@
 ;
 ; CommonExceptionHandler()
 ;
-CommonExceptionHandler             PROTO   C
+externdef CommonExceptionHandler:near
 
 EXTRN mErrorCodeFlag:DWORD ; Error code flags for exceptions
 
