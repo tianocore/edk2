c9bf2a9e2d32bd633be4a020eb0f6872c89801bc
oliviermartin <oliviermartin@6f19259b-4bc3-4df7-8a09-765794883524>
2013-03-12 00:38:15 +0000

    EmbeddedPkg/Metronome.c: Fix delay computed by WaitForTick() function.
    Before the tick period used by the Metronome was hardcoded to 100.
    This has been changed since but WaitForTick() function was still relying
    on this hardcoded value to compute the number of milliseconds to stall
    the CPU.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Olivier Martin <olivier.martin@arm.com>
    
    
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@14172 6f19259b-4bc3-4df7-8a09-765794883524

--- EmbeddedPkg/MetronomeDxe/Metronome.c
+++ EmbeddedPkg/MetronomeDxe/Metronome.c
@@ -1,8 +1,8 @@
 /** @file
 
   Copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
-  Copyright (c) 2012, ARM Ltd. All rights reserved.
-  
+  Copyright (c) 2013, ARM Ltd. All rights reserved.
+
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -25,20 +25,54 @@
 
 #include <Protocol/Metronome.h>
 
+EFI_STATUS
+EFIAPI
+WaitForTick (
+  IN EFI_METRONOME_ARCH_PROTOCOL  *This,
+  IN UINT32                       TickNumber
+  );
+
+/**
+  Interface structure for the Metronome Architectural Protocol.
+
+  @par Protocol Description:
+  This protocol provides access to a known time source in the platform to the
+  core.  The core uses this known time source to produce core services that
+  require calibrated delays.
+
+  @param WaitForTick
+  Waits for a specified number of ticks from a known time source
+  in the platform.  The actual time passed between entry of this
+  function and the first tick is between 0 and TickPeriod 100 nS
+  units.  If you want to guarantee that at least TickPeriod time
+  has elapsed, wait for two ticks.
+
+  @param TickPeriod
+  The period of platform's known time source in 100 nS units.
+  This value on any platform must be at least 10 uS, and must not
+  exceed 200 uS.  The value in this field is a constant that must
+  not be modified after the Metronome architectural protocol is
+  installed.  All consumers must treat this as a read-only field.
+
+**/
+EFI_METRONOME_ARCH_PROTOCOL gMetronome = {
+  WaitForTick,
+  FixedPcdGet32 (PcdMetronomeTickPeriod)
+};
 
 
 /**
-  The WaitForTick() function waits for the number of ticks specified by 
-  TickNumber from a known time source in the platform.  If TickNumber of 
-  ticks are detected, then EFI_SUCCESS is returned.  The actual time passed 
-  between entry of this function and the first tick is between 0 and 
-  TickPeriod 100 nS units.  If you want to guarantee that at least TickPeriod 
-  time has elapsed, wait for two ticks.  This function waits for a hardware 
-  event to determine when a tick occurs.  It is possible for interrupt 
-  processing, or exception processing to interrupt the execution of the 
-  WaitForTick() function.  Depending on the hardware source for the ticks, it 
-  is possible for a tick to be missed.  This function cannot guarantee that 
-  ticks will not be missed.  If a timeout occurs waiting for the specified 
+  The WaitForTick() function waits for the number of ticks specified by
+  TickNumber from a known time source in the platform.  If TickNumber of
+  ticks are detected, then EFI_SUCCESS is returned.  The actual time passed
+  between entry of this function and the first tick is between 0 and
+  TickPeriod 100 nS units.  If you want to guarantee that at least TickPeriod
+  time has elapsed, wait for two ticks.  This function waits for a hardware
+  event to determine when a tick occurs.  It is possible for interrupt
+  processing, or exception processing to interrupt the execution of the
+  WaitForTick() function.  Depending on the hardware source for the ticks, it
+  is possible for a tick to be missed.  This function cannot guarantee that
+  ticks will not be missed.  If a timeout occurs waiting for the specified
   number of ticks, then EFI_TIMEOUT is returned.
 
   @param  This             The EFI_METRONOME_ARCH_PROTOCOL instance.
@@ -57,41 +91,13 @@ WaitForTick (
   )
 {
   //
-  // Fill me in
+  // Compute how long to stall the CPU.
+  // gMetronome.TickPeriod is in 100 ns units so it needs to be divided by 10
+  // to get it in microseconds units.
   //
-  MicroSecondDelay (10 * TickNumber);
-  return EFI_UNSUPPORTED;
+  MicroSecondDelay (TickNumber * gMetronome.TickPeriod / 10);
+  return EFI_SUCCESS;
 }
-
-
-
-/**
-  Interface structure for the Metronome Architectural Protocol.
-
-  @par Protocol Description:
-  This protocol provides access to a known time source in the platform to the
-  core.  The core uses this known time source to produce core services that 
-  require calibrated delays.  
-
-  @param WaitForTick
-  Waits for a specified number of ticks from a known time source 
-  in the platform.  The actual time passed between entry of this 
-  function and the first tick is between 0 and TickPeriod 100 nS 
-  units.  If you want to guarantee that at least TickPeriod time 
-  has elapsed, wait for two ticks.
-
-  @param TickPeriod
-  The period of platform's known time source in 100 nS units.  
-  This value on any platform must be at least 10 uS, and must not 
-  exceed 200 uS.  The value in this field is a constant that must 
-  not be modified after the Metronome architectural protocol is 
-  installed.  All consumers must treat this as a read-only field.
-
-**/
-EFI_METRONOME_ARCH_PROTOCOL gMetronome = {
-  WaitForTick,
-  FixedPcdGet32 (PcdMetronomeTickPeriod)
-};
 
 
 EFI_HANDLE  gMetronomeHandle = NULL;
@@ -116,7 +122,7 @@ MetronomeInitialize (
   )
 {
   EFI_STATUS  Status;
-  
+
   //
   // Do any hardware init required to make WaitForTick () to work here.
   //
