0c9dc2132096b95c14c6c69c3d5622de07a9b827
lzeng14 <lzeng14@6f19259b-4bc3-4df7-8a09-765794883524>
2013-04-22 09:51:19 +0000

    Enhance InitializeLanguage() to set PcdUefiVariableDefaultPlatformLang to PlatformLang variable if the value of PlatformLang variable has been set an unsupported value(not one of PlatformLangCodes variable), and assert when default (Platform)Lang PCD value is not set to one of (Platform)LangCodes PCD value.
    Signed-off-by: Star Zeng <star.zeng@intel.com>
    Reviewed-by: Ruiyu Ni <ruiyu.ni@intel.com>
    
    git-svn-id: https://edk2.svn.sourceforge.net/svnroot/edk2/trunk/edk2@14307 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkModulePkg/Universal/BdsDxe/Language.c
+++ IntelFrameworkModulePkg/Universal/BdsDxe/Language.c
@@ -1,7 +1,7 @@
 /** @file
   Language settings
 
-Copyright (c) 2004 - 2010, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2013, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -312,10 +312,139 @@ GetNextLanguage (
 }
 
 /**
+  Check if lang is in supported language codes according to language string.
+
+  This code is used to check if lang is in in supported language codes. It can handle
+  RFC4646 and ISO639 language tags.
+  In ISO639 language tags, take 3-characters as a delimitation to find matched string.
+  In RFC4646 language tags, take semicolon as a delimitation to find matched string.
+
+  For example:
+    SupportedLang  = "engfraengfra"
+    Iso639Language = TRUE
+    Lang           = "eng", the return value is "TRUE", or
+    Lang           = "chs", the return value is "FALSE".
+  Another example:
+    SupportedLang  = "en;fr;en-US;fr-FR"
+    Iso639Language = FALSE
+    Lang           = "en", the return value is "TRUE", or
+    Lang           = "zh", the return value is "FALSE".
+
+  @param  SupportedLang               Platform supported language codes.
+  @param  Lang                        Configured language.
+  @param  Iso639Language              A bool value to signify if the handler is operated on ISO639 or RFC4646.
+
+  @retval TRUE  lang is in supported language codes.
+  @retval FALSE lang is not in supported language codes.
+
+**/
+BOOLEAN
+IsLangInSupportedLangCodes(
+  IN  CHAR8            *SupportedLang,
+  IN  CHAR8            *Lang,
+  IN  BOOLEAN          Iso639Language
+  ) 
+{
+  UINTN    Index;
+  UINTN    CompareLength;
+  UINTN    LanguageLength;
+
+  if (Iso639Language) {
+    CompareLength = ISO_639_2_ENTRY_SIZE;
+    for (Index = 0; Index < AsciiStrLen (SupportedLang); Index += CompareLength) {
+      if (AsciiStrnCmp (Lang, SupportedLang + Index, CompareLength) == 0) {
+        //
+        // Successfully find the Lang string in SupportedLang string.
+        //
+        return TRUE;
+      }
+    }
+    return FALSE;
+  } else {
+    //
+    // Compare RFC4646 language code
+    //
+    for (LanguageLength = 0; Lang[LanguageLength] != '\0'; LanguageLength++);
+
+    for (; *SupportedLang != '\0'; SupportedLang += CompareLength) {
+      //
+      // Skip ';' characters in SupportedLang
+      //
+      for (; *SupportedLang != '\0' && *SupportedLang == ';'; SupportedLang++);
+      //
+      // Determine the length of the next language code in SupportedLang
+      //
+      for (CompareLength = 0; SupportedLang[CompareLength] != '\0' && SupportedLang[CompareLength] != ';'; CompareLength++);
+      
+      if ((CompareLength == LanguageLength) && 
+          (AsciiStrnCmp (Lang, SupportedLang, CompareLength) == 0)) {
+        //
+        // Successfully find the Lang string in SupportedLang string.
+        //
+        return TRUE;
+      }
+    }
+    return FALSE;
+  }
+}
+
+/**
+  Initialize Lang or PlatformLang variable, if Lang or PlatformLang variable is not found,
+  or it has been set to an unsupported value(not one of platform supported language codes),
+  set the default language code to it.
+
+  @param  LangName                    Language name, L"Lang" or L"PlatformLang".
+  @param  SupportedLang               Platform supported language codes.
+  @param  DefaultLang                 Default language code.
+  @param  Iso639Language              A bool value to signify if the handler is operated on ISO639 or RFC4646,
+                                      TRUE for L"Lang" LangName or FALSE for L"PlatformLang" LangName.
+
+**/
+
+VOID
+InitializeLangVariable (
+  IN CHAR16     *LangName,
+  IN CHAR8      *SupportedLang,
+  IN CHAR8      *DefaultLang,     
+  IN BOOLEAN    Iso639Language
+  )
+{
+  EFI_STATUS  Status;
+  CHAR8       *Lang;
+
+  //
+  // Find current Lang or PlatformLang from EFI Variable.
+  //
+  Status = GetEfiGlobalVariable2 (LangName, &Lang, NULL);
+  //
+  // If Lang or PlatformLang variable is not found,
+  // or it has been set to an unsupported value(not one of the supported language codes),
+  // set the default language code to it.
+  //
+  if (EFI_ERROR (Status) || !IsLangInSupportedLangCodes (SupportedLang, Lang, Iso639Language)) {
+    //
+    // The default language code should be one of the supported language codes.
+    //
+    ASSERT (IsLangInSupportedLangCodes (SupportedLang, DefaultLang, Iso639Language));
+    Status = gRT->SetVariable (
+                    LangName,
+                    &gEfiGlobalVariableGuid,
+                    EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS,
+                    AsciiStrSize (DefaultLang),
+                    DefaultLang
+                    );
+  }
+
+  if (Lang != NULL) {
+    FreePool (Lang);
+  }
+}
+
+/**
   Determine the current language that will be used
   based on language related EFI Variables.
 
-  @param LangCodesSettingRequired - If required to set LangCode variable
+  @param LangCodesSettingRequired - If required to set LangCodes variable
 
 **/
 VOID
@@ -324,18 +453,13 @@ InitializeLanguage (
   )
 {
   EFI_STATUS  Status;
-  UINTN       Size;
-  CHAR8       *Lang;
-  CHAR8       LangCode[ISO_639_2_ENTRY_SIZE + 1];
   CHAR8       *LangCodes;
-  CHAR8       *PlatformLang;
   CHAR8       *PlatformLangCodes;
-  UINTN       Index;
-  BOOLEAN     Invalid;
 
   ExportFonts ();
 
   LangCodes = (CHAR8 *)PcdGetPtr (PcdUefiVariableDefaultLangCodes);
+  PlatformLangCodes = (CHAR8 *)PcdGetPtr (PcdUefiVariableDefaultPlatformLangCodes);
   if (LangCodesSettingRequired) {
     if (!FeaturePcdGet (PcdUefiVariableDefaultLangDeprecate)) {
       //
@@ -350,8 +474,6 @@ InitializeLanguage (
                       );
     }
 
-
-    PlatformLangCodes = (CHAR8 *)PcdGetPtr (PcdUefiVariableDefaultPlatformLangCodes);
     Status = gRT->SetVariable (
                     L"PlatformLangCodes",
                     &gEfiGlobalVariableGuid,
@@ -365,67 +487,7 @@ InitializeLanguage (
     //
     // UEFI 2.1 depricated this variable so we support turning it off
     //
-
-    //
-    // Find current LangCode from Lang NV Variable
-    //
-    Size = ISO_639_2_ENTRY_SIZE + 1;
-    Status = gRT->GetVariable (
-                    L"Lang",
-                    &gEfiGlobalVariableGuid,
-                    NULL,
-                    &Size,
-                    &LangCode
-                    );
-    if (!EFI_ERROR (Status)) {
-      Status = EFI_NOT_FOUND;
-      for (Index = 0; LangCodes[Index] != 0; Index += ISO_639_2_ENTRY_SIZE) {
-        if (CompareMem (&LangCodes[Index], LangCode, ISO_639_2_ENTRY_SIZE) == 0) {
-          Status = EFI_SUCCESS;
-          break;
-        }
-      }
-    }
-
-    //
-    // If we cannot get language code from Lang variable,
-    // or LangCode cannot be found from language table,
-    // set the mDefaultLangCode to Lang variable.
-    //
-    if (EFI_ERROR (Status)) {
-      Lang = (CHAR8 *)PcdGetPtr (PcdUefiVariableDefaultLang);
-      Status = gRT->SetVariable (
-                      L"Lang",
-                      &gEfiGlobalVariableGuid,
-                      EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS,
-                      ISO_639_2_ENTRY_SIZE + 1,
-                      Lang
-                      );
-    }
+    InitializeLangVariable (L"Lang", LangCodes, (CHAR8 *) PcdGetPtr (PcdUefiVariableDefaultLang), TRUE);
   }
-
-  Invalid = FALSE;
-  PlatformLang = BdsLibGetVariableAndSize (L"PlatformLang", &gEfiGlobalVariableGuid, &Size);
-  if (PlatformLang != NULL) {
-    //
-    // Check Current PlatformLang value against PlatformLangCode. Need a library that is TBD
-    // Set Invalid based on state of PlatformLang.
-    //
-
-    FreePool (PlatformLang);
-  } else {
-    // No valid variable is set
-    Invalid = TRUE;
-  }
-
-  if (Invalid) {
-    PlatformLang = (CHAR8 *)PcdGetPtr (PcdUefiVariableDefaultPlatformLang);
-    Status = gRT->SetVariable (
-                    L"PlatformLang",
-                    &gEfiGlobalVariableGuid,
-                    EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS,
-                    AsciiStrSize (PlatformLang),
-                    PlatformLang
-                    );
-  }
+  InitializeLangVariable (L"PlatformLang", PlatformLangCodes, (CHAR8 *) PcdGetPtr (PcdUefiVariableDefaultPlatformLang), FALSE);
 }
