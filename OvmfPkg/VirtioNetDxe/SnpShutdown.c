80682e9b2fe076aaa6eaf871a69da663ae57dcba
Laszlo Ersek <lersek@redhat.com>
2013-06-14 07:40:11 +0000

    OvmfPkg: VirtioNetDxe: SNP.Shutdown
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@14411 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ OvmfPkg/VirtioNetDxe/SnpShutdown.c
@@ -0,0 +1,78 @@
+/** @file
+
+  Implementation of the SNP.Shutdown() function and its private helpers if any.
+
+  Copyright (C) 2013, Red Hat, Inc.
+  Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
+
+  This program and the accompanying materials are licensed and made available
+  under the terms and conditions of the BSD License which accompanies this
+  distribution. The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
+  WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#include <Library/UefiBootServicesTableLib.h>
+
+#include "VirtioNet.h"
+
+/**
+  Resets a network adapter and leaves it in a state that is safe for  another
+  driver to initialize.
+
+  @param  This Protocol instance pointer.
+
+  @retval EFI_SUCCESS           The network interface was shutdown.
+  @retval EFI_NOT_STARTED       The network interface has not been started.
+  @retval EFI_INVALID_PARAMETER One or more of the parameters has an
+                                unsupported value.
+  @retval EFI_DEVICE_ERROR      The command could not be sent to the network
+                                interface.
+  @retval EFI_UNSUPPORTED       This function is not supported by the network
+                                interface.
+
+**/
+
+EFI_STATUS
+EFIAPI
+VirtioNetShutdown (
+  IN EFI_SIMPLE_NETWORK_PROTOCOL *This
+  )
+{
+  VNET_DEV   *Dev;
+  EFI_TPL    OldTpl;
+  EFI_STATUS Status;
+
+  if (This == NULL) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  Dev = VIRTIO_NET_FROM_SNP (This);
+  OldTpl = gBS->RaiseTPL (TPL_CALLBACK);
+  switch (Dev->Snm.State) {
+  case EfiSimpleNetworkStopped:
+    Status = EFI_NOT_STARTED;
+    goto Exit;
+  case EfiSimpleNetworkStarted:
+    Status = EFI_DEVICE_ERROR;
+    goto Exit;
+  default:
+    break;
+  }
+
+  VIRTIO_CFG_WRITE (Dev, Generic.VhdrDeviceStatus, 0);
+  VirtioNetShutdownRx (Dev);
+  VirtioNetShutdownTx (Dev);
+  VirtioRingUninit (&Dev->TxRing);
+  VirtioRingUninit (&Dev->RxRing);
+
+  Dev->Snm.State = EfiSimpleNetworkStarted;
+  Status = EFI_SUCCESS;
+
+Exit:
+  gBS->RestoreTPL (OldTpl);
+  return Status;
+}
