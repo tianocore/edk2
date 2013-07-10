6c657d742e7a05786a74ee75e1237d9192913a44
Fu Siyuan <siyuan.fu@intel.com>
2013-07-10 05:52:58 +0000

    Roll back the changes in revision 14294 since it will cause iSCSI security authentication issue. Signed-off-by: Fu Siyuan <siyuan.fu@intel.com> Reviewed-by: Ouyang Qian <qian.ouyang@intel.com> Reviewed-by: Ye Ting <ting.ye@intel.com>
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@14461 6f19259b-4bc3-4df7-8a09-765794883524

--- NetworkPkg/IScsiDxe/IScsiProto.c
+++ NetworkPkg/IScsiDxe/IScsiProto.c
@@ -1,7 +1,7 @@
 /** @file
   The implementation of iSCSI protocol based on RFC3720.
 
-Copyright (c) 2004 - 2012, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2013, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -975,7 +975,7 @@ IScsiProcessLoginRsp (
   NetbufQueAppend (&Conn->RspQue, Pdu);
 
   Conn->PartialRspRcvd = Continue;
-  if ((!Transit) || Continue) {
+  if (Continue) {
     //
     // It is a partial response; must wait for another or more Request/Response
     // conversations to get the full response.
