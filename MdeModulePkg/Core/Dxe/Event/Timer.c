d1edec0a33dd6c10249c094c6ccb60c6c98d7b27
Elvin Li <elvin.li@intel.com>
2013-10-29 06:47:47 +0000

    Fix the bug that SetTimer () cannot work when Type is TimerPeriodic and TriggerTime is 0.
    Signed-off-by: Elvin Li <elvin.li@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>
    
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@14811 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Core/Dxe/Event/Timer.c
+++ MdeModulePkg/Core/Dxe/Event/Timer.c
@@ -1,7 +1,7 @@
 /** @file
   Core Timer Services
 
-Copyright (c) 2006 - 2012, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2013, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -281,6 +281,9 @@ CoreSetTimer (
   if (Type != TimerCancel) {
 
     if (Type == TimerPeriodic) {
+      if (TriggerTime == 0) {
+        gTimer->GetTimerPeriod (gTimer, &TriggerTime);
+      }
       Event->Timer.Period = TriggerTime;
     }
 
