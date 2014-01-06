979fc9eab575aa33bdcccec709e1791cd1cf5148
Fu Siyuan <siyuan.fu@intel.com>
2014-01-06 03:01:09 +0000

    Bug fix: IpSec driver treats BOOLEAN as EFI_STATUS in error handling code. Signed-off-by: Fu Siyuan <siyuan.fu@intel.com> Reviewed-by: Dong, Guo <guo.dong@intel.com> Reviewed-by: Jin, Eric <eric.jin@intel.com>
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@15049 6f19259b-4bc3-4df7-8a09-765794883524

--- NetworkPkg/IpSecDxe/Ikev2/Exchange.c
+++ NetworkPkg/IpSecDxe/Ikev2/Exchange.c
@@ -1,7 +1,7 @@
 /** @file
   The general interfaces of the IKEv2.
 
-  Copyright (c) 2010 - 2011, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2010 - 2014, Intel Corporation. All rights reserved.<BR>
 
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
@@ -467,8 +467,7 @@ Ikev2HandleSa (
   //
   // Validate the IKE packet header.
   //
-  Status = Ikev2ValidateHeader (IkeSaSession, IkePacket->Header);
-  if (EFI_ERROR (Status)) {
+  if (!Ikev2ValidateHeader (IkeSaSession, IkePacket->Header)) {
     //
     // Drop the packet if invalid IKE header.
     //
