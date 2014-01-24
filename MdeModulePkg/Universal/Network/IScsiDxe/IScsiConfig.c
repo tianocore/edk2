ace287611f35b7525bc8e7468e9093ea32d2464e
Gao, Liming <liming.gao@intel.com>
2014-01-24 09:33:11 +0000

    Update Module BaseName to avoid the same name.
    Signed-off-by: Gao, Liming <liming.gao@intel.com>
    Reviewed-by: Dong, Guo <guo.dong@intel.com>
    Reviewed-by: Fu, Siyuan <siyuan.fu@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@15176 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/Network/IScsiDxe/IScsiConfig.c
+++ MdeModulePkg/Universal/Network/IScsiDxe/IScsiConfig.c
@@ -1,7 +1,7 @@
 /** @file
   Helper functions for configuring or getting the parameters relating to iSCSI.
 
-Copyright (c) 2004 - 2012, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2014, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -1171,7 +1171,7 @@ IScsiConfigFormInit (
   CallbackInfo->RegisteredHandle = HiiAddPackages (
                                      &gIp4IScsiConfigGuid,
                                      CallbackInfo->DriverHandle,
-                                     IScsiDxeStrings,
+                                     IScsi4DxeStrings,
                                      IScsiConfigDxeBin,
                                      NULL
                                      );
