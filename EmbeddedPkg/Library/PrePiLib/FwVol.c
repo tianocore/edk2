c1cdcab9526506673b882017845a043cead8bc69
Andrew Fish <afish@apple.com>
2014-02-24 10:30:48 +0000

    EmbeddedPkg/PrePiLib: Fixed incorrect type casting
    A pointer on a UINTN variable was used instead of UINT32 one.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Andrew Fish <afish@apple.com>
    
    
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@15252 6f19259b-4bc3-4df7-8a09-765794883524

--- EmbeddedPkg/Library/PrePiLib/FwVol.c
+++ EmbeddedPkg/Library/PrePiLib/FwVol.c
@@ -293,7 +293,7 @@ FfsProcessSection (
   UINT32                                  SectionLength;
   UINT32                                  ParsedLength;
   EFI_COMPRESSION_SECTION                 *CompressionSection;
-  UINTN                                   DstBufferSize;
+  UINT32                                  DstBufferSize;
   VOID                                    *ScratchBuffer;
   UINT32                                  ScratchBufferSize;
   VOID                                    *DstBuffer;
@@ -322,13 +322,13 @@ FfsProcessSection (
         Status = UefiDecompressGetInfo (
                    (UINT8 *) ((EFI_COMPRESSION_SECTION *) Section + 1),
                    (UINT32) SectionLength - sizeof (EFI_COMPRESSION_SECTION),
-                   (UINT32 *) &DstBufferSize,
+                   &DstBufferSize,
                    &ScratchBufferSize
                    );
       } else if (Section->Type == EFI_SECTION_GUID_DEFINED) {
         Status = ExtractGuidedSectionGetInfo (
                    Section,
-                   (UINT32 *) &DstBufferSize,
+                   &DstBufferSize,
                    &ScratchBufferSize,
                    &SectionAttribute
                    );
