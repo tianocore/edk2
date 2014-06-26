d0f65b21c58b958ba370e774c6fe3ddc5ff1f3c7
Eric Dong <eric.dong@intel.com>
2014-06-26 03:18:44 +0000

    Refine code to make it more safely.
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Eric Dong <eric.dong@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@15597 6f19259b-4bc3-4df7-8a09-765794883524

--- PerformancePkg/Library/TscTimerLib/DxeTscTimerLib.c
+++ PerformancePkg/Library/TscTimerLib/DxeTscTimerLib.c
@@ -59,11 +59,13 @@ DxeTscTimerLibConstructor (
   EFI_STATUS  Status;
   UINT64      *TscFrequency;
 
+  TscFrequency = NULL;
   //
   // Get TSC frequency from system configuration table with TSC frequency GUID.
   //
   Status = EfiGetSystemConfigurationTable (&gEfiTscFrequencyGuid, (VOID **) &TscFrequency);
   if (Status == EFI_SUCCESS) {
+    ASSERT (TscFrequency != NULL);
     mTscFrequency = *TscFrequency;
     return EFI_SUCCESS;
   }
