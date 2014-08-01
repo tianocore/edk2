90152e593bf0f77c6cd01e08118e3c74dd8ee508
Olivier Martin <olivier.martin@arm.com>
2014-08-01 18:34:20 +0000

    EmbeddedPkg: FDT Configuration Table GUID
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Olivier Martin <olivier.martin@arm.com>
    
    
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@15738 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ EmbeddedPkg/Include/Guid/Fdt.h
@@ -0,0 +1,23 @@
+/** @file
+*
+*  Copyright (c) 2013-2014, ARM Limited. All rights reserved.
+*
+*  This program and the accompanying materials
+*  are licensed and made available under the terms and conditions of the BSD License
+*  which accompanies this distribution.  The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+
+#ifndef __FDT_GUID_H__
+#define __FDT_GUID_H__
+
+#define FDT_TABLE_GUID \
+  { 0xb1b621d5, 0xf19c, 0x41a5, { 0x83, 0x0b, 0xd9, 0x15, 0x2c, 0x69, 0xaa, 0xe0 } }
+
+extern EFI_GUID gFdtTableGuid;
+
+#endif /* __FDT_GUID_H__ */
