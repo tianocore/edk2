c687b1464bb42a3c2d3eb2935ddd09752c6d2f2a
Ruiyu Ni <ruiyu.ni@intel.com>
2014-08-13 06:18:06 +0000

    Fix VS2013 build failure.
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Ruiyu Ni <ruiyu.ni@intel.com>
    
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@15794 6f19259b-4bc3-4df7-8a09-765794883524

--- DuetPkg/Library/DuetBdsLib/BdsPlatform.c
+++ DuetPkg/Library/DuetBdsLib/BdsPlatform.c
@@ -1,6 +1,6 @@
 /*++
 
-Copyright (c) 2006 - 2011, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2014, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -721,7 +721,8 @@ Returns:
   EFI_DEVICE_PATH_PROTOCOL  *DevicePath;
   EFI_DEVICE_PATH_PROTOCOL  *GopDevicePath;
 
-  DevicePath = NULL;
+  DevicePath    = NULL;
+  GopDevicePath = NULL;
   Status = gBS->HandleProtocol (
                   DeviceHandle,
                   &gEfiDevicePathProtocolGuid,
