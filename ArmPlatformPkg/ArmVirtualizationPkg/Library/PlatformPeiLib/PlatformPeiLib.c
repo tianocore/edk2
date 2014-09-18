433b31ddeeeb5f521b2159c0ae317a16129aac0a
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2014-09-18 17:58:01 +0000

    ArmPlatformPkg/ArmVirtualizationPkg: Add PlatformPeiLib library
    This is a fork of the ARM PlatformPeiLib for virtual machines. The main
    purpose of having this specific implementation is that it allows us to
    preserve the device tree blob if it was passed to us in system DRAM.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Olivier Martin <olivier.martin@arm.com>
    
    
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@16136 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ ArmPlatformPkg/ArmVirtualizationPkg/Library/PlatformPeiLib/PlatformPeiLib.c
@@ -0,0 +1,47 @@
+/** @file
+*
+*  Copyright (c) 2011-2012, ARM Limited. All rights reserved.
+*  Copyright (c) 2014, Linaro Limited. All rights reserved.
+*
+*  This program and the accompanying materials
+*  are licensed and made available under the terms and conditions of the BSD License
+*  which accompanies this distribution.  The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+
+#include <PiPei.h>
+
+#include <Library/MemoryAllocationLib.h>
+#include <Library/DebugLib.h>
+#include <Library/HobLib.h>
+#include <Library/PcdLib.h>
+#include <libfdt.h>
+
+EFI_STATUS
+EFIAPI
+PlatformPeim (
+  VOID
+  )
+{
+  VOID               *Base;
+  VOID               *NewBase;
+  UINTN              FdtSize;
+
+  Base = (VOID*)(UINTN)FixedPcdGet64 (PcdDeviceTreeInitialBaseAddress);
+  ASSERT (fdt_check_header (Base) == 0);
+
+  FdtSize = fdt_totalsize (Base);
+  NewBase = AllocatePages (EFI_SIZE_TO_PAGES (FdtSize));
+  ASSERT (NewBase != NULL);
+
+  CopyMem (NewBase, Base, FdtSize);
+  PcdSet64 (PcdDeviceTreeBaseAddress, (UINT64)(UINTN)NewBase);
+
+  BuildFvHob (PcdGet32(PcdFvBaseAddress), PcdGet32(PcdFvSize));
+
+  return EFI_SUCCESS;
+}
