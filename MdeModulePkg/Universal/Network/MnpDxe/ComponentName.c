44833d441beec53e8eaf5acb7d26c018f704247b
Eric Dong <eric.dong@intel.com>
2014-09-18 08:45:26 +0000

    Add more check for the code to make it more safely.
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Eric Dong <eric.dong@intel.com>
    Reviewed-by: Siyuan Fu <siyuan.fu@intel.com>
    
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@16129 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Universal/Network/MnpDxe/ComponentName.c
+++ MdeModulePkg/Universal/Network/MnpDxe/ComponentName.c
@@ -1,7 +1,7 @@
 /** @file
   UEFI Component Name(2) protocol implementation for MnpDxe driver.
 
-Copyright (c) 2005 - 2012, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2005 - 2014, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions
 of the BSD License which accompanies this distribution.  The full
@@ -153,6 +153,7 @@ UpdateName (
                   SnpModeData.CurrentAddress.Addr[Index]
                   );
     }
+    ASSERT (OffSet > 0);
     //
     // Remove the last '-'
     //
