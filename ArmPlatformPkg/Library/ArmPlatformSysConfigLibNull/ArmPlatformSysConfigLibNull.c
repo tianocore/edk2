601e3607864e680aaabea81f6eb3c49d9edd17e9
Olivier Martin <olivier.martin@arm.com>
2014-09-18 21:15:29 +0000

    ArmPlatformPkg: Introduced ArmPlatformSysConfigLib NULL library
    Some platforms do not have SysConfig controller used by the ARM
    Versatile Express.
    Some peripheral drivers currently rely on SysConfigLib (eg: PL031
    RTC driver, LCD driver).
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Olivier Martin <olivier.martin@arm.com>
    
    
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@16143 6f19259b-4bc3-4df7-8a09-765794883524

--- ArmPlatformPkg/Library/ArmPlatformSysConfigLibNull/ArmPlatformSysConfigLibNull.c
+++ ArmPlatformPkg/Library/ArmPlatformSysConfigLibNull/ArmPlatformSysConfigLibNull.c
@@ -1,6 +1,7 @@
-/** @file  SysConfigArmRealViewEb.c
+/** @file
 
-  Copyright (c) 2011-2012, ARM Ltd. All rights reserved.<BR>
+  Copyright (c) 2011-2014, ARM Ltd. All rights reserved.<BR>
+
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -12,18 +13,9 @@
 **/
 
 #include <Base.h>
-#include <Library/IoLib.h>
-#include <Library/DebugLib.h>
 
 #include <Library/ArmPlatformSysConfigLib.h>
-#include <ArmPlatform.h>
 
-/****************************************************************************
- *
- *  This file makes it easier to access the System Configuration Registers
- *  in the ARM Versatile Express motherboard.
- *
- ****************************************************************************/
 
 RETURN_STATUS
 ArmPlatformSysConfigInitialize (
@@ -33,24 +25,44 @@ ArmPlatformSysConfigInitialize (
   return RETURN_SUCCESS;
 }
 
+/***************************************
+ * GENERAL FUNCTION: AccessSysCfgRegister
+ * Interacts with
+ *    SYS_CFGSTAT
+ *    SYS_CFGDATA
+ *    SYS_CFGCTRL
+ * for setting and for reading out values
+ ***************************************/
+RETURN_STATUS
+AccessSysCfgRegister (
+  IN     UINT32   ReadWrite,
+  IN     UINT32   Function,
+  IN     UINT32   Site,
+  IN     UINT32   Position,
+  IN     UINT32   Device,
+  IN OUT UINT32*  Data
+  )
+{
+  return RETURN_UNSUPPORTED;
+}
+
 RETURN_STATUS
 ArmPlatformSysConfigGet (
   IN  SYS_CONFIG_FUNCTION   Function,
   OUT UINT32*               Value
   )
 {
-  RETURN_STATUS Status;
+  return RETURN_UNSUPPORTED;
+}
 
-  Status = RETURN_SUCCESS;
-
-  // Intercept some functions
-  switch(Function) {
-
-  default:
-    Status = RETURN_UNSUPPORTED;
-  }
-
-  return Status;
+RETURN_STATUS
+ArmPlatformSysConfigGetValues (
+  IN  SYS_CONFIG_FUNCTION   Function,
+  IN  UINTN                 Size,
+  OUT UINT32*               Values
+  )
+{
+  return RETURN_UNSUPPORTED;
 }
 
 RETURN_STATUS
@@ -59,18 +71,7 @@ ArmPlatformSysConfigSet (
   IN  UINT32                Value
   )
 {
-  RETURN_STATUS Status;
-
-  Status = RETURN_SUCCESS;
-
-  // Intercept some functions
-  switch(Function) {
-
-  default:
-    Status = RETURN_UNSUPPORTED;
-  }
-
-  return Status;
+  return RETURN_UNSUPPORTED;
 }
 
 RETURN_STATUS
@@ -80,16 +81,5 @@ ArmPlatformSysConfigSetDevice (
   IN  UINT32                Value
   )
 {
-  RETURN_STATUS Status;
-
-  Status = RETURN_SUCCESS;
-
-  // Intercept some functions
-  switch(Function) {
-
-  default:
-    Status = RETURN_UNSUPPORTED;
-  }
-
-  return Status;
+  return RETURN_UNSUPPORTED;
 }
