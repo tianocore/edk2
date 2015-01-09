a5077fd0d280b7160e14159144934b1d7391a045
Shumin Qiu <shumin.qiu@intel.com>
2015-01-09 04:50:11 +0000

    MdePkg IA32/X64 ProcessorBind.h : Disable the C4701 and C4703 warnings for VS2013.
    As they may be raised as false positive in building.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Shumin Qiu <shumin.qiu@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@16594 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Ia32/ProcessorBind.h
+++ MdePkg/Include/Ia32/ProcessorBind.h
@@ -1,7 +1,7 @@
 /** @file
   Processor or Compiler specific defines and types for IA-32 architecture.
 
-Copyright (c) 2006 - 2013, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2015, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials are licensed and made available under 
 the terms and conditions of the BSD License that accompanies this distribution.  
 The full text of the license may be found at
@@ -92,6 +92,26 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 // This warning is caused by empty (after preprocessing) source file. For precompiled header only.
 //
 #pragma warning ( disable : 4206 )
+
+#if _MSC_VER == 1800
+
+//
+// Disable these warnings for VS2013.
+//
+
+//
+// This warning is for potentially uninitialized local variable, and it may cause false 
+// positive issues in VS2013 build
+//
+#pragma warning ( disable : 4701 )
+  
+//
+// This warning is for potentially uninitialized local pointer variable, and it may cause 
+// false positive issues in VS2013 build
+//
+#pragma warning ( disable : 4703 )
+  
+#endif
 
 #endif
 
