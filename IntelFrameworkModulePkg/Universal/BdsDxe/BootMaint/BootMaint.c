08640acc7335d5ccfa7332bf8c3c4812766e4ee6
Eric Dong <eric.dong@intel.com>
2015-02-12 08:50:03 +0000

    Remove invalid value sync action when changing value. Else later save change action will fail.
    (Sync patch r16839 from main trunk.)
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Eric Dong <eric.dong@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/branches/UDK2014.SP1@16840 6f19259b-4bc3-4df7-8a09-765794883524

--- IntelFrameworkModulePkg/Universal/BdsDxe/BootMaint/BootMaint.c
+++ IntelFrameworkModulePkg/Universal/BdsDxe/BootMaint/BootMaint.c
@@ -1,7 +1,7 @@
 /** @file
   The functions for Boot Maintainence Main menu.
 
-Copyright (c) 2004 - 2014, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2015, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -1003,12 +1003,6 @@ BootMaintCallback (
 
           Index++;
         }
-
-        CopyMem (
-          OldLegacyDev,
-          NewLegacyDev,
-          Number
-          );
 
         //
         //  Return correct question value.
