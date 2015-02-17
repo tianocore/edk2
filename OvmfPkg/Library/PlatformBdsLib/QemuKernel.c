28a34033eeab2a5078b9d92bcd6ed507a7ea191f
Jordan Justen <jordan.l.justen@intel.com>
2015-02-17 00:05:36 +0000

    OvmfPkg/PlatformBdsLib: Signal ReadyToBoot before booting QEMU kernel
    Before we launch the QEMU kernel, we should signal the ReadyToBoot
    event.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@16878 6f19259b-4bc3-4df7-8a09-765794883524

--- OvmfPkg/Library/PlatformBdsLib/QemuKernel.c
+++ OvmfPkg/Library/PlatformBdsLib/QemuKernel.c
@@ -1,6 +1,6 @@
 /** @file
 
-  Copyright (c) 2006 - 2013, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2015, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -19,6 +19,7 @@
 #include <Library/MemoryAllocationLib.h>
 #include <Library/QemuFwCfgLib.h>
 #include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiLib.h>
 
 
 EFI_STATUS
@@ -142,6 +143,11 @@ TryRunningQemuKernel (
   if (EFI_ERROR (Status)) {
     goto FreeAndReturn;
   }
+
+  //
+  // Signal the EVT_SIGNAL_READY_TO_BOOT event
+  //
+  EfiSignalEventReadyToBoot();
 
   Status = LoadLinux (KernelBuf, SetupBuf);
 
