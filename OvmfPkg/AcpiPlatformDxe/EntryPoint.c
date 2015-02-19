818bc86aa77da1777dbef87893dc1f7967f78b5f
Laszlo Ersek <lersek@redhat.com>
2015-02-19 23:46:27 +0000

    OvmfPkg: AcpiPlatformDxe: make dependency on PCI enumeration dynamic
    SVN r16411 delayed ACPI table installation until PCI enumeration was
    complete, because on QEMU the ACPI-related fw_cfg files should have been
    downloaded only after PCI enumeration. Said commit implemented the
    dependency by tightening the module's depex.
    
    This patch replaces the EFI_PCI_ENUMERATION_COMPLETE_PROTOCOL depex with a
    matching protocol registration callback. The depex was static, and it
    could not handle dynamically discovered situations when the dependency
    would turn out invalid.
    
    Namely:
    
    - At the moment, the depex in "QemuFwCfgAcpiPlatformDxe.inf" assumes
      that "ArmPlatformPkg/ArmVirtualizationPkg/ArmVirtualizationQemu.dsc"
      lacks PCI support. However, PCI support is about to become run-time
      discoverable on that platform. If PCI support is missing, then
      ArmVirtualizationPkg will set PcdPciDisableBusEnumeration to TRUE.
    
      Hence, when PcdPciDisableBusEnumeration is TRUE, we invalidate the
      dependency by not registering the callback and installing the ACPI
      tables right away.
    
    - InitializeXen() in "OvmfPkg/PlatformPei/Xen.c" sets
      PcdPciDisableBusEnumeration to TRUE. This causes
      PciBusDriverBindingStart() in "MdeModulePkg/Bus/Pci/PciBusDxe/PciBus.c"
      to set gFullEnumeration to FALSE, which in turn makes PciEnumerator() in
      "MdeModulePkg/Bus/Pci/PciBusDxe/PciEnumerator.c" branch to
      PciEnumeratorLight(). The installation of
      EFI_PCI_ENUMERATION_COMPLETE_PROTOCOL at the end of PciEnumerator() is
      not reached.
    
      Which means that starting with SVN r16411, AcpiPlatformDxe is never
      dispatched on Xen.
    
      Hence, when PcdPciDisableBusEnumeration is TRUE, we invalidate the
      dependency by not registering the callback and installing the ACPI
      tables right away.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
    [jordan.l.justen@intel.com: Removed PcdOvmfPciEnabled]
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@16887 6f19259b-4bc3-4df7-8a09-765794883524

--- OvmfPkg/AcpiPlatformDxe/EntryPoint.c
+++ OvmfPkg/AcpiPlatformDxe/EntryPoint.c
@@ -13,6 +13,7 @@
   WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 **/
 
+#include <Protocol/PciEnumerationComplete.h>
 #include "AcpiPlatform.h"
 
 STATIC
@@ -33,6 +34,27 @@ FindAcpiTableProtocol (
   return AcpiTable;
 }
 
+
+STATIC
+VOID
+EFIAPI
+OnPciEnumerated (
+  IN EFI_EVENT Event,
+  IN VOID      *Context
+  )
+{
+  EFI_STATUS Status;
+
+  DEBUG ((EFI_D_INFO, "%a: PCI enumeration complete, installing ACPI tables\n",
+    __FUNCTION__));
+  Status = InstallAcpiTables (FindAcpiTableProtocol ());
+  if (EFI_ERROR (Status)) {
+    DEBUG ((EFI_D_ERROR, "%a: InstallAcpiTables: %r\n", __FUNCTION__, Status));
+  }
+  gBS->CloseEvent (Event);
+}
+
+
 EFI_STATUS
 EFIAPI
 AcpiPlatformEntryPoint (
@@ -41,7 +63,54 @@ AcpiPlatformEntryPoint (
   )
 {
   EFI_STATUS Status;
+  VOID       *Interface;
+  EFI_EVENT  PciEnumerated;
+  VOID       *Registration;
 
-  Status = InstallAcpiTables (FindAcpiTableProtocol ());
+  //
+  // If the platform doesn't support PCI, or PCI enumeration has been disabled,
+  // install the tables at once, and let the entry point's return code reflect
+  // the full functionality.
+  //
+  if (PcdGetBool (PcdPciDisableBusEnumeration)) {
+    DEBUG ((EFI_D_INFO, "%a: PCI or its enumeration disabled, installing "
+      "ACPI tables\n", __FUNCTION__));
+    return InstallAcpiTables (FindAcpiTableProtocol ());
+  }
+
+  //
+  // Similarly, if PCI enumeration has already completed, install the tables
+  // immediately.
+  //
+  Status = gBS->LocateProtocol (&gEfiPciEnumerationCompleteProtocolGuid,
+                  NULL /* Registration */, &Interface);
+  if (!EFI_ERROR (Status)) {
+    DEBUG ((EFI_D_INFO, "%a: PCI enumeration already complete, "
+      "installing ACPI tables\n", __FUNCTION__));
+    return InstallAcpiTables (FindAcpiTableProtocol ());
+  }
+  ASSERT (Status == EFI_NOT_FOUND);
+
+  //
+  // Otherwise, delay installing the ACPI tables until PCI enumeration
+  // completes. The entry point's return status will only reflect the callback
+  // setup.
+  //
+  Status = gBS->CreateEvent (EVT_NOTIFY_SIGNAL, TPL_CALLBACK, OnPciEnumerated,
+                  NULL /* Context */, &PciEnumerated);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  Status = gBS->RegisterProtocolNotify (
+                  &gEfiPciEnumerationCompleteProtocolGuid, PciEnumerated,
+                  &Registration);
+  if (EFI_ERROR (Status)) {
+    gBS->CloseEvent (PciEnumerated);
+  } else {
+    DEBUG ((EFI_D_INFO, "%a: PCI enumeration pending, registered callback\n",
+      __FUNCTION__));
+  }
+
   return Status;
 }
