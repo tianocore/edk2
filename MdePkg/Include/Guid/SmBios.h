09d190dc973b568893ceb2da238f76e33f63dbab
Elvin Li <elvin.li@intel.com>
2015-03-16 02:34:30 +0000

    MdePkg: Add SMBIOS 3.0 configuration table Guid.
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Elvin Li <elvin.li@intel.com>
    Reviewed-by: Star Zeng <star.zeng@intel.com>
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@17050 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/Guid/SmBios.h
+++ MdePkg/Include/Guid/SmBios.h
@@ -5,7 +5,7 @@
   locate the SMBIOS tables. Do not search the 0xF0000 segment to find SMBIOS
   tables.
 
-  Copyright (c) 2006 - 2009, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2015, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials                          
   are licensed and made available under the terms and conditions of the BSD License         
   which accompanies this distribution.  The full text of the license may be found at        
@@ -27,6 +27,12 @@
     0xeb9d2d31, 0x2d88, 0x11d3, {0x9a, 0x16, 0x0, 0x90, 0x27, 0x3f, 0xc1, 0x4d } \
   }
 
+#define SMBIOS3_TABLE_GUID \
+  { \
+    0xf2fd1544, 0x9794, 0x4a2c, {0x99, 0x2e, 0xe5, 0xbb, 0xcf, 0x20, 0xe3, 0x94 } \
+  }
+
 extern EFI_GUID       gEfiSmbiosTableGuid;
+extern EFI_GUID       gEfiSmbios3TableGuid;
 
 #endif
