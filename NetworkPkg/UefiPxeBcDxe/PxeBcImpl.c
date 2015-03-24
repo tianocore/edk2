e29fc5022042f7c86b07673741749cf3de5f3816
Fu Siyuan <siyuan.fu@intel.com>
2015-03-24 02:09:36 +0000

    PXE driver bug fix.
    A failed PXEv6 after a success PXEv4 will cause ASSERT.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Fu Siyuan <siyuan.fu@intel.com>
    Reviewed-by: Ye Ting <ting.ye@intel.com>
    Reviewed-by: Wu Jiaxin <jiaxin.wu@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@17070 6f19259b-4bc3-4df7-8a09-765794883524

--- NetworkPkg/UefiPxeBcDxe/PxeBcImpl.c
+++ NetworkPkg/UefiPxeBcDxe/PxeBcImpl.c
@@ -1,7 +1,7 @@
 /** @file
   This implementation of EFI_PXE_BASE_CODE_PROTOCOL and EFI_LOAD_FILE_PROTOCOL.
 
-  Copyright (c) 2007 - 2014, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2007 - 2015, Intel Corporation. All rights reserved.<BR>
 
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
@@ -346,6 +346,7 @@ EfiPxeBcStop (
       gBS->CloseEvent (Private->IcmpToken.Event);
       Private->IcmpToken.Event = NULL;
     }
+    Private->BootFileName = NULL;
   }
 
   gBS->CloseEvent (Private->UdpTimeOutEvent);
