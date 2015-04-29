a1b7065b2e24794baf573666fe9024a53512be06
Liming Gao <liming.gao@intel.com>
2015-04-29 05:10:41 +0000

    EdkCompatibilityPkg: Add PersistentMemory definition
    Include this definition to make sure EfiMaxMemoryType be same value in EDKII and ECP code.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Star Zeng <star.zeng@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@17252 6f19259b-4bc3-4df7-8a09-765794883524

--- EdkCompatibilityPkg/Foundation/Efi/Include/EfiTypes.h
+++ EdkCompatibilityPkg/Foundation/Efi/Include/EfiTypes.h
@@ -1,6 +1,6 @@
 /*++
 
-Copyright (c) 2004 - 2010, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2015, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -195,6 +195,7 @@ typedef enum {
   EfiMemoryMappedIO,
   EfiMemoryMappedIOPortSpace,
   EfiPalCode,
+  EfiPersistentMemory,
   EfiMaxMemoryType
 } EFI_MEMORY_TYPE;
 
