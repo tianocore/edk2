168895ddf3d3d29c29e6eca6539f674596ac5aab
Yao, Jiewen <Jiewen.Yao@intel.com>
2015-04-29 04:53:22 +0000

    Add ACPI6.0 header file.
    (Sync patch r17191 from main trunk.)
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: "Yao, Jiewen" <Jiewen.Yao@intel.com>
    Reviewed-by: "Liming Gao" <liming.gao@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/branches/UDK2014.SP1@17250 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/IndustryStandard/Acpi.h
+++ MdePkg/Include/IndustryStandard/Acpi.h
@@ -2,7 +2,7 @@
   This file contains the latest ACPI definitions that are
   consumed by drivers that do not care about ACPI versions.
 
-  Copyright (c) 2006 - 2014, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2015, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -16,6 +16,6 @@
 #ifndef _ACPI_H_
 #define _ACPI_H_
 
-#include <IndustryStandard/Acpi51.h>
+#include <IndustryStandard/Acpi60.h>
 
 #endif
