afc244a59d76be23471719d1cffd27785c59a3e8
Eric Dong <eric.dong@intel.com>
2015-08-05 07:10:01 +0000

    UiApp: Update copyright info, cover old code existed in old BdsDxe driver.
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Eric Dong <eric.dong@intel.com>
    Reviewed-by: Ruiyu Ni <ruiyu.ni@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@18163 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Application/UiApp/BootMaint/BootOption.c
+++ MdeModulePkg/Application/UiApp/BootMaint/BootOption.c
@@ -5,7 +5,7 @@
 
   Boot option manipulation
 
-Copyright (c) 2015, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2015, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
