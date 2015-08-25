a60a462e530ef8a74846c03e1aefa59a366e29a1
Star Zeng <star.zeng@intel.com>
2015-08-25 06:29:33 +0000

    MdeModulePkg SmmLegacyBoot.h: Add a "," after copyright year
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Star Zeng <star.zeng@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@18299 6f19259b-4bc3-4df7-8a09-765794883524

--- MdeModulePkg/Include/Protocol/SmmLegacyBoot.h
+++ MdeModulePkg/Include/Protocol/SmmLegacyBoot.h
@@ -4,7 +4,7 @@
   This SMM protocol is to be published by the SMM Foundation code to associate
   with EFI_EVENT_LEGACY_BOOT_GUID to notify SMM driver that system enter legacy boot.
 
-  Copyright (c) 2015 Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2015, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
