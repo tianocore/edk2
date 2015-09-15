2c55a81a0a94656d88dbd8ed56b37c5a53f5e20f
Samer El-Haj-Mahmoud <samer.el-haj-mahmoud@hpe.com>
2015-09-15 04:54:06 +0000

    ShellPkg: Rename some functions in Dp to avoid build errors.
    There are other libraries with similarly named functions that could be linked with the Shell
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Samer El-Haj-Mahmoud <samer.el-haj-mahmoud@hpe.com>
    Reviewed-by: Jaben Carsey <jaben.carsey@intel.com>
    Reviewed-by: Qiu Shumin <shumin.qiu@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@18456 6f19259b-4bc3-4df7-8a09-765794883524

--- ShellPkg/Library/UefiDpLib/DpInternal.h
+++ ShellPkg/Library/UefiDpLib/DpInternal.h
@@ -7,6 +7,7 @@
   DpUtilities.c, DpTrace.c, and DpProfile.c are included here.
 
   Copyright (c) 2009 - 2013, Intel Corporation. All rights reserved.
+  (C) Copyright 2015 Hewlett Packard Enterprise Development LP<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -96,7 +97,7 @@ IsPhase(
   
 **/
 VOID
-GetShortPdbFileName (
+DpGetShortPdbFileName (
   IN  CHAR8     *PdbFileName,
   OUT CHAR16    *UnicodeBuffer
   );
@@ -118,7 +119,7 @@ GetShortPdbFileName (
   
 **/
 VOID
-GetNameFromHandle (
+DpGetNameFromHandle (
   IN EFI_HANDLE Handle
   );
 
