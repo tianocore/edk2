338bfd970ad9f852ed82090c240f10a74a77b656
David Woodhouse <David.Woodhouse@intel.com>
2015-10-29 14:16:30 +0000

    CryptoPkg/BaseCryptLib: Use accessor functions for ASN1_OBJECT
    OpenSSL 1.1 introduces new OBJ_get0_data() and OBJ_length() accessor
    functions and makes ASN1_OBJECT an opaque type.
    
    Unlike the accessors in previous commits which *did* actually exist
    already but just weren't mandatory, these don't exist in older versions
    of OpenSSL. So introduce macros which do the right thing, for
    compatibility.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>
    Tested-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Qin Long <qin.long@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@18701 6f19259b-4bc3-4df7-8a09-765794883524

--- CryptoPkg/Library/BaseCryptLib/InternalCryptLib.h
+++ CryptoPkg/Library/BaseCryptLib/InternalCryptLib.h
@@ -1,7 +1,7 @@
 /** @file  
   Internal include file for BaseCryptLib.
 
-Copyright (c) 2010 - 2012, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2010 - 2015, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -22,6 +22,13 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #include <Library/BaseCryptLib.h>
 
 #include "OpenSslSupport.h"
+
+#include <openssl/opensslv.h>
+
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#define OBJ_get0_data(o) ((o)->data)
+#define OBJ_length(o) ((o)->length)
+#endif
 
 //
 // Environment Setting for OpenSSL-based UEFI Crypto Library.
