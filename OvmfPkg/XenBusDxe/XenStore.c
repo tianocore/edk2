17247f53d59077ca04dea27e848c409d065eb478
Liming Gao <liming.gao@intel.com>
2015-12-04 03:16:37 +0000

    OvmfPkg: Fix VS2015 warning C4459 in XenBusDxe
    warning C4459: declaration of 'xs' hides global declaration.
    Update code to rename local variable xs to xsp to be different.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
    Acked-by: Michael Kinney <michael.d.kinney@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@19116 6f19259b-4bc3-4df7-8a09-765794883524

--- OvmfPkg/XenBusDxe/XenStore.c
+++ OvmfPkg/XenBusDxe/XenStore.c
@@ -992,9 +992,9 @@ NotifyEventChannelCheckForEvent (
   IN VOID *Context
   )
 {
-  XENSTORE_PRIVATE *xs;
-  xs = (XENSTORE_PRIVATE *)Context;
-  if (TestAndClearBit (xs->EventChannel, xs->Dev->SharedInfo->evtchn_pending)) {
+  XENSTORE_PRIVATE *xsp;
+  xsp = (XENSTORE_PRIVATE *)Context;
+  if (TestAndClearBit (xsp->EventChannel, xsp->Dev->SharedInfo->evtchn_pending)) {
     gBS->SignalEvent (Event);
   }
 }
@@ -1007,12 +1007,12 @@ NotifyEventChannelCheckForEvent (
 STATIC
 EFI_STATUS
 XenStoreInitComms (
-  XENSTORE_PRIVATE *xs
+  XENSTORE_PRIVATE *xsp
   )
 {
   EFI_STATUS Status;
   EFI_EVENT TimerEvent;
-  struct xenstore_domain_interface *XenStore = xs->XenStore;
+  struct xenstore_domain_interface *XenStore = xsp->XenStore;
 
   Status = gBS->CreateEvent (EVT_TIMER, 0, NULL, NULL, &TimerEvent);
   Status = gBS->SetTimer (TimerEvent, TimerRelative,
@@ -1029,8 +1029,8 @@ XenStoreInitComms (
   gBS->CloseEvent (TimerEvent);
 
   Status = gBS->CreateEvent (EVT_NOTIFY_WAIT, TPL_NOTIFY,
-                             NotifyEventChannelCheckForEvent, xs,
-                             &xs->EventChannelEvent);
+                             NotifyEventChannelCheckForEvent, xsp,
+                             &xsp->EventChannelEvent);
   ASSERT_EFI_ERROR (Status);
 
   return Status;
