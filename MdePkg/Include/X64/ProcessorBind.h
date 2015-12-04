ea6947cdfb224d2544cb962f301c4ef0b66b5eb0
Liming Gao <liming.gao@intel.com>
2015-12-04 03:44:11 +0000

    MdePkg: Disable VS2015 warning C4701 & C4703
    C4701 & C4703 may cause false positive issues.
    They have been disabled in VS2013.
    
    (Sync patch r19111 from main trunk.)
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Michael Kinney <michael.d.kinney@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/branches/UDK2015@19118 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/X64/ProcessorBind.h
+++ MdePkg/Include/X64/ProcessorBind.h
@@ -94,7 +94,7 @@
 //
 #pragma warning ( disable : 4206 )
 
-#if _MSC_VER == 1800
+#if _MSC_VER == 1800 || _MSC_VER == 1900
 
 //
 // Disable these warnings for VS2013.
@@ -102,13 +102,13 @@
 
 //
 // This warning is for potentially uninitialized local variable, and it may cause false 
-// positive issues in VS2013 build
+// positive issues in VS2013 and VS2015 build
 //
 #pragma warning ( disable : 4701 )
   
 //
 // This warning is for potentially uninitialized local pointer variable, and it may cause 
-// false positive issues in VS2013 build
+// false positive issues in VS2013 and VS2015 build
 //
 #pragma warning ( disable : 4703 )
   
