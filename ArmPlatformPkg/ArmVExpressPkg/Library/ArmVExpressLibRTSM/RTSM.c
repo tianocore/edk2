aaa0820553db9da8dee3ea562942dce89873886d
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2015-12-14 13:00:30 +0000

    ArmPlatformPkg/RTSM: use declared PPI rather than module local var
    We can refer to gArmMpCoreInfoPpiGuid directly if we declare it under
    [Ppis] in the library's .inf so there is no need to copy it into a
    module local variable.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@19243 6f19259b-4bc3-4df7-8a09-765794883524

--- ArmPlatformPkg/ArmVExpressPkg/Library/ArmVExpressLibRTSM/RTSM.c
+++ ArmPlatformPkg/ArmVExpressPkg/Library/ArmVExpressLibRTSM/RTSM.c
@@ -175,14 +175,12 @@ PrePeiCoreGetMpCoreInfo (
   }
 }
 
-// Needs to be declared in the file. Otherwise gArmMpCoreInfoPpiGuid is undefined in the contect of PrePeiCore
-EFI_GUID mArmMpCoreInfoPpiGuid = ARM_MP_CORE_INFO_PPI_GUID;
 ARM_MP_CORE_INFO_PPI mMpCoreInfoPpi = { PrePeiCoreGetMpCoreInfo };
 
 EFI_PEI_PPI_DESCRIPTOR      gPlatformPpiTable[] = {
   {
     EFI_PEI_PPI_DESCRIPTOR_PPI,
-    &mArmMpCoreInfoPpiGuid,
+    &gArmMpCoreInfoPpiGuid,
     &mMpCoreInfoPpi
   }
 };
