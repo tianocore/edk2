23df19a7781885d9220144f2e2678550ea013c66
Samer El-Haj-Mahmoud <elhaj@hpe.com>
2015-12-16 00:56:20 +0000

    MdePkg: Add missing SMBIOS definitions for SATA and SAS Ports
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Samer El-Haj-Mahmoud <elhaj@hpe.com>
    Reviewed-by: Star Zeng <star.zeng@intel.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@19289 6f19259b-4bc3-4df7-8a09-765794883524

--- MdePkg/Include/IndustryStandard/SmBios.h
+++ MdePkg/Include/IndustryStandard/SmBios.h
@@ -2,6 +2,7 @@
   Industry Standard Definitions of SMBIOS Table Specification v3.0.0.
 
 Copyright (c) 2006 - 2015, Intel Corporation. All rights reserved.<BR>
+(C) Copyright 2015 Hewlett Packard Enterprise Development LP<BR>
 This program and the accompanying materials are licensed and made available under 
 the terms and conditions of the BSD License that accompanies this distribution.  
 The full text of the license may be found at
@@ -1077,6 +1078,8 @@ typedef enum {
   PortTypeAudioPort                 = 0x1D,
   PortTypeModemPort                 = 0x1E,
   PortTypeNetworkPort               = 0x1F,
+  PortTypeSata                      = 0x20,
+  PortTypeSas                       = 0x21,
   PortType8251Compatible            = 0xA0,
   PortType8251FifoCompatible        = 0xA1,
   PortTypeOther                     = 0xFF
