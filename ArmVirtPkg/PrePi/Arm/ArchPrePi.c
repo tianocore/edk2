809a639ee2594bde29b35840f518840ef5e2d95b
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2015-12-17 17:11:16 +0000

    ArmVirtPkg/PrePiRelocatable: add ARM support
    This adds support to the self relocating PrePi instance that is built
    as a PIE ET_DYN executable. It primarily involves porting the relocation
    routine to use ELF32 REL entries instead of ELF64 RELA entries which is
    what AArch64 uses.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Acked-by: Laszlo Ersek <lersek@redhat.com>
    
    git-svn-id: https://svn.code.sf.net/p/edk2/code/trunk/edk2@19331 6f19259b-4bc3-4df7-8a09-765794883524

---
+++ ArmVirtPkg/PrePi/Arm/ArchPrePi.c
@@ -0,0 +1,26 @@
+/** @file
+*
+*  Copyright (c) 2011-2013, ARM Limited. All rights reserved.
+*
+*  This program and the accompanying materials
+*  are licensed and made available under the terms and conditions of the BSD License
+*  which accompanies this distribution.  The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+
+#include "PrePi.h"
+
+VOID
+ArchInitialize (
+  VOID
+  )
+{
+  // Enable Floating Point
+  if (FixedPcdGet32 (PcdVFPEnabled)) {
+    ArmEnableVFP ();
+  }
+}
