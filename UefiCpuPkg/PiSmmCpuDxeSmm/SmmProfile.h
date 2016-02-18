d691abec1bae4db5e4c58be7dbce40974d810f7a
Michael Kinney <michael.d.kinney@intel.com>
2016-02-18 09:46:25 -0800

    UefiCpuPkg/PiSmmCpuDxeSmm: Add EFIAPI to CheckFeatureSupported()
    The function CheckFeatureSupported() is used as an EFI_AP_PROCEDURE
    in the MP Services Protocol service StartAllAPs().  Any function
    used as an EFI_AP_PROCEDURE must use EFIAPI calling convention.
    
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Jeff Fan <jeff.fan@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Michael Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Jeff Fan <jeff.fan@intel.com>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Jiewen Yao <jiewen.yao@intel.com>

--- UefiCpuPkg/PiSmmCpuDxeSmm/SmmProfile.h
+++ UefiCpuPkg/PiSmmCpuDxeSmm/SmmProfile.h
@@ -1,7 +1,7 @@
 /** @file
 SMM profile header file.
 
-Copyright (c) 2012 - 2015, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2012 - 2016, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -95,10 +95,13 @@ PageFaultIdtHandlerSmmProfile (
 /**
   Check if XD feature is supported by a processor.
 
+  @param[in,out] Buffer  The pointer to private data buffer.
+
 **/
 VOID
+EFIAPI
 CheckFeatureSupported (
-  VOID
+  IN OUT VOID   *Buffer
   );
 
 /**
