4cc1e1833501f169f068d8a82b4d58da70297748
dawid.ciecierski <dawid.ciecierski@gmail.com>
2016-02-26 18:24:45 +0000

    Screen centered, installer happy

--- MdeModulePkg/Application/VgaShim/VgaShim.c
+++ MdeModulePkg/Application/VgaShim/VgaShim.c
@@ -3,11 +3,6 @@
 
 **/
 
-#define VROM_ADDRESS	0xc0000
-#define VROM_SIZE		0x10000
-#define FIXED_MTRR_SIZE	0x20000
-#define IVT_ADDRESS		0x00000
-
 #include <Uefi.h>
 
 #include <Library/DebugLib.h>
@@ -36,11 +31,21 @@ typedef enum
 	MEM_UNLOCK
 } MEMORY_LOCK_OPERATION;
 
+typedef struct {
+	UINT32						HorizontalResolution;
+	UINT32						VerticalResolution;
+	EFI_GRAPHICS_PIXEL_FORMAT	PixelFormat;
+	UINT32						PixelsPerScanLine;
+	EFI_PHYSICAL_ADDRESS		FrameBufferBase;
+	UINTN						FrameBufferSize;
+} VIDEO_INFO;
+
 BOOLEAN CanWriteAtAddress(EFI_PHYSICAL_ADDRESS address);
 EFI_STATUS EnsureMemoryLock(EFI_PHYSICAL_ADDRESS Address, UINT32 Length, MEMORY_LOCK_OPERATION Operation);
 BOOLEAN IsInt10HandlerDefined();
-VBE_MODE_INFO* FillVesaInformation(EFI_PHYSICAL_ADDRESS Address);
+EFI_STATUS FillVesaInformation(IN EFI_PHYSICAL_ADDRESS StartAddress, OUT EFI_PHYSICAL_ADDRESS *EndAddress);
 VOID ShowVideoInfo();
+EFI_STATUS GetVideoInfo(VIDEO_INFO *Info);
 
 // ensure correct alignment
 #pragma pack (1)
@@ -50,14 +55,14 @@ typedef struct {
 } IVT_ENTRY;
 #pragma pack ()
 
-//
-// This string is displayed by Windows 2008 R2 SP1 in the Screen Resolution,
-// Advanced Settings dialog. It should be short.
-//
 STATIC CONST CHAR8 VENDOR_NAME[] = "OVMF";
 STATIC CONST CHAR8 PRODUCT_NAME[] = "DummyCard";
 STATIC CONST CHAR8 PRODUCT_REVISION[] = "OVMF Int10h (fake)";
-STATIC CONST CHAR8 mProductRevision[] = "OVMF Int10h (fake)";
+STATIC CONST EFI_PHYSICAL_ADDRESS VROM_ADDRESS = 0xc0000;
+STATIC CONST EFI_PHYSICAL_ADDRESS IVT_ADDRESS = 0x00000;
+STATIC CONST UINTN VROM_SIZE = 0x10000;
+STATIC CONST UINTN FIXED_MTRR_SIZE = 0x20000;
+
 
 EFI_STATUS
 EFIAPI
@@ -65,6 +70,7 @@ UefiMain (
 	IN EFI_HANDLE		ImageHandle,
 	IN EFI_SYSTEM_TABLE	*SystemTable)
 {
+	EFI_PHYSICAL_ADDRESS	Int10HandlerAddress;
 	EFI_PHYSICAL_ADDRESS	TempAddress;
 	IVT_ENTRY				*Int0x10;
 	EFI_STATUS				Status;
@@ -107,6 +113,7 @@ UefiMain (
 	Status = gBS->AllocatePages(AllocateAddress, EfiBootServicesCode, 1, &TempAddress);
 	if (EFI_ERROR(Status)) {
 		Print(L"failure: %r\n", Status);
+		return EFI_ABORTED;
 	} else {
 		Print(L"success\n");
 		ASSERT(Int0x10->Segment == IVT_ADDRESS);
@@ -118,14 +125,21 @@ UefiMain (
 	//
 	SetMem((VOID *)(UINTN)VROM_ADDRESS, VROM_SIZE, 0);
 	CopyMem((VOID *)(UINTN)VROM_ADDRESS, mVgaShim, sizeof mVgaShim);
-	VbeModeInfo = FillVesaInformation(VROM_ADDRESS);
+	Status = FillVesaInformation(VROM_ADDRESS, &Int10HandlerAddress);
+	if (EFI_ERROR(Status)) {
+		Print(L"%a: Cannot complete shim installation, aborting\n", __FUNCTION__);
+		return EFI_ABORTED;
+	} else {
+		Print(L"%a: VESA information filled in, Int10h handler address = %x\n", 
+			__FUNCTION__, Int10HandlerAddress);
+	}
 	
 	//
 	// Lock the VGA ROM memory to prevent further writes.
 	//
 	Status = EnsureMemoryLock(VROM_ADDRESS, VROM_SIZE, MEM_LOCK);
 	if (EFI_ERROR(Status)) {
-		Print(L"%a: Unable to lock VGA ROM memory at %x but continuing anyway\n", 
+		Print(L"%a: Unable to lock VGA ROM memory at %x but this is not essential\n", 
 			__FUNCTION__, VROM_ADDRESS);
 	}
 
@@ -134,12 +148,12 @@ UefiMain (
 	//
 	// convert from real 32bit physical address to real mode segment address
 	Int0x10->Segment = (UINT16)((UINT32)VROM_ADDRESS >> 4);
-	Int0x10->Offset = (UINT16)((UINTN)(VbeModeInfo + 1) - VROM_ADDRESS);
+	Int0x10->Offset = (UINT16)(Int10HandlerAddress - VROM_ADDRESS);
 	Print(L"%a: Int10h handler installed at %04x:%04x\n",
 		__FUNCTION__, Int0x10->Segment, Int0x10->Offset);
 
 Exit:
-	Print(L"%a: All done, exiting\n", __FUNCTION__);
+	Print(L"%a: Done!\n", __FUNCTION__);
 	return EFI_SUCCESS;
 }
 
@@ -147,102 +161,132 @@ Exit:
 VOID
 ShowVideoInfo()
 {
+	VIDEO_INFO	Info;
+
+	if (EFI_ERROR(GetVideoInfo(&Info))) {
+		return;
+	}
+
+	Print(L"%a: HorizontalResolution = %u\n", __FUNCTION__, Info.HorizontalResolution);
+	Print(L"%a: VerticalResolution = %u\n", __FUNCTION__, Info.VerticalResolution);
+	Print(L"%a: PixelFormat = %u\n", __FUNCTION__, Info.PixelFormat);
+	Print(L"%a: PixelsPerScanLine = %u\n", __FUNCTION__, Info.PixelsPerScanLine);
+	Print(L"%a: FrameBufferBase = %x\n", __FUNCTION__, Info.FrameBufferBase);
+	Print(L"%a: FrameBufferSize = %u\n", __FUNCTION__, Info.FrameBufferSize);
+}
+
+
+EFI_STATUS
+GetVideoInfo(
+	VIDEO_INFO	*Info)
+{
 	EFI_STATUS						Status;
 	EFI_UGA_DRAW_PROTOCOL			*UgaDraw;
 	EFI_GRAPHICS_OUTPUT_PROTOCOL	*GraphicsOutput;
 
+	SetMem(Info, sizeof Info, 0);
+
 	//
-	// Enumerate GOP adapters first.
+	// Try a GOP adapter first.
 	//
 	Status = gBS->HandleProtocol(gST->ConsoleOutHandle, &gEfiGraphicsOutputProtocolGuid, (VOID **)&GraphicsOutput);
 	if (!EFI_ERROR(Status)) {
 		Print(L"%a: Found a GOP protocol provider\n", __FUNCTION__);
+		Info->HorizontalResolution = GraphicsOutput->Mode->Info->HorizontalResolution;
+		Info->VerticalResolution = GraphicsOutput->Mode->Info->VerticalResolution;
+		Info->PixelFormat = GraphicsOutput->Mode->Info->PixelFormat;
+		Info->PixelsPerScanLine = GraphicsOutput->Mode->Info->PixelsPerScanLine;
+		Info->FrameBufferBase = GraphicsOutput->Mode->FrameBufferBase;
+		// usually = PixelsPerScanLine * VerticalResolution * BytesPerPixel
+		// for MacBookAir7,2: 1536 * 900 * 4 = 5,529,600 bytes
+		Info->FrameBufferSize = GraphicsOutput->Mode->FrameBufferSize;
 
-		Print(L"%a: HorizontalResolution = %u\n", __FUNCTION__, 
-			GraphicsOutput->Mode->Info->HorizontalResolution);
-		
-		Print(L"%a: VerticalResolution = %u\n", __FUNCTION__, 
-			GraphicsOutput->Mode->Info->VerticalResolution);
-
-		Print(L"%a: PixelFormat = %u\n", __FUNCTION__, 
-			GraphicsOutput->Mode->Info->PixelFormat);
-		
-		Print(L"%a: Pixel masks: red = %u green = %u blue = %u reserved = %u\n", __FUNCTION__, 
-			GraphicsOutput->Mode->Info->PixelInformation.RedMask,
-			GraphicsOutput->Mode->Info->PixelInformation.GreenMask,
-			GraphicsOutput->Mode->Info->PixelInformation.BlueMask,
-			GraphicsOutput->Mode->Info->PixelInformation.ReservedMask);
-		
-		Print(L"%a: PixelsPerScanLine = %u\n", __FUNCTION__, 
-			GraphicsOutput->Mode->Info->PixelsPerScanLine);
-
-		// B000 0000
-		Print(L"%a: FrameBufferBase = %x\n", __FUNCTION__, 
-			GraphicsOutput->Mode->FrameBufferBase);
-
-		// PixelsPerScanLine * VerticalResolution * BytesPerPixel
-		// In this case 1536*900*4 = 5,529,600
-		Print(L"%a: FrameBufferSize = %u\n", __FUNCTION__, 
-			GraphicsOutput->Mode->FrameBufferSize);
-	} else {
-		Print(L"%a: GOP protocol provider not found\n", __FUNCTION__);
+		return EFI_SUCCESS;
 	}
 
 	//
-	// Enumerate UGA adapters
+	// Try a UGA adapter.
 	//
 	Status = gBS->HandleProtocol(gST->ConsoleOutHandle, &gEfiUgaDrawProtocolGuid, (VOID **)&UgaDraw);
 	if (!EFI_ERROR(Status)) {
-		Print(L"%a: Found an UGA protocol provider\n", __FUNCTION__);
-		//Status = UgaDraw->GetMode(UgaDraw, &SizeOfX, &SizeOfY, &ColorDepth, &RefreshRate);
-		// pixel format is BGRA8
-		// need to find scanline length
-		// need to find framebuffer base
+		UINT32 Temp1;
+		UINT32 Temp2;
+		Print(L"%a: Found a UGA protocol provider\n", __FUNCTION__);
+		
+		Status = UgaDraw->GetMode(UgaDraw, 
+			&Info->HorizontalResolution, &Info->VerticalResolution, &Temp1, &Temp2);
+		if (EFI_ERROR(Status)) {
+			Print(L"%a: Unable to get current UGA mode\n", __FUNCTION__);
+			return EFI_DEVICE_ERROR;
+		}
+
+		Info->PixelFormat = PixelBlueGreenRedReserved8BitPerColor; // default for UGA
+		
+		// TODO: find framebuffer base
+		// TODO: find scanline length
 		// https://github.com/coreos/grub/blob/master/grub-core%2Fvideo%2Fefi_uga.c
+
+		return EFI_SUCCESS;
 	}
+
+	Print(L"%a: No GOP or UGA adapters found\n", __FUNCTION__);
+	return EFI_NOT_FOUND;
 }
 
 
-VBE_MODE_INFO*
+EFI_STATUS
 FillVesaInformation(
-	EFI_PHYSICAL_ADDRESS	Address)
+	IN	EFI_PHYSICAL_ADDRESS	StartAddress,
+	OUT	EFI_PHYSICAL_ADDRESS	*EndAddress)
 {
 	// (Page 26 in VESA BIOS EXTENSION Core Functions Standard v3.0.)
 	// (Page 30 in VESA BIOS EXTENSION Core Functions Standard v3.0.)
 
-	VBE_INFO		*VbeInfoFull;
-	VBE_INFO_BASE	*VbeInfo;
-	VBE_MODE_INFO	*VbeModeInfo;
-	UINT8			*BufferPtr;
-	EFI_GRAPHICS_OUTPUT_PROTOCOL	*GraphicsOutput;
-	gBS->HandleProtocol(gST->ConsoleOutHandle, &gEfiGraphicsOutputProtocolGuid, (VOID **)&GraphicsOutput);
+	EFI_STATUS				Status;
+	VBE_INFO				*VbeInfoFull;
+	VBE_INFO_BASE			*VbeInfo;
+	VBE_MODE_INFO			*VbeModeInfo;
+	UINT8					*BufferPtr;
+	VIDEO_INFO				VideoInfo;
+	UINT32					HorizontalOffsetPx;
+	UINT32					VerticalOffsetPx;
+	EFI_PHYSICAL_ADDRESS	FrameBufferBaseWithOffset;
 
+	//
+	// Get basic video hardware information first.
+	//
+	Status = GetVideoInfo(&VideoInfo);
+	if (EFI_ERROR(Status)) {
+		Print(L"%a: Unable to fill in VESA information as no adapters were found\n", __FUNCTION__);
+		return EFI_NOT_FOUND;
+	}
+	
 	//
 	// VESA general information.
 	//
-	VbeInfoFull = (VBE_INFO *)(UINTN)Address;
+	VbeInfoFull = (VBE_INFO *)(UINTN)StartAddress;
 	VbeInfo = &VbeInfoFull->Base;
 	BufferPtr = VbeInfoFull->Buffer;
 	CopyMem(VbeInfo->Signature, "VESA", 4);
 	VbeInfo->VesaVersion = 0x0300;
-	VbeInfo->OemNameAddress = (UINT32)Address << 12 | (UINT16)(UINTN)BufferPtr;
+	VbeInfo->OemNameAddress = (UINT32)StartAddress << 12 | (UINT16)(UINTN)BufferPtr;
 	CopyMem(BufferPtr, VENDOR_NAME, sizeof VENDOR_NAME);
 	BufferPtr += sizeof VENDOR_NAME;
 	VbeInfo->Capabilities = BIT0;			// DAC width supports 8-bit color mode
-	VbeInfo->ModeListAddress = (UINT32)Address << 12 | (UINT16)(UINTN)BufferPtr;
+	VbeInfo->ModeListAddress = (UINT32)StartAddress << 12 | (UINT16)(UINTN)BufferPtr;
 	*(UINT16*)BufferPtr = 0x00f1;			// mode number
 	BufferPtr += 2;
 	*(UINT16*)BufferPtr = 0xFFFF;			// mode list terminator
 	BufferPtr += 2;
-	VbeInfo->VideoMem64K = (UINT16)((GraphicsOutput->Mode->FrameBufferSize + 65535) / 65536); // 64KB units available to fb
+	VbeInfo->VideoMem64K = (UINT16)((VideoInfo.FrameBufferSize + 65535) / 65536);
 	VbeInfo->OemSoftwareVersion = 0x0000;
-	VbeInfo->VendorNameAddress = (UINT32)Address << 12 | (UINT16)(UINTN)BufferPtr;
+	VbeInfo->VendorNameAddress = (UINT32)StartAddress << 12 | (UINT16)(UINTN)BufferPtr;
 	CopyMem(BufferPtr, VENDOR_NAME, sizeof VENDOR_NAME);
 	BufferPtr += sizeof VENDOR_NAME;
-	VbeInfo->ProductNameAddress = (UINT32)Address << 12 | (UINT16)(UINTN)BufferPtr;
+	VbeInfo->ProductNameAddress = (UINT32)StartAddress << 12 | (UINT16)(UINTN)BufferPtr;
 	CopyMem(BufferPtr, PRODUCT_NAME, sizeof PRODUCT_NAME);
 	BufferPtr += sizeof PRODUCT_NAME;
-	VbeInfo->ProductRevAddress = (UINT32)Address << 12 | (UINT16)(UINTN)BufferPtr;
+	VbeInfo->ProductRevAddress = (UINT32)StartAddress << 12 | (UINT16)(UINTN)BufferPtr;
 	CopyMem(BufferPtr, PRODUCT_REVISION, sizeof PRODUCT_REVISION);
 	BufferPtr += sizeof PRODUCT_REVISION;
 	
@@ -252,7 +296,7 @@ FillVesaInformation(
 	//
 	// Basic VESA mode information
 	//
-	VbeModeInfo = (VBE_MODE_INFO *)(VbeInfoFull + 1);
+	VbeModeInfo = (VBE_MODE_INFO *)(VbeInfoFull + 1); // jump ahead by sizeof VBE_INFO ie. 256
 	// bit0: mode supported by present hardware configuration
 	// bit1: must be set for VBE v1.2+
 	// bit3: color mode
@@ -265,27 +309,34 @@ FillVesaInformation(
 	//
 	// Resolution
 	//
-	VbeModeInfo->Width = 1024;
-	VbeModeInfo->Height = 768;
+	VbeModeInfo->Width = 1024;						// as expected by Windows installer
+	VbeModeInfo->Height = 768;						// as expected by Windows installer
 	VbeModeInfo->CharCellWidth = 8;					// used to calculate resolution in text modes
 	VbeModeInfo->CharCellHeight = 16;				// used to calculate resolution in text modes
 	
+	// Calculate offsets so that the smaller image appears centered on the screen
+	HorizontalOffsetPx = (VideoInfo.HorizontalResolution - 1024) / 2;
+	VerticalOffsetPx = (VideoInfo.VerticalResolution - 768) / 2 * VideoInfo.PixelsPerScanLine;
+	FrameBufferBaseWithOffset = VideoInfo.FrameBufferBase 
+		+ VerticalOffsetPx * 4
+		+ HorizontalOffsetPx * 4;
+
 	//
 	// Memory access (banking, windowing, paging)
 	//
 	VbeModeInfo->NumBanks = 1;						// disable memory banking
 	VbeModeInfo->BankSizeKB = 0;					// disable memory banking
-	VbeModeInfo->BytesPerScanLine = 1536 * 4;		// logical bytes in banked modes
-	VbeModeInfo->LfbAddress = (UINT32)GraphicsOutput->Mode->FrameBufferBase;	// 32-bit physical address
-	VbeModeInfo->BytesPerScanLineLinear = 1536 * 4;	// logical bytes in linear modes
+	VbeModeInfo->LfbAddress = 
+		(UINT32)FrameBufferBaseWithOffset;			// 32-bit physical address
+	VbeModeInfo->BytesPerScanLineLinear = 
+		VideoInfo.PixelsPerScanLine * 4;			// logical bytes in linear modes
 	VbeModeInfo->NumImagePagesLessOne = 0;			// disable image paging
-	VbeModeInfo->NumImagesLessOneBanked = 0;		// disable image paging
 	VbeModeInfo->NumImagesLessOneLinear = 0;		// disable image paging
 	VbeModeInfo->WindowPositioningAddress = 0x0;	// force windowing to Function 5h
 	VbeModeInfo->WindowAAttr = 0x0;					// window disabled
 	VbeModeInfo->WindowBAttr = 0x0;					// window disabled
 	VbeModeInfo->WindowGranularityKB = 0x0;			// window disabled and not relocatable
-	VbeModeInfo->WindowSizeKB = 0x0040;				// set to 64KB even thou window is disabled
+	VbeModeInfo->WindowSizeKB = 0x0;				// set to 64KB even thou window is disabled
 	VbeModeInfo->WindowAStartSegment = 0x0;			// linear framebuffer only
 	VbeModeInfo->WindowBStartSegment = 0x0;			// linear framebuffer only
 
@@ -295,24 +346,28 @@ FillVesaInformation(
 	VbeModeInfo->NumPlanes = 1;						// packed pixel mode
 	VbeModeInfo->MemoryModel = 6;					// Direct Color
 	VbeModeInfo->DirectColorModeInfo = BIT1;		// alpha bytes may be used by application
-	VbeModeInfo->BitsPerPixel = 32;					// 8+8+8+8
-	VbeModeInfo->BlueMaskSize = 8;					// 8 bits for blue
+	VbeModeInfo->BitsPerPixel = 32;					// 8+8+8+8 bits per channel
 	VbeModeInfo->BlueMaskSizeLinear = 8;
-	VbeModeInfo->GreenMaskSize = 8;					// 8 bits for green
 	VbeModeInfo->GreenMaskSizeLinear = 8;
-	VbeModeInfo->RedMaskSize = 8;					// 8 bits for red
 	VbeModeInfo->RedMaskSizeLinear = 8;
-	VbeModeInfo->ReservedMaskSize = 8;				// 8 bits for alpha
 	VbeModeInfo->ReservedMaskSizeLinear = 8;
-	VbeModeInfo->BlueMaskPos = 0;					// blue offset
-	VbeModeInfo->BlueMaskPosLinear = 0;
-	VbeModeInfo->GreenMaskPos = 8;					// green offset
-	VbeModeInfo->GreenMaskPosLinear = 8;
-	VbeModeInfo->RedMaskPos = 16;					// red offset
-	VbeModeInfo->RedMaskPosLinear = 16;
-	VbeModeInfo->ReservedMaskPos = 24;				// alpha offset
-	VbeModeInfo->ReservedMaskPosLinear = 24;
 
+	if (VideoInfo.PixelFormat == PixelBlueGreenRedReserved8BitPerColor) {
+		VbeModeInfo->BlueMaskPosLinear = 0;			// blue offset
+		VbeModeInfo->GreenMaskPosLinear = 8;		// green offset
+		VbeModeInfo->RedMaskPosLinear = 16;			// green offset
+		VbeModeInfo->ReservedMaskPosLinear = 24;	// reserved offset
+	} else if (VideoInfo.PixelFormat == PixelRedGreenBlueReserved8BitPerColor) {
+		VbeModeInfo->RedMaskPosLinear = 0;			// red offset
+		VbeModeInfo->GreenMaskPosLinear = 8;		// green offset
+		VbeModeInfo->BlueMaskPosLinear = 16;		// blue offset
+		VbeModeInfo->ReservedMaskPosLinear = 24;	// alpha offset
+	} else {
+		Print(L"%a: Unsupported value of PixelFormat (%d), aborting\n", 
+			__FUNCTION__, VideoInfo.PixelFormat);
+		return EFI_UNSUPPORTED;
+	}
+	
 	//
 	// Other
 	//
@@ -321,7 +376,8 @@ FillVesaInformation(
 	VbeModeInfo->MaxPixelClockHz = 0;				// maximum available refresh rate
 	VbeModeInfo->Vbe3 = 0x01;						// reserved, always set to 1
 
-	return VbeModeInfo;
+	*EndAddress = (UINTN)(VbeModeInfo + 1);			// jump ahead by sizeof VBE_MODE_INFO ie. 256
+	return EFI_SUCCESS;
 }
 
 
