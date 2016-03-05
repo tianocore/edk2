93338232ea58d10bb2cb65cf4a0124f19c798acb
Qin Long <qin.long@intel.com>
2016-03-05 23:38:51 +0800

    CryptoPkg/OpensslLib: Switch to upstream fix for OpenSSL RT#3628
    A complete implementation of the no-filenames configuration option was
    added to OpenSSL 1.1 in commit 02f7114a7. Drop our own version and use
    a backport of what was committed upstream.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Qin Long <qin.long@intel.com>
    Tested-by: Qin Long <qin.long@intel.com>
    (cherry picked from commit e578aa19dcd57734e9b4615e39d8d409384329a0)

--- CryptoPkg/Library/OpensslLib/opensslconf.h
+++ CryptoPkg/Library/OpensslLib/opensslconf.h
@@ -318,6 +318,16 @@ extern "C" {
 
 /* crypto/opensslconf.h.in */
 
+#ifndef OPENSSL_FILE
+#ifdef OPENSSL_NO_FILENAMES
+#define OPENSSL_FILE ""
+#define OPENSSL_LINE 0
+#else
+#define OPENSSL_FILE __FILE__
+#define OPENSSL_LINE __LINE__
+#endif
+#endif
+
 /* Generate 80386 code? */
 #undef I386_ONLY
 
