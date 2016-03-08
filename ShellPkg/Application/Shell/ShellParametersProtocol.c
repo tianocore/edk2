e9223dc34eaedf0c0f198765d4c86f73d37e3387
Qiu Shumin <shumin.qiu@intel.com>
2016-03-08 20:51:43 +0800

    ShellPkg: Use DOS format end of line.
    Use DOS format end of line(CR, LF).
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Qiu Shumin <shumin.qiu@intel.com>
    Reviewed-by: Jaben Carsey <jaben.carsey@intel.com>
    (cherry picked from commit 231ad7d8660d674d95fc4e03361338ac573c1f9f)

--- ShellPkg/Application/Shell/ShellParametersProtocol.c
+++ ShellPkg/Application/Shell/ShellParametersProtocol.c
@@ -4,7 +4,7 @@
 
   Copyright (C) 2014, Red Hat, Inc.
   (C) Copyright 2013 Hewlett-Packard Development Company, L.P.<BR>
-  Copyright (c) 2009 - 2015, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2009 - 2016, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
