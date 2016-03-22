36e8e6992d0cd43891e584b24c556e6abc62b6ec
Laszlo Ersek <lersek@redhat.com>
2016-03-22 17:37:36 +0100

    OvmfPkg/PlatformBdsLib: rebase to EfiEventGroupSignal
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>

--- OvmfPkg/Library/PlatformBdsLib/BdsPlatform.c
+++ OvmfPkg/Library/PlatformBdsLib/BdsPlatform.c
@@ -1164,27 +1164,6 @@ Returns:
 
 
 /**
-  Empty callback function executed when the EndOfDxe event group is signaled.
-
-  We only need this function because we'd like to signal EndOfDxe, and for that
-  we need to create an event, with a callback function.
-
-  @param[in] Event    Event whose notification function is being invoked.
-  @param[in] Context  The pointer to the notification function's context, which
-                      is implementation-dependent.
-**/
-STATIC
-VOID
-EFIAPI
-OnEndOfDxe (
-  IN EFI_EVENT Event,
-  IN VOID      *Context
-  )
-{
-}
-
-
-/**
   Save the S3 boot script.
 
   Note that we trigger DxeSmmReadyToLock here -- otherwise the script wouldn't
@@ -1259,7 +1238,6 @@ Returns:
 {
   EFI_STATUS                         Status;
   EFI_BOOT_MODE                      BootMode;
-  EFI_EVENT                          EndOfDxeEvent;
 
   DEBUG ((EFI_D_INFO, "PlatformBdsPolicyBehavior\n"));
 
@@ -1273,13 +1251,7 @@ Returns:
   // installed after PCI enumeration completes, we must not trigger the S3 save
   // earlier, hence we can't signal End-of-Dxe earlier.
   //
-  Status = gBS->CreateEventEx (EVT_NOTIFY_SIGNAL, TPL_CALLBACK, OnEndOfDxe,
-                  NULL /* NotifyContext */, &gEfiEndOfDxeEventGroupGuid,
-                  &EndOfDxeEvent);
-  if (!EFI_ERROR (Status)) {
-    gBS->SignalEvent (EndOfDxeEvent);
-    gBS->CloseEvent (EndOfDxeEvent);
-  }
+  EfiEventGroupSignal (&gEfiEndOfDxeEventGroupGuid);
 
   if (QemuFwCfgS3Enabled ()) {
     //
