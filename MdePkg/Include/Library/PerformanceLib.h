ef8d41707d2ce14b0eb294ec7b2cd96549215dd1
Liming Gao <liming.gao@intel.com>
2016-04-08 17:34:58 +0800

    MdePkg: Update PerformanceLib comments not to check Identifier.
    In PerformanceLib, Identifier is for single PERF, not the pair of PERF.
    When find the matched START and END pair, the identifier will not be checked.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Star Zeng <star.zeng@intel.com>

--- MdePkg/Include/Library/PerformanceLib.h
+++ MdePkg/Include/Library/PerformanceLib.h
@@ -1,7 +1,7 @@
 /** @file
   Provides services to log the execution times and retrieve them later.
 
-Copyright (c) 2006 - 2012, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2016, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -168,7 +168,7 @@ StartPerformanceMeasurementEx (
 /**
   Fills in the end time of a performance measurement.
 
-  Looks up the record that matches Handle, Token, Module and Identifier.
+  Looks up the record that matches Handle, Token and Module.
   If the record can not be found then return RETURN_NOT_FOUND.
   If the record is found and TimeStamp is not zero,
   then TimeStamp is added to the record as the end time.
