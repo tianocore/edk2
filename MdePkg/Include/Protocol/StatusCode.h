28c1711d4ffc82166e47fc9088c50d9a984acdfc
Liming Gao <liming.gao@intel.com>
2016-04-13 10:57:25 +0800

    MdePkg: Update StatusCode Runtime Protocol comments to follow PI spec 1.4a.
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>

--- MdePkg/Include/Protocol/StatusCode.h
+++ MdePkg/Include/Protocol/StatusCode.h
@@ -1,15 +1,7 @@
 /** @file
-  Status code Runtime Protocol as defined in PI Specification VOLUME 2 DXE
+  Status code Runtime Protocol as defined in PI Specification 1.4a VOLUME 2 DXE
 
-  The StatusCode () service is added to the EFI system table and the 
-  EFI_STATUS_CODE_ARCH_PROTOCOL_GUID protocol is registered with a NULL 
-  pointer.
-
-  No CRC of the EFI system table is required, since that is done in the DXE core.
-
-  This code abstracts Status Code reporting.
-
-  Copyright (c) 2006 - 2009, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2016, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials                          
   are licensed and made available under the terms and conditions of the BSD License         
   which accompanies this distribution.  The full text of the license may be found at        
@@ -56,8 +48,7 @@ EFI_STATUS
 
 ///
 /// Provides the service required to report a status code to the platform firmware.
-/// This protocol must be produced by a runtime DXE driver and may be consumed 
-/// only by the DXE Foundation.
+/// This protocol must be produced by a runtime DXE driver.
 ///
 typedef struct _EFI_STATUS_CODE_PROTOCOL {
   EFI_REPORT_STATUS_CODE         ReportStatusCode;
