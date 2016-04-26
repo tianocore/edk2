2c1c50fc61132517e352862b6748c57c5d443837
Nagaraj Hegde <nagaraj-p.hegde@hpe.com>
2016-04-26 11:46:45 +0800

    MdePkg:Http11.h: Add defines for "Expect" header
    Add #defines for "Expect" header, which is a part of RFC 2616
    and used for HTTP PUT/POST operations.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Hegde, Nagaraj P <nagaraj-p.hegde@hpe.com>
    Reviewed-by: Samer El-Haj-Mahmoud <elhaj@hpe.com>
    Reviewed-by: Jiaxin Wu <jiaxin.wu@intel.com>
    Reviewed-by: Fu Siyuan <siyuan.fu@intel.com>

--- MdePkg/Include/IndustryStandard/Http11.h
+++ MdePkg/Include/IndustryStandard/Http11.h
@@ -235,6 +235,20 @@
 ///
 #define  HTTP_HEADER_X_AUTH_TOKEN      "X-Auth-Token"
 
+///
+/// Expect Header
+/// The "Expect" header field in a request indicates a certain set of
+/// behaviors (expectations) that need to be supported by the server in
+/// order to properly handle this request. The only such expectation
+/// defined by this specification is 100-continue.
+///
+#define  HTTP_HEADER_EXPECT            "Expect"
+
+///
+/// Expect Header Value
+///
+#define  HTTP_EXPECT_100_CONTINUE       "100-continue"
+
 #pragma pack()
 
 #endif
