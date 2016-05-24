e5501ce2adff58de6036e5d9956b2da7e44327d6
Jeff Fan <jeff.fan@intel.com>
2016-05-24 20:21:04 +0800

    UefiCpuPkg/ExceptionLib: Add EXCEPTION_HANDLER_DATA definition
    Cc: Michael Kinney <michael.d.kinney@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Feng Tian <feng.tian@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Jeff Fan <jeff.fan@intel.com>
    Reviewed-by: Feng Tian <feng.tian@intel.com>
    (cherry picked from commit 2c5873fede073ae1d83a4cb830ee1bad7bea1a62)

--- UefiCpuPkg/Library/CpuExceptionHandlerLib/DxeException.c
+++ UefiCpuPkg/Library/CpuExceptionHandlerLib/DxeException.c
@@ -1,7 +1,7 @@
 /** @file
   CPU exception handler library implemenation for DXE modules.
 
-  Copyright (c) 2013, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2013 - 2016, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -21,6 +21,7 @@ CONST UINTN    mDoFarReturnFlag  = 0;
 
 extern SPIN_LOCK                   mDisplayMessageSpinLock;
 extern EFI_CPU_INTERRUPT_HANDLER   *mExternalInterruptHandler;
+EXCEPTION_HANDLER_DATA      mExceptionHandlerData;
 
 /**
   Initializes all CPU exceptions entries and provides the default exception handlers.
--- UefiCpuPkg/Library/CpuExceptionHandlerLib/SmmException.c
+++ UefiCpuPkg/Library/CpuExceptionHandlerLib/SmmException.c
@@ -1,7 +1,7 @@
 /** @file
   CPU exception handler library implemenation for SMM modules.
 
-  Copyright (c) 2013, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2013 - 2016, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -17,6 +17,7 @@
 
 CONST UINTN   mDoFarReturnFlag   = 1; 
 
+EXCEPTION_HANDLER_DATA      mExceptionHandlerData;
 /**
   Initializes all CPU exceptions entries and provides the default exception handlers.
   
