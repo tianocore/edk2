631c942726640615d53e4a358c078bb915e1bdd4
Liming Gao <liming.gao@intel.com>
2016-06-23 17:53:57 +0800

    PcAtChipsetPkg: Update ResetSystemLib with PCDs
    Update ResetSystemLib with PCDs for Reset Control Register and Value
    instead of hard code 0x64 and 0xFE.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Giri P Mudusuru <giri.p.mudusuru@intel.com>

--- PcAtChipsetPkg/Library/ResetSystemLib/ResetSystemLib.c
+++ PcAtChipsetPkg/Library/ResetSystemLib/ResetSystemLib.c
@@ -1,7 +1,7 @@
 /** @file
   Reset System Library functions for PCAT platforms
 
-  Copyright (c) 2006 - 2009, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2006 - 2016, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -32,7 +32,7 @@ ResetCold (
   VOID
   )
 {
-  IoWrite8 (0x64, 0xfe);
+  IoWrite8 ((UINTN) PcdGet64 (PcdResetControlRegister), PcdGet8 (PcdResetControlValueColdReset));
 }
 
 /**
@@ -48,7 +48,7 @@ ResetWarm (
   VOID
   )
 {
-  IoWrite8 (0x64, 0xfe);
+  IoWrite8 ((UINTN) PcdGet64 (PcdResetControlRegister), PcdGet8 (PcdResetControlValueColdReset));
 }
 
 /**
