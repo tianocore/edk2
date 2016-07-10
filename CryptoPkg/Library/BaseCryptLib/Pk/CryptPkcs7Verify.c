9dccc10708b0bf6ec8feaf18f4ed75fc082572bc
Hao Wu <hao.a.wu@intel.com>
2016-07-10 09:24:53 +0800

    CryptoPkg BaseCryptLib: Init the content of struct 'CertCtx' before use
    Some fields in structure 'CertCtx' might be used uninitialized in function
    Pkcs7GetCertificatesList().
    
    This commit makes sure that 'CertCtx' gets initialized before being used.
    
    Cc: Long Qin <qin.long@intel.com>
    Cc: Ye Ting <ting.ye@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Hao Wu <hao.a.wu@intel.com>
    Reviewed-by: Qin Long <qin.long@intel.com>
    Reviewed-by: Ye Ting <ting.ye@intel.com>
    (cherry picked from commit 07cae0659795b4d6b59f67eabb8426c7c7742318)

--- CryptoPkg/Library/BaseCryptLib/Pk/CryptPkcs7Verify.c
+++ CryptoPkg/Library/BaseCryptLib/Pk/CryptPkcs7Verify.c
@@ -10,7 +10,7 @@
   WrapPkcs7Data(), Pkcs7GetSigners(), Pkcs7Verify() will get UEFI Authenticated
   Variable and will do basic check for data structure.
 
-Copyright (c) 2009 - 2015, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2009 - 2016, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -488,6 +488,8 @@ Pkcs7GetCertificatesList (
   CertBuf        = NULL;
   OldBuf         = NULL;
   Signers        = NULL;
+
+  ZeroMem (&CertCtx, sizeof (CertCtx));
 
   //
   // Parameter Checking
