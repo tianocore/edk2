4b1f70e2d70f1cec5d7b806d7c70a8bfefde003c
Star Zeng <star.zeng@intel.com>
2016-07-28 10:49:50 +0800

    MdeModulePkg SmbiosDxe: Use definition in IndustryStandard/Smbios.h
    Cc: Feng Tian <feng.tian@intel.com>
    Cc: Amy Chan <amy.chan@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Star Zeng <star.zeng@intel.com>
    Reviewed-by: Giri P Mudusuru <giri.p.mudusuru@intel.com>
    Reviewed-by: Amy Chan <amy.chan@intel.com>

--- MdeModulePkg/Universal/SmbiosDxe/SmbiosDxe.c
+++ MdeModulePkg/Universal/SmbiosDxe/SmbiosDxe.c
@@ -2,7 +2,7 @@
   This code produces the Smbios protocol. It also responsible for constructing 
   SMBIOS table into system table.
   
-Copyright (c) 2009 - 2015, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2009 - 2016, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -1127,7 +1127,7 @@ SmbiosCreateTable (
   // Create End-Of-Table structure
   //
   GetMaxSmbiosHandle(SmbiosProtocol, &SmbiosHandle);
-  EndStructure.Header.Type = EFI_SMBIOS_TYPE_END_OF_TABLE;
+  EndStructure.Header.Type = SMBIOS_TYPE_END_OF_TABLE;
   EndStructure.Header.Length = (UINT8) sizeof (EFI_SMBIOS_TABLE_HEADER);
   EndStructure.Header.Handle = SmbiosHandle;
   EndStructure.Tailing[0] = 0;
@@ -1300,7 +1300,7 @@ SmbiosCreate64BitTable (
   // Create End-Of-Table structure
   //
   GetMaxSmbiosHandle(SmbiosProtocol, &SmbiosHandle);
-  EndStructure.Header.Type = EFI_SMBIOS_TYPE_END_OF_TABLE;
+  EndStructure.Header.Type = SMBIOS_TYPE_END_OF_TABLE;
   EndStructure.Header.Length = (UINT8) sizeof (EFI_SMBIOS_TABLE_HEADER);
   EndStructure.Header.Handle = SmbiosHandle;
   EndStructure.Tailing[0] = 0;
