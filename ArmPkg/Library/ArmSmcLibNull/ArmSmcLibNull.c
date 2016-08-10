5e32710023e2af6bf86aa946cafa3e0cfed16bbe
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2016-08-10 14:09:56 +0200

    ArmPkg/ArmSmcLibNull: move to generic C implementation
    The C language is powerful enough to implement a function that does
    absolutely nothing, so there is no need to resort to implementations
    in assembler for various toolchains/architectures.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>

---
+++ ArmPkg/Library/ArmSmcLibNull/ArmSmcLibNull.c
@@ -0,0 +1,22 @@
+//
+//  Copyright (c) 2016, Linaro Limited. All rights reserved.
+//
+//  This program and the accompanying materials
+//  are licensed and made available under the terms and conditions of the BSD License
+//  which accompanies this distribution.  The full text of the license may be found at
+//  http://opensource.org/licenses/bsd-license.php
+//
+//  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+//
+//
+
+#include <Base.h>
+#include <Library/ArmSmcLib.h>
+
+VOID
+ArmCallSmc (
+  IN OUT ARM_SMC_ARGS *Args
+  )
+{
+}
