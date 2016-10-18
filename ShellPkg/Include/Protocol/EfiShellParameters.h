acf634b1318f6306bad07816251d17ee657c2d61
Ruiyu Ni <ruiyu.ni@intel.com>
2016-10-18 16:27:48 +0800

    ShellPkg: Update header files to reference MdePkg protocol definition
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Ruiyu Ni <ruiyu.ni@intel.com>
    Reviewed-by: Jaben Carsey <jaben.carsey@intel.com>

---
+++ ShellPkg/Include/Protocol/EfiShellParameters.h
@@ -0,0 +1,20 @@
+/** @file
+  EFI Shell protocol as defined in the UEFI Shell 2.0 specification.
+
+  Copyright (c) 2006 - 2016, Intel Corporation. All rights reserved.<BR>
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef __EFI_SHELL_PARAMETERS_PROTOCOL_H__WRAPPER
+#define __EFI_SHELL_PARAMETERS_PROTOCOL_H__WRAPPER
+
+#include <Protocol/ShellParameters.h>
+
+#endif
