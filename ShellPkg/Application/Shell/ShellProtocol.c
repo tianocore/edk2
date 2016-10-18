09bd67f09573ad5a40875f3d7bfbf49c43756cf5
Laszlo Ersek <lersek@redhat.com>
2016-10-18 12:53:48 +0200

    ShellPkg/Shell: fix CopyGuid() arg order in EfiShellGetGuidFromName()
    The destination GUID comes first; from
    "MdePkg/Include/Library/BaseMemoryLib.h":
    
    > GUID *
    > EFIAPI
    > CopyGuid (
    >   OUT GUID       *DestinationGuid,
    >   IN CONST GUID  *SourceGuid
    >   );
    
    Here "NewGuid" is the GUID looked up by GetGuidFromStringName(), and
    "Guid" is where EfiShellGetGuidFromName() has to propagate that result to.
    
    Cc: Jaben Carsey <jaben.carsey@intel.com>
    Cc: Ruiyu Ni <ruiyu.ni@intel.com>
    Cc: Tim Lewis <tim.lewis@insyde.com>
    Reported-by: Tim Lewis <tim.lewis@insyde.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Tim Lewis <tim.lewis@insyde.com>
    Reviewed-by: Jaben Carsey <jaben.carsey@intel.com>

--- ShellPkg/Application/Shell/ShellProtocol.c
+++ ShellPkg/Application/Shell/ShellProtocol.c
@@ -2207,7 +2207,7 @@ EfiShellGetGuidFromName(
   Status = GetGuidFromStringName(GuidName, NULL, &NewGuid);
 
   if (!EFI_ERROR(Status)) {
-    CopyGuid(NewGuid, Guid);
+    CopyGuid(Guid, NewGuid);
   }
 
   return (Status);
