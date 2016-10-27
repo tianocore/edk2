1420143f014a4c14bd7cb430aaacd6b2c4bc8043
Eric Dong <eric.dong@intel.com>
2016-10-27 11:05:51 +0800

    MdePkg DevicePathLib: Rollback former change.
    Former patch still has some bugs, so rollback it and
    enhance the original code.
    
    Cc: Ruiyu NI <ruiyu.ni@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Eric Dong <eric.dong@intel.com>
    Reviewed-by: Jiewen Yao <jiewen.yao@intel.com>

--- MdePkg/Library/UefiDevicePathLibDevicePathProtocol/UefiDevicePathLib.c
+++ MdePkg/Library/UefiDevicePathLibDevicePathProtocol/UefiDevicePathLib.c
@@ -103,26 +103,18 @@ IsDevicePathValid (
 
   ASSERT (DevicePath != NULL);
 
-  if (MaxSize == 0){
-    MaxSize = MAX_UINTN;
-  }
-
-  Size = 0;
-  Count = 0;
-
-  while (MaxSize >= sizeof (EFI_DEVICE_PATH_PROTOCOL) &&
-        (MaxSize - sizeof (EFI_DEVICE_PATH_PROTOCOL) >= Size) &&
-        !IsDevicePathEnd (DevicePath)) {
+  for (Count = 0, Size = 0; !IsDevicePathEnd (DevicePath); DevicePath = NextDevicePathNode (DevicePath)) {
     NodeLength = DevicePathNodeLength (DevicePath);
     if (NodeLength < sizeof (EFI_DEVICE_PATH_PROTOCOL)) {
       return FALSE;
     }
 
-    if (NodeLength > MAX_UINTN - Size) {
-      return FALSE;
+    if (MaxSize > 0) {
+      Size += NodeLength;
+      if (Size + END_DEVICE_PATH_LENGTH > MaxSize) {
+        return FALSE;
+      }
     }
-
-    Size += NodeLength;
 
     if (PcdGet32 (PcdMaximumDevicePathNodeCount) > 0) {
       Count++;
@@ -130,8 +122,6 @@ IsDevicePathValid (
         return FALSE;
       }
     }
-
-    DevicePath = NextDevicePathNode (DevicePath);
   }
 
   //
