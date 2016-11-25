52e7754ed0a1edd5fa853fa52c37ec9fb1d06a5f
Dandan Bi <dandan.bi@intel.com>
2016-11-25 14:39:32 +0800

    MdeModulePkg/DriverSample: Remove the password related codes
    In current DriverSampleDxe, the sample code of password is
    not a good example, so we plan to remove it.
    
    Cc: Liming Gao <liming.gao@intel.com>
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Dandan Bi <dandan.bi@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>

--- MdeModulePkg/Universal/DriverSampleDxe/NVDataStruc.h
+++ MdeModulePkg/Universal/DriverSampleDxe/NVDataStruc.h
@@ -1,6 +1,6 @@
 /** @file
 
-Copyright (c) 2007 - 2011, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2007 - 2016, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -34,9 +34,7 @@ Revision History:
 
 #pragma pack(1)
 typedef struct {
-  UINT16  WhatIsThePassword2[20];
   UINT16  MyStringData[40];
-  UINT16  PasswordClearText[20];
   UINT16  SomethingHiddenForHtml;
   UINT8   HowOldAreYouInYearsManual;
   UINT16  HowTallAreYouManual;
