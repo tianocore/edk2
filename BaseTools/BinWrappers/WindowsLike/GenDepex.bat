83c6c3bfe293cd0a5983375dfbf46d4f364b38aa
Yonghong Zhu <yonghong.zhu@intel.com>
2016-12-19 10:52:20 +0800

    BaseTools: Correct bin wrappers for GenDepex.py path
    The WindowsLike and PosixLike bin wrappers for GenDepex.py do not use
    correct path. GenDepex.py is not in a GenDepex directory. Instead, it is
    in the AutoGen directory.
    
    Fixes: https://bugzilla.tianocore.org/show_bug.cgi?id=292
    Cc: Liming Gao <liming.gao@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Yonghong Zhu <yonghong.zhu@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>

--- BaseTools/BinWrappers/WindowsLike/GenDepex.bat
+++ BaseTools/BinWrappers/WindowsLike/GenDepex.bat
@@ -1,3 +1,3 @@
 @setlocal
 @set ToolName=%~n0%
-@%PYTHON_HOME%\python.exe %BASE_TOOLS_PATH%\Source\Python\%ToolName%\%ToolName%.py %*
+@%PYTHON_HOME%\python.exe %BASE_TOOLS_PATH%\Source\Python\AutoGen\%ToolName%.py %*
