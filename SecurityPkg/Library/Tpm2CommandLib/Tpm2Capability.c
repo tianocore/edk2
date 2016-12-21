d7ac6d64ca65c61728b05393d46ad12133fa7483
Zhang, Chao B <chao.b.zhang@intel.com>
2016-12-21 14:31:47 +0800

    SecurityPkg: Tcg2Smm: TPM2 Vendor specific HID
    Update TPM2 HID using vendor ManufacturerID & FirmwareVersion1.
    
    Cc: Yao Jiewen <jiewen.yao@intel.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Chao Zhang <chao.b.zhang@intel.com>
    Reviewed-by: Yao Jiewen <jiewen.yao@intel.com>
    Reviewed-by: Star Zeng <star.zeng@intel.com>

--- SecurityPkg/Library/Tpm2CommandLib/Tpm2Capability.c
+++ SecurityPkg/Library/Tpm2CommandLib/Tpm2Capability.c
@@ -1,7 +1,7 @@
 /** @file
   Implement TPM2 Capability related command.
 
-Copyright (c) 2013, Intel Corporation. All rights reserved. <BR>
+Copyright (c) 2013 - 2016, Intel Corporation. All rights reserved. <BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -189,7 +189,7 @@ Tpm2GetCapabilityManufactureID (
   if (EFI_ERROR (Status)) {
     return Status;
   }
-  *ManufactureId = SwapBytes32 (TpmCap.data.tpmProperties.tpmProperty->value);
+  *ManufactureId = TpmCap.data.tpmProperties.tpmProperty->value;
 
   return EFI_SUCCESS;
 }
