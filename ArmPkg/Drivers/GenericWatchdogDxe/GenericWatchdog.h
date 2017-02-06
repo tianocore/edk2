7c609a144b6636577dd60fbaa5fc64efeecd7baf
Alexei <Alexei.Fedorov@arm.com>
2017-02-06 19:05:54 +0000

    ArmPkg/GenericWatchdogDxe: Declare MMIO PCDs as UINT64
    PcdGenericWatchdogControlBase & PcdGenericWatchdogRefreshBase
    are declared as UINT32 values in ArmPkg.dec, but for platforms
    with addresses in the memory range above 4GB this causes build
    error F000: Too large PCD value for datum type [UINT32]
    of PCD gArmTokenSpaceGuid.PcdGenericWatchdogControlBase
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Alexei Fedorov <alexei.fedorov@arm.com>
    Signed-off-by: Evan Lloyd <evan.lloyd@arm.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=361
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>

--- ArmPkg/Drivers/GenericWatchdogDxe/GenericWatchdog.h
+++ ArmPkg/Drivers/GenericWatchdogDxe/GenericWatchdog.h
@@ -1,6 +1,6 @@
 /** @file
 *
-*  Copyright (c) 2013-2014, ARM Limited. All rights reserved.
+*  Copyright (c) 2013-2017, ARM Limited. All rights reserved.
 *
 *  This program and the accompanying materials
 *  are licensed and made available under the terms and conditions of the BSD
@@ -15,12 +15,12 @@
 #define __GENERIC_WATCHDOG_H__
 
 // Refresh Frame:
-#define GENERIC_WDOG_REFRESH_REG              ((UINT32)FixedPcdGet32 (PcdGenericWatchdogRefreshBase) + 0x000)
+#define GENERIC_WDOG_REFRESH_REG              ((UINTN)FixedPcdGet64 (PcdGenericWatchdogRefreshBase) + 0x000)
 
 // Control Frame:
-#define GENERIC_WDOG_CONTROL_STATUS_REG       ((UINT32)FixedPcdGet32 (PcdGenericWatchdogControlBase) + 0x000)
-#define GENERIC_WDOG_OFFSET_REG               ((UINT32)FixedPcdGet32 (PcdGenericWatchdogControlBase) + 0x008)
-#define GENERIC_WDOG_COMPARE_VALUE_REG        ((UINT32)FixedPcdGet32 (PcdGenericWatchdogControlBase) + 0x010)
+#define GENERIC_WDOG_CONTROL_STATUS_REG       ((UINTN)FixedPcdGet64 (PcdGenericWatchdogControlBase) + 0x000)
+#define GENERIC_WDOG_OFFSET_REG               ((UINTN)FixedPcdGet64 (PcdGenericWatchdogControlBase) + 0x008)
+#define GENERIC_WDOG_COMPARE_VALUE_REG        ((UINTN)FixedPcdGet64 (PcdGenericWatchdogControlBase) + 0x010)
 
 // Values of bit 0 of the Control/Status Register
 #define GENERIC_WDOG_ENABLED          1
