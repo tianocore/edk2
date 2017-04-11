ddbe62d6aec809f8049c5925e555db2f501b798b
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2017-04-11 10:12:50 +0100

    ArmPlatformPkg/EblCmdLib: remove dependency on deprecated ARM BdsLib
    The EBL alternative shell depends indirectly on the deprecated ARM
    BdsLib via EblCmdLib, which only uses a single helper function that
    can easily be cloned. So clone it, and remove the dependency.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>

--- ArmPlatformPkg/Library/EblCmdLib/EblCmdLib.c
+++ ArmPlatformPkg/Library/EblCmdLib/EblCmdLib.c
@@ -50,6 +50,53 @@ EblDumpFdt (
   );
 
 /**
+  Connect all DXE drivers
+
+  @retval EFI_SUCCESS           All drivers have been connected
+  @retval EFI_NOT_FOUND         No handles match the search.
+  @retval EFI_OUT_OF_RESOURCES  There is not resource pool memory to store the matching results.
+
+**/
+STATIC
+EFI_STATUS
+ConnectAllDrivers (
+  VOID
+  )
+{
+  UINTN                     HandleCount, Index;
+  EFI_HANDLE                *HandleBuffer;
+  EFI_STATUS                Status;
+
+  do {
+    // Locate all the driver handles
+    Status = gBS->LocateHandleBuffer (
+                AllHandles,
+                NULL,
+                NULL,
+                &HandleCount,
+                &HandleBuffer
+                );
+    if (EFI_ERROR (Status)) {
+      break;
+    }
+
+    // Connect every handles
+    for (Index = 0; Index < HandleCount; Index++) {
+      gBS->ConnectController (HandleBuffer[Index], NULL, NULL, TRUE);
+    }
+
+    if (HandleBuffer != NULL) {
+      FreePool (HandleBuffer);
+    }
+
+    // Check if new handles have been created after the start of the previous handles
+    Status = gDS->Dispatch ();
+  } while (!EFI_ERROR(Status));
+
+  return EFI_SUCCESS;
+}
+
+/**
   Simple arm disassembler via a library
 
   Argv[0] - symboltable
@@ -393,7 +440,7 @@ EblDevicePaths (
   EFI_DEVICE_PATH_PROTOCOL*          DevicePathProtocol;
   EFI_DEVICE_PATH_TO_TEXT_PROTOCOL*  DevicePathToTextProtocol;
 
-  BdsConnectAllDrivers();
+  ConnectAllDrivers();
 
   Status = gBS->LocateProtocol(&gEfiDevicePathToTextProtocolGuid, NULL, (VOID **)&DevicePathToTextProtocol);
   if (EFI_ERROR (Status)) {
