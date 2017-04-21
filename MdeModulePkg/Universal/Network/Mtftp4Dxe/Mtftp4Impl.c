7430771ec340941c11b6f867c0022ad1867e2b24
Jiaxin Wu <jiaxin.wu@intel.com>
2017-04-21 10:15:27 +0800

    MdeModulePkg/Mtftp4Dxe: Add invalid ServerIp check during MTFTP configuration
    Cc: Ye Ting <ting.ye@intel.com>
    Cc: Fu Siyuan <siyuan.fu@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Wu Jiaxin <jiaxin.wu@intel.com>
    Reviewed-by: Ye Ting <ting.ye@intel.com>
    Reviewed-by: Fu Siyuan <siyuan.fu@intel.com>
    (cherry picked from commit 17b25f520302f813a50f7876edc2e8fc901e7a7c)

--- MdeModulePkg/Universal/Network/Mtftp4Dxe/Mtftp4Impl.c
+++ MdeModulePkg/Universal/Network/Mtftp4Dxe/Mtftp4Impl.c
@@ -2,7 +2,7 @@
   Interface routine for Mtftp4.
   
 (C) Copyright 2014 Hewlett-Packard Development Company, L.P.<BR>
-Copyright (c) 2006 - 2016, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2017, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -666,6 +666,10 @@ EfiMtftp4Configure (
     Netmask  = NTOHL (Netmask);
     Gateway  = NTOHL (Gateway);
     ServerIp = NTOHL (ServerIp);
+
+    if (ServerIp == 0 || IP4_IS_LOCAL_BROADCAST (ServerIp)) {
+      return EFI_INVALID_PARAMETER;
+    }
 
     if (!ConfigData->UseDefaultSetting &&
         ((!IP4_IS_VALID_NETMASK (Netmask) || (Netmask != 0 && !NetIp4IsUnicast (Ip, Netmask))))) {
