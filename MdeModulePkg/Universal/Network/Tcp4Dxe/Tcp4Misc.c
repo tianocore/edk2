ca12a0c83b7b889fc807cb2dd47356f0fd1253d6
Fu Siyuan <siyuan.fu@intel.com>
2017-05-03 15:22:08 +0800

    MdeModulePkg: Addressing TCP Window Retraction when window scale factor is used.
    The RFC1323 which defines the TCP window scale option has been obsoleted by RFC7323.
    This patch is to follow the RFC7323 to address the TCP window retraction problem
    when a non-zero scale factor is used.
    The changes has been test in high packet loss rate network by using HTTP boot and
    iSCSI file read/write.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Fu Siyuan <siyuan.fu@intel.com>
    Reviewed-by: Wu Jiaxin <jiaxin.wu@intel.com>
    Reviewed-by: Ye Ting <ting.ye@intel.com>

--- MdeModulePkg/Universal/Network/Tcp4Dxe/Tcp4Misc.c
+++ MdeModulePkg/Universal/Network/Tcp4Dxe/Tcp4Misc.c
@@ -1,7 +1,7 @@
 /** @file
   Misc support routines for tcp.
 
-Copyright (c) 2005 - 2016, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2005 - 2017, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -78,7 +78,8 @@ TcpInitTcbLocal (
   // First window size is never scaled
   //
   Tcb->RcvWndScale  = 0;
-
+  Tcb->RetxmitSeqMax = 0;
+  
   Tcb->ProbeTimerOn = FALSE;
 }
 
