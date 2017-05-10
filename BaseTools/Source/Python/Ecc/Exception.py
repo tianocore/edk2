72d014cd1871ebbcfe75e6ff4050f5a9de283b34
Hess Chen <hesheng.chen@intel.com>
2017-05-10 09:29:50 +0800

    BaseTools/Ecc: Add line break support for exception list
    Add line break support for exception list.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Hess Chen <hesheng.chen@intel.com>
    Reviewed-by: Yonghong Zhu <yonghong.zhu@intel.com>

--- BaseTools/Source/Python/Ecc/Exception.py
+++ BaseTools/Source/Python/Ecc/Exception.py
@@ -1,7 +1,7 @@
 ## @file
 # This file is used to parse exception items found by ECC tool
 #
-# Copyright (c) 2009 - 2014, Intel Corporation. All rights reserved.<BR>
+# Copyright (c) 2009 - 2017, Intel Corporation. All rights reserved.<BR>
 # This program and the accompanying materials
 # are licensed and made available under the terms and conditions of the BSD License
 # which accompanies this distribution.  The full text of the license may be found at
@@ -72,7 +72,7 @@ class ExceptionCheck(object):
             self.ExceptionList = self.ExceptionListXml.ToList()
     
     def IsException(self, ErrorID, KeyWord, FileID=-1):
-        if (str(ErrorID), KeyWord) in self.ExceptionList:
+        if (str(ErrorID), KeyWord.replace('\r\n', '\n')) in self.ExceptionList:
             return True
         else:
             return False
