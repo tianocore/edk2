ac8d86234d0b2672d62079c35c9a8629c3dad987
Dandan Bi <dandan.bi@intel.com>
2017-05-18 09:51:42 +0800

    MdeModulePkg/PciHostBridgeDxe: Fix EBC build failure
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Dandan Bi <dandan.bi@intel.com>
    Reviewed-by: Jiewen Yao <jiewen.yao@intel.com>
    (cherry picked from commit 7a85e8474127ae6df47337a04797b2b443b57682)

--- MdeModulePkg/Bus/Pci/PciHostBridgeDxe/PciRootBridgeIo.c
+++ MdeModulePkg/Bus/Pci/PciHostBridgeDxe/PciRootBridgeIo.c
@@ -2,7 +2,7 @@
 
   PCI Root Bridge Io Protocol code.
 
-Copyright (c) 1999 - 2016, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 1999 - 2017, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -1085,7 +1085,7 @@ RootBridgeIoMap (
     }
     Status = mIoMmuProtocol->Map (
                                mIoMmuProtocol,
-                               Operation,
+                               (EDKII_IOMMU_OPERATION) Operation,
                                HostAddress,
                                NumberOfBytes,
                                DeviceAddress,
