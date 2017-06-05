1a0ad70ee190210084f1453b309f7c2035a8b0b3
Star Zeng <star.zeng@intel.com>
2017-06-05 10:50:43 +0800

    MdePkg SmmMemLib: Remove ASSERT in SmmIsBufferOutsideSmmValid
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=577
    
    Currently the SmmIsBufferOutsideSmmValid() function in SmmMemLib.c will
    ASSERT in certain conditions. Since this function is a "test" function,
    it should not be making decisions on how to handle a failure.
    Handling a failure should be left to the caller.
    
    This patch is to remove ASSERT(FALSE) at line 178 of SmmMemLib.c.
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Bret Barkelew <bret.barkelew@microsoft.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Star Zeng <star.zeng@intel.com>
    Reviewed-by: Jiewen Yao <jiewen.yao@intel.com>

--- MdePkg/Library/SmmMemLib/SmmMemLib.c
+++ MdePkg/Library/SmmMemLib/SmmMemLib.c
@@ -6,7 +6,7 @@
   all SMRAM range via SMM_ACCESS2_PROTOCOL, including the range for firmware (like SMM Core
   and SMM driver) and/or specific dedicated hardware.
 
-  Copyright (c) 2015 - 2016, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2015 - 2017, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -175,7 +175,6 @@ SmmIsBufferOutsideSmmValid (
         Buffer,
         Length
         ));
-      ASSERT (FALSE);
       return FALSE;
     }
   }
