ff5623e990782ad62c5f6943087ebafb17afe8ba
Ruiyu Ni <ruiyu.ni@intel.com>
2017-06-06 10:03:09 +0800

    MdePkg/DevicePath: Add BluetoothLe device path node support
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Ruiyu Ni <ruiyu.ni@intel.com>
    Reviewed-by: Hao A Wu <hao.a.wu@intel.com>

--- MdePkg/Include/IndustryStandard/Bluetooth.h
+++ MdePkg/Include/IndustryStandard/Bluetooth.h
@@ -2,7 +2,7 @@
   This file contains the Bluetooth definitions that are consumed by drivers.
   These definitions are from Bluetooth Core Specification Version 4.0 June, 2010
 
-  Copyright (c) 2015, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2015 - 2017, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -37,6 +37,21 @@ typedef struct {
   UINT16     MajorDeviceClass: 5;
   UINT16     MajorServiceClass:11;
 } BLUETOOTH_CLASS_OF_DEVICE;
+
+///
+/// BLUETOOTH_LE_ADDRESS
+///
+typedef struct {
+  ///
+  /// 48-bit Bluetooth device address
+  ///
+  UINT8      Address[6];
+  ///
+  /// 0x00 - Public Device Address
+  /// 0x01 - Random Device Address
+  ///
+  UINT8      Type;
+} BLUETOOTH_LE_ADDRESS;
 
 #pragma pack()
 
