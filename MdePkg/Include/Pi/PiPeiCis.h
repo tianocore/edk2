c867c8ce9f5c81f792152ce86a47295b12cbdc06
Star Zeng <star.zeng@intel.com>
2017-06-29 22:47:56 +0800

    MdePkg: Correct the comments of EFI_PEI_RESET2_SYSTEM
    Cc: Liming Gao <liming.gao@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Star Zeng <star.zeng@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>

--- MdePkg/Include/Pi/PiPeiCis.h
+++ MdePkg/Include/Pi/PiPeiCis.h
@@ -1,7 +1,7 @@
 /** @file
   PI PEI master include file. This file should match the PI spec.
 
-Copyright (c) 2006 - 2016, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2017, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials are licensed and made available under 
 the terms and conditions of the BSD License that accompanies this distribution.  
 The full text of the license may be found at
@@ -590,7 +590,7 @@ EFI_STATUS
 
   @param[in] ResetType      The type of reset to perform.
   @param[in] ResetStatus    The status code for the reset.
-  @param[in] DataSize       The size, in bytes, of WatchdogData.
+  @param[in] DataSize       The size, in bytes, of ResetData.
   @param[in] ResetData      For a ResetType of EfiResetCold, EfiResetWarm, or EfiResetShutdown
                             the data buffer starts with a Null-terminated string, optionally
                             followed by additional binary data. The string is a description
