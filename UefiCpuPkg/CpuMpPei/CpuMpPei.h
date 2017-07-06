e3ae7f52738667c7540486d91eff47efd0906fee
Eric Dong <eric.dong@intel.com>
2017-07-06 09:25:19 +0800

    UefiCpuPkg CpuMpPei: Update return status to follow spec.
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Eric Dong <eric.dong@intel.com>
    Reviewed-by: Jeff Fan <jeff.fan@intel.com>

--- UefiCpuPkg/CpuMpPei/CpuMpPei.h
+++ UefiCpuPkg/CpuMpPei/CpuMpPei.h
@@ -271,7 +271,7 @@ PeiStartupThisAP (
   @retval EFI_UNSUPPORTED         Switching the BSP cannot be completed prior to this
                                   service returning.
   @retval EFI_UNSUPPORTED         Switching the BSP is not supported.
-  @retval EFI_SUCCESS             The calling processor is an AP.
+  @retval EFI_DEVICE_ERROR        The calling processor is an AP.
   @retval EFI_NOT_FOUND           The processor with the handle specified by
                                   ProcessorNumber does not exist.
   @retval EFI_INVALID_PARAMETER   ProcessorNumber specifies the current BSP or a disabled
