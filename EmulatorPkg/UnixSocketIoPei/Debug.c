d460e4be09d1f83de11cb97ab651afe53466b67e
Tlusty, Thomas <thomas.tlusty@intel.com>
2017-08-09 11:43:49 -0400

    EmulatorPkg: extend EmulatorPkg to provide RcSim legacy functionality
    Extend EmulatorPkg to provide some basic services required to provide
    equivalent functionality to legacy RcSim development environments.
    
    Change-Id: I5b982eaaef1df02b965b9de6919dbde6b85cd084
    (cherry picked from commit 57d48ba6fd230c899f7b7e34fb6381048231f770)
    (cherry picked from commit 90482566da352f9d2731ab09ade943d72ac65b30 on Override/UDK2018Official/Server branch)
    (cherry picked from commit 3a5c67dcbedc01fcb6fbbebf8086f6abd72d5278 on Override/UDK2018_BP1520/Server branch)

---
+++ EmulatorPkg/UnixSocketIoPei/Debug.c
@@ -0,0 +1,28 @@
+/*++
+  This file contains a 'Sample Driver' and is licensed as such
+  under the terms of your license agreement with Intel or your
+  vendor.  This file may be modified by the user, subject to
+  the additional terms of the license agreement
+
+  Module Description:
+
+  Debug implementation.
+
+  Copyright (c) 2016 Intel Corporation.  All rights reserved.
+  This software and associated documentation (if any) is furnished
+  under a license and may only be used or copied in accordance
+  with the terms of the license. Except as permitted by such
+  license, no part of this software or documentation may be
+  reproduced, stored in a retrieval system, or transmitted in any
+  form or by any means without the express written consent of
+  Intel Corporation.
+
+**/
+
+#include "UnixSocketIoPei.h"
+
+//
+// Debug functions.
+//
+
+
