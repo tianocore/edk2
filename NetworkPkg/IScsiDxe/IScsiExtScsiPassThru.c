39db198dae01b3400cc0829120672d9f49bce413
Liming Gao <liming.gao@intel.com>
2017-11-28 11:19:23 +0800

    NetworkPkg: Update IScsiDxe to pass XCODE build
    Fix the warning equality comparison with extraneous parentheses
    [-Werror,-Wparentheses-equality].
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Liang Vincent <vincent.liang@intel.com>
    Reviewed-by: Jiaxin Wu <jiaxin.wu@intel.com>
    (cherry picked from commit cf260a47bec5db03cab77378da008338c106a9c6)

--- NetworkPkg/IScsiDxe/IScsiExtScsiPassThru.c
+++ NetworkPkg/IScsiDxe/IScsiExtScsiPassThru.c
@@ -1,7 +1,7 @@
 /** @file
   The implementation of EFI_EXT_SCSI_PASS_THRU_PROTOCOL.
 
-Copyright (c) 2004 - 2015, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2017, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -219,7 +219,7 @@ IScsiExtScsiPassThruBuildDevicePath (
   EFI_DEV_PATH                  *Node;
   UINTN                         DevPathNodeLen;
 
-  if ((DevicePath == NULL)) {
+  if (DevicePath == NULL) {
     return EFI_INVALID_PARAMETER;
   }
 
