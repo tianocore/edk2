9edcd2788d7baa767b0761f0917c014612b6d72a
Hao Wu <hao.a.wu@intel.com>
2017-11-30 16:40:42 +0800

    BaseTools/VfrCompile: Assign 'NULL' for closed file handle
    Assign 'NULL' value to the already-closed file handle to avoid closing
    the handle multiple times.
    
    Cc: Yonghong Zhu <yonghong.zhu@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Hao Wu <hao.a.wu@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>

--- BaseTools/Source/C/VfrCompile/VfrCompiler.cpp
+++ BaseTools/Source/C/VfrCompile/VfrCompiler.cpp
@@ -2,7 +2,7 @@
   
   VfrCompiler main class and main function.
 
-Copyright (c) 2004 - 2016, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2017, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials                          
 are licensed and made available under the terms and conditions of the BSD License         
 which accompanies this distribution.  The full text of the license may be found at        
@@ -703,6 +703,7 @@ CVfrCompiler::Compile (
   }
 
   fclose (pInFile);
+  pInFile = NULL;
 
   if (gCFormPkg.HavePendingUnassigned () == TRUE) {
     gCFormPkg.PendingAssignPrintAll ();
