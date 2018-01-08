2e94e412977900157296cdbd01b2d5135a66ba35
Ruiyu Ni <ruiyu.ni@intel.com>
2018-01-08 15:51:00 +0800

    MdeModulePkg/PciBusDxe: cope with HPCs that request no bus nr padding
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Ruiyu Ni <ruiyu.ni@intel.com>

--- MdeModulePkg/Bus/Pci/PciBusDxe/PciLib.c
+++ MdeModulePkg/Bus/Pci/PciBusDxe/PciLib.c
@@ -1,7 +1,7 @@
 /** @file
   Internal library implementation for PCI Bus module.
 
-Copyright (c) 2006 - 2017, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2018, Intel Corporation. All rights reserved.<BR>
 (C) Copyright 2015 Hewlett Packard Enterprise Development LP<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
@@ -1154,11 +1154,20 @@ PciScanBus (
 
               FreePool (Descriptors);
 
-              if (EFI_ERROR (Status)) {
-                return Status;
-              }
+              switch (Status) {
+                case EFI_SUCCESS:
+                  BusPadding = TRUE;
+                  break;
 
-              BusPadding = TRUE;
+                case EFI_NOT_FOUND:
+                  //
+                  // no bus number padding requested
+                  //
+                  break;
+
+                default:
+                  return Status;
+              }
             }
           }
         }
