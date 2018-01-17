8bb7fa235f13f3e74b2d967dde6a621724e8c076
Star Zeng <star.zeng@intel.com>
2018-01-17 11:10:57 +0800

    IntelSiliconPkg IntelVTdPmrPei: Get high top by host address width
    Get high top by host address width instead of resource HOB.
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Star Zeng <star.zeng@intel.com>
    Reviewed-by: Jiewen Yao <jiewen.yao@intel.com>
    (cherry picked from commit b2725f57c7a1e6feeb176f1563a4f1a8c2eb6c6f)

--- IntelSiliconPkg/Feature/VTd/IntelVTdPmrPei/IntelVTdPmrPei.h
+++ IntelSiliconPkg/Feature/VTd/IntelVTdPmrPei/IntelVTdPmrPei.h
@@ -1,7 +1,7 @@
 /** @file
   The definition for DMA access Library.
 
-  Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2017 - 2018, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -157,16 +157,6 @@ ParseDmarAcpiTableRmrr (
 VOID
 DumpAcpiDMAR (
   IN EFI_ACPI_DMAR_HEADER  *Dmar
-  );
-
-/**
-  Get the highest memory.
-
-  @return the highest memory.
-**/
-UINT64
-GetTopMemory (
-  VOID
   );
 
 extern EFI_GUID mVTdInfoGuid;
