c1ec9d01949d3d76ab06de69ca44f63980860158
Star Zeng <star.zeng@intel.com>
2018-02-02 15:34:48 +0800

    SourceLevelDebugPkg DebugAgentLib: Rename IsBsp to DebugAgentIsBsp
    For avoiding function name confliction,
    rename IsBsp to DebugAgentIsBsp.
    
    Cc: Ruiyu Ni <ruiyu.ni@intel.com>
    Cc: Hao Wu <hao.a.wu@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Star Zeng <star.zeng@intel.com>
    Reviewed-by: Hao Wu <hao.a.wu@intel.com>
    Reviewed-by: Ruiyu Ni <ruiyu.ni@intel.com>
    (cherry picked from commit a2acb04ca67853519a514a986de11b6bc468d564)

--- SourceLevelDebugPkg/Library/DebugAgent/DebugAgentCommon/DebugMp.h
+++ SourceLevelDebugPkg/Library/DebugAgent/DebugAgentCommon/DebugMp.h
@@ -1,7 +1,7 @@
 /** @file
   Header file for Multi-Processor support.
 
-  Copyright (c) 2010 - 2013, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2010 - 2018, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -96,7 +96,7 @@ ReleaseMpSpinLock (
 
 **/
 BOOLEAN
-IsBsp (
+DebugAgentIsBsp (
   IN UINT32             ProcessorIndex
   );
 
