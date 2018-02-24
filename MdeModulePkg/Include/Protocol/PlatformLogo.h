342865cd61b0326bebdf7e10811a315ac007f451
Ruiyu Ni <ruiyu.ni@intel.com>
2018-02-24 15:46:52 +0800

    MdeModulePkg/PlatformLogo.h: Correct function header comments
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Ruiyu Ni <ruiyu.ni@intel.com>
    Reviewed-by: Star Zeng <star.zeng@intel.com>
    (cherry picked from commit de707b08668bb86c6d873239a18e5de652f13ea6)

--- MdeModulePkg/Include/Protocol/PlatformLogo.h
+++ MdeModulePkg/Include/Protocol/PlatformLogo.h
@@ -2,7 +2,7 @@
   The Platform Logo Protocol defines the interface to get the Platform logo
   image with the display attribute.
 
-Copyright (c) 2015 - 2016, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2015 - 2018, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials are licensed and made available under 
 the terms and conditions of the BSD License that accompanies this distribution.  
 The full text of the license may be found at
@@ -39,22 +39,17 @@ typedef enum {
 } EDKII_PLATFORM_LOGO_DISPLAY_ATTRIBUTE;
 
 /**
-
   Load a platform logo image and return its data and attributes.
 
   @param This              The pointer to this protocol instance.
   @param Instance          The visible image instance is found.
-  @param Format            The format of the image. Examples: BMP, JPEG.
-  @param ImageData         The image data for the badge file. Currently only 
-                           supports the .bmp file format. 
-  @param ImageSize         The size of the image returned.
+  @param Image             Points to the image.
   @param Attribute         The display attributes of the image returned.
   @param OffsetX           The X offset of the image regarding the Attribute.
   @param OffsetY           The Y offset of the image regarding the Attribute.
 
   @retval EFI_SUCCESS      The image was fetched successfully.
   @retval EFI_NOT_FOUND    The specified image could not be found.
-
 **/
 typedef
 EFI_STATUS
