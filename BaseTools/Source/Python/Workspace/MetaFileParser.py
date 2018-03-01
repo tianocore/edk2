1fa7fdf6eada55316d7b5b45a0d116625d697fc5
Feng, YunhuaX </o=Intel/ou=Exchange Administrative Group (FYDIBOHF23SPDLT)/cn=Recipients/cn=Feng, YunhuaX4e1>
2018-03-01 20:42:01 +0800

    BaseTools: Dsc/Fdf conditional statement parse issue
    Set PCD value with --pcd argument not replace DSC/Fdf PCD value.
    
    Cc: Liming Gao <liming.gao@intel.com>
    Cc: Yonghong Zhu <yonghong.zhu@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Yunhua Feng <yunhuax.feng@intel.com>
    Reviewed-by: Yonghong Zhu <yonghong.zhu@intel.com>

--- BaseTools/Source/Python/Workspace/MetaFileParser.py
+++ BaseTools/Source/Python/Workspace/MetaFileParser.py
@@ -1243,6 +1243,10 @@ class DscParser(MetaFileParser):
         # PCD cannot be referenced in macro definition
         if self._ItemType not in [MODEL_META_DATA_DEFINE, MODEL_META_DATA_GLOBAL_DEFINE]:
             Macros.update(self._Symbols)
+        if GlobalData.BuildOptionPcd:
+            for Item in GlobalData.BuildOptionPcd:
+                PcdName, TmpValue = Item.split("=")
+                Macros[PcdName.strip()] = TmpValue
         return Macros
 
     def _PostProcess(self):
