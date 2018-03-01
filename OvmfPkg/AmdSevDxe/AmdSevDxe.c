c16d4e35d1d68d78178c96ad46d5e55ff3a44332
Laszlo Ersek <lersek@redhat.com>
2018-03-01 14:32:23 +0100

    OvmfPkg/AmdSevDxe: rewrap to 79 characters width
    There are many overlong lines; it's hard to work with the module like
    this. Rewrap all files to 79 columns.
    
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Brijesh Singh <brijesh.singh@amd.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Tested-by: Brijesh Singh <brijesh.singh@amd.com>
    Reviewed-by: Brijesh Singh <brijesh.singh@amd.com>

--- OvmfPkg/AmdSevDxe/AmdSevDxe.c
+++ OvmfPkg/AmdSevDxe/AmdSevDxe.c
@@ -1,18 +1,18 @@
 /** @file
 
   AMD Sev Dxe driver. This driver is dispatched early in DXE, due to being list
-  in APRIORI. It clears C-bit from MMIO and NonExistent Memory space when SEV is
-  enabled.
+  in APRIORI. It clears C-bit from MMIO and NonExistent Memory space when SEV
+  is enabled.
 
   Copyright (c) 2017, AMD Inc. All rights reserved.<BR>
 
-  This program and the accompanying materials
-  are licensed and made available under the terms and conditions of the BSD
-  License which accompanies this distribution.  The full text of the license may
-  be found at http://opensource.org/licenses/bsd-license.php
+  This program and the accompanying materials are licensed and made available
+  under the terms and conditions of the BSD License which accompanies this
+  distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
 
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
+  WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
@@ -47,8 +47,8 @@ AmdSevDxeEntryPoint (
 
   //
   // Iterate through the GCD map and clear the C-bit from MMIO and NonExistent
-  // memory space. The NonExistent memory space will be used for mapping the MMIO
-  // space added later (eg PciRootBridge). By clearing both known MMIO and
+  // memory space. The NonExistent memory space will be used for mapping the
+  // MMIO space added later (eg PciRootBridge). By clearing both known MMIO and
   // NonExistent memory space can gurantee that current and furture MMIO adds
   // will have C-bit cleared.
   //
@@ -60,10 +60,12 @@ AmdSevDxeEntryPoint (
       Desc = &AllDescMap[Index];
       if (Desc->GcdMemoryType == EfiGcdMemoryTypeMemoryMappedIo ||
           Desc->GcdMemoryType == EfiGcdMemoryTypeNonExistent) {
-        Status = MemEncryptSevClearPageEncMask (0,
-                                                Desc->BaseAddress,
-                                                EFI_SIZE_TO_PAGES(Desc->Length),
-                                                FALSE);
+        Status = MemEncryptSevClearPageEncMask (
+                   0,
+                   Desc->BaseAddress,
+                   EFI_SIZE_TO_PAGES (Desc->Length),
+                   FALSE
+                   );
         ASSERT_EFI_ERROR (Status);
       }
     }
