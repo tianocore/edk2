cec7b8fb86cfe2e9a1930aee44af171ed7e5385e
Kinney, Michael D <michael.d.kinney@intel.com>
2018-03-14 15:05:29 -0700

    FmpDevicePkg: Add package, library classes, and PCDs
    https://bugzilla.tianocore.org/show_bug.cgi?id=922
    
    Based on content from the following branch:
    
    https://github.com/Microsoft/MS_UEFI/tree/share/MsCapsuleSupport/MsCapsuleUpdatePkg
    
    Create FmpDevicePkg with library classes and PCDs used to
    customize the behavior of a Firmware Management Protocol
    instance.
    
    Library Classes
    ===============
    * FmpDeviceLib - Provides firmware device specific services
      to support updates of a firmware image stored in a firmware
      device.
    * CapsuleUpdatePolicyLib - Provides platform policy services
      used during a capsule update.
    * FmpPayloadHeaderLib - Provides services to retrieve values
      from a capsule's FMP Payload Header.  The structure is not
      included in the library class.  Instead, services are
      provided to retrieve information from the FMP Payload Header.
      If information is added to the FMP Payload Header, then new
      services may be added to this library class to retrieve the
      new information.
    
    PCDs set per module
    ====================
    * PcdFmpDeviceSystemResetRequired - Indicates if a full
      system reset is required before a firmware update to a
      firmware devices takes effect
    * PcdFmpDeviceTestKeySha256Digest - The SHA-256 hash of a
      PKCS7 test key that is used to detect if a test key is
      being used to authenticate capsules.  Test key detection
      is disabled by setting the value to {0}.
    * PcdFmpDeviceProgressColor - The color of the progress bar
      during a firmware update.
    * PcdFmpDeviceImageIdName - The Null-terminated Unicode
      string used to fill in the ImageIdName field of the
      EFI_FIRMWARE_IMAGE_DESCRIPTOR structure that is returned
      by the GetImageInfo() service of the Firmware Management
      Protocol for the firmware device.
    * PcdFmpDeviceBuildTimeLowestSupportedVersion - The build
      time value used to fill in the LowestSupportedVersion field
      of the EFI_FIRMWARE_IMAGE_DESCRIPTOR structure that is
      returned by the GetImageInfo() service of the Firmware
      Management Protocol.
    * PcdFmpDeviceProgressWatchdogTimeInSeconds - The time in
      seconds to arm a watchdog timer during the update of a
      firmware device.
    
    PCDs set per module or for entire platform
    ==========================================
    * PcdFmpDevicePkcs7CertBufferXdr - One or more PKCS7
      certificates used to verify a firmware device capsule
      update image.
    * PcdFmpDeviceLockEventGuid - An event GUID that locks
      the firmware device when the event is signaled.
    
    Cc: Sean Brogan <sean.brogan@microsoft.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Star Zeng <star.zeng@intel.com>

---
+++ FmpDevicePkg/Include/Library/CapsuleUpdatePolicyLib.h
@@ -0,0 +1,120 @@
+/**  @file
+  Provides platform policy services used during a capsule update.
+
+  Copyright (c) 2016, Microsoft Corporation. All rights reserved.<BR>
+  Copyright (c) 2018, Intel Corporation. All rights reserved.<BR>
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+  1. Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+**/
+
+#ifndef __CAPSULE_UPDATE_POLICY_LIB__
+#define __CAPSULE_UPDATE_POLICY_LIB__
+
+/**
+  Determine if the system power state supports a capsule update.
+
+  @param[out] Good  Returns TRUE if system power state supports a capsule
+                    update.  Returns FALSE if system power state does not
+                    support a capsule update.  Return value is only valid if
+                    return status is EFI_SUCCESS.
+
+  @retval EFI_SUCCESS            Good parameter has been updated with result.
+  @retval EFI_INVALID_PARAMETER  Good is NULL.
+  @retval EFI_DEVICE_ERROR       System power state can not be determined.
+
+**/
+EFI_STATUS
+EFIAPI
+CheckSystemPower (
+  OUT BOOLEAN  *Good
+  );
+
+/**
+  Determines if the system thermal state supports a capsule update.
+
+  @param[out] Good  Returns TRUE if system thermal state supports a capsule
+                    update.  Returns FALSE if system thermal state does not
+                    support a capsule update.  Return value is only valid if
+                    return status is EFI_SUCCESS.
+
+  @retval EFI_SUCCESS            Good parameter has been updated with result.
+  @retval EFI_INVALID_PARAMETER  Good is NULL.
+  @retval EFI_DEVICE_ERROR       System thermal state can not be determined.
+
+**/
+EFI_STATUS
+EFIAPI
+CheckSystemThermal (
+  IN OUT BOOLEAN  *Good
+  );
+
+/**
+  Determines if the system environment state supports a capsule update.
+
+  @param[out] Good  Returns TRUE if system environment state supports a capsule
+                    update.  Returns FALSE if system environment state does not
+                    support a capsule update.  Return value is only valid if
+                    return status is EFI_SUCCESS.
+
+  @retval EFI_SUCCESS            Good parameter has been updated with result.
+  @retval EFI_INVALID_PARAMETER  Good is NULL.
+  @retval EFI_DEVICE_ERROR       System environment state can not be determined.
+
+**/
+EFI_STATUS
+EFIAPI
+CheckSystemEnvironment (
+  IN OUT BOOLEAN  *Good
+  );
+
+/**
+  Determines if the Lowest Supported Version checks should be performed.  The
+  expected result from this function is TRUE.  A platform can choose to return
+  FALSE (e.g. during manufacturing or servicing) to allow a capsule update to a
+  version below the current Lowest Supported Version.
+
+  @retval TRUE   The lowest supported version check is required.
+  @retval FALSE  Do not perform lowest support version check.
+
+**/
+BOOLEAN
+EFIAPI
+IsLowestSupportedVersionCheckRequired (
+  VOID
+  );
+
+/**
+  Determines if the FMP device should be locked when the event specified by
+  PcdFmpDeviceLockEventGuid is signaled. The expected result from this function
+  is TRUE so the FMP device is always locked.  A platform can choose to return
+  FALSE (e.g. during manufacturing) to allow FMP devices to remain unlocked.
+
+  @retval TRUE   The FMP device lock action is required at lock event guid.
+  @retval FALSE  Do not perform FMP device lock at lock event guid.
+
+**/
+BOOLEAN
+EFIAPI
+IsLockFmpDeviceAtLockEventGuidRequired (
+  VOID
+  );
+
+#endif
