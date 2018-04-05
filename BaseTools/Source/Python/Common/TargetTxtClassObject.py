eb2c0b2c80fc684f7ff230daf99b9f0d2fb77d4a
Carsey, Jaben </o=Intel/ou=Americas01/cn=Workers/cn=Carsey, Jaben>
2018-04-06 07:13:50 +0800

    BaseTools: remove uncalled functions
    Cc: Liming Gao <liming.gao@intel.com>
    Cc: Yonghong Zhu <yonghong.zhu@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Jaben Carsey <jaben.carsey@intel.com>
    Reviewed-by: Yonghong Zhu <yonghong.zhu@intel.com>

--- BaseTools/Source/Python/Common/TargetTxtClassObject.py
+++ BaseTools/Source/Python/Common/TargetTxtClassObject.py
@@ -137,33 +137,6 @@ class TargetTxtClassObject(object):
         F.close()
         return 0
 
-    ## Print the dictionary
-    #
-    # Print all items of dictionary one by one
-    #
-    # @param Dict:  The dictionary to be printed
-    #
-    def printDict(Dict):
-        if Dict is not None:
-            KeyList = Dict.keys()
-            for Key in KeyList:
-                if Dict[Key] != '':
-                    print Key + ' = ' + str(Dict[Key])
-
-    ## Print the dictionary
-    #
-    # Print the items of dictionary which matched with input key
-    #
-    # @param list:  The dictionary to be printed
-    # @param key:   The key of the item to be printed
-    #
-    def printList(Key, List):
-        if type(List) == type([]):
-            if len(List) > 0:
-                if Key.find(TAB_SPLIT) != -1:
-                    print "\n" + Key
-                    for Item in List:
-                        print Item
 ## TargetTxtDict
 #
 # Load target.txt in input Conf dir
