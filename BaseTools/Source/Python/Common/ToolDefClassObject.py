ce3082a6e799ebcd621c8983ee4a8b6d6ca9b472
Carsey, Jaben </o=Intel/ou=Americas01/cn=Workers/cn=Carsey, Jaben>
2018-04-06 07:13:49 +0800

    BaseTools: remove unused file
    ToolsDefClassObject didnt need Dictionary, it needed an import from there.
    
    Cc: Liming Gao <liming.gao@intel.com>
    Cc: Yonghong Zhu <yonghong.zhu@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Jaben Carsey <jaben.carsey@intel.com>
    Reviewed-by: Yonghong Zhu <yonghong.zhu@intel.com>

--- BaseTools/Source/Python/Common/ToolDefClassObject.py
+++ BaseTools/Source/Python/Common/ToolDefClassObject.py
@@ -1,7 +1,7 @@
 ## @file
 # This file is used to define each component of tools_def.txt file
 #
-# Copyright (c) 2007 - 2016, Intel Corporation. All rights reserved.<BR>
+# Copyright (c) 2007 - 2018, Intel Corporation. All rights reserved.<BR>
 # This program and the accompanying materials
 # are licensed and made available under the terms and conditions of the BSD License
 # which accompanies this distribution.  The full text of the license may be found at
@@ -18,7 +18,6 @@ import Common.LongFilePathOs as os
 import re
 import EdkLogger
 
-from Dictionary import *
 from BuildToolError import *
 from TargetTxtClassObject import *
 from Common.LongFilePathSupport import OpenLongFilePath as open
@@ -27,6 +26,10 @@ from Common.String import NormPath
 import Common.GlobalData as GlobalData
 from Common import GlobalData
 from Common.MultipleWorkspace import MultipleWorkspace as mws
+from DataType import TAB_TOD_DEFINES_TARGET,TAB_TOD_DEFINES_TOOL_CHAIN_TAG,\
+                     TAB_TOD_DEFINES_TARGET_ARCH,TAB_TOD_DEFINES_COMMAND_TYPE\
+                     ,TAB_TOD_DEFINES_FAMILY,TAB_TOD_DEFINES_BUILDRULEFAMILY
+
 
 ##
 # Static variables used for pattern
