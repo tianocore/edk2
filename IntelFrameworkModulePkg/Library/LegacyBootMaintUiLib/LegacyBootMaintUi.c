a82385958e6e5ffe8151ad71f0a47cb06e2d5b20
Thomas Palmer <thomas.palmer@hpe.com>
2018-04-19 04:31:35 +0800

    IntelFrameworkModulePkg/LegacyBootMaintUiLib: Update RouteConfig function
    According to UEFI spec, the RouteConfig protocol function should populate
    the Progress pointer with an address inside Configuration.  This patch
    ensures that these functions are compliant when EFI_NOT_FOUND is returned.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Thomas Palmer <thomas.palmer@hpe.com>
    Reviewed-by: Dandan Bi <dandan.bi@intel.com>
    Reviewed-by: Eric Dong <eric.dong@intel.com>

--- IntelFrameworkModulePkg/Library/LegacyBootMaintUiLib/LegacyBootMaintUi.c
+++ IntelFrameworkModulePkg/Library/LegacyBootMaintUiLib/LegacyBootMaintUi.c
@@ -2,6 +2,7 @@
   Legacy Boot Maintainence UI implementation.
 
 Copyright (c) 2004 - 2016, Intel Corporation. All rights reserved.<BR>
+(C) Copyright 2018 Hewlett Packard Enterprise Development LP<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -562,6 +563,8 @@ LegacyBootOptionRouteConfig (
   if (Configuration == NULL || Progress == NULL) {
     return EFI_INVALID_PARAMETER;
   }
+
+  *Progress = Configuration;
 
   //
   // Check routing data in <ConfigHdr>.
