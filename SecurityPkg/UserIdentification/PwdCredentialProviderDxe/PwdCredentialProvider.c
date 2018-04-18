b59ac35e7ac6bb6e46d73beae63dbfd5cb1d6d29
Thomas Palmer <thomas.palmer@hpe.com>
2018-04-19 04:33:12 +0800

    SecurityPkg/PwdCredentialProviderDxe: Update RouteConfig function
    According to UEFI spec, the RouteConfig protocol function should populate
    the Progress pointer with an address inside Configuration.  This patch
    ensures that these functions are compliant when EFI_NOT_FOUND is returned.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Thomas Palmer <thomas.palmer@hpe.com>
    Reviewed-by: Dandan Bi <dandan.bi@intel.com>
    Reviewed-by: Eric Dong <eric.dong@intel.com>

--- SecurityPkg/UserIdentification/PwdCredentialProviderDxe/PwdCredentialProvider.c
+++ SecurityPkg/UserIdentification/PwdCredentialProviderDxe/PwdCredentialProvider.c
@@ -2,6 +2,7 @@
   Password Credential Provider driver implementation.
     
 Copyright (c) 2009 - 2014, Intel Corporation. All rights reserved.<BR>
+(C) Copyright 2018 Hewlett Packard Enterprise Development LP<BR>
 This program and the accompanying materials 
 are licensed and made available under the terms and conditions of the BSD License 
 which accompanies this distribution.  The full text of the license may be found at 
@@ -645,6 +646,8 @@ FakeRouteConfig (
   if (Configuration == NULL || Progress == NULL) {
     return EFI_INVALID_PARAMETER;
   }
+
+  *Progress = Configuration;
 
   return EFI_NOT_FOUND;
 }
