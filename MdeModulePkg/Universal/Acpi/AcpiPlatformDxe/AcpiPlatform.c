d9476a7ef1476621c14a7e632b8b8fc3ca419f61
Marvin.Haeuser@outlook.com <Marvin.Haeuser@outlook.com>
2018-05-07 17:08:06 +0800

    MdeModulePkg/AcpiPlatformDxe: Unload after execution.
    AcpiPlatformDxe solely performs one-time tasks and does not expose
    any services or create any events. Hence it can safely be unloaded
    after the Entry Point execution by returning an error code.
    
    V2: Return EFI_REQUEST_UNLOAD_IMAGE.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Marvin Haeuser <Marvin.Haeuser@outlook.com>
    Reviewed-by: Star Zeng <star.zeng@intel.com>

--- MdeModulePkg/Universal/Acpi/AcpiPlatformDxe/AcpiPlatform.c
+++ MdeModulePkg/Universal/Acpi/AcpiPlatformDxe/AcpiPlatform.c
@@ -1,7 +1,7 @@
 /** @file
   Sample ACPI Platform Driver
 
-  Copyright (c) 2008 - 2011, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2008 - 2018, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -255,6 +255,9 @@ AcpiPlatformEntryPoint (
     }
   }
 
-  return EFI_SUCCESS;
+  //
+  // The driver does not require to be kept loaded.
+  //
+  return EFI_REQUEST_UNLOAD_IMAGE;
 }
 
