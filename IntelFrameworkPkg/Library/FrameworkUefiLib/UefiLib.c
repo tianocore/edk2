2e3daaf68890a1ef31c24eaa5658202621bfd907
Liming Gao <liming.gao@intel.com>
2018-05-28 15:30:51 +0800

    IntelFrameworkPkg UefiLib: Use comparison logic to check UINTN parameter
    Commit cb96e7d4f7afdbaef0706f9251ae479639d85a28 changes the input parameter
    from BOOLEAN to UINTN. Its comparison logic should be updated.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Star Zeng <star.zeng@intel.com>

--- IntelFrameworkPkg/Library/FrameworkUefiLib/UefiLib.c
+++ IntelFrameworkPkg/Library/FrameworkUefiLib/UefiLib.c
@@ -1457,8 +1457,8 @@ GetEfiGlobalVariable2 (
   @param[in]  SupportedLanguages  A pointer to a Null-terminated ASCII string that
                                   contains a set of language codes in the format 
                                   specified by Iso639Language.
-  @param[in]  Iso639Language      If TRUE, then all language codes are assumed to be
-                                  in ISO 639-2 format.  If FALSE, then all language
+  @param[in]  Iso639Language      If not zero, then all language codes are assumed to be
+                                  in ISO 639-2 format.  If zero, then all language
                                   codes are assumed to be in RFC 4646 language format
   @param[in]  ...                 A variable argument list that contains pointers to 
                                   Null-terminated ASCII strings that contain one or more
@@ -1510,7 +1510,7 @@ GetBestLanguage (
     //
     // If in RFC 4646 mode, then determine the length of the first RFC 4646 language code in Language
     //
-    if (!Iso639Language) {
+    if (Iso639Language == 0) {
       for (LanguageLength = 0; Language[LanguageLength] != 0 && Language[LanguageLength] != ';'; LanguageLength++);
     }
 
@@ -1525,7 +1525,7 @@ GetBestLanguage (
         //
         // In RFC 4646 mode, then Loop through all language codes in SupportedLanguages
         //
-        if (!Iso639Language) {
+        if (Iso639Language == 0) {
           //
           // Skip ';' characters in Supported
           //
@@ -1557,7 +1557,7 @@ GetBestLanguage (
         }
       }
 
-      if (Iso639Language) {
+      if (Iso639Language != 0) {
         //
         // If ISO 639 mode, then each language can only be tested once
         //
