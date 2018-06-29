56ca900782d5d478e6e6389aca42b7ea0e0b3193
chenc2 <chen.a.chen@intel.com>
2018-06-29 11:36:02 +0800

    StdLib: Removing ipf which is no longer supported from edk2.
    Removing rules for Ipf sources file:
    * Remove the source file which path with "ipf" and also listed in
      [Sources.IPF] section of INF file.
    * Remove the source file which listed in [Components.IPF] section
      of DSC file and not listed in any other [Components] section.
    * Remove the embedded Ipf code for MDE_CPU_IPF.
    
    Removing rules for Inf file:
    * Remove IPF from VALID_ARCHITECTURES comments.
    * Remove DXE_SAL_DRIVER from LIBRARY_CLASS in [Defines] section.
    * Remove the INF which only listed in [Components.IPF] section in DSC.
    * Remove statements from [BuildOptions] that provide IPF specific flags.
    * Remove any IPF sepcific sections.
    
    Removing rules for Dec file:
    * Remove [Includes.IPF] section from Dec.
    
    Removing rules for Dsc file:
    * Remove IPF from SUPPORTED_ARCHITECTURES in [Defines] section of DSC.
    * Remove any IPF specific sections.
    * Remove statements from [BuildOptions] that provide IPF specific flags.
    
    Cc: Jaben Carsey <jaben.carsey@intel.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Chen A Chen <chen.a.chen@intel.com>
    Reviewed-by: Jaben Carsey <jaben.carsey@intel.com>

--- StdLib/Include/Ipf/machine/bswap.h
+++
@@ -1,8 +0,0 @@
-/*      $NetBSD: bswap.h,v 1.1 2006/04/07 14:21:18 cherry Exp $      */
-
-#ifndef _MACHINE_BSWAP_H_
-#define	_MACHINE_BSWAP_H_
-
-#include <sys/bswap.h>
-
-#endif /* !_MACHINE_BSWAP_H_ */
