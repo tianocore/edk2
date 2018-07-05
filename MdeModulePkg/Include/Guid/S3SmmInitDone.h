tianocore/edk2

commit d1102dba7210b95e41d06c2338a22ba6af248645

MdeModulePkg: Clean up source files
1. Do not use tab characters
2. No trailing white space in one line
3. All files must end with CRLF

Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: Liming Gao <liming.gao@intel.com>
Reviewed-by: Star Zeng <star.zeng@intel.com>

MdeModulePkg/Include/Guid/S3SmmInitDone.h
@@ -1,16 +1,16 @@
 /** @file
   After S3 SMM initialization is done and before S3 boot script is executed,
-  this GUID is installed as PPI in PEI and protocol in SMM environment. 
+  this GUID is installed as PPI in PEI and protocol in SMM environment.
   It allows for PEIMs or SMM drivers to hook this point and do the required tasks.
 
-  Copyright (c) 2018, Intel Corporation. All rights reserved.<BR>
-  This program and the accompanying materials                          
-  are licensed and made available under the terms and conditions of the BSD License         
-  which accompanies this distribution.  The full text of the license may be found at        
-  http://opensource.org/licenses/bsd-license.php                                            
+  Copyright (c) 2018 - 2018, Intel Corporation. All rights reserved.<BR>
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
 
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 

commit 9917def3294bac4d14cd86ba382652aa41c6c1d6

MdeModulePkg/S3SmmInitDone.h: Fix copyright coding style error.
It is caused by change d1102dba7210b95e41d06c2338a22ba6af248645.

Cc: Star Zeng <star.zeng@intel.com>
Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: Eric Dong <eric.dong@intel.com>
Reviewed-by: Star Zeng <star.zeng@intel.com>

MdeModulePkg/Include/Guid/S3SmmInitDone.h
@@ -3,7 +3,7 @@
   this GUID is installed as PPI in PEI and protocol in SMM environment.
   It allows for PEIMs or SMM drivers to hook this point and do the required tasks.
 
-  Copyright (c) 2018 - 2018, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2018, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at