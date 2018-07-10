cfc2c2415bbd60533fd69c73f88d4cd02e999488
Star Zeng <star.zeng@intel.com>
2018-07-10 17:18:27 +0800

    MdeModulePkg DxeCapsuleLibFmp: Fix typo 'Press' to 'Process'
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Ming Shao <ming.shao@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Star Zeng <star.zeng@intel.com>
    Reviewed-by: Jiewen Yao <jiewen.yao@intel.com>
    (cherry picked from commit a891bd733781f5430abe0a670e1bbe79e16baa28)

--- MdeModulePkg/Library/DxeCapsuleLibFmp/DxeCapsuleLib.c
+++ MdeModulePkg/Library/DxeCapsuleLibFmp/DxeCapsuleLib.c
@@ -1418,7 +1418,7 @@ ProcessCapsuleImage (
     }
 
     //
-    // Press EFI FMP Capsule
+    // Process EFI FMP Capsule
     //
     DEBUG((DEBUG_INFO, "ProcessFmpCapsuleImage ...\n"));
     Status = ProcessFmpCapsuleImage(CapsuleHeader);
