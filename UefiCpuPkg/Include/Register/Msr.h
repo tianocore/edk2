b715c37eaa2bbb375a11c9fb5229d6d46181d3d0
Eric Dong <eric.dong@intel.com>
2018-08-27 20:19:20 +0800

    UefiCpuPkg/Include/Register/Msr/GoldmontPlusMsr.h: Add new MSR file for goldmont plus microarchitecture.
    Changes includes:
      1. Add new MSR file which used for goldmont plus microarchitecture.
    
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Ruiyu Ni <ruiyu.ni@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Eric Dong <eric.dong@intel.com>
    Reviewed-by: Ruiyu Ni <ruiyu.ni@intel.com>
    Acked-by: Laszlo Ersek <lersek@redhat.com>

--- UefiCpuPkg/Include/Register/Msr.h
+++ UefiCpuPkg/Include/Register/Msr.h
@@ -6,7 +6,7 @@
   returned is a single 32-bit or 64-bit value, then a data structure is not
   provided for that MSR.
 
-  Copyright (c) 2016, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2016 ~ 2018, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -16,8 +16,8 @@
   WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
   @par Specification Reference:
-  Intel(R) 64 and IA-32 Architectures Software Developer's Manual, Volume 3,
-  September 2016, Chapter 35 Model-Specific-Registers (MSR), Chapter 35.
+  Intel(R) 64 and IA-32 Architectures Software Developer's Manual, Volume 4,
+  May 2018, Volume 4: Model-Specific-Registers (MSR)
 
 **/
 
@@ -29,6 +29,7 @@
 #include <Register/Msr/AtomMsr.h>
 #include <Register/Msr/SilvermontMsr.h>
 #include <Register/Msr/GoldmontMsr.h>
+#include <Register/Msr/GoldmontPlusMsr.h>
 #include <Register/Msr/NehalemMsr.h>
 #include <Register/Msr/Xeon5600Msr.h>
 #include <Register/Msr/XeonE7Msr.h>
