b633c7357d30c0efc782eefdbc81b739aee5e427
Liming Gao <liming.gao@intel.com>
2018-11-09 07:58:14 +0800

    BaseTools: Fix UEFI and Tiano Decompression logic issue
    https://bugzilla.tianocore.org/show_bug.cgi?id=1317
    
    This is a regression issue caused by 041d89bc0f0119df37a5fce1d0f16495ff905089.
    In Decode() function, once mOutBuf is fully filled, Decode() should return.
    Current logic misses the checker of mOutBuf after while() loop.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Liming Gao <liming.gao@intel.com>
    Cc: Yonghong Zhu <yonghong.zhu@intel.com>
    Reviewed-by: Yonghong Zhu <yonghong.zhu@intel.com>
    (cherry picked from commit 5e45a1fdcfbf9b2b389122eb97475148594625f8)

--- BaseTools/Source/C/Common/Decompress.c
+++ BaseTools/Source/C/Common/Decompress.c
@@ -662,6 +662,12 @@ Returns: (VOID)
 
         BytesRemain--;
       }
+      //
+      // Once mOutBuf is fully filled, directly return
+      //
+      if (Sd->mOutBuf >= Sd->mOrigSize) {
+        return ;
+      }
     }
   }
 
