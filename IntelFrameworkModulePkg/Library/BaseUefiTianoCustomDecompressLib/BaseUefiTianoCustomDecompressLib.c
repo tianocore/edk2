79daf8c766a38d79bf6ed5381680ad83cf770e08
Liming Gao <liming.gao@intel.com>
2018-11-09 07:58:16 +0800

    IntelFrameworkModulePkg: Fix UEFI and Tiano Decompression logic issue
    https://bugzilla.tianocore.org/show_bug.cgi?id=1317
    
    This is a regression issue caused by 684db6da64bc7b5faee4e1174e801c245f563b5c.
    In Decode() function, once mOutBuf is fully filled, Decode() should return.
    Current logic misses the checker of mOutBuf after while() loop.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Yonghong Zhu <yonghong.zhu@intel.com>
    (cherry picked from commit ade71c52a49d659b20c0b433fb11ddb4f4f543c4)

--- IntelFrameworkModulePkg/Library/BaseUefiTianoCustomDecompressLib/BaseUefiTianoCustomDecompressLib.c
+++ IntelFrameworkModulePkg/Library/BaseUefiTianoCustomDecompressLib/BaseUefiTianoCustomDecompressLib.c
@@ -634,6 +634,12 @@ Decode (
 
         BytesRemain--;
       }
+      //
+      // Once mOutBuf is fully filled, directly return
+      //
+      if (Sd->mOutBuf >= Sd->mOrigSize) {
+        goto Done ;
+      }
     }
   }
 
