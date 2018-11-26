32f79df36eb8755e71431d5e670f0deeba88ffe3
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2018-11-26 22:36:33 +0100

    BeagleBoardPkg/PrePi: base GCD memory space size on CPU's PA range
    Derive the size of the GCD memory space map directly from the CPU's
    information registers rather than from the PcdPrePiCpuMemorySize PCD,
    which will be removed.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Acked-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>

--- BeagleBoardPkg/PrePi/PrePi.c
+++ BeagleBoardPkg/PrePi/PrePi.c
@@ -110,7 +110,7 @@ PrePiMain (
   BuildStackHob (StacksBase, StacksSize);
 
   //TODO: Call CpuPei as a library
-  BuildCpuHob (PcdGet8 (PcdPrePiCpuMemorySize), PcdGet8 (PcdPrePiCpuIoSize));
+  BuildCpuHob (ArmGetPhysicalAddressBits (), PcdGet8 (PcdPrePiCpuIoSize));
 
   // Store timer value logged at the beginning of firmware image execution
   Performance.ResetEnd = GetTimeInNanoSecond (StartTimeStamp);
