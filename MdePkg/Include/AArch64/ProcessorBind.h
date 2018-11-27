82379bf6603274e81604d5a6f6bb14bdde616286
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2018-11-27 13:23:35 +0100

    MdePkg/ProcessorBind.h AARCH64: limit MAX_ADDRESS to 48 bits
    AArch64 supports the use of more than 48 bits for physical and/or
    virtual addressing, but only if the page size is set to 64 KB,
    which is not supported by UEFI. So redefine MAX_ADDRESS to cover
    only 48 address bits.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>
    Reviewed-by: Liming Gao <liming.gao@intel.com>

--- MdePkg/Include/AArch64/ProcessorBind.h
+++ MdePkg/Include/AArch64/ProcessorBind.h
@@ -138,9 +138,9 @@ typedef INT64   INTN;
 #define MAX_2_BITS  0xC000000000000000ULL
 
 ///
-/// Maximum legal AARCH64  address
+/// Maximum legal AARCH64  address (48 bits for 4 KB page size)
 ///
-#define MAX_ADDRESS   0xFFFFFFFFFFFFFFFFULL
+#define MAX_ADDRESS   0xFFFFFFFFFFFFULL
 
 ///
 /// Maximum legal AArch64 INTN and UINTN values.
