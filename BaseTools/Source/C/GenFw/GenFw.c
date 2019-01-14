2d4fd226da4245fa18088cdc10239e12343c688a
Liming Gao <liming.gao@intel.com>
2019-01-14 21:50:11 +0800

    BaseTools GenFw: Fix XCODE5 build issue
    Warning message is that "equality comparison with extraneous parentheses"
    Remove extraneous parentheses around the comparison to silence this warning.
    This issue is caused by commit 8daa4278e80c70e6caabc525cd122744488253f5.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Liming Gao <liming.gao@intel.com>
    Cc: Feng Bob C <bob.c.feng@intel.com>
    Reviewed-by: Feng Bob C <bob.c.feng@intel.com>

--- BaseTools/Source/C/GenFw/GenFw.c
+++ BaseTools/Source/C/GenFw/GenFw.c
@@ -1014,7 +1014,7 @@ Returns:
   //
   // Update Image Base Address
   //
-  if ((ImgHdr->Pe32.OptionalHeader.Magic == EFI_IMAGE_NT_OPTIONAL_HDR32_MAGIC)) {
+  if (ImgHdr->Pe32.OptionalHeader.Magic == EFI_IMAGE_NT_OPTIONAL_HDR32_MAGIC) {
     ImgHdr->Pe32.OptionalHeader.ImageBase = (UINT32) NewPe32BaseAddress;
   } else if (ImgHdr->Pe32Plus.OptionalHeader.Magic == EFI_IMAGE_NT_OPTIONAL_HDR64_MAGIC) {
     ImgHdr->Pe32Plus.OptionalHeader.ImageBase = NewPe32BaseAddress;
@@ -2180,7 +2180,7 @@ Returns:
   // Set new base address into image
   //
   if (mOutImageType == FW_REBASE_IMAGE || mOutImageType == FW_SET_ADDRESS_IMAGE) {
-    if ((PeHdr->Pe32.OptionalHeader.Magic == EFI_IMAGE_NT_OPTIONAL_HDR32_MAGIC)) {
+    if (PeHdr->Pe32.OptionalHeader.Magic == EFI_IMAGE_NT_OPTIONAL_HDR32_MAGIC) {
       if (NewBaseAddress >= 0x100000000ULL) {
         Error (NULL, 0, 3000, "Invalid", "New base address is larger than 4G for 32bit PE image");
         goto Finish;
