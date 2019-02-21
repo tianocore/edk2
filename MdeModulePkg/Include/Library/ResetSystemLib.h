23812dcea9196ca4b0e72794aa7c81dbc9e06c5c
Zhichao Gao <zhichao.gao@intel.com>
2019-02-21 08:59:39 +0800

    MdeModulePkg: Add the new API ResetSystem in the head file
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=1460
    
    Add the new API ResetSystem in the related head file so that
    the consumer can use it through the combination of library
    instance and head file.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Zhichao Gao <zhichao.gao@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>

--- MdeModulePkg/Include/Library/ResetSystemLib.h
+++ MdeModulePkg/Include/Library/ResetSystemLib.h
@@ -2,7 +2,7 @@
   System reset Library Services.  This library class defines a set of
   methods that reset the whole system.
 
-Copyright (c) 2005 - 2018, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2005 - 2019, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials are licensed and made available under
 the terms and conditions of the BSD License that accompanies this distribution.
 The full text of the license may be found at
@@ -15,6 +15,9 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 #ifndef __RESET_SYSTEM_LIB_H__
 #define __RESET_SYSTEM_LIB_H__
+
+#include <Uefi/UefiBaseType.h>
+#include <Uefi/UefiMultiPhase.h>
 
 /**
   This function causes a system-wide reset (cold reset), in which
@@ -81,6 +84,29 @@ EFIAPI
 ResetPlatformSpecific (
   IN UINTN   DataSize,
   IN VOID    *ResetData
+  );
+
+/**
+  The ResetSystem function resets the entire platform.
+
+  @param[in] ResetType      The type of reset to perform.
+  @param[in] ResetStatus    The status code for the reset.
+  @param[in] DataSize       The size, in bytes, of ResetData.
+  @param[in] ResetData      For a ResetType of EfiResetCold, EfiResetWarm, or EfiResetShutdown
+                            the data buffer starts with a Null-terminated string, optionally
+                            followed by additional binary data. The string is a description
+                            that the caller may use to further indicate the reason for the
+                            system reset. ResetData is only valid if ResetStatus is something
+                            other than EFI_SUCCESS unless the ResetType is EfiResetPlatformSpecific
+                            where a minimum amount of ResetData is always required.
+**/
+VOID
+EFIAPI
+ResetSystem (
+  IN EFI_RESET_TYPE               ResetType,
+  IN EFI_STATUS                   ResetStatus,
+  IN UINTN                        DataSize,
+  IN VOID                         *ResetData OPTIONAL
   );
 
 #endif
