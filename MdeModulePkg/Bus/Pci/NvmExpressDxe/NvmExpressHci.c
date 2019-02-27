c676cf740256a0eb88e75ab512069b5c8a44da9b
Zhichao Gao <zhichao.gao@intel.com>
2019-02-27 14:38:16 +0800

    MdeModulePkg/NvmExpressHci.c: Change comments
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=1460
    
    Refer to Uefi spec 2.8, the ResetData is valid while ResetStatus
    is EFI_SUCCESS regardless of the ResetType is EfiResetPlatformSpecific
    or not. So change the comments of NvmeShutdownAllControllers.
    
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Hao Wu <hao.a.wu@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Liming Gao <liming.gao@intel.com>
    Cc: Sean Brogan <sean.brogan@microsoft.com>
    Cc: Michael Turner <Michael.Turner@microsoft.com>
    Cc: Bret Barkelew <Bret.Barkelew@microsoft.com>
    Signed-off-by: Zhichao Gao <zhichao.gao@intel.com>
    Reviewed-by: Hao Wu <hao.a.wu@intel.com>

--- MdeModulePkg/Bus/Pci/NvmExpressDxe/NvmExpressHci.c
+++ MdeModulePkg/Bus/Pci/NvmExpressDxe/NvmExpressHci.c
@@ -966,10 +966,7 @@ NvmeControllerInit (
                                 EfiResetShutdown the data buffer starts with a Null-terminated
                                 string, optionally followed by additional binary data.
                                 The string is a description that the caller may use to further
-                                indicate the reason for the system reset. ResetData is only
-                                valid if ResetStatus is something other than EFI_SUCCESS
-                                unless the ResetType is EfiResetPlatformSpecific
-                                where a minimum amount of ResetData is always required.
+                                indicate the reason for the system reset.
                                 For a ResetType of EfiResetPlatformSpecific the data buffer
                                 also starts with a Null-terminated string that is followed
                                 by an EFI_GUID that describes the specific type of reset to perform.
