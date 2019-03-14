b87a9a76ebc3b8831cff9117a2640955be463641
Bret Barkelew <Bret.Barkelew@microsoft.com>
2019-03-14 17:03:35 +0800

    MdePkg/DebugLib.h: Add new APIs for DebugLib
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=1395
    
    Add new APIs DebugVPrint and DebugBPrint prototype
    definition in the DebugLib header file. These APIs
    would expose a print routine with VaList parameter
    and BaseList parameter.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Zhichao Gao <zhichao.gao@intel.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Liming Gao <liming.gao@intel.com>
    Cc: Sean Brogan <sean.brogan@microsoft.com>
    Cc: Michael Turner <Michael.Turner@microsoft.com>
    Cc: Bret Barkelew <Bret.Barkelew@microsoft.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>

--- MdePkg/Include/Library/DebugLib.h
+++ MdePkg/Include/Library/DebugLib.h
@@ -8,7 +8,7 @@
   of size reduction when compiler optimization is disabled. If MDEPKG_NDEBUG is
   defined, then debug and assert related macros wrapped by it are the NULL implementations.
 
-Copyright (c) 2006 - 2018, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2019, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials are licensed and made available under
 the terms and conditions of the BSD License that accompanies this distribution.
 The full text of the license may be found at
@@ -98,6 +98,56 @@ DebugPrint (
   IN  UINTN        ErrorLevel,
   IN  CONST CHAR8  *Format,
   ...
+  );
+
+
+/**
+  Prints a debug message to the debug output device if the specified
+  error level is enabled.
+
+  If any bit in ErrorLevel is also set in DebugPrintErrorLevelLib function
+  GetDebugPrintErrorLevel (), then print the message specified by Format and
+  the associated variable argument list to the debug output device.
+
+  If Format is NULL, then ASSERT().
+
+  @param  ErrorLevel    The error level of the debug message.
+  @param  Format        Format string for the debug message to print.
+  @param  VaListMarker  VA_LIST marker for the variable argument list.
+
+**/
+VOID
+EFIAPI
+DebugVPrint (
+  IN  UINTN         ErrorLevel,
+  IN  CONST CHAR8   *Format,
+  IN  VA_LIST       VaListMarker
+  );
+
+
+/**
+  Prints a debug message to the debug output device if the specified
+  error level is enabled.
+  This function use BASE_LIST which would provide a more compatible
+  service than VA_LIST.
+
+  If any bit in ErrorLevel is also set in DebugPrintErrorLevelLib function
+  GetDebugPrintErrorLevel (), then print the message specified by Format and
+  the associated variable argument list to the debug output device.
+
+  If Format is NULL, then ASSERT().
+
+  @param  ErrorLevel      The error level of the debug message.
+  @param  Format          Format string for the debug message to print.
+  @param  BaseListMarker  BASE_LIST marker for the variable argument list.
+
+**/
+VOID
+EFIAPI
+DebugBPrint (
+  IN  UINTN         ErrorLevel,
+  IN  CONST CHAR8   *Format,
+  IN  BASE_LIST     BaseListMarker
   );
 
 
