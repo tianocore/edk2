56ba37463a5ffa44df1c5741184ba0594fadbeba
Michael D Kinney <michael.d.kinney@intel.com>
2019-04-03 16:07:06 -0700

    ShellPkg: Replace BSD License with BSD+Patent License
    https://bugzilla.tianocore.org/show_bug.cgi?id=1373
    
    Replace BSD 2-Clause License with BSD+Patent License.  This change is
    based on the following emails:
    
      https://lists.01.org/pipermail/edk2-devel/2019-February/036260.html
      https://lists.01.org/pipermail/edk2-devel/2018-October/030385.html
    
    RFCs with detailed process for the license change:
    
      V3: https://lists.01.org/pipermail/edk2-devel/2019-March/038116.html
      V2: https://lists.01.org/pipermail/edk2-devel/2019-March/037669.html
      V1: https://lists.01.org/pipermail/edk2-devel/2019-March/037500.html
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Ray Ni <ray.ni@intel.com>
    Reviewed-by: Jaben Carsey <jaben.carsey@intel.com>

--- ShellPkg/Application/Shell/ConsoleLogger.c
+++ ShellPkg/Application/Shell/ConsoleLogger.c
@@ -4,13 +4,7 @@
   (C) Copyright 2013 Hewlett-Packard Development Company, L.P.<BR>
   Copyright (c) 2009 - 2018, Intel Corporation. All rights reserved.<BR>
   (C) Copyright 2016 Hewlett-Packard Development Company, L.P.<BR>
-  This program and the accompanying materials
-  are licensed and made available under the terms and conditions of the BSD License
-  which accompanies this distribution.  The full text of the license may be found at
-  http://opensource.org/licenses/bsd-license.php
-
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
 #include "Shell.h"
--- ShellPkg/DynamicCommand/DpDynamicCommand/Literals.h
+++ ShellPkg/DynamicCommand/DpDynamicCommand/Literals.h
@@ -2,13 +2,7 @@
   Declarations of ASCII string literals used by DP.
 
   Copyright (c) 2009 - 2018, Intel Corporation. All rights reserved.<BR>
-  This program and the accompanying materials
-  are licensed and made available under the terms and conditions of the BSD License
-  which accompanies this distribution.  The full text of the license may be found at
-  http://opensource.org/licenses/bsd-license.php
-
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 #ifndef _LITERALS_H_
 #define _LITERALS_H_
