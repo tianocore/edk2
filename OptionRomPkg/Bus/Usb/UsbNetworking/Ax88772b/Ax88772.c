96ae5934cfc393e2c53198e2a067a54202d487a6
Michael D Kinney <michael.d.kinney@intel.com>
2019-04-03 16:06:23 -0700

    OptionRomPkg: Replace BSD License with BSD+Patent License
    https://bugzilla.tianocore.org/show_bug.cgi?id=1373
    
    Replace BSD 2-Clause License with BSD+Patent License.  This change is
    based on the following emails:
    
      https://lists.01.org/pipermail/edk2-devel/2019-February/036260.html
      https://lists.01.org/pipermail/edk2-devel/2018-October/030385.html
    
    RFCs with detailed process for the license change:
    
      V3: https://lists.01.org/pipermail/edk2-devel/2019-March/038116.html
      V2: https://lists.01.org/pipermail/edk2-devel/2019-March/037669.html
      V1: https://lists.01.org/pipermail/edk2-devel/2019-March/037500.html
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Ray Ni <ray.ni@intel.com>

--- OptionRomPkg/Bus/Usb/UsbNetworking/Ax88772b/Ax88772.c
+++ OptionRomPkg/Bus/Usb/UsbNetworking/Ax88772b/Ax88772.c
@@ -5,14 +5,8 @@
   USB to Ethernet MAC with integrated 10/100 PHY.  Note that this implementation
   only supports the integrated PHY since no other test cases were available.
 
-  Copyright (c) 2011, Intel Corporation
-  All rights reserved. This program and the accompanying materials
-  are licensed and made available under the terms and conditions of the BSD License
-  which accompanies this distribution.  The full text of the license may be found at
-  http://opensource.org/licenses/bsd-license.php
-
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+  Copyright (c) 2011, Intel Corporation. All rights reserved.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
--- OptionRomPkg/Bus/Usb/UsbNetworking/Ax88772b/ComponentName.c
+++ OptionRomPkg/Bus/Usb/UsbNetworking/Ax88772b/ComponentName.c
@@ -1,14 +1,8 @@
 /** @file
   UEFI Component Name(2) protocol implementation.
 
-  Copyright (c) 2011, Intel Corporation
-  All rights reserved. This program and the accompanying materials
-  are licensed and made available under the terms and conditions of the BSD License
-  which accompanies this distribution.  The full text of the license may be found at
-  http://opensource.org/licenses/bsd-license.php
-
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+  Copyright (c) 2011, Intel Corporation. All rights reserved.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
