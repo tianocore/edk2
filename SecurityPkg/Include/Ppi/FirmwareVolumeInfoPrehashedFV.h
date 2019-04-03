289b714b77008aa4200c0be25c4b4e25df04955a
Michael D Kinney <michael.d.kinney@intel.com>
2019-04-03 16:06:56 -0700

    SecurityPkg: Replace BSD License with BSD+Patent License
    https://bugzilla.tianocore.org/show_bug.cgi?id=1373
    
    Replace BSD 2-Clause License with BSD+Patent License.  This change is
    based on the following emails:
    
      https://lists.01.org/pipermail/edk2-devel/2019-February/036260.html
      https://lists.01.org/pipermail/edk2-devel/2018-October/030385.html
    
    RFCs with detailed process for the license change:
    
      V3: https://lists.01.org/pipermail/edk2-devel/2019-March/038116.html
      V2: https://lists.01.org/pipermail/edk2-devel/2019-March/037669.html
      V1: https://lists.01.org/pipermail/edk2-devel/2019-March/037500.html
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Jiewen Yao <Jiewen.yao@intel.com>
    Reviewed-by: Jian J Wang <jian.j.wang@intel.com>

--- SecurityPkg/Include/Ppi/FirmwareVolumeInfoPrehashedFV.h
+++ SecurityPkg/Include/Ppi/FirmwareVolumeInfoPrehashedFV.h
@@ -4,13 +4,7 @@ PPI to describe all hash digests for a given FV
 Copyright (c) 2017, Microsoft Corporation
 Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
 
-This program and the accompanying materials
-are licensed and made available under the terms and conditions of the BSD License
-which accompanies this distribution.  The full text of the license may be found at
-http://opensource.org/licenses/bsd-license.php
-
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
