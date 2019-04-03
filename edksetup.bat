9ca2b9b9abeff5db9da35dc8a506e7ebd632655f
Michael D Kinney <michael.d.kinney@intel.com>
2019-04-03 16:02:46 -0700

    edk2: Replace BSD License with BSD+Patent License
    https://bugzilla.tianocore.org/show_bug.cgi?id=1373
    
    Replace BSD 2-Clause License with BSD+Patent License.  This change is
    based on the following emails:
    
      https://lists.01.org/pipermail/edk2-devel/2019-February/036260.html
      https://lists.01.org/pipermail/edk2-devel/2018-October/030385.html
    
    RFCs with detailed process for the license change:
    
      V3: https://lists.01.org/pipermail/edk2-devel/2019-March/038116.html
      V2: https://lists.01.org/pipermail/edk2-devel/2019-March/037669.html
      V1: https://lists.01.org/pipermail/edk2-devel/2019-March/037500.html
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>

--- edksetup.bat
+++ edksetup.bat
@@ -3,13 +3,7 @@
 @REM
 @REM Copyright (c) 2006 - 2016, Intel Corporation. All rights reserved.<BR>
 @REM (C) Copyright 2016 Hewlett Packard Enterprise Development LP<BR>
-@REM This program and the accompanying materials
-@REM are licensed and made available under the terms and conditions of the BSD License
-@REM which accompanies this distribution.  The full text of the license may be found at
-@REM http://opensource.org/licenses/bsd-license.php
-@REM
-@REM THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-@REM WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+@REM SPDX-License-Identifier: BSD-2-Clause-Patent
 @REM
 
 @REM set CYGWIN_HOME=C:\cygwin
