a40f30398ab0f13cd79048b4b2f98449ebad96cd
Krzysztof Koch <krzysztof.koch@arm.com>
2019-04-24 10:50:22 +0100

    MdePkg: Add ACPI 6.3 header file
    The patch includes the following ACPI 6.3 updates:
     1. Reserve CRAT and CDIT table names in ACPI 6.3 header.
        - ACPI 6.3 January 2019, Table 5-30
        - Mantis ID 1883 (https://mantis.uefi.org/mantis/view.php?id=1883)
     2. Add new processor structure flags in PPTT.
        - ACPI 6.3 January 2019, Section 5.2.29
        - Mantis ID 1870 (https://mantis.uefi.org/mantis/view.php?id=1870)
        - Mantis ID 1934 (https://mantis.uefi.org/mantis/view.php?id=1934)
     3. Add SPE support to MADT.
        - ACPI 6.3 January 2019, Table 5-60
        - Mantis ID 1934 (https://mantis.uefi.org/mantis/view.php?id=1934)
     4. Add 'Hot-plug Capable' flag to APIC.
        - ACPI 6.3 January 2019, Table 5-44, Table 5-47 & Table 5-58
        - Mantis ID 1948 (https://mantis.uefi.org/mantis/view.php?id=1948)
     5. Add CNTHV timer to GTDT.
        - ACPI 6.3 January 2019, Section 5.2.24
        - Mantis ID 1851 (https://mantis.uefi.org/mantis/view.php?id=1851)
     6. Add 'Trigger Order' to Platform Communication Channel Identification
        Structure.
        - ACPI 6.3 January 2019, Section 5.2.28
        - Mantis ID 1867 (https://mantis.uefi.org/mantis/view.php?id=1867)
     7. Add Generic Initiator Affinity Structure to SRAT.
        - ACPI 6.3 January 2019, Section 5.2.16.6
        - Mantis ID 1904 (https://mantis.uefi.org/mantis/view.php?id=1904)
     8. Add 'HMAT Enhancements'.
        - ACPI 6.3 January 2019, Section 5.2.27
        - Mantis ID 1914 (https://mantis.uefi.org/mantis/view.php?id=1914)
        - Mantis ID 1959 (https://mantis.uefi.org/mantis/view.php?id=1959)
     9. Fix generic address structure definition to include all address
        space ID keywords.
       - ACPI 6.3 January 2019, Table 5-25
       - Mantis ID 1965 (https://mantis.uefi.org/mantis/view.php?id=1965)
    10. Make Acpi63.h the latest ACPI definition.
    
    Signed-off-by: Krzysztof Koch <krzysztof.koch@arm.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>

--- MdePkg/Include/IndustryStandard/Acpi.h
+++ MdePkg/Include/IndustryStandard/Acpi.h
@@ -3,6 +3,7 @@
   consumed by drivers that do not care about ACPI versions.
 
   Copyright (c) 2006 - 2017, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2019, ARM Ltd. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -10,6 +11,6 @@
 #ifndef _ACPI_H_
 #define _ACPI_H_
 
-#include <IndustryStandard/Acpi62.h>
+#include <IndustryStandard/Acpi63.h>
 
 #endif
