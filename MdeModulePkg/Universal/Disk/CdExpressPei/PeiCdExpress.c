2e217e402215c1bb2493c379bbc39dca09a8c9cc
Dandan Bi <dandan.bi@intel.com>
2019-04-28 21:36:03 +0800

    MdeModulePkg/CdExpress: Remove PcdFrameworkCompatibilitySupport usage
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=1464
    
    Currently Framework compatibility is not needed and
    PcdFrameworkCompatibilitySupport will be removed from edk2.
    So remove the usage of this PCD firstly.
    
    Cc: Hao Wu <hao.a.wu@intel.com>
    Cc: Liming Gao <liming.gao@intel.com>
    Signed-off-by: Dandan Bi <dandan.bi@intel.com>
    Reviewed-by: Hao Wu <hao.a.wu@intel.com>

--- MdeModulePkg/Universal/Disk/CdExpressPei/PeiCdExpress.c
+++ MdeModulePkg/Universal/Disk/CdExpressPei/PeiCdExpress.c
@@ -1,7 +1,7 @@
 /** @file
   Source file for CD recovery PEIM
 
-Copyright (c) 2006 - 2018, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2019, Intel Corporation. All rights reserved.<BR>
 
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -587,10 +587,6 @@ GetRecoveryCapsuleInfo (
     return Status;
   }
 
-  if (FeaturePcdGet (PcdFrameworkCompatibilitySupport)) {
-    CapsuleInstance = CapsuleInstance + 1;
-  }
-
   if ((CapsuleInstance == 0) || (CapsuleInstance > NumberRecoveryCapsules)) {
     return EFI_NOT_FOUND;
   }
@@ -645,10 +641,6 @@ LoadRecoveryCapsule (
 
   if (EFI_ERROR (Status)) {
     return Status;
-  }
-
-  if (FeaturePcdGet (PcdFrameworkCompatibilitySupport)) {
-    CapsuleInstance = CapsuleInstance + 1;
   }
 
   if ((CapsuleInstance == 0) || (CapsuleInstance > NumberRecoveryCapsules)) {
