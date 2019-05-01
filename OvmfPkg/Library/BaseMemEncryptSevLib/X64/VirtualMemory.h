2fcd54ce39df4866dde6ffc8abdef3409910c326
Peter Gonda <pgonda@users.noreply.github.com>
2019-05-01 10:27:21 -0700

    Fix typo in VirtualMemory.h
    encyrption->encryption

--- OvmfPkg/Library/BaseMemEncryptSevLib/X64/VirtualMemory.h
+++ OvmfPkg/Library/BaseMemEncryptSevLib/X64/VirtualMemory.h
@@ -195,7 +195,7 @@ typedef struct {
   @retval RETURN_SUCCESS              The attributes were cleared for the
                                       memory region.
   @retval RETURN_INVALID_PARAMETER    Number of pages is zero.
-  @retval RETURN_UNSUPPORTED          Clearing the memory encyrption attribute
+  @retval RETURN_UNSUPPORTED          Clearing the memory encryption attribute
                                       is not supported
 **/
 RETURN_STATUS
@@ -222,7 +222,7 @@ InternalMemEncryptSevSetMemoryDecrypted (
   @retval RETURN_SUCCESS              The attributes were set for the memory
                                       region.
   @retval RETURN_INVALID_PARAMETER    Number of pages is zero.
-  @retval RETURN_UNSUPPORTED          Setting the memory encyrption attribute
+  @retval RETURN_UNSUPPORTED          Setting the memory encryption attribute
                                       is not supported
 **/
 RETURN_STATUS
