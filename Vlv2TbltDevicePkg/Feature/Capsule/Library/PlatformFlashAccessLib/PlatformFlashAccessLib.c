3eac7496bef1636b3745ff015581012534a47721
Michael D Kinney <michael.d.kinney@intel.com>
2019-05-08 14:34:58 -0700

    Vlv2TbltDevicePkg/PlatformFlashAccessLib: Add error return
    If the FLASH device cannot be erased or written, then the
    flag FlashError is set to TRUE.
    
    Add the missing logic to return an error if FlashError
    is TRUE.  The missing logic was found from a GCC build that
    noticed a local variable was declared and assigned, but was
    not being used.
    
    Cc: Zailiang Sun <zailiang.sun@intel.com>
    Cc: Yi Qian <yi.qian@intel.com>
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Zailiang Sun <zailiang.sun@intel.com>
    Reviewed-by: Yi Qian <yi.qian@intel.com>

--- Vlv2TbltDevicePkg/Feature/Capsule/Library/PlatformFlashAccessLib/PlatformFlashAccessLib.c
+++ Vlv2TbltDevicePkg/Feature/Capsule/Library/PlatformFlashAccessLib/PlatformFlashAccessLib.c
@@ -1,7 +1,7 @@
 /** @file
   Platform Flash Access library.
 
-  Copyright (c) 2016 - 2018, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2016 - 2019, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -527,6 +527,10 @@ Done:
 
   if (Progress != NULL) {
     Progress (EndPercentage);
+  }
+
+  if (FlashError) {
+    return EFI_WRITE_PROTECTED;
   }
 
   return EFI_SUCCESS;
