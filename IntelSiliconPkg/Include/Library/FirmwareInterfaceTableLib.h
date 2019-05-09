cf2ea7099246e30c41ae1d76d59202ca9eb7d772
Michael Kubacki <michael.a.kubacki@intel.com>
2019-05-09 00:11:10 -0700

    IntelSiliconPkg: Replace BSD license with BSD+Patent license
    Aligns the license with the latest in the edk2 project.
    
    Signed-off-by: Michael Kubacki <michael.a.kubacki@intel.com>

--- IntelSiliconPkg/Include/Library/FirmwareInterfaceTableLib.h
+++ IntelSiliconPkg/Include/Library/FirmwareInterfaceTableLib.h
@@ -1,14 +1,8 @@
 /** @file
-Firmware Interface Table (FIT) library
+  Firmware Interface Table (FIT) library
 
-Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
-This program and the accompanying materials are licensed and made available under
-the terms and conditions of the BSD License that accompanies this distribution.
-The full text of the license may be found at
-http://opensource.org/licenses/bsd-license.php.
-
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+  Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
