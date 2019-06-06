6631c096ee3e38b957808820c1acdae6cd0c41a0
Anthony PERARD <anthony.perard@citrix.com>
2019-06-06 14:14:58 +0100

    ArmPkg/PlatformBootManagerLib: Use EDKII_SERIAL_PORT_LIB_VENDOR_GUID from MdeModulePkg
    SERIAL_DXE_FILE_GUID is now defined in MdeModulePkg as
    EDKII_SERIAL_PORT_LIB_VENDOR_GUID, simply use it.
    
    Signed-off-by: Anthony PERARD <anthony.perard@citrix.com>
    Message-Id: <20190606131459.1464-4-anthony.perard@citrix.com>
    Acked-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>

--- ArmPkg/Library/PlatformBootManagerLib/PlatformBm.c
+++ ArmPkg/Library/PlatformBootManagerLib/PlatformBm.c
@@ -28,6 +28,7 @@
 #include <Protocol/PlatformBootManager.h>
 #include <Guid/EventGroup.h>
 #include <Guid/TtyTerm.h>
+#include <Guid/SerialPortLibVendor.h>
 
 #include "PlatformBm.h"
 
@@ -42,18 +43,13 @@ typedef struct {
 } PLATFORM_SERIAL_CONSOLE;
 #pragma pack ()
 
-#define SERIAL_DXE_FILE_GUID { \
-          0xD3987D4B, 0x971A, 0x435F, \
-          { 0x8C, 0xAF, 0x49, 0x67, 0xEB, 0x62, 0x72, 0x41 } \
-          }
-
 STATIC PLATFORM_SERIAL_CONSOLE mSerialConsole = {
   //
   // VENDOR_DEVICE_PATH SerialDxe
   //
   {
     { HARDWARE_DEVICE_PATH, HW_VENDOR_DP, DP_NODE_LEN (VENDOR_DEVICE_PATH) },
-    SERIAL_DXE_FILE_GUID
+    EDKII_SERIAL_PORT_LIB_VENDOR_GUID
   },
 
   //
