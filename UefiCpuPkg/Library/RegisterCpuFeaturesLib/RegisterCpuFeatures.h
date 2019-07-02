f312da8046ace52e112802ddde95c780cad324c9
Ray Ni <ray.ni@intel.com>
2019-07-02 17:21:02 +0800

    UefiCpuPkg/RegisterCpuFeaturesLib: Rename [Before|After]FeatureBitMask
    The patch doesn't have any functionality impact.
    
    Signed-off-by: Ray Ni <ray.ni@intel.com>
    Cc: Eric Dong <eric.dong@intel.com>

--- UefiCpuPkg/Library/RegisterCpuFeaturesLib/RegisterCpuFeatures.h
+++ UefiCpuPkg/Library/RegisterCpuFeaturesLib/RegisterCpuFeatures.h
@@ -43,8 +43,8 @@ typedef struct {
   CPU_FEATURE_GET_CONFIG_DATA  GetConfigDataFunc;
   CPU_FEATURE_SUPPORT          SupportFunc;
   CPU_FEATURE_INITIALIZE       InitializeFunc;
-  UINT8                        *BeforeFeatureBitMask;
-  UINT8                        *AfterFeatureBitMask;
+  UINT8                        *ThreadBeforeFeatureBitMask;
+  UINT8                        *ThreadAfterFeatureBitMask;
   UINT8                        *CoreBeforeFeatureBitMask;
   UINT8                        *CoreAfterFeatureBitMask;
   UINT8                        *PackageBeforeFeatureBitMask;
