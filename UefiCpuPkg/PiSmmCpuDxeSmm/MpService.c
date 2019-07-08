65ecb04cd8c182edaac6a85b69a76fce23910470
Zhichao Gao <zhichao.gao@intel.com>
2019-07-08 22:08:02 +0800

    Revert "UefiCpuPkg\CpuSmm: Save & restore CR2 on-demand paging in SMM"
    This change would cause "BSP sync with APs timeout in SmmRuntimeCtlExit!"
    in file
    Intel\ServerSiliconPkg\Cpu\Library\SmmCpuFeaturesLib\SmmCpuFeaturesLib.c
    line 1524.
    This commit should be reverted after fix the issue.
    
    This reverts commit 37f9fea5b88d2aa0cc702e1b315a1b939447eb01.
    
    Change-Id: If9fdd9e8be4e1b0598c634093cb57ffad7c9155d
    
    SyncScript: BaseCommit is 7ecb4fd21d54aa75b0878720289c3182cbb19a4f

--- UefiCpuPkg/PiSmmCpuDxeSmm/MpService.c
+++ UefiCpuPkg/PiSmmCpuDxeSmm/MpService.c
@@ -1566,11 +1566,9 @@ SmiRendezvous (
   ASSERT(CpuIndex < mMaxNumberOfCpus);
 
   //
-  // Save Cr2 because Page Fault exception in SMM may override its value,
-  // when using on-demand paging for above 4G memory.
+  // Save Cr2 because Page Fault exception in SMM may override its value
   //
-  Cr2 = 0;
-  SaveCr2 (&Cr2);
+  Cr2 = AsmReadCr2 ();
 
   //
   // Call the user register Startup function first.
@@ -1716,11 +1714,10 @@ SmiRendezvous (
 
 Exit:
   SmmCpuFeaturesRendezvousExit (CpuIndex);
-
   //
   // Restore Cr2
   //
-  RestoreCr2 (Cr2);
+  AsmWriteCr2 (Cr2);
 }
 
 /**
