c1b4d5c4eed65a0d495b434fd162345021feee6d
Tom Xu <tom.xu@intel.com>
2019-07-31 13:55:57 +0800

    UefiCpuPkg/PiSmmCpu: Allow SMM access-out when static paging is OFF
    Updated page fault handler to treat SMM access-out as allowed
    address when static paging is not used.
    
    But that commit is not complete because the page table is still
    updated in SetUefiMemMapAttributes() for non-SMRAM memory. When SMM
    code accesses non-SMRAM memory, page fault is still generated.
    
    This patch skips to update page table for non-SMRAM memory and
    page table itself.
    
    This patch is being reviewed in open source and needs extended discussion, and this feature is needed urgently in ICX, and will closely follow edk progress to emililate this override.
    
    Change-Id: Icfa840113688f394fbe16db70c1f19dc4d83ba70
    Hsd-es-id: 1507338557
    Signed-off-by: tom.xu@intel.com
    Attestation-link: https://sharepoint.amr.ith.intel.com/sites/epsdswlc/EPSD%20SWLC%20Library/10nm/Developer%20Attestations
    Reviewed-on: https://git-amr-7.devtools.intel.com/gerrit/55053
    Tested-by: CR <cr2@intel.com>
    Reviewed-by: You, Benjamin <benjamin.you@intel.com>
    Test-Verified: CR <cr2@intel.com>
    Test-Verified: caf_test <caf_test@intel.com>
    Reviewed-by: Ray Ni <ray.ni@intel.com>

--- UefiCpuPkg/PiSmmCpuDxeSmm/PiSmmCpuDxeSmm.c
+++ UefiCpuPkg/PiSmmCpuDxeSmm/PiSmmCpuDxeSmm.c
@@ -130,6 +130,8 @@ UINT8                    mPhysicalAddressBits;
 UINT32                   mSmmCr0;
 UINT32                   mSmmCr4;
 
+extern BOOLEAN           mCpuSmmStaticPageTable;
+
 /**
   Initialize IDT to setup exception handlers for SMM.
 
@@ -1121,6 +1123,9 @@ FindSmramInfo (
   *SmrrBase = (UINT32)CurrentSmramRange->CpuStart;
   *SmrrSize = (UINT32)CurrentSmramRange->PhysicalSize;
 
+  //
+  // Extend *SmrrBase/*SmrrSize to include adjacent SMRAM ranges
+  //
   do {
     Found = FALSE;
     for (Index = 0; Index < mSmmCpuSmramRangeCount; Index++) {
@@ -1432,14 +1437,20 @@ PerformRemainingTasks (
     SetMemMapAttributes ();
 
     //
-    // For outside SMRAM, we only map SMM communication buffer or MMIO.
+    // Do not protect memory outside SMRAM when SMM static page table is not enabled.
     //
-    SetUefiMemMapAttributes ();
+    if (mCpuSmmStaticPageTable) {
 
-    //
-    // Set page table itself to be read-only
-    //
-    SetPageTableAttributes ();
+      //
+      // For outside SMRAM, we only map SMM communication buffer or MMIO.
+      //
+      SetUefiMemMapAttributes ();
+
+      //
+      // Set page table itself to be read-only
+      //
+      SetPageTableAttributes ();
+    }
 
     //
     // Configure SMM Code Access Check feature if available.
