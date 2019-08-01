05579e51824a1ea393e52a3bda7d0c35cb060bc3
Michael D Kinney <michael.d.kinney@intel.com>
2019-08-01 15:57:18 -0700

    BaseTools/PatchCheck: Ignore blank lines in diff
    https://bugzilla.tianocore.org/show_bug.cgi?id=2044
    
    When fixes are made for incorrect line endings, there
    are cases where the diff contains blank lines.  Ignore
    these blank lines.
    
    Cc: Bob Feng <bob.c.feng@intel.com>
    Cc: Liming Gao <liming.gao@intel.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Bob Feng <bob.c.feng@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>

--- BaseTools/Scripts/PatchCheck.py
+++ BaseTools/Scripts/PatchCheck.py
@@ -270,6 +270,7 @@ class GitDiffCheck:
             if line.startswith('@@ '):
                 self.state = PRE_PATCH
             elif len(line) >= 1 and line[0] not in ' -+' and \
+                 not line.startswith('\r\n') and  \
                  not line.startswith(r'\ No newline ') and not self.binary:
                 for line in self.lines[self.line_num + 1:]:
                     if line.startswith('diff --git'):
@@ -313,6 +314,8 @@ class GitDiffCheck:
                 pass
             elif line.startswith('+'):
                 self.check_added_line(line[1:])
+            elif line.startswith('\r\n'):
+                pass
             elif line.startswith(r'\ No newline '):
                 pass
             elif not line.startswith(' '):
