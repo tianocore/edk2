8633e79e3c76e7894f59fc7b42648a69d8d93dcd
Michael D Kinney <michael.d.kinney@intel.com>
2019-11-07 02:29:16 -0800

    CryptoPkg: Add EDK II Crypto Protocols/PPIs/PCDs
    Based on the following package with changes to merge into
    CryptoPkg.
    
    https://github.com/microsoft/mu_plus/tree/dev/201908/SharedCryptoPkg
    
    Add the EDK II Crypto Protocol, EDK II SMM Crypto Protocol
    and EDK II Crypto PPI that provide the same services as the
    BaseCryptLib.  One additional GetVersion() services is provided
    to support backward compatible extensions to these new
    protocols/ppis if new services are added to BaseCryptLib in the
    future.  The EDK II Crypto Protocols/PPIs are located in a
    Private directory so they are only available CryptoPkg components.
    
    In order to optimize the size of modules that produce the
    EDK II Crypto Protocols/PPIs define a FixedAtBuild PCD
    gEfiCryptoPkgTokenSpaceGuid.PcdCryptoServiceFamilyEnable.
    This PCD is type VOID* and is associated with the structure
    PCD_CRYPTO_SERVICE_FAMILY_ENABLE.  This structure contains
    bitfields and unions that allow a platform DSC files to
    enable/disable entire families of services or enable/disable
    individual services in the produced EDK II Crypto
    protocols/ppis.
    
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Xiaoyu Lu <xiaoyux.lu@intel.com>
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>

---
+++ CryptoPkg/Private/Protocol/SmmCrypto.h
@@ -0,0 +1,40 @@
+/** @file
+This Protocol provides Crypto services to SMM modules
+
+Copyright (c) 2019, Microsoft Corporation
+
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+**/
+
+#ifndef __EDKII_SMM_CRYPTO_PROTOCOL_H__
+#define __EDKII_SMM_CRYPTO_PROTOCOL_H__
+
+#include <Protocol/Crypto.h>
+
+///
+/// EDK II SMM Crypto Protocol is identical to EDK II Crypto Protocol
+///
+typedef EDKII_CRYPTO_PROTOCOL EDKII_SMM_CRYPTO_PROTOCOL;
+
+extern GUID gEdkiiSmmCryptoProtocolGuid;
+
+#endif
