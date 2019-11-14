7653beb638fb5e45a4df73409ed8baed1d8b0d8c
Shenglei Zhang <shenglei.zhang@intel.com>
2019-11-14 16:30:10 +0530

    UefiCpuPkg: Update the coding styles
    (cherry picked from commit 9c33f16f8cecba845e877295e8b9be805dd2fbb8)
    
    In MpLib.c, remove the white space on a new line.
    In PageTbl.c and PiSmmCpuDxeSmm.h, update the comment style.
    
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Shenglei Zhang <shenglei.zhang@intel.com>
    Reviewed-by: Eric Dong <eric.dong@intel.com>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    (cherry picked from commit 9c33f16f8cecba845e877295e8b9be805dd2fbb8)

--- UefiCpuPkg/PiSmmCpuDxeSmm/PiSmmCpuDxeSmm.h
+++ UefiCpuPkg/PiSmmCpuDxeSmm/PiSmmCpuDxeSmm.h
@@ -1455,7 +1455,7 @@ InitializeDataForMmMp (
 
   @retval TRUE  Access to non-SMRAM is restricted.
   @retval FALSE Access to non-SMRAM is not restricted.
-*/
+**/
 BOOLEAN
 IsRestrictedMemoryAccess (
   VOID
