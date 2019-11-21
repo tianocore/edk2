0ee225ace227d6b4a8f306c31bc75209a39509a9
Michael D Kinney <michael.d.kinney@intel.com>
2019-11-21 09:24:41 -0800

    CryptoPkg/Driver: Add Crypto PEIM, DXE, and SMM modules
    Based on the following package with changes to merge into
    CryptoPkg.
    
    https://github.com/microsoft/mu_plus/tree/dev/201908/SharedCryptoPkg
    
    Add the CryptoPei, CryptoDxe, and CryptoSmm modules that produce
    EDK II Crypto Protocols/PPIs that provide the same services as
    the BaseCryptLib class.
    
    In order to optimize the size of CryptoPei, CryptoDxe, and
    CryptoSmm modules for a specific platform, the FixedAtBuild
    PCD gEfiCryptoPkgTokenSpaceGuid.PcdCryptoServiceFamilyEnable
    is used to determine if a specific service is enabled or
    disabled.  If a service is enabled, then a call is made to
    the BaseCryptLib service.  If the service is disabled, then
    a DEBUG() message and ASSERT() are performed and a default
    return value is returned.  This provides simple detection
    of a services that is disabled services but is used by
    another module when DEBUG()/ASSERT() macros are enabled.
    
    The use of a FixedAtBuild PCD is required so the compiler
    and linker to know each services enable/disable setting at
    build time and allows disabled services to be optimized away.
    
    CryptoPei supports both pre-mem and post-mem use cases.
    If initially dispatched pre-mmem, then register for shadow,
    so the Crypto PPI can be reinstalled post-mem.
    
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Xiaoyu Lu <xiaoyux.lu@intel.com>
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>

---
+++ CryptoPkg/Driver/CryptoSmm.c
@@ -0,0 +1,41 @@
+/** @file
+  Installs the EDK II Crypto SMM Protocol
+
+  Copyright (C) Microsoft Corporation. All rights reserved.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <PiSmm.h>
+#include <Library/SmmServicesTableLib.h>
+#include <Protocol/SmmCrypto.h>
+
+extern CONST EDKII_CRYPTO_PROTOCOL  mEdkiiCrypto;
+
+/**
+  The module Entry Point of the Crypto SMM Driver.
+
+  @param[in]  ImageHandle    The firmware allocated handle for the EFI image.
+  @param[in]  SystemTable    A pointer to the EFI System Table.
+
+  @retval EFI_SUCCESS    The entry point is executed successfully.
+  @retval Other          Some error occurs when executing this entry point.
+
+**/
+EFI_STATUS
+EFIAPI
+CryptoSmmEntry (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  EFI_HANDLE  Handle;
+
+  Handle = NULL;
+  return gSmst->SmmInstallProtocolInterface (
+                  &Handle,
+                  &gEdkiiSmmCryptoProtocolGuid,
+                  EFI_NATIVE_INTERFACE,
+                  (EDKII_CRYPTO_PROTOCOL *) &mEdkiiCrypto
+                  );
+}
