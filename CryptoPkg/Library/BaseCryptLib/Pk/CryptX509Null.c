f41d368fd9b4fd0754d3184f2dc5a5c44aa3c832
Michael D Kinney <michael.d.kinney@intel.com>
2019-11-20 17:14:16 -0800

    CryptoPkg/BaseCryptLib: Add X509ConstructCertificateStackV().
    Add X509ConstructCertificateStackV() to BaseCryptLib that is
    identical in behavior to X509ConstructCertificateStack(), but
    it takes a VA_LIST parameter for the variable argument list.
    
    The VA_LIST form of this function is required for BaseCryptLib
    functions to be wrapped in protocols/ppis.
    
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Xiaoyu Lu <xiaoyux.lu@intel.com>
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>

--- CryptoPkg/Library/BaseCryptLib/Pk/CryptX509Null.c
+++ CryptoPkg/Library/BaseCryptLib/Pk/CryptX509Null.c
@@ -36,6 +36,36 @@ X509ConstructCertificate (
 /**
   Construct a X509 stack object from a list of DER-encoded certificate data.
 
+  If X509Stack is NULL, then return FALSE.
+  If this interface is not supported, then return FALSE.
+
+  @param[in, out]  X509Stack  On input, pointer to an existing or NULL X509 stack object.
+                              On output, pointer to the X509 stack object with new
+                              inserted X509 certificate.
+  @param[in]       Args       VA_LIST marker for the variable argument list.
+                              A list of DER-encoded single certificate data followed
+                              by certificate size. A NULL terminates the list. The
+                              pairs are the arguments to X509ConstructCertificate().
+
+  @retval     TRUE            The X509 stack construction succeeded.
+  @retval     FALSE           The construction operation failed.
+  @retval     FALSE           This interface is not supported.
+
+**/
+BOOLEAN
+EFIAPI
+X509ConstructCertificateStackV (
+  IN OUT  UINT8    **X509Stack,
+  IN      VA_LIST  Args
+  )
+{
+  ASSERT (FALSE);
+  return FALSE;
+}
+
+/**
+  Construct a X509 stack object from a list of DER-encoded certificate data.
+
   Return FALSE to indicate this interface is not supported.
 
   @param[in, out]  X509Stack  On input, pointer to an existing or NULL X509 stack object.
