4a3420cfbe9d6a2145382f5d467f05541232377e
swang112 <sanyo.wang@intel.com>
2019-11-25 15:48:41 +0800

    MdePkg/Include/IndustryStandard: SMBIOS 3.4.0 Update "adding DDR5 definitions".
    Cherry-pick the patch from edk2-staging https://github.com/tianocore/edk2-staging/tree/Smbios34 branch for
    DDR5 definitions support.
    
    Change-Id: I9875029fe82a9ce2c1ba55a9b2610d4f10e759aa
    Hsd-es-id: 1507478478
    Signed-off-by: Wang, Sanyo <sanyo.wang@intel.com>
    Reviewed-on: https://git-amr-7.devtools.intel.com/gerrit/65417
    Tested-by: CR <cr2@intel.com>
    KW: CR <cr2@intel.com>
    Test-Verified: caf_test <caf_test@intel.com>
    Reviewed-by: Dong, Daquan <daquan.dong@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>
    
    SyncScript: BaseCommit is 0c898be65177b8e9dd652ec056f39274da286f89

--- MdePkg/Include/IndustryStandard/SmBios.h
+++ MdePkg/Include/IndustryStandard/SmBios.h
@@ -1718,7 +1718,9 @@ typedef enum {
   MemoryTypeLpddr4                         = 0x1E,
   MemoryTypeLogicalNonVolatileDevice       = 0x1F,
   MemoryTypeHBM                            = 0x20,
-  MemoryTypeHBM2                           = 0x21
+  MemoryTypeHBM2                           = 0x21,
+  MemoryTypeDdr5                           = 0x22,
+  MemoryTypeLpddr5                         = 0x23
 } MEMORY_DEVICE_TYPE;
 
 ///
