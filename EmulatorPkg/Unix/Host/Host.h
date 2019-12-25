e9f6b599fc6a1b076662caf0d6bd81acdfb68d86
GuoMinJ <newexplorerj@gmail.com>
2019-12-25 15:24:16 +0800

    EmulatorPkg: Remove All UGA Support
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=2368
    
    Remove All UGA Support in EmulatorPkg.
    
    Signed-off-by: Guomin Jiang <guomin.jiang@intel.com>
    Cc: Andrew Fish <afish@apple.com>
    Cc: Ray Ni <ray.ni@intel.com>

--- EmulatorPkg/Unix/Host/Host.h
+++ EmulatorPkg/Unix/Host/Host.h
@@ -1,6 +1,6 @@
 /*++ @file
 
-Copyright (c) 2006 - 2019, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2006 - 2021, Intel Corporation. All rights reserved.<BR>
 Portions copyright (c) 2008 - 2011, Apple Inc. All rights reserved.<BR>
 
 SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -106,7 +106,6 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Protocol/SimplePointer.h>
 #include <Protocol/SimpleTextIn.h>
 #include <Protocol/SimpleTextInEx.h>
-#include <Protocol/UgaDraw.h>
 #include <Protocol/SimpleFileSystem.h>
 
 #include <Protocol/EmuThunk.h>
