88870da843c3868f925c41a4dfc814061e1a834c
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2020-01-06 16:01:20 +0100

    MdeModulePkg: remove EnterS3WithImmediateWake () from ResetSystemLib
    EnterS3WithImmediateWake () no longer has any callers, so remove it
    from ResetSystemLib.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Ray Ni <ray.ni@intel.com>

--- MdeModulePkg/Library/PeiResetSystemLib/PeiResetSystemLib.c
+++ MdeModulePkg/Library/PeiResetSystemLib/PeiResetSystemLib.c
@@ -58,19 +58,6 @@ ResetShutdown (
 }
 
 /**
-  This function causes the system to enter S3 and then wake up immediately.
-
-  If this function returns, it means that the system does not support S3 feature.
-**/
-VOID
-EFIAPI
-EnterS3WithImmediateWake (
-  VOID
-  )
-{
-}
-
-/**
   This function causes a systemwide reset. The exact type of the reset is
   defined by the EFI_GUID that follows the Null-terminated Unicode string passed
   into ResetData. If the platform does not recognize the EFI_GUID in ResetData
