5e8c82c99280705dae7971c3b715460d6cd11493
Albecki, Mateusz <mateusz.albecki@intel.com>
2020-01-14 20:05:27 +0800

    MdeModulePkg/SdMmcPciHcDxe: Fix DAT lane SW reset
    Driver used to reset the DAT lane on a current error which
    is not required according to SD specification(it's not going
    to help). This patch will reset the DAT lane only on DAT
    lane specific errors.
    
    Cc: Hao A Wu <hao.a.wu@intel.com>
    Cc: Marcin Wojtas <mw@semihalf.com>
    Cc: Zhichao Gao <zhichao.gao@intel.com>
    Cc: Liming Gao <liming.gao@intel.com>
    
    Signed-off-by: Mateusz Albecki <mateusz.albecki@intel.com>
    Reviewed-by: Hao A Wu <hao.a.wu@intel.com>

--- MdeModulePkg/Bus/Pci/SdMmcPciHcDxe/SdMmcPciHci.c
+++ MdeModulePkg/Bus/Pci/SdMmcPciHcDxe/SdMmcPciHci.c
@@ -7,7 +7,7 @@
   It would expose EFI_SD_MMC_PASS_THRU_PROTOCOL for upper layer use.
 
   Copyright (c) 2018-2019, NVIDIA CORPORATION. All rights reserved.
-  Copyright (c) 2015 - 2019, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2015 - 2020, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -2229,7 +2229,7 @@ SdMmcCheckTrbResult (
     if ((IntStatus & 0x0F) != 0) {
       SwReset |= BIT1;
     }
-    if ((IntStatus & 0xF0) != 0) {
+    if ((IntStatus & 0x70) != 0) {
       SwReset |= BIT2;
     }
 
