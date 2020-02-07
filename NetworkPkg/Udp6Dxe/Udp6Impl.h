8af99b448b95f5ea4d54d0de48c42e7515f29dbc
Antoine Coeur <coeur@gmx.fr>
2020-02-07 02:08:04 +0100

    NetworkPkg/Udp6Dxe: Fix various typos
    Fix various typos in comments and documentation.
    
    Cc: Jiaxin Wu <jiaxin.wu@intel.com>
    Cc: Siyuan Fu <siyuan.fu@intel.com>
    Cc: Maciej Rabeda <maciej.rabeda@intel.com>
    Signed-off-by: Antoine Coeur <coeur@gmx.fr>
    Reviewed-by: Philippe Mathieu-Daude <philmd@redhat.com>
    Reviewed-by: Maciej Rabeda <maciej.rabeda@intel.com>
    Signed-off-by: Philippe Mathieu-Daude <philmd@redhat.com>
    Message-Id: <20200207010831.9046-52-philmd@redhat.com>

--- NetworkPkg/Udp6Dxe/Udp6Impl.h
+++ NetworkPkg/Udp6Dxe/Udp6Impl.h
@@ -151,7 +151,7 @@ Udp6CleanInstance (
   );
 
 /**
-  This function intializes the new created udp instance.
+  This function initializes the new created udp instance.
 
   @param[in]      Udp6Service      Pointer to the UDP6_SERVICE_DATA.
   @param[in, out]  Instance         Pointer to the un-initialized UDP6_INSTANCE_DATA.
@@ -221,7 +221,7 @@ Udp6GetModeData (
 
   @retval EFI_SUCCESS            The configuration settings were set, changed, or
                                  reset successfully.
-  @retval EFI_NO_MAPPING         When the UdpConifgData.UseAnyStationAddress is set
+  @retval EFI_NO_MAPPING         When the UdpConfigData.UseAnyStationAddress is set
                                  to true  and there is no address available for IP6
                                  driver to binding  source address to this
                                  instance.
@@ -281,12 +281,12 @@ Udp6Configure (
                                  .FragmentBuffer fields is NULL.
                                  One or more of the
                                  Token.Packet.TxData.UdpSessionData.
-                                 DestinationAddres are not valid unicast IPv6
+                                 DestinationAddress are not valid unicast IPv6
                                  addresses, if the  UdpSessionData is not NULL.
                                  Token.Packet.TxData.UdpSessionData.
-                                 DestinationAddres is NULL
+                                 DestinationAddress is NULL
                                  Token.Packet.TxData.UdpSessionData.
-                                 DestinatioPort is zero.
+                                 DestinationPort is zero.
                                  Token.Packet.TxData.UdpSessionData is
                                  NULL and this  instance's
                                  UdpConfigData.RemoteAddress is unspecified.
@@ -430,7 +430,7 @@ Udp6Groups (
 
 /**
   This function tries to bind the udp instance according to the configured port
-  allocation stragety.
+  allocation strategy.
 
   @param[in]  InstanceList       Pointer to the head of the list linking the udp
                                  instances.
