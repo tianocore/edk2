5cb3e16a54c04da4af711c411a4aba81810bcd91
Siyuan Fu <siyuan.fu@intel.com>
2020-02-11 21:30:48 +0800

    UefiCpuPkg: Remove FIT based microcode shadow logic from MpInitLib.
    (cherry picked from commit c788c2b1ade657e01a63001e8fee621001404af7)
    
    Commit c7c964b and dd01704 add header file for FIT table and update
    MpInitLib to support FIT based microcode shadow operation. There are
    comments that FIT is Intel specific specification instead of industry
    standard, which should not be placed in EDK2 MdePkg and UefiCpuPkg.
    So this patch adds a platform PPI for the microcode shadow logic, and
    remove the FIT related code from EDK2.
    The FIT based microcode shadow support will be implemented as a new
    platform PEIM in IntelSiliconPkg in edk2-platforms.
    This patch doesn't provide a DXE version shadow microcode protocol,
    a platform which only uses DxeMpInitLib instance only supports PCD
    based microcode shadowing.
    
    A detailed design doc can be found here:
    https://edk2.groups.io/g/devel/files/Designs/2020/0214/Support%20
    the%202nd%20Microcode%20FV%20Flash%20Region.pdf
    
    TEST: Tested on FIT enabled platform.
    BZ: https://tianocore.acgmultimedia.com/show_bug.cgi?id=2449
    
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Siyuan Fu <siyuan.fu@intel.com>
    Reviewed-by: Michael D Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Ray Ni <ray.ni@intel.com>
    Acked-by: Laszlo Ersek <lersek@redhat.com>
    (cherry picked from commit c788c2b1ade657e01a63001e8fee621001404af7)

--- UefiCpuPkg/Library/MpInitLib/DxeMpLib.c
+++ UefiCpuPkg/Library/MpInitLib/DxeMpLib.c
@@ -1,7 +1,7 @@
 /** @file
   MP initialize support functions for DXE phase.
 
-  Copyright (c) 2016 - 2019, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2016 - 2020, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -815,4 +815,28 @@ MpInitLibEnableDisableAP (
   }
 
   return Status;
+}
+
+/**
+  This funtion will try to invoke platform specific microcode shadow logic to
+  relocate microcode update patches into memory.
+
+  @param[in] CpuMpData  The pointer to CPU MP Data structure.
+
+  @retval EFI_SUCCESS              Shadow microcode success.
+  @retval EFI_OUT_OF_RESOURCES     No enough resource to complete the operation.
+  @retval EFI_UNSUPPORTED          Can't find platform specific microcode shadow
+                                   PPI/Protocol.
+**/
+EFI_STATUS
+PlatformShadowMicrocode (
+  IN OUT CPU_MP_DATA             *CpuMpData
+  )
+{
+  //
+  // There is no DXE version of platform shadow microcode protocol so far.
+  // A platform which only uses DxeMpInitLib instance could only supports
+  // the PCD based microcode shadowing.
+  //
+  return EFI_UNSUPPORTED;
 }
