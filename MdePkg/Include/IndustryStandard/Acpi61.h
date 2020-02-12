45aaa0f93ce7f1f06590c7a70fb860ef389196d7
Krzysztof Koch <krzysztof.koch@arm.com>
2020-02-13 01:27:39 +0800

    MdePkg: Add PCCT table signature definition
    The Platform Communications Channel Table (PCCT) was defined in:
    ACPI Specification Version 5.0, Errata A - Published Nov. 13, 2013.
    
    Starting from the Acpi50.h header file, there are definitions
    describing the table but a macro with the table's signature is missing.
    
    This patch adds the definition of Platform Communications Channel
    Table's signature to the relevant ACPI header files.
    
    Signed-off-by: Krzysztof Koch <krzysztof.koch@arm.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>

--- MdePkg/Include/IndustryStandard/Acpi61.h
+++ MdePkg/Include/IndustryStandard/Acpi61.h
@@ -3,6 +3,7 @@
 
   Copyright (c) 2016 - 2018, Intel Corporation. All rights reserved.<BR>
  (C) Copyright 2016 Hewlett Packard Enterprise Development LP<BR>
+  Copyright (c) 2020, ARM Ltd. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
@@ -2352,6 +2353,11 @@ typedef struct {
 /// "MSDM" MS Data Management Table
 ///
 #define EFI_ACPI_6_1_DATA_MANAGEMENT_TABLE_SIGNATURE  SIGNATURE_32('M', 'S', 'D', 'M')
+
+///
+/// "PCCT" Platform Communications Channel Table
+///
+#define EFI_ACPI_6_1_PLATFORM_COMMUNICATIONS_CHANNEL_TABLE_SIGNATURE  SIGNATURE_32('P', 'C', 'C', 'T')
 
 ///
 /// "SLIC" MS Software Licensing Table Specification
