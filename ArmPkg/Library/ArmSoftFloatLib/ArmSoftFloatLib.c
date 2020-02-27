6d4ffad0d5ae7b0c8479c606bf23544f68c6015f
Laszlo Ersek <lersek@redhat.com>
2020-02-27 22:39:01 +0100

    ArmPkg: convert LFs to CRLF, expand hard TABs
    We're going to switch the internal line terminators globally to LF at some
    point, but until then, let's use CRLF consistently. Convert source files
    with LFs in them to CRLF, using "unix2dos".
    
    "git show -b" prints no code changes for this patch.
    
    (I collected all the file name suffixes in this package, with:
    
    $ git ls-files -- $PACKAGE | rev | cut -f 1 -d . | sort -u | rev
    
    I eliminated those suffixes that didn't stand for text files, then
    blanket-converted the rest with unix2dos. Finally, picked up the actual
    changes with git-add.)
    
    At the same time, the following three files had to undergo TAB expansion:
    
      ArmPkg/Library/ArmSoftFloatLib/ArmSoftFloatLib.c
      ArmPkg/Library/GccLto/liblto-aarch64.s
      ArmPkg/Library/GccLto/liblto-arm.s
    
    I used "expand -t 2", in order to stay close to the edk2 coding style
    (which uses two spaces for indentation.)
    
    Both the CRLF conversion and the TAB expansion are motivated by
    "PatchCheck.py". "PatchCheck.py" is also the reason why CRLF conversion
    and TAB expansion have to happen in the same patch.
    
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Leif Lindholm <leif@nuviainc.com>
    Cc: Philippe Mathieu-Daudé <philmd@redhat.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=1659
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Message-Id: <20200227213903.13884-2-lersek@redhat.com>
    Acked-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Philippe Mathieu-Daude <philmd@redhat.com>

--- ArmPkg/Library/ArmSoftFloatLib/ArmSoftFloatLib.c
+++ ArmPkg/Library/ArmSoftFloatLib/ArmSoftFloatLib.c
@@ -27,30 +27,30 @@ typedef uint64_t aeabi_double_t;
  */
 static aeabi_float_t f32_to_f(float32_t val)
 {
-	return val.v;
+  return val.v;
 }
 
 static float32_t f32_from_f(aeabi_float_t val)
 {
-	float32_t res;
+  float32_t res;
 
-	res.v = val;
+  res.v = val;
 
-	return res;
+  return res;
 }
 
 static aeabi_double_t f64_to_d(float64_t val)
 {
-	return val.v;
+  return val.v;
 }
 
 static float64_t f64_from_d(aeabi_double_t val)
 {
-	float64_t res;
+  float64_t res;
 
-	res.v = val;
+  res.v = val;
 
-	return res;
+  return res;
 }
 
 /*
@@ -67,28 +67,28 @@ static float64_t f64_from_d(aeabi_double_t val)
 
 aeabi_double_t __aeabi_dadd(aeabi_double_t a, aeabi_double_t b)
 {
-	return f64_to_d(f64_add(f64_from_d(a), f64_from_d(b)));
+  return f64_to_d(f64_add(f64_from_d(a), f64_from_d(b)));
 }
 
 aeabi_double_t __aeabi_ddiv(aeabi_double_t a, aeabi_double_t b)
 {
-	return f64_to_d(f64_div(f64_from_d(a), f64_from_d(b)));
+  return f64_to_d(f64_div(f64_from_d(a), f64_from_d(b)));
 }
 
 aeabi_double_t __aeabi_dmul(aeabi_double_t a, aeabi_double_t b)
 {
-	return f64_to_d(f64_mul(f64_from_d(a), f64_from_d(b)));
+  return f64_to_d(f64_mul(f64_from_d(a), f64_from_d(b)));
 }
 
 
 aeabi_double_t __aeabi_drsub(aeabi_double_t a, aeabi_double_t b)
 {
-	return f64_to_d(f64_sub(f64_from_d(b), f64_from_d(a)));
+  return f64_to_d(f64_sub(f64_from_d(b), f64_from_d(a)));
 }
 
 aeabi_double_t __aeabi_dsub(aeabi_double_t a, aeabi_double_t b)
 {
-	return f64_to_d(f64_sub(f64_from_d(a), f64_from_d(b)));
+  return f64_to_d(f64_sub(f64_from_d(a), f64_from_d(b)));
 }
 
 /*
@@ -97,27 +97,27 @@ aeabi_double_t __aeabi_dsub(aeabi_double_t a, aeabi_double_t b)
 
 int __aeabi_dcmpeq(aeabi_double_t a, aeabi_double_t b)
 {
-	return f64_eq(f64_from_d(a), f64_from_d(b));
+  return f64_eq(f64_from_d(a), f64_from_d(b));
 }
 
 int __aeabi_dcmplt(aeabi_double_t a, aeabi_double_t b)
 {
-	return f64_lt(f64_from_d(a), f64_from_d(b));
+  return f64_lt(f64_from_d(a), f64_from_d(b));
 }
 
 int __aeabi_dcmple(aeabi_double_t a, aeabi_double_t b)
 {
-	return f64_le(f64_from_d(a), f64_from_d(b));
+  return f64_le(f64_from_d(a), f64_from_d(b));
 }
 
 int __aeabi_dcmpge(aeabi_double_t a, aeabi_double_t b)
 {
-	return f64_le(f64_from_d(b), f64_from_d(a));
+  return f64_le(f64_from_d(b), f64_from_d(a));
 }
 
 int __aeabi_dcmpgt(aeabi_double_t a, aeabi_double_t b)
 {
-	return f64_lt(f64_from_d(b), f64_from_d(a));
+  return f64_lt(f64_from_d(b), f64_from_d(a));
 }
 
 /*
@@ -127,27 +127,27 @@ int __aeabi_dcmpgt(aeabi_double_t a, aeabi_double_t b)
 
 aeabi_float_t __aeabi_fadd(aeabi_float_t a, aeabi_float_t b)
 {
-	return f32_to_f(f32_add(f32_from_f(a), f32_from_f(b)));
+  return f32_to_f(f32_add(f32_from_f(a), f32_from_f(b)));
 }
 
 aeabi_float_t __aeabi_fdiv(aeabi_float_t a, aeabi_float_t b)
 {
-	return f32_to_f(f32_div(f32_from_f(a), f32_from_f(b)));
+  return f32_to_f(f32_div(f32_from_f(a), f32_from_f(b)));
 }
 
 aeabi_float_t __aeabi_fmul(aeabi_float_t a, aeabi_float_t b)
 {
-	return f32_to_f(f32_mul(f32_from_f(a), f32_from_f(b)));
+  return f32_to_f(f32_mul(f32_from_f(a), f32_from_f(b)));
 }
 
 aeabi_float_t __aeabi_frsub(aeabi_float_t a, aeabi_float_t b)
 {
-	return f32_to_f(f32_sub(f32_from_f(b), f32_from_f(a)));
+  return f32_to_f(f32_sub(f32_from_f(b), f32_from_f(a)));
 }
 
 aeabi_float_t __aeabi_fsub(aeabi_float_t a, aeabi_float_t b)
 {
-	return f32_to_f(f32_sub(f32_from_f(a), f32_from_f(b)));
+  return f32_to_f(f32_sub(f32_from_f(a), f32_from_f(b)));
 }
 
 /*
@@ -157,27 +157,27 @@ aeabi_float_t __aeabi_fsub(aeabi_float_t a, aeabi_float_t b)
 
 int __aeabi_fcmpeq(aeabi_float_t a, aeabi_float_t b)
 {
-	return f32_eq(f32_from_f(a), f32_from_f(b));
+  return f32_eq(f32_from_f(a), f32_from_f(b));
 }
 
 int __aeabi_fcmplt(aeabi_float_t a, aeabi_float_t b)
 {
-	return f32_lt(f32_from_f(a), f32_from_f(b));
+  return f32_lt(f32_from_f(a), f32_from_f(b));
 }
 
 int __aeabi_fcmple(aeabi_float_t a, aeabi_float_t b)
 {
-	return f32_le(f32_from_f(a), f32_from_f(b));
+  return f32_le(f32_from_f(a), f32_from_f(b));
 }
 
 int __aeabi_fcmpge(aeabi_float_t a, aeabi_float_t b)
 {
-	return f32_le(f32_from_f(b), f32_from_f(a));
+  return f32_le(f32_from_f(b), f32_from_f(a));
 }
 
 int __aeabi_fcmpgt(aeabi_float_t a, aeabi_float_t b)
 {
-	return f32_lt(f32_from_f(b), f32_from_f(a));
+  return f32_lt(f32_from_f(b), f32_from_f(a));
 }
 
 /*
@@ -186,42 +186,42 @@ int __aeabi_fcmpgt(aeabi_float_t a, aeabi_float_t b)
 
 int __aeabi_d2iz(aeabi_double_t a)
 {
-	return f64_to_i32_r_minMag(f64_from_d(a), false);
+  return f64_to_i32_r_minMag(f64_from_d(a), false);
 }
 
 unsigned __aeabi_d2uiz(aeabi_double_t a)
 {
-	return f64_to_ui32_r_minMag(f64_from_d(a), false);
+  return f64_to_ui32_r_minMag(f64_from_d(a), false);
 }
 
 long long __aeabi_d2lz(aeabi_double_t a)
 {
-	return f64_to_i64_r_minMag(f64_from_d(a), false);
+  return f64_to_i64_r_minMag(f64_from_d(a), false);
 }
 
 unsigned long long __aeabi_d2ulz(aeabi_double_t a)
 {
-	return f64_to_ui64_r_minMag(f64_from_d(a), false);
+  return f64_to_ui64_r_minMag(f64_from_d(a), false);
 }
 
 int __aeabi_f2iz(aeabi_float_t a)
 {
-	return f32_to_i32_r_minMag(f32_from_f(a), false);
+  return f32_to_i32_r_minMag(f32_from_f(a), false);
 }
 
 unsigned __aeabi_f2uiz(aeabi_float_t a)
 {
-	return f32_to_ui32_r_minMag(f32_from_f(a), false);
+  return f32_to_ui32_r_minMag(f32_from_f(a), false);
 }
 
 long long __aeabi_f2lz(aeabi_float_t a)
 {
-	return f32_to_i64_r_minMag(f32_from_f(a), false);
+  return f32_to_i64_r_minMag(f32_from_f(a), false);
 }
 
 unsigned long long __aeabi_f2ulz(aeabi_float_t a)
 {
-	return f32_to_ui64_r_minMag(f32_from_f(a), false);
+  return f32_to_ui64_r_minMag(f32_from_f(a), false);
 }
 
 /*
@@ -230,12 +230,12 @@ unsigned long long __aeabi_f2ulz(aeabi_float_t a)
 
 aeabi_float_t __aeabi_d2f(aeabi_double_t a)
 {
-	return f32_to_f(f64_to_f32(f64_from_d(a)));
+  return f32_to_f(f64_to_f32(f64_from_d(a)));
 }
 
 aeabi_double_t __aeabi_f2d(aeabi_float_t a)
 {
-	return f64_to_d(f32_to_f64(f32_from_f(a)));
+  return f64_to_d(f32_to_f64(f32_from_f(a)));
 }
 
 /*
@@ -244,40 +244,40 @@ aeabi_double_t __aeabi_f2d(aeabi_float_t a)
 
 aeabi_double_t __aeabi_i2d(int a)
 {
-	return f64_to_d(i32_to_f64(a));
+  return f64_to_d(i32_to_f64(a));
 }
 
 aeabi_double_t __aeabi_ui2d(unsigned a)
 {
-	return f64_to_d(ui32_to_f64(a));
+  return f64_to_d(ui32_to_f64(a));
 }
 
 aeabi_double_t __aeabi_l2d(long long a)
 {
-	return f64_to_d(i64_to_f64(a));
+  return f64_to_d(i64_to_f64(a));
 }
 
 aeabi_double_t __aeabi_ul2d(unsigned long long a)
 {
-	return f64_to_d(ui64_to_f64(a));
+  return f64_to_d(ui64_to_f64(a));
 }
 
 aeabi_float_t __aeabi_i2f(int a)
 {
-	return f32_to_f(i32_to_f32(a));
+  return f32_to_f(i32_to_f32(a));
 }
 
 aeabi_float_t __aeabi_ui2f(unsigned a)
 {
-	return f32_to_f(ui32_to_f32(a));
+  return f32_to_f(ui32_to_f32(a));
 }
 
 aeabi_float_t __aeabi_l2f(long long a)
 {
-	return f32_to_f(i64_to_f32(a));
+  return f32_to_f(i64_to_f32(a));
 }
 
 aeabi_float_t __aeabi_ul2f(unsigned long long a)
 {
-	return f32_to_f(ui64_to_f32(a));
+  return f32_to_f(ui64_to_f32(a));
 }
