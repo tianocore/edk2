c219ca726b3a251ca7f836060aecb3994a6bc16f
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2020-02-28 14:13:03 +0100

    OvmfPkg: add GUID for the QEMU kernel loader fs media device path
    (cherry picked from commit 6ae2d31ca250ed1041f260f37b1b3761cbeab8ba)
    
    In an upcoming patch, we will introduce a separate DXE driver that
    exposes the virtual SimpleFileSystem implementation that carries the
    kernel and initrd passed via the QEMU command line, and a separate
    library that consumes it, to be incorporated into the boot manager.
    
    Since the GUID used for the SimpleFileSystem implementation's device
    path will no longer be for internal use only, create a well defined
    GUID to identify the media device path.
    
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=2566
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    (cherry picked from commit 6ae2d31ca250ed1041f260f37b1b3761cbeab8ba)

---
+++ OvmfPkg/Include/Guid/QemuKernelLoaderFsMedia.h
@@ -0,0 +1,18 @@
+/** @file
+  GUID definition for the QEMU LoaderFs media device path, containing the
+  kernel, initrd and command line as file objects
+
+  Copyright (c) 2020, Arm, Ltd. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef QEMU_KERNEL_LOADER_FS_MEDIA_GUID_H__
+#define QEMU_KERNEL_LOADER_FS_MEDIA_GUID_H__
+
+#define QEMU_KERNEL_LOADER_FS_MEDIA_GUID \
+  {0x1428f772, 0xb64a, 0x441e, {0xb8, 0xc3, 0x9e, 0xbd, 0xd7, 0xf8, 0x93, 0xc7}}
+
+extern EFI_GUID gQemuKernelLoaderFsMediaGuid;
+
+#endif
