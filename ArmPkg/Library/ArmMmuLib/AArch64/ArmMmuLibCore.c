edfe4f94531f6d8697ee042b47fc36aadd5cbe03
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2020-03-07 13:35:55 +0100

    ArmPkg/ArmMmuLib AARCH64: preserve attributes when replacing a table entry
    Currently, depending on the size of the region being (re)mapped, the
    page table manipulation code may replace a table entry with a block entry,
    even if the existing table entry uses different mapping attributes to
    describe different parts of the region it covers. This is undesirable, and
    instead, we should avoid doing so unless we are disregarding the original
    attributes anyway. And if we make such a replacement, we should free all
    the page tables that have become orphaned in the process.
    
    So let's implement this, by taking the table entry path through the code
    for block sized regions if a table entry already exists, and the clear
    mask is set (which means we are preserving attributes from the existing
    mapping). And when we do replace a table entry with a block entry, free
    all the pages that are no longer referenced.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Leif Lindholm <leif@nuviainc.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>
    Tested-by: Ashish Singhal <ashishsingha@nvidia.com>
    Tested-by: Laszlo Ersek <lersek@redhat.com>

--- ArmPkg/Library/ArmMmuLib/AArch64/ArmMmuLibCore.c
+++ ArmPkg/Library/ArmMmuLib/AArch64/ArmMmuLibCore.c
@@ -229,8 +229,12 @@ UpdateRegionMappingRecursive (
     // than a block, and recurse to create the block or page entries at
     // the next level. No block mappings are allowed at all at level 0,
     // so in that case, we have to recurse unconditionally.
+    // If we are changing a table entry and the AttributeClearMask is non-zero,
+    // we cannot replace it with a block entry without potentially losing
+    // attribute information, so keep the table entry in that case.
     //
-    if (Level == 0 || ((RegionStart | BlockEnd) & BlockMask) != 0) {
+    if (Level == 0 || ((RegionStart | BlockEnd) & BlockMask) != 0 ||
+        (IsTableEntry (*Entry, Level) && AttributeClearMask != 0)) {
       ASSERT (Level < 3);
 
       if (!IsTableEntry (*Entry, Level)) {
@@ -251,6 +255,8 @@ UpdateRegionMappingRecursive (
           InvalidateDataCacheRange (TranslationTable, EFI_PAGE_SIZE);
         }
 
+        ZeroMem (TranslationTable, EFI_PAGE_SIZE);
+
         if (IsBlockEntry (*Entry, Level)) {
           //
           // We are splitting an existing block entry, so we have to populate
@@ -268,8 +274,6 @@ UpdateRegionMappingRecursive (
             FreePages (TranslationTable, 1);
             return Status;
           }
-        } else {
-          ZeroMem (TranslationTable, EFI_PAGE_SIZE);
         }
       } else {
         TranslationTable = (VOID *)(UINTN)(*Entry & TT_ADDRESS_MASK_BLOCK_ENTRY);
@@ -306,7 +310,20 @@ UpdateRegionMappingRecursive (
       EntryValue |= (Level == 3) ? TT_TYPE_BLOCK_ENTRY_LEVEL3
                                  : TT_TYPE_BLOCK_ENTRY;
 
-      ReplaceTableEntry (Entry, EntryValue, RegionStart, FALSE);
+      if (IsTableEntry (*Entry, Level)) {
+        //
+        // We are replacing a table entry with a block entry. This is only
+        // possible if we are keeping none of the original attributes.
+        // We can free the table entry's page table, and all the ones below
+        // it, since we are dropping the only possible reference to it.
+        //
+        ASSERT (AttributeClearMask == 0);
+        TranslationTable = (VOID *)(UINTN)(*Entry & TT_ADDRESS_MASK_BLOCK_ENTRY);
+        ReplaceTableEntry (Entry, EntryValue, RegionStart, TRUE);
+        FreePageTablesRecursive (TranslationTable, Level + 1);
+      } else {
+        ReplaceTableEntry (Entry, EntryValue, RegionStart, FALSE);
+      }
     }
   }
   return EFI_SUCCESS;
