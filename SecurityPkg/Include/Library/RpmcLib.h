e85784c15048e891f591a93a384687625efadb95
Jian J Wang <jian.j.wang@intel.com>
2020-03-12 13:40:24 +0800

    SecurityPkg: add RpmcLib and VariableKeyLib public headers
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=2594
    
    RpmcLib.h and VariableKeyLib.h are header files required to access RPMC
    device and Key generator from platform. They will be used to ensure the
    integrity and confidentiality of NV variables.
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Chao Zhang <chao.b.zhang@intel.com>
    Cc: Nishant C Mistry <nishant.c.mistry@intel.com>
    Signed-off-by: Jian J Wang <jian.j.wang@intel.com>
    Reviewed-by: Michael Kubacki <michael.kubacki@microsoft.com>
    Reviewed-by: Jiewen Yao <jiewen.yao@intel.com>

---
+++ SecurityPkg/Include/Library/RpmcLib.h
@@ -0,0 +1,42 @@
+/** @file
+  Public definitions for the Replay Protected Monotonic Counter (RPMC) Library.
+
+Copyright (c) 2020, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _RPMC_LIB_H_
+#define _RPMC_LIB_H_
+
+#include <Uefi/UefiBaseType.h>
+
+/**
+  Requests the monotonic counter from the designated RPMC counter.
+
+  @param[out]   CounterValue            A pointer to a buffer to store the RPMC value.
+
+  @retval       EFI_SUCCESS             The operation completed successfully.
+  @retval       EFI_DEVICE_ERROR        A device error occurred while attempting to update the counter.
+  @retval       EFI_UNSUPPORTED         The operation is un-supported.
+**/
+EFI_STATUS
+EFIAPI
+RequestMonotonicCounter (
+  OUT UINT32  *CounterValue
+  );
+
+/**
+  Increments the monotonic counter in the SPI flash device by 1.
+
+  @retval       EFI_SUCCESS             The operation completed successfully.
+  @retval       EFI_DEVICE_ERROR        A device error occurred while attempting to update the counter.
+  @retval       EFI_UNSUPPORTED         The operation is un-supported.
+**/
+EFI_STATUS
+EFIAPI
+IncrementMonotonicCounter (
+  VOID
+  );
+
+#endif
