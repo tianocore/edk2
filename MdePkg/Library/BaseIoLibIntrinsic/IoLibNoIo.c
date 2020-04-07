f87c7ba163f295475eda737aacc9b236732610b3
Abner Chang <abner.chang@hpe.com>
2020-04-07 15:55:56 +0800

    MdePkg/BaseIoLibIntrinsic: Rename IoLibArm.c=>IoLibNoIo.c
    RISC-V MMIO library instance.
    IoLibArm.c in fact implements a generic Mmio-only (and ANSI
    C compliant), so rename it to better reflect this.
    
    Signed-off-by: Abner Chang <abner.chang@hpe.com>
    Co-authored-by: Gilbert Chen <gilbert.chen@hpe.com>
    Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>
    
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Liming Gao <liming.gao@intel.com>
    Cc: Leif Lindholm <leif.lindholm@linaro.org>
    Cc: Gilbert Chen <gilbert.chen@hpe.com>

--- MdePkg/Library/BaseIoLibIntrinsic/IoLibNoIo.c
+++ MdePkg/Library/BaseIoLibIntrinsic/IoLibNoIo.c
@@ -1,9 +1,11 @@
 /** @file
-  I/O Library for ARM.
+  I/O library for non I/O read and write access (memory map I/O read and
+  write only) architecture, such as ARM and RISC-V processor.
 
   Copyright (c) 2006 - 2018, Intel Corporation. All rights reserved.<BR>
   Portions copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
   Copyright (c) 2017, AMD Incorporated. All rights reserved.<BR>
+  Copyright (c) 2020, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
