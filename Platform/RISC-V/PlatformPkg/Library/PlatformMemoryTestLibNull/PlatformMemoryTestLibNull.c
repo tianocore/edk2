dc30984ebd1bf46bb90039d39c6943d3eab63013
Abner Chang <abner.chang@hpe.com>
2020-04-08 15:12:54 +0800

    PlatformPkg/Library: NULL instance of PlatformMemoryTestLib
    (This is migrated from edk2-platforms:Platform/RISC-V)
    NULL instance of PlatformMemoryTestLib.
    
    Signed-off-by: Abner Chang <abner.chang@hpe.com>
    Co-authored-by: Gilbert Chen <gilbert.chen@hpe.com>
    Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>
    
    Cc: Leif Lindholm <leif.lindholm@linaro.org>
    Cc: Gilbert Chen <gilbert.chen@hpe.com>

---
+++ Platform/RISC-V/PlatformPkg/Library/PlatformMemoryTestLibNull/PlatformMemoryTestLibNull.c
@@ -0,0 +1,29 @@
+/** @file
+  Platform Memory Test NULL library
+
+  Copyright (c) 2019, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Uefi.h>
+#include <Protocol/GenericMemoryTest.h>
+
+/**
+  Perform the memory test base on the memory test intensive level,
+  and update the memory resource.
+
+  @param  Level         The memory test intensive level.
+
+  @retval EFI_STATUS    Success test all the system memory and update
+                        the memory resource
+
+**/
+EFI_STATUS
+PlatformBootManagerMemoryTest (
+  IN EXTENDMEM_COVERAGE_LEVEL Level
+  )
+{
+  return EFI_SUCCESS;
+}
