a7ebb6b91f439648f4a8a95095b0789a5ccc8aee
Michael Kubacki <michael.kubacki@microsoft.com>
2020-04-13 16:38:19 -0700

    PrmPkg: Add ALLOCATE_CONTEXT_BUFFER_IN_FW build option
    There's currently two approaches being considered for how to allocate the
    context buffer passed to PRM handlers:
    
    1. The context buffer is allocated and populated in firmware. As such, the
       FW converts all pointers internal to the buffer to virtual memory
       addresses at the virtual address change event. A single context buffer
       pointer is given to the OS via the PRM ACPI table and the OS converts
       this single physical address to a virtual address when it passes the
       context buffer as a pointer to PRM handlers.
    
    2. The context buffer is allocated and populated in the OS. The OS gets
       all the information needed to populate the context buffer from other
       pre-existing resources (mainly physical addresses in the PRM ACPI
       table). The OS converts all the physical addresses to virtual addresses,
       allocates the context buffer instances, and fills in the information.
       The OS passes the context buffer virtual address to PRM handlers.
    
    The prior behavior was (1). The current POR behavior has moved to (2).
    Until (2) is used more widely, it can be kept around with fairly minimal
    overhead via a build flag in a few places.
    
    So the default behavior is now (2) (the expected permanent behavior) with
    (1) easily enabled by defining "ALLOCATE_CONTEXT_BUFFER_IN_FW" in the
    compiler defined macros. A DSC define was added in PrmPkg.dsc to set this
    compiler macro in the package  build.
    
    At some point in the future, all code (and some peripheral code)
    surrounded with this build flag can be removed if (2) is fully
    decided upon.
    
    Cc: Andrew Fish <afish@apple.com>
    Cc: Kang Gao <kang.gao@intel.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Michael Kubacki <michael.kubacki@microsoft.com>
    Cc: Leif Lindholm <leif@nuviainc.com>
    Cc: Benjamin You <benjamin.you@intel.com>
    Cc: Liu Yun <yun.y.liu@intel.com>
    Cc: Ankit Sinha <ankit.sinha@intel.com>
    Cc: Nate DeSimone <nathaniel.l.desimone@intel.com>
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>
    Acked-by: Michael D Kinney <michael.d.kinney@intel.com>
    Acked-by: Liming Gao <gaoliming@byosoft.com.cn>
    Acked-by: Leif Lindholm <quic_llindhol@quicinc.com>
    Reviewed-by: Ankit Sinha <ankit.sinha@intel.com>

--- PrmPkg/Samples/PrmSampleContextBufferModule/Library/DxeContextBufferModuleConfigLib/DxeContextBufferModuleConfigLib.c
+++ PrmPkg/Samples/PrmSampleContextBufferModule/Library/DxeContextBufferModuleConfigLib/DxeContextBufferModuleConfigLib.c
@@ -151,10 +151,26 @@ ContextBufferModuleConfigLibConstructor (
   //
   // Allocate and populate the context buffer
   //
+#ifdef ALLOCATE_CONTEXT_BUFFER_IN_FW
+  //
+  // The context buffer allocated in FW will continue being used at OS runtime so it must
+  // be a runtime services data buffer.
+  //
   // This sample module uses a single context buffer for all the handlers
   // Todo: This can be done more elegantly in the future. Likely though a library service.
   //
   PrmContextBuffer = AllocateRuntimeZeroPool (sizeof (*PrmContextBuffer));
+#else
+  //
+  // This context buffer is not actually used by PRM handler at OS runtime. The OS will allocate
+  // the actual context buffer passed to the PRM handler.
+  //
+  // This context buffer is used internally in the firmware to associate a PRM handler with a
+  // a static data buffer and a runtime MMIO ranges array so those can be placed into the
+  // PRM_HANDLER_INFORMATION_STRUCT and PRM_MODULE_INFORMATION_STRUCT respectively for the PRM handler.
+  //
+  PrmContextBuffer = AllocateZeroPool (sizeof (*PrmContextBuffer));
+#endif
   ASSERT (PrmContextBuffer != NULL);
   if (PrmContextBuffer == NULL) {
     Status = EFI_OUT_OF_RESOURCES;
