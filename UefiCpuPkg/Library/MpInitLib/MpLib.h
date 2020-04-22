7594c420ab165d92240e586e3a2481af8f59fd03
Tom Lendacky <thomas.lendacky@amd.com>
2020-04-23 01:41:54 +0800

    UefiCpuPkg/MpInitLib: Add CPU MP data flag to indicate if SEV-ES is enabled
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=2198
    
    When starting APs in an SMP configuration, the AP needs to know if it is
    running as an SEV-ES guest in order to assign a GHCB page.
    
    Add a field to the CPU_MP_DATA structure that will indicate if SEV-ES is
    enabled. This new field is set during MP library initialization with the
    PCD value PcdSevEsIsEnabled. This flag can then be used to determine if
    SEV-ES is enabled.
    
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- UefiCpuPkg/Library/MpInitLib/MpLib.h
+++ UefiCpuPkg/Library/MpInitLib/MpLib.h
@@ -275,6 +275,8 @@ struct _CPU_MP_DATA {
   // driver.
   //
   BOOLEAN                        WakeUpByInitSipiSipi;
+
+  BOOLEAN                        SevEsIsEnabled;
 };
 
 extern EFI_GUID mCpuInitMpLibHobGuid;
