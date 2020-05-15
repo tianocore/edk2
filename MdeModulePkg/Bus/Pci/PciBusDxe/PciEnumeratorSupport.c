1d6a32fd4eddfb90130c02d85cc4599029d0d711
Patrick Rudolph <patrick.rudolph@9elements.com>
2020-05-15 16:47:18 +0200

    MdeModulePkg: Fix OptionROM scanning
    The Option ROM scanner can't work as enumeration was done by the
    first stage bootloader. Running it will disable the ability of the
    PCIPlatform code to scan for ROMs.
    
    Required for the following patch that enables custom Option ROM
    scanning using gPciPlatformProtocol.
    
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
    Signed-off-by: Marcello Sylvester Bauer <marcello.bauer@9elements.com>
    Cc: Patrick Rudolph <patrick.rudolph@9elements.com>
    Cc: Christian Walter <christian.walter@9elements.com>
    Cc: Hao A Wu <hao.a.wu@intel.com>
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>

--- MdeModulePkg/Bus/Pci/PciBusDxe/PciEnumeratorSupport.c
+++ MdeModulePkg/Bus/Pci/PciBusDxe/PciEnumeratorSupport.c
@@ -2530,10 +2530,12 @@ PciEnumeratorLight (
       //
       RemoveRejectedPciDevices (RootBridgeDev->Handle, RootBridgeDev);
 
-      //
-      // Process option rom light
-      //
-      ProcessOptionRomLight (RootBridgeDev);
+      if (!PcdGetBool (PcdPciDisableBusEnumeration)) {
+        //
+        // Process option rom light
+        //
+        ProcessOptionRomLight (RootBridgeDev);
+      }
 
       //
       // Determine attributes for all devices under this root bridge
