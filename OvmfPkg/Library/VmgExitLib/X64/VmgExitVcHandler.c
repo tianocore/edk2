76623308c1419adc6d9dd3ff2a6255b9e713fd6e
Tom Lendacky <thomas.lendacky@amd.com>
2020-05-18 11:55:34 -0500

    OvmfPkg/VmgExitLib: Add support for MONITOR/MONITORX NAE events
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=2198
    
    Under SEV-ES, a MONITOR/MONITORX intercept generates a #VC exception.
    VMGEXIT must be used to allow the hypervisor to handle this intercept.
    
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- OvmfPkg/Library/VmgExitLib/X64/VmgExitVcHandler.c
+++ OvmfPkg/Library/VmgExitLib/X64/VmgExitVcHandler.c
@@ -844,6 +844,47 @@ MmioExit (
 }
 
 /**
+  Handle a MONITOR event.
+
+  Use the VMGEXIT instruction to handle a MONITOR event.
+
+  @param[in, out] Ghcb             Pointer to the Guest-Hypervisor Communication
+                                   Block
+  @param[in, out] Regs             x64 processor context
+  @param[in]      InstructionData  Instruction parsing context
+
+  @retval 0                        Event handled successfully
+  @retval Others                   New exception value to propagate
+
+**/
+STATIC
+UINT64
+MonitorExit (
+  IN OUT GHCB                     *Ghcb,
+  IN OUT EFI_SYSTEM_CONTEXT_X64   *Regs,
+  IN     SEV_ES_INSTRUCTION_DATA  *InstructionData
+  )
+{
+  UINT64  Status;
+
+  DecodeModRm (Regs, InstructionData);
+
+  Ghcb->SaveArea.Rax = Regs->Rax;  // Identity mapped, so VA = PA
+  GhcbSetRegValid (Ghcb, GhcbRax);
+  Ghcb->SaveArea.Rcx = Regs->Rcx;
+  GhcbSetRegValid (Ghcb, GhcbRcx);
+  Ghcb->SaveArea.Rdx = Regs->Rdx;
+  GhcbSetRegValid (Ghcb, GhcbRdx);
+
+  Status = VmgExit (Ghcb, SVM_EXIT_MONITOR, 0, 0);
+  if (Status) {
+    return Status;
+  }
+
+  return 0;
+}
+
+/**
   Handle a WBINVD event.
 
   Use the VMGEXIT instruction to handle a WBINVD event.
@@ -1492,6 +1533,10 @@ VmgExitHandleVc (
 
   case SVM_EXIT_WBINVD:
     NaeExit = WbinvdExit;
+    break;
+
+  case SVM_EXIT_MONITOR:
+    NaeExit = MonitorExit;
     break;
 
   case SVM_EXIT_NPF:
