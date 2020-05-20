4a4795c934a9bfaf03931f137a00d74de90cc488
Laszlo Ersek <lersek@redhat.com>
2020-05-21 00:58:40 +0200

    OvmfPkg/Tcg2ConfigPei: factor out InternalTpm12Detect()
    Move the calls to the Tpm12RequestUseTpm() and Tpm12SubmitCommand()
    Tpm12DeviceLib functions to a separate C file, so that we can override
    these actions in a subsequent patch.
    
    This code movement requires moving the TPM_RSP_GET_TICKS / TestTpm12()
    helper structure / function too.
    
    While at it, give the TestTpm12() function @retval / @return
    documentation, plus wrap an overlong line in it.
    
    Functionally, this patch is a no-op.
    
    Cc: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Cc: Eric Auger <eric.auger@redhat.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Marc-André Lureau <marcandre.lureau@redhat.com>
    Cc: Philippe Mathieu-Daudé <philmd@redhat.com>
    Cc: Simon Hardy <simon.hardy@itdev.co.uk>
    Cc: Stefan Berger <stefanb@linux.ibm.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=2728
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Message-Id: <20200520225841.17793-3-lersek@redhat.com>
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Tested-by: Ard Biesheuvel <ard.biesheuvel@arm.com>

---
+++ OvmfPkg/Tcg/Tcg2Config/Tpm12Support.c
@@ -0,0 +1,79 @@
+/** @file
+  Implement the InternalTpm12Detect() function on top of the Tpm12DeviceLib
+  class.
+
+  Copyright (C) 2020, Red Hat, Inc.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#include <Library/BaseLib.h>
+#include <Library/Tpm12DeviceLib.h>
+
+#include "Tpm12Support.h"
+
+#pragma pack (1)
+typedef struct {
+  TPM_RSP_COMMAND_HDR   Hdr;
+  TPM_CURRENT_TICKS     CurrentTicks;
+} TPM_RSP_GET_TICKS;
+#pragma pack ()
+
+/**
+  Probe for the TPM for 1.2 version, by sending TPM1.2 GetTicks
+
+  Sending a TPM1.2 command to a TPM2 should return a TPM1.2
+  header (tag = 0xc4) and error code (TPM_BADTAG = 0x1e)
+
+  @retval EFI_SUCCESS  TPM version 1.2 probing successful.
+
+  @return              Error codes propagated from Tpm12SubmitCommand().
+**/
+STATIC
+EFI_STATUS
+TestTpm12 (
+  )
+{
+  EFI_STATUS           Status;
+  TPM_RQU_COMMAND_HDR  Command;
+  TPM_RSP_GET_TICKS    Response;
+  UINT32               Length;
+
+  Command.tag       = SwapBytes16 (TPM_TAG_RQU_COMMAND);
+  Command.paramSize = SwapBytes32 (sizeof (Command));
+  Command.ordinal   = SwapBytes32 (TPM_ORD_GetTicks);
+
+  Length = sizeof (Response);
+  Status = Tpm12SubmitCommand (sizeof (Command), (UINT8 *)&Command, &Length,
+             (UINT8 *)&Response);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  return EFI_SUCCESS;
+}
+
+/**
+  Detect the presence of a TPM with interface version 1.2.
+
+  @retval EFI_SUCCESS      TPM-1.2 available. The Tpm12RequestUseTpm() and
+                           Tpm12SubmitCommand(TPM_ORD_GetTicks) operations
+                           (from the Tpm12DeviceLib class) have succeeded.
+
+  @return                  Error codes propagated from Tpm12RequestUseTpm() and
+                           Tpm12SubmitCommand().
+**/
+EFI_STATUS
+InternalTpm12Detect (
+  VOID
+  )
+{
+  EFI_STATUS Status;
+
+  Status = Tpm12RequestUseTpm ();
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  return TestTpm12 ();
+}
---
+++ OvmfPkg/Tcg/Tcg2Config/Tpm12Support.h
@@ -0,0 +1,30 @@
+/** @file
+  Declare the InternalTpm12Detect() function, hiding the TPM-1.2 detection
+  internals.
+
+  Copyright (C) 2020, Red Hat, Inc.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef TPM12_SUPPORT_H_
+#define TPM12_SUPPORT_H_
+
+#include <Uefi/UefiBaseType.h>
+
+/**
+  Detect the presence of a TPM with interface version 1.2.
+
+  @retval EFI_SUCCESS      TPM-1.2 available. The Tpm12RequestUseTpm() and
+                           Tpm12SubmitCommand(TPM_ORD_GetTicks) operations
+                           (from the Tpm12DeviceLib class) have succeeded.
+
+  @return                  Error codes propagated from Tpm12RequestUseTpm() and
+                           Tpm12SubmitCommand().
+**/
+EFI_STATUS
+InternalTpm12Detect (
+  VOID
+  );
+
+#endif // TPM12_SUPPORT_H_
