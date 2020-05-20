fc72a6ceaa4859ef3043b706100827d4fbcc3bed
Laszlo Ersek <lersek@redhat.com>
2020-05-21 00:58:40 +0200

    OvmfPkg/Tcg2ConfigPei: factor out InternalTpm12Detect()
    Move the calls to the Tpm12RequestUseTpm() and Tpm12SubmitCommand()
    Tpm12DeviceLib functions to a separate C file, so that we can override
    these actions in a subsequent patch.
    
    This code movement requires moving the TPM_RSP_GET_TICKS / TestTpm12()
    helper structure / function too.
    
    While at it, give the TestTpm12() function @retval / @return
    documentation, plus wrap an overlong line in it.
    
    Functionally, this patch is a no-op.
    
    Cc: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Cc: Eric Auger <eric.auger@redhat.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Marc-André Lureau <marcandre.lureau@redhat.com>
    Cc: Philippe Mathieu-Daudé <philmd@redhat.com>
    Cc: Simon Hardy <simon.hardy@itdev.co.uk>
    Cc: Stefan Berger <stefanb@linux.ibm.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=2728
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Message-Id: <20200520225841.17793-3-lersek@redhat.com>
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Tested-by: Ard Biesheuvel <ard.biesheuvel@arm.com>

--- OvmfPkg/Tcg/Tcg2Config/Tcg2ConfigPeim.c
+++ OvmfPkg/Tcg/Tcg2Config/Tcg2ConfigPeim.c
@@ -15,12 +15,12 @@
 #include <PiPei.h>
 
 #include <Guid/TpmInstance.h>
-#include <Library/BaseLib.h>
 #include <Library/DebugLib.h>
 #include <Library/PeiServicesLib.h>
 #include <Library/Tpm2DeviceLib.h>
-#include <Library/Tpm12DeviceLib.h>
 #include <Ppi/TpmInitialized.h>
+
+#include "Tpm12Support.h"
 
 STATIC CONST EFI_PEI_PPI_DESCRIPTOR mTpmSelectedPpi = {
   (EFI_PEI_PPI_DESCRIPTOR_PPI | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
@@ -33,44 +33,6 @@ STATIC CONST EFI_PEI_PPI_DESCRIPTOR  mTpmInitializationDonePpiList = {
   &gPeiTpmInitializationDonePpiGuid,
   NULL
 };
-
-#pragma pack (1)
-
-typedef struct {
-  TPM_RSP_COMMAND_HDR   Hdr;
-  TPM_CURRENT_TICKS     CurrentTicks;
-} TPM_RSP_GET_TICKS;
-
-#pragma pack ()
-
-/**
-  Probe for the TPM for 1.2 version, by sending TPM1.2 GetTicks
-
-  Sending a TPM1.2 command to a TPM2 should return a TPM1.2
-  header (tag = 0xc4) and error code (TPM_BADTAG = 0x1e)
-**/
-static
-EFI_STATUS
-TestTpm12 (
-  )
-{
-  EFI_STATUS           Status;
-  TPM_RQU_COMMAND_HDR  Command;
-  TPM_RSP_GET_TICKS    Response;
-  UINT32               Length;
-
-  Command.tag       = SwapBytes16 (TPM_TAG_RQU_COMMAND);
-  Command.paramSize = SwapBytes32 (sizeof (Command));
-  Command.ordinal   = SwapBytes32 (TPM_ORD_GetTicks);
-
-  Length = sizeof (Response);
-  Status = Tpm12SubmitCommand (sizeof (Command), (UINT8 *)&Command, &Length, (UINT8 *)&Response);
-  if (EFI_ERROR (Status)) {
-    return Status;
-  }
-
-  return EFI_SUCCESS;
-}
 
 /**
   The entry point for Tcg2 configuration driver.
@@ -90,8 +52,8 @@ Tcg2ConfigPeimEntryPoint (
 
   DEBUG ((DEBUG_INFO, "%a\n", __FUNCTION__));
 
-  Status = Tpm12RequestUseTpm ();
-  if (!EFI_ERROR (Status) && !EFI_ERROR (TestTpm12 ())) {
+  Status = InternalTpm12Detect ();
+  if (!EFI_ERROR (Status)) {
     DEBUG ((DEBUG_INFO, "%a: TPM1.2 detected\n", __FUNCTION__));
     Size = sizeof (gEfiTpmDeviceInstanceTpm12Guid);
     Status = PcdSetPtrS (
