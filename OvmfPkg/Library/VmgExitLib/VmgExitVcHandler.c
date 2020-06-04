906ed51838fe4382bf6dc0ef49bc0af0f6095ea6
Tom Lendacky <thomas.lendacky@amd.com>
2020-06-04 12:38:42 -0500

    OvmfPkg/VmgExitLib: Add support for WBINVD NAE events
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=2198
    
    Under SEV-ES, a WBINVD intercept generates a #VC exception. VMGEXIT must be
    used to allow the hypervisor to handle this intercept.
    
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Acked-by: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- OvmfPkg/Library/VmgExitLib/VmgExitVcHandler.c
+++ OvmfPkg/Library/VmgExitLib/VmgExitVcHandler.c
@@ -855,6 +855,31 @@ MmioExit (
 }
 
 /**
+  Handle a WBINVD event.
+
+  Use the VMGEXIT instruction to handle a WBINVD event.
+
+  @param[in, out] Ghcb             Pointer to the Guest-Hypervisor Communication
+                                   Block
+  @param[in, out] Regs             x64 processor context
+  @param[in]      InstructionData  Instruction parsing context
+
+  @return 0                        Event handled successfully
+  @return Others                   New exception value to propagate
+
+**/
+STATIC
+UINT64
+WbinvdExit (
+  IN OUT GHCB                     *Ghcb,
+  IN OUT EFI_SYSTEM_CONTEXT_X64   *Regs,
+  IN     SEV_ES_INSTRUCTION_DATA  *InstructionData
+  )
+{
+  return VmgExit (Ghcb, SVM_EXIT_WBINVD, 0, 0);
+}
+
+/**
   Handle an MSR event.
 
   Use the VMGEXIT instruction to handle either a RDMSR or WRMSR event.
@@ -1248,6 +1273,10 @@ VmgExitHandleVc (
 
   case SVM_EXIT_MSR:
     NaeExit = MsrExit;
+    break;
+
+  case SVM_EXIT_WBINVD:
+    NaeExit = WbinvdExit;
     break;
 
   case SVM_EXIT_NPF:
