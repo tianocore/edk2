ecc5c67b4851dfa93647726b9ea8db10a563a659
Tomas Pilar <Tomas.Pilar@arm.com>
2020-06-19 12:59:54 +0100

    SyncScript: Revert override commits.
    (cherry picked from commit 422fe85cc3109684c81990911c79dc18d1828d96)
    
    (cherry picked from commit e46e3040fc3b39dcade8b88cdc02565ff6b26374)
    
    (cherry picked from commit e18ac66d84e68387dbbe4fd8761b8f3a889534a9)
    
    (cherry picked from commit cae974bea25a3193360390af0b405488a19e1e15)
    
    (cherry picked from commit 0f01cec52f4794777feb067e4fa0bfcedfdc124e)
    
    (cherry picked from commit acfd55579542de64418cd4fb1c5560524a79d61f)
    
    (cherry picked from commit 0060e0a694f3f249c3ec081b0e61287c36f64ebb)
    
    (cherry picked from commit 654dc3ed852aafa126e9d539b7002db348dd6eb0)
    
    (cherry picked from commit a4a2258a1fec66665481b0bd929b049921cb07a0)
    
    (cherry picked from commit 20286e168b6bd68901b62fea0dde7d712175374f)
    
    (cherry picked from commit 3900a63e3a1b9ba9a4105bedead7b986188cec2c)
    
    (cherry picked from commit 03013d999c85ac15e03ad39d589f7ae2bb7b026f)

--- ShellPkg/Library/UefiShellAcpiViewCommandLib/UefiShellAcpiViewCommandLib.h
+++ ShellPkg/Library/UefiShellAcpiViewCommandLib/UefiShellAcpiViewCommandLib.h
@@ -1,7 +1,7 @@
 /** @file
   Header file for 'acpiview' Shell command functions.
 
-  Copyright (c) 2016 - 2017, ARM Limited. All rights reserved.<BR>
+  Copyright (c) 2016 - 2020, ARM Limited. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
@@ -13,8 +13,12 @@ extern EFI_HII_HANDLE gShellAcpiViewHiiHandle;
 /**
   Function for 'acpiview' command.
 
-  @param[in] ImageHandle  Handle to the Image (NULL if Internal).
-  @param[in] SystemTable  Pointer to the System Table (NULL if Internal).
+  @param[in] ImageHandle  Handle to the Image (NULL if internal).
+  @param[in] SystemTable  Pointer to the System Table (NULL if internal).
+
+  @retval SHELL_INVALID_PARAMETER The command line invocation could not be parsed
+  @retval SHELL_NOT_FOUND         The command failed
+  @retval SHELL_SUCCESS           The command was successful
 **/
 SHELL_STATUS
 EFIAPI
