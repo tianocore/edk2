869dbba4de17646085df8d76d1a0e2b1f5e09bd9
edk2buildsystem <edk2buildsystem@intel.com>
2020-06-21 20:05:38 -0700

    SyncScript: Revert override commits.

--- MdeModulePkg/Bus/Pci/PciBusDxe/PciLib.c
+++ MdeModulePkg/Bus/Pci/PciBusDxe/PciLib.c
@@ -1055,7 +1055,7 @@ PciScanBus (
                 );
 
       if (EFI_ERROR (Status)) {
-        break;
+        continue;
       }
 
       PciAddress = EFI_PCI_ADDRESS (StartBusNumber, Device, Func, 0);
