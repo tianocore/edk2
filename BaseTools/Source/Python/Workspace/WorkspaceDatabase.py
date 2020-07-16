723620bccb947e8d831d37bde4112492b203ff68
Bob Feng <bob.c.feng@intel.com>
2020-07-16 19:22:04 +0800

    BaseTools: Remove the file timestamp checking during parsing
    (cherry picked from commit b3ee616e67d3fac426f80a4f3f2d7738c6fef779)
    
    During build, the meta files are not changed, so it's no need
    to check file timestamp.
    
    This patch is to remove useless logic.
    
    Signed-off-by: Bob Feng <bob.c.feng@intel.com>
    Cc: Liming Gao <liming.gao@intel.com>
    
    Reviewed-by: Liming Gao <liming.gao@intel.com>
    (cherry picked from commit b3ee616e67d3fac426f80a4f3f2d7738c6fef779)

--- BaseTools/Source/Python/Workspace/WorkspaceDatabase.py
+++ BaseTools/Source/Python/Workspace/WorkspaceDatabase.py
@@ -158,12 +158,6 @@ class WorkspaceDatabase(object):
         self.BuildObject = WorkspaceDatabase.BuildObjectFactory(self)
         self.TransformObject = WorkspaceDatabase.TransformObjectFactory(self)
 
-    def SetFileTimeStamp(self,FileId,TimeStamp):
-        self.TblFile[FileId-1][6] = TimeStamp
-
-    def GetFileTimeStamp(self,FileId):
-        return self.TblFile[FileId-1][6]
-
 
     ## Summarize all packages in the database
     def GetPackageList(self, Platform, Arch, TargetName, ToolChainTag):
@@ -192,16 +186,6 @@ class WorkspaceDatabase(object):
             PackageList.append(Package)
 
         return PackageList
-
-    ## Summarize all platforms in the database
-    def PlatformList(self):
-        RetVal = []
-        for PlatformFile in [item[3] for item in self.TblFile if item[5] == MODEL_FILE_DSC]:
-            try:
-                RetVal.append(self.BuildObject[PathClass(PlatformFile), TAB_COMMON])
-            except:
-                pass
-        return RetVal
 
     def MapPlatform(self, Dscfile):
         Platform = self.BuildObject[PathClass(Dscfile), TAB_COMMON]
