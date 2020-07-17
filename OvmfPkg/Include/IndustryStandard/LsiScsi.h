79f802a50ee942e297c86138e6c19cab77a3a2eb
Gary Lin <glin@suse.com>
2020-07-17 14:11:23 +0800

    OvmfPkg/LsiScsiDxe: Probe PCI devices and look for LsiScsi
    Implement LsiScsiControllerSupported() to probe the PCI ID and look for
    LSI 53C895A.
    
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Signed-off-by: Gary Lin <glin@suse.com>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    Message-Id: <20200717061130.8881-5-glin@suse.com>

---
+++ OvmfPkg/Include/IndustryStandard/LsiScsi.h
@@ -0,0 +1,20 @@
+/** @file
+
+  Macros and type definitions for LSI 53C895A SCSI devices.
+
+  Copyright (C) 2020, SUSE LLC.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _LSI_SCSI_H_
+#define _LSI_SCSI_H_
+
+//
+// Device ID
+//
+#define LSI_LOGIC_PCI_VENDOR_ID   0x1000
+#define LSI_53C895A_PCI_DEVICE_ID 0x0012
+
+#endif // _LSI_SCSI_H_
