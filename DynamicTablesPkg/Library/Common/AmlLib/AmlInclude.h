292e5408547767553e838b3e825a49c338224c16
Pierre Gondois <pierre.gondois@arm.com>
2020-07-29 13:11:20 +0100

    DynamicTablesPkg: AmlLib definitions
    Dynamic AML is a solution to generate Definition Block tables
    at runtime. Dynamic AML provides the following techniques for
    generating AML tables.
      - AML Fixup
      - AML Codegen
      - AML Fixup + Codegen
    
    AML fixup involves patching small sections of a template AML
    code at runtime, while AML Codegen provides APIs to generate
    small sections of AML code at runtime. A combination of
    Fixup and Codegen can also be used.
    
    AML has a complex grammar. To simplify the generation of
    AML tables, Dynamic AML introduces AmlLib that provides a
    rich set of APIs for parsing, traversing, fixup, codegen
    and serialisation of AML byte code.
    
    This patch introduces the definitions used by AmlLib.
    
    Signed-off-by: Pierre Gondois <pierre.gondois@arm.com>
    Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>
    Reviewed-by: Alexei Fedorov <Alexei.Fedorov@arm.com>

---
+++ DynamicTablesPkg/Library/Common/AmlLib/AmlInclude.h
@@ -0,0 +1,18 @@
+/** @file
+  AML Include file
+
+  Copyright (c) 2019 - 2020, Arm Limited. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef AML_INCLUDE_H_
+#define AML_INCLUDE_H_
+
+#include <Base.h>
+#include <Library/BaseLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
+#include <Library/MemoryAllocationLib.h>
+
+#endif // AML_INCLUDE_H_
