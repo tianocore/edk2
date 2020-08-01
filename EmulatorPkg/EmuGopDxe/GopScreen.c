96971dfa8606a064e60ec16f9d3f05f65e7a9540
Michael D Kinney <michael.d.kinney@intel.com>
2020-07-31 17:47:26 -0700

    EmulatorPkg: Fix spelling mistake for occurred
    (cherry picked from commit f034c05cc40bf4033b23f4c69285e14d73ed6d1d)
    
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=2361
    
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Andrew Fish <afish@apple.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Guomin Jiang <guomin.jiang@intel.com>
    Reviewed-by: Liming Gao <liming.gao@intel.com>
    Reviewed-by: Ray Ni <ray.ni@intel.com>
    (cherry picked from commit f034c05cc40bf4033b23f4c69285e14d73ed6d1d)
    
    Change-Id: I1d5892786fe28660567dda3b45eab3118002b07b

--- EmulatorPkg/EmuGopDxe/GopScreen.c
+++ EmulatorPkg/EmuGopDxe/GopScreen.c
@@ -177,7 +177,7 @@ EmuGopSetMode (
 
   @retval EFI_SUCCESS           The Blt operation completed.
   @retval EFI_INVALID_PARAMETER BltOperation is not valid.
-  @retval EFI_DEVICE_ERROR      A hardware error occured writting to the video buffer.
+  @retval EFI_DEVICE_ERROR      A hardware error occurred writting to the video buffer.
 
 **/
 EFI_STATUS
