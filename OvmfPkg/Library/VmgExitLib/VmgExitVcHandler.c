5894fb1fa2a4a977c9c8cec16f24d9e621a7e09b
Tom Lendacky <thomas.lendacky@amd.com>
2020-08-12 15:21:38 -0500

    OvmfPkg/VmgExitLib: Add support for RDPMC NAE events
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=2198
    
    Under SEV-ES, a RDPMC intercept generates a #VC exception. VMGEXIT must be
    used to allow the hypervisor to handle this intercept.
    
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Acked-by: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Regression-tested-by: Laszlo Ersek <lersek@redhat.com>

--- OvmfPkg/Library/VmgExitLib/VmgExitVcHandler.c
+++ OvmfPkg/Library/VmgExitLib/VmgExitVcHandler.c
@@ -1224,6 +1224,48 @@ CpuidExit (
 }
 
 /**
+  Handle a RDPMC event.
+
+  Use the VMGEXIT instruction to handle a RDPMC event.
+
+  @param[in, out] Ghcb             Pointer to the Guest-Hypervisor Communication
+                                   Block
+  @param[in, out] Regs             x64 processor context
+  @param[in]      InstructionData  Instruction parsing context
+
+  @retval 0                        Event handled successfully
+  @return                          New exception value to propagate
+
+**/
+STATIC
+UINT64
+RdpmcExit (
+  IN OUT GHCB                     *Ghcb,
+  IN OUT EFI_SYSTEM_CONTEXT_X64   *Regs,
+  IN     SEV_ES_INSTRUCTION_DATA  *InstructionData
+  )
+{
+  UINT64  Status;
+
+  Ghcb->SaveArea.Rcx = Regs->Rcx;
+  GhcbSetRegValid (Ghcb, GhcbRcx);
+
+  Status = VmgExit (Ghcb, SVM_EXIT_RDPMC, 0, 0);
+  if (Status != 0) {
+    return Status;
+  }
+
+  if (!GhcbIsRegValid (Ghcb, GhcbRax) ||
+      !GhcbIsRegValid (Ghcb, GhcbRdx)) {
+    return UnsupportedExit (Ghcb, Regs, InstructionData);
+  }
+  Regs->Rax = Ghcb->SaveArea.Rax;
+  Regs->Rdx = Ghcb->SaveArea.Rdx;
+
+  return 0;
+}
+
+/**
   Handle a RDTSC event.
 
   Use the VMGEXIT instruction to handle a RDTSC event.
@@ -1308,6 +1350,10 @@ VmgExitHandleVc (
   switch (ExitCode) {
   case SVM_EXIT_RDTSC:
     NaeExit = RdtscExit;
+    break;
+
+  case SVM_EXIT_RDPMC:
+    NaeExit = RdpmcExit;
     break;
 
   case SVM_EXIT_CPUID:
