c4b8c399f1a11112cbeae38d1be9add8270fefcb
Jiewen Yao <jiewen.yao@intel.com>
2020-08-18 14:26:12 +0800

    IntelFsp2WrapperPkg/FspMeasurementLib: Add header file.
    (cherry picked from commit 17f65e4063f4b2053830ebcbc0cea32bb476813a)
    
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=2376
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Chasel Chiu <chasel.chiu@intel.com>
    Cc: Nate DeSimone <nathaniel.l.desimone@intel.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Qi Zhang <qi1.zhang@intel.com>
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>
    Message-Id: <20200818062618.3698-3-qi1.zhang@intel.com>
    Reviewed-by: Jian J Wang <jian.j.wang@intel.com>
    Reviewed-by: Chasel Chiu <chasel.chiu@intel.com>
    (cherry picked from commit 17f65e4063f4b2053830ebcbc0cea32bb476813a)
    
    Change-Id: Id048fde40f1d7f4d11c6651f2d7c9a888e1a8ca9

---
+++ IntelFsp2WrapperPkg/Include/Library/FspMeasurementLib.h
@@ -0,0 +1,39 @@
+/** @file
+  This library is used by FSP modules to measure data to TPM.
+
+Copyright (c) 2020, Intel Corporation. All rights reserved. <BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _FSP_MEASUREMENT_LIB_H_
+#define _FSP_MEASUREMENT_LIB_H_
+
+#define FSP_MEASURE_FSP       BIT0
+#define FSP_MEASURE_FSPT      BIT1
+#define FSP_MEASURE_FSPM      BIT2
+#define FSP_MEASURE_FSPS      BIT3
+#define FSP_MEASURE_FSPUPD    BIT31
+
+/**
+  Measure a FSP FirmwareBlob.
+
+  @param[in]  PcrIndex                PCR Index.
+  @param[in]  Description             Description for this FirmwareBlob.
+  @param[in]  FirmwareBlobBase        Base address of this FirmwareBlob.
+  @param[in]  FirmwareBlobLength      Size in bytes of this FirmwareBlob.
+
+  @retval EFI_SUCCESS           Operation completed successfully.
+  @retval EFI_UNSUPPORTED       TPM device not available.
+  @retval EFI_OUT_OF_RESOURCES  Out of memory.
+  @retval EFI_DEVICE_ERROR      The operation was unsuccessful.
+*/
+EFI_STATUS
+EFIAPI
+MeasureFspFirmwareBlob (
+  IN UINT32                         PcrIndex,
+  IN CHAR8                          *Description OPTIONAL,
+  IN EFI_PHYSICAL_ADDRESS           FirmwareBlobBase,
+  IN UINT64                         FirmwareBlobLength
+  );
+#endif
