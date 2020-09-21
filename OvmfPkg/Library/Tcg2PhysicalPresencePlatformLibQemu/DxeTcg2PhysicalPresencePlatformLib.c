c4085da24d31429c6957fbed7e12e9aad609963f
Patrick Rudolph <patrick.rudolph@9elements.com>
2020-09-21 11:54:25 +0200

    OvmfPkg: Introduce Tcg2PhysicalPresencePlatformLib
    Move the QEMU TPM PPI config retrieval into a platform library class so Tcg2PhysicalPresenceLibQemu can be reused across platforms.
    
    Also add a user-confirmation timeout to avoid hanging indefinitely on headless systems.
    
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

---
+++ OvmfPkg/Library/Tcg2PhysicalPresencePlatformLibQemu/DxeTcg2PhysicalPresencePlatformLib.c
@@ -0,0 +1,62 @@
+/** @file
+  Returns the platform specific configuration for the QEMU PPI.
+
+  Caution: This module requires additional review when modified.
+  This driver will have external input - variable.
+  This external input must be validated carefully to avoid security issue.
+
+Copyright (C) 2018, Red Hat, Inc.
+Copyright (c) 2018, IBM Corporation. All rights reserved.<BR>
+Copyright (c) 2013 - 2016, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <PiDxe.h>
+
+#include <IndustryStandard/QemuTpm.h>
+
+#include <Library/QemuFwCfgLib.h>
+#include <Library/Tcg2PhysicalPresencePlatformLib.h>
+
+/**
+  Reads QEMU PPI config from fw_cfg.
+
+  @param[out]  Config        The config structure to fill in.
+  @param[out]  PpiInMmio     TRUE when the PPI lives in MMIO space.
+
+  @retval EFI_SUCCESS           Operation completed successfully.
+  @retval EFI_INVALID_PARAMETER A required pointer argument was NULL.
+  @retval EFI_PROTOCOL_ERROR    Invalid fw_cfg entry size.
+**/
+EFI_STATUS
+EFIAPI
+TpmPpiPlatformReadConfig (
+  OUT QEMU_FWCFG_TPM_CONFIG  *Config,
+  OUT BOOLEAN                *PpiInMmio
+  )
+{
+  EFI_STATUS           Status;
+  FIRMWARE_CONFIG_ITEM FwCfgItem;
+  UINTN                FwCfgSize;
+
+  if ((Config == NULL) || (PpiInMmio == NULL)) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  Status = QemuFwCfgFindFile ("etc/tpm/config", &FwCfgItem, &FwCfgSize);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  if (FwCfgSize != sizeof (*Config)) {
+    return EFI_PROTOCOL_ERROR;
+  }
+
+  QemuFwCfgSelectItem (FwCfgItem);
+  QemuFwCfgReadBytes (sizeof (*Config), Config);
+
+  *PpiInMmio = TRUE;
+
+  return EFI_SUCCESS;
+}
