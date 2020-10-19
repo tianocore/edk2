2c1e9f1dc5efda0787d059cc1d5534a6c4409503
Michael Kubacki <michael.kubacki@microsoft.com>
2020-10-20 07:59:35 +0800

    FmpDevicePkg: Add Last Attempt Status header files
    Introduces a public and a private header file to define more
    granular usage of the UEFI Specification defined unsuccessful
    vendor range for Last Attempt Status codes. The unsuccessful
    vendor range is described in UEFI Specification 2.8A section 23.4.
    
    The public header file Include/LastAttemptStatus.h defines ranges
    within the unsuccessful vendor range. At a high-level, the two
    main ranges are defined are the FMP Reserved range and the Device
    Library Reserved range.
    
    The FMP Reserved range is reserved for usage of components within
    FmpDevicePkg. PrivateInclude/FmpLastAttemptStatus.h contains
    usage details and specific Last Attempt Status code definitions.
    
    The Device Library Reserved range is reserved for usage by
    FmpDeviceLib instances. Each library may define custom Last
    Attempt Status codes within the bounds defined in
    Include/LastAttemptStatus.h:
    [LAST_ATTEMPT_STATUS_DEVICE_LIBRARY_MIN_ERROR_CODE_VALUE,
     LAST_ATTEMPT_STATUS_DEVICE_LIBRARY_MAX_ERROR_CODE_VALUE]
    
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Guomin Jiang <guomin.jiang@intel.com>
    Cc: Wei6 Xu <wei6.xu@intel.com>
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>
    Acked-by: Liming Gao <gaoliming@byosoft.com.cn>
    Reviewed-by: Wei6 Xu <wei6.xu@intel.com>
    Reviewed-by: Michael D Kinney <michael.d.kinney@intel.com>

---
+++ FmpDevicePkg/Include/LastAttemptStatus.h
@@ -0,0 +1,81 @@
+/** @file
+  Defines last attempt status code ranges within the UEFI Specification
+  defined unsuccessful vendor range.
+
+  Copyright (c) Microsoft Corporation.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef __LAST_ATTEMPT_STATUS_H__
+#define __LAST_ATTEMPT_STATUS_H__
+
+///
+/// Last Attempt Status Unsuccessful Vendor Range Map
+///
+/// Update this map any time new ranges are added. Pre-existing range definitions cannot be modified
+/// to keep status code definitions consistent over time.
+///
+/// START     | END       | Usage
+/// ------------------------------------------------------------------|
+/// 0x1000    | 0x17FF    | FmpDevicePkg                              |
+///    0x1000 |    0x107F | FmpDxe driver                             |
+///    0x1080 |    0x109F | FmpDependencyLib                          |
+///    0x10A0 |    0x10BF | FmpDependencyCheckLib                     |
+///    0x10C0 |    0x17FF | Unused. Available for future expansion.   |
+/// 0x1800    | 0x1FFF    | FmpDeviceLib instances implementation     |
+/// 0x2000    | 0x3FFF    | Unused. Available for future expansion.   |
+///
+
+///
+/// The minimum value of the FMP reserved range.
+///
+#define LAST_ATTEMPT_STATUS_FMP_RESERVED_MIN_ERROR_CODE_VALUE               0x1000
+
+///
+/// The maximum value of the FMP reserved range.
+///
+#define LAST_ATTEMPT_STATUS_FMP_RESERVED_MAX_ERROR_CODE_VALUE               0x1FFF
+
+///
+/// The minimum value allowed for FmpDxe driver-specific errors.
+///
+#define LAST_ATTEMPT_STATUS_DRIVER_MIN_ERROR_CODE_VALUE                     0x1000
+
+///
+/// The maximum value allowed for FmpDxe driver-specific errors.
+///
+#define LAST_ATTEMPT_STATUS_DRIVER_MAX_ERROR_CODE_VALUE                     0x107F
+
+///
+/// The minimum value allowed for FmpDependencyLib related errors.
+///
+#define LAST_ATTEMPT_STATUS_FMP_DEPENDENCY_LIB_MIN_ERROR_CODE_VALUE         0x1080
+
+///
+/// The maximum value allowed for FmpDependencyLib related errors.
+///
+#define LAST_ATTEMPT_STATUS_FMP_DEPENDENCY_LIB_MAX_ERROR_CODE_VALUE         0x109F
+
+///
+/// The minimum value allowed for FmpDependencyCheckLib related errors.
+///
+#define LAST_ATTEMPT_STATUS_FMP_DEPENDENCY_CHECK_LIB_MIN_ERROR_CODE_VALUE   0x10A0
+
+///
+/// The maximum value allowed for FmpDependencyCheckLib related errors.
+///
+#define LAST_ATTEMPT_STATUS_FMP_DEPENDENCY_CHECK_LIB_MAX_ERROR_CODE_VALUE   0x10BF
+
+///
+/// The minimum value allowed for FMP device library errors.
+///
+#define LAST_ATTEMPT_STATUS_DEVICE_LIBRARY_MIN_ERROR_CODE_VALUE             0x1800
+
+///
+/// The maximum value allowed for FMP device library errors.
+///
+#define LAST_ATTEMPT_STATUS_DEVICE_LIBRARY_MAX_ERROR_CODE_VALUE             0x1FFF
+
+#endif
