4510f2ee3d83590a0a1db8ba77776bf3a1ce6329
Pierre Gondois <Pierre.Gondois@arm.com>
2020-12-10 13:12:00 +0000

    ArmPkg: Fix Ecc error 5007 in ArmLib
    This patch fixes the following Ecc reported error:
    There should be no initialization of a variable as
    part of its declaration
    
    Signed-off-by: Pierre Gondois <Pierre.Gondois@arm.com>
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@arm.com>

--- ArmPkg/Library/ArmLib/ArmLib.c
+++ ArmPkg/Library/ArmLib/ArmLib.c
@@ -1,7 +1,7 @@
 /** @file
 
   Copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
-  Copyright (c) 2011 - 2014, ARM Ltd. All rights reserved.<BR>
+  Copyright (c) 2011 - 2021, ARM Ltd. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -19,9 +19,7 @@ ArmSetAuxCrBit (
   IN  UINT32    Bits
   )
 {
-  UINT32 val = ArmReadAuxCr();
-  val |= Bits;
-  ArmWriteAuxCr(val);
+  ArmWriteAuxCr(ArmReadAuxCr() | Bits);
 }
 
 VOID
@@ -30,9 +28,7 @@ ArmUnsetAuxCrBit (
   IN  UINT32    Bits
   )
 {
-  UINT32 val = ArmReadAuxCr();
-  val &= ~Bits;
-  ArmWriteAuxCr(val);
+  ArmWriteAuxCr(ArmReadAuxCr() & ~Bits);
 }
 
 //
@@ -45,10 +41,7 @@ ArmSetCpuActlrBit (
   IN  UINTN    Bits
   )
 {
-  UINTN Value;
-  Value =  ArmReadCpuActlr ();
-  Value |= Bits;
-  ArmWriteCpuActlr (Value);
+  ArmWriteCpuActlr (ArmReadCpuActlr () | Bits);
 }
 
 VOID
@@ -57,10 +50,7 @@ ArmUnsetCpuActlrBit (
   IN  UINTN    Bits
   )
 {
-  UINTN Value;
-  Value = ArmReadCpuActlr ();
-  Value &= ~Bits;
-  ArmWriteCpuActlr (Value);
+  ArmWriteCpuActlr (ArmReadCpuActlr () & ~Bits);
 }
 
 UINTN
