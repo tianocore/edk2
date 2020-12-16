0033217aa280fdad1bb5075e27a6d6023f4ff97f
Laszlo Ersek <lersek@redhat.com>
2020-12-16 22:11:08 +0100

    OvmfPkg/VirtioFsDxe: implement EFI_FILE_PROTOCOL.GetPosition, .SetPosition
    Using the functions introduced previously, we can now implement
    VirtioFsSimpleFileGetPosition() and VirtioFsSimpleFileSetPosition().
    
    Cc: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Philippe Mathieu-Daudé <philmd@redhat.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=3097
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Message-Id: <20201216211125.19496-32-lersek@redhat.com>
    Acked-by: Ard Biesheuvel <ard.biesheuvel@arm.com>

--- OvmfPkg/VirtioFsDxe/SimpleFsSetPosition.c
+++ OvmfPkg/VirtioFsDxe/SimpleFsSetPosition.c
@@ -16,5 +16,43 @@ VirtioFsSimpleFileSetPosition (
   IN UINT64            Position
   )
 {
-  return EFI_DEVICE_ERROR;
+  VIRTIO_FS_FILE                     *VirtioFsFile;
+  VIRTIO_FS                          *VirtioFs;
+  EFI_STATUS                         Status;
+  VIRTIO_FS_FUSE_ATTRIBUTES_RESPONSE FuseAttr;
+
+  VirtioFsFile = VIRTIO_FS_FILE_FROM_SIMPLE_FILE (This);
+
+  //
+  // Directories can only be rewound, per spec.
+  //
+  if (VirtioFsFile->IsDirectory) {
+    if (Position != 0) {
+      return EFI_UNSUPPORTED;
+    }
+    VirtioFsFile->FilePosition = 0;
+    return EFI_SUCCESS;
+  }
+
+  //
+  // Regular file.
+  //
+  if (Position < MAX_UINT64) {
+    //
+    // Caller is requesting absolute file position.
+    //
+    VirtioFsFile->FilePosition = Position;
+    return EFI_SUCCESS;
+  }
+
+  //
+  // Caller is requesting a seek to EOF.
+  //
+  VirtioFs = VirtioFsFile->OwnerFs;
+  Status = VirtioFsFuseGetAttr (VirtioFs, VirtioFsFile->NodeId, &FuseAttr);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+  VirtioFsFile->FilePosition = FuseAttr.Size;
+  return EFI_SUCCESS;
 }
