7e8c83f7d4823009d1f37ec6a0fa2c4ef888c2d5
Laszlo Ersek <lersek@redhat.com>
2020-12-16 22:10:54 +0100

    OvmfPkg/VirtioFsDxe: manage path lifecycle in OpenVolume, Close, Delete
    Add a canonical pathname field to VIRTIO_FS_FILE.
    
    Initialize the new field in EFI_SIMPLE_FILE_SYSTEM_PROTOCOL.OpenVolume().
    
    Release the new field in EFI_FILE_PROTOCOL.Close() and
    EFI_FILE_PROTOCOL.Delete().
    
    Cc: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Philippe Mathieu-Daudé <philmd@redhat.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=3097
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Message-Id: <20201216211125.19496-18-lersek@redhat.com>
    Acked-by: Ard Biesheuvel <ard.biesheuvel@arm.com>

--- OvmfPkg/VirtioFsDxe/VirtioFsDxe.h
+++ OvmfPkg/VirtioFsDxe/VirtioFsDxe.h
@@ -137,6 +137,7 @@ typedef struct {
   BOOLEAN           IsOpenForWriting;
   VIRTIO_FS         *OwnerFs;
   LIST_ENTRY        OpenFilesEntry;
+  CHAR8             *CanonicalPathname;
   //
   // In the FUSE wire protocol, every request except FUSE_INIT refers to a
   // file, namely by the "VIRTIO_FS_FUSE_REQUEST.NodeId" field; that is, by the
