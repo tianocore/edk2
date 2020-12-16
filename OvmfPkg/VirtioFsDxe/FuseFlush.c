3ca8531d781c34e910ad57b3ac2a5362f98a8cc0
Laszlo Ersek <lersek@redhat.com>
2020-12-16 22:10:51 +0100

    OvmfPkg/VirtioFsDxe: implement the wrapper function for FUSE_FLUSH
    (cherry picked from commit d0474399521789646ebc66b3450a4983d6fd07d9)
    
    Add the VirtioFsFuseFlush() function, for sending the FUSE_FLUSH command
    to the Virtio Filesystem device.
    
    Cc: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Philippe Mathieu-Daudé <philmd@redhat.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=3097
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Message-Id: <20201216211125.19496-15-lersek@redhat.com>
    Acked-by: Ard Biesheuvel <ard.biesheuvel@arm.com>
    (cherry picked from commit d0474399521789646ebc66b3450a4983d6fd07d9)
    
    Change-Id: Ibc765363538ce66ceda3130864cbd1d5058d565c

---
+++ OvmfPkg/VirtioFsDxe/FuseFlush.c
@@ -0,0 +1,111 @@
+/** @file
+  FUSE_FLUSH wrapper for the Virtio Filesystem device.
+
+  Copyright (C) 2020, Red Hat, Inc.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#include "VirtioFsDxe.h"
+
+/**
+  Flush changes queued on the local virtualization host to the remote storage
+  server's memory (not storage device), over the network, by sending the
+  FUSE_FLUSH request to the Virtio Filesystem device.
+
+  The function may only be called after VirtioFsFuseInitSession() returns
+  successfully and before VirtioFsUninit() is called.
+
+  @param[in,out] VirtioFs  The Virtio Filesystem device to send the FUSE_FLUSH
+                           request to. On output, the FUSE request counter
+                           "VirtioFs->RequestId" will have been incremented.
+
+  @param[in] NodeId        The inode number of the regular file to flush.
+
+  @param[in] FuseHandle    The open handle to the regular file to flush.
+
+  @retval EFI_SUCCESS  The regular file has been flushed.
+
+  @return              The "errno" value mapped to an EFI_STATUS code, if the
+                       Virtio Filesystem device explicitly reported an error.
+
+  @return              Error codes propagated from VirtioFsSgListsValidate(),
+                       VirtioFsFuseNewRequest(), VirtioFsSgListsSubmit(),
+                       VirtioFsFuseCheckResponse().
+**/
+EFI_STATUS
+VirtioFsFuseFlush (
+  IN OUT VIRTIO_FS *VirtioFs,
+  IN     UINT64    NodeId,
+  IN     UINT64    FuseHandle
+  )
+{
+  VIRTIO_FS_FUSE_REQUEST        CommonReq;
+  VIRTIO_FS_FUSE_FLUSH_REQUEST  FlushReq;
+  VIRTIO_FS_IO_VECTOR           ReqIoVec[2];
+  VIRTIO_FS_SCATTER_GATHER_LIST ReqSgList;
+  VIRTIO_FS_FUSE_RESPONSE       CommonResp;
+  VIRTIO_FS_IO_VECTOR           RespIoVec[1];
+  VIRTIO_FS_SCATTER_GATHER_LIST RespSgList;
+  EFI_STATUS                    Status;
+
+  //
+  // Set up the scatter-gather lists.
+  //
+  ReqIoVec[0].Buffer = &CommonReq;
+  ReqIoVec[0].Size   = sizeof CommonReq;
+  ReqIoVec[1].Buffer = &FlushReq;
+  ReqIoVec[1].Size   = sizeof FlushReq;
+  ReqSgList.IoVec    = ReqIoVec;
+  ReqSgList.NumVec   = ARRAY_SIZE (ReqIoVec);
+
+  RespIoVec[0].Buffer = &CommonResp;
+  RespIoVec[0].Size   = sizeof CommonResp;
+  RespSgList.IoVec    = RespIoVec;
+  RespSgList.NumVec   = ARRAY_SIZE (RespIoVec);
+
+  //
+  // Validate the scatter-gather lists; calculate the total transfer sizes.
+  //
+  Status = VirtioFsSgListsValidate (VirtioFs, &ReqSgList, &RespSgList);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  //
+  // Populate the common request header.
+  //
+  Status = VirtioFsFuseNewRequest (VirtioFs, &CommonReq, ReqSgList.TotalSize,
+             VirtioFsFuseOpFlush, NodeId);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  //
+  // Populate the FUSE_FLUSH-specific fields.
+  //
+  FlushReq.FileHandle = FuseHandle;
+  FlushReq.Unused     = 0;
+  FlushReq.Padding    = 0;
+  FlushReq.LockOwner  = 0;
+
+  //
+  // Submit the request.
+  //
+  Status = VirtioFsSgListsSubmit (VirtioFs, &ReqSgList, &RespSgList);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  //
+  // Verify the response (all response buffers are fixed size).
+  //
+  Status = VirtioFsFuseCheckResponse (&RespSgList, CommonReq.Unique, NULL);
+  if (Status == EFI_DEVICE_ERROR) {
+    DEBUG ((DEBUG_ERROR, "%a: Label=\"%s\" NodeId=%Lu FuseHandle=%Lu "
+      "Errno=%d\n", __FUNCTION__, VirtioFs->Label, NodeId, FuseHandle,
+      CommonResp.Error));
+    Status = VirtioFsErrnoToEfiStatus (CommonResp.Error);
+  }
+  return Status;
+}
