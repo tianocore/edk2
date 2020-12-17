0d8e7402c32373ad346e16c06c8428854ffcc3ff
Kun Qin <kun.q@outlook.com>
2020-12-17 14:47:07 -0800

    PcAtChipsetPkg: AcpiTimerLib: Added StandaloneMm instance of AcpiTimerLib
    This change added a new instance of AcpiTimerLib for StandaloneMm core
    and drivers. It centralizes the common routines into shared files and
    abstract the library constructor into corresponding files to accommodate
    each constructor function prototypes.
    
    Cc: Ray Ni <ray.ni@intel.com>
    
    Signed-off-by: Kun Qin <kun.q@outlook.com>

---
+++ PcAtChipsetPkg/Library/AcpiTimerLib/CommonAcpiTimerLib.h
@@ -0,0 +1,25 @@
+/** @file
+  Header file internal to ACPI TimerLib.
+
+Copyright (c) Microsoft Corporation.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+
+#ifndef _COMMON_ACPI_TIMER_LIB_H_
+#define _COMMON_ACPI_TIMER_LIB_H_
+
+/**
+  The constructor function enables ACPI IO space, and caches PerformanceCounterFrequency.
+
+  @retval EFI_SUCCESS   The constructor always returns RETURN_SUCCESS.
+
+**/
+EFI_STATUS
+EFIAPI
+CommonAcpiTimerLibConstructor (
+  VOID
+  );
+
+#endif
