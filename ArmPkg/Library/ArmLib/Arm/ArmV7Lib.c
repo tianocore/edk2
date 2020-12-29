79d45168dbf231728ce8bed1c3095ca203d3686a
Rebecca Cran <rebecca@nuviainc.com>
2020-12-28 19:26:09 -0700

    ArmPkg: Add helper to read CCIDX status
    Add a helper function to determine CCIDX support.
    
    Signed-off-by: Rebecca Cran <rebecca@nuviainc.com>
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>
    Reviewed-by: Leif Lindholm <leif@nuviainc.com>

--- ArmPkg/Library/ArmLib/Arm/ArmV7Lib.c
+++ ArmPkg/Library/ArmLib/Arm/ArmV7Lib.c
@@ -2,7 +2,7 @@
 
   Copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
   Copyright (c) 2011 - 2014, ARM Limited. All rights reserved.
-  Copyright (c) 2020, NUVIA Inc. All rights reserved.<BR>
+  Copyright (c) 2021, NUVIA Inc. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -101,4 +101,21 @@ ArmHasSecurityExtensions (
   )
 {
   return ((ArmReadIdPfr1 () & ARM_PFR1_SEC) != 0);
+}
+
+/** Checks if CCIDX is implemented.
+
+   @retval TRUE  CCIDX is implemented.
+   @retval FALSE CCIDX is not implemented.
+**/
+BOOLEAN
+EFIAPI
+ArmHasCcidx (
+  VOID
+  )
+{
+  UINTN Mmfr4;
+
+  Mmfr4 = ArmReadIdMmfr4 ();
+  return (((Mmfr4 >> 24) & 0xF) == 1) ? TRUE : FALSE;
 }
