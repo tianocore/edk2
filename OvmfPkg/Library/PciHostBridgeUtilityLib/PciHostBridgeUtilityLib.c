51428e5a7f063121a67623183287c417a8d940b6
Jiahui Cen via groups.io <cenjiahui=huawei.com@groups.io>
2021-01-19 09:12:56 +0800

    OvmfPkg/PciHostBridgeUtilityLib: Extend parameters of InitRootBridge()
    (cherry picked from commit e1b259da426b3bc0cddf7a293bc8c5c8d85c6d8d)
    
    Extend parameter list of PciHostBridgeUtilityInitRootBridge() with
    DmaAbove4G and NoExtendedConfigSpace to prepare for sharing with
    ArmVirtPkg.
    
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=3059
    
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Cc: Anthony Perard <anthony.perard@citrix.com>
    Cc: Julien Grall <julien@xen.org>
    Signed-off-by: Jiahui Cen <cenjiahui@huawei.com>
    Signed-off-by: Yubo Miao <miaoyubo@huawei.com>
    Message-Id: <20210119011302.10908-6-cenjiahui@huawei.com>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    (cherry picked from commit e1b259da426b3bc0cddf7a293bc8c5c8d85c6d8d)
    
    Change-Id: Ie5b7727b771610443e7f4571c1c0997652ab74ed

--- OvmfPkg/Library/PciHostBridgeUtilityLib/PciHostBridgeUtilityLib.c
+++ OvmfPkg/Library/PciHostBridgeUtilityLib/PciHostBridgeUtilityLib.c
@@ -11,12 +11,10 @@
 **/
 
 #include <IndustryStandard/Acpi10.h>
-#include <IndustryStandard/Q35MchIch9.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
 #include <Library/DevicePathLib.h>
 #include <Library/MemoryAllocationLib.h>
-#include <Library/PcdLib.h>
 #include <Library/PciHostBridgeUtilityLib.h>
 
 
@@ -64,43 +62,49 @@ OVMF_PCI_ROOT_BRIDGE_DEVICE_PATH mRootBridgeDevicePathTemplate = {
 /**
   Utility function to initialize a PCI_ROOT_BRIDGE structure.
 
-  @param[in]  Supports         Supported attributes.
+  @param[in]  Supports               Supported attributes.
 
-  @param[in]  Attributes       Initial attributes.
+  @param[in]  Attributes             Initial attributes.
 
-  @param[in]  AllocAttributes  Allocation attributes.
+  @param[in]  AllocAttributes        Allocation attributes.
 
-  @param[in]  RootBusNumber    The bus number to store in RootBus.
+  @param[in]  DmaAbove4G             DMA above 4GB memory.
 
-  @param[in]  MaxSubBusNumber  The inclusive maximum bus number that can be
-                               assigned to any subordinate bus found behind any
-                               PCI bridge hanging off this root bus.
+  @param[in]  NoExtendedConfigSpace  No Extended Config Space.
 
-                               The caller is repsonsible for ensuring that
-                               RootBusNumber <= MaxSubBusNumber. If
-                               RootBusNumber equals MaxSubBusNumber, then the
-                               root bus has no room for subordinate buses.
+  @param[in]  RootBusNumber          The bus number to store in RootBus.
 
-  @param[in]  Io               IO aperture.
+  @param[in]  MaxSubBusNumber        The inclusive maximum bus number that can
+                                     be assigned to any subordinate bus found
+                                     behind any PCI bridge hanging off this
+                                     root bus.
 
-  @param[in]  Mem              MMIO aperture.
+                                     The caller is repsonsible for ensuring
+                                     that RootBusNumber <= MaxSubBusNumber. If
+                                     RootBusNumber equals MaxSubBusNumber, then
+                                     the root bus has no room for subordinate
+                                     buses.
 
-  @param[in]  MemAbove4G       MMIO aperture above 4G.
+  @param[in]  Io                     IO aperture.
 
-  @param[in]  PMem             Prefetchable MMIO aperture.
+  @param[in]  Mem                    MMIO aperture.
 
-  @param[in]  PMemAbove4G      Prefetchable MMIO aperture above 4G.
+  @param[in]  MemAbove4G             MMIO aperture above 4G.
 
-  @param[out] RootBus          The PCI_ROOT_BRIDGE structure (allocated by the
-                               caller) that should be filled in by this
-                               function.
+  @param[in]  PMem                   Prefetchable MMIO aperture.
 
-  @retval EFI_SUCCESS           Initialization successful. A device path
-                                consisting of an ACPI device path node, with
-                                UID = RootBusNumber, has been allocated and
-                                linked into RootBus.
+  @param[in]  PMemAbove4G            Prefetchable MMIO aperture above 4G.
 
-  @retval EFI_OUT_OF_RESOURCES  Memory allocation failed.
+  @param[out] RootBus                The PCI_ROOT_BRIDGE structure (allocated
+                                     by the caller) that should be filled in by
+                                     this function.
+
+  @retval EFI_SUCCESS                Initialization successful. A device path
+                                     consisting of an ACPI device path node,
+                                     with UID = RootBusNumber, has been
+                                     allocated and linked into RootBus.
+
+  @retval EFI_OUT_OF_RESOURCES       Memory allocation failed.
 **/
 EFI_STATUS
 EFIAPI
@@ -108,6 +112,8 @@ PciHostBridgeUtilityInitRootBridge (
   IN  UINT64                   Supports,
   IN  UINT64                   Attributes,
   IN  UINT64                   AllocAttributes,
+  IN  BOOLEAN                  DmaAbove4G,
+  IN  BOOLEAN                  NoExtendedConfigSpace,
   IN  UINT8                    RootBusNumber,
   IN  UINT8                    MaxSubBusNumber,
   IN  PCI_ROOT_BRIDGE_APERTURE *Io,
@@ -130,7 +136,7 @@ PciHostBridgeUtilityInitRootBridge (
   RootBus->Supports   = Supports;
   RootBus->Attributes = Attributes;
 
-  RootBus->DmaAbove4G = FALSE;
+  RootBus->DmaAbove4G = DmaAbove4G;
 
   RootBus->AllocationAttributes = AllocAttributes;
   RootBus->Bus.Base  = RootBusNumber;
@@ -141,8 +147,7 @@ PciHostBridgeUtilityInitRootBridge (
   CopyMem (&RootBus->PMem, PMem, sizeof (*PMem));
   CopyMem (&RootBus->PMemAbove4G, PMemAbove4G, sizeof (*PMemAbove4G));
 
-  RootBus->NoExtendedConfigSpace = (PcdGet16 (PcdOvmfHostBridgePciDevId) !=
-                                    INTEL_Q35_MCH_DEVICE_ID);
+  RootBus->NoExtendedConfigSpace = NoExtendedConfigSpace;
 
   DevicePath = AllocateCopyPool (sizeof mRootBridgeDevicePathTemplate,
                  &mRootBridgeDevicePathTemplate);
