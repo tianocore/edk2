cf6e1d67181c37c044aa1bf621aee4870d1821ab
Jiahui Cen via groups.io <cenjiahui=huawei.com@groups.io>
2021-01-19 09:12:55 +0800

    OvmfPkg/PciHostBridgeLib: Extract InitRootBridge() / UninitRootBridge()
    (cherry picked from commit 7ac1f28d4dbd0d47a0336c841388e77051fca001)
    
    Extract InitRootBridge() / UninitRootBridge() to PciHostBridgeUtilityLib
    as common utility functions. No change of functionality.
    
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=3059
    
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@arm.com>
    Cc: Anthony Perard <anthony.perard@citrix.com>
    Cc: Julien Grall <julien@xen.org>
    Signed-off-by: Jiahui Cen <cenjiahui@huawei.com>
    Signed-off-by: Yubo Miao <miaoyubo@huawei.com>
    Message-Id: <20210119011302.10908-5-cenjiahui@huawei.com>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    (cherry picked from commit 7ac1f28d4dbd0d47a0336c841388e77051fca001)
    
    Change-Id: Ic0822daa71888d1fca9212480ea0bf65f2ca69d2

--- OvmfPkg/Include/Library/PciHostBridgeUtilityLib.h
+++ OvmfPkg/Include/Library/PciHostBridgeUtilityLib.h
@@ -14,6 +14,83 @@
 #define __PCI_HOST_BRIDGE_UTILITY_LIB_H__
 
 
+#include <Library/PciHostBridgeLib.h>
+
+
+/**
+  Utility function to initialize a PCI_ROOT_BRIDGE structure.
+
+  @param[in]  Supports         Supported attributes.
+
+  @param[in]  Attributes       Initial attributes.
+
+  @param[in]  AllocAttributes  Allocation attributes.
+
+  @param[in]  RootBusNumber    The bus number to store in RootBus.
+
+  @param[in]  MaxSubBusNumber  The inclusive maximum bus number that can be
+                               assigned to any subordinate bus found behind any
+                               PCI bridge hanging off this root bus.
+
+                               The caller is repsonsible for ensuring that
+                               RootBusNumber <= MaxSubBusNumber. If
+                               RootBusNumber equals MaxSubBusNumber, then the
+                               root bus has no room for subordinate buses.
+
+  @param[in]  Io               IO aperture.
+
+  @param[in]  Mem              MMIO aperture.
+
+  @param[in]  MemAbove4G       MMIO aperture above 4G.
+
+  @param[in]  PMem             Prefetchable MMIO aperture.
+
+  @param[in]  PMemAbove4G      Prefetchable MMIO aperture above 4G.
+
+  @param[out] RootBus          The PCI_ROOT_BRIDGE structure (allocated by the
+                               caller) that should be filled in by this
+                               function.
+
+  @retval EFI_SUCCESS           Initialization successful. A device path
+                                consisting of an ACPI device path node, with
+                                UID = RootBusNumber, has been allocated and
+                                linked into RootBus.
+
+  @retval EFI_OUT_OF_RESOURCES  Memory allocation failed.
+**/
+EFI_STATUS
+EFIAPI
+PciHostBridgeUtilityInitRootBridge (
+  IN  UINT64                   Supports,
+  IN  UINT64                   Attributes,
+  IN  UINT64                   AllocAttributes,
+  IN  UINT8                    RootBusNumber,
+  IN  UINT8                    MaxSubBusNumber,
+  IN  PCI_ROOT_BRIDGE_APERTURE *Io,
+  IN  PCI_ROOT_BRIDGE_APERTURE *Mem,
+  IN  PCI_ROOT_BRIDGE_APERTURE *MemAbove4G,
+  IN  PCI_ROOT_BRIDGE_APERTURE *PMem,
+  IN  PCI_ROOT_BRIDGE_APERTURE *PMemAbove4G,
+  OUT PCI_ROOT_BRIDGE          *RootBus
+  );
+
+
+/**
+  Utility function to uninitialize a PCI_ROOT_BRIDGE structure set up with
+  PciHostBridgeUtilityInitRootBridge().
+
+  @param[in] RootBus  The PCI_ROOT_BRIDGE structure, allocated by the caller and
+                      initialized with PciHostBridgeUtilityInitRootBridge(),
+                      that should be uninitialized. This function doesn't free
+                      RootBus.
+**/
+VOID
+EFIAPI
+PciHostBridgeUtilityUninitRootBridge (
+  IN PCI_ROOT_BRIDGE *RootBus
+  );
+
+
 /**
   Utility function to inform the platform that the resource conflict happens.
 
