8dfa7f9d11d694781d5d3f20a833d80fd981571c
Kun Qin <kun.q@outlook.com>
2021-02-02 12:36:21 -0800

    MdePkg: MmUnblockMemoryLib: Added definition and null instance
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3168
    
    This interface definition provides an abstraction layer for applicable
    drivers to request certain memory blocks to be mapped/unblocked for
    accessibility inside MM environment.
    
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Zhiguang Liu <zhiguang.liu@intel.com>
    Cc: Hao A Wu <hao.a.wu@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    
    Signed-off-by: Kun Qin <kun.q@outlook.com>

---
+++ MdePkg/Include/Library/MmUnblockMemoryLib.h
@@ -0,0 +1,40 @@
+/** @file
+  MM Unblock Memory Library Interface.
+
+  This library provides an abstraction layer of requesting certain page access to be unblocked
+  by MM environment if applicable.
+
+  Copyright (c) Microsoft Corporation.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _MM_UNBLOCK_MEMORY_LIB_H_
+#define _MM_UNBLOCK_MEMORY_LIB_H_
+
+/**
+  This API provides a way to unblock certain data pages to be accessible inside MM environment.
+
+  @param  UnblockAddress          The address of buffer caller requests to unblock, the address
+                                  has to be page aligned.
+  @param  NumberOfPages           The number of pages requested to be unblocked from MM
+                                  environment.
+
+  @return EFI_SUCCESS             The request goes through successfully.
+  @return EFI_NOT_AVAILABLE_YET   The requested functionality is not produced yet.
+  @return EFI_UNSUPPORTED         The requested functionality is not supported on current platform.
+  @return EFI_SECURITY_VIOLATION  The requested address failed to pass security check for
+                                  unblocking.
+  @return EFI_INVALID_PARAMETER   Input address either NULL pointer or not page aligned.
+  @return EFI_ACCESS_DENIED       The request is rejected due to system has passed certain boot
+                                  phase.
+
+**/
+EFI_STATUS
+EFIAPI
+MmUnblockMemoryRequest (
+  IN EFI_PHYSICAL_ADDRESS   UnblockAddress,
+  IN UINT64                 NumberOfPages
+);
+
+#endif // _MM_UNBLOCK_MEMORY_LIB_H_
