c574a5d2dee236c13409a34091e14fd7371cd812
Michael D Kinney <michael.d.kinney@intel.com>
2021-02-02 15:26:38 -0800

    CryptoPkg/Library/Include: Allow CPU specific defines to be predefined
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3308
    
    The EDK II Build Specifications do not restrict the set of
    CPU architectures that can be supported.  Remove places in
    the EDK II that assume a fixed set of CPU architectures.
    
    Update SIXTY_FOUR_BIT and THIRTY_TWO_BIT defines in the
    CryptoPkg to allow one of them to be predefined on the EDK II
    build command line using /D flag or in DSC file [BuildOptions]
    sections.
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Xiaoyu Lu <xiaoyux.lu@intel.com>
    Cc: Guomin Jiang <guomin.jiang@intel.com>
    Cc: Andrew Fish <afish@apple.com>
    Cc: Abner Chang <abner.chang@hpe.com>
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Abner Chang <abner.chang@hpe.com>
    Reviewed-by: Liming Gao <gaoliming@byosoft.com.cn>
    Reviewed-by: Jian J Wang <jian.j.wang@intel.com>

--- CryptoPkg/Library/Include/CrtLibSupport.h
+++ CryptoPkg/Library/Include/CrtLibSupport.h
@@ -2,7 +2,7 @@
   Root include file of C runtime library to support building the third-party
   cryptographic library.
 
-Copyright (c) 2010 - 2019, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2010 - 2021, Intel Corporation. All rights reserved.<BR>
 Copyright (c) 2020, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -44,6 +44,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #define CONFIG_HEADER_BN_H
 
+#if !defined(SIXTY_FOUR_BIT) && !defined (THIRTY_TWO_BIT)
 #if defined(MDE_CPU_X64) || defined(MDE_CPU_AARCH64) || defined(MDE_CPU_IA64) || defined(MDE_CPU_RISCV64)
 //
 // With GCC we would normally use SIXTY_FOUR_BIT_LONG, but MSVC needs
@@ -55,6 +56,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #define THIRTY_TWO_BIT
 #else
 #error Unknown target architecture
+#endif
 #endif
 
 //
