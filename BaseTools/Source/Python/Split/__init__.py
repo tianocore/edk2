8d8378b57fe61400ed2ef40e1fb0b0b4338cc430
edk2buildsystem <edk2buildsystem@intel.com>
2021-02-08 00:25:25 -0800

    SyncScript: Revert override commits.
    Change-Id: Ic960e8e7fadf2b34793e6b4b06eede00a81788eb

---
+++ BaseTools/Source/Python/Split/__init__.py
@@ -0,0 +1,10 @@
+# @file
+#  Split a file into two pieces at the request offset.
+#
+#  Copyright (c) 2021, Intel Corporation. All rights reserved.<BR>
+#
+#  SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+##
+
+# Import Modules
