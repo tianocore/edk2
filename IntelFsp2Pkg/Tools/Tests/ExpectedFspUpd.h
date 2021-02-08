273261126e30c46737736bff3ece64c4bd77bedd
Loo Tung Lun <tung.lun.loo@intel.com>
2021-02-08 13:04:16 +0800

    IntelFsp2Pkg: Add YAML file generation support
    REF:https://bugzilla.tianocore.org/show_bug.cgi?id=3091
    
    Add support for YAML format file generation in addition
    to current BSF structure. Configuration of YAML format
    output will be supported by an open source ConfigEditor.
    
    Reference to YAML code, test and ConfigEditor is at
    https://github.com/joshloo/fsp_yaml_cfg/tree/master/Tools
    
    A unit test is also added in Tests folder. This test compares
    the generated yaml file against the expected output to know
    if it is constructing the yaml data structure as expected.
    
    Cc: Maurice Ma <maurice.ma@intel.com>
    Cc: Nate DeSimone <nathaniel.l.desimone@intel.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Chasel Chiu <chasel.chiu@intel.com>
    Signed-off-by: Loo Tung Lun <tung.lun.loo@intel.com>
    Reviewed-by: Chasel Chiu <chasel.chiu@intel.com>

---
+++ IntelFsp2Pkg/Tools/Tests/ExpectedFspUpd.h
@@ -0,0 +1,16 @@
+#ifndef __FSPUPD_H__
+#define __FSPUPD_H__
+
+#include <FspEas.h>
+
+#pragma pack(1)
+
+#define FSPT_UPD_SIGNATURE               0x545F4450554D4551        /* 'QEMUPD_T' */
+
+#define FSPM_UPD_SIGNATURE               0x4D5F4450554D4551        /* 'QEMUPD_M' */
+
+#define FSPS_UPD_SIGNATURE               0x535F4450554D4551        /* 'QEMUPD_S' */
+
+#pragma pack()
+
+#endif
