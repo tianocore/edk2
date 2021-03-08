11933fdf1f1e16ecee9726852f714345ed0c9c0b
Chandramohan Akula <chandramohan.akula@broadcom.com>
2021-03-08 11:02:47 +0800

    MdePkg/UefiLib: Correct the arguments passed to IsLanguageSupported()
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3211
    
    Correct the arguments passed to the IsLanguageSupported() function in
    AddUnicodeString2() and LookupUnicodeString2() as expected by the function
    
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Zhiguang Liu <zhiguang.liu@intel.com>
    Signed-off-by: Chandramohan Akula <chandramohan.akula@broadcom.com>
    Reviewed-by: Liming Gao <gaoliming@byosoft.com.cn>

--- MdePkg/Library/UefiLib/UefiLib.c
+++ MdePkg/Library/UefiLib/UefiLib.c
@@ -839,7 +839,7 @@ LookupUnicodeString2 (
       SupportedLanguages += 3;
     }
   } else {
-    Found = !IsLanguageSupported(Language, SupportedLanguages);
+    Found = !IsLanguageSupported(SupportedLanguages, Language);
   }
 
 
@@ -1133,7 +1133,7 @@ AddUnicodeString2 (
       SupportedLanguages += 3;
     }
   } else {
-    Found = !IsLanguageSupported(Language, SupportedLanguages);
+    Found = !IsLanguageSupported(SupportedLanguages, Language);
   }
   //
   // If Language is not a member of SupportedLanguages, then return EFI_UNSUPPORTED
