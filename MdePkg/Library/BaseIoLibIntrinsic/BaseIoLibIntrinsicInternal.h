b9a58669f4b1749bbf1fa7c49da8c5d6c3a6ce52
Dandan Bi <dandan.bi@intel.com>
2021-03-12 10:26:10 +0800

    MdePkg/IoLib: Filter/trace port IO/MMIO access
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3246
    
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Zhiguang Liu <zhiguang.liu@intel.com>
    Signed-off-by: Dandan Bi <dandan.bi@intel.com>

--- MdePkg/Library/BaseIoLibIntrinsic/BaseIoLibIntrinsicInternal.h
+++ MdePkg/Library/BaseIoLibIntrinsic/BaseIoLibIntrinsicInternal.h
@@ -3,7 +3,7 @@
 
   This file includes package header files, dependent library classes.
 
-  Copyright (c) 2007 - 2008, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2007 - 2021, Intel Corporation. All rights reserved.<BR>
    SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
@@ -17,5 +17,6 @@
 #include <Library/IoLib.h>
 #include <Library/BaseLib.h>
 #include <Library/DebugLib.h>
+#include <Library/RegisterFilterLib.h>
 
 #endif
