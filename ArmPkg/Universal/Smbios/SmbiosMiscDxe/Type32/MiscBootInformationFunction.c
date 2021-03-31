336c0834b54b64ee679d2084c1d53d02e3b6e2e0
Rebecca Cran <rebecca@nuviainc.com>
2021-03-30 20:16:17 -0600

    ArmPkg: Allow platforms to report their boot status via OemMiscLib call
    Add a new function to OemMiscLib to allow platforms to report their boot
    status into the Type32 SMBIOS table.
    
    Signed-off-by: Rebecca Cran <rebecca@nuviainc.com>
    Reviewed-by: Leif Lindholm <leif@nuviainc.com>

--- ArmPkg/Universal/Smbios/SmbiosMiscDxe/Type32/MiscBootInformationFunction.c
+++ ArmPkg/Universal/Smbios/SmbiosMiscDxe/Type32/MiscBootInformationFunction.c
@@ -16,6 +16,7 @@
 #include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
 #include <Library/MemoryAllocationLib.h>
+#include <Library/OemMiscLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 
 #include "SmbiosMisc.h"
@@ -58,6 +59,8 @@ SMBIOS_MISC_TABLE_FUNCTION(MiscBootInformation)
   (VOID)CopyMem (SmbiosRecord, InputData, sizeof (SMBIOS_TABLE_TYPE32));
 
   SmbiosRecord->Hdr.Length = sizeof (SMBIOS_TABLE_TYPE32);
+
+  SmbiosRecord->BootStatus = OemGetBootStatus ();
 
   //
   // Now we have got the full smbios record, call smbios protocol to add this record.
