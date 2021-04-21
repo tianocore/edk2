8ebcfd9a8f2e649c1c5e4ae6a290cbf6147bf8c9
Pierre Gondois <Pierre.Gondois@arm.com>
2021-04-21 11:04:11 +0100

    .pytool: Enable CI for ArmPkg
    (cherry picked from commit 2942cb5815f43f36d6719eb82aef6b2a40873257)
    
    Enable the CI for the ArmPkg.
    
    Cc: Bret Barkelew <bret.barkelew@microsoft.com>
    Cc: Sean Brogan <sean.brogan@microsoft.com>
    Cc: Leif Lindholm <leif@nuviainc.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Sami Mujawar <sami.mujawar@arm.com>
    Signed-off-by: Pierre Gondois <Pierre.Gondois@arm.com>
    Reviewed-by: Bret Barkelew <bret.barkelew@microsoft.com>
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>
    (cherry picked from commit 2942cb5815f43f36d6719eb82aef6b2a40873257)
    
    Change-Id: I10847156fc15c03bdb242936c8a2f46a48325034

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -49,7 +49,8 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
         ''' return iterable of edk2 packages supported by this build.
         These should be edk2 workspace relative paths '''
 
-        return ("ArmVirtPkg",
+        return ("ArmPkg",
+                "ArmVirtPkg",
                 "DynamicTablesPkg",
                 "EmulatorPkg",
                 "MdePkg",
