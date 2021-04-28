e7ee90dda4fd1a6672d57a91449ff7a217fc9d6a
Brijesh Singh <brijesh.singh@amd.com>
2021-04-28 13:47:48 -0500

    OvmfPkg/AmdSev: Expose the SNP reserved pages through configuration table
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3275
    
    Now that both the secrets and cpuid pages are reserved in the HOB,
    extract the location details through fixed PCD and make it available
    to the guest OS through the configuration table.
    
    Cc: James Bottomley <jejb@linux.ibm.com>
    Cc: Min Xu <min.m.xu@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- OvmfPkg/AmdSev/SecretDxe/SecretDxe.c
+++ OvmfPkg/AmdSev/SecretDxe/SecretDxe.c
@@ -6,11 +6,21 @@
 **/
 #include <PiDxe.h>
 #include <Library/UefiBootServicesTableLib.h>
+#include <Library/MemEncryptSevLib.h>
 #include <Guid/ConfidentialComputingSecret.h>
 
 STATIC CONFIDENTIAL_COMPUTING_SECRET_LOCATION mSecretDxeTable = {
   FixedPcdGet32 (PcdSevLaunchSecretBase),
   FixedPcdGet32 (PcdSevLaunchSecretSize),
+};
+
+STATIC AMD_SNP_CONFIDENTIAL_COMPUTING_BLOB_LOCATION mSnpBootDxeTable = {
+  0x414d4445,     // AMDE
+  1,
+  (UINT64)(UINTN) FixedPcdGet32 (PcdSevLaunchSecretBase),
+  FixedPcdGet32 (PcdSevLaunchSecretSize),
+  (UINT64)(UINTN) FixedPcdGet32 (PcdOvmfSnpCpuidBase),
+  FixedPcdGet32 (PcdOvmfSnpCpuidSize),
 };
 
 EFI_STATUS
@@ -20,6 +30,16 @@ InitializeSecretDxe(
   IN EFI_SYSTEM_TABLE     *SystemTable
   )
 {
+  //
+  // If its SEV-SNP active guest then install the CONFIDENTIAL_COMPUTING_BLOB
+  //
+  if (MemEncryptSevSnpIsEnabled ()) {
+    return gBS->InstallConfigurationTable (
+                  &gAmdSnpConfidentialComputingBlobGuid,
+                  &mSnpBootDxeTable
+                  );
+  }
+
   return gBS->InstallConfigurationTable (
                 &gConfidentialComputingSecretGuid,
                 &mSecretDxeTable
