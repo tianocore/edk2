3df5a185f8a0bd6092f39c708f5c1150d993081b
Brijesh Singh <brijesh.singh@amd.com>
2021-05-04 08:00:59 -0500

    OvmfPkg: reserve Secrets page in MEMFD
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=3275
    
    When AMD SEV is enabled in the guest VM, a hypervisor need to insert a
    secrets page.
    
    When SEV-SNP is enabled, the secrets page contains the VM platform
    communication keys. The guest BIOS and OS can use this key to communicate
    with the SEV firmware to get attesation report. See the SEV-SNP firmware
    spec for more details for the content of the secrets page.
    
    When SEV and SEV-ES is enabled, the secrets page contains the information
    provided by the guest owner after the attestation. See the SEV
    LAUNCH_SECRET command for more details.
    
    Cc: James Bottomley <jejb@linux.ibm.com>
    Cc: Min Xu <min.m.xu@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Erdem Aktas <erdemaktas@google.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- OvmfPkg/AmdSev/SecretPei/SecretPei.c
+++ OvmfPkg/AmdSev/SecretPei/SecretPei.c
@@ -7,6 +7,7 @@
 #include <PiPei.h>
 #include <Library/HobLib.h>
 #include <Library/PcdLib.h>
+#include <Library/MemEncryptSevLib.h>
 
 EFI_STATUS
 EFIAPI
@@ -15,10 +16,22 @@ InitializeSecretPei (
   IN CONST EFI_PEI_SERVICES     **PeiServices
   )
 {
+  UINTN   Type;
+
+  //
+  // The location of the secret page should be marked reserved so that guest OS
+  // does not treated as a system RAM.
+  //
+  if (MemEncryptSevSnpIsEnabled ()) {
+    Type = EfiReservedMemoryType;
+  } else {
+    Type = EfiBootServicesData;
+  }
+
   BuildMemoryAllocationHob (
     PcdGet32 (PcdSevLaunchSecretBase),
     PcdGet32 (PcdSevLaunchSecretSize),
-    EfiBootServicesData
+    Type
     );
 
   return EFI_SUCCESS;
