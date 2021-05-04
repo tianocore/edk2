f7edaa05b2e9a3b331d2c0b5885e155433b51845
Brijesh Singh <brijesh.singh@amd.com>
2021-05-04 08:01:02 -0500

    UefiCpuPkg/MpLib: add support to register GHCB GPA when SEV-SNP is enabled
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=3275
    
    An SEV-SNP guest requires that the physical address of the GHCB must
    be registered with the hypervisor before using it. See the GHCB
    specification for the futher detail.
    
    Cc: James Bottomley <jejb@linux.ibm.com>
    Cc: Min Xu <min.m.xu@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Erdem Aktas <erdemaktas@google.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- UefiCpuPkg/Library/MpInitLib/MpLib.h
+++ UefiCpuPkg/Library/MpInitLib/MpLib.h
@@ -218,6 +218,7 @@ typedef struct {
   //
   BOOLEAN               Enable5LevelPaging;
   BOOLEAN               SevEsIsEnabled;
+  BOOLEAN               SevSnpIsEnabled;
   UINTN                 GhcbBase;
 } MP_CPU_EXCHANGE_INFO;
 
@@ -287,6 +288,7 @@ struct _CPU_MP_DATA {
   BOOLEAN                        WakeUpByInitSipiSipi;
 
   BOOLEAN                        SevEsIsEnabled;
+  BOOLEAN                        SevSnpIsEnabled;
   UINTN                          SevEsAPBuffer;
   UINTN                          SevEsAPResetStackStart;
   CPU_MP_DATA                    *NewCpuMpData;
