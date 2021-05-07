5aa3bac06364c0a86bf713f1aa2ddd0fed496988
Zhiguang Liu <zhiguang.liu@intel.com>
2021-05-07 13:34:20 +0800

    UefiPayloadPkg: Add HobLib for UniversalPayload
    For payload entry, use PayloadEntryHobLib as HobLib and payload entry should initialize hob base.
    For DxeCore, use new added DxeHobLib as HobLib, and DxeCore will initialize hob base.
    For Dxe Driver, use new added DxeHobLib as HobLib, and use DxeHobListLib to initialize hob base.
    
    Cc: Maurice Ma <maurice.ma@intel.com>
    Cc: Guo Dong <guo.dong@intel.com>
    Cc: Benjamin You <benjamin.you@intel.com>
    Signed-off-by: Ray Ni <ray.ni@intel.com>
    Signed-off-by: Zhiguang Liu <zhiguang.liu@intel.com>

---
+++ UefiPayloadPkg/Library/DxeHobListLibNull/DxeHobListLibNull.c
@@ -0,0 +1,20 @@
+/** @file
+  This library retrieve the EFI_BOOT_SERVICES pointer from EFI system table in
+  library's constructor.
+
+  Copyright (c) 2021, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+
+#include <Uefi.h>
+
+EFI_STATUS
+EFIAPI
+DxeHobListLibNullConstructor (
+  VOID
+  )
+{
+  return EFI_SUCCESS;
+}
