8bd091a933c6872f9f5cd6f9ced4261ba70c675a
Zhiguang Liu <zhiguang.liu@intel.com>
2021-05-07 13:34:20 +0800

    UefiPayloadPkg: Add HobLib for UniversalPayload
    (cherry picked from commit 82f727c4af3289e7765b36e9c55c57cc61de4065)
    
    For payload entry, use PayloadEntryHobLib as HobLib and payload entry
    should initialize hob base.
    For DxeCore, use new added DxeHobLib as HobLib, and DxeCore will
    initialize hob base.
    For Dxe Driver, use new added DxeHobLib as HobLib, and use DxeHobListLib
    to initialize hob base.
    
    Adding a new library DxeHobLib + DxeHobListLib instead of using the
    DxeHobLib.inf in MdePkg is because the constructor needed be separated
    from DxeHobLib.
    If not, when building UefiPayloadPkg, the dependency chain is as below:
    DebugLib -> SerialPortLib -> PlatformHookLib -> HobLib -> DebugLib
    Each library has a constructor, and this becomes a constructor circle.
    To break the circle, separate the constructor from the HobLib as a new
    DxeHobListLib, which won't depend on DebugLib.
    
    Cc: Maurice Ma <maurice.ma@intel.com>
    Cc: Guo Dong <guo.dong@intel.com>
    Cc: Benjamin You <benjamin.you@intel.com>
    Reviewed-by: Guo Dong <guo.dong@intel.com>
    Signed-off-by: Ray Ni <ray.ni@intel.com>
    Signed-off-by: Zhiguang Liu <zhiguang.liu@intel.com>
    (cherry picked from commit 82f727c4af3289e7765b36e9c55c57cc61de4065)
    
    Change-Id: I2c496f585414af76b48adf726c67a52bbcb1dcaf

---
+++ UefiPayloadPkg/Library/DxeHobListLib/DxeHobListLib.c
@@ -0,0 +1,66 @@
+/** @file
+  This library retrieve the EFI_BOOT_SERVICES pointer from EFI system table in
+  library's constructor.
+
+  Copyright (c) 2021, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+
+#include <Uefi.h>
+
+VOID  *gHobList = NULL;
+
+/**
+  Local implementation of GUID comparasion that doesn't depend on DebugLib::ASSERT().
+
+  This function compares Guid1 to Guid2.  If the GUIDs are identical then TRUE is returned.
+  If there are any bit differences in the two GUIDs, then FALSE is returned.
+
+  @param  Guid1       A pointer to a 128 bit GUID.
+  @param  Guid2       A pointer to a 128 bit GUID.
+
+  @retval TRUE        Guid1 and Guid2 are identical.
+  @retval FALSE       Guid1 and Guid2 are not identical.
+**/
+BOOLEAN
+LocalCompareGuid (
+  IN CONST GUID  *Guid1,
+  IN CONST GUID  *Guid2
+  )
+{
+  UINT64  *Left;
+  UINT64  *Right;
+
+  Left  = (UINT64 *) Guid1;
+  Right = (UINT64 *) Guid2;
+
+  return (BOOLEAN) (Left[0] == Right[0] && Left[1] == Right[1]);
+}
+
+/**
+  @param  ImageHandle   The firmware allocated handle for the EFI image.
+  @param  SystemTable   A pointer to the EFI System Table.
+
+  @retval EFI_SUCCESS   The constructor always returns EFI_SUCCESS.
+
+**/
+EFI_STATUS
+EFIAPI
+DxeHobListLibConstructor (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  UINTN             Index;
+
+  for (Index = 0; Index < SystemTable->NumberOfTableEntries; Index++) {
+    if (LocalCompareGuid (&gEfiHobListGuid, &SystemTable->ConfigurationTable[Index].VendorGuid)) {
+      gHobList = SystemTable->ConfigurationTable[Index].VendorTable;
+      return EFI_SUCCESS;
+    }
+  }
+
+  return EFI_NOT_FOUND;
+}
