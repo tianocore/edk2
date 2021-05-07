9c6912c502b42679bd67d42bb26e96163b3d028a
Zhiguang Liu <zhiguang.liu@intel.com>
2021-05-07 13:49:24 +0800

    MdeModulePkg: Add new structure for the Universal Payload Serial Port Info
    Add Universal Payload Serial Port Info definition header file according to
    Universal Payload's documentation as below:
    https://universalpayload.github.io/documentation/
    
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Hao A Wu <hao.a.wu@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Reviewed-by: Hao A Wu <hao.a.wu@intel.com>
    Signed-off-by: Zhiguang Liu <zhiguang.liu@intel.com>

---
+++ MdeModulePkg/Include/UniversalPayload/SerialPortInfo.h
@@ -0,0 +1,30 @@
+/** @file
+  This file defines the structure for serial port info.
+
+  Copyright (c) 2021, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+  @par Revision Reference:
+    - Universal Payload Specification 0.75 (https://universalpayload.github.io/documentation/)
+**/
+
+#ifndef UNIVERSAL_PAYLOAD_SERIAL_PORT_INFO_H_
+#define UNIVERSAL_PAYLOAD_SERIAL_PORT_INFO_H_
+
+#include <UniversalPayload/UniversalPayload.h>
+
+#pragma pack(1)
+typedef struct {
+  UNIVERSAL_PAYLOAD_GENERIC_HEADER   Header;
+  BOOLEAN                            UseMmio;
+  UINT8                              RegisterStride;
+  UINT32                             BaudRate;
+  EFI_PHYSICAL_ADDRESS               RegisterBase;
+} UNIVERSAL_PAYLOAD_SERIAL_PORT_INFO;
+#pragma pack()
+
+#define UNIVERSAL_PAYLOAD_SERIAL_PORT_INFO_REVISION 1
+
+extern GUID gUniversalPayloadSerialPortInfoGuid;
+
+#endif //  UNIVERSAL_PAYLOAD_SERIAL_PORT_INFO_H_
