9301e5644cef5a5234f71b178373dd508cabb9ee
Rebecca Cran <rebecca@nuviainc.com>
2021-05-10 15:53:07 -0600

    MdePkg/BaseRngLib: Add support for ARMv8.5 RNG instructions
    Make BaseRngLib more generic by moving x86-specific functionality into
    'Rand' and adding files under 'AArch64' to support the optional ARMv8.5
    RNG instruction RNDR that is a part of FEAT_RNG.
    
    Signed-off-by: Rebecca Cran <rebecca@nuviainc.com>
    Reviewed-by: Liming Gao <gaoliming@byosoft.com.cn>
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>

---
+++ MdePkg/Library/BaseRngLib/AArch64/ArmRng.h
@@ -0,0 +1,42 @@
+/** @file
+  Random number generator service that uses the RNDR instruction
+  to provide pseudorandom numbers.
+
+  Copyright (c) 2021, NUVIA Inc. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef ARM_RNG_H_
+#define ARM_RNG_H_
+
+/**
+  Generates a random number using RNDR.
+  Returns TRUE on success; FALSE on failure.
+
+  @param[out] Rand     Buffer pointer to store the 64-bit random value.
+
+  @retval TRUE         Random number generated successfully.
+  @retval FALSE        Failed to generate the random number.
+
+**/
+BOOLEAN
+EFIAPI
+ArmRndr (
+  OUT UINT64 *Rand
+  );
+
+/**
+  Reads the ID_AA64ISAR0 Register.
+
+  @return The contents of the ID_AA64ISAR0 register.
+
+**/
+UINT64
+EFIAPI
+ArmReadIdIsar0 (
+  VOID
+  );
+
+#endif /* ARM_RNG_H_ */
