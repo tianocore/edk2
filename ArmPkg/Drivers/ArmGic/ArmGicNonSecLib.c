d2f71f199fa1a53302f59b5e06d792da69be63af
Sami Mujawar <sami.mujawar@arm.com>
2021-05-26 18:14:01 +0100

    ArmPkg: Fix Non-Boolean comparison in ArmGicEnableDistributor
    According to edk2 coding standard specification, Non-Boolean
    comparisons must use a compare operator (==, !=, >, < >=, <=).
    See Section 5.7.2.1 at https://edk2-docs.gitbook.io/
    edk-ii-c-coding-standards-specification/5_source_files/
    57_c_programming
    
    Therefore, fix the comparison in ArmGicEnableDistributor()
    
    Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>

--- ArmPkg/Drivers/ArmGic/ArmGicNonSecLib.c
+++ ArmPkg/Drivers/ArmGic/ArmGicNonSecLib.c
@@ -17,6 +17,7 @@ ArmGicEnableDistributor (
   )
 {
   ARM_GIC_ARCH_REVISION  Revision;
+  UINT32                 GicDistributorCtl;
 
   /*
    * Enable GIC distributor in Non-Secure world.
@@ -26,7 +27,8 @@ ArmGicEnableDistributor (
   if (Revision == ARM_GIC_ARCH_REVISION_2) {
     MmioWrite32 (GicDistributorBase + ARM_GIC_ICDDCR, 0x1);
   } else {
-    if (MmioRead32 (GicDistributorBase + ARM_GIC_ICDDCR) & ARM_GIC_ICDDCR_ARE) {
+    GicDistributorCtl = MmioRead32 (GicDistributorBase + ARM_GIC_ICDDCR);
+    if ((GicDistributorCtl & ARM_GIC_ICDDCR_ARE) != 0) {
       MmioOr32 (GicDistributorBase + ARM_GIC_ICDDCR, 0x2);
     } else {
       MmioOr32 (GicDistributorBase + ARM_GIC_ICDDCR, 0x1);
