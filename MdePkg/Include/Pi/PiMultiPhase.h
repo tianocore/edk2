27e3f9a00c7fc63c5765c20d0b79fda6433b4f43
Kun Qin <kuqin12@gmail.com>
2021-06-24 11:33:07 +0800

    MdePkg: MmConfiguration: Move definition of EFI_MM_RESERVED_MMRAM_REGION
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3440
    
    The definition of EFI_MM_RESERVED_MMRAM_REGION, according to PI Spec 1.5
    is also referenced in EFI_PEI_MM_CONFIGURATION_PPI. Defining this
    structure as is will enforce any potential usage of MM Configuration PPI
    interface to include <Protocol/MmConfiguration.h>.
    
    This change moves this structure definition to PiMultiPhase.h, which is
    already included by Protocol/MmConfiguration.h through PiMmCis.h. It also
    paves way for introducing Ppi/MmConfiguration.h with proper dependency.
    
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Zhiguang Liu <zhiguang.liu@intel.com>
    Cc: Michael Kubacki <michael.kubacki@microsoft.com>
    
    Signed-off-by: Kun Qin <kuqin12@gmail.com>
    Reviewed-by: Liming Gao <gaoliming@byosoft.com.cn>

--- MdePkg/Include/Pi/PiMultiPhase.h
+++ MdePkg/Include/Pi/PiMultiPhase.h
@@ -133,6 +133,22 @@ typedef struct {
 
 typedef EFI_MMRAM_DESCRIPTOR  EFI_SMRAM_DESCRIPTOR;
 
+///
+/// Structure describing a MMRAM region which cannot be used for the MMRAM heap.
+///
+typedef struct _EFI_MM_RESERVED_MMRAM_REGION {
+  ///
+  /// Starting address of the reserved MMRAM area, as it appears while MMRAM is open.
+  /// Ignored if MmramReservedSize is 0.
+  ///
+  EFI_PHYSICAL_ADDRESS    MmramReservedStart;
+  ///
+  /// Number of bytes occupied by the reserved MMRAM area. A size of zero indicates the
+  /// last MMRAM area.
+  ///
+  UINT64                  MmramReservedSize;
+} EFI_MM_RESERVED_MMRAM_REGION;
+
 typedef enum {
   EFI_PCD_TYPE_8,
   EFI_PCD_TYPE_16,
