fafee564ee93ead08d1698d2cb76fbe92e0336eb
Min Xu <min.m.xu@intel.com>
2021-07-19 09:35:39 +0800

    UefiCpuPkg: Enable Tdx support in MpInitLib
    RFC: https://bugzilla.tianocore.org/show_bug.cgi?id=3429
    
    In TDVF BSP and APs are simplified. BSP is the vCPU-0, while the others
    are treated as APs.
    
    So MP intialization is rather simple. The processor info is retrieved by
    TDCALL, ApWorker is not supported, BSP is always the working processor,
    while the APs are just in a wait-for-precedure state.
    
    Cc: Brijesh Singh <brijesh.singh@amd.com>
    Cc: Erdem Aktas <erdemaktas@google.com>
    Cc: James Bottomley <jejb@linux.ibm.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Rahul Kumar <rahul1.kumar@intel.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- UefiCpuPkg/Library/MpInitLib/DxeMpLib.c
+++ UefiCpuPkg/Library/MpInitLib/DxeMpLib.c
@@ -7,6 +7,7 @@
 **/
 
 #include "MpLib.h"
+#include "MpIntelTdx.h"
 
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
@@ -15,8 +16,8 @@
 #include <Library/VmgExitLib.h>
 #include <Register/Amd/Fam17Msr.h>
 #include <Register/Amd/Ghcb.h>
-
 #include <Protocol/Timer.h>
+#include <ConfidentialComputingGuestAttr.h>
 
 #define  AP_SAFE_STACK_SIZE  128
 
@@ -824,6 +825,10 @@ MpInitLibStartupThisAP (
 {
   EFI_STATUS  Status;
 
+  if (CC_GUEST_IS_TDX (PcdGet64 (PcdConfidentialComputingGuestAttr))) {
+    return EFI_UNSUPPORTED;
+  }
+
   //
   // temporarily stop checkAllApsStatus for avoid resource dead-lock.
   //
@@ -879,6 +884,10 @@ MpInitLibSwitchBSP (
   EFI_STATUS               Status;
   EFI_TIMER_ARCH_PROTOCOL  *Timer;
   UINT64                   TimerPeriod;
+
+  if (CC_GUEST_IS_TDX (PcdGet64 (PcdConfidentialComputingGuestAttr))) {
+    return EFI_UNSUPPORTED;
+  }
 
   TimerPeriod = 0;
   //
@@ -952,6 +961,10 @@ MpInitLibEnableDisableAP (
 {
   EFI_STATUS  Status;
   BOOLEAN     TempStopCheckState;
+
+  if (CC_GUEST_IS_TDX (PcdGet64 (PcdConfidentialComputingGuestAttr))) {
+    return EFI_UNSUPPORTED;
+  }
 
   TempStopCheckState = FALSE;
   //
