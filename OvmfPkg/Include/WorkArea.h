cc0b41ab78ceb07f56a25709c97886c364dc3799
Brijesh Singh <brijesh.singh@amd.com>
2021-08-17 21:46:49 +0800

    OvmfPkg: introduce a common work area
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=3429
    
    Both the TDX and SEV support needs to reserve a page in MEMFD as a work
    area. The page will contain meta data specific to the guest type.
    Currently, the SEV-ES support reserves a page in MEMFD
    (PcdSevEsWorkArea) for the work area. This page can be reused as a TDX
    work area when Intel TDX is enabled.
    
    Based on the discussion [1], it was agreed to rename the SevEsWorkArea
    to the OvmfWorkArea, and add a header that can be used to indicate the
    work area type.
    
    [1] https://edk2.groups.io/g/devel/message/78262?p=,,,20,0,0,0::\
        created,0,SNP,20,2,0,84476064
    
    Cc: James Bottomley <jejb@linux.ibm.com>
    Cc: Min Xu <min.m.xu@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Erdem Aktas <erdemaktas@google.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Reviewed-by: Min Xu <min.m.xu@intel.com>
    Reviewed-by: Jiewen Yao <Jiewen.yao@intel.com>

---
+++ OvmfPkg/Include/WorkArea.h
@@ -0,0 +1,67 @@
+/** @file
+
+  Work Area structure definition
+
+  Copyright (c) 2021, AMD Inc.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef __OVMF_WORK_AREA_H__
+#define __OVMF_WORK_AREA_H__
+
+//
+// Guest type for the work area
+//
+typedef enum {
+  GUEST_TYPE_NON_ENCRYPTED,
+  GUEST_TYPE_AMD_SEV,
+  GUEST_TYPE_INTEL_TDX,
+
+} GUEST_TYPE;
+
+//
+// Confidential computing work area header definition. Any change
+// to the structure need to be kept in sync with the
+// PcdOvmfConfidentialComputingWorkAreaHeader.
+//
+typedef struct _CONFIDENTIAL_COMPUTING_WORK_AREA_HEADER {
+  UINT8                   GuestType;
+  UINT8                   Reserved1[3];
+} CONFIDENTIAL_COMPUTING_WORK_AREA_HEADER;
+
+//
+// Internal structure for holding SEV-ES information needed during SEC phase
+// and valid only during SEC phase and early PEI during platform
+// initialization.
+//
+// This structure is also used by assembler files:
+//   OvmfPkg/ResetVector/ResetVector.nasmb
+//   OvmfPkg/ResetVector/Ia32/PageTables64.asm
+//   OvmfPkg/ResetVector/Ia32/Flat32ToFlat64.asm
+// any changes must stay in sync with its usage.
+//
+typedef struct _SEC_SEV_ES_WORK_AREA {
+  UINT8    SevEsEnabled;
+  UINT8    Reserved1[7];
+
+  UINT64   RandomData;
+
+  UINT64   EncryptionMask;
+} SEC_SEV_ES_WORK_AREA;
+
+//
+// The SEV work area definition.
+//
+typedef struct _SEV_WORK_AREA {
+  CONFIDENTIAL_COMPUTING_WORK_AREA_HEADER   Header;
+
+  SEC_SEV_ES_WORK_AREA                      SevEsWorkArea;
+} SEV_WORK_AREA;
+
+typedef union {
+  CONFIDENTIAL_COMPUTING_WORK_AREA_HEADER   Header;
+  SEV_WORK_AREA                             SevWorkArea;
+} OVMF_WORK_AREA;
+
+#endif
