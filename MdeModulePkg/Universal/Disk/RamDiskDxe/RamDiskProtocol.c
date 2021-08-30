c5e805ffe1098601e84fd501d5fa1d45e9d96a62
Rebecca Cran <rebecca@bsdio.com>
2021-08-31 06:36:36 +0800

    MdeModulePkg: Fix typo of "memory" in RamDiskDxe debug message
    Fix a typo of "memory" in a debug message in RamDiskProtocol.c.
    
    Signed-off-by: Rebecca Cran <rebecca@bsdio.com>
    Reviewed-by: Hao A Wu <hao.a.wu@intel.com>
    Reviewed-by: Philippe Mathieu-Daude <philmd@redhat.com>

--- MdeModulePkg/Universal/Disk/RamDiskDxe/RamDiskProtocol.c
+++ MdeModulePkg/Universal/Disk/RamDiskDxe/RamDiskProtocol.c
@@ -195,7 +195,7 @@ RamDiskPublishNfit (
       MemoryFound = TRUE;
       DEBUG ((
         EFI_D_INFO,
-        "RamDiskPublishNfit: RAM disk with reserved meomry type, will publish to NFIT.\n"
+        "RamDiskPublishNfit: RAM disk with reserved memory type, will publish to NFIT.\n"
         ));
       break;
     }
