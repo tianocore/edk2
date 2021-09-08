36d25c35f4d7fbf3d77efd54b2ce86cb7a4dff6b
Gerd Hoffmann <kraxel@redhat.com>
2021-09-08 11:01:13 +0200

    OvmfPkg/Microvm: PlatformPei/Platform: add id.
    (cherry picked from commit bf02d73e74ec2825551126c780df726cc543cfb0)
    
    Add + set microvm id for PcdOvmfHostBridgePciDevId.
    
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=3599
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Acked-by: Jiewen Yao <Jiewen.yao@intel.com>
    (cherry picked from commit bf02d73e74ec2825551126c780df726cc543cfb0)
    
    Change-Id: I3075d5d7f5c869d1df912c827e410a1c3835bd52

---
+++ OvmfPkg/Include/IndustryStandard/Microvm.h
@@ -0,0 +1,12 @@
+/** @file
+  Various defines for qemu microvm
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+#ifndef __MICROVM_H__
+#define __MICROVM_H__
+
+#define MICROVM_PSEUDO_DEVICE_ID 0xfff1
+
+#endif // __MICROVM_H__
