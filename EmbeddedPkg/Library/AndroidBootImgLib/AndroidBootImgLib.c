32a88874a63036bd8ca8443c49679aed484de576
Jeff Brasen <jbrasen@nvidia.com>
2021-09-13 23:18:50 +0000

    EmbeddedPkg: Install FDT if UpdateDtb is not present
    (cherry picked from commit c0cd26f43c278781bf9a24a8268104292494f655)
    
    Currently if mAndroidBootImg->UpdateDtb is not supported on the platform
    the device tree updates of the initrd are not made.
    
    Signed-off-by: Jeff Brasen <jbrasen@nvidia.com>
    Reviewed-by: Leif Lindholm <leif@nuviainc.com>
    (cherry picked from commit c0cd26f43c278781bf9a24a8268104292494f655)
    
    Change-Id: I518b7494d76c2d25e2c135906886db0099d31d88

--- EmbeddedPkg/Library/AndroidBootImgLib/AndroidBootImgLib.c
+++ EmbeddedPkg/Library/AndroidBootImgLib/AndroidBootImgLib.c
@@ -349,12 +349,13 @@ AndroidBootImgUpdateFdt (
     if (EFI_ERROR (Status)) {
       goto Fdt_Exit;
     }
-
-    Status = gBS->InstallConfigurationTable (
-                    &gFdtTableGuid,
-                    (VOID *)(UINTN)NewFdtBase
-                    );
+  } else {
+    NewFdtBase = UpdatedFdtBase;
   }
+  Status = gBS->InstallConfigurationTable (
+                  &gFdtTableGuid,
+                  (VOID *)(UINTN)NewFdtBase
+                  );
 
   if (!EFI_ERROR (Status)) {
     return EFI_SUCCESS;
