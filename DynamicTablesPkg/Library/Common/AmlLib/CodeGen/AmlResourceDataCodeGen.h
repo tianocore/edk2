22873f58c40c496d59a0553bee1c720192ac35c9
Pierre Gondois <Pierre.Gondois@arm.com>
2021-09-30 08:48:21 +0100

    DynamicTablesPkg: Rework AmlResourceDataCodegen.c/h
    Rework all the functions to to have a generic prototype:
     - First take take the resource data specific arguments.
       E.g.: for a Register(): the AddressSpace, BitWidth, ...
     - The penultimate parameter is a NameOpNode. The resource data
       created is appended to the ResourceTemplate() contained in the
       NameOpNode.
     - The last parameter is a pointer holding the created resource data.
    
    A least one of the two last parameter must be provided. One of them can
    be omitted. This generic interface allows to either:
     - Add the resource data to a NameOpNode. This is a common case for the
       Ssdt tables generator.
     - Get the created resource data and let the caller place it in an AML
       tree.
    
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>
    Signed-off-by: Pierre Gondois <Pierre.Gondois@arm.com>

--- DynamicTablesPkg/Library/Common/AmlLib/CodeGen/AmlResourceDataCodeGen.h
+++ DynamicTablesPkg/Library/Common/AmlLib/CodeGen/AmlResourceDataCodeGen.h
@@ -1,7 +1,7 @@
 /** @file
   AML Resource Data Code Generation.
 
-  Copyright (c) 2019 - 2020, Arm Limited. All rights reserved.<BR>
+  Copyright (c) 2019 - 2021, Arm Limited. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
@@ -11,33 +11,32 @@
 
 /** Code generation for the "Interrupt ()" ASL function.
 
-  This function creates a Resource Data element corresponding to the
-  "Interrupt ()" ASL function and stores it in an AML Data Node.
-
   The Resource Data effectively created is an Extended Interrupt Resource
-  Data. See ACPI 6.3 specification, s6.4.3.6 "Extended Interrupt Descriptor"
-  for more information about Extended Interrupt Resource Data.
+  Data. Cf ACPI 6.4:
+   - s6.4.3.6 "Extended Interrupt Descriptor"
+   - s19.6.64 "Interrupt (Interrupt Resource Descriptor Macro)"
 
-  This function allocates memory to create a data node. It is the caller's
-  responsibility to either:
-   - attach this node to an AML tree;
-   - delete this node.
+  The created resource data node can be:
+   - appended to the list of resource data elements of the NameOpNode.
+     In such case NameOpNode must be defined by a the "Name ()" ASL statement
+     and initially contain a "ResourceTemplate ()".
+   - returned through the NewRdNode parameter.
 
-  @param [in]  ResourceConsumer    The device consumes the specified interrupt
-                                   or produces it for use by a child device.
-  @param [in]  EdgeTriggered       The interrupt is edge triggered or
-                                   level triggered.
-  @param [in]  ActiveLow           The interrupt is active-high or active-low.
-  @param [in]  Shared              The interrupt can be shared with other
-                                   devices or not (Exclusive).
-  @param [in]  IrqList             Interrupt list. Must be non-NULL.
-  @param [in]  IrqCount            Interrupt count. Must be non-zero.
-  @param [in]  ParentNode          If not NULL, add the generated node
-                                   to the end of the variable list of
-                                   argument of the ParentNode, but
-                                   before the "End Tag" Resource Data.
-                                   Must be a BufferOpNode.
-  @param  [out] NewRdNode          If success, contains the generated node.
+  @param  [in]  ResourceConsumer The device consumes the specified interrupt
+                                 or produces it for use by a child device.
+  @param  [in]  EdgeTriggered    The interrupt is edge triggered or
+                                 level triggered.
+  @param  [in]  ActiveLow        The interrupt is active-high or active-low.
+  @param  [in]  Shared           The interrupt can be shared with other
+                                 devices or not (Exclusive).
+  @param  [in]  IrqList          Interrupt list. Must be non-NULL.
+  @param  [in]  IrqCount         Interrupt count. Must be non-zero.
+  @param  [in]  NameOpNode       NameOp object node defining a named object.
+                                 If provided, append the new resource data node
+                                 to the list of resource data elements of this
+                                 node.
+  @param  [out] NewRdNode        If provided and success,
+                                 contain the created node.
 
   @retval EFI_SUCCESS             The function completed successfully.
   @retval EFI_INVALID_PARAMETER   Invalid parameter.
@@ -45,15 +44,15 @@
 **/
 EFI_STATUS
 EFIAPI
-AmlCodeGenInterrupt (
-  IN  BOOLEAN             ResourceConsumer,
-  IN  BOOLEAN             EdgeTriggered,
-  IN  BOOLEAN             ActiveLow,
-  IN  BOOLEAN             Shared,
-  IN  UINT32            * IrqList,
-  IN  UINT8               IrqCount,
-  IN  AML_OBJECT_NODE   * ParentNode,   OPTIONAL
-  OUT AML_DATA_NODE    ** NewRdNode     OPTIONAL
+AmlCodeGenRdInterrupt (
+  IN  BOOLEAN                 ResourceConsumer,
+  IN  BOOLEAN                 EdgeTriggered,
+  IN  BOOLEAN                 ActiveLow,
+  IN  BOOLEAN                 Shared,
+  IN  UINT32                  *IrqList,
+  IN  UINT8                   IrqCount,
+  IN  AML_OBJECT_NODE_HANDLE  NameOpNode, OPTIONAL
+  OUT AML_DATA_NODE_HANDLE    *NewRdNode  OPTIONAL
   );
 
 #endif // AML_RESOURCE_DATA_CODE_GEN_H_
