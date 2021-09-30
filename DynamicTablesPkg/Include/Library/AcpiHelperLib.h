3af871c79387120073fe48a550786bd7607a31cc
Pierre Gondois <Pierre.Gondois@arm.com>
2021-09-30 08:48:14 +0100

    DynamicTablesPkg: Rename single char input parameter
    The Ecc tool forbids the usage of one char variable: Ecc error 8007:
    "There should be no use of short (single character) variable names"
    
    To follow this policy, rename this one letter parameter.
    
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>
    Signed-off-by: Pierre Gondois <Pierre.Gondois@arm.com>

--- DynamicTablesPkg/Include/Library/AcpiHelperLib.h
+++ DynamicTablesPkg/Include/Library/AcpiHelperLib.h
@@ -22,15 +22,16 @@
 
 /** Convert a hex number to its ASCII code.
 
- @param [in]  x   Hex number to convert.
-                  Must be 0 <= x < 16.
+ @param [in]  Hex   Hex number to convert.
+                    Must be 0 <= x < 16.
 
  @return The ASCII code corresponding to x.
+         -1 if error.
 **/
 UINT8
 EFIAPI
 AsciiFromHex (
-  IN  UINT8   x
+  IN  UINT8   Hex
   );
 
 /** Check if a HID is a valid PNP ID.
