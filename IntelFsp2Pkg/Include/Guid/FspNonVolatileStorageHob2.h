4cc1458dbe004b1d70534caa55f475f6d19fe14a
Chasel Chiu <chasel.chiu@intel.com>
2021-10-01 15:34:39 +0800

    IntelFsp2Pkg: Adopt FSP 2.3 specification.
    REF:https://bugzilla.tianocore.org/show_bug.cgi?id=3674
    
    Add ExtendedImageRevision in FSP_INFO_HEADER structure, also add
    FSP_NON_VOLATILE_STORAGE_HOB2 header.
    
    Cc: Maurice Ma <maurice.ma@intel.com>
    Cc: Nate DeSimone <nathaniel.l.desimone@intel.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Signed-off-by: Chasel Chiu <chasel.chiu@intel.com>
    Reviewed-by: Nate DeSimone <nathaniel.l.desimone@intel.com>

---
+++ IntelFsp2Pkg/Include/Guid/FspNonVolatileStorageHob2.h
@@ -0,0 +1,24 @@
+/** @file
+  Intel FSP Non-Volatile Storage (NVS) HOB version 2 definition from
+  Intel Firmware Support Package External Architecture Specification v2.3.
+
+  Copyright (c) 2021, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef __FSP_NON_VOLATILE_STORAGE_HOB2_H__
+#define __FSP_NON_VOLATILE_STORAGE_HOB2_H__
+
+///
+/// The Non-Volatile Storage (NVS) HOB version 2 provides > 64KB buffer support.
+///
+typedef struct {
+  EFI_HOB_GUID_TYPE    GuidHob;
+  EFI_PHYSICAL_ADDRESS NvsDataPtr;
+  UINT64               NvsDataLength;
+} FSP_NON_VOLATILE_STORAGE_HOB2;
+
+extern EFI_GUID gFspNonVolatileStorageHob2Guid;
+
+#endif
