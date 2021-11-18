7f03d25f60e7415bb90a43e501bf5486f2638b9f
Michael Kubacki <michael.kubacki@microsoft.com>
2021-11-18 11:01:30 -0800

    UefiPayloadPkg: Apply uncrusitify changes
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3737
    
    Apply uncrustify changes to .c/.h files in the UefiPayloadPkg package
    
    Cc: Andrew Fish <afish@apple.com>
    Cc: Leif Lindholm <leif@nuviainc.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- UefiPayloadPkg/FvbRuntimeDxe/FvbSmmCommon.h
+++ UefiPayloadPkg/FvbRuntimeDxe/FvbSmmCommon.h
@@ -11,20 +11,19 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #include <Protocol/SmmFirmwareVolumeBlock.h>
 
-#define EFI_FUNCTION_GET_ATTRIBUTES           1
-#define EFI_FUNCTION_SET_ATTRIBUTES           2
-#define EFI_FUNCTION_GET_PHYSICAL_ADDRESS     3
-#define EFI_FUNCTION_GET_BLOCK_SIZE           4
-#define EFI_FUNCTION_READ                     5
-#define EFI_FUNCTION_WRITE                    6
-#define EFI_FUNCTION_ERASE_BLOCKS             7
+#define EFI_FUNCTION_GET_ATTRIBUTES        1
+#define EFI_FUNCTION_SET_ATTRIBUTES        2
+#define EFI_FUNCTION_GET_PHYSICAL_ADDRESS  3
+#define EFI_FUNCTION_GET_BLOCK_SIZE        4
+#define EFI_FUNCTION_READ                  5
+#define EFI_FUNCTION_WRITE                 6
+#define EFI_FUNCTION_ERASE_BLOCKS          7
 
 typedef struct {
-  UINTN       Function;
-  EFI_STATUS  ReturnStatus;
-  UINT8       Data[1];
+  UINTN         Function;
+  EFI_STATUS    ReturnStatus;
+  UINT8         Data[1];
 } SMM_FVB_COMMUNICATE_FUNCTION_HEADER;
-
 
 ///
 /// Size of SMM communicate header, without including the payload.
@@ -37,33 +36,33 @@ typedef struct {
 #define SMM_FVB_COMMUNICATE_HEADER_SIZE  (OFFSET_OF (SMM_FVB_COMMUNICATE_FUNCTION_HEADER, Data))
 
 typedef struct {
-  EFI_SMM_FIRMWARE_VOLUME_BLOCK_PROTOCOL     *SmmFvb;
-  EFI_FVB_ATTRIBUTES_2                       Attributes;
+  EFI_SMM_FIRMWARE_VOLUME_BLOCK_PROTOCOL    *SmmFvb;
+  EFI_FVB_ATTRIBUTES_2                      Attributes;
 } SMM_FVB_ATTRIBUTES_HEADER;
 
 typedef struct {
-  EFI_SMM_FIRMWARE_VOLUME_BLOCK_PROTOCOL     *SmmFvb;
-  EFI_PHYSICAL_ADDRESS                       Address;
+  EFI_SMM_FIRMWARE_VOLUME_BLOCK_PROTOCOL    *SmmFvb;
+  EFI_PHYSICAL_ADDRESS                      Address;
 } SMM_FVB_PHYSICAL_ADDRESS_HEADER;
 
 typedef struct {
-  EFI_SMM_FIRMWARE_VOLUME_BLOCK_PROTOCOL     *SmmFvb;
-  EFI_LBA                                    Lba;
-  UINTN                                      BlockSize;
-  UINTN                                      NumOfBlocks;
+  EFI_SMM_FIRMWARE_VOLUME_BLOCK_PROTOCOL    *SmmFvb;
+  EFI_LBA                                   Lba;
+  UINTN                                     BlockSize;
+  UINTN                                     NumOfBlocks;
 } SMM_FVB_BLOCK_SIZE_HEADER;
 
 typedef struct {
-  EFI_SMM_FIRMWARE_VOLUME_BLOCK_PROTOCOL     *SmmFvb;
-  EFI_LBA                                    Lba;
-  UINTN                                      Offset;
-  UINTN                                      NumBytes;
+  EFI_SMM_FIRMWARE_VOLUME_BLOCK_PROTOCOL    *SmmFvb;
+  EFI_LBA                                   Lba;
+  UINTN                                     Offset;
+  UINTN                                     NumBytes;
 } SMM_FVB_READ_WRITE_HEADER;
 
 typedef struct {
-  EFI_FIRMWARE_VOLUME_BLOCK_PROTOCOL         *SmmFvb;
-  EFI_LBA                                    StartLba;
-  UINTN                                      NumOfLba;
+  EFI_FIRMWARE_VOLUME_BLOCK_PROTOCOL    *SmmFvb;
+  EFI_LBA                               StartLba;
+  UINTN                                 NumOfLba;
 } SMM_FVB_BLOCKS_HEADER;
 
 #endif
