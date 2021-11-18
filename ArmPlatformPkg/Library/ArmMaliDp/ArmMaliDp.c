73d6a88a9a6609a8ac0e1cc481fd9861c77f418c
Michael Kubacki <michael.kubacki@microsoft.com>
2021-11-18 11:00:46 -0800

    ArmPlatformPkg: Apply uncrusitify changes
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3737
    
    Apply uncrustify changes to .c/.h files in the ArmPlatformPkg package
    
    Cc: Andrew Fish <afish@apple.com>
    Cc: Leif Lindholm <leif@nuviainc.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- ArmPlatformPkg/Library/ArmMaliDp/ArmMaliDp.c
+++ ArmPlatformPkg/Library/ArmMaliDp/ArmMaliDp.c
@@ -17,7 +17,7 @@
 #include "ArmMaliDp.h"
 
 // CORE_ID of the MALI DP
-STATIC UINT32 mDpDeviceId;
+STATIC UINT32  mDpDeviceId;
 
 /** Disable the graphics layer
 
@@ -25,7 +25,9 @@ STATIC UINT32 mDpDeviceId;
 **/
 STATIC
 VOID
-LayerGraphicsDisable (VOID)
+LayerGraphicsDisable (
+  VOID
+  )
 {
   MmioAnd32 (DP_BASE + DP_DE_LG_CONTROL, ~DP_DE_LG_ENABLE);
 }
@@ -36,7 +38,9 @@ LayerGraphicsDisable (VOID)
 **/
 STATIC
 VOID
-LayerGraphicsEnable (VOID)
+LayerGraphicsEnable (
+  VOID
+  )
 {
   MmioOr32 (DP_BASE + DP_DE_LG_CONTROL, DP_DE_LG_ENABLE);
 }
@@ -49,7 +53,7 @@ LayerGraphicsEnable (VOID)
 STATIC
 VOID
 LayerGraphicsSetFrame (
-  IN CONST EFI_PHYSICAL_ADDRESS FrameBaseAddress
+  IN CONST EFI_PHYSICAL_ADDRESS  FrameBaseAddress
   )
 {
   // Disable the graphics layer.
@@ -84,12 +88,12 @@ LayerGraphicsSetFrame (
 STATIC
 VOID
 LayerGraphicsConfig (
-  IN CONST EFI_GRAPHICS_PIXEL_FORMAT UefiGfxPixelFormat,
-  IN CONST UINT32 HRes,
-  IN CONST UINT32 VRes
+  IN CONST EFI_GRAPHICS_PIXEL_FORMAT  UefiGfxPixelFormat,
+  IN CONST UINT32                     HRes,
+  IN CONST UINT32                     VRes
   )
 {
-  UINT32 PixelFormat;
+  UINT32  PixelFormat;
 
   // Disable the graphics layer before configuring any settings.
   LayerGraphicsDisable ();
@@ -134,26 +138,26 @@ LayerGraphicsConfig (
 STATIC
 VOID
 SetDisplayEngineTiming (
-  IN CONST SCAN_TIMINGS * CONST Horizontal,
-  IN CONST SCAN_TIMINGS * CONST Vertical
+  IN CONST SCAN_TIMINGS *CONST  Horizontal,
+  IN CONST SCAN_TIMINGS *CONST  Vertical
   )
 {
-  UINTN RegHIntervals;
-  UINTN RegVIntervals;
-  UINTN RegSyncControl;
-  UINTN RegHVActiveSize;
+  UINTN  RegHIntervals;
+  UINTN  RegVIntervals;
+  UINTN  RegSyncControl;
+  UINTN  RegHVActiveSize;
 
   if (mDpDeviceId == MALIDP_500) {
     // MALI DP500 timing registers.
-    RegHIntervals = DP_BASE + DP_DE_DP500_H_INTERVALS;
-    RegVIntervals = DP_BASE + DP_DE_DP500_V_INTERVALS;
-    RegSyncControl = DP_BASE + DP_DE_DP500_SYNC_CONTROL;
+    RegHIntervals   = DP_BASE + DP_DE_DP500_H_INTERVALS;
+    RegVIntervals   = DP_BASE + DP_DE_DP500_V_INTERVALS;
+    RegSyncControl  = DP_BASE + DP_DE_DP500_SYNC_CONTROL;
     RegHVActiveSize = DP_BASE + DP_DE_DP500_HV_ACTIVESIZE;
   } else {
     // MALI DP550/DP650 timing registers.
-    RegHIntervals = DP_BASE + DP_DE_H_INTERVALS;
-    RegVIntervals = DP_BASE + DP_DE_V_INTERVALS;
-    RegSyncControl = DP_BASE + DP_DE_SYNC_CONTROL;
+    RegHIntervals   = DP_BASE + DP_DE_H_INTERVALS;
+    RegVIntervals   = DP_BASE + DP_DE_V_INTERVALS;
+    RegSyncControl  = DP_BASE + DP_DE_SYNC_CONTROL;
     RegHVActiveSize = DP_BASE + DP_DE_HV_ACTIVESIZE;
   }
 
@@ -194,11 +198,11 @@ UINT32
 ArmMaliDpGetCoreId (
   )
 {
-  UINT32 DpCoreId;
+  UINT32  DpCoreId;
 
   // First check for DP500 as register offset for DP550/DP650 CORE_ID
   // is beyond 3K/4K register space of the DP500.
-  DpCoreId = MmioRead32 (DP_BASE + DP_DE_DP500_CORE_ID);
+  DpCoreId   = MmioRead32 (DP_BASE + DP_DE_DP500_CORE_ID);
   DpCoreId >>= DP_DE_DP500_CORE_ID_SHIFT;
 
   if (DpCoreId == MALIDP_500) {
@@ -206,7 +210,7 @@ ArmMaliDpGetCoreId (
   }
 
   // Check for DP550 or DP650.
-  DpCoreId = MmioRead32 (DP_BASE + DP_DC_CORE_ID);
+  DpCoreId   = MmioRead32 (DP_BASE + DP_DC_CORE_ID);
   DpCoreId >>= DP_DC_CORE_ID_SHIFT;
 
   if ((DpCoreId == MALIDP_550) || (DpCoreId == MALIDP_650)) {
@@ -227,9 +231,12 @@ ArmMaliDpGetCoreId (
                                 on the platform.
 **/
 EFI_STATUS
-LcdIdentify (VOID)
+LcdIdentify (
+  VOID
+  )
 {
-  DEBUG ((DEBUG_WARN,
+  DEBUG ((
+    DEBUG_WARN,
     "Probing ARM Mali DP500/DP550/DP650 at base address 0x%p\n",
     DP_BASE
     ));
@@ -239,8 +246,8 @@ LcdIdentify (VOID)
   }
 
   if (mDpDeviceId == MALIDP_NOT_PRESENT) {
-     DEBUG ((DEBUG_WARN, "ARM Mali DP not found...\n"));
-     return EFI_NOT_FOUND;
+    DEBUG ((DEBUG_WARN, "ARM Mali DP not found...\n"));
+    return EFI_NOT_FOUND;
   }
 
   DEBUG ((DEBUG_WARN, "Found ARM Mali DP %x\n", mDpDeviceId));
@@ -256,7 +263,7 @@ LcdIdentify (VOID)
 **/
 EFI_STATUS
 LcdInitialize (
-  IN CONST EFI_PHYSICAL_ADDRESS FrameBaseAddress
+  IN CONST EFI_PHYSICAL_ADDRESS  FrameBaseAddress
   )
 {
   DEBUG ((DEBUG_WARN, "Framebuffer base address = %p\n", FrameBaseAddress));
@@ -266,8 +273,11 @@ LcdInitialize (
   }
 
   if (mDpDeviceId == MALIDP_NOT_PRESENT) {
-    DEBUG ((DEBUG_ERROR, "ARM Mali DP initialization failed,"
-      "no ARM Mali DP present\n"));
+    DEBUG ((
+      DEBUG_ERROR,
+      "ARM Mali DP initialization failed,"
+      "no ARM Mali DP present\n"
+      ));
     return EFI_NOT_FOUND;
   }
 
@@ -285,7 +295,9 @@ LcdInitialize (
 **/
 STATIC
 VOID
-SetConfigurationMode (VOID)
+SetConfigurationMode (
+  VOID
+  )
 {
   // Request configuration Mode.
   if (mDpDeviceId == MALIDP_500) {
@@ -303,7 +315,9 @@ SetConfigurationMode (VOID)
 **/
 STATIC
 VOID
-SetNormalMode (VOID)
+SetNormalMode (
+  VOID
+  )
 {
   // Disable configuration Mode.
   if (mDpDeviceId == MALIDP_500) {
@@ -321,7 +335,9 @@ SetNormalMode (VOID)
 **/
 STATIC
 VOID
-SetConfigValid (VOID)
+SetConfigValid (
+  VOID
+  )
 {
   if (mDpDeviceId == MALIDP_500) {
     MmioOr32 (DP_BASE + DP_DP500_CONFIG_VALID, DP_DC_CONFIG_VALID);
@@ -396,7 +412,9 @@ LcdSetMode (
 
 **/
 VOID
-LcdShutdown (VOID)
+LcdShutdown (
+  VOID
+  )
 {
   // Disable graphics layer.
   LayerGraphicsDisable ();
