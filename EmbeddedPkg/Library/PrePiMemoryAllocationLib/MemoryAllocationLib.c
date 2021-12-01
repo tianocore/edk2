8e3848bcb5b00ab7c05b146754d8bf9c746a97c3
Min Xu <min.m.xu@intel.com>
2021-12-01 16:25:09 +0800

    EmbeddedPkg/MemoryAllocationLib: Add null stub for AllocateCopyPool
    RFC: https://bugzilla.tianocore.org/show_bug.cgi?id=3429
    
    This function is a null stub to make the build success.
    
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Brijesh Singh <brijesh.singh@amd.com>
    Cc: Erdem Aktas <erdemaktas@google.com>
    Cc: James Bottomley <jejb@linux.ibm.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Leif Lindholm <leif@nuviainc.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Abner Chang <abner.chang@hpe.com>
    Cc: Daniel Schaefer <daniel.schaefer@hpe.com>
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- EmbeddedPkg/Library/PrePiMemoryAllocationLib/MemoryAllocationLib.c
+++ EmbeddedPkg/Library/PrePiMemoryAllocationLib/MemoryAllocationLib.c
@@ -221,6 +221,34 @@ AllocateZeroPool (
 }
 
 /**
+  Copies a buffer to an allocated buffer of type EfiBootServicesData.
+
+  Allocates the number bytes specified by AllocationSize of type EfiBootServicesData, copies
+  AllocationSize bytes from Buffer to the newly allocated buffer, and returns a pointer to the
+  allocated buffer.  If AllocationSize is 0, then a valid buffer of 0 size is returned.  If there
+  is not enough memory remaining to satisfy the request, then NULL is returned.
+
+  If Buffer is NULL, then ASSERT().
+  If AllocationSize is greater than (MAX_ADDRESS - Buffer + 1), then ASSERT().
+
+  @param  AllocationSize        The number of bytes to allocate and zero.
+  @param  Buffer                The buffer to copy to the allocated buffer.
+
+  @return A pointer to the allocated buffer or NULL if allocation fails.
+
+**/
+VOID *
+EFIAPI
+AllocateCopyPool (
+  IN UINTN       AllocationSize,
+  IN CONST VOID  *Buffer
+  )
+{
+  ASSERT (FALSE);
+  return NULL;
+}
+
+/**
   Frees a buffer that was previously allocated with one of the pool allocation functions in the
   Memory Allocation Library.
 
