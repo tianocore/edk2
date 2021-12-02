7e35ce318e0d2f83872ac9e80ec9a9cb2035ec09
Gerd Hoffmann <kraxel@redhat.com>
2021-12-02 01:56:25 +0100

    CryptoPkg/openssl: add dummy file store
    Needed to link openssl 3.0 with storage management code excluded.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- CryptoPkg/Library/OpensslLib/ossl_store.c
+++ CryptoPkg/Library/OpensslLib/ossl_store.c
@@ -6,6 +6,8 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
+#include <openssl/store.h>       /* The OSSL_STORE_INFO type numbers */
+
 /*
  * This function is cleanup ossl store.
  *
@@ -17,3 +19,17 @@ ossl_store_cleanup_int (
   )
 {
 }
+
+static void *
+file_open (
+  void        *provctx,
+  const char  *uri
+  )
+{
+  return NULL;
+}
+
+const OSSL_DISPATCH  ossl_file_store_functions[] = {
+  { OSSL_FUNC_STORE_OPEN, (void (*)(void)) file_open },
+  { 0,                    NULL                       },
+};
