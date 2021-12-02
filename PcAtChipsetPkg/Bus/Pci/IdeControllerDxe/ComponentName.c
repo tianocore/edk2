cf89aa3c3b66a42f269ccf4ac6e79e529ffdfc87
Michael Kubacki <michael.kubacki@microsoft.com>
2021-12-02 13:51:29 -0800

    PcAtChipsetPkg: Apply uncrusitify changes
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3737
    
    Apply uncrustify changes to .c/.h files in the PcAtChipsetPkg package
    
    Cc: Andrew Fish <afish@apple.com>
    Cc: Leif Lindholm <leif@nuviainc.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- PcAtChipsetPkg/Bus/Pci/IdeControllerDxe/ComponentName.c
+++ PcAtChipsetPkg/Bus/Pci/IdeControllerDxe/ComponentName.c
@@ -21,16 +21,16 @@ GLOBAL_REMOVE_IF_UNREFERENCED EFI_COMPONENT_NAME_PROTOCOL  gIdeControllerCompone
 //
 /// EFI Component Name 2 Protocol
 ///
-GLOBAL_REMOVE_IF_UNREFERENCED EFI_COMPONENT_NAME2_PROTOCOL gIdeControllerComponentName2 = {
-  (EFI_COMPONENT_NAME2_GET_DRIVER_NAME) IdeControllerComponentNameGetDriverName,
-  (EFI_COMPONENT_NAME2_GET_CONTROLLER_NAME) IdeControllerComponentNameGetControllerName,
+GLOBAL_REMOVE_IF_UNREFERENCED EFI_COMPONENT_NAME2_PROTOCOL  gIdeControllerComponentName2 = {
+  (EFI_COMPONENT_NAME2_GET_DRIVER_NAME)IdeControllerComponentNameGetDriverName,
+  (EFI_COMPONENT_NAME2_GET_CONTROLLER_NAME)IdeControllerComponentNameGetControllerName,
   "en"
 };
 
 //
 /// Driver Name Strings
 ///
-GLOBAL_REMOVE_IF_UNREFERENCED EFI_UNICODE_STRING_TABLE mIdeControllerDriverNameTable[] = {
+GLOBAL_REMOVE_IF_UNREFERENCED EFI_UNICODE_STRING_TABLE  mIdeControllerDriverNameTable[] = {
   {
     "eng;en",
     (CHAR16 *)L"IDE Controller Init Driver"
@@ -44,7 +44,7 @@ GLOBAL_REMOVE_IF_UNREFERENCED EFI_UNICODE_STRING_TABLE mIdeControllerDriverNameT
 ///
 /// Controller Name Strings
 ///
-GLOBAL_REMOVE_IF_UNREFERENCED EFI_UNICODE_STRING_TABLE mIdeControllerControllerNameTable[] = {
+GLOBAL_REMOVE_IF_UNREFERENCED EFI_UNICODE_STRING_TABLE  mIdeControllerControllerNameTable[] = {
   {
     "eng;en",
     (CHAR16 *)L"PCAT IDE Controller"
@@ -136,14 +136,14 @@ IdeControllerComponentNameGetDriverName (
 EFI_STATUS
 EFIAPI
 IdeControllerComponentNameGetControllerName (
-  IN  EFI_COMPONENT_NAME_PROTOCOL                     *This,
-  IN  EFI_HANDLE                                      ControllerHandle,
-  IN  EFI_HANDLE                                      ChildHandle        OPTIONAL,
-  IN  CHAR8                                           *Language,
-  OUT CHAR16                                          **ControllerName
+  IN  EFI_COMPONENT_NAME_PROTOCOL  *This,
+  IN  EFI_HANDLE                   ControllerHandle,
+  IN  EFI_HANDLE                   ChildHandle        OPTIONAL,
+  IN  CHAR8                        *Language,
+  OUT CHAR16                       **ControllerName
   )
 {
-  EFI_STATUS            Status;
+  EFI_STATUS  Status;
 
   //
   // Make sure this driver is currently managing ControllHandle
@@ -162,10 +162,10 @@ IdeControllerComponentNameGetControllerName (
   }
 
   return LookupUnicodeString2 (
-          Language,
-          This->SupportedLanguages,
-          mIdeControllerControllerNameTable,
-          ControllerName,
-          (BOOLEAN)(This == &gIdeControllerComponentName)
-          );
+           Language,
+           This->SupportedLanguages,
+           mIdeControllerControllerNameTable,
+           ControllerName,
+           (BOOLEAN)(This == &gIdeControllerComponentName)
+           );
 }
