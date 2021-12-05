cde7fa1f57148d35f4b1b9fbb7d7eb67287e70ee
Michael Kubacki <michael.kubacki@microsoft.com>
2021-12-05 14:54:15 -0800

    SourceLevelDebugPkg: Apply uncrustify changes
    (cherry picked from commit c1e126b1196de75e0a4cda21e4551ea9bb05e059)
    
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3737
    
    Apply uncrustify changes to .c/.h files in the SourceLevelDebugPkg package
    
    Cc: Andrew Fish <afish@apple.com>
    Cc: Leif Lindholm <leif@nuviainc.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>
    Reviewed-by: Hao A Wu <hao.a.wu@intel.com>
    Reviewed-by: Ray Ni <ray.ni@intel.com>
    (cherry picked from commit c1e126b1196de75e0a4cda21e4551ea9bb05e059)
    
    Change-Id: I048b5e3cf440ad7f00861bce6eb45bbbe254172f

--- SourceLevelDebugPkg/Library/DebugAgent/DebugAgentCommon/Ia32/DebugException.h
+++ SourceLevelDebugPkg/Library/DebugAgent/DebugAgentCommon/Ia32/DebugException.h
@@ -9,22 +9,22 @@
 #ifndef _DEBUG_EXCEPTION_H_
 #define _DEBUG_EXCEPTION_H_
 
-#define DEBUG_EXCEPT_DIVIDE_ERROR             0
-#define DEBUG_EXCEPT_DEBUG                    1
-#define DEBUG_EXCEPT_NMI                      2
-#define DEBUG_EXCEPT_BREAKPOINT               3
-#define DEBUG_EXCEPT_OVERFLOW                 4
-#define DEBUG_EXCEPT_BOUND                    5
-#define DEBUG_EXCEPT_INVALID_OPCODE           6
-#define DEBUG_EXCEPT_DOUBLE_FAULT             8
-#define DEBUG_EXCEPT_INVALID_TSS             10
-#define DEBUG_EXCEPT_SEG_NOT_PRESENT         11
-#define DEBUG_EXCEPT_STACK_FAULT             12
-#define DEBUG_EXCEPT_GP_FAULT                13
-#define DEBUG_EXCEPT_PAGE_FAULT              14
-#define DEBUG_EXCEPT_FP_ERROR                16
-#define DEBUG_EXCEPT_ALIGNMENT_CHECK         17
-#define DEBUG_EXCEPT_MACHINE_CHECK           18
-#define DEBUG_EXCEPT_SIMD                    19
+#define DEBUG_EXCEPT_DIVIDE_ERROR     0
+#define DEBUG_EXCEPT_DEBUG            1
+#define DEBUG_EXCEPT_NMI              2
+#define DEBUG_EXCEPT_BREAKPOINT       3
+#define DEBUG_EXCEPT_OVERFLOW         4
+#define DEBUG_EXCEPT_BOUND            5
+#define DEBUG_EXCEPT_INVALID_OPCODE   6
+#define DEBUG_EXCEPT_DOUBLE_FAULT     8
+#define DEBUG_EXCEPT_INVALID_TSS      10
+#define DEBUG_EXCEPT_SEG_NOT_PRESENT  11
+#define DEBUG_EXCEPT_STACK_FAULT      12
+#define DEBUG_EXCEPT_GP_FAULT         13
+#define DEBUG_EXCEPT_PAGE_FAULT       14
+#define DEBUG_EXCEPT_FP_ERROR         16
+#define DEBUG_EXCEPT_ALIGNMENT_CHECK  17
+#define DEBUG_EXCEPT_MACHINE_CHECK    18
+#define DEBUG_EXCEPT_SIMD             19
 
 #endif
