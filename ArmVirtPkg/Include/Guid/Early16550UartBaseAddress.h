28932ea856f7f1fecd99123aa557f8456e752454
Michael Kubacki <michael.kubacki@microsoft.com>
2021-12-05 14:53:52 -0800

    ArmVirtPkg: Apply uncrustify changes
    (cherry picked from commit 2b16a4fb91b9b31c0d152588f5ac51080c6c0763)
    
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3737
    
    Apply uncrustify changes to .c/.h files in the ArmVirtPkg package
    
    Cc: Andrew Fish <afish@apple.com>
    Cc: Leif Lindholm <leif@nuviainc.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>
    (cherry picked from commit 2b16a4fb91b9b31c0d152588f5ac51080c6c0763)
    
    Change-Id: I4300dfabfac53432e6e670d2fbd6393cbba399a4

--- ArmVirtPkg/Include/Guid/Early16550UartBaseAddress.h
+++ ArmVirtPkg/Include/Guid/Early16550UartBaseAddress.h
@@ -11,12 +11,12 @@
 #ifndef EARLY_16550_UART_BASE_ADDRESS_H__
 #define EARLY_16550_UART_BASE_ADDRESS_H__
 
-#define EARLY_16550_UART_BASE_ADDRESS_GUID {       \
+#define EARLY_16550_UART_BASE_ADDRESS_GUID  {      \
   0xea67ca3e, 0x1f54, 0x436b, {                    \
     0x97, 0x88, 0xd4, 0xeb, 0x29, 0xc3, 0x42, 0x67 \
     }                                              \
   }
 
-extern EFI_GUID gEarly16550UartBaseAddressGuid;
+extern EFI_GUID  gEarly16550UartBaseAddressGuid;
 
 #endif // EARLY_16550_UART_BASE_ADDRESS_H__
