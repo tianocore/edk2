f62d6c79c333ec6420be26abc4908b0f961ee890
Brijesh Singh <brijesh.singh@amd.com>
2021-12-09 11:27:59 +0800

    OvmfPkg/AmdSev: expose the SNP reserved pages through configuration table
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=3275
    
    Now that both the secrets and cpuid pages are reserved in the HOB,
    extract the location details through fixed PCD and make it available
    to the guest OS through the configuration table.
    
    Cc: Michael Roth <michael.roth@amd.com>
    Cc: James Bottomley <jejb@linux.ibm.com>
    Cc: Min Xu <min.m.xu@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Erdem Aktas <erdemaktas@google.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

---
+++ OvmfPkg/Include/Guid/ConfidentialComputingSevSnpBlob.h
@@ -0,0 +1,33 @@
+/** @file
+   UEFI Configuration Table for exposing the SEV-SNP launch blob.
+
+   Copyright (c) 2021, Advanced Micro Devices Inc. All right reserved.
+
+   SPDX-License-Identifier: BSD-2-Clause-Patent
+ **/
+
+#ifndef CONFIDENTIAL_COMPUTING_SEV_SNP_BLOB_H_
+#define CONFIDENTIAL_COMPUTING_SEV_SNP_BLOB_H_
+
+#include <Uefi/UefiBaseType.h>
+
+#define CONFIDENTIAL_COMPUTING_SNP_BLOB_GUID            \
+  { 0x067b1f5f,                                         \
+    0xcf26,                                             \
+    0x44c5,                                             \
+    { 0x85, 0x54, 0x93, 0xd7, 0x77, 0x91, 0x2d, 0x42 }, \
+  }
+
+typedef struct {
+  UINT32    Header;
+  UINT16    Version;
+  UINT16    Reserved1;
+  UINT64    SecretsPhysicalAddress;
+  UINT32    SecretsSize;
+  UINT64    CpuidPhysicalAddress;
+  UINT32    CpuidLSize;
+} CONFIDENTIAL_COMPUTING_SNP_BLOB_LOCATION;
+
+extern EFI_GUID  gConfidentialComputingSevSnpBlobGuid;
+
+#endif
