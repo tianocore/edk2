8791f6af5dd158a641b28a9a2ffa322f0fac3ab8
Abner Chang <abner.chang@hpe.com>
2021-12-13 10:51:05 +0800

    RISC-V: Use U5 SMBIOS library only for those platforms
    (This is migrated from edk2-platforms)
    Cc: Abner Chang <abner.chang@hpe.com>
    Cc: Sunil V L <sunilvl@ventanamicro.com>
    Reviewed-by: Abner Chang <abner.chang@hpe.com>
    
    Signed-off-by: Daniel Schaefer <daniel.schaefer@hpe.com>

---
+++ Platform/RISC-V/PlatformPkg/Library/PeiCoreInfoHobLibNull/CoreInfoHob.c
@@ -0,0 +1,37 @@
+/**@file
+  Build up platform processor information.
+
+  Copyright (c) 2021, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+//
+// The package level header files this module uses
+//
+#include <PiPei.h>
+
+//
+// The Library classes this module consumes
+//
+#include <Library/DebugLib.h>
+#include <Library/HobLib.h>
+#include <Library/BaseMemoryLib.h>
+
+#include <ProcessorSpecificHobData.h>
+
+
+/**
+  Build processor and platform information for a generic RISC-V
+
+  @return EFI_SUCCESS     Status.
+
+**/
+EFI_STATUS
+BuildRiscVSmbiosHobs (
+  VOID
+)
+{
+  return EFI_SUCCESS;
+}
