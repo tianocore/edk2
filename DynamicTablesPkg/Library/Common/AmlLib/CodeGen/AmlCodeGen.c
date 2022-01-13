f94e5ad24d0fbf57bdee51145cf191cc85da78d5
Rebecca Cran <quic_rcran@quicinc.com>
2022-01-13 09:40:51 -0700

    DynamicTablesPkg: Remove redundant cast in AmlCodeGenReturn
    In AmlCodeGenReturn, the cast to AML_NODE_HEADER* in the call to
    AmlSetFixedArgument is redundant because ReturnNode is already a
    AML_NODE_HEADER* .
    
    Signed-off-by: Rebecca Cran <quic_rcran@quicinc.com>
    Reviewed-by: Pierre Gondois <pierre.gondois@arm.com>
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>

--- DynamicTablesPkg/Library/Common/AmlLib/CodeGen/AmlCodeGen.c
+++ DynamicTablesPkg/Library/Common/AmlLib/CodeGen/AmlCodeGen.c
@@ -1585,7 +1585,7 @@ AmlCodeGenReturn (
   Status = AmlSetFixedArgument (
              ObjectNode,
              EAmlParseIndexTerm0,
-             (AML_NODE_HEADER *)ReturnNode
+             ReturnNode
              );
   if (EFI_ERROR (Status)) {
     ASSERT (0);
