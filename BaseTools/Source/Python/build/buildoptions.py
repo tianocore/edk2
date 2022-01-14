d62afd7e2c5f217656bcc725144b6699fc1b4955
Chao Li <lichao@loongson.cn>
2022-01-14 12:19:15 +0800

    BaseTools: Updated build tools to support new LoongArch.
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4053
    
    Python code changes for building EDK2 LoongArch platform.
    
    Cc: Bob Feng <bob.c.feng@intel.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Yuwei Chen <yuwei.chen@intel.com>
    
    Signed-off-by: Chao Li <lichao@loongson.cn>
    Co-authored-by: Baoqi Zhang <zhangbaoqi@loongson.cn>
    Reviewed-by: Liming Gao <gaoliming@byosoft.com.cn>

--- BaseTools/Source/Python/build/buildoptions.py
+++ BaseTools/Source/Python/build/buildoptions.py
@@ -4,6 +4,7 @@
 #  Copyright (c) 2014, Hewlett-Packard Development Company, L.P.<BR>
 #  Copyright (c) 2007 - 2021, Intel Corporation. All rights reserved.<BR>
 #  Copyright (c) 2018 - 2020, Hewlett Packard Enterprise Development, L.P.<BR>
+#  Copyright (c) 2022, Loongson Technology Corporation Limited. All rights reserved.<BR>
 #
 #  SPDX-License-Identifier: BSD-2-Clause-Patent
 #
@@ -41,7 +42,7 @@ class MyOptionParser():
     def GetOption(self):
         Parser = OptionParser(description=__copyright__, version=__version__, prog="build.exe", usage="%prog [options] [all|fds|genc|genmake|clean|cleanall|cleanlib|modules|libraries|run]")
         Parser.add_option("-a", "--arch", action="append", dest="TargetArch",
-            help="ARCHS is one of list: IA32, X64, ARM, AARCH64, RISCV64 or EBC, which overrides target.txt's TARGET_ARCH definition. To specify more archs, please repeat this option.")
+            help="ARCHS is one of list: IA32, X64, ARM, AARCH64, RISCV64, LOONGARCH64 or EBC, which overrides target.txt's TARGET_ARCH definition. To specify more archs, please repeat this option.")
         Parser.add_option("-p", "--platform", action="callback", type="string", dest="PlatformFile", callback=SingleCheckCallback,
             help="Build the platform specified by the DSC file name argument, overriding target.txt's ACTIVE_PLATFORM definition.")
         Parser.add_option("-m", "--module", action="callback", type="string", dest="ModuleFile", callback=SingleCheckCallback,
