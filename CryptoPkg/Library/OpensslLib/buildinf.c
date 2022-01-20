9992485256ce8598015adef50f0808b68a1ccf2c
Gerd Hoffmann <kraxel@redhat.com>
2022-01-20 07:13:59 +0100

    [fixup] buildinf licence tag

--- CryptoPkg/Library/OpensslLib/buildinf.c
+++ CryptoPkg/Library/OpensslLib/buildinf.c
@@ -1,3 +1,10 @@
+/** @file
+  compiler_flags string for openssl
+
+  Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
 #include "buildinf.h"
 
 const char  *compiler_flags = "compiler: information not available from edk2";
