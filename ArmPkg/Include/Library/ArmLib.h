ac478d92340a5000e89435f4a7e5e5e843eb5bc4
Pierre Gondois <Pierre.Gondois@arm.com>
2022-01-21 12:17:04 +0100

    ArmPkg/ArmLib: Add ArmHasAesExt()
    Add a ArmHasAesExt() to check for the FEAT_AES extension.
    
    Change-Id: Idbb95cc4b67153f8ccefd1c56bc622eacee29ded
    Signed-off-by: Pierre Gondois <pierre.gondois@arm.com>

--- ArmPkg/Include/Library/ArmLib.h
+++ ArmPkg/Include/Library/ArmLib.h
@@ -1,7 +1,7 @@
 /** @file
 
   Copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
-  Copyright (c) 2011 - 2022, Arm Limited. All rights reserved.<BR>
+  Copyright (c) 2011 - 2022, Arm Ltd. All rights reserved.<BR>
   Copyright (c) 2020 - 2021, NUVIA Inc. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -776,6 +776,16 @@ ArmHasCcidx (
 **/
 BOOLEAN
 ArmHasRngExt (
+  VOID
+  );
+
+/** Check if FEAT_AES extension is available.
+
+  @retval TRUE if FEAT_AES extension is available.
+  @retval FALSE otherwise.
+**/
+BOOLEAN
+ArmHasAesExt (
   VOID
   );
 
