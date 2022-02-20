7a5a591f118670304f4ad37b6d80b718eb4292f7
Patrick Rudolph <patrick.rudolph@9elements.com>
2022-02-20 14:22:46 +0100

    UefiPayloadPkg/Include/Coreboot: Add headers for SMMSTOREv2 table
    Since commit bc744f5893fc4d53275ed26dd8d968011c6a09c1 coreboot supports
    the SMMSTORE v2 feature. It implements an SMI handler that is able to
    write, read and erase pages in the boot media (SPI flash).
    The existence of this optional feature is advertised by a coreboot table.
    
    Add the tag and headers to parse the table.
    
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- UefiPayloadPkg/Include/Coreboot.h
+++ UefiPayloadPkg/Include/Coreboot.h
@@ -249,6 +249,19 @@ struct cb_cbmem_tab {
   UINT64    cbmem_tab;
 };
 
+#define CB_TAG_SMMSTOREV2  0x0039
+struct cb_smmstorev2 {
+  UINT32    tag;
+  UINT32    size;
+  UINT32    num_blocks;      /* Number of writeable blocks in Smm */
+  UINT32    block_size;      /* Size of a block in byte. Default: 64 KiB */
+  UINT32    mmap_addr;       /* MMIO address of the store for read only access */
+  UINT32    com_buffer;      /* Physical address of the communication buffer */
+  UINT32    com_buffer_size; /* Size of the communication buffer in byte */
+  UINT8     apm_cmd;         /* The command byte to write to the APM I/O port */
+  UINT8     unused[3];       /* Set to zero */
+};
+
 /*
  * Machine-friendly version of a system firmware component.  A component is
  * identified by a GUID.  coreboot is an obvious main component but there could
