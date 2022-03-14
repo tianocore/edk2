256e39eddf13ec2107a703b9b30e64f107722260
Judah Vang <judah.vang@intel.com>
2022-03-14 14:03:36 -0700

    SecurityPkg: Add new Protected variable GUIDs REF: https://bugzilla.tianocore.org/show_bug.cgi?id=2594
    Add new Protected variable GUIDs
    
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Nishant C Mistry <nishant.c.mistry@intel.com>
    Signed-off-by: judah.vang@intel.com

---
+++ SecurityPkg/Include/Guid/ProtectedVariable.h
@@ -0,0 +1,22 @@
+/** @file
+  The GUID definitions specific for protected variable services.
+
+Copyright (c) 2022, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef __PROTECTED_VARIABLE_H__
+#define __PROTECTED_VARIABLE_H__
+
+#define EDKII_PROTECTED_VARIABLE_GLOBAL_GUID \
+  { 0x8ebf379a, 0xf18e, 0x4728, { 0xa4, 0x10, 0x0, 0xcf, 0x9a, 0x65, 0xbe, 0x91 } }
+
+#define EDKII_METADATA_HMAC_VARIABLE_GUID \
+  { 0xb54cda50, 0xec54, 0x4b20, { 0x85, 0xb4, 0x57, 0xbf, 0x52, 0x98, 0x68, 0x3d } }
+
+extern EFI_GUID gEdkiiProtectedVariableGlobalGuid;
+extern EFI_GUID gEdkiiMetaDataHmacVariableGuid;
+extern EFI_GUID gEdkiiProtectedVariableContextGuid;
+
+#endif // __PROTECTED_VARIABLE_H__
