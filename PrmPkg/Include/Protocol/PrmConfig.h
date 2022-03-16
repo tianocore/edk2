ucsb-seclab/harden-HBFA

PrmPkg: Add PrmConfig protocol interface
The PRM Configuration protocol is used by PRM module configuration
libraries to describe their resources so that a generic PRM Configuration
DXE driver can prepare those resources for OS runtime.

Therefore, multiple instances are expected in the system per boot with
approximately one instance per PRM module. All PRM Configuration Protocol
instances must be installed prior to end of DXE.

Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

PrmPkg/Include/Protocol/PrmConfig.h
@@ -1,0 +1,32 @@
+/** @file
+
+  PRM Configuration protocol
+
+  PRM Configuration protocol is used by PRM module configuration libraries to
+  describe their resources so that a generic PRM Configuration DXE driver can prepare those
+  resources for OS runtime.
+
+  Copyright (c) Microsoft Corporation
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef __PRM_CONFIG_H__
+#define __PRM_CONFIG_H__
+
+#include <PrmContextBuffer.h>
+#include <Uefi.h>
+
+typedef struct _PRM_CONFIG_PROTOCOL PRM_CONFIG_PROTOCOL;
+
+#define PRM_CONFIG_PROTOCOL_SIGNATURE   SIGNATURE_32('P','M','C','P')
+#define PRM_CONFIG_PROTOCOL_VERSION     1
+
+struct _PRM_CONFIG_PROTOCOL
+{
+  PRM_MODULE_CONTEXT_BUFFERS            ModuleContextBuffers;
+};
+
+extern EFI_GUID gPrmConfigProtocolGuid;
+
+#endif

tianocore/edk2

REF:https://bugzilla.tianocore.org/show_bug.cgi?id=3812
Cc: Andrew Fish <afish@apple.com>
Cc: Kang Gao <kang.gao@intel.com>
Cc: Michael D Kinney <michael.d.kinney@intel.com>
Cc: Michael Kubacki <michael.kubacki@microsoft.com>
Cc: Leif Lindholm <leif@nuviainc.com>
Cc: Benjamin You <benjamin.you@intel.com>
Cc: Liu Yun <yun.y.liu@intel.com>
Cc: Ankit Sinha <ankit.sinha@intel.com>

PrmPkg/Include/Protocol/PrmConfig.h
@@ -1,0 +1,32 @@
+/** @file
+
+  PRM Configuration protocol
+
+  PRM Configuration protocol is used by PRM module configuration libraries to
+  describe their resources so that a generic PRM Configuration DXE driver can prepare those
+  resources for OS runtime.
+
+  Copyright (c) Microsoft Corporation
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef PRM_CONFIG_H_
+#define PRM_CONFIG_H_
+
+#include <PrmContextBuffer.h>
+#include <Uefi.h>
+
+typedef struct _PRM_CONFIG_PROTOCOL PRM_CONFIG_PROTOCOL;
+
+#define PRM_CONFIG_PROTOCOL_SIGNATURE   SIGNATURE_32('P','M','C','P')
+#define PRM_CONFIG_PROTOCOL_VERSION     1
+
+struct _PRM_CONFIG_PROTOCOL
+{
+  PRM_MODULE_CONTEXT_BUFFERS            ModuleContextBuffers;
+};
+
+extern EFI_GUID gPrmConfigProtocolGuid;
+
+#endif