ucsb-seclab/harden-HBFA

PrmPkg: Add initial PrmSampleContextBufferModule
Adds a sample PRM module that demonstrates:
  1. How to write a PRM module
  2. How to use a basic PRM OS services
  3. How to use a basic PRM module configuration library
  4. How to use a context buffer during PRM handler execution

Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

PrmPkg/Samples/PrmSampleContextBufferModule/Include/StaticData.h
@@ -1,0 +1,24 @@
+/** @file
+  PRM Module Static Data
+
+  Defines the structure of the static data buffer for the PRM Sample Context Buffer module.
+
+  Copyright (c) Microsoft Corporation
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef __PRM_STATIC_DATA_SAMPLE_CONTEXT_BUFFER_MODULE_H__
+#define __PRM_STATIC_DATA_SAMPLE_CONTEXT_BUFFER_MODULE_H__
+
+#include <Base.h>
+
+#define   SOME_VALUE_ARRAY_MAX_VALUES   16
+
+typedef struct {
+  BOOLEAN       Policy1Enabled;
+  BOOLEAN       Policy2Enabled;
+  UINT8         SomeValueArray[SOME_VALUE_ARRAY_MAX_VALUES];
+} STATIC_DATA_SAMPLE_CONTEXT_BUFFER_MODULE;
+
+#endif

tianocore/edk2

REF:https://bugzilla.tianocore.org/show_bug.cgi?id=3812
Cc: Andrew Fish <afish@apple.com>
Cc: Kang Gao <kang.gao@intel.com>
Cc: Michael D Kinney <michael.d.kinney@intel.com>
Cc: Michael Kubacki <michael.kubacki@microsoft.com>
Cc: Leif Lindholm <leif@nuviainc.com>
Cc: Benjamin You <benjamin.you@intel.com>
Cc: Liu Yun <yun.y.liu@intel.com>
Cc: Ankit Sinha <ankit.sinha@intel.com>
Cc: Nate DeSimone <nathaniel.l.desimone@intel.com>
Acked-by: Michael D Kinney <michael.d.kinney@intel.com>
Acked-by: Liming Gao <gaoliming@byosoft.com.cn>
Acked-by: Leif Lindholm <quic_llindhol@quicinc.com>
Reviewed-by: Ankit Sinha <ankit.sinha@intel.com>

PrmPkg/Samples/PrmSampleContextBufferModule/Include/StaticData.h
@@ -1,0 +1,24 @@
+/** @file
+  PRM Module Static Data
+
+  Defines the structure of the static data buffer for the PRM Sample Context Buffer module.
+
+  Copyright (c) Microsoft Corporation
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef PRM_STATIC_DATA_SAMPLE_CONTEXT_BUFFER_MODULE_H_
+#define PRM_STATIC_DATA_SAMPLE_CONTEXT_BUFFER_MODULE_H_
+
+#include <Base.h>
+
+#define   SOME_VALUE_ARRAY_MAX_VALUES   16
+
+typedef struct {
+  BOOLEAN       Policy1Enabled;
+  BOOLEAN       Policy2Enabled;
+  UINT8         SomeValueArray[SOME_VALUE_ARRAY_MAX_VALUES];
+} STATIC_DATA_SAMPLE_CONTEXT_BUFFER_MODULE;
+
+#endif