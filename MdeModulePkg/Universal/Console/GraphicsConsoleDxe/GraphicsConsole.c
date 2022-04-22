a7b9c66a40f78760ccd703b51dc03ced7e95e2f0
Gerd Hoffmann <kraxel@redhat.com>
2022-04-22 14:41:18 +0200

    MdeModulePkg/GraphicsConsoleDxe: add modes
    Add modes for a few common display resolutions higher than 800x600,
    specifically 1024x768, 1280x800 and 1920x1080, so ConSplitterDxe has
    more options available.
    
    The mode list is not use as-is, InitializeGraphicsConsoleTextMode() will
    check the list and filter out any modes which don't fit to the screen,
    so this will also work fine for small displays.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Liming Gao <gaoliming@byosoft.com.cn>
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>

--- MdeModulePkg/Universal/Console/GraphicsConsoleDxe/GraphicsConsole.c
+++ MdeModulePkg/Universal/Console/GraphicsConsoleDxe/GraphicsConsole.c
@@ -40,7 +40,10 @@ GRAPHICS_CONSOLE_DEV  mGraphicsConsoleDevTemplate = {
 };
 
 GRAPHICS_CONSOLE_MODE_DATA  mGraphicsConsoleModeData[] = {
-  { 100, 31 },
+  { 100, 31 },  //  800 x 600
+  { 128, 40 },  // 1024 x 768
+  { 160, 42 },  // 1280 x 800
+  { 240, 56 },  // 1920 x 1080
   //
   // New modes can be added here.
   // The last entry is specific for full screen mode.
