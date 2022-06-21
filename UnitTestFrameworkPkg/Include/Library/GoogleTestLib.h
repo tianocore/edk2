28a03d79132e19a23f21d5ac4677d4d5ad880815
Michael D Kinney <michael.d.kinney@intel.com>
2022-06-20 19:52:03 -0700

    UnitTestFrameworkPkg: Add googletest submodule and GoogleTestLib
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4134
    
    Add submodule for googletest and add GoogleTestLib that is
    required for GoogleTest based unit tests. Add GoogleTest
    documentation to Readme.md along with a port of the sample
    unit test to the GoogleTest style.
    
    A few typos in Readme.md are also fixed.
    
    Cc: Michael Kubacki <mikuback@linux.microsoft.com>
    Cc: Sean Brogan <sean.brogan@microsoft.com>
    Cc: Andrew Fish <afish@apple.com>
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>
    Reviewed-by: Michael Kubacki <mikuback@linux.microsoft.com>
    Acked-by: Leif Lindholm <quic_llindhol@quicinc.com>

---
+++ UnitTestFrameworkPkg/Include/Library/GoogleTestLib.h
@@ -0,0 +1,14 @@
+/** @file
+  GoogleTestLib class with APIs from the googletest project
+
+  Copyright (c) 2022, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef GOOGLE_TEST_LIB_H_
+#define GOOGLE_TEST_LIB_H_
+
+#include <gtest/gtest.h>
+
+#endif
