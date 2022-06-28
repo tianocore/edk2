bf8aa8880c98d130b0235c4bb15319c647e944ba
Pierre Gondois <pierre.gondois@arm.com>
2022-06-28 14:29:14 +0200

    ArmPkg/ArmLib: Add ArmHasRngExt()
    Add a ArmHasRngExt() to check for the FEAT_RNG extension.
    Also add a mask for the RNDR bits.
    
    Signed-off-by: Pierre Gondois <pierre.gondois@arm.com>

--- ArmPkg/Library/ArmLib/Arm/ArmV7Lib.c
+++ ArmPkg/Library/ArmLib/Arm/ArmV7Lib.c
@@ -1,7 +1,7 @@
 /** @file
 
   Copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
-  Copyright (c) 2011 - 2014, ARM Limited. All rights reserved.
+  Copyright (c) 2011 - 2022, Arm Limited. All rights reserved.
   Copyright (c) 2021, NUVIA Inc. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -118,4 +118,18 @@ ArmHasCcidx (
 
   Mmfr4 = ArmReadIdMmfr4 ();
   return (((Mmfr4 >> 24) & 0xF) == 1) ? TRUE : FALSE;
+}
+
+/** Check if FEAT_RNG extension is available.
+
+  @retval TRUE if FEAT_RNG extension is available.
+  @retval FALSE otherwise.
+**/
+BOOLEAN
+ArmHasRngExt (
+  VOID
+  )
+{
+  // Not supported.
+  return FALSE;
 }
