a388340d65d0ece6ef6a682230443810be1c17eb
Sami Mujawar <sami.mujawar@arm.com>
2022-07-12 11:19:22 +0100

    ArmVirtPkg: Add NULL instance of ArmCcaInitPeiLib
    Add a NULL instance of ArmCcaInitPeiLib library that guest firmware
    for VMMs that do not implement Arm CCA Realms can use.
    
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Leif Lindholm <leif.lindholm@oss.qualcomm.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>

---
+++ ArmVirtPkg/Library/ArmCcaInitPeiLibNull/ArmCcaInitPeiLibNull.c
@@ -0,0 +1,31 @@
+/** @file
+  Library that implements a NULL implementation of the ArmCcaInitPeiLib.
+
+  Copyright (c) 2022 - 2026, Arm Limited. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+  @par Glossary:
+    - Rsi or RSI   - Realm Service Interface
+    - IPA          - Intermediate Physical Address
+    - RIPAS        - Realm IPA state
+**/
+
+#include <Base.h>
+
+/**
+  Configure the System Memory region as Protected RAM.
+
+  When a VMM creates a Realm, a small amount of DRAM (which contains the
+  firmware image) and the initial content is configured as Protected RAM.
+  The remaining System Memory is in the Protected Empty state. The firmware
+  must then initialise the remaining System Memory as Protected RAM before
+  it can be accessed.
+
+**/
+VOID
+EFIAPI
+ArmCcaConfigureSystemMemory (
+  VOID
+  )
+{
+}
