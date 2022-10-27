e56839cd178ffa71826e1548d8427de7b9c0f2e8
Min M Xu <min.m.xu@intel.com>
2022-10-27 14:00:19 +0800

    OvmfPkg: Implement CcExitLib
    https://bugzilla.tianocore.org/show_bug.cgi?id=4123
    
    The base CcExitLib library provides a default limited interface. As it
    does not provide full support, create an OVMF version of this library to
    begin the process of providing full support of Cc guest (such as SEV-ES,
    TDX) within OVMF.
    
    Cc: Brijesh Singh <brijesh.singh@amd.com>
    Cc: Erdem Aktas <erdemaktas@google.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: James Bottomley <jejb@linux.ibm.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- OvmfPkg/Library/CcExitLib/CcExitLib.c
+++ OvmfPkg/Library/CcExitLib/CcExitLib.c
@@ -1,24 +1,92 @@
 /** @file
-  CcExit Base Support Library.
+  CcExitLib Support Library.
 
   Copyright (C) 2020, Advanced Micro Devices, Inc. All rights reserved.<BR>
-  Copyright (c) 2020 - 2022, Intel Corporation. All rights reserved.<BR>
+  Copyright (C) 2020 - 2022, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
 #include <Base.h>
 #include <Uefi.h>
+#include <Library/BaseMemoryLib.h>
 #include <Library/CcExitLib.h>
+#include <Register/Amd/Msr.h>
+
+/**
+  Check for VMGEXIT error
+
+  Check if the hypervisor has returned an error after completion of the VMGEXIT
+  by examining the SwExitInfo1 field of the GHCB.
+
+  @param[in]  Ghcb       A pointer to the GHCB
+
+  @retval  0             VMGEXIT succeeded.
+  @return                Exception number to be propagated, VMGEXIT processing
+                         did not succeed.
+
+**/
+STATIC
+UINT64
+VmgExitErrorCheck (
+  IN GHCB  *Ghcb
+  )
+{
+  GHCB_EVENT_INJECTION  Event;
+  GHCB_EXIT_INFO        ExitInfo;
+  UINT64                Status;
+
+  ExitInfo.Uint64 = Ghcb->SaveArea.SwExitInfo1;
+  ASSERT (
+    (ExitInfo.Elements.Lower32Bits == 0) ||
+    (ExitInfo.Elements.Lower32Bits == 1)
+    );
+
+  Status = 0;
+  if (ExitInfo.Elements.Lower32Bits == 0) {
+    return Status;
+  }
+
+  if (ExitInfo.Elements.Lower32Bits == 1) {
+    ASSERT (Ghcb->SaveArea.SwExitInfo2 != 0);
+
+    //
+    // Check that the return event is valid
+    //
+    Event.Uint64 = Ghcb->SaveArea.SwExitInfo2;
+    if (Event.Elements.Valid &&
+        (Event.Elements.Type == GHCB_EVENT_INJECTION_TYPE_EXCEPTION))
+    {
+      switch (Event.Elements.Vector) {
+        case GP_EXCEPTION:
+        case UD_EXCEPTION:
+          //
+          // Use returned event as return code
+          //
+          Status = Event.Uint64;
+      }
+    }
+  }
+
+  if (Status == 0) {
+    GHCB_EVENT_INJECTION  GpEvent;
+
+    GpEvent.Uint64          = 0;
+    GpEvent.Elements.Vector = GP_EXCEPTION;
+    GpEvent.Elements.Type   = GHCB_EVENT_INJECTION_TYPE_EXCEPTION;
+    GpEvent.Elements.Valid  = 1;
+
+    Status = GpEvent.Uint64;
+  }
+
+  return Status;
+}
 
 /**
   Perform VMGEXIT.
 
   Sets the necessary fields of the GHCB, invokes the VMGEXIT instruction and
   then handles the return actions.
-
-  The base library function returns an error in the form of a
-  GHCB_EVENT_INJECTION representing a GP_EXCEPTION.
 
   @param[in, out]  Ghcb       A pointer to the GHCB
   @param[in]       ExitCode   VMGEXIT code to be assigned to the SwExitCode
@@ -42,14 +110,24 @@ CcExitLibVmgExit (
   IN     UINT64  ExitInfo2
   )
 {
-  GHCB_EVENT_INJECTION  Event;
+  Ghcb->SaveArea.SwExitCode  = ExitCode;
+  Ghcb->SaveArea.SwExitInfo1 = ExitInfo1;
+  Ghcb->SaveArea.SwExitInfo2 = ExitInfo2;
 
-  Event.Uint64          = 0;
-  Event.Elements.Vector = GP_EXCEPTION;
-  Event.Elements.Type   = GHCB_EVENT_INJECTION_TYPE_EXCEPTION;
-  Event.Elements.Valid  = 1;
+  CcExitLibVmgSetOffsetValid (Ghcb, GhcbSwExitCode);
+  CcExitLibVmgSetOffsetValid (Ghcb, GhcbSwExitInfo1);
+  CcExitLibVmgSetOffsetValid (Ghcb, GhcbSwExitInfo2);
 
-  return Event.Uint64;
+  //
+  // Guest memory is used for the guest-hypervisor communication, so fence
+  // the invocation of the VMGEXIT instruction to ensure GHCB accesses are
+  // synchronized properly.
+  //
+  MemoryFence ();
+  AsmVmgExit ();
+  MemoryFence ();
+
+  return VmgExitErrorCheck (Ghcb);
 }
 
 /**
@@ -70,6 +148,16 @@ CcExitLibVmgInit (
   IN OUT BOOLEAN  *InterruptState
   )
 {
+  //
+  // Be sure that an interrupt can't cause a #VC while the GHCB is
+  // being used.
+  //
+  *InterruptState = GetInterruptState ();
+  if (*InterruptState) {
+    DisableInterrupts ();
+  }
+
+  SetMem (&Ghcb->SaveArea, sizeof (Ghcb->SaveArea), 0);
 }
 
 /**
@@ -90,6 +178,9 @@ CcExitLibVmgDone (
   IN     BOOLEAN  InterruptState
   )
 {
+  if (InterruptState) {
+    EnableInterrupts ();
+  }
 }
 
 /**
@@ -109,6 +200,13 @@ CcExitLibVmgSetOffsetValid (
   IN     GHCB_REGISTER  Offset
   )
 {
+  UINT32  OffsetIndex;
+  UINT32  OffsetBit;
+
+  OffsetIndex = Offset / 8;
+  OffsetBit   = Offset % 8;
+
+  Ghcb->SaveArea.ValidBitmap[OffsetIndex] |= (1 << OffsetBit);
 }
 
 /**
@@ -131,64 +229,11 @@ CcExitLibVmgIsOffsetValid (
   IN GHCB_REGISTER  Offset
   )
 {
-  return FALSE;
-}
+  UINT32  OffsetIndex;
+  UINT32  OffsetBit;
 
-/**
-  Handle a #VC exception.
+  OffsetIndex = Offset / 8;
+  OffsetBit   = Offset % 8;
 
-  Performs the necessary processing to handle a #VC exception.
-
-  The base library function returns an error equal to VC_EXCEPTION,
-  to be propagated to the standard exception handling stack.
-
-  @param[in, out]  ExceptionType  Pointer to an EFI_EXCEPTION_TYPE to be set
-                                  as value to use on error.
-  @param[in, out]  SystemContext  Pointer to EFI_SYSTEM_CONTEXT
-
-  @retval  EFI_SUCCESS            Exception handled
-  @retval  EFI_UNSUPPORTED        #VC not supported, (new) exception value to
-                                  propagate provided
-  @retval  EFI_PROTOCOL_ERROR     #VC handling failed, (new) exception value to
-                                  propagate provided
-
-**/
-EFI_STATUS
-EFIAPI
-CcExitLibHandleVc (
-  IN OUT EFI_EXCEPTION_TYPE  *ExceptionType,
-  IN OUT EFI_SYSTEM_CONTEXT  SystemContext
-  )
-{
-  *ExceptionType = VC_EXCEPTION;
-
-  return EFI_UNSUPPORTED;
-}
-
-/**
-  Handle a #VE exception.
-
-  Performs the necessary processing to handle a #VE exception.
-
-  @param[in, out]  ExceptionType  Pointer to an EFI_EXCEPTION_TYPE to be set
-                                  as value to use on error.
-  @param[in, out]  SystemContext  Pointer to EFI_SYSTEM_CONTEXT
-
-  @retval  EFI_SUCCESS            Exception handled
-  @retval  EFI_UNSUPPORTED        #VE not supported, (new) exception value to
-                                  propagate provided
-  @retval  EFI_PROTOCOL_ERROR     #VE handling failed, (new) exception value to
-                                  propagate provided
-
-**/
-EFI_STATUS
-EFIAPI
-CcExitLibHandleVe (
-  IN OUT EFI_EXCEPTION_TYPE  *ExceptionType,
-  IN OUT EFI_SYSTEM_CONTEXT  SystemContext
-  )
-{
-  *ExceptionType = VE_EXCEPTION;
-
-  return EFI_UNSUPPORTED;
+  return ((Ghcb->SaveArea.ValidBitmap[OffsetIndex] & (1 << OffsetBit)) != 0);
 }
