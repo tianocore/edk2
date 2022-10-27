bcdce90787a4ad5c8e73bab13ea8282fe8478aed
Min M Xu <min.m.xu@intel.com>
2022-10-27 14:56:45 +0800

    UefiCpuPkg: Use CcExitLib instead of VmgExitLib
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=4123
    
    VmgExitLib once was designed to provide interfaces to support #VC handler
    and issue VMGEXIT instruction. After TDVF (enable TDX feature in OVMF) is
    introduced, this library is updated to support #VE as well. Now the name
    of VmgExitLib cannot reflect what the lib does. So VmgExitLib is replaced
    by CcExitLib.
    
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Brijesh Singh <brijesh.singh@amd.com>
    Cc: Erdem Aktas <erdemaktas@google.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: James Bottomley <jejb@linux.ibm.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- UefiCpuPkg/Library/CpuExceptionHandlerLib/SecPeiCpuException.c
+++ UefiCpuPkg/Library/CpuExceptionHandlerLib/SecPeiCpuException.c
@@ -7,7 +7,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
 #include <PiPei.h>
-#include <Library/VmgExitLib.h>
+#include <Library/CcExitLib.h>
 #include "CpuExceptionCommon.h"
 
 CONST UINTN  mDoFarReturnFlag = 0;
@@ -39,7 +39,7 @@ CommonExceptionHandler (
       //   On other       - ExceptionType contains (possibly new) exception
       //                    value
       //
-      Status = VmgExitHandleVc (&ExceptionType, SystemContext);
+      Status = CcExitHandleVc (&ExceptionType, SystemContext);
       if (!EFI_ERROR (Status)) {
         return;
       }
@@ -57,7 +57,7 @@ CommonExceptionHandler (
       //   On other       - ExceptionType contains (possibly new) exception
       //                    value
       //
-      Status = VmTdExitHandleVe (&ExceptionType, SystemContext);
+      Status = CcExitHandleVe (&ExceptionType, SystemContext);
       if (!EFI_ERROR (Status)) {
         return;
       }
