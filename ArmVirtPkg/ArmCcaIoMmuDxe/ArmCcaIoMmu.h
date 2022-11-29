072f4dc78d7ff5affeb2897c42391ca0a6045dd1
Sami Mujawar <sami.mujawar@arm.com>
2022-11-29 15:50:59 +0000

    ArmVirtPkg: IoMMU driver to DMA from Realms
    On Arm CCA systems the access to pages inside the Realm is protected.
    
    However, software executing in a Realm needs to interact with the
    external world. This may be done using para virtualisation of the
    disk, network interfaces, etc. For this to work the buffers in the
    Realm need to be shared with the Host. The sharing and management
    of the Realm buffers is done by the Realm Aperture Management
    Protocol, which invokes the necessary Realm Service Interfaces
    to transition the buffers from Protected IPA to Unprotected IPA.
    
    The ArmCcaIoMmu driver provides the necessary hooks so that DMA
    operations can be performed by bouncing buffers using pages shared
    with the Host. It uses the Realm Aperture Management protocol to
    share the buffers with the Host.
    
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Leif Lindholm <leif.lindholm@oss.qualcomm.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>

---
+++ ArmVirtPkg/ArmCcaIoMmuDxe/ArmCcaIoMmu.h
@@ -0,0 +1,64 @@
+/** @file
+  The Arm CCA IoMMU protocol provides support to allocate, free, map and umap
+  DMA buffers for bus masters (e.g PciHostBridge).
+  When the execution context is a Realm, the DMA operations must be performed
+  on buffers that are shared with the Host.
+  Hence the Realm Aperture Management Protocol (RAMP) is used to manage the
+  sharing of the DMA buffers or in some cases to bounce the buffers.
+
+  Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2017, AMD Inc. All rights reserved.<BR>
+  (C) Copyright 2017 Hewlett Packard Enterprise Development LP<BR>
+  Copyright (c) 2022 - 2023, Arm Limited. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#pragma once
+
+#include <Protocol/IoMmu.h>
+
+#include <Library/BaseLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Protocol/RealmApertureManagementProtocol.h>
+
+/**
+  A macro defning the signature for the MAP_INFO structure.
+*/
+#define MAP_INFO_SIG  SIGNATURE_64 ('M', 'A', 'P', '_', 'I', 'N', 'F', 'O')
+
+/** A structure describing the mapping for the buffers shared with the host.
+*/
+typedef struct {
+  /// Signature.
+  UINT64                   Signature;
+  /// Linked List node entry.
+  LIST_ENTRY               Link;
+  /// IoMMU operation.
+  EDKII_IOMMU_OPERATION    Operation;
+  /// Number of bytes.
+  UINTN                    NumberOfBytes;
+  /// Number of pages.
+  UINTN                    NumberOfPages;
+  /// Address of the Host buffer.
+  VOID                     *HostAddress;
+
+  /// Address for the Bounce Buffer.
+  EFI_PHYSICAL_ADDRESS     BbAddress;
+  /// Handle to the Aperture.
+  EFI_HANDLE               ApertureRef;
+} MAP_INFO;
+
+/**
+  Install IOMMU protocol to provide the DMA support for PciHostBridge and
+  RAMP.
+
+**/
+EFI_STATUS
+EFIAPI
+ArmCcaInstallIoMmuProtocol (
+  VOID
+  );
