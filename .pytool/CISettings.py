3c2d9c06856606cbcbb2bd5b81f410999eafcd00
Qi Zhang <qi1.zhang@intel.com>
2022-12-07 08:15:13 +0800

    .pytool/CISettings.py: add libspdm submodule.
    Signed-off-by: Qi Zhang <qi1.zhang@intel.com>

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -202,6 +202,8 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
             "BaseTools/Source/C/BrotliCompress/brotli", False))
         rs.append(RequiredSubmodule(
             "RedfishPkg/Library/JsonLib/jansson", False))
+        rs.append(RequiredSubmodule(
+            "SecurityPkg/DeviceSecurity/SpdmLib/libspdm", False))
         return rs
 
     def GetName(self):
