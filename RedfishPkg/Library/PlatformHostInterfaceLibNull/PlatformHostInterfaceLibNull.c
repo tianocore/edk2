4967cb06360f05ef510a0ae588aed6d431c6347a
Abner Chang <abner.chang@amd.com>
2022-12-19 21:18:00 +0800

    RedfishPkg/RedfishHostInterface: Platform Redfish HI notification
    For some use cases, Redfish host interface table relies on
    the certain EFI protocols installation at the driver connection.
    Redfish host interface DXE driver is not able to build the
    SMBIOS type 42h record at driver entry point. This patch adds
    the mechanism in Redfish host interface DXE driver to listen
    to EFI protocol installed by platform library that indicates
    the necessary information is ready for building SMBIOS 42h
    record.
    
    Signed-off-by: Abner Chang <abner.chang@amd.com>
    Cc: Nickle Wang <nicklew@nvidia.com>
    Cc: Igor Kulchytskyy <igork@ami.com>
    Reviewed-by: Nickle Wang <nicklew@nvidia.com>

--- RedfishPkg/Library/PlatformHostInterfaceLibNull/PlatformHostInterfaceLibNull.c
+++ RedfishPkg/Library/PlatformHostInterfaceLibNull/PlatformHostInterfaceLibNull.c
@@ -2,6 +2,7 @@
   NULL instace of RedfishPlatformHostInterfaceLib
 
   (C) Copyright 2020 Hewlett Packard Enterprise Development LP<BR>
+  Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -23,7 +24,7 @@
 **/
 EFI_STATUS
 RedfishPlatformHostInterfaceDeviceDescriptor (
-  IN UINT8                    *DeviceType,
+  OUT UINT8                   *DeviceType,
   OUT REDFISH_INTERFACE_DATA  **DeviceDescriptor
   )
 {
@@ -50,4 +51,27 @@ RedfishPlatformHostInterfaceProtocolData (
   )
 {
   return EFI_NOT_FOUND;
+}
+
+/**
+  Get the EFI protocol GUID installed by platform library which
+  indicates the necessary information is ready for building
+  SMBIOS 42h record.
+
+  @param[out] InformationReadinessGuid  Pointer to retrive the protocol
+                                        GUID.
+
+  @retval EFI_SUCCESS          Notification is required for building up
+                               SMBIOS type 42h record.
+  @retval EFI_UNSUPPORTED      Notification is not required for building up
+                               SMBIOS type 42h record.
+  @retval EFI_ALREADY_STARTED  Platform host information is already ready.
+  @retval Others               Other errors.
+**/
+EFI_STATUS
+RedfishPlatformHostInterfaceNotification (
+  OUT EFI_GUID  **InformationReadinessGuid
+  )
+{
+  return EFI_UNSUPPORTED;
 }
