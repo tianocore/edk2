f0e3965fe097f3d09d562897ebd57dd27001be02
Chris Johnson <chris.n.johnson@intel.com>
2022-12-21 11:56:07 -0800

    UnitTestFrameworkPkg: Add subhook submodule required for gmock
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4389
    
    Add subhook submodule that is required to hook internal functions
    when using gmock.
    
        https://github.com/Zeex/subhook
    
    Add SubhookLib library class and SubhookLib library instance.
    Include the SUBHOOK_STATIC define in the SubhookLib INF file so
    it builds as a static library. Also include the SUBHOOK_STATIC
    define in SubhookLib.h so all modules using SubhookLib properly
    link SubhookLib as a static library.
    
    Cc: Andrew Fish <afish@apple.com>
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Michael Kubacki <mikuback@linux.microsoft.com>
    Cc: Sean Brogan <sean.brogan@microsoft.com>
    Signed-off-by: Chris Johnson <chris.n.johnson@intel.com>
    Reviewed-by: Leif Lindholm <quic_llindhol@quicinc.com>
    Reviewed-by: Michael Kubacki <michael.kubacki@microsoft.com>
    Reviewed-by: Oliver Smith-Denny <osde@linux.microsoft.com>
    Reviewed-by: Michael D Kinney <michael.d.kinney@intel.com>

---
+++ UnitTestFrameworkPkg/Include/Library/SubhookLib.h
@@ -0,0 +1,15 @@
+/** @file
+  SubhookLib class with APIs from the subhook project
+
+  Copyright (c) 2022, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef SUBHOOK_LIB_H_
+#define SUBHOOK_LIB_H_
+
+#define SUBHOOK_STATIC
+#include <subhook.h>
+
+#endif
