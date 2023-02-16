caec7802d27323be779f05c86dcc985b465f2c28
Wu, Jiaxin <jiaxin.wu@intel.com>
2023-02-16 14:16:33 +0800

    OvmfPkg/SmmCpuFeaturesLib: Check SmBase relocation supported or not
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4337
    
    This patch is to check SmBase relocation supported or not.
    If gSmmBaseHobGuid found, means SmBase info has been relocated
    and recorded in the SmBase array. ASSERT it's not supported in OVMF.
    
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Zeng Star <star.zeng@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Rahul Kumar <rahul1.kumar@intel.com>
    Signed-off-by: Jiaxin Wu <jiaxin.wu@intel.com>
    Reviewed-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Ray Ni <ray.ni@intel.com>

--- OvmfPkg/Library/SmmCpuFeaturesLib/SmmCpuFeaturesLib.c
+++ OvmfPkg/Library/SmmCpuFeaturesLib/SmmCpuFeaturesLib.c
@@ -1,7 +1,7 @@
 /** @file
   The CPU specific programming for PiSmmCpuDxeSmm module.
 
-  Copyright (c) 2010 - 2015, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2010 - 2023, Intel Corporation. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
@@ -17,10 +17,12 @@
 #include <Library/SmmCpuFeaturesLib.h>
 #include <Library/SmmServicesTableLib.h>
 #include <Library/UefiBootServicesTableLib.h>
+#include <Library/HobLib.h>
 #include <Pcd/CpuHotEjectData.h>
 #include <PiSmm.h>
 #include <Register/Intel/SmramSaveStateMap.h>
 #include <Register/QemuSmramSaveStateMap.h>
+#include <Guid/SmmBaseHob.h>
 
 //
 // EFER register LMA bit
@@ -43,6 +45,12 @@ SmmCpuFeaturesLibConstructor (
   IN EFI_SYSTEM_TABLE  *SystemTable
   )
 {
+  //
+  // If gSmmBaseHobGuid found, means SmBase info has been relocated and recorded
+  // in the SmBase array. ASSERT it's not supported in OVMF.
+  //
+  ASSERT (GetFirstGuidHob (&gSmmBaseHobGuid) == NULL);
+
   //
   // No need to program SMRRs on our virtual platform.
   //
