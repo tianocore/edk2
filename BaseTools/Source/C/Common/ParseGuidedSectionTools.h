8d90827cfce78aaf1b261e3bf6aa0137210e1124
Rebecca Cran <rebecca@bsdio.com>
2023-02-19 23:21:43 -0700

    BaseTools: Source/C/Common: Fix doc block locations and convert to Doxygen
    Move the documentation blocks from between the parameter list and function
    body to above the function.
    
    Convert all the documentation blocks to Doxygen format.
    
    Signed-off-by: Rebecca Cran <rebecca@bsdio.com>

--- BaseTools/Source/C/Common/ParseGuidedSectionTools.h
+++ BaseTools/Source/C/Common/ParseGuidedSectionTools.h
@@ -15,106 +15,73 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 // Functions declarations
 //
 
+/**
+  This function parses the tools_def.txt file.  It returns a
+  EFI_HANDLE object which can be used for the other library
+  functions and should be passed to FreeParsedToolsDefHandle
+  to free resources when the tools_def.txt information is no
+  longer needed.
+
+  @param InputFile     Path name of file to read
+
+  @retval NULL if error parsing
+  @retval A non-NULL EFI_HANDLE otherwise
+**/
 EFI_HANDLE
 ParseGuidedSectionToolsFile (
   IN CHAR8    *InputFile
   )
 ;
+
 /**
-
-Routine Description:
-
   This function parses the tools_def.txt file.  It returns a
   EFI_HANDLE object which can be used for the other library
   functions and should be passed to FreeParsedToolsDefHandle
   to free resources when the tools_def.txt information is no
   longer needed.
 
-Arguments:
+  @param InputFile     Memory file image.
 
-  InputFile     Path name of file to read
-
-Returns:
-
-  NULL if error parsing
-  A non-NULL EFI_HANDLE otherwise
-
+  @retval NULL if error parsing
+  @retval A non-NULL EFI_HANDLE otherwise
 **/
-
-
 EFI_HANDLE
 ParseGuidedSectionToolsMemoryFile (
   IN EFI_HANDLE    InputFile
   )
 ;
+
 /**
+  This function looks up the appropriate tool to use for extracting
+  a GUID defined FV section.
 
-Routine Description:
+  @param ParsedGuidedSectionToolsHandle    A parsed GUID section tools handle.
+  @param SectionGuid                       The GUID for the section.
 
-  This function parses the tools_def.txt file.  It returns a
-  EFI_HANDLE object which can be used for the other library
-  functions and should be passed to FreeParsedToolsDefHandle
-  to free resources when the tools_def.txt information is no
-  longer needed.
-
-Arguments:
-
-  InputFile     Memory file image.
-
-Returns:
-
-  NULL if error parsing
-  A non-NULL EFI_HANDLE otherwise
-
+  @retval NULL     if no tool is found or there is another error
+  @retval Non-NULL The tool to use to access the section contents.  (The caller
+             must free the memory associated with this string.)
 **/
-
 CHAR8*
 LookupGuidedSectionToolPath (
   IN EFI_HANDLE ParsedGuidedSectionToolsHandle,
   IN EFI_GUID   *SectionGuid
   )
 ;
+
 /**
+  Frees resources that were allocated by ParseGuidedSectionToolsFile.
+  After freeing these resources, the information that was parsed
+  is no longer accessible.
 
-Routine Description:
+  @param ParsedToolDefHandle   Handle returned from ParseGuidedSectionToolsFile
 
-  This function looks up the appropriate tool to use for extracting
-  a GUID defined FV section.
-
-Arguments:
-
-  ParsedGuidedSectionToolsHandle    A parsed GUID section tools handle.
-  SectionGuid                       The GUID for the section.
-
-Returns:
-
-  NULL     - if no tool is found or there is another error
-  Non-NULL - The tool to use to access the section contents.  (The caller
-             must free the memory associated with this string.)
-
+  @return EFI_STATUS
 **/
-
 EFI_STATUS
 FreeParsedGuidedSectionToolsHandle (
   IN EFI_HANDLE ParsedGuidedSectionToolsHandle
   )
 ;
-/**
-
-Routine Description:
-
-  Frees resources that were allocated by ParseGuidedSectionToolsFile.
-  After freeing these resources, the information that was parsed
-  is no longer accessible.
-
-Arguments:
-
-  ParsedToolDefHandle   Handle returned from ParseGuidedSectionToolsFile
-
-Returns:
-
-  EFI_STATUS
-
-**/
 
 #endif
