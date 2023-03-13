b53fca23d339cdc043cfb5ed473edbf422e09997
Ard Biesheuvel <ardb@kernel.org>
2023-03-13 18:17:05 +0100

    MdePkg/PeCoffLib: Capture DLL characteristics fields in image context
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4405
    
    When loading a PE/COFF image, capture the DLL characteristics fields of
    the header into our image context structure so we can refer to them when
    mapping the image.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Reviewed-by: Leif Lindholm <quic_llindhol@quicinc.com>
    Reviewed-by: Oliver Smith-Denny <osde@linux.microsoft.com>
    Reviewed-by: Michael Kubacki <michael.kubacki@microsoft.com>
    Reviewed-by: Liming Gao <gaoliming@byosoft.com.cn>

--- MdePkg/Include/IndustryStandard/PeImage.h
+++ MdePkg/Include/IndustryStandard/PeImage.h
@@ -625,7 +625,8 @@ typedef struct {
   UINT32    FileOffset;  ///< The file pointer to the debug data.
 } EFI_IMAGE_DEBUG_DIRECTORY_ENTRY;
 
-#define EFI_IMAGE_DEBUG_TYPE_CODEVIEW  2    ///< The Visual C++ debug information.
+#define EFI_IMAGE_DEBUG_TYPE_CODEVIEW               2    ///< The Visual C++ debug information.
+#define EFI_IMAGE_DEBUG_TYPE_EX_DLLCHARACTERISTICS  20
 
 ///
 /// Debug Data Structure defined in Microsoft C++.
@@ -668,6 +669,16 @@ typedef struct {
   //  Filename of .DLL (Mach-O with debug info) goes here
   //
 } EFI_IMAGE_DEBUG_CODEVIEW_MTOC_ENTRY;
+
+///
+/// Extended DLL Characteristics
+///
+#define EFI_IMAGE_DLLCHARACTERISTICS_EX_CET_COMPAT          0x0001
+#define EFI_IMAGE_DLLCHARACTERISTICS_EX_FORWARD_CFI_COMPAT  0x0040
+
+typedef struct {
+  UINT32    DllCharacteristicsEx;
+} EFI_IMAGE_DEBUG_EX_DLLCHARACTERISTICS_ENTRY;
 
 ///
 /// Resource format.
