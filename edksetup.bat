4df4c2771b9c63905f1928644dd64c1778b339cd
Rebecca Cran <rebecca@bsdio.com>
2023-03-21 16:45:58 -0600

    edksetup.bat: Remove VS2008-VS2013 remnants
    Since Visual Studio versions older than VS2015 are no longer supported,
    remove mention of them from edksetup.bat.
    
    Signed-off-by: Rebecca Cran <rebecca@bsdio.com>
    Reviewed-by: Oliver Smith-Denny <osd@smith-denny.com>
    Reviewed-by: Liming Gao <gaoliming@byosoft.com.cn>
    Reviewed-by: Leif Lindholm <quic_llindhol@quicinc.com>
    Reviewed-by: Michael D Kinney <michael.d.kinney@intel.com>
    Tested-by: Michael D Kinney <michael.d.kinney@intel.com>

--- edksetup.bat
+++ edksetup.bat
@@ -148,19 +148,15 @@ if /I "%1"=="ForceRebuild" shift
 if /I "%1"=="VS2019" shift
 if /I "%1"=="VS2017" shift
 if /I "%1"=="VS2015" shift
-if /I "%1"=="VS2013" shift
-if /I "%1"=="VS2012" shift
 if "%1"=="" goto end
 
 :Usage
   @echo.
-  @echo  Usage: "%0 [-h | -help | --help | /h | /help | /?] [Reconfig] [Rebuild] [ForceRebuild] [VS2019] [VS2017] [VS2015] [VS2013] [VS2012]"
+  @echo  Usage: "%0 [-h | -help | --help | /h | /help | /?] [Reconfig] [Rebuild] [ForceRebuild] [VS2019] [VS2017] [VS2015]"
   @echo.
   @echo         Reconfig       Reinstall target.txt, tools_def.txt and build_rule.txt.
   @echo         Rebuild        Perform incremental rebuild of BaseTools binaries.
   @echo         ForceRebuild   Force a full rebuild of BaseTools binaries.
-  @echo         VS2012         Set the env for VS2012 build.
-  @echo         VS2013         Set the env for VS2013 build.
   @echo         VS2015         Set the env for VS2015 build.
   @echo         VS2017         Set the env for VS2017 build.
   @echo         VS2019         Set the env for VS2019 build.
