2a28419aaa2042a608f0f7a16ea32f53a79880d3
Nickle Wang <nicklew@nvidia.com>
2023-04-21 14:48:30 +0800

    MdeModulePkg/RegularExpressionDxe: Fix Arm build error
    Arm CI build error:
    - ArmPkg/Library/CompilerIntrinsicsLib/memset.c:39:1: warning: type of
    ‘memset’ does not match original declaration [-Wlto-type-mismatch]
    MdeModulePkg/Universal/RegularExpressionDxe/OnigurumaUefiPort.c:123:1:
    note: type ‘char’ should match type ‘int’
    - multiple definition of `memcpy'; OnigurumaUefiPort.obj (symbol from
    plugin):(.text+0x0): first defined here
    
    Fix:
    - Update memset() implementation to match memset() definition in
    ArmPkg/Library/CompilerIntrinsicsLib.
    - memcpy() is supported by ArmPkg/Library/CompilerIntrinsicsLib. Exclude
    it in OnigurumaUefiPort.c.
    
    Signed-off-by: Nickle Wang <nicklew@nvidia.com>
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Nick Ramirez <nramirez@nvidia.com>
    Reviewed-by: Liming Gao <gaoliming@byosoft.com.cn>

--- MdeModulePkg/Universal/RegularExpressionDxe/OnigurumaUefiPort.c
+++ MdeModulePkg/Universal/RegularExpressionDxe/OnigurumaUefiPort.c
@@ -4,6 +4,7 @@
 
   (C) Copyright 2014-2021 Hewlett Packard Enterprise Development LP<BR>
   Copyright (c) 2020, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
@@ -109,6 +110,7 @@ realloc (
   return NULL;
 }
 
+#if !defined (MDE_CPU_ARM)
 void *
 memcpy (
   void          *dest,
@@ -119,14 +121,16 @@ memcpy (
   return CopyMem (dest, src, (UINTN)count);
 }
 
+#endif
+
 void *
 memset (
   void          *dest,
-  char          ch,
+  int           ch,
   unsigned int  count
   )
 {
-  return SetMem (dest, count, ch);
+  return SetMem (dest, (UINTN)count, (UINT8)ch);
 }
 
 void
