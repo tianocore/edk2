ab58e9b6c5ded534063ab036b5c4143fb5bf60cd
Rebecca Cran <rebecca@bsdio.com>
2023-04-24 11:10:54 -0600

    BaseTools: Update toolsetup.bat and Tests/PythonTest.py to check ver
    Update toolsetup.bat and Tests/PythonTest.py to check if we're running a
    version of Python that's compatible with BaseTools and the Pip
    BaseTools.
    
    BaseTools uses syntax from Python 3.5 or newer, so set that as the minimum
    version EDK2 requires.
    
    Signed-off-by: Rebecca Cran <rebecca@bsdio.com>
    Reviewed-by: Liming Gao <gaoliming@byosoft.com.cn>

--- BaseTools/Tests/PythonTest.py
+++ BaseTools/Tests/PythonTest.py
@@ -1,9 +1,26 @@
 ## @file
-# Test whether PYTHON_COMMAND is available
+# Test whether PYTHON_COMMAND is available and the
+# minimum Python version is installed.
 #
 # Copyright (c) 2013 - 2018, Intel Corporation. All rights reserved.<BR>
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 #
 
+import sys
+
 if __name__ == '__main__':
-    print('TRUE')
+    # Check if the major and minor versions required were specified.
+    if len(sys.argv) >= 3:
+        req_major_version = int(sys.argv[1])
+        req_minor_version = int(sys.argv[2])
+    else:
+        # If the minimum version wasn't specified on the command line,
+        # default to 3.5
+        req_major_version = 3
+        req_minor_version = 5
+
+    if sys.version_info.major == req_major_version and \
+       sys.version_info.minor >= req_minor_version:
+        sys.exit(0)
+    else:
+        sys.exit(1)
