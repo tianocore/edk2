bc9a2cb8b83321589ce0c4bc706f337bf7ffe778
Liu, Zhiguang <Zhiguang.Liu@intel.com>
2023-05-08 16:15:02 +0800

    UefiCpuPkg/ResetVector: Combine PageTables1G.asm and PageTables2M.asm
    Combine PageTables1G.asm and PageTables2M.asm to reuse code.
    
    Cc: Eric Dong <eric.dong@intel.com>
    Reviewed-by: Ray Ni <ray.ni@intel.com>
    Cc: Rahul Kumar <rahul1.kumar@intel.com>
    Tested-by: Gerd Hoffmann <kraxel@redhat.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Debkumar De <debkumar.de@intel.com>
    Cc: Catharine West <catharine.west@intel.com>
    Signed-off-by: Zhiguang Liu <zhiguang.liu@intel.com>

--- UefiCpuPkg/ResetVector/Vtf0/X64/PageTables.asm
+++ UefiCpuPkg/ResetVector/Vtf0/X64/PageTables.asm
@@ -1,8 +1,10 @@
 ;------------------------------------------------------------------------------
 ; @file
-; Emits Page Tables for 1:1 mapping of the addresses 0 - 0x100000000 (4GB)
+; Emits Page Tables for 1:1 mapping.
+; If using 1G page table, map addresses 0 - 0x8000000000 (512GB),
+; else, map addresses 0 - 0x100000000 (4GB)
 ;
-; Copyright (c) 2008 - 2023, Intel Corporation. All rights reserved.<BR>
+; Copyright (c) 2021 - 2023, Intel Corporation. All rights reserved.<BR>
 ; SPDX-License-Identifier: BSD-2-Clause-Patent
 ;
 ;------------------------------------------------------------------------------
@@ -12,25 +14,32 @@ BITS    64
 %define ALIGN_TOP_TO_4K_FOR_PAGING
 
 ;
-; Page table big leaf entry attribute:
-; PDPTE 1GB entry or PDE 2MB entry
-;
-%define PAGE_BLE_ATTR   (PAGE_SIZE + \
-                          PAGE_ACCESSED + \
-                          PAGE_DIRTY + \
-                          PAGE_READ_WRITE + \
-                          PAGE_PRESENT)
-
-;
 ; Page table non-leaf entry attribute
 ;
 %define PAGE_NLE_ATTR (PAGE_ACCESSED + \
                         PAGE_READ_WRITE + \
                         PAGE_PRESENT)
 
+;
+; Page table big leaf entry attribute:
+; PDPTE 1GB entry or PDE 2MB entry
+;
+%define PAGE_BLE_ATTR (PAGE_ACCESSED + \
+                        PAGE_READ_WRITE + \
+                        PAGE_DIRTY + \
+                        PAGE_PRESENT + \
+                        PAGE_SIZE)
+
+;
+; Page table non-leaf entry
+;
 %define PAGE_NLE(address) (ADDR_OF(address) + \
                     PAGE_NLE_ATTR)
+
+%define PAGE_PDPTE_1GB(x) ((x << 30) + PAGE_BLE_ATTR)
 %define PAGE_PDE_2MB(x) ((x << 21) + PAGE_BLE_ATTR)
+
+ALIGN 16
 
 Pml4:
     ;
@@ -39,6 +48,17 @@ Pml4:
     DQ      PAGE_NLE(Pdp)
     TIMES   0x1000 - ($ - Pml4) DB 0
 
+%ifdef PAGE_TABLE_1G
+Pdp:
+    ;
+    ; Page-directory pointer table (512 * 1GB entries => 512GB)
+    ;
+    %assign i 0
+    %rep      512
+        DQ    PAGE_PDPTE_1GB(i)
+        %assign i i+1
+    %endrep
+%else
 Pdp:
     ;
     ; Page-directory pointer table (4 * 1GB entries => 4GB)
@@ -54,10 +74,10 @@ Pd:
     ; Page-Directory (2048 * 2MB entries => 4GB)
     ; Four pages below, each is pointed by one entry in Pdp.
     ;
-%assign i 0
-%rep    0x800
-    DQ      PAGE_PDE_2MB(i)
-    %assign i i+1
-%endrep
-
+    %assign i 0
+    %rep    0x800
+        DQ      PAGE_PDE_2MB(i)
+        %assign i i+1
+    %endrep
+%endif
 EndOfPageTables:
