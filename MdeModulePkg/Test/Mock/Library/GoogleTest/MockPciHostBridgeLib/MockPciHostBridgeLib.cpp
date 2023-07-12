e90f7df80a15ba0acecc6545d236cd7ba0b1a896
Vivian Nowka-Keane <vnowkakeane@microsoft.com>
2023-07-12 15:32:29 -0700

    MdeModeulePkg: Apply uncrustify formatting to relevant file
    Apply uncrustify formatting to GoogleTest cpp file.
    
    Signed-off-by: Vivian Nowka-Keane <vnowkakeane@microsoft.com>

--- MdeModulePkg/Test/Mock/Library/GoogleTest/MockPciHostBridgeLib/MockPciHostBridgeLib.cpp
+++ MdeModulePkg/Test/Mock/Library/GoogleTest/MockPciHostBridgeLib/MockPciHostBridgeLib.cpp
@@ -7,8 +7,8 @@
 
 #include <GoogleTest/Library/MockPciHostBridgeLib.h>
 
-MOCK_INTERFACE_DEFINITION(MockPciHostBridgeLib);
+MOCK_INTERFACE_DEFINITION (MockPciHostBridgeLib);
 
-MOCK_FUNCTION_DEFINITION(MockPciHostBridgeLib, PciHostBridgeGetRootBridges, 1, EFIAPI);
-MOCK_FUNCTION_DEFINITION(MockPciHostBridgeLib, PciHostBridgeFreeRootBridges, 2, EFIAPI);
-MOCK_FUNCTION_DEFINITION(MockPciHostBridgeLib, PciHostBridgeResourceConflict, 2, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockPciHostBridgeLib, PciHostBridgeGetRootBridges, 1, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockPciHostBridgeLib, PciHostBridgeFreeRootBridges, 2, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockPciHostBridgeLib, PciHostBridgeResourceConflict, 2, EFIAPI);
