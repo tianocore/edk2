6f9f7fe7530773eef0aa586f1b89e9825247eb1e
Nickle Wang <nicklew@nvidia.com>
2023-07-21 21:43:17 +0800

    RedfishPkg/RedfishCrtLib: multiple definitions of strncpy.
    There are two definitions for strncpy() function in RedfishCrtLib.h
    
    Signed-off-by: Nickle Wang <nicklew@nvidia.com>
    Cc: Abner Chang <abner.chang@amd.com>
    Cc: Igor Kulchytskyy <igork@ami.com>
    Cc: Nick Ramirez <nramirez@nvidia.com>

--- RedfishPkg/Include/Library/RedfishCrtLib.h
+++ RedfishPkg/Include/Library/RedfishCrtLib.h
@@ -3,6 +3,7 @@
 
   Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
   (C) Copyright 2021 Hewlett Packard Enterprise Development LP<BR>
+  Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
     SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -304,8 +305,7 @@ char           *
 strncpy    (
   char *,
   size_t,
-  const char *,
-  size_t
+  const char *
   );
 
 int
