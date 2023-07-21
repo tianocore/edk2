1b1509abee839b74d3232bbd6a256a1bdc230925
Nickle Wang <nicklew@nvidia.com>
2023-07-21 21:43:17 +0800

    RedfishPkg/RedfishCrtLib: remove multiple definitions.
    There are two definitions for below functions in RedfishCrtLib.h. Create
    this change to remote duplicated functions.
    Function list: strcmp(), strncmp(), strncpy(), strcpy(), strcat(),
    strlen(), strchr(), strcasecmp(), strstr(), memcmp(), memset(),
    memcpy(), memchr(), memcmp() and memmove().
    
    Signed-off-by: Nickle Wang <nicklew@nvidia.com>
    Cc: Abner Chang <abner.chang@amd.com>
    Cc: Igor Kulchytskyy <igork@ami.com>
    Cc: Nick Ramirez <nramirez@nvidia.com>
    Cc: Mike Maslenkin <mike.maslenkin@gmail.com>
    Reviewed-by: Abner Chang <abner.chang@amd.com>
    Acked-by: Mike Maslenkin <mike.maslenkin@gmail.com>

--- RedfishPkg/Include/Library/RedfishCrtLib.h
+++ RedfishPkg/Include/Library/RedfishCrtLib.h
@@ -172,20 +172,6 @@ free        (
   void *
   );
 
-void           *
-memset     (
-  void *,
-  int,
-  size_t
-  );
-
-int
-memcmp      (
-  const void *,
-  const void *,
-  size_t
-  );
-
 int
 isdigit     (
   int
@@ -216,100 +202,15 @@ isalnum     (
   int
   );
 
-void           *
-memcpy     (
-  void *,
-  const void *,
-  size_t
-  );
-
-void           *
-memset     (
-  void *,
-  int,
-  size_t
-  );
-
-void           *
-memchr     (
-  const void *,
-  int,
-  size_t
-  );
-
-int
-memcmp      (
-  const void *,
-  const void *,
-  size_t
-  );
-
-void           *
-memmove    (
-  void *,
-  const void *,
-  size_t
-  );
-
-int
-strcmp      (
-  const char *,
-  const char *
-  );
-
 int
 strncmp     (
   const char *,
   const char *,
   size_t
-  );
-
-char           *
-strcpy     (
-  char *,
-  const char *
-  );
-
-size_t
-strlen      (
-  const char *
-  );
-
-char           *
-strcat     (
-  char *,
-  const char *
-  );
-
-char           *
-strchr     (
-  const char *,
-  int
-  );
-
-int
-strcasecmp  (
-  const char *,
-  const char *
   );
 
 int
 strncasecmp (
-  const char *,
-  const char *,
-  size_t
-  );
-
-char           *
-strncpy    (
-  char *,
-  size_t,
-  const char *,
-  size_t
-  );
-
-int
-strncmp     (
   const char *,
   const char *,
   size_t
@@ -326,12 +227,6 @@ strtoul     (
   const char *,
   char **,
   int
-  );
-
-char *
-strstr      (
-  const char  *s1,
-  const char  *s2
   );
 
 long
