536abfa795ad3a39df4cb6b0dac17c42e337709f
Matt DeVillier <matt.devillier@gmail.com>
2023-07-31 11:40:32 -0500

    MdeModulePkg/Include/UniversalPayload: Add ClockRate field to UNIVERSAL_PAYLOAD_SERIAL_PORT_INFO
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Change-Id: I9bcaf03ab63f6a45d2cf25a580f7a2eba388cbbd

--- MdeModulePkg/Include/UniversalPayload/SerialPortInfo.h
+++ MdeModulePkg/Include/UniversalPayload/SerialPortInfo.h
@@ -19,6 +19,7 @@ typedef struct {
   BOOLEAN                             UseMmio;
   UINT8                               RegisterStride;
   UINT32                              BaudRate;
+  UINT32                              ClockRate;
   EFI_PHYSICAL_ADDRESS                RegisterBase;
 } UNIVERSAL_PAYLOAD_SERIAL_PORT_INFO;
 #pragma pack()
