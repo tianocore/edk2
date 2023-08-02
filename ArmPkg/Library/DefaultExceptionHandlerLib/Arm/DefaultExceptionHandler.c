ef051451367fa5455d75b38c407ca352a43b4849
Oliver Smith-Denny <osde@linux.microsoft.com>
2023-08-02 11:59:28 -0700

    ArmPkg: DefaultExceptionHandlerLib: Do Not Allocate Memory
    If gST->ConOut is available when Arm's DefaultExceptionHandler is
    running, AsciiPrint will get called to attempt to print to ConOut, in
    addition to the serial output.
    
    AsciiPrint calls AsciiInternalPrint in UefiLibPrint.c which in turn
    calls AllocatePool to allocate a buffer to convert the Ascii input
    string to a Unicode string to pass to ConOut->OutputString.
    
    Per the comment on DefaultExceptionHandler, we should not be allocating
    memory in the exception handler, as this can cause the exception handler
    to fail if we had a memory exception or the system state is such that we
    cannot allocate memory.
    
    It has been observed on ArmVirtQemu that exceptions generated in the
    memory handling code will fail to output the stack dump and CPU state
    that is critical to debugging because the AllocatePool will fail.
    
    This patch fixes the Arm and AARCH64 DefaultExceptionHandlers to not
    allocate memory when ConOut is available and instead use stack memory to
    convert the Ascii string needed for SerialPortWrite to the Unicode
    string needed for ConOut->OutputString. Correspondingly, ArmVirtQemu can
    now output the stack dump and CPU state when hitting an exception in
    memory code.
    
    Signed-off-by: Oliver Smith-Denny <osde@linux.microsoft.com>

--- ArmPkg/Library/DefaultExceptionHandlerLib/Arm/DefaultExceptionHandler.c
+++ ArmPkg/Library/DefaultExceptionHandlerLib/Arm/DefaultExceptionHandler.c
@@ -24,6 +24,12 @@
 #include <Library/DefaultExceptionHandlerLib.h>
 
 //
+// Maximum number of characters to print to serial (UINT8s) and to console if
+// available (as UINT16s)
+//
+#define MAX_PRINT_CHARS  100
+
+//
 // The number of elements in a CHAR8 array, including the terminating NUL, that
 // is meant to hold the string rendering of the CPSR.
 //
@@ -198,7 +204,8 @@ DefaultExceptionHandler (
   IN OUT EFI_SYSTEM_CONTEXT  SystemContext
   )
 {
-  CHAR8    Buffer[100];
+  CHAR8    Buffer[MAX_PRINT_CHARS];
+  CHAR16   UnicodeBuffer[MAX_PRINT_CHARS];
   UINTN    CharCount;
   UINT32   DfsrStatus;
   UINT32   IfsrStatus;
@@ -216,9 +223,10 @@ DefaultExceptionHandler (
                 SystemContext.SystemContextArm->CPSR
                 );
   SerialPortWrite ((UINT8 *)Buffer, CharCount);
-  if (gST->ConOut != NULL) {
-    AsciiPrint (Buffer);
-  }
+
+  // Prepare a unicode buffer for ConOut, if applicable, as Buffer is used
+  // below.
+  UnicodeSPrintAsciiFormat (UnicodeBuffer, MAX_PRINT_CHARS, Buffer);
 
   DEBUG_CODE_BEGIN ();
   CHAR8   *Pdb;
@@ -289,6 +297,14 @@ DefaultExceptionHandler (
   }
 
   DEBUG ((DEBUG_ERROR, "\n"));
+
+  // Attempt to print that we had a synchronous exception to ConOut.  We do
+  // this after the serial logging as ConOut's logging is more complex and we
+  // aren't guaranteed to succeed.
+  if (gST->ConOut != NULL) {
+    gST->ConOut->OutputString (gST->ConOut, UnicodeBuffer);
+  }
+
   ASSERT (FALSE);
 
   CpuDeadLoop ();   // may return if executing under a debugger
