773f3908590b6b748af46dce0fe520c5b96c52e8
Wenxing Hou <wenxing.hou@intel.com>
2023-08-21 10:25:43 +0800

    CryptoPkg: Add Mbedtls submodule in CI
    Change CI for new Mbedtls submodule.
    
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4177
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Yi Li <yi1.li@intel.com>
    Cc: Xiaoyu Lu <xiaoyu1.lu@intel.com>
    Cc: Guomin Jiang <guomin.jiang@intel.com>
    Signed-off-by: Wenxing Hou <wenxing.hou@intel.com>

---
+++ CryptoPkg/Private/Library/MbedTlsLib.h
@@ -0,0 +1,12 @@
+/** @file
+  OpensslLib class with APIs from the openssl project
+
+  Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef MBEDTLS_LIB_H_
+#define MBEDTLS_LIB_H_
+
+#endif
