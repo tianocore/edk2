762133ee5287bb625cf1956158f142caea2ae8de
Corvin Köhne <corvink@FreeBSD.org>
2023-09-07 10:34:00 +0200

    OvmfPkg/Bhyve: don't exit early if RSDP is not found in memory
    If OVMF fails to find the RSDP in memory, it should fall back installing
    the statically provided ACPI tables.
    
    Signed-off-by: Corvin Köhne <corvink@FreeBSD.org>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Rebecca Cran <rebecca@bsdio.com>
    Cc: Peter Grehan <grehan@freebsd.org>
    Reviewed-by: Rebecca Cran <rebecca@bsdio.com>

--- OvmfPkg/Bhyve/AcpiPlatformDxe/AcpiPlatform.c
+++ OvmfPkg/Bhyve/AcpiPlatformDxe/AcpiPlatform.c
@@ -259,19 +259,17 @@ InstallAcpiTables (
              BHYVE_BIOS_PHYSICAL_END,
              &Rsdp
              );
-  if (EFI_ERROR (Status)) {
-    return Status;
-  }
-
-  Status = InstallAcpiTablesFromRsdp (
-             AcpiTable,
-             Rsdp
-             );
   if (!EFI_ERROR (Status)) {
-    return EFI_SUCCESS;
+    Status = InstallAcpiTablesFromRsdp (
+               AcpiTable,
+               Rsdp
+               );
+    if (!EFI_ERROR (Status)) {
+      return EFI_SUCCESS;
+    }
   }
 
-  if (Status != EFI_NOT_FOUND) {
+  if (EFI_ERROR (Status)) {
     DEBUG (
       (
        DEBUG_WARN,
@@ -280,7 +278,6 @@ InstallAcpiTables (
        Status
       )
       );
-    return Status;
   }
 
   Status = InstallOvmfFvTables (AcpiTable);
