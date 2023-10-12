89aaf52d992f0e417670906023b1e475be977849
Laszlo Ersek <lersek@redhat.com>
2023-10-12 11:10:56 +0200

    EmbeddedPkg/VirtualRealTimeClockLib: drop LibRtcVirtualNotifyEvent
    The RealTimeClockLib class header in edk2 mistakenly declares a function
    called LibRtcVirtualNotifyEvent(). No component ever calls this function
    crossing module boundaries; all RealTimeClockLib instances in edk2 and
    edk2-platforms are supposed to register (and do register) their
    SetVirtualAddressMap() notification functions.
    
    In particular, VirtualRealTimeClockLib *itself* doesn't even use
    LibRtcVirtualNotifyEvent() -- the function is defined with an empty body,
    clearly in an understandable, but misguided, attempt, to conform to the
    (bogus) library interface. Remove the function.
    
    Build-tested only (with "RiscVVirtQemu.dsc").
    
    Cc: Abner Chang <abner.chang@amd.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Daniel Schaefer <git@danielschaefer.me>
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=4564
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Message-Id: <20231012091057.108728-5-lersek@redhat.com>
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>

--- EmbeddedPkg/Library/VirtualRealTimeClockLib/VirtualRealTimeClockLib.c
+++ EmbeddedPkg/Library/VirtualRealTimeClockLib/VirtualRealTimeClockLib.c
@@ -402,21 +402,3 @@ LibRtcInitialize (
 {
   return EFI_SUCCESS;
 }
-
-/**
-   Fixup internal data so that EFI can be call in virtual mode.
-   Call the passed in Child Notify event and convert any pointers in
-   lib to virtual mode.
-
-   @param[in]    Event   The Event that is being processed
-   @param[in]    Context Event Context
-**/
-VOID
-EFIAPI
-LibRtcVirtualNotifyEvent (
-  IN EFI_EVENT  Event,
-  IN VOID       *Context
-  )
-{
-  return;
-}
