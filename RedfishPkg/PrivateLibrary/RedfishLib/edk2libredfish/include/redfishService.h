8e9910789621158227ce0ca2e68919d775b4496c
Nickle Wang <nicklew@nvidia.com>
2023-10-24 14:43:10 +0800

    RedfishPkg/libredfish: introduce new interfaces.
    - Add new interfaces to return HTTP headers back to caller.
    New interfaces are: getUriFromServiceEx(), patchUriFromServiceEx(),
    postUriFromServiceEx() and putUriFromServiceEx().
    - Fix compile error in payload.c
    
    Signed-off-by: Nickle Wang <nicklew@nvidia.com>
    Cc: Abner Chang <abner.chang@amd.com>
    Cc: Igor Kulchytskyy <igork@ami.com>
    Cc: Nick Ramirez <nramirez@nvidia.com>
    Reviewed-by: Abner Chang <abner.chang@amd.com>

--- RedfishPkg/PrivateLibrary/RedfishLib/edk2libredfish/include/redfishService.h
+++ RedfishPkg/PrivateLibrary/RedfishLib/edk2libredfish/include/redfishService.h
@@ -10,6 +10,7 @@
 
   Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
   (C) Copyright 2021 Hewlett Packard Enterprise Development LP<BR>
+  Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -105,10 +106,29 @@ getUriFromService (
   );
 
 json_t *
+getUriFromServiceEx (
+  redfishService        *service,
+  const char            *uri,
+  EFI_HTTP_HEADER       **Headers,
+  UINTN                 *HeaderCount,
+  EFI_HTTP_STATUS_CODE  **StatusCode
+  );
+
+json_t *
 patchUriFromService (
   redfishService        *service,
   const char            *uri,
   const char            *content,
+  EFI_HTTP_STATUS_CODE  **StatusCode
+  );
+
+json_t *
+patchUriFromServiceEx (
+  redfishService        *service,
+  const char            *uri,
+  const char            *content,
+  EFI_HTTP_HEADER       **Headers,
+  UINTN                 *HeaderCount,
   EFI_HTTP_STATUS_CODE  **StatusCode
   );
 
@@ -119,6 +139,30 @@ postUriFromService (
   const char            *content,
   size_t                contentLength,
   const char            *contentType,
+  EFI_HTTP_STATUS_CODE  **StatusCode
+  );
+
+json_t *
+postUriFromServiceEx (
+  redfishService        *service,
+  const char            *uri,
+  const char            *content,
+  size_t                contentLength,
+  const char            *contentType,
+  EFI_HTTP_HEADER       **Headers,
+  UINTN                 *HeaderCount,
+  EFI_HTTP_STATUS_CODE  **StatusCode
+  );
+
+json_t *
+putUriFromServiceEx (
+  redfishService        *service,
+  const char            *uri,
+  const char            *content,
+  size_t                contentLength,
+  const char            *contentType,
+  EFI_HTTP_HEADER       **Headers,
+  UINTN                 *HeaderCount,
   EFI_HTTP_STATUS_CODE  **StatusCode
   );
 
--- RedfishPkg/PrivateLibrary/RedfishLib/edk2libredfish/src/payload.c
+++ RedfishPkg/PrivateLibrary/RedfishLib/edk2libredfish/src/payload.c
@@ -10,6 +10,7 @@
 
   Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
   (C) Copyright 2021 Hewlett Packard Enterprise Development LP<BR>
+  Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -524,7 +525,7 @@ getOpResult (
   }
 
   stringProp = prop->json;
-  jsonType   = prop->json->type;
+  jsonType   = JsonGetType (prop->json);
   switch (jsonType) {
     case JSON_OBJECT:
       stringProp = json_object_get (prop->json, propName);
