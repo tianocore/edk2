0a2fdda17216d805a5cd0b3caba3309f2f9d49c8
Joey Vagedes <joeyvagedes@microsoft.com>
2023-10-27 08:15:50 -0700

    .pytool: Integration of edk2-pytools
    Performs Integration instructions necessary to upgrade edk2-pytool-library
    to 0.19.3 and edk2-pytool-extensions to 0.25.1. This includes resolving
    deprecation warnings in the UncrustifyCheck plugin, and Updating the
    HostUnitTestDscCompleteCheck plguin to account for a change such that inf's
    that do not filter the LIBRARY_CLASS define to certain types, are assumed
    to also support HOST_APPLICATION.
    
    Cc: Sean Brogan <sean.brogan@microsoft.com>
    Cc: Michael Kubacki <mikuback@linux.microsoft.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Signed-off-by: Joey Vagedes <joeyvagedes@gmail.com>
    Message-Id: <20231027151551.1043941-2-joeyvagedes@microsoft.com>
    Reviewed-by: Rebecca Cran <rebecca@os.amperecomputing.com>
    Reviewed-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- .pytool/Plugin/UncrustifyCheck/UncrustifyCheck.py
+++ .pytool/Plugin/UncrustifyCheck/UncrustifyCheck.py
@@ -299,7 +299,7 @@ class UncrustifyCheck(ICiBuildPlugin):
         If git is not found, an empty list will be returned.
         """
         if not shutil.which("git"):
-            logging.warn(
+            logging.warning(
                 "Git is not found on this system. Git submodule paths will not be considered.")
             return []
 
@@ -325,7 +325,7 @@ class UncrustifyCheck(ICiBuildPlugin):
         If git is not found, an empty list will be returned.
         """
         if not shutil.which("git"):
-            logging.warn(
+            logging.warning(
                 "Git is not found on this system. Git submodule paths will not be considered.")
             return []
 
@@ -372,9 +372,9 @@ class UncrustifyCheck(ICiBuildPlugin):
                 file_template_path = pathlib.Path(os.path.join(self._plugin_path, file_template_name))
                 self._file_template_contents = file_template_path.read_text()
         except KeyError:
-            logging.warn("A file header template is not specified in the config file.")
+            logging.warning("A file header template is not specified in the config file.")
         except FileNotFoundError:
-            logging.warn("The specified file header template file was not found.")
+            logging.warning("The specified file header template file was not found.")
         try:
             func_template_name = parser["dummy_section"]["cmt_insert_func_header"]
 
@@ -384,9 +384,9 @@ class UncrustifyCheck(ICiBuildPlugin):
                 func_template_path = pathlib.Path(os.path.join(self._plugin_path, func_template_name))
                 self._func_template_contents = func_template_path.read_text()
         except KeyError:
-            logging.warn("A function header template is not specified in the config file.")
+            logging.warning("A function header template is not specified in the config file.")
         except FileNotFoundError:
-            logging.warn("The specified function header template file was not found.")
+            logging.warning("The specified function header template file was not found.")
 
     def _initialize_app_info(self) -> None:
         """
