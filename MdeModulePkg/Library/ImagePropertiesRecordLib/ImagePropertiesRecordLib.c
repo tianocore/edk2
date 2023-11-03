70beb8a215822adff133874e88f550afe5d7106a
Taylor Beebe <taylor.d.beebe@gmail.com>
2023-11-03 08:29:42 -0700

    MdeModulePkg: Add ImagePropertiesRecordLib
    Create a library for manipulating image properties records. The
    library is currently blank and will be filled in a future patch
    to help with reviewer readability.
    
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Dandan Bi <dandan.bi@intel.com>
    Signed-off-by: Taylor Beebe <taylor.d.beebe@gmail.com>
    Reviewed-by: Liming Gao <gaoliming@byosoft.com.cn>

---
+++ MdeModulePkg/Library/ImagePropertiesRecordLib/ImagePropertiesRecordLib.c
@@ -0,0 +1,9 @@
+/** @file
+
+  Provides definitions and functionality for manipulating IMAGE_PROPERTIES_RECORD.
+
+  Copyright (c) 2016 - 2018, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) Microsoft Corporation.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
