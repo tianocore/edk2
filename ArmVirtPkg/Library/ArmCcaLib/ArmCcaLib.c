8be75251cd9d42dd06333e19c87d64dca84234ff
Sami Mujawar <sami.mujawar@arm.com>
2023-11-30 14:08:51 +0000

    ArmVirtPkg: Refactor ArmCcaLib to optimise and support safe early boot
    ArmCcaLib exposes interfaces to determine whether the current
    execution state is a Realm and to query the Realm IPA width.
    This currently relies on issuing Realm Service Interface (RSI)
    calls, which cause exits from the Realm to retrieve the required
    information. To reduce the number of Realm exits, this information
    should be cached once retrieved.
    
    Some firmware builds do not support writing to static variables
    during early boot phases leading to potential synchronous exceptions,
    while other firmware builds that utilise PrePi (PeilessSec) do not
    support accessing GUID HOBs during the early boot phase.
    
    To support all configurations:
     - First, check whether HOBs are initialized using GetHobList().
     - If HOBs are not initialized, fall back to issuing an RSI call
       to retrieve the requested information.
     - If HOBs are initialized:
       * Check for the presence of a GUID HOB containing the required
         data.
       * If present, return the cached value from the HOB.
       * If absent, issue an RSI call to retrieve the requested
         information. This is required as the PlatformPeim() which
         calls the ArmCcaInitialiseHobs() to create the GUID HOBs
         to cache the information may not have been invoked as yet.
    
    This approach ensures safe operation during early boot while reducing
    unnecessary Realm exits once HOBs are available.
    
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Leif Lindholm <leif.lindholm@oss.qualcomm.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>

--- ArmVirtPkg/Library/ArmCcaLib/ArmCcaLib.c
+++ ArmVirtPkg/Library/ArmCcaLib/ArmCcaLib.c
@@ -18,6 +18,43 @@
 #include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
 
+#include <Uefi/UefiMultiPhase.h>
+#include <Uefi/UefiSpec.h>
+#include <Pi/PiBootMode.h>
+
+#include <Pi/PiHob.h>
+#include <Library/HobLib.h>
+
+/**
+  Queries to see if running in a Realm.
+
+    @retval TRUE    The execution is within the context of a Realm.
+    @retval FALSE   The execution is not within the context of a Realm.
+**/
+STATIC
+BOOLEAN
+EFIAPI
+ReadIsRealm (
+  VOID
+  )
+{
+  RETURN_STATUS  Status;
+  UINT32         UefiImpl;
+  UINT32         RmmImplLow;
+  UINT32         RmmImplHigh;
+
+  Status = RsiGetVersion (
+             &UefiImpl,
+             &RmmImplLow,
+             &RmmImplHigh
+             );
+  if (!RETURN_ERROR (Status)) {
+    return TRUE;
+  }
+
+  return FALSE;
+}
+
 /**
   Check if running in a Realm.
 
@@ -30,23 +67,79 @@ IsRealm (
   VOID
   )
 {
-  RETURN_STATUS  Status;
-  UINT32         UefiImpl;
-  UINT32         RmmImplLow;
-  UINT32         RmmImplHigh;
+  BOOLEAN  RealmWorld;
+  VOID     *HobList;
 
-  if (ArmHasRme ()) {
-    Status = RsiGetVersion (
-               &UefiImpl,
-               &RmmImplLow,
-               &RmmImplHigh
-               );
-    if (!RETURN_ERROR (Status)) {
-      return TRUE;
-    }
+  VOID    *Hob;
+  UINT64  *IsRealmHobData;
+
+  if (!ArmHasRme ()) {
+    return FALSE;
   }
 
-  return FALSE;
+  HobList = GetHobList ();
+  if (HobList == NULL) {
+    // HOB not initialised.
+    return ReadIsRealm ();
+  }
+
+  RealmWorld = FALSE;
+  Hob        = GetFirstGuidHob (&gArmCcaIsRealmGuid);
+  if ((Hob == NULL) ||
+      (GET_GUID_HOB_DATA_SIZE (Hob) < sizeof (*IsRealmHobData)))
+  {
+    // The gArmCcaIsRealmGuid is created in the PlatformPeim() which
+    // may not have been called for caching the IsRealm value yet.
+    // So, read the Realm value.
+    RealmWorld = ReadIsRealm ();
+  } else {
+    // Read the value from the HOB
+    IsRealmHobData = GET_GUID_HOB_DATA (Hob);
+    RealmWorld     = *IsRealmHobData;
+  }
+
+  return RealmWorld;
+}
+
+/**
+  Read the IPA width of the Realm.
+
+  The IPA width of the Realm is used to configure the protection attribute
+  for memory regions, see ArmCcaSetMemoryProtectionAttribute().
+
+    @param [out] IpaWidth  IPA width of the Realm.
+
+    @retval RETURN_SUCCESS            Success.
+    @retval RETURN_INVALID_PARAMETER  A parameter is invalid.
+
+**/
+STATIC
+RETURN_STATUS
+EFIAPI
+ReadIpaWidth (
+  OUT UINT64  *IpaWidth
+  )
+{
+  RETURN_STATUS  Status;
+  REALM_CONFIG   *Config;
+  UINT8          ConfigBuffer[sizeof (REALM_CONFIG) * 2];
+
+  Config = ALIGN_POINTER (ConfigBuffer, sizeof (REALM_CONFIG));
+  ZeroMem (Config, sizeof (REALM_CONFIG));
+
+  Status = RsiGetRealmConfig (Config);
+  if (RETURN_ERROR (Status)) {
+    ASSERT (0);
+    return Status;
+  }
+
+  if (Config->IpaWidth > ArmGetPhysicalAddressBits ()) {
+    ASSERT (0);
+    return RETURN_INVALID_PARAMETER;
+  }
+
+  *IpaWidth = Config->IpaWidth;
+  return RETURN_SUCCESS;
 }
 
 /**
@@ -69,30 +162,42 @@ GetIpaWidth (
   )
 {
   RETURN_STATUS  Status;
-  REALM_CONFIG   *Config;
-  UINT8          ConfigBuffer[sizeof (REALM_CONFIG) * 2];
+  VOID           *HobList;
+  VOID           *Hob;
+  UINT64         *CcaIpaWidth;
 
   if (!IsRealm ()) {
     *IpaWidth = 0;
     return RETURN_UNSUPPORTED;
   }
 
-  Config = ALIGN_POINTER (ConfigBuffer, sizeof (REALM_CONFIG));
-  ZeroMem (Config, sizeof (REALM_CONFIG));
-
-  Status = RsiGetRealmConfig (Config);
-  if (RETURN_ERROR (Status)) {
-    ASSERT (0);
-    *IpaWidth = 0;
-    return Status;
+  HobList = GetHobList ();
+  if (HobList == NULL) {
+    // HOB not initialised.
+    return ReadIpaWidth (IpaWidth);
   }
 
-  if (Config->IpaWidth > ArmGetPhysicalAddressBits ()) {
-    ASSERT (0);
-    *IpaWidth = 0;
-    return RETURN_INVALID_PARAMETER;
+  Hob = GetFirstGuidHob (&gArmCcaIpaWidthGuid);
+  if ((Hob == NULL) ||
+      (GET_GUID_HOB_DATA_SIZE (Hob) < sizeof (UINT64)))
+  {
+    // The gArmCcaIpaWidthGuid is created in the PlatformPeim() which
+    // may not have been called for caching the IpaWidth value yet.
+    // So, read the IPA Width value.
+    Status = ReadIpaWidth (IpaWidth);
+    if (RETURN_ERROR (Status)) {
+      ASSERT (0);
+      return Status;
+    }
+  } else {
+    // Read the value from the HOB.
+    CcaIpaWidth = GET_GUID_HOB_DATA (Hob);
+    if ((UINT64)*CcaIpaWidth == 0) {
+      return RETURN_NOT_FOUND;
+    }
+
+    *IpaWidth = *CcaIpaWidth;
   }
 
-  *IpaWidth = Config->IpaWidth;
   return RETURN_SUCCESS;
 }
