09b299c7aa9ebb674c946cdb4e8871f6c1a959b3
Ceping Sun <cepingx.sun@intel.com>
2023-11-30 21:00:32 -0500

    OvmfPkg: Add the ResetVector in TDX MailBox
    Base on ACPI 6.6 Multiprocessor Wakeup Structure, TDVF needs to provide
    a physical address of the ResetVector to OS. And it should allow the
    Multiprocessor Wakeup Mailbox to be reset in order to be used once again
    with a given AP.
    
    Signed-off-by: Ceping Sun <cepingx.sun@intel.com>
    Reviewed-by: Min Xu <min.m.xu@intel.com>
    Tested-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- OvmfPkg/Include/IndustryStandard/IntelTdx.h
+++ OvmfPkg/Include/IndustryStandard/IntelTdx.h
@@ -60,6 +60,7 @@ typedef struct {
 typedef struct {
   UINT8    *RelocateApLoopFuncAddress;
   UINTN    RelocateApLoopFuncSize;
+  UINT8    *RelocateApResetVector;
 } MP_RELOCATION_MAP;
 
 #pragma pack()
