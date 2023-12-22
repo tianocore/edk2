fb25a8a4bcec4f79738e88bb0280c6d5dd9400af
Sunil V L <sunilvl@ventanamicro.com>
2023-12-22 21:57:12 +0530

    DynamicTablesPkg: DynamicPlatRepoLib: Rename ArmObjList as ArchObjList
    DynamicPlatRepoLib maintains list of CmObjs and named as ArmObjList.
    However, to support other architectures, it is better to name this in a
    architecture neutral way.
    
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Cc: Pierre Gondois <pierre.gondois@arm.com>
    Cc: Sami Mujawar <Sami.Mujawar@arm.com>
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

--- DynamicTablesPkg/Library/Common/DynamicPlatRepoLib/DynamicPlatRepo.c
+++ DynamicTablesPkg/Library/Common/DynamicPlatRepoLib/DynamicPlatRepo.c
@@ -174,7 +174,7 @@ DynPlatRepoAddObject (
   }
 
   // Add to link list.
-  InsertTailList (&This->ArmCmObjList[ArmNamespaceObjId], &ObjNode->Link);
+  InsertTailList (&This->ArchCmObjList[ArmNamespaceObjId], &ObjNode->Link);
   This->ObjectCount += 1;
 
   if (Token != NULL) {
@@ -223,7 +223,7 @@ GroupCmObjNodes (
   Count    = 0;
   Size     = 0;
   CmObjId  = CREATE_CM_ARCH_OBJECT_ID (ArchObjIndex);
-  ListHead = &This->ArmCmObjList[ArchObjIndex];
+  ListHead = &This->ArchCmObjList[ArchObjIndex];
   Link     = GetFirstNode (ListHead);
 
   // Compute the total count and size of the CmObj in the list.
@@ -286,7 +286,7 @@ GroupCmObjNodes (
     Link  = GetNextNode (ListHead, Link);
   } // while
 
-  CmObjDesc           = &This->ArmCmObjArray[ArchObjIndex];
+  CmObjDesc           = &This->ArchCmObjArray[ArchObjIndex];
   CmObjDesc->ObjectId = CmObjId;
   CmObjDesc->Size     = (UINT32)Size;
   CmObjDesc->Count    = (UINT32)Count;
@@ -412,7 +412,7 @@ DynamicPlatRepoGetObject (
     return EFI_INVALID_PARAMETER;
   }
 
-  Desc = &This->ArmCmObjArray[ArmNamespaceObjId];
+  Desc = &This->ArchCmObjArray[ArmNamespaceObjId];
 
   // Nothing here.
   if (Desc->Count == 0) {
@@ -459,7 +459,7 @@ DynamicPlatRepoInit (
 
   // Initialise the CmObject List.
   for (Index = 0; Index < EArchObjMax; Index++) {
-    InitializeListHead (&Repo->ArmCmObjList[Index]);
+    InitializeListHead (&Repo->ArchCmObjList[Index]);
   }
 
   Repo->ObjectCount = 0;
@@ -499,14 +499,14 @@ DynamicPlatRepoShutdown (
   // Free the list of objects.
   for (Index = 0; Index < EArchObjMax; Index++) {
     // Free all the nodes with this object Id.
-    ListHead = &DynPlatRepo->ArmCmObjList[Index];
+    ListHead = &DynPlatRepo->ArchCmObjList[Index];
     while (!IsListEmpty (ListHead)) {
       FreeCmObjNode ((CM_OBJ_NODE *)GetFirstNode (ListHead));
     } // while
   } // for
 
   // Free the arrays.
-  CmObjDesc = DynPlatRepo->ArmCmObjArray;
+  CmObjDesc = DynPlatRepo->ArchCmObjArray;
   for (Index = 0; Index < EArchObjMax; Index++) {
     Data = CmObjDesc[Index].Data;
     if (Data != NULL) {
