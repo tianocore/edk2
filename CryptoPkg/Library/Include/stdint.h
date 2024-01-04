2af1a658b3791e0b26a5ece3925b9a4cf2b4501f
Hou, Wenxing <wenxing.hou@intel.com>
2024-01-04 16:19:55 +0800

    CryptoPkg: move define to CrtLibSupport
    REF:https://bugzilla.tianocore.org/show_bug.cgi?id=4632
    
    The before code will cause redefine error.
    This patch move them to CrtLibSupport header.
    But Openssl has already defined them internally,
    need to increase support for OPENSLL_SYS_UEFI judgment.
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Yi Li <yi1.li@intel.com>
    Cc: Guomin Jiang <guomin.jiang@intel.com>
    Signed-off-by: Wenxing Hou <wenxing.hou@intel.com>
    Reviewed-by: Yi Li <yi1.li@intel.com>

--- CryptoPkg/Library/Include/stdint.h
+++ CryptoPkg/Library/Include/stdint.h
@@ -7,13 +7,3 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
 #include <CrtLibSupport.h>
-
-typedef INT8    int8_t;
-typedef UINT8   uint8_t;
-typedef INT16   int16_t;
-typedef UINT16  uint16_t;
-typedef INT32   int32_t;
-typedef UINT32  uint32_t;
-typedef INT64   int64_t;
-typedef UINT64  uint64_t;
-typedef UINTN   uintptr_t;
