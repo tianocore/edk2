0cc92d037ca0849da6c43b029103f7bfae125593
Himanshu Sharma <Himanshu.Sharma@arm.com>
2024-01-04 13:32:56 +0530

    ArmPkg/ArmGicArchLib: Add macros for SPI and extended SPI ranges
    Taking reference from Table 2-1 of the Arm Generic Interrupt Controller
    Architecture Specification, Issue H, January 2022, add macros for the
    SPI and extended SPI ranges with the purpose of reusability on including
    the ArmPkg.
    
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Sami Mujawar <sami.mujawar@arm.com>
    Signed-off-by: Himanshu Sharma <Himanshu.Sharma@arm.com>
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>
    Reviewed-by: Pierre Gondois <pierre.gondois@arm.com>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>

--- ArmPkg/Include/Library/ArmGicArchLib.h
+++ ArmPkg/Include/Library/ArmGicArchLib.h
@@ -1,8 +1,14 @@
 /** @file
 *
 *  Copyright (c) 2015, Linaro Ltd. All rights reserved.
+*  Copyright (c) 2024, Arm Limited. All rights reserved.
 *
 *  SPDX-License-Identifier: BSD-2-Clause-Patent
+*
+*  @par Reference(s):
+*  - Arm Generic Interrupt Controller Architecture Specification,
+*    Issue H, January 2022.
+*    (https://developer.arm.com/documentation/ihi0069/)
 *
 **/
 
@@ -22,5 +28,13 @@ EFIAPI
 ArmGicGetSupportedArchRevision (
   VOID
   );
+
+//
+// GIC SPI and extended SPI ranges
+//
+#define ARM_GIC_ARCH_SPI_MIN      32
+#define ARM_GIC_ARCH_SPI_MAX      1019
+#define ARM_GIC_ARCH_EXT_SPI_MIN  4096
+#define ARM_GIC_ARCH_EXT_SPI_MAX  5119
 
 #endif // ARM_GIC_ARCH_LIB_H_
