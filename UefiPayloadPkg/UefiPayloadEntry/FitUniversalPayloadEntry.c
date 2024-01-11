9add2ea434db8f5819df4b69e7bf0b515f1b4e70
Gua Guo <gua.guo@intel.com>
2024-01-11 13:01:19 +0800

    UefiPayloadPkg/Hob: Integer Overflow in CreateHob()
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4166
    
    Fix integer overflow in various CreateHob instances.
    Fixes: CVE-2022-36765
    
    The CreateHob() function aligns the requested size to 8
    performing the following operation:
    ```
    HobLength = (UINT16)((HobLength + 0x7) & (~0x7));
    ```
    
    No checks are performed to ensure this value doesn't
    overflow, and could lead to CreateHob() returning a smaller
    HOB than requested, which could lead to OOB HOB accesses.
    
    Reported-by: Marc Beatove <mbeatove@google.com>
    Cc: Guo Dong <guo.dong@intel.com>
    Cc: Sean Rhodes <sean@starlabs.systems>
    Cc: James Lu <james.lu@intel.com>
    Reviewed-by: Gua Guo <gua.guo@intel.com>
    Cc: John Mathew <john.mathews@intel.com>
    Authored-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Gua Guo <gua.guo@intel.com>

--- UefiPayloadPkg/UefiPayloadEntry/FitUniversalPayloadEntry.c
+++ UefiPayloadPkg/UefiPayloadEntry/FitUniversalPayloadEntry.c
@@ -207,10 +207,12 @@ AddNewHob (
   }
 
   NewHob.Header = CreateHob (Hob->Header->HobType, Hob->Header->HobLength);
-
-  if (NewHob.Header != NULL) {
-    CopyMem (NewHob.Header + 1, Hob->Header + 1, Hob->Header->HobLength - sizeof (EFI_HOB_GENERIC_HEADER));
+  ASSERT (NewHob.Header != NULL);
+  if (NewHob.Header == NULL) {
+    return;
   }
+
+  CopyMem (NewHob.Header + 1, Hob->Header + 1, Hob->Header->HobLength - sizeof (EFI_HOB_GENERIC_HEADER));
 }
 
 /**
