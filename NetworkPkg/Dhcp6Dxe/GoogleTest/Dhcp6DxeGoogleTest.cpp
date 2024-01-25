4646e16cda110a1b63265b94d4f912cbe715087d
Doug Flick via groups.io <dougflick=microsoft.com@groups.io>
2024-01-26 05:54:45 +0800

    NetworkPkg: Dhcp6Dxe: SECURITY PATCH CVE-2023-45230 Unit Tests
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4535
    
    Confirms that reported issue...
    
    "Buffer overflow in the DHCPv6 client via a long Server ID option"
    
    ..has been corrected by the provided patch.
    
    Tests the following functions to ensure they appropriately handle
    untrusted data (either too long or too small) to prevent a buffer
    overflow:
    
    Dhcp6AppendOption
    Dhcp6AppendETOption
    Dhcp6AppendIaOption
    
    Cc: Saloni Kasbekar <saloni.kasbekar@intel.com>
    Cc: Zachary Clark-williams <zachary.clark-williams@intel.com>
    
    Signed-off-by: Doug Flick [MSFT] <doug.edk2@gmail.com>
    Reviewed-by: Saloni Kasbekar <saloni.kasbekar@intel.com>

---
+++ NetworkPkg/Dhcp6Dxe/GoogleTest/Dhcp6DxeGoogleTest.cpp
@@ -0,0 +1,20 @@
+/** @file
+  Acts as the main entry point for the tests for the Dhcp6Dxe module.
+
+  Copyright (c) Microsoft Corporation
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+#include <gtest/gtest.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Run the tests
+////////////////////////////////////////////////////////////////////////////////
+int
+main (
+  int   argc,
+  char  *argv[]
+  )
+{
+  testing::InitGoogleTest (&argc, argv);
+  return RUN_ALL_TESTS ();
+}
