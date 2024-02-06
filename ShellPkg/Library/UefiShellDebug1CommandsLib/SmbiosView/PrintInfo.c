c7b3e64922ff8557014ad37bf92089834bef9d18
Harshal Ratan Patil <hapatil@nvidia.com>
2024-02-06 11:18:59 +0000

    ShellPkg : Add ARMv9 support in smbiosview
    Add ARMv9 processor family support.
    
    Signed-off-by: Harshal Ratan Patil <hapatil@nvidia.com>

--- ShellPkg/Library/UefiShellDebug1CommandsLib/SmbiosView/PrintInfo.c
+++ ShellPkg/Library/UefiShellDebug1CommandsLib/SmbiosView/PrintInfo.c
@@ -2562,6 +2562,10 @@ DisplayProcessorFamily2 (
       Print (L"ARMv8\n");
       break;
 
+    case 0x102:
+      Print (L"ARMv9\n");
+      break;
+
     case 0x104:
       Print (L"SH-3\n");
       break;
