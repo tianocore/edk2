bf78bc6a169a30eab67686ac725846bc9f9c12f6
Michael D Kinney <michael.d.kinney@intel.com>
2024-02-13 14:52:36 -0800

    BaseTools/Scripts/PatchCheck: Error if commit modifies multiple packages
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4679
    
    Update PatchCheck.py to evaluate all the files modified in
    each commit and generate an error if:
    * A single commit modifies files in multiple packages
    * A single commit modifies files in multiple non-package dirs
    * A single commit modifies files in both a package and a
      non-package dir.
    
    Modifications to files in the root of the repository are not
    evaluated.
    
    The set of packages are found by search for DEC files in the
    repository. The list of DEC files in the repository is collected
    with the following git command:
    
      git ls-files *.dec
    
    The set of files added/modified by each commit is found using
    the following git command:
    
      git diff-tree --no-commit-id --name-only --diff-filter=AM -r <commit>
    
    The set of files deleted by each commit is found using the
    following git command:
    
      git diff-tree --no-commit-id --name-only --diff-filter=D -r <commit>
    
    Cc: Rebecca Cran <rebecca@bsdio.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Bob Feng <bob.c.feng@intel.com>
    Cc: Yuwei Chen <yuwei.chen@intel.com>
    Cc: Michael Kubacki <mikuback@linux.microsoft.com>
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>

--- BaseTools/Scripts/PatchCheck.py
+++ BaseTools/Scripts/PatchCheck.py
@@ -665,6 +665,7 @@ class CheckGitCommits:
     """
 
     def __init__(self, rev_spec, max_count):
+        dec_files = self.read_dec_files_from_git()
         commits = self.read_commit_list_from_git(rev_spec, max_count)
         if len(commits) == 1 and Verbose.level > Verbose.ONELINE:
             commits = [ rev_spec ]
@@ -681,8 +682,56 @@ class CheckGitCommits:
             self.ok &= EmailAddressCheck(email, 'Committer').ok
             patch = self.read_patch_from_git(commit)
             self.ok &= CheckOnePatch(commit, patch).ok
+            self.ok &= self.check_parent_packages (dec_files, commit)
+
         if not commits:
             print("Couldn't find commit matching: '{}'".format(rev_spec))
+
+    def get_parent_packages(self, dec_files, commit, filter):
+        filelist = self.read_files_modified_from_git (commit, filter)
+        parents = set()
+        for file in filelist:
+            dec_found = False
+            for dec_file in dec_files:
+                if os.path.commonpath([dec_file, file]):
+                    dec_found = True
+                    parents.add(dec_file)
+            if not dec_found and os.path.dirname (file):
+                # No DEC file found and file is in a subdir
+                # Covers BaseTools, .github, .azurepipelines, .pytool
+                parents.add(file.split('/')[0])
+        return list(parents)
+
+    def check_parent_packages(self, dec_files, commit):
+        modified = self.get_parent_packages (dec_files, commit, 'AM')
+        if len (modified) > 1:
+            print("The commit adds/modifies files in multiple packages:\n *",
+                  '\n * '.join(modified))
+            self.ok = False
+        deleted = self.get_parent_packages (dec_files, commit, 'D')
+        if len (deleted) > 1:
+            print("The commit deletes files from multiple packages:\n *",
+                  '\n * '.join(deleted))
+            self.ok = False
+        return self.ok
+
+    def read_dec_files_from_git(self):
+        # run git ls-files *.dec
+        out = self.run_git('ls-files', '*.dec')
+        # return list of .dec files
+        try:
+            return out.split()
+        except:
+            return []
+
+    def read_files_modified_from_git(self, commit, filter):
+        # run git diff-tree --no-commit-id --name-only -r <commit>
+        out = self.run_git('diff-tree', '--no-commit-id', '--name-only',
+                           '--diff-filter=' + filter, '-r', commit)
+        try:
+            return out.split()
+        except:
+            return []
 
     def read_commit_list_from_git(self, rev_spec, max_count):
         # Run git to get the commit patch
