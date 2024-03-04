06f3cf6bb1bc2658fa3489f4700891143fb68849
Hongbin1 Zhang <hongbin1.zhang@intel.com>
2024-03-04 18:09:54 +0800

    StandaloneMmPkg: StandaloneMmIplPei remove MM_CORE_PRIVATE_DATA code
    MM_CORE_PRIVATE_DATA related code was not needed
    
    Signed-off-by: Hongbin1 Zhang <hongbin1.zhang@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Jiaxin Wu <jiaxin.wu@intel.com>
    Cc: Sami Mujawar <sami.mujawar@arm.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Supreeth Venkatesh <supreeth.venkatesh@arm.com>

--- MdePkg/Include/Guid/MmCommBuffer.h
+++ MdePkg/Include/Guid/MmCommBuffer.h
@@ -15,6 +15,22 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 extern EFI_GUID  gEdkiiCommunicationBufferGuid;
 
+///
+/// Define values for the communications buffer used when gEfiEventDxeDispatchGuid is
+/// event signaled.  This event is signaled by the DXE Core each time the DXE Core
+/// dispatcher has completed its work.  When this event is signaled, the MM Core
+/// if notified, so the MM Core can dispatch MM drivers.  If COMM_BUFFER_MM_DISPATCH_ERROR
+/// is returned in the communication buffer, then an error occurred dispatching MM
+/// Drivers.  If COMM_BUFFER_MM_DISPATCH_SUCCESS is returned, then the MM Core
+/// dispatched all the drivers it could.  If COMM_BUFFER_MM_DISPATCH_RESTART is
+/// returned, then the MM Core just dispatched the MM Driver that registered
+/// the MM Entry Point enabling the use of MM Mode.  In this case, the MM Core
+/// should be notified again to dispatch more MM Drivers using MM Mode.
+///
+#define COMM_BUFFER_MM_DISPATCH_ERROR    0x00
+#define COMM_BUFFER_MM_DISPATCH_SUCCESS  0x01
+#define COMM_BUFFER_MM_DISPATCH_RESTART  0x02
+
 typedef struct {
   //
   // Address pointer to MM_COMM_BUFFER_DATA
