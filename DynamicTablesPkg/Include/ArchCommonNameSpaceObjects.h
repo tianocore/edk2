02138e71309005d928179861a77b98d8005a628a
Sami Mujawar <sami.mujawar@arm.com>
2024-03-05 16:34:46 +0000

    DynamicTablesPkg: Move Power Mgmt Profile Info Object
    Move PowerManagementProfileInfo Object from Arm Namespace to the
    Arch Common namespace.
    The following updates are also done to reflect the changes introduced
    by the move:
     - Update the FADT Generator to migrate to use the Power Management
       Profile Info object CM_ARCH_COMMON_POWER_MANAGEMENT_PROFILE_INFO
       and EArchCommonObjPowerManagementProfileInfo.
     - Update the Configuration manager object parser to parse Arch
       Common namespace objects and update the parsing of the Power
       Management Profile information object from Arm namespace to
       the Arch Common namespace.
     - Update the Dynamic Plat Repo TokenFixer map
    
    Cc: Pierre Gondois <Pierre.Gondois@arm.com>
    Cc: Yeo Reum Yun <YeoReum.Yun@arm.com>
    Cc: AbdulLateef Attar <AbdulLateef.Attar@amd.com>
    Cc: Jeshua Smith <jeshuas@nvidia.com>
    Cc: Jeff Brasen <jbrasen@nvidia.com>
    Cc: Girish Mahadevan <gmahadevan@nvidia.com>
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Cc: Meenakshi Aggarwal <meenakshi.aggarwal@nxp.com>
    Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>
    Reviewed-by: Sunil V L <sunilvl@ventanamicro.com>

--- DynamicTablesPkg/Include/ArchCommonNameSpaceObjects.h
+++ DynamicTablesPkg/Include/ArchCommonNameSpaceObjects.h
@@ -21,7 +21,25 @@
 */
 typedef enum ArchCommonObjectID {
   EArchCommonObjReserved,                       ///<  0 - Reserved
+  EArchCommonObjPowerManagementProfileInfo,     ///<  1 - Power Management Profile Info
   EArchCommonObjMax
 } EARCH_COMMON_OBJECT_ID;
+
+#pragma pack(1)
+
+/** A structure that describes the
+    Power Management Profile Information for the Platform.
+
+    ID: EArchCommonObjPowerManagementProfileInfo
+*/
+typedef struct CmArchCommonPowerManagementProfileInfo {
+  /** This is the Preferred_PM_Profile field of the FADT Table
+      described in the ACPI Specification
+  */
+  UINT8    PowerManagementProfile;
+} CM_ARCH_COMMON_POWER_MANAGEMENT_PROFILE_INFO;
+
+
+#pragma pack()
 
 #endif // ARCH_COMMON_NAMESPACE_OBJECTS_H_
