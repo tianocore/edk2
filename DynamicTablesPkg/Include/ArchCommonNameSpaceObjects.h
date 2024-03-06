84ce8a3283d69802e571c83d2961ee40db01708d
Sami Mujawar <sami.mujawar@arm.com>
2024-03-06 09:04:09 +0000

    DynamicTablesPkg: Introduce an Arch Common Namespace header file
    Introduce a new header file for defining the Arch Common Namespace
    objects. Also include it in the Configuration Manager Object header
    file so that the required definitions are propagated.
    
    Cc: Pierre Gondois <Pierre.Gondois@arm.com>
    Cc: Yeo Reum Yun <YeoReum.Yun@arm.com>
    Cc: AbdulLateef Attar <AbdulLateef.Attar@amd.com>
    Cc: Jeshua Smith <jeshuas@nvidia.com>
    Cc: Jeff Brasen <jbrasen@nvidia.com>
    Cc: Girish Mahadevan <gmahadevan@nvidia.com>
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Cc: Meenakshi Aggarwal <meenakshi.aggarwal@nxp.com>
    Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>
    Signed-off-by: Pierre Gondois <pierre.gondois@arm.com>
    Reviewed-by: Sunil V L <sunilvl@ventanamicro.com>

---
+++ DynamicTablesPkg/Include/ArchCommonNameSpaceObjects.h
@@ -0,0 +1,27 @@
+/** @file
+
+  Copyright (c) 2024, Arm Limited. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+  @par Glossary:
+    - Cm or CM   - Configuration Manager
+    - Obj or OBJ - Object
+    - Std or STD - Standard
+**/
+
+#ifndef ARCH_COMMON_NAMESPACE_OBJECTS_H_
+#define ARCH_COMMON_NAMESPACE_OBJECTS_H_
+
+#include <AcpiObjects.h>
+#include <StandardNameSpaceObjects.h>
+
+/** The EARCH_COMMON_OBJECT_ID enum describes the Object IDs
+    in the Arch Common Namespace
+*/
+typedef enum ArchCommonObjectID {
+  EArchCommonObjReserved,                       ///<  0 - Reserved
+  EArchCommonObjMax
+} EARCH_COMMON_OBJECT_ID;
+
+#endif // ARCH_COMMON_NAMESPACE_OBJECTS_H_
