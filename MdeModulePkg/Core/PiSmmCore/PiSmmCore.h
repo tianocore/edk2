5d48dce9c7df122cc466561be1004f923eac8064
Zhiguang Liu <zhiguang.liu@intel.com>
2024-03-11 15:32:19 +0800

    MdeModulePkg/SMM: Support to unregister SMI handler in SMI handlers
    Unregistering SMI handler will free the SMI_HANDLER. However, the
    SmiManage() may be using the link node from SMI_HANDLER for loop if
    the unregistering happens in SMI handlers.
    To avoid that, the idea is to inform SmiHandlerUnRegister() whether
    it's running or not running on the stack of SmiManage(), and to
    postpone SMI_HANDLER deletion before SmiManage() returns.
    Noted SmiManage() may be called recursively, the SmiHandlerUnRegister()
    won't take effect until the root SmiManage() returns.
    
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Jiaxin Wu <jiaxin.wu@intel.com>
    Reviewed-by: Ray Ni <ray.ni@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Zhiguang Liu <zhiguang.liu@intel.com>

--- MdeModulePkg/Core/PiSmmCore/PiSmmCore.h
+++ MdeModulePkg/Core/PiSmmCore/PiSmmCore.h
@@ -93,6 +93,7 @@ typedef struct {
   SMI_ENTRY                       *SmiEntry;
   VOID                            *Context;    // for profile
   UINTN                           ContextSize; // for profile
+  BOOLEAN                         ToRemove;    // To remove this SMI_HANDLER later
 } SMI_HANDLER;
 
 //
