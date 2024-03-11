5eca3aa96a58ed8256af68ededf28ba3c1a992b4
Pierre Gondois <pierre.gondois@arm.com>
2024-03-11 16:05:46 +0100

    ArmPkg,MdePkg: Move ArmPkg/Chipset/ArmV7[|Mmu].h to MdePkg
    Following the discussion at [1] and as the ArmLib relies on them,
    move ArmPkg/Chipset/ArmV7[|Mmu].h files to the MdePkg.
    
    Update the path to correctly include the moved files.
    
    [1] https://edk2.groups.io/g/devel/message/111566
    
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Sami Mujawar <sami.mujawar@arm.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Zhiguang Liu <zhiguang.liu@intel.com>
    Signed-off-by: Pierre Gondois <pierre.gondois@arm.com>

--- MdePkg/Include/Library/ArmLib.h
+++ MdePkg/Include/Library/ArmLib.h
@@ -14,7 +14,7 @@
 #include <Uefi/UefiBaseType.h>
 
 #ifdef MDE_CPU_ARM
-  #include <Chipset/ArmV7.h>
+  #include <Register/Arm/AArch32.h>
 #elif defined (MDE_CPU_AARCH64)
   #include <Register/AArch64/AArch64.h>
 #else
