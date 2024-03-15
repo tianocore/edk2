23b564b9be23856d942d8346edf1336cc013d1a6
Jiaxin Wu <jiaxin.wu@intel.com>
2024-03-15 18:37:40 +0800

    [SmmProfileData Support - 1] Add gEdkiiSmmProfileDataGuid defination.
    Signed-off-by: Jiaxin Wu <jiaxin.wu@intel.com>

---
+++ UefiCpuPkg/Include/Guid/SmmProfileDataHob.h
@@ -0,0 +1,23 @@
+/** @file
+  This file defines a SMM_PROFILE_DATA_HOB.
+  This is a Memory Allocation HOB including the SmmProfileData address/size info.
+
+  Copyright (c) 2024, Intel Corporation. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef SMM_PROFILE_DATA_HOB_H_
+#define SMM_PROFILE_DATA_HOB_H_
+
+#include <PiPei.h>
+
+#define SMM_PROFILE_DATA_GUID \
+  { \
+    0x26ef081d, 0x19b0, 0x4c42, {0xa2, 0x57, 0xa7, 0xf5, 0x9f, 0x8b, 0xd0, 0x38}  \
+  }
+
+extern EFI_GUID  gEdkiiSmmProfileDataGuid;
+
+#endif
