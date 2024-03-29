bb49986f0d8f30756864c825dab185176de013bd
Hou, Wenxing <wenxing.hou@intel.com>
2024-03-29 10:32:40 +0800

    CryptoPkg: Update OPTIONAL location for BaseCryptLibMbedTls
    REF:https://bugzilla.tianocore.org/show_bug.cgi?id=4740
    
    There is a wrong usage for OPTIONAL.
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Yi Li <yi1.li@intel.com>
    Signed-off-by: Wenxing Hou <wenxing.hou@intel.com>
    Reviewed-by: Yi Li <yi1.li@intel.com>

--- CryptoPkg/Library/BaseCryptLibMbedTls/Pk/CryptPkcs1OaepNull.c
+++ CryptoPkg/Library/BaseCryptLibMbedTls/Pk/CryptPkcs1OaepNull.c
@@ -38,10 +38,8 @@ Pkcs1v2Encrypt (
   IN   UINTN        PublicKeySize,
   IN   UINT8        *InData,
   IN   UINTN        InDataSize,
-  IN   CONST UINT8  *PrngSeed,
-  OPTIONAL
-  IN   UINTN        PrngSeedSize,
-  OPTIONAL
+  IN   CONST UINT8  *PrngSeed  OPTIONAL,
+  IN   UINTN        PrngSeedSize  OPTIONAL,
   OUT  UINT8        **EncryptedData,
   OUT  UINTN        *EncryptedDataSize
   )
