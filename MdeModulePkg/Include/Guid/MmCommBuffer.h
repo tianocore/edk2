e47045d5f408893f8c4bd7e11f3620fb1d42345a
Hongbin1 Zhang <hongbin1.zhang@intel.com>
2024-05-06 21:17:54 +0800

    MdeModulePkg: Add MM communication buffer structure and GUID
    MM communication buffer structure including following parameters:
    1.FixedCommBuffer     : Buffer for data transmission between
      MM communication protocol and MMI handler
    2.FixedCommBufferSize : The fixed communication buffer size,
      the buffer size could be changed by PcdFixedCommBufferPages
    3.CommunicationInOut  : it will include return status, return
      buffer size and communication buffer flag
    
    Signed-off-by: Hongbin1 Zhang <hongbin1.zhang@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Jiaxin Wu <jiaxin.wu@intel.com>
    Cc: Wei6 Xu <wei6.xu@intel.com>
    Cc: Sami Mujawar <sami.mujawar@arm.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Supreeth Venkatesh <supreeth.venkatesh@arm.com>

---
+++ MdeModulePkg/Include/Guid/MmCommBuffer.h
@@ -0,0 +1,65 @@
+/** @file
+  MM Communication buffer data.
+
+Copyright (c) 2024, Intel Corporation. All rights reserved.<BR>
+
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef MM_COMM_BUFFER_H_
+#define MM_COMM_BUFFER_H_
+
+#define MM_COMM_BUFFER_HOB_GUID \
+  { 0x6c2a2520, 0x0131, 0x4aee, { 0xa7, 0x50, 0xcc, 0x38, 0x4a, 0xac, 0xe8, 0xc6 }}
+
+extern EFI_GUID  gEdkiiCommunicationBufferGuid;
+
+///
+/// This structure is allocated from memory of type EfiRuntimeServicesData.
+/// Since runtime memory types are converted to available memory when a legacy boot
+/// is performed, there are three parameters, ReturnStatus is the status from
+/// MM handler, ReturnBufferSize is the buffer size returned from MM handler,
+/// IsCommBufferValid is standed for if the software SMI with a valid communication buffer,
+/// If TRUE, it is standed for valid communication buffer,
+/// If FALSE, it is standed for invalid communication buffer.
+///
+typedef struct {
+  UINT64     ReturnStatus;
+  UINT64     ReturnBufferSize;
+  BOOLEAN    IsCommBufferValid;
+} COMMUNICATION_IN_OUT;
+
+///
+/// Communicate buffer structure that is used to share information between the MM IPL and
+/// the MM Core.  This structure is allocated from memory of type EfiRuntimeServicesData.
+/// Since runtime memory types are converted to available memory when a legacy boot
+/// is performed, the MM Core must not access any fields of this structure if a legacy
+/// boot is performed.  As a result, the MM IPL must create an event notification
+/// for the Legacy Boot event and notify the MM Core that a legacy boot is being
+/// performed.  The MM Core can then use this information to filter accesses to
+/// thos structure.
+///
+typedef struct {
+  ///
+  /// This field is used by the MM Communication Protocol to pass a buffer into
+  /// a software MMI handler and for the software MMI handler to pass a buffer back to
+  /// the caller of the MM Communication Protocol.
+  ///
+  EFI_PHYSICAL_ADDRESS    FixedCommBuffer;
+
+  ///
+  /// This field is used by the MM Communication Protocol to pass the size of a buffer,
+  /// in bytes, into a software MMI handler and for the software MMI handler to pass the
+  /// size, in bytes, of a buffer back to the caller of the MM Communication Protocol.
+  ///
+  UINT64                  FixedCommBufferSize;
+
+  ///
+  /// This field is used by the MM Communication Protocol to pass the return status from
+  /// a software MMI handler back to the caller of the MM Communication Protocol.
+  ///
+  EFI_PHYSICAL_ADDRESS    CommunicationInOut;
+} MM_COMM_BUFFER;
+
+#endif
