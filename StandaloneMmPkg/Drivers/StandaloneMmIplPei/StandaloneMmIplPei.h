f0a596bb7e7a6465e2fab6338cd0f9a4c21c4b06
Hongbin1 Zhang <hongbin1.zhang@intel.com>
2024-05-07 15:46:49 +0800

    StandaloneMmPkg/MmIpl: load MM Core and execute MM Core in MM RAM
    StandaloneMmIplPei will search the MM Core driver in all FV and relocate
    it to MM RAM, and enter MM Core entrypoint to run MM Core.
    
    Signed-off-by: Hongbin1 Zhang <hongbin1.zhang@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Jiaxin Wu <jiaxin.wu@intel.com>
    Cc: Wei6 Xu <wei6.xu@intel.com>
    Cc: Sami Mujawar <sami.mujawar@arm.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Supreeth Venkatesh <supreeth.venkatesh@arm.com>

--- StandaloneMmPkg/Drivers/StandaloneMmIplPei/StandaloneMmIplPei.h
+++ StandaloneMmPkg/Drivers/StandaloneMmIplPei/StandaloneMmIplPei.h
@@ -9,10 +9,16 @@
 #ifndef STANDALONE_MM_IPL_PEI_H_
 #define STANDALONE_MM_IPL_PEI_H_
 
+#include <StandaloneMm.h>
 #include <Guid/MmCommBuffer.h>
+#include <Guid/MmramMemoryReserve.h>
 #include <Library/HobLib.h>
 #include <Library/DebugLib.h>
 #include <Library/MemoryAllocationLib.h>
+#include <Library/PeiServicesLib.h>
 #include <Library/MmUnblockMemoryLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/PeCoffLib.h>
+#include <Library/CacheMaintenanceLib.h>
 
 #endif
