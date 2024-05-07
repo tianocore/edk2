575bfd5d923fb46fca0ee07774d5c5c6a1721e76
Hongbin1 Zhang <hongbin1.zhang@intel.com>
2024-05-07 10:48:24 +0800

    StandaloneMmPkg/MmIpl: build MM communication buffer HOB
    MM communication buffer HOB data is for StandaloneMm Core and
    MM communicate DXE driver.
    
    Signed-off-by: Hongbin1 Zhang <hongbin1.zhang@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Jiaxin Wu <jiaxin.wu@intel.com>
    Cc: Wei6 Xu <wei6.xu@intel.com>
    Cc: Sami Mujawar <sami.mujawar@arm.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Supreeth Venkatesh <supreeth.venkatesh@arm.com>

--- StandaloneMmPkg/Drivers/StandaloneMmIplPei/StandaloneMmIplPei.c
+++ StandaloneMmPkg/Drivers/StandaloneMmIplPei/StandaloneMmIplPei.c
@@ -9,6 +9,65 @@
 #include "StandaloneMmIplPei.h"
 
 /**
+  Build communication buffer HOB.
+
+  @return  MM_COMM_BUFFER     Pointer of MM communication buffer
+
+**/
+MM_COMM_BUFFER *
+MmIplBuildCommBufferHob (
+  VOID
+  )
+{
+  EFI_STATUS      Status;
+  MM_COMM_BUFFER  *MmCommBuffer;
+  UINT64          FixedCommBufferPages;
+
+  FixedCommBufferPages = PcdGet32 (PcdFixedCommBufferPages);
+
+  MmCommBuffer = BuildGuidHob (&gEdkiiCommunicationBufferGuid, sizeof (MM_COMM_BUFFER));
+  ASSERT (MmCommBuffer != NULL);
+
+  //
+  // Set fixed communicate buffer size
+  //
+  MmCommBuffer->FixedCommBufferSize = FixedCommBufferPages * EFI_PAGE_SIZE;
+
+  //
+  // Allocate runtime memory for fixed communicate buffer
+  //
+  MmCommBuffer->FixedCommBuffer = (EFI_PHYSICAL_ADDRESS)(UINTN)AllocateRuntimePages (FixedCommBufferPages);
+  if (MmCommBuffer->FixedCommBuffer == 0) {
+    DEBUG ((DEBUG_ERROR, "Fail to allocate fixed communication buffer\n"));
+    ASSERT (MmCommBuffer->FixedCommBuffer != 0);
+  }
+
+  //
+  // Build MM unblock memory region HOB for communication buffer
+  //
+  Status = MmUnblockMemoryRequest (MmCommBuffer->FixedCommBuffer, FixedCommBufferPages);
+  ASSERT_EFI_ERROR (Status);
+
+  //
+  // Allocate runtime memory for communication in and out parameters :
+  // ReturnStatus, ReturnBufferSize, IsCommBufferValid
+  //
+  MmCommBuffer->CommunicationInOut = (EFI_PHYSICAL_ADDRESS)(UINTN)AllocateRuntimePages (EFI_SIZE_TO_PAGES (sizeof (COMMUNICATION_IN_OUT)));
+  if (MmCommBuffer->CommunicationInOut == 0) {
+    DEBUG ((DEBUG_ERROR, "Fail to allocate communication in/out buffer\n"));
+    ASSERT (MmCommBuffer->CommunicationInOut != 0);
+  }
+
+  //
+  // Build MM unblock memory region HOB for communication in/out buffer
+  //
+  Status = MmUnblockMemoryRequest (MmCommBuffer->CommunicationInOut, EFI_SIZE_TO_PAGES (sizeof (COMMUNICATION_IN_OUT)));
+  ASSERT_EFI_ERROR (Status);
+
+  return MmCommBuffer;
+}
+
+/**
   The Entry Point for MM IPL at PEI stage.
 
   Load MM Core into MMRAM.
@@ -27,5 +86,13 @@ StandaloneMmIplPeiEntry (
   IN CONST EFI_PEI_SERVICES     **PeiServices
   )
 {
+  MM_COMM_BUFFER  *MmCommBuffer;
+
+  //
+  // Build communication buffer HOB.
+  //
+  MmCommBuffer = MmIplBuildCommBufferHob ();
+  ASSERT (MmCommBuffer != NULL);
+
   return EFI_SUCCESS;
 }
