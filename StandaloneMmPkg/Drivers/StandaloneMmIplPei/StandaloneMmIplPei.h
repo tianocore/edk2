cdd5b3067b5ebc582eb14a4373b16600f796a86b
Hongbin1 Zhang <hongbin1.zhang@intel.com>
2024-05-07 10:48:24 +0800

    StandaloneMmPkg/MmIpl: build MM communication buffer HOB
    MM communication buffer HOB data is for StandaloneMm Core and
    MM communicate DXE driver.
    
    Signed-off-by: Hongbin1 Zhang <hongbin1.zhang@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Jiaxin Wu <jiaxin.wu@intel.com>
    Cc: Wei6 Xu <wei6.xu@intel.com>
    Cc: Sami Mujawar <sami.mujawar@arm.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Supreeth Venkatesh <supreeth.venkatesh@arm.com>

--- StandaloneMmPkg/Drivers/StandaloneMmIplPei/StandaloneMmIplPei.h
+++ StandaloneMmPkg/Drivers/StandaloneMmIplPei/StandaloneMmIplPei.h
@@ -9,4 +9,10 @@
 #ifndef STANDALONE_MM_IPL_PEI_H_
 #define STANDALONE_MM_IPL_PEI_H_
 
+#include <Guid/MmCommBuffer.h>
+#include <Library/HobLib.h>
+#include <Library/DebugLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/MmUnblockMemoryLib.h>
+
 #endif
