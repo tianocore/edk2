f4a93689c855fd6f70e7ef6dfbfd31b691bed578
Du Lin <du.lin@intel.com>
2024-05-09 10:19:56 +0800

    MdeModulePkg: Update GCD attribute conversion to support SP attribute
    Add a new entry into GCD attribute conversion table to convert
    EFI_RESOURCE_ATTRIBUTE_SPECIAL_PURPOSE to EFI_MEMORY_SP.
    
    Signed-off-by: Du Lin <du.lin@intel.com>

--- MdeModulePkg/Core/Dxe/Gcd/Gcd.c
+++ MdeModulePkg/Core/Dxe/Gcd/Gcd.c
@@ -92,6 +92,7 @@ GCD_ATTRIBUTE_CONVERSION_ENTRY  mAttributeConversionTable[] = {
   { EFI_RESOURCE_ATTRIBUTE_TESTED,                  EFI_MEMORY_TESTED,        FALSE },
   { EFI_RESOURCE_ATTRIBUTE_PERSISTABLE,             EFI_MEMORY_NV,            TRUE  },
   { EFI_RESOURCE_ATTRIBUTE_MORE_RELIABLE,           EFI_MEMORY_MORE_RELIABLE, TRUE  },
+  { EFI_RESOURCE_ATTRIBUTE_SPECIAL_PURPOSE,         EFI_MEMORY_SP,            TRUE  },
   { 0,                                              0,                        FALSE }
 };
 
