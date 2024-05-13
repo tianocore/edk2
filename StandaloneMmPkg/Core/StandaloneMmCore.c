25bd54ef2b0255f054b1eaaf6e8a42695ef726ee
Wei6 Xu <wei6.xu@intel.com>
2024-05-14 02:54:44 +0800

    StandaloneMmPkg: Support using gEfiSmmSmramMemoryGuid to get MMRAM range
    Add the support using gEfiSmmSmramMemoryGuid to get MMRAM range for X86.
    If gEfiMmPeiMmramMemoryReserveGuid HOB is not found, then try to get the
    MMRAM ranges from gEfiSmmSmramMemoryGuid HOB.
    
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Sami Mujawar <sami.mujawar@arm.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Jiaxin Wu <jiaxin.wu@intel.com>
    Signed-off-by: Wei6 Xu <wei6.xu@intel.com>

--- StandaloneMmPkg/Core/StandaloneMmCore.c
+++ StandaloneMmPkg/Core/StandaloneMmCore.c
@@ -679,7 +679,10 @@ StandaloneMmMain (
   //
   MmramRangesHob = GetNextGuidHob (&gEfiMmPeiMmramMemoryReserveGuid, HobStart);
   if (MmramRangesHob == NULL) {
-    return EFI_UNSUPPORTED;
+    MmramRangesHob = GetFirstGuidHob (&gEfiSmmSmramMemoryGuid);
+    if (MmramRangesHob == NULL) {
+      return EFI_UNSUPPORTED;
+    }
   }
 
   MmramRangesHobData = GET_GUID_HOB_DATA (MmramRangesHob);
