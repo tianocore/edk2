437e94f446b97212e4f4edcb86ae1dc7c2543fb5
Jiaxin Wu <jiaxin.wu@intel.com>
2024-05-27 13:25:15 +0800

    SecurityPkg/Tcg: Correct buffer valid check func
    For SMM, the SMM Handlers is to validate the buffer outside MMRAM
    including the Primary & NonPrimary buffer.
    
    For MM, the MM Handlers do not need to validate the Primary buffer
    if it is passed from MmCore through the MmiHandler() parameter.
    Return TRUE directly in this case. But need to validate NonPrimary
    buffer that outside MMRAM.
    
    Signed-off-by: Jiaxin Wu <jiaxin.wu@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Hongbin1 Zhang <hongbin1.zhang@intel.com>
    Cc: Wei6 Xu <wei6.xu@intel.com>
    Cc: Dun Tan <dun.tan@intel.com>
    Cc: Yuanhao Xie <yuanhao.xie@intel.com>

--- SecurityPkg/Tcg/Tcg2Smm/Tcg2TraditionalMm.c
+++ SecurityPkg/Tcg/Tcg2Smm/Tcg2TraditionalMm.c
@@ -41,7 +41,9 @@ Tcg2NotifyMmReady (
 }
 
 /**
-  This function is an abstraction layer for implementation specific Mm buffer validation routine.
+  This function is for the Primary Buffer validation routine.
+  The Primary Buffer is the communication buffer requested from
+  Communicate protocol/PPI.
 
   @param Buffer  The buffer start address to be checked.
   @param Length  The buffer length to be checked.
@@ -50,7 +52,27 @@ Tcg2NotifyMmReady (
   @retval FALSE This buffer is not valid per processor architecture or overlap with SMRAM.
 **/
 BOOLEAN
-IsBufferOutsideMmValid (
+Tcg2IsPrimaryBufferValid (
+  IN EFI_PHYSICAL_ADDRESS  Buffer,
+  IN UINT64                Length
+  )
+{
+  return SmmIsBufferOutsideSmmValid (Buffer, Length);
+}
+
+/**
+  This function is for the NonPrimary Buffer validation routine.
+  The NonPrimary Buffer is the buffer which is pointed from the
+  communication buffer.
+
+  @param Buffer  The buffer start address to be checked.
+  @param Length  The buffer length to be checked.
+
+  @retval TRUE  This buffer is valid.
+  @retval FALSE This buffer is not valid.
+**/
+BOOLEAN
+Tcg2IsNonPrimaryBufferValid (
   IN EFI_PHYSICAL_ADDRESS  Buffer,
   IN UINT64                Length
   )
