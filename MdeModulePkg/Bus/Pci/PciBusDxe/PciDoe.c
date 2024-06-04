106cfada2e57d38c752470257f40a177a13668e8
Alistair Francis <alistair.francis@wdc.com>
2024-06-04 14:46:11 +1000

    MdeModulePkg/Bus/Pci: Support timeouts and error checking for DOE
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- MdeModulePkg/Bus/Pci/PciBusDxe/PciDoe.c
+++ MdeModulePkg/Bus/Pci/PciBusDxe/PciDoe.c
@@ -19,98 +19,220 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #define PCI_VENDOR_ID_PCI_SIG       0x01
 #define PCI_DOE_PROTOCOL_DISCOVERY  0x00
 
-VOID
+#define POLL_INTERVAL_US  500
+
+//
+// Section "6.30.2 Operation" describes a range of operations
+// that must complete within 1 second. The device could be busy
+// for any number of reasons, so let's use a 1 second
+// timeout to wait a reasonable amount of time, without stalling
+// if an error occurs.
+//
+// Timeout is in microseconds
+//
+#define TIMEOUT_MS  1000000
+
+/** Stalls the BSP for the minimum of POLL_INTERVAL_US and Timeout.
+
+   @param[in]  Timeout    The time limit in microseconds remaining for
+                          the DOE operation.
+
+   @retval     StallTime  Time of execution stall.
+**/
+STATIC
+UINTN
+CalculateAndStallInterval (
+  IN UINTN  Timeout
+  )
+{
+  UINTN  StallTime;
+
+  if ((Timeout < POLL_INTERVAL_US) && (Timeout != 0)) {
+    StallTime = Timeout;
+  } else {
+    StallTime = POLL_INTERVAL_US;
+  }
+
+  gBS->Stall (StallTime);
+
+  return StallTime;
+}
+
+/** Wait for the DOE Not Busy bit to be set
+
+  @param[in]  PciIoDevice    A pointer to the EFI_PCI_IO_PROTOCOL instance.
+
+  @retval EFI_SUCCESS Source was updated to support Handler.
+  @retval EFI_UNSUPPORTED  Hardware could not be programmed.
+  @retval EFI_TIMEOUT  Did not complete in timeout period.
+**/
+EFI_STATUS
 DoeWaitForNotBusy (
   IN PCI_IO_DEVICE  *PciIoDevice
   )
 {
+  EFI_STATUS                  Status;
   PCI_EXPRESS_REG_DOE_STATUS  DoeStatus;
+  UINTN                       Timeout;
 
   //
   // Read DOE Status
   //
-  PciIoDevice->PciIo.Pci.Read (
-                           &PciIoDevice->PciIo,
-                           EfiPciIoWidthUint32,
-                           PciIoDevice->DoeCapabilityOffset + PCI_EXPRESS_REG_DOE_STATUS_OFFSET,
-                           1,
-                           &DoeStatus
-                           );
+  Status = PciIoDevice->PciIo.Pci.Read (
+                                    &PciIoDevice->PciIo,
+                                    EfiPciIoWidthUint32,
+                                    PciIoDevice->DoeCapabilityOffset + PCI_EXPRESS_REG_DOE_STATUS_OFFSET,
+                                    1,
+                                    &DoeStatus
+                                    );
+  if (EFI_ERROR (Status)) {
+    return EFI_UNSUPPORTED;
+  }
+
+  Timeout = TIMEOUT_MS;
 
   while (DoeStatus.Bits.DoeBusy) {
-    PciIoDevice->PciIo.Pci.Read (
-                             &PciIoDevice->PciIo,
-                             EfiPciIoWidthUint32,
-                             PciIoDevice->DoeCapabilityOffset + PCI_EXPRESS_REG_DOE_STATUS_OFFSET,
-                             1,
-                             &DoeStatus
-                             );
+    Status = PciIoDevice->PciIo.Pci.Read (
+                                      &PciIoDevice->PciIo,
+                                      EfiPciIoWidthUint32,
+                                      PciIoDevice->DoeCapabilityOffset + PCI_EXPRESS_REG_DOE_STATUS_OFFSET,
+                                      1,
+                                      &DoeStatus
+                                      );
+    if (EFI_ERROR (Status)) {
+      return EFI_UNSUPPORTED;
+    }
+
+    if (Timeout == 0) {
+      return EFI_TIMEOUT;
+    }
+
+    Timeout -= CalculateAndStallInterval (Timeout);
   }
+
+  return EFI_SUCCESS;
 }
 
-VOID
+/** Set the DOE Go bit
+
+  @param[in]  PciIoDevice    A pointer to the EFI_PCI_IO_PROTOCOL instance.
+
+  @retval EFI_SUCCESS Source was updated to support Handler.
+  @retval EFI_UNSUPPORTED  Hardware could not be programmed.
+**/
+EFI_STATUS
 DoeSetGoBit (
   IN PCI_IO_DEVICE  *PciIoDevice
   )
 {
+  EFI_STATUS                   Status;
   PCI_EXPRESS_REG_DOE_CONTROL  DoeControl;
 
   //
   // Read DOE Control
   //
-  PciIoDevice->PciIo.Pci.Read (
-                           &PciIoDevice->PciIo,
-                           EfiPciIoWidthUint32,
-                           PciIoDevice->DoeCapabilityOffset + PCI_EXPRESS_REG_DOE_CONTROL_OFFSET,
-                           1,
-                           &DoeControl
-                           );
+  Status = PciIoDevice->PciIo.Pci.Read (
+                                    &PciIoDevice->PciIo,
+                                    EfiPciIoWidthUint32,
+                                    PciIoDevice->DoeCapabilityOffset + PCI_EXPRESS_REG_DOE_CONTROL_OFFSET,
+                                    1,
+                                    &DoeControl
+                                    );
+  if (EFI_ERROR (Status)) {
+    return EFI_UNSUPPORTED;
+  }
 
   // Set Go bit
   DoeControl.Bits.DoeGo = 1;
 
   DEBUG ((DEBUG_INFO, "Setting DOE Go bit\n"));
 
-  PciIoDevice->PciIo.Pci.Write (
-                           &PciIoDevice->PciIo,
-                           EfiPciIoWidthUint32,
-                           PciIoDevice->DoeCapabilityOffset + PCI_EXPRESS_REG_DOE_CONTROL_OFFSET,
-                           1,
-                           &DoeControl
-                           );
+  Status = PciIoDevice->PciIo.Pci.Write (
+                                    &PciIoDevice->PciIo,
+                                    EfiPciIoWidthUint32,
+                                    PciIoDevice->DoeCapabilityOffset + PCI_EXPRESS_REG_DOE_CONTROL_OFFSET,
+                                    1,
+                                    &DoeControl
+                                    );
+  if (EFI_ERROR (Status)) {
+    return EFI_UNSUPPORTED;
+  }
+
+  return EFI_SUCCESS;
 }
 
-VOID
+/** Wait for the DOE Data Object Ready (DOR) bit to be set
+
+  @param[in]  PciIoDevice    A pointer to the EFI_PCI_IO_PROTOCOL instance.
+
+  @retval EFI_SUCCESS Source was updated to support Handler.
+  @retval EFI_UNSUPPORTED  Hardware could not be programmed.
+  @retval EFI_TIMEOUT  Did not complete in timeout period.
+**/
+EFI_STATUS
 DoeWaitForStatusDor (
   IN PCI_IO_DEVICE  *PciIoDevice
   )
 {
+  EFI_STATUS                  Status;
   PCI_EXPRESS_REG_DOE_STATUS  DoeStatus;
+  UINTN                       Timeout;
 
   //
   // Read DOE Status
   //
-  PciIoDevice->PciIo.Pci.Read (
-                           &PciIoDevice->PciIo,
-                           EfiPciIoWidthUint32,
-                           PciIoDevice->DoeCapabilityOffset + PCI_EXPRESS_REG_DOE_STATUS_OFFSET,
-                           1,
-                           &DoeStatus
-                           );
+  Status = PciIoDevice->PciIo.Pci.Read (
+                                    &PciIoDevice->PciIo,
+                                    EfiPciIoWidthUint32,
+                                    PciIoDevice->DoeCapabilityOffset + PCI_EXPRESS_REG_DOE_STATUS_OFFSET,
+                                    1,
+                                    &DoeStatus
+                                    );
+  if (EFI_ERROR (Status)) {
+    return EFI_UNSUPPORTED;
+  }
 
   DEBUG ((DEBUG_INFO, "Waiting for DOE Data Object Read\n"));
 
+  Timeout = TIMEOUT_MS;
+
   while (!DoeStatus.Bits.DataObjectReady) {
-    PciIoDevice->PciIo.Pci.Read (
-                             &PciIoDevice->PciIo,
-                             EfiPciIoWidthUint32,
-                             PciIoDevice->DoeCapabilityOffset + PCI_EXPRESS_REG_DOE_STATUS_OFFSET,
-                             1,
-                             &DoeStatus
-                             );
+    Status = PciIoDevice->PciIo.Pci.Read (
+                                      &PciIoDevice->PciIo,
+                                      EfiPciIoWidthUint32,
+                                      PciIoDevice->DoeCapabilityOffset + PCI_EXPRESS_REG_DOE_STATUS_OFFSET,
+                                      1,
+                                      &DoeStatus
+                                      );
+    if (EFI_ERROR (Status)) {
+      return EFI_UNSUPPORTED;
+    }
+
+    if (Timeout == 0) {
+      return EFI_TIMEOUT;
+    }
+
+    Timeout -= CalculateAndStallInterval (Timeout);
   }
+
+  return EFI_SUCCESS;
 }
 
+/**
+  Performs a send operation on a DOE Mailbox
+
+  @param  This                  A pointer to the EFI_PCI_IO_PROTOCOL instance.
+  @param  Count                 The number of bytes to use in the write operation or the maximum number
+                                of bytes to read for a read operation.
+                                For read operations it is set to the number of bytes read.
+  @param  Buffer                For read operations, the destination buffer to store the results. For write
+                                operations, the source buffer to write data from.
+
+  @retval EFI_SUCCESS           The operation on the PCI controller's attributes was completed.
+  @retval EFI_INVALID_PARAMETER One or more parameters are invalid.
+  @retval EFI_UNSUPPORTED       DOE is not supported
+
+**/
 EFI_STATUS
 EFIAPI
 PciIoDoeSend (
@@ -127,7 +249,10 @@ PciIoDoeSend (
 
   PciIoDevice = PCI_IO_DEVICE_FROM_PCI_DOE_THIS (This);
 
-  DoeWaitForNotBusy (PciIoDevice);
+  Status = DoeWaitForNotBusy (PciIoDevice);
+  if (EFI_ERROR (Status)) {
+    return EFI_UNSUPPORTED;
+  }
 
   Index      = 0;
   Operations = Count / 4;
@@ -150,11 +275,29 @@ PciIoDoeSend (
   }
 
   // Set DOE Go bit
-  DoeSetGoBit (PciIoDevice);
+  Status = DoeSetGoBit (PciIoDevice);
+  if (EFI_ERROR (Status)) {
+    return EFI_UNSUPPORTED;
+  }
 
   return Status;
 }
 
+/**
+  Performs an receive operation on a DOE Mailbox
+
+  @param  This                  A pointer to the EFI_PCI_IO_PROTOCOL instance.
+  @param  Count                 The number of bytes to use in the write operation or the maximum number
+                                of bytes to read for a read operation.
+                                For read operations it is set to the number of bytes read.
+  @param  Buffer                For read operations, the destination buffer to store the results. For write
+                                operations, the source buffer to write data from.
+
+  @retval EFI_SUCCESS           The operation on the PCI controller's attributes was completed.
+  @retval EFI_INVALID_PARAMETER One or more parameters are invalid.
+  @retval EFI_UNSUPPORTED       DOE is not supported
+
+**/
 EFI_STATUS
 EFIAPI
 PciIoDoeRecieve (
@@ -172,7 +315,10 @@ PciIoDoeRecieve (
   PciIoDevice = PCI_IO_DEVICE_FROM_PCI_DOE_THIS (This);
 
   // Wait for a response
-  DoeWaitForStatusDor (PciIoDevice);
+  Status = DoeWaitForStatusDor (PciIoDevice);
+  if (EFI_ERROR (Status)) {
+    return EFI_UNSUPPORTED;
+  }
 
   //
   // Read DOE Status
@@ -219,6 +365,18 @@ PciIoDoeRecieve (
   return Status;
 }
 
+/**
+  Check if a specific Vendor ID and Data Object Type is supported
+
+  @param  This                  A pointer to the EFI_PCI_IO_PROTOCOL instance.
+  @param  VendorId              The Vendor ID to check against
+  @param  Type                  The Data Object Type to check against
+
+  @retval EFI_SUCCESS           The operation on the PCI controller's attributes was completed.
+  @retval EFI_INVALID_PARAMETER One or more parameters are invalid.
+  @retval EFI_UNSUPPORTED       DOE is not supported or the VendorId and Type are not supported
+
+**/
 EFI_STATUS
 EFIAPI
 PciIoDoeTypeCheck (
@@ -237,7 +395,10 @@ PciIoDoeTypeCheck (
 
   PciIoDevice = PCI_IO_DEVICE_FROM_PCI_DOE_THIS (This);
 
-  DoeWaitForNotBusy (PciIoDevice);
+  Status = DoeWaitForNotBusy (PciIoDevice);
+  if (EFI_ERROR (Status)) {
+    return EFI_UNSUPPORTED;
+  }
 
   Packet.Header1 = (PCI_DOE_PROTOCOL_DISCOVERY << DOE_HEADER1_OFST_TYPE) | (PCI_VENDOR_ID_PCI_SIG << DOE_HEADER1_OFST_VID);
   Packet.Header2 = 3 << DOE_HEADER2_OFST_LEN;
@@ -258,15 +419,20 @@ PciIoDoeTypeCheck (
     }
 
     // Set DOE Go bit
-    DoeSetGoBit (PciIoDevice);
+    Status = DoeSetGoBit (PciIoDevice);
+    if (EFI_ERROR (Status)) {
+      return EFI_UNSUPPORTED;
+    }
 
     // Wait for a response
-    DoeWaitForStatusDor (PciIoDevice);
+    Status = DoeWaitForStatusDor (PciIoDevice);
+    if (EFI_ERROR (Status)) {
+      return EFI_UNSUPPORTED;
+    }
 
     // Read response
     Count  = 2 * 4;
     Status = PciIoDoeRecieve (This, &Count, &Response);
-
     if (EFI_ERROR (Status)) {
       return EFI_UNSUPPORTED;
     }
