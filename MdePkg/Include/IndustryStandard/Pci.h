22fd550223e6a7a607ff91e7a47799810eabfc26
Alistair Francis <alistair.francis@wdc.com>
2024-06-07 14:39:50 +1000

    MdePkg/Include: PciExpress60: Initial commit
    Initial commit of the PCIe 6.0 specification header file. This is used
    to define the EFI_PCIE_CAPABILITY_ID_DOE macro.
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- MdePkg/Include/IndustryStandard/Pci.h
+++ MdePkg/Include/IndustryStandard/Pci.h
@@ -9,7 +9,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #ifndef _PCI_H_
 #define _PCI_H_
 
-#include <IndustryStandard/PciExpress50.h>
+#include <IndustryStandard/PciExpress60.h>
 #include <IndustryStandard/PciCodeId.h>
 
 #endif
