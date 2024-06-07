1f270e786fcb06b4af63dad48f536971f384d492
Ard Biesheuvel <ardb@kernel.org>
2024-06-07 10:23:14 +0200

    ArmPkg: Revert "Allow SMC/HVC monitor conduit to be specified at runtime"
    This reverts commit 32460bb5b17b5caec29037a4e9462ca149a190e6, which is
    no longer needed as ArmVirtQemu now has its own special implementation
    for ArmMonitorLib.
    
    Continuous-integration-options: PatchCheck.ignore-multi-package
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- ArmPkg/Library/ArmMonitorLib/ArmMonitorLib.c
+++ ArmPkg/Library/ArmMonitorLib/ArmMonitorLib.c
@@ -26,7 +26,7 @@ ArmMonitorCall (
   IN OUT ARM_MONITOR_ARGS  *Args
   )
 {
-  if (PcdGetBool (PcdMonitorConduitHvc)) {
+  if (FeaturePcdGet  (PcdMonitorConduitHvc)) {
     ArmCallHvc ((ARM_HVC_ARGS *)Args);
   } else {
     ArmCallSmc ((ARM_SMC_ARGS *)Args);
