ccdfb786ca3211a700c783c52fae2a0aa0c7acba
Dun Tan <dun.tan@intel.com>
2024-06-12 10:59:53 +0800

    StandaloneMmPkg: Add a new MmCommunicationDxe driver
    Add a new MmCommunicationDxe driver. The driver is to:
      1.Install gEfiMmCommunication2ProtocolGuid
      2.Install gEfiMmCommunicationProtocolGuid
      3.Create the notifications of some protocols and event that
        the Standalone Mm requires
    
    Signed-off-by: Dun Tan <dun.tan@intel.com>

---
+++ StandaloneMmPkg/Drivers/MmCommunicationDxe/MmCommunicationDxe.h
@@ -0,0 +1,11 @@
+/** @file
+
+  Copyright (c) 2024, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef MM_COMMUNICATION_DXE_H_
+#define MM_COMMUNICATION_DXE_H_
+
+#endif
