573b99353d743af393dbd1c1cdf5c1255c0bd1da
Taylor Beebe <taylor.d.beebe@gmail.com>
2024-06-14 13:29:59 -0700

    MdePkg: Create Stack Check Null Libs
    Add Null libs for Stack Check and Stack Check Failure Hook Lib that
    allow a platform to opt out of stack checks and the stack check failure
    hook lib.
    
    StackCheckLib allows implementation (or in this case null implementation)
    of stack checks on binaries.
    
    StackCheckFailureHookLib lets a platform do custom functionality when a
    stack check failure occurs (such as log it to a platform defined
    mechanism). The null lib simply returns.
    
    Signed-off-by: Oliver Smith-Denny <osde@linux.microsoft.com>

---
+++ MdePkg/Include/Library/StackCheckFailureHookLib.h
@@ -0,0 +1,27 @@
+/** @file
+  Library provides a hook called when a stack cookie check fails.
+
+  Copyright (c) Microsoft Corporation.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef STACK_COOKIE_FAILURE_HOOK_LIB_H_
+#define STACK_COOKIE_FAILURE_HOOK_LIB_H_
+
+#include <Uefi.h>
+
+/**
+  This function gets called when a compiler generated stack cookie fails. This allows a platform to hook this
+  call and perform any required actions/telemetry at that time.
+
+  @param  FailureAddress  The address of the function that failed the stack cookie check.
+
+**/
+NO_STACK_COOKIE
+VOID
+EFIAPI
+StackCheckFailureHook (
+  VOID  *FailureAddress
+  );
+
+#endif
