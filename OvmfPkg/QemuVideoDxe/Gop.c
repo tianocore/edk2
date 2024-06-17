3abc07402072a12f4596520094d1a493fe25fe7c
Ard Biesheuvel <ardb@kernel.org>
2024-06-17 17:07:41 +0200

    OvmfPkg/QemuVideoDxe: add feature PCD to remap framebuffer W/C
    Some platforms (such as SBSA-QEMU on recent builds of the emulator) only
    tolerate misaligned accesses to normal memory, and raise alignment
    faults on such accesses to device memory, which is the default for PCIe
    MMIO BARs.
    
    When emulating a PCIe graphics controller, the framebuffer is typically
    exposed via a MMIO BAR, while the disposition of the region is closer to
    memory (no side effects on reads or writes, except for the changing
    picture on the screen; direct random access to any pixel in the image).
    
    In order to permit the use of such controllers on platforms that only
    tolerate these types of accesses for normal memory, it is necessary to
    remap the memory. Use the DXE services to set the desired capabilities
    and attributes.
    
    Hide this behavior under a feature PCD so only platforms that really
    need it can enable it. (OVMF on x86 has no need for this)
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- OvmfPkg/QemuVideoDxe/Gop.c
+++ OvmfPkg/QemuVideoDxe/Gop.c
@@ -9,6 +9,8 @@
 
 #include "Qemu.h"
 
+#include <Library/DxeServicesTableLib.h>
+
 STATIC
 VOID
 QemuVideoCompleteModeInfo (
@@ -54,6 +56,7 @@ QemuVideoCompleteModeData (
   EFI_GRAPHICS_OUTPUT_MODE_INFORMATION  *Info;
   EFI_ACPI_ADDRESS_SPACE_DESCRIPTOR     *FrameBufDesc;
   QEMU_VIDEO_MODE_DATA                  *ModeData;
+  EFI_STATUS                            Status;
 
   ModeData = &Private->ModeData[Mode->Mode];
   Info     = Mode->Info;
@@ -78,6 +81,22 @@ QemuVideoCompleteModeData (
     Mode->FrameBufferBase,
     (UINT64)Mode->FrameBufferSize
     ));
+
+  if (FeaturePcdGet (PcdRemapFrameBufferWriteCombine)) {
+    Status = gDS->SetMemorySpaceCapabilities (
+                    FrameBufDesc->AddrRangeMin,
+                    FrameBufDesc->AddrLen,
+                    EFI_MEMORY_UC | EFI_MEMORY_WC | EFI_MEMORY_XP
+                    );
+    ASSERT_EFI_ERROR (Status);
+
+    Status = gDS->SetMemorySpaceAttributes (
+                    FrameBufDesc->AddrRangeMin,
+                    FrameBufDesc->AddrLen,
+                    EFI_MEMORY_WC | EFI_MEMORY_XP
+                    );
+    ASSERT_EFI_ERROR (Status);
+  }
 
   FreePool (FrameBufDesc);
   return EFI_SUCCESS;
