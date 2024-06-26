2feedc9847438c4af7d060a3d9423956ae3a5f56
Jiaxin Wu <jiaxin.wu@intel.com>
2024-06-26 11:58:49 +0800

    UefiCpuPkg/PiSmmCpuDxeSmm: Add empty .c for MM CPU specific impl
    This patch adds the empty .c for MM CPU specific implementation：
    NonMmramMapStandaloneMm.c
    PiSmmCpuStandaloneMm.c
    
    Signed-off-by: Jiaxin Wu <jiaxin.wu@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Rahul Kumar <rahul1.kumar@intel.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Dun Tan <dun.tan@intel.com>
    Cc: Hongbin1 Zhang <hongbin1.zhang@intel.com>
    Cc: Wei6 Xu <wei6.xu@intel.com>
    Cc: Yuanhao Xie <yuanhao.xie@intel.com>

---
+++ UefiCpuPkg/PiSmmCpuDxeSmm/PiSmmCpuStandaloneMm.c
@@ -0,0 +1,10 @@
+/** @file
+Agent Module to load other modules to deploy MM Entry Vector for X86 CPU.
+
+Copyright (c) 2024, Intel Corporation. All rights reserved.<BR>
+
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include "PiSmmCpuCommon.h"
