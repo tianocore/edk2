6f81a460955b41590e9994bffb2bbe1e880f8601
Jiaxin Wu <jiaxin.wu@intel.com>
2024-06-26 15:43:52 +0800

    UefiCpuPkg/PiSmmCpuDxeSmm: Differentiate PerformRemainingTasks
    For MM:
    SMRAM & PageTable itself & SMM Paging State shall be configured
    once the gEdkiiPiMmMemoryAttributesTableGuid is installed by
    SMM core. It will happen after MmIpl.Entrypoint.
    PerformRemainingTasks will be called before MmIpl.Entrypoint
    exit.
    
    For SMM:
    SMRAM & PageTable itself & SMM Paging State are still
    configured in the first SMI when SMM ready to lock happen.
    
    So, this patch is to differentiate PerformRemainingTasks for MM
    and SMM.
    
    Signed-off-by: Jiaxin Wu <jiaxin.wu@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Rahul Kumar <rahul1.kumar@intel.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Dun Tan <dun.tan@intel.com>
    Cc: Hongbin1 Zhang <hongbin1.zhang@intel.com>
    Cc: Wei6 Xu <wei6.xu@intel.com>
    Cc: Yuanhao Xie <yuanhao.xie@intel.com>

--- UefiCpuPkg/PiSmmCpuDxeSmm/PiSmmCpuStandaloneMm.c
+++ UefiCpuPkg/PiSmmCpuDxeSmm/PiSmmCpuStandaloneMm.c
@@ -15,6 +15,63 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 //
 const BOOLEAN  mIsStandaloneMm = TRUE;
 
+//
+// RemainingTasks Done flag
+//
+BOOLEAN  mRemainingTasksDone = FALSE;
+
+/**
+  Perform the remaining tasks.
+
+**/
+VOID
+PerformRemainingTasks (
+  VOID
+  )
+{
+  EDKII_PI_SMM_MEMORY_ATTRIBUTES_TABLE  *MemoryAttributesTable;
+
+  if (!mRemainingTasksDone) {
+    PERF_FUNCTION_BEGIN ();
+
+    //
+    // gEdkiiPiSmmMemoryAttributesTableGuid should have been published after SmmCore dispatched all MM drivers (MmDriverDispatchHandler).
+    // Note: gEdkiiPiSmmMemoryAttributesTableGuid is not always installed since it depends on
+    //       the memory protection attribute setting in MM Core.
+    //
+    SmmGetSystemConfigurationTable (&gEdkiiPiSmmMemoryAttributesTableGuid, (VOID **)&MemoryAttributesTable);
+
+    //
+    // Set critical region attribute in page table according to the MemoryAttributesTable
+    //
+    if (MemoryAttributesTable != NULL) {
+      SetMemMapAttributes (MemoryAttributesTable);
+    }
+
+    if (IsRestrictedMemoryAccess ()) {
+      //
+      // Set page table itself to be read-only
+      //
+      SetPageTableAttributes ();
+    }
+
+    //
+    // Measure performance of SmmCpuFeaturesCompleteSmmReadyToLock() from caller side
+    // as the implementation is provided by platform.
+    //
+    PERF_START (NULL, "SmmCompleteReadyToLock", NULL, 0);
+    SmmCpuFeaturesCompleteSmmReadyToLock ();
+    PERF_END (NULL, "SmmCompleteReadyToLock", NULL, 0);
+
+    //
+    // Mark RemainingTasks Done flag to TRUE
+    //
+    mRemainingTasksDone = TRUE;
+
+    PERF_FUNCTION_END ();
+  }
+}
+
 /**
   To get system port address of the SMI Command Port.
 
