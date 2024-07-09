cbe386269069c3e73ad4c6c634fb6f41b00df15b
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2024-07-09 18:13:31 +0300

    UefiPayloadPkg/BlParseLib: Add parsing of firmware info
    The implementation is for CbParseLib, SblParseLib returns an error.
    
    coreboot's CB_TAG_FW_INFO is a machine-friendly version of a system
    firmware component.  A component is identified by a GUID.  This is meant
    to be a source of information for ESRT.
    
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- UefiPayloadPkg/Library/CbParseLib/CbParseLib.c
+++ UefiPayloadPkg/Library/CbParseLib/CbParseLib.c
@@ -638,3 +638,42 @@ ParseSmmStoreInfo (
 
   return RETURN_SUCCESS;
 }
+
+/**
+  Parse firmware information passed in by bootloader
+
+  @param  Guid     Kind of the firmware.
+  @param  Version  Current version.
+  @param  Lsv      Lowest supported version.
+  @param  Size     Firmware size in bytes.
+
+  @retval RETURN_INVALID_PARAMETER  At least one of the parameters is NULL.
+  @retval RETURN_SUCCESS            Successfully parsed capsules.
+  @retval RETURN_NOT_FOUND          The information is missing.
+**/
+RETURN_STATUS
+EFIAPI
+ParseFwInfo (
+  OUT EFI_GUID  *Guid,
+  OUT UINT32    *Version,
+  OUT UINT32    *Lsv,
+  OUT UINT32    *Size
+  )
+{
+  struct lb_efi_fw_info  *FwInfo;
+
+  if ((Guid == NULL) || (Version == NULL) || (Lsv == NULL) || (Size == NULL)) {
+    return RETURN_INVALID_PARAMETER;
+  }
+
+  FwInfo = FindCbTag (CB_TAG_FW_INFO);
+  if (FwInfo == NULL) {
+    return RETURN_NOT_FOUND;
+  }
+
+  CopyMem (Guid, &FwInfo->guid, sizeof (*Guid));
+  *Version = FwInfo->version;
+  *Lsv     = FwInfo->lowest_supported_version;
+  *Size    = FwInfo->fw_size;
+  return RETURN_SUCCESS;
+}
