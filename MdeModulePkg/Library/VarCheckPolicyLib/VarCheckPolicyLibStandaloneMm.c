e544cbefc7b752b3a5443b1bce84d0e1a1a4898e
Ashwin Veeraiah <ashwin.veeraiah@hp.com>
2024-07-12 15:54:07 -0500

    revert merge
    revert merge
    
    Signed-off-by: Ashwin Veeraiah <ashwin.veeraiah@hp.com>

--- MdeModulePkg/Library/VarCheckPolicyLib/VarCheckPolicyLibStandaloneMm.c
+++ MdeModulePkg/Library/VarCheckPolicyLib/VarCheckPolicyLibStandaloneMm.c
@@ -1,6 +1,7 @@
 /** @file -- VarCheckPolicyLibStandaloneMm.c
 This is an instance of a VarCheck lib constructor for Standalone MM.
 
+Copyright (c) 2024, Intel Corporation. All rights reserved.<BR>
 Copyright (c) Microsoft Corporation. All rights reserved.
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -31,20 +32,20 @@ VarCheckPolicyLibStandaloneConstructor (
 }
 
 /**
-  This function is wrapper function to validate the buffer.
+  This function is wrapper function to validate the Primary Buffer (CommBuffer).
 
   @param Buffer  The buffer start address to be checked.
   @param Length  The buffer length to be checked.
 
-  @retval TRUE  This buffer is valid per processor architectureand not overlap with MMRAM.
-  @retval FALSE This buffer is not valid per processor architecture or overlap with MMRAM.
+  @retval TRUE  This buffer is valid.
+  @retval FALSE This buffer is not valid.
 **/
 BOOLEAN
 EFIAPI
-VarCheckPolicyIsBufferOutsideValid (
+VarCheckPolicyIsPrimaryBufferValid (
   IN EFI_PHYSICAL_ADDRESS  Buffer,
   IN UINT64                Length
   )
 {
-  return MmIsBufferOutsideMmValid (Buffer, Length);
+  return TRUE;
 }
