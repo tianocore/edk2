d6ce97e5fe2cd56a9f2899bf79ce6a7389aebfc6
Sunil V L <sunilvl@ventanamicro.com>
2024-07-23 20:39:33 +0530

    DynamicTablesPkg: RISC-V: Add AcpiSsdtPlicAplicLib library
    RISC-V needs PLIC or APLIC devices in the namespace as well. So, add a
    library to create these devices.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

--- DynamicTablesPkg/Include/AcpiTableGenerator.h
+++ DynamicTablesPkg/Include/AcpiTableGenerator.h
@@ -99,6 +99,7 @@ typedef enum StdAcpiTableId {
   EStdAcpiTableIdSsdtCmn600,                    ///< SSDT Cmn-600 Generator
   EStdAcpiTableIdSsdtCpuTopology,               ///< SSDT Cpu Topology
   EStdAcpiTableIdSsdtPciExpress,                ///< SSDT Pci Express Generator
+  EStdAcpiTableIdSsdtPlicAplic,                 ///< SSDT Plic/Aplic Generator
   EStdAcpiTableIdPcct,                          ///< PCCT Generator
   EStdAcpiTableIdTpm2,                          ///< TPM2 Generator
   EStdAcpiTableIdRhct,                          ///< RHCT Generator
