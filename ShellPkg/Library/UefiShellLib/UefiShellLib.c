e11a912aa3bd6b3ad69bb90f36f6860de33d846a
Tormod Volden <debian.tormod@gmail.com>
2024-07-23 23:51:13 +0200

    ShellPkg/UefiShellLib: Correct check for empty string
    StrSize() will never return zero since it counts the terminating NULL
    character.
    
    An empty string will have the storage size of the terminator.
    
    Signed-off-by: Tormod Volden <debian.tormod@gmail.com>

--- ShellPkg/Library/UefiShellLib/UefiShellLib.c
+++ ShellPkg/Library/UefiShellLib/UefiShellLib.c
@@ -4012,7 +4012,7 @@ InternalShellStrHexToUint64 (
   UINT64   Result;
   BOOLEAN  LeadingZero;
 
-  if ((String == NULL) || (StrSize (String) == 0) || (Value == NULL)) {
+  if ((String == NULL) || (StrSize (String) == sizeof (*String)) || (Value == NULL)) {
     return (EFI_INVALID_PARAMETER);
   }
 
@@ -4116,7 +4116,7 @@ InternalShellStrDecimalToUint64 (
 {
   UINT64  Result;
 
-  if ((String == NULL) || (StrSize (String) == 0) || (Value == NULL)) {
+  if ((String == NULL) || (StrSize (String) == sizeof (*String)) || (Value == NULL)) {
     return (EFI_INVALID_PARAMETER);
   }
 
