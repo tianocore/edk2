microsoft/mu_basecore

MdeModulePkg: Add the UT_CLEANUP_ASSERT_* macros to UnitTestLib.h

MdePkg/Include/Library/UnitTestLib.h
@@ -441,6 +441,61 @@ SaveFrameworkState (
     return UNIT_TEST_ERROR_TEST_FAILED;                                                         \
   }
 
+// MU_CHANGE [BEGIN] - Add test asserts that allow cleanup
+
+/**
+  The following macros are almost identical to the UT_ASSERT_* macros, but rather than returning
+  immediately, they jump to a `Cleanup` label for late memory cleanup before exiting.
+**/
+#define UT_CLEANUP_ASSERT_TRUE(Expression)                                                         \
+  if(!UnitTestAssertTrue ((Expression), __FUNCTION__, DEBUG_LINE_NUMBER, __FILE__, #Expression)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                      \
+    goto Cleanup;                                                                                  \
+  }
+
+#define UT_CLEANUP_ASSERT_FALSE(Expression)                                                         \
+  if(!UnitTestAssertFalse ((Expression), __FUNCTION__, DEBUG_LINE_NUMBER, __FILE__, #Expression)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                       \
+    goto Cleanup;                                                                                   \
+  }
+
+#define UT_CLEANUP_ASSERT_EQUAL(ValueA, ValueB)                                                                                \
+  if(!UnitTestAssertEqual ((UINT64)(ValueA), (UINT64)(ValueB), __FUNCTION__, DEBUG_LINE_NUMBER, __FILE__, #ValueA, #ValueB)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                                                  \
+    goto Cleanup;                                                                                                              \
+  }
+
+#define UT_CLEANUP_ASSERT_MEM_EQUAL(BufferA, BufferB, Length)                                                                                                      \
+  if(!UnitTestAssertMemEqual ((VOID *)(UINTN)(BufferA), (VOID *)(UINTN)(BufferB), (UINTN)Length, __FUNCTION__, DEBUG_LINE_NUMBER, __FILE__, #BufferA, #BufferB)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                                                                                      \
+    goto Cleanup;                                                                                                                                                  \
+  }
+
+#define UT_CLEANUP_ASSERT_NOT_EQUAL(ValueA, ValueB)                                                                               \
+  if(!UnitTestAssertNotEqual ((UINT64)(ValueA), (UINT64)(ValueB), __FUNCTION__, DEBUG_LINE_NUMBER, __FILE__, #ValueA, #ValueB)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                                                     \
+    goto Cleanup;                                                                                                                 \
+  }
+
+#define UT_CLEANUP_ASSERT_NOT_EFI_ERROR(Status)                                                   \
+  if(!UnitTestAssertNotEfiError ((Status), __FUNCTION__, DEBUG_LINE_NUMBER, __FILE__, #Status)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                     \
+    goto Cleanup;                                                                                 \
+  }
+
+#define UT_CLEANUP_ASSERT_STATUS_EQUAL(Status, Expected)                                                      \
+  if(!UnitTestAssertStatusEqual ((Status), (Expected), __FUNCTION__, DEBUG_LINE_NUMBER, __FILE__, #Status)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                                 \
+    goto Cleanup;                                                                                             \
+  }
+
+#define UT_CLEANUP_ASSERT_NOT_NULL(Pointer)                                                     \
+  if(!UnitTestAssertNotNull ((Pointer), __FUNCTION__, DEBUG_LINE_NUMBER, __FILE__, #Pointer)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                   \
+    goto Cleanup;                                                                               \
+  }
+// MU_CHANGE [END] - Add test asserts that allow cleanup
+
 /**
   This macro uses the framework assertion logic to check whether a function call
   triggers an ASSERT() condition.  The BaseLib SetJump()/LongJump() services

tianocore/edk2

Signed-off-by: Vivian Nowka-Keane <vnowkakeane@linux.microsoft.com>

MdePkg/Include/Library/UnitTestLib.h
@@ -442,6 +442,58 @@ SaveFrameworkState (
   }
 
 /**
+  The following macros are almost identical to the UT_ASSERT_* macros, but rather than returning
+  immediately, they jump to a `Cleanup` label for late memory cleanup before exiting.
+**/
+#define UT_CLEANUP_ASSERT_TRUE(Expression)                                                         \
+  if(!UnitTestAssertTrue ((Expression), __FUNC__, DEBUG_LINE_NUMBER, __FILE__, #Expression)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                      \
+    goto Cleanup;                                                                                  \
+  }
+
+#define UT_CLEANUP_ASSERT_FALSE(Expression)                                                         \
+  if(!UnitTestAssertFalse ((Expression), __FUNC__, DEBUG_LINE_NUMBER, __FILE__, #Expression)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                       \
+    goto Cleanup;                                                                                   \
+  }
+
+#define UT_CLEANUP_ASSERT_EQUAL(ValueA, ValueB)                                                                                \
+  if(!UnitTestAssertEqual ((UINT64)(ValueA), (UINT64)(ValueB), __FUNC__, DEBUG_LINE_NUMBER, __FILE__, #ValueA, #ValueB)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                                                  \
+    goto Cleanup;                                                                                                              \
+  }
+
+#define UT_CLEANUP_ASSERT_MEM_EQUAL(BufferA, BufferB, Length)                                                                                                      \
+  if(!UnitTestAssertMemEqual ((VOID *)(UINTN)(BufferA), (VOID *)(UINTN)(BufferB), (UINTN)Length, __FUNC__, DEBUG_LINE_NUMBER, __FILE__, #BufferA, #BufferB)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                                                                                      \
+    goto Cleanup;                                                                                                                                                  \
+  }
+
+#define UT_CLEANUP_ASSERT_NOT_EQUAL(ValueA, ValueB)                                                                               \
+  if(!UnitTestAssertNotEqual ((UINT64)(ValueA), (UINT64)(ValueB), __FUNC__, DEBUG_LINE_NUMBER, __FILE__, #ValueA, #ValueB)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                                                     \
+    goto Cleanup;                                                                                                                 \
+  }
+
+#define UT_CLEANUP_ASSERT_NOT_EFI_ERROR(Status)                                                   \
+  if(!UnitTestAssertNotEfiError ((Status), __FUNC__, DEBUG_LINE_NUMBER, __FILE__, #Status)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                     \
+    goto Cleanup;                                                                                 \
+  }
+
+#define UT_CLEANUP_ASSERT_STATUS_EQUAL(Status, Expected)                                                      \
+  if(!UnitTestAssertStatusEqual ((Status), (Expected), __FUNC__, DEBUG_LINE_NUMBER, __FILE__, #Status)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                                 \
+    goto Cleanup;                                                                                             \
+  }
+
+#define UT_CLEANUP_ASSERT_NOT_NULL(Pointer)                                                     \
+  if(!UnitTestAssertNotNull ((Pointer), __FUNC__, DEBUG_LINE_NUMBER, __FILE__, #Pointer)) { \
+    TestResult = UNIT_TEST_ERROR_TEST_FAILED;                                                   \
+    goto Cleanup;                                                                               \
+  }
+
+/**
   This macro uses the framework assertion logic to check whether a function call
   triggers an ASSERT() condition.  The BaseLib SetJump()/LongJump() services
   are used to establish a safe return point when an ASSERT() is triggered.