341a07b3f9c19af057f01065e77ede9b020e18ef
HoraceX Lien <horacex.lien@intel.com>
2024-08-09 17:07:18 +0800

    test pr

--- ShellPkg/Library/UefiShellDebug1CommandsLib/SmbiosView/PrintInfo.c
+++ ShellPkg/Library/UefiShellDebug1CommandsLib/SmbiosView/PrintInfo.c
@@ -3212,10 +3212,7 @@ DisplaySystemSlotId (
     // Slot Type: PCI 66MHz Capable, AGP, PCI-E, etc
     //
     default:
-      if (((SlotType >= 0x0E) && (SlotType <= 0x13)) ||
-          ((SlotType >= 0x1F) && (SlotType <= 0x25)) ||
-          ((SlotType >= 0xA5) && (SlotType <= 0xC6))
-      ) {
+      if (((SlotType >= 0x0E) && (SlotType <= 0x13)) || ((SlotType >= 0x1F) && (SlotType <= 0x25)) || ((SlotType >= 0xA5) && (SlotType <= 0xC6))) {
         ShellPrintHiiEx (-1, -1, NULL, STRING_TOKEN (STR_SMBIOSVIEW_PRINTINFO_VALUE_PRESENT), gShellDebug1HiiHandle, SlotId);
       } else {
         ShellPrintHiiEx (-1, -1, NULL, STRING_TOKEN (STR_SMBIOSVIEW_PRINTINFO_UNDEFINED_SLOT_ID), gShellDebug1HiiHandle);
