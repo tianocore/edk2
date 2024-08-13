13984e79082278cb774843c23f8884605202ad1c
Dhaval <dhaval@rivosinc.com>
2024-08-13 12:33:58 +0530

    UefiPayloadPkg: Add support for Root bridge parser
    In order to properly enable multisegment RB, we need
    to grab ecam data from the FDT for each bridge.
    Current UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES struct from
    MdeModulePkg does not include definition for ecam. In
    order to maintain backward compatibility and also avoid
    diverging too much from core, we are going to define a
    new HOB for UPL segment information and pass it to
    GetPciSegmentInfo function. Ths function then grabs specifically
    ecam info from the segment hob along with other rb specific
    information to create final RB info required by multi segment
    PCI driver.
    
    Additionally we would like to support legacy implementations which
    rely on ACPIBoard HOB to fill up segment info. So if UplSegmentInfo Hob
    is not found we try and look for other hob.
    
    Signed-off-by: Dhaval Sharma <dhaval@rivosinc.com>
    Signed-off-by: Chasel Chiu <chasel.chiu@intel.com>

--- UefiPayloadPkg/Library/PciSegmentInfoLibAcpiBoardInfo/PciSegmentInfoLibAcpiBoardInfo.c
+++ UefiPayloadPkg/Library/PciSegmentInfoLibAcpiBoardInfo/PciSegmentInfoLibAcpiBoardInfo.c
@@ -3,6 +3,8 @@
   segment base address is retrieved from AcpiBoardInfo HOB.
 
   Copyright (c) 2020, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2024, Rivos Inc. All rights reserved.<BR>
+
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -13,22 +15,187 @@
 #include <Library/HobLib.h>
 #include <Library/PciSegmentInfoLib.h>
 #include <Library/DebugLib.h>
+#include <UniversalPayload/PciRootBridges.h>
+#include <Library/PciLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/PcdLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Guid/PciSegmentInfoGuid.h>
 
-STATIC PCI_SEGMENT_INFO  mPciSegment0 = {
-  0,  // Segment number
-  0,  // To be fixed later
-  0,  // Start bus number
-  255 // End bus number
-};
+static PCI_SEGMENT_INFO  *mPciSegments;
+static UINTN             mCount;
 
 /**
-  Return an array of PCI_SEGMENT_INFO holding the segment information.
+  Find segment info from all root bridges
 
-  Note: The returned array/buffer is owned by callee.
+  @param[in]  PciRootBridgeInfo    Pointer of Universal Payload PCI Root Bridge Info Hob
+  @param[in]  UplSegmentInfo       Pointer of Universal UPL Segment Info
 
-  @param  Count  Return the count of segments.
+  @param[out] NumberOfRootBridges  Number of root bridges detected
 
-  @retval A callee owned array holding the segment information.
+**/
+VOID
+RetrieveMultiSegmentInfoFromHob (
+  IN  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES  *PciRootBridgeInfo,
+  IN  UPL_PCI_SEGMENT_INFO_HOB            *UplSegmentInfo,
+  OUT UINTN                               *NumberOfRootBridges
+  )
+{
+  UINTN  Size;
+  UINT8  Index;
+
+  if (PciRootBridgeInfo == NULL) {
+    mPciSegments = NULL;
+    return;
+  }
+
+  *NumberOfRootBridges = PciRootBridgeInfo->Count;
+
+  Size         = PciRootBridgeInfo->Count * sizeof (PCI_SEGMENT_INFO);
+  mPciSegments = (PCI_SEGMENT_INFO *)AllocatePool (Size);
+  ASSERT (mPciSegments != NULL);
+  ZeroMem (mPciSegments, PciRootBridgeInfo->Count * sizeof (PCI_SEGMENT_INFO));
+
+  //
+  // Create all root bridges with PciRootBridgeInfoHob
+  //
+  for (Index = 0; Index < PciRootBridgeInfo->Count; Index++) {
+    if (UplSegmentInfo->SegmentInfo[Index].SegmentNumber == (UINT16)(PciRootBridgeInfo->RootBridge[Index].Segment)) {
+      mPciSegments[Index].BaseAddress = UplSegmentInfo->SegmentInfo[Index].BaseAddress;
+    }
+
+    mPciSegments[Index].SegmentNumber  = (UINT16)(PciRootBridgeInfo->RootBridge[Index].Segment);
+    mPciSegments[Index].StartBusNumber = (UINT8)PciRootBridgeInfo->RootBridge[Index].Bus.Base;
+    mPciSegments[Index].EndBusNumber   = (UINT8)PciRootBridgeInfo->RootBridge[Index].Bus.Limit;
+  }
+
+  return;
+}
+
+/**
+  Find segment info from all root bridges for legacy systems
+
+  @param[in]  PciRootBridgeInfo    Pointer of Universal Payload PCI Root Bridge Info Hob
+  @param[out] NumberOfRootBridges  Number of root bridges detected
+
+**/
+VOID
+RetrieveSegmentInfoFromHob (
+  OUT UINTN  *NumberOfRootBridges
+  )
+{
+  EFI_HOB_GUID_TYPE  *GuidHob;
+  ACPI_BOARD_INFO    *AcpiBoardInfo;
+
+  *NumberOfRootBridges = 1;
+  // old model relies on gUefiAcpiBoardInfoGuid and hardcoded values for single segment only.
+  // This is only for backward compatibility, new platforms should adopt new model even in single segment cases.
+  //
+  mPciSegments = (PCI_SEGMENT_INFO *)AllocatePool (sizeof (PCI_SEGMENT_INFO));
+  ASSERT (mPciSegments != NULL);
+  GuidHob = GetFirstGuidHob (&gUefiAcpiBoardInfoGuid);
+  ASSERT (GuidHob != NULL);
+  if (GuidHob != NULL) {
+    AcpiBoardInfo                = (ACPI_BOARD_INFO *)GET_GUID_HOB_DATA (GuidHob);
+    mPciSegments->SegmentNumber  = 0;
+    mPciSegments->BaseAddress    = AcpiBoardInfo->PcieBaseAddress;
+    mPciSegments->StartBusNumber = 0;
+    mPciSegments->EndBusNumber   = 0xFF;
+  }
+}
+
+/**
+  Return info for all root bridges
+
+  @return All the root bridge info instances in an array.
+**/
+UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES *
+Get_RBInfo (
+  VOID
+  )
+{
+  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES  *PciRootBridgeInfo;
+  EFI_HOB_GUID_TYPE                   *GuidHob;
+  UNIVERSAL_PAYLOAD_GENERIC_HEADER    *GenericHeader;
+
+  //
+  // Find Universal Payload PCI Root Bridge Info hob
+  //
+  GuidHob = GetFirstGuidHob (&gUniversalPayloadPciRootBridgeInfoGuid);
+  if ((GuidHob == NULL) || (sizeof (UNIVERSAL_PAYLOAD_GENERIC_HEADER) > GET_GUID_HOB_DATA_SIZE (GuidHob))) {
+    return NULL;
+  }
+
+  GenericHeader = (UNIVERSAL_PAYLOAD_GENERIC_HEADER *)GET_GUID_HOB_DATA (GuidHob);
+  if (GenericHeader->Length > GET_GUID_HOB_DATA_SIZE (GuidHob)) {
+    return NULL;
+  }
+
+  if ((GenericHeader->Revision != UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES_REVISION) || (GenericHeader->Length < sizeof (UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES))) {
+    return NULL;
+  }
+
+  //
+  // UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES structure is used when Revision equals to UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES_REVISION
+  //
+  PciRootBridgeInfo = (UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES *)GET_GUID_HOB_DATA (GuidHob);
+  if (PciRootBridgeInfo->Count <= (GET_GUID_HOB_DATA_SIZE (GuidHob) - sizeof (UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES)) / sizeof (UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGE)) {
+    return PciRootBridgeInfo;
+  }
+
+  return NULL;
+}
+
+/**
+  Return info for all root bridge segments
+
+  @return All the segment info instances in an array.
+**/
+UPL_PCI_SEGMENT_INFO_HOB *
+Get_UPLSegInfo (
+  VOID
+  )
+{
+  UPL_PCI_SEGMENT_INFO_HOB          *UplSegmentInfo;
+  EFI_HOB_GUID_TYPE                 *GuidHob;
+  UNIVERSAL_PAYLOAD_GENERIC_HEADER  *GenericHeader;
+
+  //
+  // Find Universal Payload Segment Info hob
+  //
+  GuidHob = GetFirstGuidHob (&gUplPciSegmentInfoHobGuid);
+  if ((GuidHob == NULL) || (sizeof (UNIVERSAL_PAYLOAD_GENERIC_HEADER) > GET_GUID_HOB_DATA_SIZE (GuidHob))) {
+    return NULL;
+  }
+
+  GenericHeader = (UNIVERSAL_PAYLOAD_GENERIC_HEADER *)GET_GUID_HOB_DATA (GuidHob);
+  if (GenericHeader->Length > GET_GUID_HOB_DATA_SIZE (GuidHob)) {
+    return NULL;
+  }
+
+  if ((GenericHeader->Revision != UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES_REVISION) || (GenericHeader->Length < sizeof (UPL_PCI_SEGMENT_INFO_HOB))) {
+    return NULL;
+  }
+
+  //
+  // UPL_PCI_SEGMENT_INFO_HOB structure is used when Revision equals to UPL_PCI_SEGMENT_INFO_HOB_REVISION
+  //
+  UplSegmentInfo = (UPL_PCI_SEGMENT_INFO_HOB *)GET_GUID_HOB_DATA (GuidHob);
+  if (UplSegmentInfo->Count <= (GET_GUID_HOB_DATA_SIZE (GuidHob) - sizeof (UPL_PCI_SEGMENT_INFO_HOB)) / sizeof (UPL_SEGMENT_INFO)) {
+    return UplSegmentInfo;
+  }
+
+  return NULL;
+}
+
+/**
+  Return all the root bridge instances in an array.
+
+  @param Count  Return the count of root bridge instances.
+
+  @return All the root bridge instances in an array.
+          The array should be passed into PciHostBridgeFreeRootBridges()
+          when it's not used.
 **/
 PCI_SEGMENT_INFO *
 EFIAPI
@@ -36,25 +203,27 @@ GetPciSegmentInfo (
   UINTN  *Count
   )
 {
-  EFI_HOB_GUID_TYPE  *GuidHob;
-  ACPI_BOARD_INFO    *AcpiBoardInfo;
+  UNIVERSAL_PAYLOAD_PCI_ROOT_BRIDGES  *PciRootBridgeInfo;
+  UPL_PCI_SEGMENT_INFO_HOB            *UplSegmentInfo;
 
-  ASSERT (Count != NULL);
-  if (Count == NULL) {
-    return NULL;
+  if (mPciSegments != NULL) {
+    *Count = mCount;
+    return mPciSegments;
   }
 
-  if (mPciSegment0.BaseAddress == 0) {
-    //
-    // Find the acpi board information guid hob
-    //
-    GuidHob = GetFirstGuidHob (&gUefiAcpiBoardInfoGuid);
-    ASSERT (GuidHob != NULL);
+  UplSegmentInfo = Get_UPLSegInfo ();
 
-    AcpiBoardInfo            = (ACPI_BOARD_INFO *)GET_GUID_HOB_DATA (GuidHob);
-    mPciSegment0.BaseAddress = AcpiBoardInfo->PcieBaseAddress;
+  if (UplSegmentInfo == NULL) {
+    RetrieveSegmentInfoFromHob (Count);
+  } else {
+    PciRootBridgeInfo = Get_RBInfo ();
+    if (PciRootBridgeInfo == NULL) {
+      return 0;
+    }
+
+    RetrieveMultiSegmentInfoFromHob (PciRootBridgeInfo, UplSegmentInfo, Count);
   }
 
-  *Count = 1;
-  return &mPciSegment0;
+  mCount = *Count;
+  return mPciSegments;
 }
