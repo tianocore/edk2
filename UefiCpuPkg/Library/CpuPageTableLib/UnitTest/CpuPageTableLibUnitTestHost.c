4def9c95073c99be3705c2d8923702f18d1e79d4
Oliver Smith-Denny <osde@microsoft.com>
2024-08-23 14:47:55 -0700

    UefiCpuPkg: CpuPageTableLibTestHost: Disable Random Test Suite
    Commit 2f499c36db51980ad43fc6b578c7678a1720bd9c commented out the
    RandomTestCase tests in CpuPageTableLibTestHost, but it left the
    test suite being registered without any tests. This causes a failure
    for tools that check to ensure tests are being registered with test
    suites.
    
    This patch comments out the test suite in addition to the tests
    being added to it.
    
    Signed-off-by: Oliver Smith-Denny <osde@linux.microsoft.com>

--- UefiCpuPkg/Library/CpuPageTableLib/UnitTest/CpuPageTableLibUnitTestHost.c
+++ UefiCpuPkg/Library/CpuPageTableLib/UnitTest/CpuPageTableLibUnitTestHost.c
@@ -838,7 +838,8 @@ UefiTestMain (
   EFI_STATUS                  Status;
   UNIT_TEST_FRAMEWORK_HANDLE  Framework;
   UNIT_TEST_SUITE_HANDLE      ManualTestCase;
-  UNIT_TEST_SUITE_HANDLE      RandomTestCase;
+
+  // UNIT_TEST_SUITE_HANDLE      RandomTestCase;
 
   Framework = NULL;
 
@@ -874,12 +875,12 @@ UefiTestMain (
   //
   // Populate the Random Test Cases.
   //
-  Status = CreateUnitTestSuite (&RandomTestCase, Framework, "Random Test Cases", "CpuPageTableLib.Random", NULL, NULL);
-  if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "Failed in CreateUnitTestSuite for Random Test Cases\n"));
-    Status = EFI_OUT_OF_RESOURCES;
-    goto EXIT;
-  }
+  // Status = CreateUnitTestSuite (&RandomTestCase, Framework, "Random Test Cases", "CpuPageTableLib.Random", NULL, NULL);
+  // if (EFI_ERROR (Status)) {
+  //   DEBUG ((DEBUG_ERROR, "Failed in CreateUnitTestSuite for Random Test Cases\n"));
+  //   Status = EFI_OUT_OF_RESOURCES;
+  //   goto EXIT;
+  // }
 
   // AddTestCase (RandomTestCase, "Random Test for Paging4Level", "Random Test Case1", TestCaseforRandomTest, NULL, NULL, &mTestContextPaging4Level);
   // AddTestCase (RandomTestCase, "Random Test for Paging4Level1G", "Random Test Case2", TestCaseforRandomTest, NULL, NULL, &mTestContextPaging4Level1GB);
