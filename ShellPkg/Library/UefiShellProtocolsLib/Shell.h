eb2a3f8b627f5ab9f39d2ad4fd8956340e28696d
Benjamin Doron <benjamin.doron@9elements.com>
2024-09-27 13:40:43 -0400

    WIP ShellPkg: UefiShellProtocolsLib: Add new library to install the 'shell'
    This patch represents the primary patch of this series: By linking
    against this library, a shell-like environment will be available to the
    consuming app.
    
    It's mainly a copy and refactor of the shell app.
    
    Signed-off-by: Benjamin Doron <benjamin.doron@9elements.com>

---
+++ ShellPkg/Library/UefiShellProtocolsLib/Shell.h
@@ -0,0 +1,30 @@
+/** @file
+  Provides interface to the shell protocols.
+
+  Copyright (c) 2024, 9elements GmbH.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _SHELL_INTERNAL_HEADER_
+#define _SHELL_INTERNAL_HEADER_
+
+#include <Uefi.h>
+
+#include <Library/BaseLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
+#include <Library/DevicePathLib.h>
+#include <Library/HiiLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/PrintLib.h>
+#include <Library/ShellLib.h>
+#include <Library/ShellCommandLib.h>
+#include <Library/ShellProtocolsLib.h>
+
+#include <Protocol/Shell.h>
+#include <Protocol/ShellParameters.h>
+
+#include <ShellInternals.h>
+
+#endif
