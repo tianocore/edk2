30d3c333ed4354623dffde89976233d39cb22ac0
Hongbin1 Zhang <hongbin1.zhang@intel.com>
2024-10-07 13:29:56 +0800

    IntelFsp2WrapperPkg/FspiWrapperPeim : Support dispatch mode
    Add FSP-SMM code for dispatch mode.
    
    Signed-off-by: Hongbin1 Zhang <hongbin1.zhang@intel.com>
    Cc: Chasel Chiu <chasel.chiu@intel.com>
    Cc: Nate DeSimone <nathaniel.l.desimone@intel.com>
    Cc: Duggapu Chinni B <chinni.b.duggapu@intel.com>
    Cc: Chen Gang C <gang.c.chen@intel.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Ted Kuo <ted.kuo@intel.com>
    Cc: Ashraf Ali S <ashraf.ali.s@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>

--- IntelFsp2WrapperPkg/FspiWrapperPeim/FspiWrapperPeim.c
+++ IntelFsp2WrapperPkg/FspiWrapperPeim/FspiWrapperPeim.c
@@ -10,7 +10,64 @@
 
 #include <PiPei.h>
 #include <Library/PeimEntryPoint.h>
+#include <Library/PeiServicesLib.h>
 #include <Library/PeiServicesTablePointerLib.h>
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Ppi/FirmwareVolumeInfoMeasurementExcluded.h>
+
+/**
+  Do FSP SMM initialization in Dispatch mode.
+
+  @retval FSP SMM initialization status.
+**/
+EFI_STATUS
+EFIAPI
+FspiWrapperInitDispatchMode (
+  VOID
+  )
+{
+  EFI_STATUS                                             Status;
+  EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_PPI  *MeasurementExcludedFvPpi;
+  EFI_PEI_PPI_DESCRIPTOR                                 *MeasurementExcludedPpiList;
+
+  MeasurementExcludedFvPpi = AllocatePool (sizeof (*MeasurementExcludedFvPpi));
+  ASSERT (MeasurementExcludedFvPpi != NULL);
+  if (MeasurementExcludedFvPpi == NULL) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  MeasurementExcludedFvPpi->Count          = 1;
+  MeasurementExcludedFvPpi->Fv[0].FvBase   = PcdGet32 (PcdFspiBaseAddress);
+  MeasurementExcludedFvPpi->Fv[0].FvLength = ((EFI_FIRMWARE_VOLUME_HEADER *)(UINTN)PcdGet32 (PcdFspiBaseAddress))->FvLength;
+
+  MeasurementExcludedPpiList = AllocatePool (sizeof (*MeasurementExcludedPpiList));
+  ASSERT (MeasurementExcludedPpiList != NULL);
+  if (MeasurementExcludedPpiList == NULL) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  MeasurementExcludedPpiList->Flags = EFI_PEI_PPI_DESCRIPTOR_PPI | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST;
+  MeasurementExcludedPpiList->Guid  = &gEfiPeiFirmwareVolumeInfoMeasurementExcludedPpiGuid;
+  MeasurementExcludedPpiList->Ppi   = MeasurementExcludedFvPpi;
+
+  Status = PeiServicesInstallPpi (MeasurementExcludedPpiList);
+  ASSERT_EFI_ERROR (Status);
+
+  //
+  // FSP-I Wrapper running in Dispatch mode and reports FSP-I FV to PEI dispatcher.
+  //
+  PeiServicesInstallFvInfoPpi (
+    &((EFI_FIRMWARE_VOLUME_HEADER *)(UINTN)PcdGet32 (PcdFspiBaseAddress))->FileSystemGuid,
+    (VOID *)(UINTN)PcdGet32 (PcdFspiBaseAddress),
+    (UINT32)((EFI_FIRMWARE_VOLUME_HEADER *)(UINTN)PcdGet32 (PcdFspiBaseAddress))->FvLength,
+    NULL,
+    NULL
+    );
+
+  return Status;
+}
 
 /**
   This is the entrypoint of PEIM.
@@ -27,6 +84,11 @@ FspiWrapperPeimEntryPoint (
   IN CONST EFI_PEI_SERVICES     **PeiServices
   )
 {
+  EFI_STATUS  Status;
 
-  return EFI_SUCCESS;
+  DEBUG ((DEBUG_INFO, "FspiWrapperPeimEntryPoint\n"));
+
+  Status = FspiWrapperInitDispatchMode ();
+
+  return Status;
 }
