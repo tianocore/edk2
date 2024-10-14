786ccf97811163126618adcee57890e08dadbe88
ishih1 <ishih1@lenovo.com>
2024-10-14 10:16:20 +0800

    Update Copyright and merge MockSmmAccess2.cpp.
    1. Update Copyright in MockSmmAccess2.h.
    2. Merge MockSmmAccess2.cpp into MockSmmAccess2.h.
    
    Signed-off-by: Ian Shih <ishih1@lenovo.com>

--- MdePkg/Test/Mock/Include/GoogleTest/Protocol/MockSmmAccess2.h
+++ MdePkg/Test/Mock/Include/GoogleTest/Protocol/MockSmmAccess2.h
@@ -1,8 +1,19 @@
-/** @file MockSmmAccess2.h
-  Declare mock SMM Access2 Protocol.
+/** @file
+  EFI SMM Access2 Protocol as defined in the PI 1.2 specification.
 
-  Copyright (c) Microsoft Corporation.
+  This protocol is used to control the visibility of the SMRAM on the platform.
+  It abstracts the location and characteristics of SMRAM.  The expectation is
+  that the north bridge or memory controller would publish this protocol.
+
+  The principal functionality found in the memory controller includes the following:
+  - Exposing the SMRAM to all non-SMM agents, or the "open" state
+  - Shrouding the SMRAM to all but the SMM agents, or the "closed" state
+  - Preserving the system integrity, or "locking" the SMRAM, such that the settings cannot be
+    perturbed by either boot service or runtime agents
+
+  Copyright (c) 2009 - 2018, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
+
 **/
 
 #ifndef MOCK_SMM_ACCESS2_H_
@@ -46,8 +57,23 @@ struct MockSmmAccess2Protocol {
     );
 };
 
+MOCK_INTERFACE_DEFINITION (MockSmmAccess2Protocol);
+MOCK_FUNCTION_DEFINITION (MockSmmAccess2Protocol, Open, 1, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockSmmAccess2Protocol, Close, 1, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockSmmAccess2Protocol, Lock, 1, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockSmmAccess2Protocol, GetCapabilities, 3, EFIAPI);
+
+EFI_SMM_ACCESS2_PROTOCOL  SMM_ACCESS2_PROTOCOL_INSTANCE = {
+  Open,
+  Close,
+  Lock,
+  GetCapabilities,
+  FALSE,
+  FALSE
+};
+
 extern "C" {
-  extern EFI_SMM_ACCESS2_PROTOCOL  *gSmmAccess2Protocol;
+  EFI_SMM_ACCESS2_PROTOCOL  *gSmmAccess2Protocol = &SMM_ACCESS2_PROTOCOL_INSTANCE;
 }
 
 #endif
