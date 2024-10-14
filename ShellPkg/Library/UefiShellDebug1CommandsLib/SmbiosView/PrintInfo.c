bd93c1eb13d6a4baf1c55531d9a71fd1a438dd2f
Revathy V <revathyv@ami.com>
2024-10-14 11:31:39 +0530

    ShellPkg: Updated Type 4 Info as per Smbios 3.8.0
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4861
    
    Added PROCESSOR_FAMILY_NAME support in ShellPkg.
    
    Signed-off-by: Revathy <revathyv@ami.com>

--- ShellPkg/Library/UefiShellDebug1CommandsLib/SmbiosView/PrintInfo.c
+++ ShellPkg/Library/UefiShellDebug1CommandsLib/SmbiosView/PrintInfo.c
@@ -2423,6 +2423,10 @@ DisplayProcessorFamily (
       Print (L"Intel Core i9 processor\n");
       break;
 
+    case 0xD0:
+      Print (L"Intel Xeon D Processor\n");
+      break;
+
     case 0xD2:
       Print (L"ViaC7M\n");
       break;
