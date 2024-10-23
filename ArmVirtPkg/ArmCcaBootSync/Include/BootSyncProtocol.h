3b46b2f9fee166d98c9843299347c5bcb9044b5f
Sami Mujawar <sami.mujawar@arm.com>
2024-10-23 17:33:38 +0100

    ArmVirtPkg: Add Boot Sync protocol Fin and NACK messages
    A Fin message is used to indicate end of communication
    while an NACK message is used to indicate that a message
    was not successfully received or processed.
    
    Therefore, add interfaces to support sending of Fin and
    NACK messages.
    
    Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>

---
+++ ArmVirtPkg/ArmCcaBootSync/Include/BootSyncProtocol.h
@@ -0,0 +1,42 @@
+/** @file
+  Arm CCA Boot Sync Protocol interfaces.
+
+  Copyright (c) 2023, Arm Limited. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#pragma once
+
+/**
+  Send FIN request message.
+
+  @param[in]  SecChannel    Pointer to the secure channel.
+  @param[in]  Reason        Reason code for the FIN message.
+
+  @retval EFI_INVALID_PARAMETER   A parameter was invalid.
+  @retval EFI_OUT_OF_RESOURCES    Failed to allocate memory.
+  @retval EFI_SUCCESS             Success.
+**/
+EFI_STATUS
+EFIAPI
+SendFin (
+  IN SECURE_CHANNEL  *SecChannel,
+  IN UINT64          Reason
+  );
+
+/**
+  Send a negetive acknowlegdement (NACK) message.
+
+  @param[in]  SecChannel    Pointer to the secure channel.
+  @param[in]  Reason        Reason code for the NACK message.
+
+  @retval EFI_INVALID_PARAMETER   A parameter was invalid.
+  @retval EFI_OUT_OF_RESOURCES    Failed to allocate memory.
+  @retval EFI_SUCCESS             Success.
+**/
+EFI_STATUS
+EFIAPI
+SendNack (
+  IN SECURE_CHANNEL  *SecChannel,
+  IN UINT64          Reason
+  );
