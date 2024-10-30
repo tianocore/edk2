b75fa9f0110a72bb77d08ff5f366e17b44b050aa
Sami Mujawar <sami.mujawar@arm.com>
2024-10-30 17:26:25 +0000

    ArmVirtPkg: BootSyncTestApp: Establish a secure channel
    The Boot Sync Test app is a UEFI application that is used
    to unit test the Boot Sync Blocks (BSB) protocol which
    is described in Appendix A of the Realm Host Interface
    (RHI) Specification:
    https://developer.arm.com/documentation/den0148/
    
    The BSB protocol involves establishing a secure session
    between the User Context and the Realm guest firmware.
    A ECDH key exchange sequence is followed and a common
    key is computed. Following this, two additional keys,
    an encryption key and a binding key are derived from
    the common key. The encryption key is used to encrypt
    the data that is sent over the channel.
    
    Therefore, add support to establish a secure channel
    with the User Context.
    
    Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>

--- ArmVirtPkg/ArmCcaBootSync/BootSyncTestApp/BootSyncTestApp.c
+++ ArmVirtPkg/ArmCcaBootSync/BootSyncTestApp/BootSyncTestApp.c
@@ -5,8 +5,14 @@
   SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
+#include <Library/ArmCcaBootSyncCryptoLib.h>
 #include <Library/BaseLib.h>
 #include <Library/UefiLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
+#include "Include/BootSyncDebug.h"
+#include "Include/BootSyncSecureChannel.h"
+#include "Include/BootSyncProtocol.h"
 
 /**
   Entrypoint for the Boot Sync Test application.
@@ -23,7 +29,34 @@ BootSyncTestApp (
   IN EFI_SYSTEM_TABLE  *SystemTable
   )
 {
+  EFI_STATUS      Status;
+  SECURE_CHANNEL  SecChannel;
+
   Print (L"Boot Sync Test Application\n");
 
-  return EFI_SUCCESS;
+  Status = ArmCcaBootSyncCryptoInit ();
+  if (EFI_ERROR (Status)) {
+    Print (L"Error: Failed to init Crypto interfaces, Status = %r\n", Status);
+    return Status;
+  }
+
+  ZeroMem (&SecChannel, sizeof (SECURE_CHANNEL));
+  Status = EstablishSecureChannel (&SecChannel);
+  if (EFI_ERROR (Status)) {
+    Print (L"Error: Failed to establish Secure Session, Status = %r\n", Status);
+    return Status;
+  }
+
+  DBG_DUMP_KEYS ("Sec", &SecChannel);
+
+  // Send the FIN message to indicate End of Transmission.
+  Status = SendFin (&SecChannel, BOOT_SYNC_COMM_END_REASON_EOT);
+  ASSERT_EFI_ERROR (Status);
+
+  Status = TerminateSecureChannel (&SecChannel);
+  if (EFI_ERROR (Status)) {
+    Print (L"Error: Failed to Close Secure Session Status = %r\n", Status);
+  }
+
+  return Status;
 }
