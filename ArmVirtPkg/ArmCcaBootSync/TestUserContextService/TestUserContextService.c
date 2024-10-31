1881e39282b21fdc070e541ac78108b60935748f
Sami Mujawar <sami.mujawar@arm.com>
2024-10-31 12:39:55 +0000

    ArmVirtPkg: HostTestService: Add support to start a socket server
    The Realm Host Interface (RHI) Specification describes
    the Boot Sync Blocks protocol which involves establishing
    a secure session between the User Context and the Realm
    guest firmware.
    
    The RHI session protocol allows communication of messages
    from the Realm Guest firmware to the Virtual machine
    manager(VMM). These messages are then relayed by the
    VMM to the User Context over a network socket.
    
    The Host Test Service is a unit testing application
    that is used to validate the Boot Sync Blocks protocol
    and plays the role of the User Context.
    
    Therefore, add support to the HostTestService to start
    a socket server so that the VMM can connect and relay
    the messages from the Realm guest firmware.
    
    Note: This patch create the groundwork required for
    creating a socket server. The listen() call is
    implemented in a thread in a subsequent patch.
    
    Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>

--- ArmVirtPkg/ArmCcaBootSync/TestUserContextService/TestUserContextService.c
+++ ArmVirtPkg/ArmCcaBootSync/TestUserContextService/TestUserContextService.c
@@ -6,10 +6,81 @@
 **/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #include <Base.h>
 #include <Library/ArmCcaBootSyncCryptoLib.h>
 #include <Uefi/UefiBaseType.h>
+
+/**
+  Create and initialise a server socket for the service.
+
+  @param[in]  PortNo          PortNo to start the server socket on.
+  @param[out] ServerSockFd    The server socket fd.
+
+  @retval   Negative value on Failure, otherwise success.
+
+**/
+int
+CreateServer (
+  IN  int  PortNo,
+  OUT int  *ServerSockFd
+  )
+{
+  int                 Err;
+  int                 SockFd;
+  int                 EnableOption;
+  struct sockaddr_in  ServerAddr;
+
+  SockFd = socket (AF_INET, SOCK_STREAM, 0);
+  if (SockFd < 0) {
+    printf ("Error: Failed to create socket\n");
+    *ServerSockFd = -1;
+    return -1;
+  }
+
+  EnableOption = 1;
+  Err          = setsockopt (
+                   SockFd,
+                   SOL_SOCKET,
+                   SO_REUSEADDR,
+                   &EnableOption,
+                   sizeof (EnableOption)
+                   );
+  if (Err < 0) {
+    printf ("Error: Failed to set socket options\n");
+    goto ExitHandler;
+  }
+
+  memset ((char *)&ServerAddr, 0x0, sizeof (ServerAddr));
+
+  ServerAddr.sin_family      = AF_INET;
+  ServerAddr.sin_addr.s_addr = INADDR_ANY;
+  ServerAddr.sin_port        = htons (PortNo);
+  Err                        = bind (
+                                 SockFd,
+                                 (struct sockaddr *)&ServerAddr,
+                                 sizeof (ServerAddr)
+                                 );
+  if (Err < 0) {
+    printf ("Error: Failed to bind socket\n");
+    goto ExitHandler;
+  }
+
+  *ServerSockFd = SockFd;
+  return Err;
+
+ExitHandler:
+  shutdown (SockFd, SHUT_RDWR);
+  close (SockFd);
+  *ServerSockFd = -1;
+  return Err;
+}
 
 /**
   Entrypoint for the Test User Context service.
@@ -27,8 +98,18 @@ main (
   )
 {
   EFI_STATUS  Status;
+  int         ServerSockFd;
+  int         PortNo;
 
   printf ("Test User Context Service.\n");
+
+  if (argc < 2) {
+    printf ("Error: Service port number not provided\n");
+    return -1;
+  }
+
+  PortNo = atoi (argv[1]);
+  printf ("Info: Service port number: %d\n", PortNo);
 
   Status = ArmCcaBootSyncCryptoInit ();
   if (EFI_ERROR (Status)) {
@@ -38,6 +119,18 @@ main (
       );
     return -1;
   }
+
+  if (CreateServer (PortNo, &ServerSockFd) < 0) {
+    printf ("Error: Failed to create server socket\n");
+    return -1;
+  }
+
+  printf ("Info: Server scoket created fd = %d\n", ServerSockFd);
+
+  printf ("Info: Shutting down.\n");
+  // Shutdown and close the server socket.
+  shutdown (ServerSockFd, SHUT_RDWR);
+  close (ServerSockFd);
 
   return 0;
 }
