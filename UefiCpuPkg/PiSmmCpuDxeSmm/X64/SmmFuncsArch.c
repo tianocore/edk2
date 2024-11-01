13fad60156f18cf0d2043fb7f05c1dc5e3d91fb7
kenlautner <85201046+kenlautner@users.noreply.github.com>
2024-11-01 12:00:25 -0700

    UefiCpuPkg: Fix unchecked returns and potential integer overflows
    Resolves several issues in UefiCpuPkg related to:
    
    1. Unchecked returns leading to potential NULL or uninitialized access.
    2. Potential unchecked integer overflows.
    3. Incorrect comparison between integers of different sizes.
    
    Co-authored-by: kenlautner <85201046+kenlautner@users.noreply.github.com>
    Signed-off-by: Chris Fernald <chfernal@microsoft.com>

--- UefiCpuPkg/PiSmmCpuDxeSmm/X64/SmmFuncsArch.c
+++ UefiCpuPkg/PiSmmCpuDxeSmm/X64/SmmFuncsArch.c
@@ -118,7 +118,7 @@ GetProtectedModeCS (
   AsmReadGdtr (&GdtrDesc);
   GdtEntryCount = (GdtrDesc.Limit + 1) / sizeof (IA32_SEGMENT_DESCRIPTOR);
   GdtEntry      = (IA32_SEGMENT_DESCRIPTOR *)GdtrDesc.Base;
-  for (Index = 0; Index < GdtEntryCount; Index++) {
+  for (Index = 0; (UINTN)Index < GdtEntryCount; Index++) {
     if (GdtEntry->Bits.L == 0) {
       if ((GdtEntry->Bits.Type > 8) && (GdtEntry->Bits.DB == 1)) {
         break;
