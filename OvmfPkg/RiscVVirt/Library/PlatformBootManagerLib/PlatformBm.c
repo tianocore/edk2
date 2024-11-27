0cfe6ee859900f48b10038268592e6f2573b22a2
Rebecca Cran <rebecca@bsdio.com>
2024-11-26 21:24:03 -0700

    OvmfPkg/RiscVVirt: Check PcdUefiShellCreateBootOption
    Check the PcdUefiShellCreateBootOption PCD and only create the boot
    entry for the UEFI Shell if it's TRUE.
    
    Signed-off-by: Rebecca Cran <rebecca@bsdio.com>

--- OvmfPkg/RiscVVirt/Library/PlatformBootManagerLib/PlatformBm.c
+++ OvmfPkg/RiscVVirt/Library/PlatformBootManagerLib/PlatformBm.c
@@ -965,11 +965,13 @@ PlatformBootManagerAfterConsole (
   //
   // Register UEFI Shell
   //
-  PlatformRegisterFvBootOption (
-    &gUefiShellFileGuid,
-    L"EFI Internal Shell",
-    LOAD_OPTION_ACTIVE
-    );
+  if (FixedPcdGetBool (PcdUefiShellCreateBootOption)) {
+    PlatformRegisterFvBootOption (
+      &gUefiShellFileGuid,
+      L"EFI Internal Shell",
+      LOAD_OPTION_ACTIVE
+      );
+  }
 
   RemoveStaleFvFileOptions ();
   SetBootOrderFromQemu ();
