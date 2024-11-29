3737e7e7ac96bd94b872a0044722d9a02c5fabae
Abdul Lateef Attar <AbdulLateef.Attar@amd.com>
2024-11-29 11:51:50 +0000

    DynamicTablesPkg: Add _PSD and _CPC support for X64
    Introduce _PSD and _CPC ACPI objects for X64 platforms.
    
    Signed-off-by: Abdul Lateef Attar <AbdulLateef.Attar@amd.com>

--- DynamicTablesPkg/Library/Common/TableHelperLib/ConfigurationManagerObjectParser.c
+++ DynamicTablesPkg/Library/Common/TableHelperLib/ConfigurationManagerObjectParser.c
@@ -937,7 +937,9 @@ STATIC CONST CM_OBJ_PARSER  CmX64ObjLocalApicX2ApicInfoParser[] = {
   { "AcpiProcessorUid", 4,                        "0x%x", NULL },
   { "CstToken",         sizeof (CM_OBJECT_TOKEN), "0x%p", NULL },
   { "CsdToken",         sizeof (CM_OBJECT_TOKEN), "0x%p", NULL },
-  { "PstateToken",      sizeof (CM_OBJECT_TOKEN), "0x%p", NULL }
+  { "PstateToken",      sizeof (CM_OBJECT_TOKEN), "0x%p", NULL },
+  { "PsdToken",         sizeof (CM_OBJECT_TOKEN), "0x%p", NULL },
+  { "CpcToken",         sizeof (CM_OBJECT_TOKEN), "0x%p", NULL }
 };
 
 /** A parser for CmX64IoApicInfoParser.
