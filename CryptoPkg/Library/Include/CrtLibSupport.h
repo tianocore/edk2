5e72284e696bfd2571c55f16d9d0fa519543fe0d
leenasoman <165866710+leenasoman@users.noreply.github.com>
2024-12-02 17:30:48 +0530

    Update CryptoPkg/Library/Include/CrtLibSupport.h
    Co-authored-by: Dionna Amalie Glaze <drdeeglaze@gmail.com>

--- CryptoPkg/Library/Include/CrtLibSupport.h
+++ CryptoPkg/Library/Include/CrtLibSupport.h
@@ -521,9 +521,9 @@ void *
 bsearch (
   const void *key,
   const void *base,
-  size_t nitems,
-  size_t width,
-  int         ( *cmp )(const void *, const void *)
+  size_t nmemb,
+  size_t size,
+  int       ( *compar )(const void *, const void *)
   );
 
 int
