9382612db5843a6b7d86f604e918cb34a4002937
Kane Chen <kane.chen@microsoft.com>
2024-12-08 12:19:39 +0800

    Add the rest 3 mock functions for MockVariablePolicyHelperLib
    Signed-off-by: Kane Chen <kane.chen@microsoft.com>

--- MdeModulePkg/Test/Mock/Include/GoogleTest/Library/MockVariablePolicyHelperLib.h
+++ MdeModulePkg/Test/Mock/Include/GoogleTest/Library/MockVariablePolicyHelperLib.h
@@ -23,6 +23,34 @@ struct MockVariablePolicyHelperLib {
 
   MOCK_FUNCTION_DECLARATION (
     EFI_STATUS,
+    CreateBasicVariablePolicy,
+    (IN CONST  EFI_GUID         *Namespace,
+     IN CONST  CHAR16           *Name OPTIONAL,
+     IN        UINT32           MinSize,
+     IN        UINT32           MaxSize,
+     IN        UINT32           AttributesMustHave,
+     IN        UINT32           AttributesCantHave,
+     IN        UINT8            LockPolicyType,
+     OUT VARIABLE_POLICY_ENTRY  **NewEntry)
+    );
+
+  MOCK_FUNCTION_DECLARATION (
+    EFI_STATUS,
+    CreateVarStateVariablePolicy,
+    (IN CONST  EFI_GUID         *Namespace,
+     IN CONST  CHAR16           *Name OPTIONAL,
+     IN        UINT32           MinSize,
+     IN        UINT32           MaxSize,
+     IN        UINT32           AttributesMustHave,
+     IN        UINT32           AttributesCantHave,
+     IN CONST  EFI_GUID         *VarStateNamespace,
+     IN        UINT8            VarStateValue,
+     IN CONST  CHAR16           *VarStateName,
+     OUT VARIABLE_POLICY_ENTRY  **NewEntry)
+    );
+
+  MOCK_FUNCTION_DECLARATION (
+    EFI_STATUS,
     RegisterBasicVariablePolicy,
     (IN        EDKII_VARIABLE_POLICY_PROTOCOL  *VariablePolicy,
      IN CONST  EFI_GUID                        *Namespace,
@@ -32,6 +60,21 @@ struct MockVariablePolicyHelperLib {
      IN        UINT32                          AttributesMustHave,
      IN        UINT32                          AttributesCantHave,
      IN        UINT8                           LockPolicyType)
+    );
+
+  MOCK_FUNCTION_DECLARATION (
+    EFI_STATUS,
+    RegisterVarStateVariablePolicy,
+    (IN        EDKII_VARIABLE_POLICY_PROTOCOL  *VariablePolicy,
+     IN CONST  EFI_GUID                        *Namespace,
+     IN CONST  CHAR16                          *Name OPTIONAL,
+     IN        UINT32                          MinSize,
+     IN        UINT32                          MaxSize,
+     IN        UINT32                          AttributesMustHave,
+     IN        UINT32                          AttributesCantHave,
+     IN CONST  EFI_GUID                        *VarStateNamespace,
+     IN CONST  CHAR16                          *VarStateName,
+     IN        UINT8                           VarStateValue)
     );
 };
 
--- MdeModulePkg/Test/Mock/Library/GoogleTest/MockVariablePolicyHelperLib/MockVariablePolicyHelperLib.cpp
+++ MdeModulePkg/Test/Mock/Library/GoogleTest/MockVariablePolicyHelperLib/MockVariablePolicyHelperLib.cpp
@@ -8,4 +8,7 @@
 #include <GoogleTest/Library/MockVariablePolicyHelperLib.h>
 
 MOCK_INTERFACE_DEFINITION (MockVariablePolicyHelperLib);
+MOCK_FUNCTION_DEFINITION (MockVariablePolicyHelperLib, CreateBasicVariablePolicy, 8, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockVariablePolicyHelperLib, CreateVarStateVariablePolicy, 10, EFIAPI);
 MOCK_FUNCTION_DEFINITION (MockVariablePolicyHelperLib, RegisterBasicVariablePolicy, 8, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockVariablePolicyHelperLib, RegisterVarStateVariablePolicy, 10, EFIAPI);
