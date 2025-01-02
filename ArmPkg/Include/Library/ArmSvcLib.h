f189e4ecd5b881f7cca3123ecfd7dd1914212cde
Kun Qin <kuqin@microsoft.com>
2025-01-02 14:44:40 -0800

    ArmPkg: ArmSvcLib: Support all 18 registers
    This extends ARM_SVC_ARGS to support all x0-x17 registers use case for
    FF-A.
    
    The ArmCallSvc function is also updated to support 18-register usage.
    
    Co-authored-by: Olivier Deprez <Olivier.Deprez@arm.com>
    Signed-off-by: Kun Qin <kun.qin@microsoft.com>

--- ArmPkg/Include/Library/ArmSvcLib.h
+++ ArmPkg/Include/Library/ArmSvcLib.h
@@ -22,6 +22,16 @@ typedef struct {
   UINTN    Arg5;
   UINTN    Arg6;
   UINTN    Arg7;
+  UINTN    Arg8;
+  UINTN    Arg9;
+  UINTN    Arg10;
+  UINTN    Arg11;
+  UINTN    Arg12;
+  UINTN    Arg13;
+  UINTN    Arg14;
+  UINTN    Arg15;
+  UINTN    Arg16;
+  UINTN    Arg17;
 } ARM_SVC_ARGS;
 
 /**
