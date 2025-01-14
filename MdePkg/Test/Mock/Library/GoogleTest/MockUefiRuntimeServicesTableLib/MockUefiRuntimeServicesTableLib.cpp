e93fd64cbf6b833b783046b006fdaa683bcbecfe
v-frankchou <v-frankchou@microsoft.com>
2025-01-14 10:44:33 +0800

    MdePkg: Update MockUefiRuntimeServicesTableLib
    Signed-off-by: v-frankchou <v-frankchou@microsoft.com>

--- MdePkg/Test/Mock/Library/GoogleTest/MockUefiRuntimeServicesTableLib/MockUefiRuntimeServicesTableLib.cpp
+++ MdePkg/Test/Mock/Library/GoogleTest/MockUefiRuntimeServicesTableLib/MockUefiRuntimeServicesTableLib.cpp
@@ -8,32 +8,48 @@
 
 MOCK_INTERFACE_DEFINITION (MockUefiRuntimeServicesTableLib);
 
-MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_GetVariable, 5, EFIAPI);
-MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_SetVariable, 5, EFIAPI);
 MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_GetTime, 2, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_SetTime, 1, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_GetWakeupTime, 3, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_SetWakeupTime, 2, EFIAPI);
+
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_SetVirtualAddressMap, 4, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_ConvertPointer, 2, EFIAPI);
+
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_GetVariable, 5, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_GetNextVariableName, 3, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_SetVariable, 5, EFIAPI);
+
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_GetNextHighMonotonicCount, 1, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_ResetSystem, 4, EFIAPI);
+
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_UpdateCapsule, 3, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_QueryCapsuleCapabilities, 4, EFIAPI);
+
+MOCK_FUNCTION_DEFINITION (MockUefiRuntimeServicesTableLib, gRT_QueryVariableInfo, 4, EFIAPI);
 
 static EFI_RUNTIME_SERVICES  localRt = {
-  { 0 },            // EFI_TABLE_HEADER
+  { 0 },                          // EFI_TABLE_HEADER
 
-  gRT_GetTime,      // EFI_GET_TIME
-  NULL,             // EFI_SET_TIME
-  NULL,             // EFI_GET_WAKEUP_TIME
-  NULL,             // EFI_SET_WAKEUP_TIME
+  gRT_GetTime,                    // EFI_GET_TIME
+  gRT_SetTime,                    // EFI_SET_TIME
+  gRT_GetWakeupTime,              // EFI_GET_WAKEUP_TIME
+  gRT_SetWakeupTime,              // EFI_SET_WAKEUP_TIME
 
-  NULL,             // EFI_SET_VIRTUAL_ADDRESS_MAP
-  NULL,             // EFI_CONVERT_POINTER
+  gRT_SetVirtualAddressMap,       // EFI_SET_VIRTUAL_ADDRESS_MAP
+  gRT_ConvertPointer,             // EFI_CONVERT_POINTER
 
-  gRT_GetVariable,  // EFI_GET_VARIABLE
-  NULL,             // EFI_GET_NEXT_VARIABLE_NAME
-  gRT_SetVariable,  // EFI_SET_VARIABLE
+  gRT_GetVariable,                // EFI_GET_VARIABLE
+  gRT_GetNextVariableName,        // EFI_GET_NEXT_VARIABLE_NAME
+  gRT_SetVariable,                // EFI_SET_VARIABLE
 
-  NULL,             // EFI_GET_NEXT_HIGH_MONO_COUNT
-  NULL,             // EFI_RESET_SYSTEM
+  gRT_GetNextHighMonotonicCount,  // EFI_GET_NEXT_HIGH_MONO_COUNT
+  gRT_ResetSystem,                // EFI_RESET_SYSTEM
 
-  NULL,             // EFI_UPDATE_CAPSULE
-  NULL,             // EFI_QUERY_CAPSULE_CAPABILITIES
+  gRT_UpdateCapsule,              // EFI_UPDATE_CAPSULE
+  gRT_QueryCapsuleCapabilities,   // EFI_QUERY_CAPSULE_CAPABILITIES
 
-  NULL,             // EFI_QUERY_VARIABLE_INFO
+  gRT_QueryVariableInfo,          // EFI_QUERY_VARIABLE_INFO
 };
 
 extern "C" {
