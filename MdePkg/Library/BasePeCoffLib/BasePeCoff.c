tianocore/edk2

commit 6278bbb89822c598fcd0637ae74174e367895c84

MdePkg: Use SafeIntLib to handle overflow
Used SafeIntLib to handle the overflow check in
PeCoffLoaderRelocateImage

Signed-off-by: Sachin Ganesh <sachinganesh@ami.com>

MdePkg/Library/BasePeCoffLib/BasePeCoff.c
@@ -976,6 +976,7 @@ PeCoffLoaderRelocateImage (
   PHYSICAL_ADDRESS                     BaseAddress;
   UINT32                               NumberOfRvaAndSizes;
   UINT32                               TeStrippedOffset;
+  UINT32                               EndAddress;
 
   ASSERT (ImageContext != NULL);
 
@@ -1055,15 +1056,20 @@ PeCoffLoaderRelocateImage (
     RelocDir = &Hdr.Te->DataDirectory[0];
   }
 
-  if ((RelocDir != NULL) && (RelocDir->Size > 0) && ((RelocDir->Size - 1) < (MAX_UINT32 - RelocDir->VirtualAddress))) {
-    RelocBase    = (EFI_IMAGE_BASE_RELOCATION *)PeCoffLoaderImageAddress (ImageContext, RelocDir->VirtualAddress, TeStrippedOffset);
-    RelocBaseEnd = (EFI_IMAGE_BASE_RELOCATION *)PeCoffLoaderImageAddress (
-                                                  ImageContext,
-                                                  RelocDir->VirtualAddress + RelocDir->Size - 1,
-                                                  TeStrippedOffset
-                                                  );
+  if ((RelocDir != NULL) && (RelocDir->Size > 0)) {
+    Status = SafeUint32Add (RelocDir->VirtualAddress, (RelocDir->Size - 1), &EndAddress);
+    if (!RETURN_ERROR (Status)) {
+      RelocBase    = (EFI_IMAGE_BASE_RELOCATION *)PeCoffLoaderImageAddress (ImageContext, RelocDir->VirtualAddress, TeStrippedOffset);
+      RelocBaseEnd = (EFI_IMAGE_BASE_RELOCATION *)PeCoffLoaderImageAddress (
+                                                    ImageContext,
+                                                    EndAddress,
+                                                    TeStrippedOffset
+                                                    );
+    }
+
     if ((RelocBase == NULL) || (RelocBaseEnd == NULL) || ((UINTN)RelocBaseEnd < (UINTN)RelocBase)) {
       ImageContext->ImageError = IMAGE_ERROR_FAILED_RELOCATION;
+      DEBUG ((DEBUG_ERROR, "Relocation block is not valid\n"));
       return RETURN_LOAD_ERROR;
     }
   } else {

commit 362e3ea73394f5e0d88376ff6ea21b5d227bdd70

MdePkg: Fix Clang Build Error
Fixed Clang build error introduced by unintialized variables in
https://github.com/tianocore/edk2/commit/6278bbb89822c598fcd0637ae74174e367895c84

Signed-off-by: Sachin Ganesh <sachinganesh@ami.com>

MdePkg/Library/BasePeCoffLib/BasePeCoff.c
@@ -1056,6 +1056,8 @@ PeCoffLoaderRelocateImage (
     RelocDir = &Hdr.Te->DataDirectory[0];
   }
 
+  RelocBase    = NULL;
+  RelocBaseEnd = NULL;
   if ((RelocDir != NULL) && (RelocDir->Size > 0)) {
     Status = SafeUint32Add (RelocDir->VirtualAddress, (RelocDir->Size - 1), &EndAddress);
     if (!RETURN_ERROR (Status)) {
@@ -1072,11 +1074,6 @@ PeCoffLoaderRelocateImage (
       DEBUG ((DEBUG_ERROR, "Relocation block is not valid\n"));
       return RETURN_LOAD_ERROR;
     }
-  } else {
-    //
-    // Set base and end to bypass processing below.
-    //
-    RelocBase = RelocBaseEnd = NULL;
   }
 
   RelocBaseOrg = RelocBase;