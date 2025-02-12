8bd4dbecdbb86ec7bae5c824747cecedfea3f6c6
Dhaval <dhaval@rivosinc.com>
2025-02-12 20:11:47 +0530

    StandaloneMmPkg/RiscV64: Create boot information hob
    EFI_RISCV_SMM_PAYLOAD_INFO describes the current information
    passing structure from secure monitor to MM.
    
    TODO: Later at some point of time we would like to move to
    different interface like DT or equivalent.
    
    Signed-off-by: Dhaval Sharma <dhaval@rivosinc.com>
    Signed-off-by: Yong Li <yong.li@intel.com>
    Signed-off-by: Tuan Phan <tphan@ventanamicro.com>

--- StandaloneMmPkg/Library/StandaloneMmCoreEntryPoint/RiscV64/StandaloneMmCoreEntryPoint.c
+++ StandaloneMmPkg/Library/StandaloneMmCoreEntryPoint/RiscV64/StandaloneMmCoreEntryPoint.c
@@ -8,6 +8,62 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #include <PiMm.h>
 
+#include <StandaloneMmCpu.h>
+#include <StandaloneMmCoreEntryPoint.h>
+
+/**
+  Retrieve and print boot information provided by privileged firmware.
+
+  This function extracts and logs the RISC-V SMM boot info structure passed by
+  the secure firmware. It prints memory layout, communication buffer, and CPU topology.
+
+  @param[in] PayloadInfoAddress  Address of the EFI_RISCV_SMM_PAYLOAD_INFO structure.
+
+  @retval EFI_RISCV_SMM_PAYLOAD_INFO*   Pointer to parsed boot information if valid.
+  @retval NULL                          If the input is NULL.
+**/
+EFI_RISCV_SMM_PAYLOAD_INFO *
+GetAndPrintBootinformation (
+  IN VOID  *PayloadInfoAddress
+  )
+{
+  EFI_RISCV_SMM_PAYLOAD_INFO  *BootInfo;
+  EFI_RISCV_SMM_CPU_INFO      *CpuInfo;
+  UINTN                        Index;
+
+  BootInfo = (EFI_RISCV_SMM_PAYLOAD_INFO *)PayloadInfoAddress;
+
+  if (BootInfo == NULL) {
+    DEBUG ((DEBUG_ERROR, "GetAndPrintBootinformation: PayloadInfoAddress is NULL\n"));
+    return NULL;
+  }
+
+  DEBUG ((DEBUG_INFO, "=== SMM Boot Info ===\n"));
+  DEBUG ((DEBUG_INFO, "  NumMmMemRegions : 0x%x\n", BootInfo->NumMmMemRegions));
+  DEBUG ((DEBUG_INFO, "  MmMemBase       : 0x%lx\n", BootInfo->MmMemBase));
+  DEBUG ((DEBUG_INFO, "  MmMemLimit      : 0x%lx\n", BootInfo->MmMemLimit));
+  DEBUG ((DEBUG_INFO, "  MmImageBase     : 0x%lx\n", BootInfo->MmImageBase));
+  DEBUG ((DEBUG_INFO, "  MmStackBase     : 0x%lx\n", BootInfo->MmStackBase));
+  DEBUG ((DEBUG_INFO, "  MmHeapBase      : 0x%lx\n", BootInfo->MmHeapBase));
+  DEBUG ((DEBUG_INFO, "  MmNsCommBufBase : 0x%lx\n", BootInfo->MmNsCommBufBase));
+  DEBUG ((DEBUG_INFO, "  MmImageSize     : 0x%x\n", BootInfo->MmImageSize));
+  DEBUG ((DEBUG_INFO, "  MmPcpuStackSize : 0x%x\n", BootInfo->MmPcpuStackSize));
+  DEBUG ((DEBUG_INFO, "  MmHeapSize      : 0x%x\n", BootInfo->MmHeapSize));
+  DEBUG ((DEBUG_INFO, "  MmNsCommBufSize : 0x%x\n", BootInfo->MmNsCommBufSize));
+  DEBUG ((DEBUG_INFO, "  NumCpus         : 0x%x\n", BootInfo->NumCpus));
+
+  CpuInfo = (EFI_RISCV_SMM_CPU_INFO *)&BootInfo->CpuInfo;
+
+  for (Index = 0; Index < BootInfo->NumCpus; Index++) {
+    DEBUG ((DEBUG_INFO, "  CPU[%u] ProcessorId: 0x%lx, Package: %u, Core: %u\n",
+            Index,
+            CpuInfo[Index].ProcessorId,
+            CpuInfo[Index].Package,
+            CpuInfo[Index].Core));
+  }
+
+  return BootInfo;
+}
 /**
   The entry point of Standalone MM Foundation.
 
@@ -22,5 +78,13 @@ CModuleEntryPoint (
   IN VOID    *PayloadInfoAddress
   )
 {
+  EFI_RISCV_SMM_PAYLOAD_INFO  *BootInfo;
+  VOID                        *HobStart;
+
+  BootInfo = GetAndPrintBootinformation (PayloadInfoAddress);
+  if (BootInfo == NULL) {
+    return;
+  }
+
   ASSERT (0);
 }
