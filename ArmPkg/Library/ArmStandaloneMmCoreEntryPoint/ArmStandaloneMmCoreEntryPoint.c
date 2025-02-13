2ff6793af73d8ab48909868b0041bc073f99fa0a
Michael Kubacki <michael.kubacki@microsoft.com>
2025-02-12 19:37:34 -0500

    ArmPkg/ArmStandaloneMmCoreEntryPoint: Fix cast on clang
    Compilation error:
    
    ```
    ArmStandaloneMmCoreEntryPoint.c:1041:48: error: cast to smaller
      integer type 'unsigned long' from 'void *'
      [-Werror,-Wvoid-pointer-to-int-cast]
    
      1041 |   DEBUG ((DEBUG_INFO, "Start Dump Hob: %lx\n",
                 (unsigned long)HobStart));
    ```
    
    The `%p` specifer is now used to print the pointers.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- ArmPkg/Library/ArmStandaloneMmCoreEntryPoint/ArmStandaloneMmCoreEntryPoint.c
+++ ArmPkg/Library/ArmStandaloneMmCoreEntryPoint/ArmStandaloneMmCoreEntryPoint.c
@@ -1038,9 +1038,9 @@ CEntryPoint (
     goto finish;
   }
 
-  DEBUG ((DEBUG_INFO, "Start Dump Hob: %lx\n", (unsigned long)HobStart));
+  DEBUG ((DEBUG_INFO, "Start Dump Hob: %p\n", HobStart));
   DumpPhitHob (HobStart);
-  DEBUG ((DEBUG_INFO, "End Dump Hob: %lx\n", (unsigned long)HobStart));
+  DEBUG ((DEBUG_INFO, "End Dump Hob: %p\n", HobStart));
 
   FvHob = GetNextHob (EFI_HOB_TYPE_FV, HobStart);
   if (FvHob == NULL) {
