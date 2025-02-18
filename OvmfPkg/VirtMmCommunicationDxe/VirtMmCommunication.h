f474d61f6cca8aa7acc8b531545ac93eb7c21c97
Gerd Hoffmann <kraxel@redhat.com>
2025-02-18 11:36:07 +0100

    OvmfPkg: add new VirtMmCommunicationDxe driver
    EFI driver implementing the MmCommunication2Protocol,
    but instead of trapping to MM mode (SMM on x86, el3 on arm)
    trap to the host, using a virtual device.
    
    Moving the efi variable management to the host allows to have
    persistent efi variables without flash storage, and it also
    allows to have secure boot support without requiring MM mode
    to protect variable storage in flash.
    
    On x86 etc/hardware-info is used for device discovery, on arm
    the FDT will be consulted.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

---
+++ OvmfPkg/VirtMmCommunicationDxe/VirtMmCommunication.h
@@ -0,0 +1,53 @@
+/** @file
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _VIRT_MM_COMM_DXE_H_
+#define _VIRT_MM_COMM_DXE_H_
+
+/* communication buffer */
+
+#define MAX_BUFFER_SIZE  (64 * 1024)
+
+extern VOID                  *mCommunicateBuffer;
+extern EFI_PHYSICAL_ADDRESS  mCommunicateBufferPhys;
+extern UINT64                mUefiVarsAddr;
+extern BOOLEAN               mUsePioTransfer;
+
+/* arch specific hooks */
+
+EFI_STATUS
+EFIAPI
+VirtMmHwFind (
+  VOID
+  );
+
+EFI_STATUS
+EFIAPI
+VirtMmHwInit (
+  VOID
+  );
+
+EFI_STATUS
+EFIAPI
+VirtMmHwComm (
+  VOID
+  );
+
+EFI_STATUS
+EFIAPI
+VirtMmHwVirtMap (
+  VOID
+  );
+
+EFI_STATUS
+EFIAPI
+VirtMmHwPioTransfer (
+  VOID     *Buffer,
+  UINT32   BufferSize,
+  BOOLEAN  ToDevice
+  );
+
+#endif /* _VIRT_MM_COMM_DXE_H_ */
