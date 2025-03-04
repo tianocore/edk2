8762b58d60c49b02f21d6919de7d55a06510a07d
Luigi Leonardi <leonardi@redhat.com>
2025-03-04 12:27:55 +0100

    OvmfPkg/PlatformBootManagerCommonLib: Set UiApp as an optional boot option
    Introduce gUiAppFileGuid: it has the same value of UiApp guid defined in
    the .inf file. This is used to register UiApp as a boot entry in the
    BootManagerMenu.
    
    This registration is done in PlatformBootManagerBeforeConsole because
    it must be done before the hotkeys are registered. This is because
    in a system with hotkeys still bound to UiApp, but with firmware disabled,
    you can still boot into the latter by hitting ESC or F2 during boot.
    
    UiApp can be enabled/disabled using fw_cfg option FirmwareSetupSupport
    
    Signed-off-by: Luigi Leonardi <leonardi@redhat.com>

--- OvmfPkg/Library/PlatformBootManagerLibLight/PlatformBm.c
+++ OvmfPkg/Library/PlatformBootManagerLibLight/PlatformBm.c
@@ -777,6 +777,7 @@ PlatformBootManagerBeforeConsole (
   UINT16         FrontPageTimeout;
   RETURN_STATUS  PcdStatus;
   EFI_STATUS     Status;
+  BOOLEAN        FirmwareSetupEnabled;
 
   //
   // Signal EndOfDxe PI Event
@@ -876,6 +877,22 @@ PlatformBootManagerBeforeConsole (
     FrontPageTimeout,
     Status
     ));
+
+  Status = QemuFwCfgParseBool (
+             "opt/org.tianocore/FirmwareSetupSupport",
+             &FirmwareSetupEnabled
+             );
+
+  if (RETURN_ERROR (Status)) {
+    FirmwareSetupEnabled = TRUE;
+  }
+
+  PlatformRegisterFvBootOption (
+    &gUiAppFileGuid,
+    L"EFI Firmware Setup",
+    LOAD_OPTION_ACTIVE | LOAD_OPTION_CATEGORY_APP,
+    FirmwareSetupEnabled
+    );
 
   //
   // Register platform-specific boot options and keyboard shortcuts.
