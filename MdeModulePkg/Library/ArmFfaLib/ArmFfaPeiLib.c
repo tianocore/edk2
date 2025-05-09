tianocore/edk2

commit 26fb5edff3976a42b640d83721b4897935cb6598

MdeModulePkg/ArmFfaLib: Add depex on gEfiPeiMemoryDiscoveredPpiGuid
If ArmFfaLibRxTxMap is called before permanent memory is installed the
memory allocated for the FF-A buffers will be migrated to the permanent
memory after it is installed without updating gArmFfaRxTxBufferInfoGuid
or unmapping the old buffers and mapping the updated buffers.

An ASSERT in MemoryServices at ExitBootServices is triggered when
ArmFfaDxeLib tries to call FreeAlignedPages on the original buffer
reference.

Add depex on gEfiPeiMemoryDiscoveredPpiGuid for ArmFfaPeiLib so any
Peims that use FF-A are only dispatched after permanent memory is
available.

Signed-off-by: Anubhav Raina <anubhav.raina@arm.com>

MdeModulePkg/Library/ArmFfaLib/ArmFfaPeiLib.inf
@@ -1,7 +1,7 @@
 ## @file
 #  Provides FF-A ABI Library used in PEI Driver.
 #
-#  Copyright (c) 2024, Arm Limited. All rights reserved.<BR>
+#  Copyright (c) 2024-2025, Arm Limited. All rights reserved.<BR>
 #
 #  SPDX-License-Identifier: BSD-2-Clause-Patent
 ##
@@ -43,3 +43,6 @@
 
 [Guids]
   gArmFfaRxTxBufferInfoGuid
+
+[Depex]
+  gEfiPeiMemoryDiscoveredPpiGuid

commit b4254d3f0e1fe2ee4164d94a52d2ee4dba4467ce

MdeModulePkg/Library: make ArmFfaPeiLib available early PEIM stage
commit 26fb5edff397 ("MdeModulePkg/ArmFfaLib: Add depex on gEfiPeiMemoryDiscoveredPpiGuid")
restricts ArmFfaPeiLib usage only after PEI phase can be used permanent
memory. However, This would be problem for PEIM which runs before
gEfiPeiMemoryDiscoveredPpiGuid Ppi installed. (i.e) Tcg2Pei PEIM.

To resolve this, remove dependency on gEfiPeiMemoryDiscoveredPpiGuid
and let ArmFfaPeiLib remap the Rx/Tx buffer after
gEfiPeiMemoryDiscoveredPpiGuid is installed so that ArmFfaPeiLib can be
used with temporary memory.

Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>
Fixes: 26fb5edff397 ("MdeModulePkg/ArmFfaLib: Add depex on gEfiPeiMemoryDiscoveredPpiGuid")

MdeModulePkg/Library/ArmFfaLib/ArmFfaPeiLib.c
@@ -35,6 +35,68 @@
 #include "ArmFfaRxTxMap.h"
 
 /**
+  Notification service to be called when gEfiPeiMemoryDiscoveredPpiGuid is installed.
+  This function change reamp Rx/Tx buffer with permanent memory from
+  temporary Rx/Tx buffer.
+
+  @param  PeiServices                 Indirect reference to the PEI Services Table.
+  @param  NotifyDescriptor            Address of the notification descriptor data structure.
+                                      Type EFI_PEI_NOTIFY_DESCRIPTOR is defined above.
+  @param  Ppi                         Address of the PPI that was installed.
+
+  @retval EFI_STATUS                  This function will install a PPI to PPI database.
+                                      The status code will be the code for (*PeiServices)->InstallPpi.
+
+**/
+STATIC
+EFI_STATUS
+EFIAPI
+PeiServicesMemoryDiscoveredNotifyCallback (
+  IN EFI_PEI_SERVICES           **PeiServices,
+  IN EFI_PEI_NOTIFY_DESCRIPTOR  *NotifyDescriptor,
+  IN VOID                       *Ppi
+  )
+{
+  EFI_STATUS                 Status;
+  EFI_HOB_GUID_TYPE          *RxTxBufferHob;
+  ARM_FFA_RX_TX_BUFFER_INFO  *BufferInfo;
+
+  RxTxBufferHob = GetFirstGuidHob (&gArmFfaRxTxBufferInfoGuid);
+  ASSERT (RxTxBufferHob != NULL);
+  BufferInfo = GET_GUID_HOB_DATA (RxTxBufferHob);
+
+  /*
+   * temporary memory doesn't need to be free...
+   * otherwise PEI memory manager using permanent memory will be confused...
+   */
+  PcdSet64S (PcdFfaTxBuffer, 0x00);
+  PcdSet64S (PcdFfaRxBuffer, 0x00);
+
+  Status = ArmFfaLibRxTxUnmap ();
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  Status = ArmFfaLibRxTxMap ();
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  BufferInfo->TxBufferAddr = (VOID *)(UINTN)PcdGet64 (PcdFfaTxBuffer);
+  BufferInfo->TxBufferSize = PcdGet64 (PcdFfaTxRxPageCount) * EFI_PAGE_SIZE;
+  BufferInfo->RxBufferAddr = (VOID *)(UINTN)PcdGet64 (PcdFfaRxBuffer);
+  BufferInfo->RxBufferSize = PcdGet64 (PcdFfaTxRxPageCount) * EFI_PAGE_SIZE;
+
+  return EFI_SUCCESS;
+}
+
+STATIC EFI_PEI_NOTIFY_DESCRIPTOR  mNotifyOnPeiMemoryDiscovered = {
+  (EFI_PEI_PPI_DESCRIPTOR_NOTIFY_CALLBACK | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
+  &gEfiPeiMemoryDiscoveredPpiGuid,
+  PeiServicesMemoryDiscoveredNotifyCallback
+};
+
+/**
   ArmFfaLib Constructor.
 
   @param [in]   FileHandle        File Handle
@@ -54,6 +116,8 @@ ArmFfaPeiLibConstructor (
   EFI_STATUS                 Status;
   EFI_HOB_GUID_TYPE          *RxTxBufferHob;
   ARM_FFA_RX_TX_BUFFER_INFO  *BufferInfo;
+  VOID                       *Dummy;
+  BOOLEAN                    TempMem;
 
   Status = ArmFfaLibCommonInit ();
   if (EFI_ERROR (Status)) {
@@ -73,6 +137,15 @@ ArmFfaPeiLibConstructor (
 
     return Status;
   }
+
+  Status = (*PeiServices)->LocatePpi (
+                             PeiServices,
+                             &gEfiPeiMemoryDiscoveredPpiGuid,
+                             0,
+                             NULL,
+                             &Dummy
+                             );
+  TempMem = (EFI_ERROR (Status)) ? TRUE : FALSE;
 
   RxTxBufferHob = GetFirstGuidHob (&gArmFfaRxTxBufferInfoGuid);
   if (RxTxBufferHob == NULL) {
@@ -94,6 +167,11 @@ ArmFfaPeiLibConstructor (
     BufferInfo->TxBufferSize = PcdGet64 (PcdFfaTxRxPageCount) * EFI_PAGE_SIZE;
     BufferInfo->RxBufferAddr = (VOID *)(UINTN)PcdGet64 (PcdFfaRxBuffer);
     BufferInfo->RxBufferSize = PcdGet64 (PcdFfaTxRxPageCount) * EFI_PAGE_SIZE;
+
+    if (TempMem) {
+      Status = (*PeiServices)->NotifyPpi (PeiServices, &mNotifyOnPeiMemoryDiscovered);
+      ASSERT_EFI_ERROR (Status);
+    }
   }
 
   return EFI_SUCCESS;

MdeModulePkg/Library/ArmFfaLib/ArmFfaPeiLib.inf
@@ -44,5 +44,5 @@
 [Guids]
   gArmFfaRxTxBufferInfoGuid
 
-[Depex]
+[Ppis]
   gEfiPeiMemoryDiscoveredPpiGuid