ca3e5c0f31a4bce8aa3c64438bba02b198a3b0cf
Michael Kubacki <michael.kubacki@microsoft.com>
2025-05-13 11:03:52 -0700

    MdeModulePkg: SmmCommunicationBuffer cumulative codeql issues.
    Running Codeql on MdeModulePkg/Universal/SmmCommunicationBuffer drivers results
    in codeql errors stemming from missing null tests.
    
    Signed-off-by: Aaron Pop <aaronpop@microsoft.com>
    
    Co-authored-by: Michael Kubacki <michael.kubacki@microsoft.com>
    Co-authored-by: Taylor Beebe <tabeebe@microsoft.com>
    Co-authored-by: pohanch <125842322+pohanch@users.noreply.github.com>
    Co-authored-by: kenlautner <85201046+kenlautner@users.noreply.github.com>
    Co-authored-by: Oliver Smith-Denny <osde@linux.microsoft.com>
    Co-authored-by: Sean Brogan <sean.brogan@microsoft.com>
    Co-authored-by: Aaron <aaronpop@microsoft>

--- MdeModulePkg/Universal/SmmCommunicationBufferDxe/SmmCommunicationBufferDxe.c
+++ MdeModulePkg/Universal/SmmCommunicationBufferDxe/SmmCommunicationBufferDxe.c
@@ -63,6 +63,10 @@ SmmCommunicationBufferEntryPoint (
   //
   PiSmmCommunicationRegionTable = AllocateReservedPool (sizeof (EDKII_PI_SMM_COMMUNICATION_REGION_TABLE) + DescriptorSize);
   ASSERT (PiSmmCommunicationRegionTable != NULL);
+  if (PiSmmCommunicationRegionTable == NULL) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
   ZeroMem (PiSmmCommunicationRegionTable, sizeof (EDKII_PI_SMM_COMMUNICATION_REGION_TABLE) + DescriptorSize);
 
   PiSmmCommunicationRegionTable->Version         = EDKII_PI_SMM_COMMUNICATION_REGION_TABLE_VERSION;
