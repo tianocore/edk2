3d95a35089fe0013a46b304a3ae52649b4d3d631
Michael Kubacki <michael.kubacki@microsoft.com>
2025-05-22 19:29:03 -0400

    ArmPkg: Fix typos
    Corrects some typos.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- ArmPkg/Library/ArmLib/AArch64/AArch64Lib.c
+++ ArmPkg/Library/ArmLib/AArch64/AArch64Lib.c
@@ -54,7 +54,7 @@ ArmHasCcidx (
   Checks whether the CPU implements the Virtualization Host Extensions.
 
   @retval TRUE  FEAT_VHE is implemented.
-  @retval FALSE FEAT_VHE is not mplemented.
+  @retval FALSE FEAT_VHE is not implemented.
 **/
 BOOLEAN
 EFIAPI
@@ -69,7 +69,7 @@ ArmHasVhe (
   Checks whether the CPU implements the Trace Buffer Extension.
 
   @retval TRUE  FEAT_TRBE is implemented.
-  @retval FALSE FEAT_TRBE is not mplemented.
+  @retval FALSE FEAT_TRBE is not implemented.
 **/
 BOOLEAN
 EFIAPI
@@ -84,7 +84,7 @@ ArmHasTrbe (
   Checks whether the CPU implements the Embedded Trace Extension.
 
   @retval TRUE  FEAT_ETE is implemented.
-  @retval FALSE FEAT_ETE is not mplemented.
+  @retval FALSE FEAT_ETE is not implemented.
 **/
 BOOLEAN
 EFIAPI
