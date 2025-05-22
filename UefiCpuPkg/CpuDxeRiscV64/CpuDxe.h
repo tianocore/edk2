b8cb8c1fb23b56151d4901ec1c6f0f751417f7a6
Tuan Phan <tphan@ventanamicro.com>
2025-05-22 16:34:12 -0700

    UefiCpuPkg/CpuDxeRiscV64: Retrieve booting info from SEC HOB data
    The booting info is now stored in SEC HOB GUID data.
    
    Signed-off-by: Tuan Phan <tphan@ventanamicro.com>

--- UefiCpuPkg/CpuDxeRiscV64/CpuDxe.h
+++ UefiCpuPkg/CpuDxeRiscV64/CpuDxe.h
@@ -12,6 +12,7 @@
 
 #include <PiDxe.h>
 
+#include <Guid/RiscVSecHobData.h>
 #include <Protocol/Cpu.h>
 #include <Protocol/RiscVBootProtocol.h>
 #include <Library/BaseRiscVSbiLib.h>
@@ -22,6 +23,7 @@
 #include <Library/CpuExceptionHandlerLib.h>
 #include <Library/CpuLib.h>
 #include <Library/DebugLib.h>
+#include <Library/HobLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiDriverEntryPoint.h>
 #include <Register/RiscV64/RiscVEncoding.h>
