c9f4920b86b004af80de889ee5fbac221d1fbdfa
Pierre Gondois <pierre.gondois@arm.com>
2025-05-28 10:37:27 +0200

    DynamicTablesPkg: Add GetProximityDomainId() to CmObjHelperLib
    Proximity domains Ids should now be described using the
    CM_ARCH_COMMON_PROXIMITY_DOMAIN_INFO object. This adds a level
    of indirection: a Token allows to retrieve a
    CM_ARCH_COMMON_PROXIMITY_DOMAIN_INFO object, and this object might
    either have a hard-coded Id, or rely on the MetadataObjLib to
    generate an Id.
    
    Add a GetProximityDomainId () function to the CmObjHelperLib to
    factorize this level of abstraction and allow other generators
    to re-use the logic.
    
    Signed-off-by: Pierre Gondois <pierre.gondois@arm.com>

--- DynamicTablesPkg/Include/Library/CmObjHelperLib.h
+++ DynamicTablesPkg/Include/Library/CmObjHelperLib.h
@@ -24,4 +24,35 @@ CheckAcpiTablePresent (
   IN        ESTD_ACPI_TABLE_ID                            AcpiTableId
   );
 
+/** Get a Proximity Domain Id.
+
+  Proximity Domain Id are now to be placed in
+  CM_ARCH_COMMON_PROXIMITY_DOMAIN_INFO objects rather than in the various
+  CmObj using them. This function handles the logic in the selection
+  of the ProximityDomainId to use.
+
+  Proximity Domain Id should be preferably placed in
+  CM_ARCH_COMMON_PROXIMITY_DOMAIN_INFO objects now.
+
+  @param [in]  CfgMgrProtocol   Pointer to the Configuration Manager
+                                Protocol Interface.
+  @param [in]  DefaultDomainId  Default per-CmObj Proximity Domain Id.
+                                The CM_ARCH_COMMON_PROXIMITY_DOMAIN_INFO
+                                should be preferably used.
+  @param [in]  Token            Token referencing a
+                                CM_ARCH_COMMON_PROXIMITY_DOMAIN_INFO object.
+  @param [out] DomainId         If Success, contains DomainId to use.
+
+  @retval EFI_SUCCESS           Table generated successfully.
+  @retval EFI_INVALID_PARAMETER The table pointer is NULL or invalid.
+**/
+EFI_STATUS
+EFIAPI
+GetProximityDomainId (
+  IN CONST  EDKII_CONFIGURATION_MANAGER_PROTOCOL  *CfgMgrProtocol,
+  IN        UINT32                                DefaultDomainId,
+  IN        CM_OBJECT_TOKEN                       Token,
+  OUT       UINT32                                *DomainId
+  );
+
 #endif // CM_OBJ_HELPER_LIB_H_
