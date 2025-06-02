6a02628d189f4cf59b72e136bb7ac5bc4a7717a4
TsunFeng Huang (AMI US Holdings Inc) <v-tsuhuang@microsoft.com>
2025-06-02 19:45:57 +0800

    Update description and Support SMM and MM protocol
    Signed-off-by: TsunFeng <v-tsuhuang@microsoft.com>

--- MdePkg/Test/Mock/Include/GoogleTest/Protocol/MockSxDispatch.h
+++ MdePkg/Test/Mock/Include/GoogleTest/Protocol/MockSxDispatch.h
@@ -1,12 +1,12 @@
-/** @file MockSmmSxDispatch2.h
-  This file declares a mock of SMM Software Dispatch Protocol
+/** @file MockSxDispatch.h
+  This file declares a mock of SMM/MM Sx Dispatch Protocol
 
   Copyright (c) Microsoft Corporation.
   SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
-#ifndef MOCK_SMM_SX_DISPATCH2_H_
-#define MOCK_SMM_SX_DISPATCH2_H_
+#ifndef MOCK_SX_DISPATCH_H_
+#define MOCK_SX_DISPATCH_H_
 
 #include <Library/GoogleTestLib.h>
 #include <Library/FunctionMockLib.h>
@@ -16,9 +16,9 @@ extern "C" {
   #include <Protocol/SmmSxDispatch2.h>
 }
 
-// Declarations to handle usage of the EFI_SMM_SX_DISPATCH2_PROTOCOL
-struct MockEfiSmmSxDispatch2Protocol {
-  MOCK_INTERFACE_DECLARATION (MockEfiSmmSxDispatch2Protocol);
+// Declarations to handle usage of the Sx Dispatch Protocol
+struct MockEfiSxDispatch2Protocol {
+  MOCK_INTERFACE_DECLARATION (MockEfiSxDispatch2Protocol);
 
   MOCK_FUNCTION_DECLARATION (
     EFI_STATUS,
@@ -39,9 +39,9 @@ struct MockEfiSmmSxDispatch2Protocol {
     );
 };
 
-MOCK_INTERFACE_DEFINITION (MockEfiSmmSxDispatch2Protocol);
-MOCK_FUNCTION_DEFINITION (MockEfiSmmSxDispatch2Protocol, Register, 4, EFIAPI);
-MOCK_FUNCTION_DEFINITION (MockEfiSmmSxDispatch2Protocol, UnRegister, 2, EFIAPI);
+MOCK_INTERFACE_DEFINITION (MockEfiSxDispatch2Protocol);
+MOCK_FUNCTION_DEFINITION (MockEfiSxDispatch2Protocol, Register, 4, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockEfiSxDispatch2Protocol, UnRegister, 2, EFIAPI);
 
 #define MOCK_EFI_SMM_SX_DISPATCH2_PROTOCOL_INSTANCE(NAME)  \
  EFI_SMM_SX_DISPATCH2_PROTOCOL NAME##_INSTANCE = {          \
@@ -49,4 +49,9 @@ MOCK_FUNCTION_DEFINITION (MockEfiSmmSxDispatch2Protocol, UnRegister, 2, EFIAPI);
   UnRegister };                                        \
  EFI_SMM_SX_DISPATCH2_PROTOCOL  *NAME = &NAME##_INSTANCE;
 
-#endif // MOCK_SMM_SW_DISPATCH2_H_
+#define MOCK_EFI_MM_SX_DISPATCH_PROTOCOL_INSTANCE(NAME)  \
+ EFI_MM_SX_DISPATCH_PROTOCOL NAME##_INSTANCE = {          \
+  Register,                                            \
+  UnRegister };                                        \
+ EFI_MM_SX_DISPATCH_PROTOCOL  *NAME = &NAME##_INSTANCE;
+#endif // MOCK_SX_DISPATCH_H_
