tianocore/edk2

commit d6d2f68e3801760646ac02117f2467382a310d61

ArmPkg/Drivers/ArmGicDxe: Add Extended SPI support for GICv3
This commit enables extended SPI support for GicV3. GicV3 must decide,
based on the source intid, whether to access the SPI-range registers,
PPI-range registers in the redistributor, or the extended SPI-range
registers.

The protocol interfaces must also support registering an interrupt
handler with an extended SPI intid. To save ~24KB of memory, handler
allocation and access is delegated to GicV2 and GicV3. GicV2 retains the
existing handler mapping scheme using intids literally. GicV3 remaps
extended SPI intids to be immediately after the highest SPI intids.

Tested on qemu with the BSA test suite.

Signed-off-by: Nick Graves <nicholasgraves@google.com>

ArmPkg/Include/Library/ArmGicLib.h
@@ -9,10 +9,57 @@
 #ifndef ARMGIC_H_
 #define ARMGIC_H_
 
+//
+// GIC SPI and extended SPI ranges
+//
+#define ARM_GIC_ARCH_SPI_MIN      32
+#define ARM_GIC_ARCH_SPI_MAX      1019
+#define ARM_GIC_ARCH_EXT_SPI_MIN  4096
+#define ARM_GIC_ARCH_EXT_SPI_MAX  5119
+
 // GIC Distributor
 #define ARM_GIC_ICDDCR   0x000        // Distributor Control Register
 #define ARM_GIC_ICDICTR  0x004        // Interrupt Controller Type Register
 #define ARM_GIC_ICDIIDR  0x008        // Implementer Identification Register
+
+// ICDICTR is also called GICD_TYPER.
+
+// Intids per LSB for EXT_SPI_RANGE and ITLINES.
+#define ARM_GIC_ICDICTR_INTID_RANGE_RESOLUTION  (32)
+
+// Converts an register range of IntIds to the maximum IntId using Base as an
+// offset.
+#define ARM_GIC_ICDICTR_INTID_RANGE_TO_MAX_INTID(Range, Base) \
+  (ARM_GIC_ICDICTR_INTID_RANGE_RESOLUTION * ((Range) + 1) - 1 + (Base))
+
+#define ARM_GIC_ICDICTR_ITLINES_MASK   (0x1F)
+#define ARM_GIC_ICDICTR_ITLINES_SHIFT  (0)
+
+// Gets the range for SPI IntIds from TypeReg.
+#define ARM_GIC_ICDICTR_GET_SPI_RANGE(TypeReg) \
+  (((TypeReg) >> ARM_GIC_ICDICTR_ITLINES_SHIFT) & ARM_GIC_ICDICTR_ITLINES_MASK)
+
+// Converts a range of SPI IntIds to the maximum SPI IntId.
+#define ARM_GIC_ICDICTR_SPI_RANGE_TO_MAX_INTID(SpiRange) \
+  (((SpiRange) == ARM_GIC_ICDICTR_ITLINES_MASK)          \
+       ? ARM_GIC_ARCH_SPI_MAX                   \
+       : ARM_GIC_ICDICTR_INTID_RANGE_TO_MAX_INTID(SpiRange, 0))
+
+// Extracts the maximum SPI IntId from TypeReg.
+#define ARM_GIC_ICDICTR_GET_SPI_MAX_INTID(TypeReg) \
+  ARM_GIC_ICDICTR_SPI_RANGE_TO_MAX_INTID(ARM_GIC_ICDICTR_GET_EXT_SPI_RANGE(TypeReg))
+
+#define ARM_GIC_ICDICTR_EXT_SPI_ENABLED      (1 << 8) // Extended SPI enabled bit.
+#define ARM_GIC_ICDICTR_EXT_SPI_RANGE_SHIFT  (27)     // Extended SPI range position.
+#define ARM_GIC_ICDICTR_EXT_SPI_RANGE_MASK   (0x1F)   // Extended SPI range mask.
+#define ARM_GIC_ICDICTR_GET_EXT_SPI_RANGE(TypeReg)      \
+  (((TypeReg) >> ARM_GIC_ICDICTR_EXT_SPI_RANGE_SHIFT) & \
+   ARM_GIC_ICDICTR_EXT_SPI_RANGE_MASK)
+
+// Extracts the maximum EXT SPI IntId from TypeReg.
+#define ARM_GIC_ICDICTR_GET_EXT_SPI_MAX_INTID(TypeReg) \
+  ARM_GIC_ICDICTR_INTID_RANGE_TO_MAX_INTID(            \
+      ARM_GIC_ICDICTR_GET_EXT_SPI_RANGE(TypeReg), ARM_GIC_ARCH_EXT_SPI_MIN)
 
 // Each reg base below repeats for Number of interrupts / 4 (see GIC spec)
 #define ARM_GIC_ICDISR   0x080        // Interrupt Security Registers
@@ -35,7 +82,8 @@
 #define ARM_GIC_ICDSGIR  0xF00        // Software Generated Interrupt Register
 
 // GICv3 specific registers
-#define ARM_GICD_IROUTER  0x6100       // Interrupt Routing Registers
+#define ARM_GICD_IROUTER    0x6100    // Interrupt Routing Registers
+#define ARM_GICD_IROUTER_E  0x8000    // Interrupt Routing Registers
 
 // GICD_CTLR bits
 #define ARM_GIC_ICDDCR_ARE  (1 << 4)     // Affinity Routing Enable (ARE)
@@ -49,6 +97,16 @@
 #define ARM_GIC_ICDICFR_F_CONFIG1_BIT    1    // Bit number within F field
 #define ARM_GIC_ICDICFR_LEVEL_TRIGGERED  0x0  // Level triggered interrupt
 #define ARM_GIC_ICDICFR_EDGE_TRIGGERED   0x1  // Edge triggered interrupt
+
+// GICD ESPI registers
+//  These registers follow the same bit pattern as the SPI registers.
+#define ARM_GIC_ICDISR_E   0x1000   // Interrupt Security Registers
+#define ARM_GIC_ICDISER_E  0x1200   // Interrupt Set-Enable for ESPI
+#define ARM_GIC_ICDICER_E  0x1400   // Interrupt Clear-Enable Registers
+#define ARM_GIC_ICDSPR_E   0x1600   // Interrupt Set-Pending Registers
+#define ARM_GIC_ICDICPR_E  0x1800   // Interrupt Clear-Pending Registers
+#define ARM_GIC_ICDIPR_E   0x2000   // Interrupt Priority Registers
+#define ARM_GIC_ICDICFR_E  0x3000   // Interrupt Configuration Registers
 
 // GIC Redistributor
 #define ARM_GICR_CTLR_FRAME_SIZE          SIZE_64KB
@@ -107,14 +165,6 @@
 
 // Bit Mask for
 #define ARM_GIC_ICCIAR_ACKINTID  0x3FF
-
-//
-// GIC SPI and extended SPI ranges
-//
-#define ARM_GIC_ARCH_SPI_MIN      32
-#define ARM_GIC_ARCH_SPI_MAX      1019
-#define ARM_GIC_ARCH_EXT_SPI_MIN  4096
-#define ARM_GIC_ARCH_EXT_SPI_MAX  5119
 
 // GIC revision 3 specific declarations
 

ArmPkg/Drivers/ArmGicDxe/ArmGicCommonDxe.c
@@ -7,6 +7,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 --*/
 
 #include "ArmGicDxe.h"
+#include "Uefi/UefiBaseType.h"
 
 // Making this global saves a few bytes in image size
 EFI_HANDLE  gHardwareInterruptHandle = NULL;
@@ -14,11 +15,55 @@ EFI_HANDLE  gHardwareInterruptHandle = NULL;
 // Notifications
 EFI_EVENT  EfiExitBootServicesEvent = (EFI_EVENT)NULL;
 
-// Maximum Number of Interrupts
-UINTN  mGicNumInterrupts = 0;
+EFI_CPU_ARCH_PROTOCOL  *gCpuArch;
 
-HARDWARE_INTERRUPT_HANDLER  *gRegisteredInterruptHandlers = NULL;
-EFI_CPU_ARCH_PROTOCOL       *gCpuArch;
+/**
+ *
+ * Return whether the Source interrupt index refers to an extended shared
+ * interrupt.
+ *
+ * @param Source  The source intid to test
+ *
+ * @return True if Source is an extended SPI intid, false otherwise.
+ */
+BOOLEAN
+GicCommonSourceIsExtSpi (
+  IN UINTN  Source
+  )
+{
+  return Source >= ARM_GIC_ARCH_EXT_SPI_MIN && Source <= ARM_GIC_ARCH_EXT_SPI_MAX;
+}
+
+/**
+ * Return whether this is a special interrupt.
+ *
+ * @param Source  The source intid to test
+ *
+ * @return True if Source is a special interrupt intid, false otherwise.
+ */
+BOOLEAN
+GicCommonSourceIsSpecialInterrupt (
+  IN UINTN  Source
+  )
+{
+  return (Source >= 1020) && (Source <= 1023);
+}
+
+/**
+ *
+ * Return whether the Source interrupt index refers to a shared interrupt (SPI)
+ *
+ * @param Source  The source intid to test
+ *
+ * @return True if Source is a SPI intid, false otherwise.
+ */
+BOOLEAN
+GicCommonSourceIsSpi (
+  IN UINTN  Source
+  )
+{
+  return (Source >= ARM_GIC_ARCH_SPI_MIN) && (Source <= ARM_GIC_ARCH_SPI_MAX);
+}
 
 /**
   Calculate GICD_ICFGRn base address and corresponding bit
@@ -32,24 +77,26 @@ EFI_CPU_ARCH_PROTOCOL       *gCpuArch;
   @retval EFI_UNSUPPORTED   Source interrupt is not supported.
 **/
 EFI_STATUS
-GicGetDistributorIcfgBaseAndBit (
+GicCommonGetDistributorIcfgBaseAndBit (
   IN HARDWARE_INTERRUPT_SOURCE  Source,
   OUT UINTN                     *RegAddress,
   OUT UINTN                     *Config1Bit
   )
 {
-  UINTN  RegIndex;
-  UINTN  Field;
+  UINTN                      RegIndex;
+  UINTN                      Field;
+  UINTN                      RegOffset;
+  HARDWARE_INTERRUPT_SOURCE  AdjustedSource;
 
-  if (Source >= mGicNumInterrupts) {
-    ASSERT (Source < mGicNumInterrupts);
-    return EFI_UNSUPPORTED;
-  }
+  // Translate ESPI sources into the SPI range for indexing purposes.
+  AdjustedSource = Source & ~(ARM_GIC_ARCH_EXT_SPI_MIN);
 
-  RegIndex    = Source / ARM_GIC_ICDICFR_F_STRIDE; // NOTE: truncation is significant
-  Field       = Source % ARM_GIC_ICDICFR_F_STRIDE;
+  RegOffset = (GicCommonSourceIsExtSpi (Source)) ? ARM_GIC_ICDICFR_E : ARM_GIC_ICDICFR;
+
+  RegIndex    = AdjustedSource / ARM_GIC_ICDICFR_F_STRIDE; // NOTE: truncation is significant
+  Field       = AdjustedSource % ARM_GIC_ICDICFR_F_STRIDE;
   *RegAddress = (UINTN)PcdGet64 (PcdGicDistributorBase)
-                + ARM_GIC_ICDICFR
+                + RegOffset
                 + (ARM_GIC_ICDICFR_BYTES * RegIndex);
   *Config1Bit = ((Field * ARM_GIC_ICDICFR_F_WIDTH)
                  + ARM_GIC_ICDICFR_F_CONFIG1_BIT);
@@ -60,9 +107,10 @@ GicGetDistributorIcfgBaseAndBit (
 /**
   Register Handler for the specified interrupt source.
 
-  @param This     Instance pointer for this protocol
-  @param Source   Hardware source of the interrupt
-  @param Handler  Callback for interrupt. NULL to unregister
+  @param This        Instance pointer for this protocol
+  @param Source      Hardware source of the interrupt
+  @param Handler     Callback for interrupt. NULL to unregister
+  @param HandlerDest Address of where to store Handler.
 
   @retval EFI_SUCCESS Source was updated to support Handler.
   @retval EFI_DEVICE_ERROR  Hardware could not be programmed.
@@ -70,58 +118,53 @@ GicGetDistributorIcfgBaseAndBit (
 **/
 EFI_STATUS
 EFIAPI
-RegisterInterruptSource (
+GicCommonRegisterInterruptSource (
   IN EFI_HARDWARE_INTERRUPT_PROTOCOL  *This,
   IN HARDWARE_INTERRUPT_SOURCE        Source,
-  IN HARDWARE_INTERRUPT_HANDLER       Handler
+  IN HARDWARE_INTERRUPT_HANDLER       Handler,
+  IN HARDWARE_INTERRUPT_HANDLER       *HandlerDest
   )
 {
-  if (Source >= mGicNumInterrupts) {
-    ASSERT (FALSE);
-    return EFI_UNSUPPORTED;
-  }
+  EFI_STATUS  Status;
 
-  if ((Handler == NULL) && (gRegisteredInterruptHandlers[Source] == NULL)) {
+  if (HandlerDest == NULL) {
     return EFI_INVALID_PARAMETER;
   }
 
-  if ((Handler != NULL) && (gRegisteredInterruptHandlers[Source] != NULL)) {
+  if ((Handler == NULL) && (*HandlerDest == NULL)) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  if ((Handler != NULL) && (*HandlerDest != NULL)) {
     return EFI_ALREADY_STARTED;
   }
 
-  gRegisteredInterruptHandlers[Source] = Handler;
-
-  // If the interrupt handler is unregistered then disable the interrupt
   if (NULL == Handler) {
-    return This->DisableInterruptSource (This, Source);
+    // Removing the handler - disable the interrupt first.
+    Status       = This->DisableInterruptSource (This, Source);
+    *HandlerDest = Handler;
+    return Status;
   } else {
+    // Registering a handler - set up the handler before enabling.
+    *HandlerDest = Handler;
     return This->EnableInterruptSource (This, Source);
   }
 }
 
 EFI_STATUS
-InstallAndRegisterInterruptService (
+GicCommonInstallAndRegisterInterruptService (
   IN EFI_HARDWARE_INTERRUPT_PROTOCOL   *InterruptProtocol,
   IN EFI_HARDWARE_INTERRUPT2_PROTOCOL  *Interrupt2Protocol,
   IN EFI_CPU_INTERRUPT_HANDLER         InterruptHandler,
   IN EFI_EVENT_NOTIFY                  ExitBootServicesEvent
   )
 {
-  EFI_STATUS   Status;
-  CONST UINTN  RihArraySize =
-    (sizeof (HARDWARE_INTERRUPT_HANDLER) * mGicNumInterrupts);
-
-  // Initialize the array for the Interrupt Handlers
-  gRegisteredInterruptHandlers = AllocateZeroPool (RihArraySize);
-  if (gRegisteredInterruptHandlers == NULL) {
-    return EFI_OUT_OF_RESOURCES;
-  }
+  EFI_STATUS  Status;
 
   // Register to receive interrupts
   Status = gCpuArch->RegisterInterruptHandler (gCpuArch, ARM_ARCH_EXCEPTION_IRQ, InterruptHandler);
   if (EFI_ERROR (Status)) {
     DEBUG ((DEBUG_ERROR, "%a: Cpu->RegisterInterruptHandler() - %r\n", __func__, Status));
-    FreePool (gRegisteredInterruptHandlers);
     return Status;
   }
 

ArmPkg/Drivers/ArmGicDxe/ArmGicDxe.h
@@ -21,42 +21,80 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Protocol/HardwareInterrupt.h>
 #include <Protocol/HardwareInterrupt2.h>
 
-extern UINTN                       mGicNumInterrupts;
-extern HARDWARE_INTERRUPT_HANDLER  *gRegisteredInterruptHandlers;
-extern EFI_CPU_ARCH_PROTOCOL       *gCpuArch;
+extern EFI_CPU_ARCH_PROTOCOL  *gCpuArch;
 
-// Common API
+// GicV2 API
 EFI_STATUS
-InstallAndRegisterInterruptService (
+GicV2DxeInitialize (
+  IN  EFI_HANDLE        ImageHandle,
+  IN  EFI_SYSTEM_TABLE  *SystemTable
+  );
+
+// GicV3 API
+EFI_STATUS
+GicV3DxeInitialize (
+  IN  EFI_HANDLE        ImageHandle,
+  IN  EFI_SYSTEM_TABLE  *SystemTable
+  );
+
+// Shared code
+
+EFI_STATUS
+EFIAPI
+GicCommonRegisterInterruptSource (
+  IN EFI_HARDWARE_INTERRUPT_PROTOCOL  *This,
+  IN HARDWARE_INTERRUPT_SOURCE        Source,
+  IN HARDWARE_INTERRUPT_HANDLER       Handler,
+  IN HARDWARE_INTERRUPT_HANDLER       *HandlerDest
+  );
+
+EFI_STATUS
+EFIAPI
+GicCommonInstallAndRegisterInterruptService (
   IN EFI_HARDWARE_INTERRUPT_PROTOCOL   *InterruptProtocol,
   IN EFI_HARDWARE_INTERRUPT2_PROTOCOL  *Interrupt2Protocol,
   IN EFI_CPU_INTERRUPT_HANDLER         InterruptHandler,
   IN EFI_EVENT_NOTIFY                  ExitBootServicesEvent
   );
 
-EFI_STATUS
-EFIAPI
-RegisterInterruptSource (
-  IN EFI_HARDWARE_INTERRUPT_PROTOCOL  *This,
-  IN HARDWARE_INTERRUPT_SOURCE        Source,
-  IN HARDWARE_INTERRUPT_HANDLER       Handler
+/**
+ *
+ * Return whether the Source interrupt index refers to an extended shared
+ * interrupt.
+ *
+ * @param Source  The source intid to test
+ *
+ * @return True if Source is an extended SPI intid, false otherwise.
+ */
+BOOLEAN
+GicCommonSourceIsExtSpi (
+  IN UINTN  Source
   );
 
-// GicV2 API
-EFI_STATUS
-GicV2DxeInitialize (
-  IN EFI_HANDLE        ImageHandle,
-  IN EFI_SYSTEM_TABLE  *SystemTable
+/**
+ * Return whether this is a special interrupt.
+ *
+ * @param Source  The source intid to test
+ *
+ * @return True if Source is a special interrupt intid, false otherwise.
+ */
+BOOLEAN
+GicCommonSourceIsSpecialInterrupt (
+  IN UINTN  Source
   );
 
-// GicV3 API
-EFI_STATUS
-GicV3DxeInitialize (
-  IN EFI_HANDLE        ImageHandle,
-  IN EFI_SYSTEM_TABLE  *SystemTable
+/**
+ *
+ * Return whether the Source interrupt index refers to a shared interrupt (SPI)
+ *
+ * @param Source  The source intid to test
+ *
+ * @return True if Source is a SPI intid, false otherwise.
+ */
+BOOLEAN
+GicCommonSourceIsSpi (
+  IN UINTN  Source
   );
-
-// Shared code
 
 /**
   Calculate GICD_ICFGRn base address and corresponding bit
@@ -70,7 +108,7 @@ GicV3DxeInitialize (
   @retval EFI_UNSUPPORTED   Source interrupt is not supported.
 **/
 EFI_STATUS
-GicGetDistributorIcfgBaseAndBit (
+GicCommonGetDistributorIcfgBaseAndBit (
   IN HARDWARE_INTERRUPT_SOURCE  Source,
   OUT UINTN                     *RegAddress,
   OUT UINTN                     *Config1Bit

ArmPkg/Drivers/ArmGicDxe/GicV2/ArmGicV2Dxe.c
@@ -19,22 +19,33 @@ Abstract:
 #include <Library/ArmGicLib.h>
 
 #include "ArmGicDxe.h"
+#include "Protocol/HardwareInterrupt.h"
+#include "Uefi/UefiBaseType.h"
 
 #define ARM_GIC_DEFAULT_PRIORITY  0x80
 
 #define GICD_V2_SIZE  SIZE_4KB
 #define GICC_V2_SIZE  SIZE_8KB
 
-// Interrupts from 1020 to 1023 are considered as special interrupts
-// (eg: spurious interrupts)
-#define ARM_GIC_IS_SPECIAL_INTERRUPTS(Interrupt) \
-          (((Interrupt) >= 1020) && ((Interrupt) <= 1023))
-
 extern EFI_HARDWARE_INTERRUPT_PROTOCOL   gHardwareInterruptV2Protocol;
 extern EFI_HARDWARE_INTERRUPT2_PROTOCOL  gHardwareInterrupt2V2Protocol;
 
 STATIC UINTN  mGicInterruptInterfaceBase;
 STATIC UINTN  mGicDistributorBase;
+
+// Maximum Number of Interrupts
+STATIC UINTN                       mGicNumInterrupts;
+STATIC HARDWARE_INTERRUPT_HANDLER  *mRegisteredInterruptHandlers;
+
+STATIC
+BOOLEAN
+EFIAPI
+GicIsValidSource (
+  IN HARDWARE_INTERRUPT_SOURCE  Source
+  )
+{
+  return Source < mGicNumInterrupts;
+}
 
 STATIC
 VOID
@@ -270,7 +281,7 @@ GicV2IrqInterruptHandler (
     return;
   }
 
-  InterruptHandler = gRegisteredInterruptHandlers[GicInterrupt];
+  InterruptHandler = mRegisteredInterruptHandlers[GicInterrupt];
   if (InterruptHandler != NULL) {
     // Call the registered interrupt handler.
     InterruptHandler (GicInterrupt, SystemContext);
@@ -280,9 +291,26 @@ GicV2IrqInterruptHandler (
   }
 }
 
+STATIC
+EFI_STATUS
+EFIAPI
+GicV2RegisterInterruptSource (
+  IN EFI_HARDWARE_INTERRUPT_PROTOCOL  *This,
+  IN HARDWARE_INTERRUPT_SOURCE        Source,
+  IN HARDWARE_INTERRUPT_HANDLER       Handler
+  )
+{
+  return GicCommonRegisterInterruptSource (
+           This,
+           Source,
+           Handler,
+           &mRegisteredInterruptHandlers[Source]
+           );
+}
+
 // The protocol instance produced by this driver
 EFI_HARDWARE_INTERRUPT_PROTOCOL  gHardwareInterruptV2Protocol = {
-  RegisterInterruptSource,
+  GicV2RegisterInterruptSource,
   GicV2EnableInterruptSource,
   GicV2DisableInterruptSource,
   GicV2GetInterruptSourceState,
@@ -312,7 +340,11 @@ GicV2GetTriggerType (
   UINTN       Config1Bit;
   EFI_STATUS  Status;
 
-  Status = GicGetDistributorIcfgBaseAndBit (
+  if (!GicIsValidSource (Source)) {
+    return EFI_UNSUPPORTED;
+  }
+
+  Status = GicCommonGetDistributorIcfgBaseAndBit (
              Source,
              &RegAddress,
              &Config1Bit
@@ -368,7 +400,11 @@ GicV2SetTriggerType (
     return EFI_UNSUPPORTED;
   }
 
-  Status = GicGetDistributorIcfgBaseAndBit (
+  if (!GicIsValidSource (Source)) {
+    return EFI_UNSUPPORTED;
+  }
+
+  Status = GicCommonGetDistributorIcfgBaseAndBit (
              Source,
              &RegAddress,
              &Config1Bit
@@ -428,7 +464,7 @@ ArmGicEnableDistributor (
 }
 
 EFI_HARDWARE_INTERRUPT2_PROTOCOL  gHardwareInterrupt2V2Protocol = {
-  (HARDWARE_INTERRUPT2_REGISTER)RegisterInterruptSource,
+  (HARDWARE_INTERRUPT2_REGISTER)GicV2RegisterInterruptSource,
   (HARDWARE_INTERRUPT2_ENABLE)GicV2EnableInterruptSource,
   (HARDWARE_INTERRUPT2_DISABLE)GicV2DisableInterruptSource,
   (HARDWARE_INTERRUPT2_INTERRUPT_STATE)GicV2GetInterruptSourceState,
@@ -493,7 +529,7 @@ GicV2ExitBootServicesEvent (
     if ((GicInterrupt & ARM_GIC_ICCIAR_ACKINTID) < mGicNumInterrupts) {
       GicV2EndOfInterrupt (&gHardwareInterruptV2Protocol, GicInterrupt);
     }
-  } while (!ARM_GIC_IS_SPECIAL_INTERRUPTS (GicInterrupt));
+  } while (!GicCommonSourceIsSpecialInterrupt (GicInterrupt));
 
   // Disable Gic Interface
   ArmGicV2DisableInterruptInterface (mGicInterruptInterfaceBase);
@@ -604,12 +640,20 @@ GicV2DxeInitialize (
   // Enable gic distributor
   ArmGicEnableDistributor (mGicDistributorBase);
 
-  Status = InstallAndRegisterInterruptService (
+  mRegisteredInterruptHandlers = AllocateZeroPool (mGicNumInterrupts * sizeof (HARDWARE_INTERRUPT_HANDLER));
+  if (mRegisteredInterruptHandlers == NULL) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  Status = GicCommonInstallAndRegisterInterruptService (
              &gHardwareInterruptV2Protocol,
              &gHardwareInterrupt2V2Protocol,
              GicV2IrqInterruptHandler,
              GicV2ExitBootServicesEvent
              );
+  if (EFI_ERROR (Status)) {
+    FreePool (mRegisteredInterruptHandlers);
+  }
 
   return Status;
 }

commit 3d92f715e36f5cc2ed72b6f711d23695aa3069e4

ArmPkg/Include: fix usage of wrong macro in ArmGicv3Dxe
To get a SPI_MAX_INTID, interrupt controller type Register
should be masked with ARM_GIC_ICDICTR_GET_SPI_RANGE.
However, since ARM_GIC_ICDICTR_SPI_RANGE_TO_MAX_INTID macro
uses ARM_GIC_ICDICTR_GET_EXT_SPI_RANGE mask,
it returns wrong SPI_MAX_INITID.

This makes a failure of loading GenericWatchDog in FVP RevC model.

Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>
Fixes: d6d2f68e3801 ("ArmPkg/Drivers/ArmGicDxe: ...")

ArmPkg/Include/Library/ArmGicLib.h
@@ -47,7 +47,7 @@
 
 // Extracts the maximum SPI IntId from TypeReg.
 #define ARM_GIC_ICDICTR_GET_SPI_MAX_INTID(TypeReg) \
-  ARM_GIC_ICDICTR_SPI_RANGE_TO_MAX_INTID(ARM_GIC_ICDICTR_GET_EXT_SPI_RANGE(TypeReg))
+  ARM_GIC_ICDICTR_SPI_RANGE_TO_MAX_INTID(ARM_GIC_ICDICTR_GET_SPI_RANGE(TypeReg))
 
 #define ARM_GIC_ICDICTR_EXT_SPI_ENABLED      (1 << 8) // Extended SPI enabled bit.
 #define ARM_GIC_ICDICTR_EXT_SPI_RANGE_SHIFT  (27)     // Extended SPI range position.