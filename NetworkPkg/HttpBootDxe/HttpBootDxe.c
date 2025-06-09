af86c61c2219feef079a1009ebe477c32c0d1a57
mark.li <mark.li@cixtech.com>
2025-06-09 17:28:10 +0800

    NetworkPkg: Add PCD to control http boot enable or disable.
    Add PCD to control http boot enable or disable.
    
    Signed-off-by: jie.fu <jie.fu@cixtech.com>

--- NetworkPkg/HttpBootDxe/HttpBootDxe.c
+++ NetworkPkg/HttpBootDxe/HttpBootDxe.c
@@ -305,6 +305,10 @@ HttpBootIp4DxeDriverBindingSupported (
 {
   EFI_STATUS  Status;
 
+  if (PcdGetBool (PcdIPv4HttpSupport) == FALSE) {
+    return EFI_UNSUPPORTED;
+  }
+
   //
   // Try to open the DHCP4, HTTP4 and Device Path protocol.
   //
@@ -794,6 +798,10 @@ HttpBootIp6DxeDriverBindingSupported (
   )
 {
   EFI_STATUS  Status;
+
+  if (PcdGetBool (PcdIPv6HttpSupport) == FALSE) {
+    return EFI_UNSUPPORTED;
+  }
 
   //
   // Try to open the DHCP6, HTTP and Device Path protocol.
