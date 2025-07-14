2303395bfd7e64a06d3cac35f27fb8eda8250f47
Benjamin Doron <benjamin.doron@9elements.com>
2025-07-14 13:22:14 -0400

    UefiPayloadPkg/BlSupportDxe: Drop manual reservations for APIC and HPET
    The entrypoint module should do this programmatically using resources
    passed by the bootloader. Under UPL, bootloaders are expected to pass
    such ranges in the FDT.
    
    Signed-off-by: Benjamin Doron <benjamin.doron@9elements.com>

--- UefiPayloadPkg/BlSupportDxe/X86/BlSupport.c
+++ UefiPayloadPkg/BlSupportDxe/X86/BlSupport.c
@@ -6,85 +6,8 @@
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
+
 #include "BlSupportDxe.h"
-
-/**
-  Reserve MMIO/IO resource in GCD
-
-  @param  IsMMIO        Flag of whether it is mmio resource or io resource.
-  @param  GcdType       Type of the space.
-  @param  BaseAddress   Base address of the space.
-  @param  Length        Length of the space.
-  @param  Alignment     Align with 2^Alignment
-  @param  ImageHandle   Handle for the image of this driver.
-
-  @retval EFI_SUCCESS   Reserve successful
-**/
-EFI_STATUS
-ReserveResourceInGcd (
-  IN BOOLEAN               IsMMIO,
-  IN UINTN                 GcdType,
-  IN EFI_PHYSICAL_ADDRESS  BaseAddress,
-  IN UINT64                Length,
-  IN UINTN                 Alignment,
-  IN EFI_HANDLE            ImageHandle
-  )
-{
-  EFI_STATUS  Status;
-
-  if (IsMMIO) {
-    Status = gDS->AddMemorySpace (
-                    GcdType,
-                    BaseAddress,
-                    Length,
-                    EFI_MEMORY_UC
-                    );
-    if (EFI_ERROR (Status)) {
-      DEBUG ((
-        DEBUG_WARN,
-        "Failed to add memory space :0x%lx 0x%lx\n",
-        BaseAddress,
-        Length
-        ));
-    }
-
-    Status = gDS->AllocateMemorySpace (
-                    EfiGcdAllocateAddress,
-                    GcdType,
-                    Alignment,
-                    Length,
-                    &BaseAddress,
-                    ImageHandle,
-                    NULL
-                    );
-  } else {
-    Status = gDS->AddIoSpace (
-                    GcdType,
-                    BaseAddress,
-                    Length
-                    );
-    if (EFI_ERROR (Status)) {
-      DEBUG ((
-        DEBUG_WARN,
-        "Failed to add IO space :0x%lx 0x%lx\n",
-        BaseAddress,
-        Length
-        ));
-    }
-
-    Status = gDS->AllocateIoSpace (
-                    EfiGcdAllocateAddress,
-                    GcdType,
-                    Alignment,
-                    Length,
-                    &BaseAddress,
-                    ImageHandle,
-                    NULL
-                    );
-  }
-
-  return Status;
-}
 
 /**
   Architecture level additional operation which needs to be performed before
@@ -104,12 +27,5 @@ BlArchAdditionalOps (
   IN EFI_SYSTEM_TABLE  *SystemTable
   )
 {
-  //
-  // Report MMIO/IO Resources
-  //
-  ReserveResourceInGcd (TRUE, EfiGcdMemoryTypeMemoryMappedIo, 0xFEC00000, SIZE_4KB, 0, ImageHandle); // IOAPIC
-
-  ReserveResourceInGcd (TRUE, EfiGcdMemoryTypeMemoryMappedIo, 0xFED00000, SIZE_1KB, 0, ImageHandle); // HPET
-
   return EFI_SUCCESS;
 }
