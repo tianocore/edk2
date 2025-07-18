df25305c080d3d89c3b1fae6c4c8fec2bf845c1c
Oliver Smith-Denny <osde@microsoft.com>
2025-07-18 08:43:34 -0700

    SecurityPkg: Move to Tpm2DumpLib
    Now that consumers have added the dependency, drop the old logic
    and move to Tpm2DumpLib.
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

--- SecurityPkg/Library/Tpm2DeviceLibDTpm/Tpm2Ptp.h
+++ SecurityPkg/Library/Tpm2DeviceLibDTpm/Tpm2Ptp.h
@@ -55,30 +55,4 @@ DTpm2RequestUseTpm (
   VOID
   );
 
-/**
-  This function dumps as much information as possible about
-  a command being sent to the TPM for maximum user-readability.
-  @param[in]  InputBlockSize  Size of the input buffer.
-  @param[in]  InputBlock      Pointer to the input buffer itself.
-**/
-VOID
-EFIAPI
-DumpTpmInputBlock (
-  IN UINT32       InputBlockSize,
-  IN CONST UINT8  *InputBlock
-  );
-
-/**
-  This function dumps as much information as possible about
-  a response from the TPM for maximum user-readability.
-  @param[in]  OutputBlockSize  Size of the output buffer.
-  @param[in]  OutputBlock      Pointer to the output buffer itself.
-**/
-VOID
-EFIAPI
-DumpTpmOutputBlock (
-  IN UINT32       OutputBlockSize,
-  IN CONST UINT8  *OutputBlock
-  );
-
 #endif // TPM2_PTP_H_
