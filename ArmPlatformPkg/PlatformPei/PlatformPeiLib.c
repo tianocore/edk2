a5cfb96eb6fb6388df7b3909cd966580c16ee02f
Levi Yun <yeoreum.yun@arm.com>
2025-07-24 11:22:38 +0100

    ArmPlatformPkg/PlatformPei: generate TPM event log hobs
    When measured boot is enabled, TF-A hands-off the early
    measurement event log to the BL33 firmware (i.e. UEFI) via
    a transfer list with tag_id
    TRANSFER_ENTRY_TAG_ID_TPM_EVENT_LOG.
    
    Therefore, generate TPM event log HOBs from the
    early measurement event log, for unifying with the
    Tcg2Dxe event logs.
    
    Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>
    Change-Id: I0b9f5586113b67df1dac6c3f72a99b978d4f070d

--- ArmPlatformPkg/PlatformPei/PlatformPeiLib.c
+++ ArmPlatformPkg/PlatformPei/PlatformPeiLib.c
@@ -8,9 +8,28 @@
 
 #include <PiPei.h>
 
+#include <Guid/TransferListHob.h>
+
 #include <Library/ArmPlatformLib.h>
+#include <Library/ArmTransferListLib.h>
+#include <Library/DebugLib.h>
 #include <Library/HobLib.h>
 #include <Library/PcdLib.h>
+
+/**
+  Build event log hob consumed by Tcg2Dxe.
+
+  @param[in]  TransferList  Transfer list header
+
+  @return EFI_SUCCESS       Success to generate HOBs from passed event log.
+  @return Others            Failed to create event log hob
+
+**/
+EFI_STATUS
+EFIAPI
+BuildPeiEventLogHobs (
+  IN TRANSFER_LIST_HEADER  *TransferList
+  );
 
 EFI_STATUS
 EFIAPI
@@ -18,7 +37,33 @@ PlatformPeim (
   VOID
   )
 {
-  BuildFvHob (PcdGet64 (PcdFvBaseAddress), PcdGet32 (PcdFvSize));
+  EFI_STATUS            Status;
+  TRANSFER_LIST_HEADER  *TransferList;
+
+  BuildFvHob (
+    PcdGet64 (PcdFvBaseAddress),
+    PcdGet32 (PcdFvSize)
+    );
+
+  Status = TransferListGetFromHobList (&TransferList);
+  if (!EFI_ERROR (Status)) {
+    if (TransferListCheckHeader (TransferList) == TRANSFER_LIST_OPS_INVALID) {
+      DEBUG ((DEBUG_ERROR, "%a: Invalid transfer list... Stop boot...\n", __func__));
+      return EFI_INVALID_PARAMETER;
+    }
+  } else {
+    // Transfer list is not found, this means
+    // early event log is not available and
+    // measured boot may not be enabled.
+    return EFI_SUCCESS;
+  }
+
+  // Create HOBs for the early measurement Event logs.
+  Status = BuildPeiEventLogHobs (TransferList);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to build event log Pei Hobs...: %r\n", __func__, Status));
+    return Status;
+  }
 
   return EFI_SUCCESS;
 }
