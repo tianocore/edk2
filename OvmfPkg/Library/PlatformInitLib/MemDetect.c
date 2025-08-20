82ad614fa1d9b69dbd08c8de8293c1b347af1cff
Tom Lendacky <thomas.lendacky@amd.com>
2025-08-20 09:53:21 -0500

    OvmfPkg/PlatformInitLib: Adjust PCI MMIO window for SEV and 5-Level paging
    The dynamic PCI MMIO window is based on the platform address width. When
    4-level paging is used, the platform address width is below any existing
    SEV memory encryption mask. However, when 5-level paging is enabled, it
    is possible that the dynamic PCI MMIO window can include the SEV memory
    encryption mask.
    
    Adjust the platform address width based on the physical address bits
    reduction when SEV is active in a guest. This will keep the dynamic PCI
    MMIO window out of the SEV encryption mask range.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- OvmfPkg/Library/PlatformInitLib/MemDetect.c
+++ OvmfPkg/Library/PlatformInitLib/MemDetect.c
@@ -672,6 +672,7 @@ PlatformAddressWidthFromCpuid (
   IA32_CR4  Cr4;
   BOOLEAN   Valid         = FALSE;
   BOOLEAN   Page1GSupport = FALSE;
+  UINT64    AddrSpace;
 
   ZeroMem (Signature, sizeof (Signature));
 
@@ -695,6 +696,20 @@ PlatformAddressWidthFromCpuid (
   } else {
     PhysBits      = 36;
     GuestPhysBits = 0;
+  }
+
+  if (PlatformInfoHob->SevIsEnabled) {
+    AddrSpace = LShiftU64 (1, PhysBits);
+    if (AddrSpace >= PlatformInfoHob->SevEncryptionMask) {
+      PhysBits -= PlatformInfoHob->SevReducedPhysBits;
+    }
+
+    if (GuestPhysBits > 0) {
+      AddrSpace = LShiftU64 (1, GuestPhysBits);
+      if (AddrSpace >= PlatformInfoHob->SevEncryptionMask) {
+        GuestPhysBits -= PlatformInfoHob->SevReducedPhysBits;
+      }
+    }
   }
 
   if (!QemuQuirk) {
