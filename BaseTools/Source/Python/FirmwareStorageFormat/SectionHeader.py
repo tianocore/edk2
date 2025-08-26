36241170ca564d4703aa1784193fd9ef8c89a2c9
Yuwei Chen <yuwei.chen@intel.com>
2025-08-26 14:57:48 +0800

    BaseTools: Enable FMMT Rebase function
    This patch adds and improves the rebase functionality
    for firmware images (PE/COFF/TE) in the FMMT tool.
    Key features include:
    
    1.Automatically rebases PE/COFF/TE images within FFS files
    when the firmware volume (FV) layout is adjusted or FFS files
    are moved, ensuring correct loading and execution at new addresses.
    2.Implements recursive rebase logic for nested sections,
    guaranteeing all relevant images are properly relocated.
    3.Adds support for rebasing subsequent FFS files within the same FV,
    enhancing compatibility and stability during firmware layout changes.
    4.Core code changes are mainly in FvHandler.py, BiosTreeNode.py,
    and BinaryFactoryProduct.py, covering rebase flag detection, address calculation,
    and actual relocation operations.
    5.This feature improves the flexibility of firmware space management
    and enhances the automation and reliability of the FMMT tool.
    
    Co-Auther: Ashraf Ali S <ashraf.ali.s@intel.com>
    Signed-off-by: Yuwei Chen <yuwei.chen@intel.com>

--- BaseTools/Source/Python/FirmwareStorageFormat/SectionHeader.py
+++ BaseTools/Source/Python/FirmwareStorageFormat/SectionHeader.py
@@ -11,6 +11,24 @@ from FirmwareStorageFormat.Common import *
 EFI_COMMON_SECTION_HEADER_LEN = 4
 EFI_COMMON_SECTION_HEADER2_LEN = 8
 
+EFI_SECTION_ALL                   = 0x00
+
+EFI_SECTION_COMPRESSION           = 0x01
+EFI_SECTION_GUID_DEFINED          = 0x02
+
+EFI_SECTION_PE32                  = 0x10
+EFI_SECTION_PIC                   = 0x11
+EFI_SECTION_TE                    = 0x12
+EFI_SECTION_DXE_DEPEX             = 0x13
+EFI_SECTION_VERSION               = 0x14
+EFI_SECTION_USER_INTERFACE        = 0x15
+EFI_SECTION_COMPATIBILITY16       = 0x16
+EFI_SECTION_FIRMWARE_VOLUME_IMAGE = 0x17
+EFI_SECTION_FREEFORM_SUBTYPE_GUID = 0x18
+EFI_SECTION_RAW                   = 0x19
+EFI_SECTION_PEI_DEPEX             = 0x1B
+EFI_SECTION_SMM_DEPEX             = 0x1C
+
 class EFI_COMMON_SECTION_HEADER(Structure):
     _pack_ = 1
     _fields_ = [
