b1c44dc4e869350b971e1214ac33c5668634c892
Michael Kubacki <michael.kubacki@microsoft.com>
2025-09-11 15:51:35 -0400

    MdeModulePkg: Add PcdDelayedDispatchMaxEntries
    The current fixed value of 8 for `DELAYED_DISPATCH_MAX_ENTRIES` is
    not large enough to accommodate platform usage. This change replaces
    the macro with a PCD that can be configured by platforms.
    
    In the case the default PCD value is too small, an error message
    explaining that the PCD should be updated will be printed followed
    by an assert.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- MdeModulePkg/Include/Guid/DelayedDispatch.h
+++ MdeModulePkg/Include/Guid/DelayedDispatch.h
@@ -1,7 +1,7 @@
 /** @file
   Definition for structure & defines exported by Delayed Dispatch PPI
 
-  Copyright (c), Microsoft Corporation.
+  Copyright (c) Microsoft Corporation.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -14,11 +14,6 @@
 #define EFI_DELAYED_DISPATCH_TABLE_GUID  {\
   0x4b733449, 0x8eff, 0x488c, { 0x92, 0x1a, 0x15, 0x4a, 0xda, 0x25, 0x18, 0x07 } \
   }
-
-//
-// Maximal number of Delayed Dispatch entries supported
-//
-#define DELAYED_DISPATCH_MAX_ENTRIES  8
 
 //
 // Internal structure for delayed dispatch entries.
@@ -37,7 +32,7 @@ typedef struct {
 typedef struct {
   UINT32                    Count;
   UINT32                    DispCount;
-  DELAYED_DISPATCH_ENTRY    Entry[DELAYED_DISPATCH_MAX_ENTRIES];
+  DELAYED_DISPATCH_ENTRY    Entry[];   // Number of entries is based on PcdDelayedDispatchMaxEntries
 } DELAYED_DISPATCH_TABLE;
 
 #pragma pack (pop)
