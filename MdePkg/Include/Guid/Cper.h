2bee14cab6c3aead35bcf385261e9d6a0615e013
Oliver Smith-Denny <osde@microsoft.com>
2025-09-15 13:03:50 -0700

    MdePkg: Remove ARM32 Support
    edk2 is dropping support for the ARM32 architecture. This
    commit removes the remaining ARM32 code from MdePkg.
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

--- MdePkg/Include/Guid/Cper.h
+++ MdePkg/Include/Guid/Cper.h
@@ -262,18 +262,16 @@ typedef struct {
 ///@{
 #define EFI_GENERIC_ERROR_PROC_TYPE_IA32_X64  0x00
 #define EFI_GENERIC_ERROR_PROC_TYPE_IA64      0x01
-#define EFI_GENERIC_ERROR_PROC_TYPE_ARM       0x02
 ///@}
 
 ///
 /// The type of the instruction set executing when the error occurred in Proessor
 /// Generic Error section.
 ///@{
-#define EFI_GENERIC_ERROR_PROC_ISA_IA32         0x00
-#define EFI_GENERIC_ERROR_PROC_ISA_IA64         0x01
-#define EFI_GENERIC_ERROR_PROC_ISA_X64          0x02
-#define EFI_GENERIC_ERROR_PROC_ISA_ARM_A32_T32  0x03
-#define EFI_GENERIC_ERROR_PROC_ISA_ARM_A64      0x04
+#define EFI_GENERIC_ERROR_PROC_ISA_IA32     0x00
+#define EFI_GENERIC_ERROR_PROC_ISA_IA64     0x01
+#define EFI_GENERIC_ERROR_PROC_ISA_X64      0x02
+#define EFI_GENERIC_ERROR_PROC_ISA_ARM_A64  0x04
 ///@}
 
 ///
