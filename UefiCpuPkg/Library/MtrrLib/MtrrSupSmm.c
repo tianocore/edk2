1d081ebc87a6db45eeaaf7daf9419835a5500d36
Richard Relph <richard.relph@amd.com>
2025-09-19 11:20:30 -0500

    UefiCpuPkg/MtrrLib: SEV-SNP does not support MTRRs
    When running OVMF in a VM using AMD's SEV-SNP, MTRRs are not supported.
    MTRR references create a dependence on host (QEMU) emulation that is at
    the very least undesirable. Therefore, SNP guests should not use MTRRs.
    PlatformQemuInitializeRam() qualifies MTRR manipulation by (eventually)
    calling MtrrLibIsMtrrSupported() which we are now making sensitive to
    SNP through PcdConfidentialComputingGuestAttr, set up earlier.
    
    Signed-off-by: Richard Relph <richard.relph@amd.com>

---
+++ UefiCpuPkg/Library/MtrrLib/MtrrSupSmm.c
@@ -0,0 +1,22 @@
+/** @file
+  CcMode support library for Smm Mode
+
+  @par Note:
+    CcMode is needed to be a library function that is different for Smm build modes
+    compared to Confidential Computing build modes (TDX and SEV).
+
+  Copyright (c) 2025, AMD Inc. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include "MtrrSup.h"
+
+BOOLEAN
+CcMode (
+  VOID
+  )
+{
+  return FALSE;
+}
