de6b2ba47c091319f80b094a3a01eb475d43da41
Aaron Pop <aaronpop@microsoft.com>
2025-10-22 11:33:02 -0700

    MdeModulePkg: Fix missing NULL tests.
    https://github.com/github/codeql/blob/codeql-cli-2.7.3/cpp/ql/src/Critical/MissingNullTest.qhelp
    
    For items which allocate memory, or get a pointer from another
    structure, it is important to validate that the pointers
    are not null before they are dereferenced.
    
    Signed-off-by: Aaron Pop <aaronpop@microsoft.com>

--- MdeModulePkg/Core/Dxe/Mem/MemoryProfileRecord.c
+++ MdeModulePkg/Core/Dxe/Mem/MemoryProfileRecord.c
@@ -1276,8 +1276,11 @@ CoreUpdateProfileFree (
       }
     }
 
-    ASSERT (DriverInfoData != NULL);
-    ASSERT (AllocInfoData != NULL);
+    if ((DriverInfoData == NULL) || (AllocInfoData == NULL)) {
+      ASSERT (DriverInfoData != NULL);
+      ASSERT (AllocInfoData != NULL);
+      return EFI_NOT_FOUND;
+    }
 
     Found = TRUE;
 
