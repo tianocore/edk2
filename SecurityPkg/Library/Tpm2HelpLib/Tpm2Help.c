486d7118fc5b56828ee19822ac1c38125b8fb8b7
rdiaz <raymonddiaz@microsoft.com>
2025-10-23 01:03:58 +0000

    SecurityPkg: Empty Tpm2HelpLib
    Empty instance of Tpm2HelpLib for integration purposes.
    
    Signed-off-by: Raymond Diaz <raymonddiaz@microsoft.com>

---
+++ SecurityPkg/Library/Tpm2HelpLib/Tpm2Help.c
@@ -0,0 +1,8 @@
+/** @file
+  Implement TPM2 help.
+
+Copyright (c), Microsoft Corporation.
+Copyright (c) 2013 - 2018, Intel Corporation. All rights reserved. <BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
