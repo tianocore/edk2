a687604a24d6f6aa37a202e71bbb01091b925fde
Abdul Lateef Attar <AbdulLateef.Attar@amd.com>
2025-10-27 14:31:18 +0000

    DynamicTablesPkg: Add MSCT standard table identifier
    Add MSCT table ID to the ESTD_ACPI_TABLE_ID enumeration.
    
    Signed-off-by: Abdul Lateef Attar <AbdulLateef.Attar@amd.com>

--- DynamicTablesPkg/Include/AcpiTableGenerator.h
+++ DynamicTablesPkg/Include/AcpiTableGenerator.h
@@ -91,6 +91,9 @@ The Dynamic Tables Framework implements the following ACPI table generators:
             Configuration Manager and builds the HEST table.
   - EINJ  : The EINJ generator collates the required platform information from
             the Configuration Manager and builds the EINJ table.
+  - MSCT  : The MSCT (Maximum System Characteristics Table) generator collates
+            the Maximum System Characteristics information from the
+            Configuration Manager and builds the MSCT table.
 */
 
 /** The ACPI_TABLE_GENERATOR_ID type describes ACPI table generator ID.
@@ -133,6 +136,7 @@ typedef enum StdAcpiTableId {
   EStdAcpiTableIdHest,                          ///< Hest Generator
   EStdAcpiTableIdEinj,                          ///< EINJ Generator
   EStdAcpiTableIdHmat,                          ///< HMAT Generator
+  EStdAcpiTableIdMsct,                          ///< MSCT Generator
   EStdAcpiTableIdMax
 } ESTD_ACPI_TABLE_ID;
 
