56c6fb30c650c792f782805bda5c29a5b8997f9f
Mike Beaton <mjsbeaton@gmail.com>
2025-11-08 05:18:52 +0000

    OvmfPkg: Revert "OvmfPkg/build.sh: Remove support for IA32 architecture"
    This reverts commit 2a6708a786eb0f0e9b979cd93c5dc12c42e337a1.
    
    1fb88ffe284782cc79e306306b8d19829b6248b7
    removed the ability to build IA32, but unfortunately left the legacy
    flag combination which would have triggered that build as building X64.
    
    This was followed by 2a6708a786eb0f0e9b979cd93c5dc12c42e337a1, which
    removed build.sh support for IA32X64.
    
    As per https://github.com/tianocore/edk2/issues/11725, there should
    be no need to remove this support. This commit reverts
    2a6708a786eb0f0e9b979cd93c5dc12c42e337a1.
    
    The following commit fixes the mentioned issue with
    1fb88ffe284782cc79e306306b8d19829b6248b7.
    
    Signed-off-by: Mike Beaton <mjsbeaton@gmail.com>

--- OvmfPkg/build.sh
+++ OvmfPkg/build.sh
@@ -35,6 +35,7 @@ fi
 # Configure defaults for various options
 #
 
+ARCH_IA32=no
 ARCH_X64=no
 BUILDTARGET=DEBUG
 BUILD_OPTIONS=
@@ -90,7 +91,6 @@ case `uname` in
         TARGET_TOOLS=GCC5
         ;;
     esac
-    ;;
 esac
 
 #
@@ -119,9 +119,9 @@ do
   else
     case $LAST_ARG in
       -a)
-        if [[ x"$arg" != x"X64" ]]; then
+        if [[ x"$arg" != x"IA32" && x"$arg" != x"X64" ]]; then
           echo Unsupported processor architecture: $arg
-          echo Only X64 is supported
+          echo Only IA32 or X64 is supported
           exit 1
         fi
         eval ARCH_$arg=yes
@@ -147,14 +147,22 @@ do
   shift
 done
 
-PROCESSOR=X64
-Processor=X64
-BUILD_OPTIONS="$BUILD_OPTIONS -a X64"
-PLATFORM_BUILD_DIR=Ovmf$Processor
-BUILD_ROOT_ARCH=X64
+if [[ "$ARCH_IA32" == "yes" && "$ARCH_X64" == "yes" ]]; then
+  PROCESSOR=IA32X64
+  Processor=Ia32X64
+  BUILD_OPTIONS="$BUILD_OPTIONS -a IA32 -a X64"
+  PLATFORM_BUILD_DIR=Ovmf3264
+  BUILD_ROOT_ARCH=X64
+else
+  PROCESSOR=X64
+  Processor=X64
+  BUILD_OPTIONS="$BUILD_OPTIONS -a X64"
+  PLATFORM_BUILD_DIR=Ovmf$Processor
+  BUILD_ROOT_ARCH=X64
+fi
 
 case $PROCESSOR in
-  X64)
+  X64|IA32X64)
     if [ -z "$QEMU_COMMAND" ]; then
       #
       # The user didn't set the QEMU_COMMAND variable.
@@ -164,7 +172,7 @@ case $PROCESSOR in
     ;;
   *)
     echo Unsupported processor architecture: $PROCESSOR
-    echo Only X64 is supported
+    echo Only IA32 or X64 is supported
     exit 1
     ;;
 esac
