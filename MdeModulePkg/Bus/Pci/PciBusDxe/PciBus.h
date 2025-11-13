be8f10bf9500d008b0941fb96ee3b5118ca68bf1
Jacek Kolakowski <Jacek.Kolakowski@intel.com>
2025-11-13 12:14:02 +0100

    MdeModulePkg: Mark PcdMrIovSupport obsolete - deprecated in PCIe 6.0
    MR-IOV was actually not used in MdeModulePkg and it has been
    deprecated in PCIe 6.0 specification. Remove code references,
    but keep PcdMrIovSupport with just a comment that it is deprecated.
    
    Signed-off-by: Jacek Kolakowski <Jacek.Kolakowski@intel.com>

--- MdeModulePkg/Bus/Pci/PciBusDxe/PciBus.h
+++ MdeModulePkg/Bus/Pci/PciBusDxe/PciBus.h
@@ -51,7 +51,6 @@ typedef struct _PCI_BAR        PCI_BAR;
 
 #define     EFI_PCI_IOV_POLICY_ARI    0x0001
 #define     EFI_PCI_IOV_POLICY_SRIOV  0x0002
-#define     EFI_PCI_IOV_POLICY_MRIOV  0x0004
 
 typedef enum {
   PciBarTypeUnknown = 0,
@@ -269,7 +268,6 @@ struct _PCI_IO_DEVICE {
   UINT8                                        PciExpressCapabilityOffset;
   UINT32                                       AriCapabilityOffset;
   UINT32                                       SrIovCapabilityOffset;
-  UINT32                                       MrIovCapabilityOffset;
   PCI_BAR                                      VfPciBar[PCI_MAX_BAR];
   UINT32                                       SystemPageSize;
   UINT16                                       InitialVFs;
