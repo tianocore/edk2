tianocore/edk2

commit 90771630bfe123de8e4bacc96f98ffe4d8308153

UefiCpuPkg/ArmMmuLib: Add support for LPA2
Add support for LPA2 (52-bit physical addressing).

Signed-off-by: Sarah Walker <Sarah.Walker2@arm.com>

UefiCpuPkg/Library/ArmMmuLib/AArch64/ArmMmuLibCore.c
@@ -1,7 +1,7 @@
 /** @file
 *  File managing the MMU for ARMv8 architecture
 *
-*  Copyright (c) 2011-2020, ARM Limited. All rights reserved.
+*  Copyright (c) 2011-2025, ARM Limited. All rights reserved.
 *  Copyright (c) 2016, Linaro Limited. All rights reserved.
 *  Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
 *
@@ -91,9 +91,41 @@ ArmMemoryAttributeToPageAttribute (
   }
 }
 
+// T0SZ can be below MIN_T0SZ when LPA2 is in use, meaning the page table starts at level -1
 #define MIN_T0SZ        16
 #define BITS_PER_LEVEL  9
-#define MAX_VA_BITS     48
+#define MAX_VA_BITS     52
+
+STATIC
+VOID
+SetOutputAddress (
+  IN  UINTN    *Entry,
+  IN  UINTN    Address,
+  IN  BOOLEAN  Lpa2Enabled
+  )
+{
+  if (Lpa2Enabled) {
+    *Entry &= ~(TT_ADDRESS_MASK_BLOCK_ENTRY_LPA2 | TT_UPPER_ADDRESS_MASK);
+    *Entry |= ((UINTN)Address & TT_ADDRESS_MASK_BLOCK_ENTRY_LPA2) | (((UINTN)Address >> 50) << 8);
+  } else {
+    *Entry &= ~TT_ADDRESS_MASK_BLOCK_ENTRY;
+    *Entry |= (Address & TT_ADDRESS_MASK_BLOCK_ENTRY);
+  }
+}
+
+STATIC
+UINT64
+GetOutputAddress (
+  IN  UINT64   Entry,
+  IN  BOOLEAN  Lpa2Enabled
+  )
+{
+  if (Lpa2Enabled) {
+    return (Entry & TT_ADDRESS_MASK_BLOCK_ENTRY_LPA2) | ((Entry & TT_UPPER_ADDRESS_MASK) << (50 - 8));
+  } else {
+    return Entry & TT_ADDRESS_MASK_BLOCK_ENTRY;
+  }
+}
 
 STATIC
 UINTN
@@ -105,12 +137,12 @@ GetRootTableEntryCount (
 }
 
 STATIC
-UINTN
+INTN
 GetRootTableLevel (
   IN  UINTN  T0SZ
   )
 {
-  return (T0SZ - MIN_T0SZ) / BITS_PER_LEVEL;
+  return (INTN)(T0SZ - MIN_T0SZ) / BITS_PER_LEVEL;
 }
 
 STATIC
@@ -159,8 +191,9 @@ ReplaceTableEntry (
 STATIC
 VOID
 FreePageTablesRecursive (
-  IN  UINT64  *TranslationTable,
-  IN  UINTN   Level
+  IN  UINT64   *TranslationTable,
+  IN  UINTN    Level,
+  IN  BOOLEAN  Lpa2Enabled
   )
 {
   UINTN  Index;
@@ -171,9 +204,12 @@ FreePageTablesRecursive (
     for (Index = 0; Index < TT_ENTRY_COUNT; Index++) {
       if ((TranslationTable[Index] & TT_TYPE_MASK) == TT_TYPE_TABLE_ENTRY) {
         FreePageTablesRecursive (
-          (VOID *)(UINTN)(TranslationTable[Index] &
-                          TT_ADDRESS_MASK_BLOCK_ENTRY),
-          Level + 1
+          (VOID *)GetOutputAddress (
+                    TranslationTable[Index],
+                    Lpa2Enabled
+                    ),
+          Level + 1,
+          Lpa2Enabled
           );
       }
     }
@@ -222,8 +258,10 @@ UpdateRegionMappingRecursive (
   IN  UINT64   AttributeSetMask,
   IN  UINT64   AttributeClearMask,
   IN  UINT64   *PageTable,
-  IN  UINTN    Level,
-  IN  BOOLEAN  TableIsLive
+  IN  INTN     Level,
+  IN  BOOLEAN  IsRootTable,
+  IN  BOOLEAN  TableIsLive,
+  IN  BOOLEAN  Lpa2Enabled
   )
 {
   UINTN       BlockShift;
@@ -316,7 +354,9 @@ UpdateRegionMappingRecursive (
                      0,
                      TranslationTable,
                      Level + 1,
-                     FALSE
+                     FALSE,
+                     FALSE,
+                     Lpa2Enabled
                      );
           if (EFI_ERROR (Status)) {
             //
@@ -331,7 +371,7 @@ UpdateRegionMappingRecursive (
 
         NextTableIsLive = FALSE;
       } else {
-        TranslationTable = (VOID *)(UINTN)(*Entry & TT_ADDRESS_MASK_BLOCK_ENTRY);
+        TranslationTable = (VOID *)GetOutputAddress (*Entry, Lpa2Enabled);
         NextTableIsLive  = TableIsLive;
       }
 
@@ -345,7 +385,9 @@ UpdateRegionMappingRecursive (
                  AttributeClearMask,
                  TranslationTable,
                  Level + 1,
-                 NextTableIsLive
+                 FALSE,
+                 NextTableIsLive,
+                 Lpa2Enabled
                  );
       if (EFI_ERROR (Status)) {
         if (!IsTableEntry (*Entry, Level)) {
@@ -356,14 +398,16 @@ UpdateRegionMappingRecursive (
           // possible for existing table entries, since we cannot revert the
           // modifications we made to the subhierarchy it represents.)
           //
-          FreePageTablesRecursive (TranslationTable, Level + 1);
+          FreePageTablesRecursive (TranslationTable, Level + 1, Lpa2Enabled);
         }
 
         return Status;
       }
 
       if (!IsTableEntry (*Entry, Level)) {
-        EntryValue = (UINTN)TranslationTable | TT_TYPE_TABLE_ENTRY;
+        EntryValue = TT_TYPE_TABLE_ENTRY;
+        SetOutputAddress (&EntryValue, (UINTN)TranslationTable, Lpa2Enabled);
+
         ReplaceTableEntry (
           Entry,
           EntryValue,
@@ -373,8 +417,9 @@ UpdateRegionMappingRecursive (
           );
       }
     } else {
-      EntryValue  = (*Entry & AttributeClearMask) | AttributeSetMask;
-      EntryValue |= RegionStart;
+      EntryValue = (*Entry & AttributeClearMask) | AttributeSetMask;
+      // Below clears shareability bits when LPA2 is in use
+      SetOutputAddress (&EntryValue, RegionStart, Lpa2Enabled);
       EntryValue |= (Level == 3) ? TT_TYPE_BLOCK_ENTRY_LEVEL3
                                  : TT_TYPE_BLOCK_ENTRY;
 
@@ -393,7 +438,8 @@ UpdateRegionMapping (
   IN  UINT64   AttributeSetMask,
   IN  UINT64   AttributeClearMask,
   IN  UINT64   *RootTable,
-  IN  BOOLEAN  TableIsLive
+  IN  BOOLEAN  TableIsLive,
+  IN  BOOLEAN  Lpa2Enabled
   )
 {
   UINTN  T0SZ;
@@ -418,7 +464,9 @@ UpdateRegionMapping (
            AttributeClearMask,
            RootTable,
            GetRootTableLevel (T0SZ),
-           TableIsLive
+           TRUE,
+           TableIsLive,
+           Lpa2Enabled
            );
 }
 
@@ -426,7 +474,8 @@ STATIC
 EFI_STATUS
 FillTranslationTable (
   IN  UINT64                        *RootTable,
-  IN  ARM_MEMORY_REGION_DESCRIPTOR  *MemoryRegion
+  IN  ARM_MEMORY_REGION_DESCRIPTOR  *MemoryRegion,
+  IN  BOOLEAN                       Lpa2Enabled
   )
 {
   return UpdateRegionMapping (
@@ -435,7 +484,8 @@ FillTranslationTable (
            ArmMemoryAttributeToPageAttribute (MemoryRegion->Attributes) | TT_AF,
            0,
            RootTable,
-           FALSE
+           FALSE,
+           Lpa2Enabled
            );
 }
 
@@ -565,7 +615,8 @@ ArmSetMemoryAttributes (
            PageAttributes,
            PageAttributeMask,
            ArmGetTTBR0BaseAddress (),
-           TRUE
+           TRUE,
+           ArmGetTCR () & TCR_DS
            );
 }
 
@@ -628,13 +679,15 @@ ArmConfigureMmu (
       TCR |= TCR_PS_16TB;
     } else if (MaxAddress < SIZE_256TB) {
       TCR |= TCR_PS_256TB;
+    } else if ((MaxAddress < SIZE_4PB) && ArmHas52BitTgran4 ()) {
+      TCR |= TCR_PS_4PB | TCR_DS;
     } else {
       DEBUG ((
         DEBUG_ERROR,
         "ArmConfigureMmu: The MaxAddress 0x%lX is not supported by this MMU configuration.\n",
         MaxAddress
         ));
-      ASSERT (0); // Bigger than 48-bit memory space are not supported
+      ASSERT (0); // Bigger than 48/52-bit memory space are not supported
       return EFI_UNSUPPORTED;
     }
   } else {
@@ -654,13 +707,15 @@ ArmConfigureMmu (
       TCR |= TCR_IPS_16TB;
     } else if (MaxAddress < SIZE_256TB) {
       TCR |= TCR_IPS_256TB;
+    } else if ((MaxAddress < SIZE_4PB) && ArmHas52BitTgran4 ()) {
+      TCR |= TCR_IPS_4PB | TCR_DS;
     } else {
       DEBUG ((
         DEBUG_ERROR,
         "ArmConfigureMmu: The MaxAddress 0x%lX is not supported by this MMU configuration.\n",
         MaxAddress
         ));
-      ASSERT (0); // Bigger than 48-bit memory space are not supported
+      ASSERT (0); // Bigger than 48/52-bit memory space are not supported
       return EFI_UNSUPPORTED;
     }
   }
@@ -709,7 +764,7 @@ ArmConfigureMmu (
   ZeroMem (TranslationTable, RootTableEntryCount * sizeof (UINT64));
 
   while (MemoryTable->Length != 0) {
-    Status = FillTranslationTable (TranslationTable, MemoryTable);
+    Status = FillTranslationTable (TranslationTable, MemoryTable, TCR & TCR_DS);
     if (EFI_ERROR (Status)) {
       goto FreeTranslationTable;
     }
@@ -730,7 +785,15 @@ ArmConfigureMmu (
     MAIR_ATTR (TT_ATTR_INDX_MEMORY_WRITE_BACK, MAIR_ATTR_NORMAL_MEMORY_WRITE_BACK)
     );
 
-  ArmSetTTBR0 (TranslationTable);
+  if ((TCR & TCR_IPS_MASK) == TCR_IPS_4PB) {
+    ArmSetTTBR0 (
+      (VOID *)
+      (((UINTN)TranslationTable & 0xffffffffffc0) |
+       (((UINTN)TranslationTable >> 48) << 2))
+      );
+  } else {
+    ArmSetTTBR0 (TranslationTable);
+  }
 
   if (!ArmMmuEnabled ()) {
     ArmDisableAlignmentCheck ();

commit f6842582cdb74d322b62bdde1a09ae44844b7a7b

ArmPkg,UefiCpuPkg: fix boot failure with LPA2
When FEAT_LPA2 is enabled:
  1. The DS bit is located at bit 32, not bit 59, when HCR_EL2.E2H = 0.
  2. The root level of the page table should be -1 when
     using a 52-bit address with a 4KB TxSZ and LPA2 feature is enabled.

However, the current implementation does not handle
these conditions correctly, causing EFI to fail
during boot when CPU is EL0&1 or EL0&2.

Fix this boot failure.

Fixes: 90771630bfe ("UefiCpuPkg/ArmMmuLib: Add support for LPA2")
Continuous-integration-options: PatchCheck.ignore-multi-package
Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>

ArmPkg/Drivers/CpuDxe/AArch64/Mmu.c
@@ -48,7 +48,8 @@ GetRootTranslationTableInfo (
   OUT UINTN  *RootTableEntryCount
   )
 {
-  *RootTableLevel      = (INTN)(T0SZ - MIN_T0SZ) / BITS_PER_LEVEL;
+  *RootTableLevel = (T0SZ < MIN_T0SZ) ? -1 : (INTN)(T0SZ - MIN_T0SZ) / BITS_PER_LEVEL;
+  ASSERT (*RootTableLevel >= 0 || ArmLpa2Enabled ());
   *RootTableEntryCount = TT_ENTRY_COUNT >> (INTN)(T0SZ - MIN_T0SZ) % BITS_PER_LEVEL;
 }
 
@@ -241,7 +242,7 @@ GetNextEntryAttribute (
 
       // Increase the level number and scan the sub-level table
       GetNextEntryAttribute (
-        (UINT64 *)GetOutputAddress (Entry, ArmGetTCR () & TCR_DS),
+        (UINT64 *)GetOutputAddress (Entry, ArmLpa2Enabled ()),
         TT_ENTRY_COUNT,
         TableLevel + 1,
         (BaseAddress + (Index * TT_ADDRESS_AT_LEVEL (TableLevel))),
@@ -329,7 +330,7 @@ SyncCacheConfig (
   GetRootTranslationTableInfo (T0SZ, &TableLevel, &TableCount);
 
   // First Attribute of the Page Tables
-  PageAttribute = GetFirstPageAttribute (FirstLevelTableAddress, TableLevel, ArmGetTCR () & TCR_DS);
+  PageAttribute = GetFirstPageAttribute (FirstLevelTableAddress, TableLevel, ArmLpa2Enabled ());
 
   // We scan from the start of the memory map (ie: at the address 0x0)
   BaseAddressGcdRegion = 0x0;
@@ -458,7 +459,7 @@ GetMemoryRegionRec (
   EntryType  = *BlockEntry & TT_TYPE_MASK;
 
   if ((TableLevel < 3) && (EntryType == TT_TYPE_TABLE_ENTRY)) {
-    NextTranslationTable = (UINT64 *)GetOutputAddress (*BlockEntry, ArmGetTCR () & TCR_DS);
+    NextTranslationTable = (UINT64 *)GetOutputAddress (*BlockEntry, ArmLpa2Enabled ());
 
     // The entry is a page table, so we go to the next level
     Status = GetMemoryRegionRec (

UefiCpuPkg/Library/ArmMmuLib/AArch64/ArmMmuLibCore.c
@@ -142,7 +142,12 @@ GetRootTableLevel (
   IN  UINTN  T0SZ
   )
 {
-  return (INTN)(T0SZ - MIN_T0SZ) / BITS_PER_LEVEL;
+  INTN  RootTableLevel;
+
+  RootTableLevel =  (T0SZ < MIN_T0SZ) ? -1 : (INTN)(T0SZ - MIN_T0SZ) / BITS_PER_LEVEL;
+  ASSERT (RootTableLevel >= 0 || ArmLpa2Enabled ());
+
+  return RootTableLevel;
 }
 
 STATIC
@@ -306,7 +311,7 @@ UpdateRegionMappingRecursive (
     // the MMU in order to update page table entries safely, so prefer page
     // mappings in that particular case.
     //
-    if ((Level == 0) || (((RegionStart | BlockEnd) & BlockMask) != 0) ||
+    if ((Level <= 0) || (((RegionStart | BlockEnd) & BlockMask) != 0) ||
         ((Level < 3) && (((UINT64)PageTable & ~BlockMask) == RegionStart)) ||
         IsTableEntry (*Entry, Level))
     {
@@ -616,7 +621,7 @@ ArmSetMemoryAttributes (
            PageAttributeMask,
            ArmGetTTBR0BaseAddress (),
            TRUE,
-           ArmGetTCR () & TCR_DS
+           ArmLpa2Enabled ()
            );
 }
 
@@ -680,7 +685,7 @@ ArmConfigureMmu (
     } else if (MaxAddress < SIZE_256TB) {
       TCR |= TCR_PS_256TB;
     } else if ((MaxAddress < SIZE_4PB) && ArmHas52BitTgran4 ()) {
-      TCR |= TCR_PS_4PB | TCR_DS;
+      TCR |= TCR_PS_4PB | TCR_DS_NVHE;
     } else {
       DEBUG ((
         DEBUG_ERROR,
@@ -764,7 +769,7 @@ ArmConfigureMmu (
   ZeroMem (TranslationTable, RootTableEntryCount * sizeof (UINT64));
 
   while (MemoryTable->Length != 0) {
-    Status = FillTranslationTable (TranslationTable, MemoryTable, TCR & TCR_DS);
+    Status = FillTranslationTable (TranslationTable, MemoryTable, ArmLpa2Enabled ());
     if (EFI_ERROR (Status)) {
       goto FreeTranslationTable;
     }