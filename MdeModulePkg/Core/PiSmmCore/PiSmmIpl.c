tianocore/edk2

commit 875362bf579486f0b72cf0cd5190d21ded769177

MdeModulePkg: Bds Fix potential infinite loop.
Bds will get the List of Boot Options and attempt to boot.
If there were no valid load options, the system could
infinitely be stuck in the attempt boot loop.

Add NULL checks to prevent a protentional infinite
boot retry loop.

Signed-off-by: Aaron Pop <aaronpop@microsoft.com>

MdeModulePkg/Universal/BdsDxe/BdsEntry.c
@@ -699,7 +699,6 @@ BdsEntry (
 
   HotkeyTriggered = NULL;
   Status          = EFI_SUCCESS;
-  BootSuccess     = FALSE;
 
   //
   // Insert the performance probe
@@ -1098,6 +1097,7 @@ BdsEntry (
       //
       // Retry to boot if any of the boot succeeds
       //
+      BootSuccess = FALSE;
       LoadOptions = EfiBootManagerGetLoadOptions (&LoadOptionCount, LoadOptionTypeBoot);
       if (LoadOptions != NULL) {
         BootSuccess = BootBootOptions (LoadOptions, LoadOptionCount, (BootManagerMenuStatus != EFI_NOT_FOUND) ? &BootManagerMenu : NULL);

commit 2c3012e49a2a6df20c0bcf43b88b0584c229ed09

MdeModulePkg: Fix recently introduced uninitialized variable usage.
Address the build regressions, introduced in #11724, #11688, #11686 #11685.
These build regressions are for uninitialized variables before use.

Signed-off-by: Aaron Pop <aaronpop@microsoft.com>

MdeModulePkg/Core/PiSmmCore/PiSmmIpl.c
@@ -1607,11 +1607,15 @@ GetFullSmramRanges (
   UINTN                           MaxCount;
   BOOLEAN                         Rescan;
 
+  SmmConfiguration    = NULL;
+  TempSmramRanges     = NULL;
+  SmramReservedRanges = NULL;
+  SmramRanges         = NULL;
+
   //
   // Get SMM Configuration Protocol if it is present.
   //
-  SmmConfiguration = NULL;
-  Status           = gBS->LocateProtocol (&gEfiSmmConfigurationProtocolGuid, NULL, (VOID **)&SmmConfiguration);
+  Status = gBS->LocateProtocol (&gEfiSmmConfigurationProtocolGuid, NULL, (VOID **)&SmmConfiguration);
 
   //
   // Get SMRAM information.

MdeModulePkg/Universal/BdsDxe/BdsEntry.c
@@ -699,6 +699,7 @@ BdsEntry (
 
   HotkeyTriggered = NULL;
   Status          = EFI_SUCCESS;
+  BootSuccess     = FALSE;
 
   //
   // Insert the performance probe

MdeModulePkg/Universal/HiiDatabaseDxe/ConfigRouting.c
@@ -795,6 +795,7 @@ CompareNameElementDefault (
 
   AppendString  = NULL;
   NvConfigExist = NULL;
+  Status        = EFI_OUT_OF_RESOURCES;
   //
   // Make NvConfigPtr point to the first <NvConfig> with AltConfigHdr in DefaultAltCfgResp.
   //

MdeModulePkg/Universal/SetupBrowserDxe/IfrParse.c
@@ -430,8 +430,11 @@ CreateStorage (
   EFI_GUID         *StorageGuid;
   CHAR8            *StorageName;
 
-  UnicodeString = NULL;
-  StorageName   = NULL;
+  UnicodeString  = NULL;
+  StorageName    = NULL;
+  BrowserStorage = NULL;
+  Storage        = NULL;
+
   switch (StorageType) {
     case EFI_HII_VARSTORE_BUFFER:
       StorageGuid = (EFI_GUID *)(CHAR8 *)&((EFI_IFR_VARSTORE *)OpCodeData)->Guid;