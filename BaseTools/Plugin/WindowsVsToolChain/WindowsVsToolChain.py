bacbe8e17959e6ce122e782a8cf39f8fdfe2f93d
Michael D Kinney <michael.d.kinney@intel.com>
2025-12-26 11:07:41 -0800

    BaseTools/WindowsVsToolChain: Add CLANG support
    Add logic to detect CLANG environments and set the
    required environment variables.
    
    The CLANGPDB tool chain tag in Windows environments can
    be used with Visual Studio compiler or Mingw CLANG
    compiler. Add detection of Mingw CLANG compiler first
    and if Mingw not detected, then use the existing logic
    for using Visual Studio.
    
    The CLANGDWARF tool chain tag in Windows environments
    can only be used with Mingw CLANG compiler. Add detection
    of Mingw CLANG compiler first and if Mingw not detected,
    then fail with an error.
    
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>

--- BaseTools/Plugin/WindowsVsToolChain/WindowsVsToolChain.py
+++ BaseTools/Plugin/WindowsVsToolChain/WindowsVsToolChain.py
@@ -256,74 +256,160 @@ class WindowsVsToolChain(IUefiBuildPlugin):
         # If CLANG_VS_HOST is not set then find the host type based on Host Info.
         ##
         elif thebuilder.env.GetValue("TOOL_CHAIN_TAG") == "CLANGPDB":
-            HostInfo = GetHostInfo()
 
-            # check to see if host is configured
-            # HostType for VS tools should be (defined in tools_def):
-            # x86   == 32bit Intel
-            # x64   == 64bit Intel
-            # arm64 == 64bit Arm
             #
-            HostType = shell_environment.GetEnvironment().get_shell_var("CLANG_VS_HOST")
-            if HostType is not None:
-                HostType = HostType.lower()
-                self.Logger.info(
-                    f"CLANG_VS_HOST defined by environment.  Value is {HostType}")
-            else:
-                #figure it out based on host info
-                if HostInfo.arch == "x86":
-                    if HostInfo.bit == "32":
-                        HostType = "x86"
-                    elif HostInfo.bit == "64":
-                        HostType = "x64"
-                else:
-                    # anything other than x86 or x64 is not supported
-                    raise NotImplementedError()
+            # Look for $(CLANG_BIN)/mingw32-make.exe to determine if we are in a
+            # mingw32 environment
+            #
+            is_mingw32_env = False
+            clang_bin = shell_environment.GetEnvironment().get_shell_var("CLANG_BIN")
+            if clang_bin is not None:
+                if os.path.exists(os.path.join(clang_bin, "mingw32-make.exe")):
+                    self.Logger.debug("CLANG_BIN is set to a mingw32 toolchain.")
+                    is_mingw32_env = True
 
-            # CLANG_VS_HOST options are not exactly the same as QueryVcVariables. This translates.
-            VC_HOST_ARCH_TRANSLATOR = {
-                "x86": "x86", "x64": "AMD64", "arm64": "not supported"}
-
-            # now get the environment variables for the platform
-            shell_env = shell_environment.GetEnvironment()
-            # Use the tools lib to determine the correct values for the vars that interest us.
-            vs_vars = locate_tools.QueryVcVariables(
-                interesting_keys, VC_HOST_ARCH_TRANSLATOR[HostType])
-            for (k, v) in vs_vars.items():
-                shell_env.set_shell_var(k, v)
-
-            ##
-            # If environment already has CLANG_HOST_BIN set then user has already
-            # set the path to the VS tools like nmake.exe
-            ##
-            if shell_environment.GetEnvironment().get_shell_var("CLANG_HOST_BIN") is not None:
-                self.Logger.debug("CLANG_HOST_BIN is already set.")
-
-            else:
-                install_path = self._get_vs_install_path(None, None)
-                vc_ver = self._get_vc_version(install_path, None)
-
-                if install_path is None or vc_ver is None:
-                    self.Logger.error("Failed to configure environment for VS")
+            if is_mingw32_env:
+                #
+                # CLANGPDB requires lld-link.exe to generate PE/COFF images. If
+                # the mingw32 environment installed does not have lld-link.exe
+                # then error out.
+                #
+                if not os.path.exists(os.path.join(clang_bin, "lld-link.exe")):
+                    self.Logger.error("CLANG_BIN is set to a mingw32 toolchain but lld-link.exe not found.")
                     return -1
 
-                version_aggregator.GetVersionAggregator().ReportVersion(
-                    "Visual Studio Install Path", install_path, version_aggregator.VersionTypes.INFO)
-                version_aggregator.GetVersionAggregator().ReportVersion(
-                    "VC Version", vc_ver, version_aggregator.VersionTypes.TOOL)
+                #
+                # Set environment variables for mingw32 CLANGPDB environment
+                #
+                basetools_mingw_build = shell_environment.GetEnvironment().get_shell_var("BASETOOLS_MINGW_BUILD")
+                clang_host_bin = shell_environment.GetEnvironment().get_shell_var("CLANG_HOST_BIN")
 
-                # make path align with tools_def.txt
-                prefix = os.path.join(install_path, "VC", "Tools", "MSVC", vc_ver)
-                clang_host_bin_prefix = os.path.join(prefix, "bin", "Host%s" % HostType, HostType)
+                if basetools_mingw_build is not None:
+                    self.Logger.debug("BASETOOLS_MINGW_BUILD is already set.")
+                else:
+                    basetools_mingw_build = 'TRUE'
+                if basetools_mingw_build.upper() != 'TRUE':
+                    self.Logger.error("BASETOOLS_MINGW_BUILD must be set to TRUE for CLANGPDB toolchain.")
+                    return -1
 
-                # now confirm it exists
-                if not os.path.exists(clang_host_bin_prefix):
-                    self.Logger.error("Path for VS toolchain is invalid")
-                    return -2
+                if clang_host_bin is not None:
+                    self.Logger.debug("CLANG_HOST_BIN is already set.")
+                else:
+                    # The environment is mingw32 make add "mingw32-" to the end of the path.
+                    # The rest of the command is derived from definitions in tools.def.
+                    clang_host_bin = 'mingw32-'
 
-                # The environment is using nmake (not make) so add "n" to the end of the path.
+                shell_environment.GetEnvironment().set_shell_var("BASETOOLS_MINGW_BUILD", basetools_mingw_build)
+                shell_environment.GetEnvironment().set_shell_var("CLANG_HOST_BIN", clang_host_bin)
+
+            else:
+                HostInfo = GetHostInfo()
+
+                # check to see if host is configured
+                # HostType for VS tools should be (defined in tools_def):
+                # x86   == 32bit Intel
+                # x64   == 64bit Intel
+                # arm64 == 64bit Arm
+                #
+                HostType = shell_environment.GetEnvironment().get_shell_var("CLANG_VS_HOST")
+                if HostType is not None:
+                    HostType = HostType.lower()
+                    self.Logger.info(
+                        f"CLANG_VS_HOST defined by environment.  Value is {HostType}")
+                else:
+                    #figure it out based on host info
+                    if HostInfo.arch == "x86":
+                        if HostInfo.bit == "32":
+                            HostType = "x86"
+                        elif HostInfo.bit == "64":
+                            HostType = "x64"
+                    else:
+                        # anything other than x86 or x64 is not supported
+                        raise NotImplementedError()
+
+                # CLANG_VS_HOST options are not exactly the same as QueryVcVariables. This translates.
+                VC_HOST_ARCH_TRANSLATOR = {
+                    "x86": "x86", "x64": "AMD64", "arm64": "not supported"}
+
+                # now get the environment variables for the platform
+                shell_env = shell_environment.GetEnvironment()
+                # Use the tools lib to determine the correct values for the vars that interest us.
+                vs_vars = locate_tools.QueryVcVariables(
+                    interesting_keys, VC_HOST_ARCH_TRANSLATOR[HostType])
+                for (k, v) in vs_vars.items():
+                    shell_env.set_shell_var(k, v)
+
+                ##
+                # If environment already has CLANG_HOST_BIN set then user has already
+                # set the path to the mingw32 make utility
+                ##
+                if shell_environment.GetEnvironment().get_shell_var("CLANG_HOST_BIN") is not None:
+                    self.Logger.debug("CLANG_HOST_BIN is already set.")
+
+                else:
+                    install_path = self._get_vs_install_path(None, None)
+                    vc_ver = self._get_vc_version(install_path, None)
+
+                    if install_path is None or vc_ver is None:
+                        self.Logger.error("Failed to configure environment for VS")
+                        return -1
+
+                    version_aggregator.GetVersionAggregator().ReportVersion(
+                        "Visual Studio Install Path", install_path, version_aggregator.VersionTypes.INFO)
+                    version_aggregator.GetVersionAggregator().ReportVersion(
+                        "VC Version", vc_ver, version_aggregator.VersionTypes.TOOL)
+
+                    # make path align with tools_def.txt
+                    prefix = os.path.join(install_path, "VC", "Tools", "MSVC", vc_ver)
+                    clang_host_bin_prefix = os.path.join(prefix, "bin", "Host%s" % HostType, HostType)
+
+                    # now confirm it exists
+                    if not os.path.exists(clang_host_bin_prefix):
+                        self.Logger.error("Path for VS toolchain is invalid")
+                        return -2
+
+                    # The environment is using nmake (not make) so add "n" to the end of the path.
+                    # The rest of the command is derived from definitions in tools.def.
+                    shell_environment.GetEnvironment().set_shell_var("CLANG_HOST_BIN", os.path.join(clang_host_bin_prefix, "n"))
+        elif thebuilder.env.GetValue("TOOL_CHAIN_TAG") == "CLANGDWARF":
+            ##
+            # If environment already has CLANG_HOST_BIN set then user has already
+            # set the path to the mingw32 make utility
+            ##
+
+            basetools_mingw_build = shell_environment.GetEnvironment().get_shell_var("BASETOOLS_MINGW_BUILD")
+            clang_host_bin = shell_environment.GetEnvironment().get_shell_var("CLANG_HOST_BIN")
+            clang_bin = shell_environment.GetEnvironment().get_shell_var("CLANG_BIN")
+
+            if basetools_mingw_build is not None:
+                self.Logger.debug("BASETOOLS_MINGW_BUILD is already set.")
+            else:
+                basetools_mingw_build = 'TRUE'
+            if basetools_mingw_build.upper() != 'TRUE':
+                self.Logger.error("BASETOOLS_MINGW_BUILD must be set to TRUE for CLANGDWARF toolchain.")
+                return -1
+
+            if clang_host_bin is not None:
+                self.Logger.debug("CLANG_HOST_BIN is already set.")
+            else:
+                # The environment is mingw32 make add "mingw32-" to the end of the path.
                 # The rest of the command is derived from definitions in tools.def.
-                shell_environment.GetEnvironment().set_shell_var("CLANG_HOST_BIN", os.path.join(clang_host_bin_prefix, "n"))
+                clang_host_bin = 'mingw32-'
+
+            if clang_bin is not None:
+                self.Logger.debug("CLANG_BIN is already set.")
+            else:
+                # Assume default installation "c:\edk2-clang\bin\"
+                clang_bin = "c:\\edk2-clang\\bin\\"
+
+            # now confirm it exists
+            if not os.path.exists(os.path.join(clang_bin, clang_host_bin + "make.exe")):
+                self.Logger.error(f"mingw32 toolchain not found in CLANG_BIN path {clang_bin}")
+                return -2
+
+            shell_environment.GetEnvironment().set_shell_var("BASETOOLS_MINGW_BUILD", basetools_mingw_build)
+            shell_environment.GetEnvironment().set_shell_var("CLANG_HOST_BIN", clang_host_bin)
+            shell_environment.GetEnvironment().set_shell_var("CLANG_BIN", clang_bin)
 
         return 0
 
