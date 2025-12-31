tianocore/edk2

commit cc149a8eaab5f09d021c035d81132bd6f21760e3

UefiPayloadPkg/UefiPayloadEntry: Import update capsules from bootloader
The implementation is for CbParseLib, SblParseLib returns success while
doing nothing.

The HOBs created by BuildCvHob() will be eventually picked up and
processed in DXE phase by
MdeModulePkg/Library/DxeCapsuleLibFmp/DxeCapsuleProcessLib.c

Because multiple capsules can be chained (simply by placing them one
after another), coreboot passes them as a single memory range which can
provide tenths of capsules.

coreboot won't pass any capsules unless it finds CapsuleUpdateData*
EFI variables and is able to parse memory ranges they point to as
scatter-gather lists of pages containing capsules.

Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

UefiPayloadPkg/Include/Library/BlParseLib.h
@@ -27,6 +27,12 @@ typedef RETURN_STATUS \
   VOID              *Param
   );
 
+typedef VOID \
+(*BL_CAPSULE_CALLBACK) (
+  EFI_PHYSICAL_ADDRESS  BaseAddress,
+  UINT64                Length
+  );
+
 /**
   This function retrieves the parameter base address from boot loader.
 
@@ -162,6 +168,20 @@ RETURN_STATUS
 EFIAPI
 ParseFirmwareInfo (
   OUT FIRMWARE_INFO  *FwInfo
+  );
+
+/**
+  Parse update capsules passed in by bootloader
+
+  @param  CapsuleCallback   The callback routine invoked for each capsule.
+
+  @retval RETURN_SUCCESS    Successfully parsed capsules.
+  @retval RETURN_NOT_FOUND  Failed to look up the information.
+**/
+RETURN_STATUS
+EFIAPI
+ParseCapsules (
+  IN BL_CAPSULE_CALLBACK  CapsuleCallback
   );
 
 #endif

UefiPayloadPkg/Include/Coreboot.h
@@ -283,6 +283,14 @@ struct lb_efi_fw_info {
   UINT32    fw_size;                  /* Size of firmware in bytes */
 } __attribute__ ((packed));
 
+#define CB_TAG_CAPSULE  0x0046
+struct cb_range {
+  UINT32    tag;
+  UINT32    size;
+  UINT64    range_start;
+  UINT32    range_size;
+} __attribute__ ((packed));
+
 /* Helpful macros */
 
 #define MEM_RANGE_COUNT(_rec) \

UefiPayloadPkg/Library/CbParseLib/CbParseLib.c
@@ -713,3 +713,40 @@ ParseFirmwareInfo (
   AsciiSPrint (FwInfo->VersionStr, sizeof (FwInfo->VersionStr), "%a%a", Version, ExtraVersion);
   return RETURN_SUCCESS;
 }
+
+/**
+  Parse update capsules passed in by bootloader
+
+  @param  CapsuleCallback   The callback routine invoked for each capsule.
+
+  @retval RETURN_SUCCESS    Successfully parsed capsules.
+  @retval RETURN_NOT_FOUND  Failed to look up the information.
+**/
+RETURN_STATUS
+EFIAPI
+ParseCapsules (
+  IN BL_CAPSULE_CALLBACK  CapsuleCallback
+  )
+{
+  struct cb_header  *Header;
+  struct cb_range   *Range;
+  UINT8             *TmpPtr;
+  UINTN             Idx;
+
+  Header = GetParameterBase ();
+  if (Header == NULL) {
+    return RETURN_NOT_FOUND;
+  }
+
+  TmpPtr = (UINT8 *)Header + Header->header_bytes;
+  for (Idx = 0; Idx < Header->table_entries; Idx++) {
+    Range = (struct cb_range *)TmpPtr;
+    if (Range->tag == CB_TAG_CAPSULE) {
+      CapsuleCallback (Range->range_start, Range->range_size);
+    }
+
+    TmpPtr += Range->size;
+  }
+
+  return RETURN_SUCCESS;
+}

UefiPayloadPkg/Library/PayloadEntryHobLib/Hob.c
@@ -558,6 +558,39 @@ BuildFv3Hob (
 }
 
 /**
+  Builds a Capsule Volume HOB.
+
+  This function builds a Capsule Volume HOB.
+  It can only be invoked during PEI phase;
+  for DXE phase, it will ASSERT() since PEI HOB is read-only for DXE phase.
+
+  If the platform does not support Capsule Volume HOBs, then ASSERT().
+  If there is no additional space for HOB creation, then ASSERT().
+
+  @param  BaseAddress   The base address of the Capsule Volume.
+  @param  Length        The size of the Capsule Volume in bytes.
+
+**/
+VOID
+EFIAPI
+BuildCvHob (
+  IN EFI_PHYSICAL_ADDRESS  BaseAddress,
+  IN UINT64                Length
+  )
+{
+  EFI_HOB_UEFI_CAPSULE  *Hob;
+
+  Hob = CreateHob (EFI_HOB_TYPE_UEFI_CAPSULE, sizeof (EFI_HOB_UEFI_CAPSULE));
+  ASSERT (Hob != NULL);
+  if (Hob == NULL) {
+    return;
+  }
+
+  Hob->BaseAddress = BaseAddress;
+  Hob->Length      = Length;
+}
+
+/**
   Builds a HOB for the CPU.
 
   This function builds a HOB for the CPU.

UefiPayloadPkg/Library/SblParseLib/SblParseLib.c
@@ -325,3 +325,23 @@ ParseFirmwareInfo (
 {
   return RETURN_NOT_FOUND;
 }
+
+/**
+  Parse update capsules passed in by bootloader
+
+  @param  CapsuleCallback   The callback routine invoked for each capsule.
+
+  @retval RETURN_SUCCESS    Successfully parsed capsules.
+  @retval RETURN_NOT_FOUND  Failed to look up the information.
+**/
+RETURN_STATUS
+EFIAPI
+ParseCapsules (
+  IN BL_CAPSULE_CALLBACK  CapsuleCallback
+  )
+{
+  //
+  // Treat not supporting this function like reporting zero capsules.
+  //
+  return RETURN_SUCCESS;
+}

UefiPayloadPkg/UefiPayloadEntry/UefiPayloadEntry.c
@@ -457,6 +457,15 @@ BuildHobFromBl (
     return Status;
   }
 
+  //
+  // Import update capsules, if there are any.
+  //
+  Status = ParseCapsules (BuildCvHob);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "Error when importing update capsules, Status = %r\n", Status));
+    return Status;
+  }
+
   return EFI_SUCCESS;
 }
 

commit 1ac358416a88b162cf46492065076ab45ea79691

UefiPayloadPkg: Fix build failure with GCC
Add missing EFIAPI to BL_CAPSULE_CALLBACK to fix build issue.

Fixes: cc149a8e

Signed-off-by: Khalid Ali <khaliidcaliy@gmail.com>

UefiPayloadPkg/Include/Library/BlParseLib.h
@@ -28,7 +28,7 @@ typedef RETURN_STATUS \
   );
 
 typedef VOID \
-(*BL_CAPSULE_CALLBACK) (
+(EFIAPI *BL_CAPSULE_CALLBACK)(
   EFI_PHYSICAL_ADDRESS  BaseAddress,
   UINT64                Length
   );