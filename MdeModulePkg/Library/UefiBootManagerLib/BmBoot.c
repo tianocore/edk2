5561429e0282328074dce62846ee0227f971d432
Sherry Fan <sherryfan@microsoft.com>
2026-01-16 17:37:59 -0800

    MdeModulePkg: Update performance measurements to use new perf macros
    Updates BmBoot and dispatcher to use new perf macros.
    
    Signed-off-by: Sherry Fan <sherryfan@microsoft.com>

--- MdeModulePkg/Library/UefiBootManagerLib/BmBoot.c
+++ MdeModulePkg/Library/UefiBootManagerLib/BmBoot.c
@@ -2072,13 +2072,15 @@ EfiBootManagerBoot (
                    );
         ASSERT_EFI_ERROR (Status);
         );
-
       mBmLegacyBoot (BootOption);
     } else {
       BootOption->Status = EFI_UNSUPPORTED;
     }
 
     PERF_END_EX (gImageHandle, "BdsAttempt", NULL, 0, (UINT32)OptionNumber);
+    PERF_INMODULE_END ("BdsAttempt");
+    PERF_CROSSMODULE_END ("BDS");   // BEGIN is in MdeModulePkg\Universal\BdsDxe\BdsEntry.c
+    PERF_CROSSMODULE_BEGIN ("BDS"); // Keep logging BDS in case of reentry
     return;
   }
 
@@ -2109,6 +2111,8 @@ EfiBootManagerBoot (
   PERF_CODE (
     BmEndOfBdsPerfCode (NULL, NULL);
     );
+  PERF_CROSSMODULE_END ("BDS");     // BEGIN is in MdeModulePkg\Universal\BdsDxe\BdsEntry.c
+  PERF_CROSSMODULE_BEGIN ("BDS");   // Keep logging BDS in case of reentry
 
   REPORT_STATUS_CODE (EFI_PROGRESS_CODE, PcdGet32 (PcdProgressCodeOsLoaderStart));
 
