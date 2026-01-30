d56ea35daebcc22deafda1d1330eb9d41f6bea70
Xiang W <wxjstz@126.com>
2026-01-30 15:25:46 +0800

    UefiPayloadPkg: BlSupportDxe add RISC-V support
    Signed-off-by: Xiang W <wangxiang@iscas.ac.cn>

---
+++ UefiPayloadPkg/BlSupportDxe/RiscV64/BlSupport.c
@@ -0,0 +1,30 @@
+/** @file
+  This file will report some MMIO/IO resources to dxe core.
+
+  Copyright (c) Xiang W <wangxiang@iscas.ac.cn>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include "BlSupportDxe.h"
+
+/**
+  Architecture level additional operation which needs to be performed before
+  launching payload.
+
+  @param[in] ImageHandle    The firmware allocated handle for the EFI image.
+  @param[in] SystemTable    A pointer to the EFI System Table.
+
+  @retval EFI_SUCCESS       The entry point is executed successfully.
+  @retval other             Some error occurs when executing this entry point.
+
+**/
+EFI_STATUS
+EFIAPI
+BlArchAdditionalOps (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  return EFI_SUCCESS;
+}
