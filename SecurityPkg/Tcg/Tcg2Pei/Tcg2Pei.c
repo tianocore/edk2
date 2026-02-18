microsoft/mu_tiano_plus

SecurityPkg: Improved performance changes for TCG2 modules
Add improved performance checks around TCG functions.

Signed-off-by: Dmitry Antipov <dmanti@microsoft.com>

cherry-pick from 2a5d467364

SecurityPkg/Tcg/Tcg2Pei/Tcg2Pei.c
@@ -21,6 +21,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #include <Guid/TcgEventHob.h>
 #include <Guid/MeasuredFvHob.h>
+#include <Guid/ExcludedFvHob.h> // MU_CHANGE
 #include <Guid/TpmInstance.h>
 #include <Guid/MigratedFvInfo.h>
 
@@ -218,9 +219,17 @@ EndofPeiSignalNotifyCallBack (
   IN VOID                       *Ppi
   )
 {
-  MEASURED_HOB_DATA  *MeasuredHobData;
+  MEASURED_HOB_DATA                                      *MeasuredHobData;
+  EXCLUDED_HOB_DATA                                      *ExcludedHobData;          // MU_CHANGE
+  EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_PPI  *MeasurementExcludedFvPpi; // MU_CHANGE
+  UINT32                                                 Instance;                  // MU_CHANGE
+  UINT32                                                 Count;                     // MU_CHANGE
+  UINT32                                                 HobIndex;                  // MU_CHANGE
+  EFI_STATUS                                             Status;                    // MU_CHANGE
 
-  MeasuredHobData = NULL;
+  MeasuredHobData          = NULL;
+  ExcludedHobData          = NULL; // MU_CHANGE
+  MeasurementExcludedFvPpi = NULL; // MU_CHANGE
 
   PERF_CALLBACK_BEGIN (&gEfiEndOfPeiSignalPpiGuid);
 
@@ -249,6 +258,76 @@ EndofPeiSignalNotifyCallBack (
     CopyMem (&MeasuredHobData->MeasuredFvBuf[mMeasuredBaseFvIndex], mMeasuredChildFvInfo, sizeof (EFI_PLATFORM_FIRMWARE_BLOB) * (mMeasuredChildFvIndex));
   }
 
+  // MU_CHANGE - START
+  // Create a guid hob to save all excluded FVs for DXE - mschange start
+  //
+
+  //
+  // Get count
+  //
+  Instance = 0;
+  Count    = 0;
+  do {
+    Status = PeiServicesLocatePpi (
+               &gEfiPeiFirmwareVolumeInfoMeasurementExcludedPpiGuid,
+               Instance,
+               NULL,
+               (VOID **)&MeasurementExcludedFvPpi
+               );
+    if (!EFI_ERROR (Status)) {
+      Count += MeasurementExcludedFvPpi->Count;
+      Instance++;
+    }
+  } while (!EFI_ERROR (Status));
+
+  // Check if there are any excluded FVs
+  if (Count > 0) {
+    DEBUG ((DEBUG_INFO, "Found %d FVs excluded.  Publishing hob\n", Count));
+    ExcludedHobData = BuildGuidHob (
+                        &gExcludedFvHobGuid,
+                        sizeof (EXCLUDED_HOB_DATA) + (sizeof (EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV) * Count)
+                        );
+
+    // Make sure allocation was successful
+    if (ExcludedHobData != NULL) {
+      ExcludedHobData->Num = Count;
+      //
+      // Copy FV info to hob data
+      //
+      Instance = 0;
+      HobIndex = 0;
+      do {
+        Status = PeiServicesLocatePpi (
+                   &gEfiPeiFirmwareVolumeInfoMeasurementExcludedPpiGuid,
+                   Instance,
+                   NULL,
+                   (VOID **)&MeasurementExcludedFvPpi
+                   );
+        if (!EFI_ERROR (Status)) {
+          if (MeasurementExcludedFvPpi->Count <= 0) {
+            DEBUG ((DEBUG_ERROR, "ExcludedFvPpi has invalid count %d", MeasurementExcludedFvPpi->Count));
+            ASSERT (MeasurementExcludedFvPpi->Count > 0);
+          } else if ( HobIndex + MeasurementExcludedFvPpi->Count > Count) {
+            DEBUG ((DEBUG_ERROR, "Found more ExcludedFvPpi fvs than when calculated buffer size.  BufferSizeCount (0x%x)  NewCount (0x%x)", Count, (HobIndex + MeasurementExcludedFvPpi->Count)));
+            ASSERT (HobIndex + MeasurementExcludedFvPpi->Count <= Count);
+          } else {
+            CopyMem (&ExcludedHobData->ExcludedFvs[HobIndex], &MeasurementExcludedFvPpi->Fv[0], sizeof (EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV) * MeasurementExcludedFvPpi->Count);
+            HobIndex += MeasurementExcludedFvPpi->Count;
+          }
+
+          Instance++;
+        }
+      } while (!EFI_ERROR (Status));
+    } else {
+      DEBUG ((
+        DEBUG_ERROR,
+        "Failed to allocate 0x%x byte of memory for ExcludedFvHob",
+        sizeof (EXCLUDED_HOB_DATA) + (sizeof (EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV) * Count)
+        ));
+    }
+  }  // Done with Excluded Fv Hob
+
+  // MU_CHANGE - END
   PERF_CALLBACK_END (&gEfiEndOfPeiSignalPpiGuid);
 
   return EFI_SUCCESS;
@@ -266,9 +345,9 @@ SyncPcrAllocationsAndPcrMask (
   EFI_STATUS                       Status;
   EFI_TCG2_EVENT_ALGORITHM_BITMAP  TpmHashAlgorithmBitmap;
   UINT32                           TpmActivePcrBanks;
-  UINT32                           NewTpmActivePcrBanks;
-  UINT32                           Tpm2PcrMask;
-  UINT32                           NewTpm2PcrMask;
+  // UINT32                            NewTpmActivePcrBanks;   // MU_CHANGE - Update PCR order, add PCD, enable deallocate *and* allocate.
+  UINT32  Tpm2PcrMask;
+  UINT32  NewTpm2PcrMask;
 
   DEBUG ((DEBUG_ERROR, "SyncPcrAllocationsAndPcrMask!\n"));
 
@@ -297,6 +376,9 @@ SyncPcrAllocationsAndPcrMask (
   // If there are active PCR banks that are not supported by the Platform mask,
   // update the TPM allocations and reboot the machine.
   //
+
+  /*
+  // MU_CHANGE - Update PCR order, add PCD, enable deallocate *and* allocate.
   if ((TpmActivePcrBanks & Tpm2PcrMask) != TpmActivePcrBanks) {
     NewTpmActivePcrBanks = TpmActivePcrBanks & Tpm2PcrMask;
 
@@ -312,14 +394,15 @@ SyncPcrAllocationsAndPcrMask (
         //
         DEBUG ((DEBUG_ERROR, "%a - Failed to reallocate PCRs!\n", __func__));
         ASSERT_EFI_ERROR (Status);
-      }
 
+      }
       //
       // Need reset system, since we just called Tpm2PcrAllocateBanks().
       //
       ResetCold ();
     }
   }
+  */
 
   //
   // If there are any PCRs that claim support in the Platform mask that are
@@ -336,6 +419,29 @@ SyncPcrAllocationsAndPcrMask (
 
     Status = PcdSet32S (PcdTpm2HashMask, NewTpm2PcrMask);
     ASSERT_EFI_ERROR (Status);
+    Tpm2PcrMask = NewTpm2PcrMask;
+  }
+
+  //
+  // If there are active PCR banks that are not supported by the Platform mask,
+  // update the TPM allocations and reboot the machine.
+  // MU_CHANGE - Update PCR order, add PCD, enable deallocate *and* allocate.
+  //
+  if ((Tpm2PcrMask != TpmActivePcrBanks) && FixedPcdGetBool (PcdForceReallocatePcrBanks)) {
+    DEBUG ((DEBUG_INFO, "%a - Reallocating PCR banks from 0x%X to 0x%X.\n", __FUNCTION__, TpmActivePcrBanks, Tpm2PcrMask));
+    Status = Tpm2PcrAllocateBanks (NULL, (UINT32)TpmHashAlgorithmBitmap, Tpm2PcrMask);
+    if (EFI_ERROR (Status)) {
+      //
+      // We can't do much here, but we hope that this doesn't happen.
+      //
+      DEBUG ((DEBUG_ERROR, "%a - Failed to reallocate PCRs!\n", __FUNCTION__));
+      ASSERT_EFI_ERROR (Status);
+    }
+
+    //
+    // Need reset system, since we just called Tpm2PcrAllocateBanks().
+    //
+    ResetCold ();
   }
 }
 
@@ -820,7 +926,7 @@ MeasureMainBios (
   EFI_FV_INFO                  VolumeInfo;
   EFI_PEI_FIRMWARE_VOLUME_PPI  *FvPpi;
 
-  PERF_START_EX (mFileHandle, "EventRec", "Tcg2Pei", 0, PERF_ID_TCG2_PEI);
+  PERF_FUNCTION_BEGIN (); // MU_CHANGE
 
   //
   // Only measure BFV at the very beginning. Other parts of Static Core Root of
@@ -851,7 +957,7 @@ MeasureMainBios (
 
   Status = MeasureFvImage ((EFI_PHYSICAL_ADDRESS)(UINTN)VolumeInfo.FvStart, VolumeInfo.FvSize);
 
-  PERF_END_EX (mFileHandle, "EventRec", "Tcg2Pei", 0, PERF_ID_TCG2_PEI + 1);
+  PERF_FUNCTION_END (); // MU_CHANGE
 
   return Status;
 }

tianocore/edk2

SecurityPkg/Tcg/Tcg2Pei/Tcg2Pei.c
@@ -21,6 +21,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #include <Guid/TcgEventHob.h>
 #include <Guid/MeasuredFvHob.h>
+#include <Guid/ExcludedFvHob.h>
 #include <Guid/TpmInstance.h>
 #include <Guid/MigratedFvInfo.h>
 
@@ -214,9 +215,17 @@ EndofPeiSignalNotifyCallBack (
   IN VOID                       *Ppi
   )
 {
-  MEASURED_HOB_DATA  *MeasuredHobData;
+  MEASURED_HOB_DATA                                      *MeasuredHobData;
+  EXCLUDED_HOB_DATA                                      *ExcludedHobData;
+  EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_PPI  *MeasurementExcludedFvPpi;
+  UINT32                                                 Instance;
+  UINT32                                                 Count;
+  UINT32                                                 HobIndex;
+  EFI_STATUS                                             Status;
 
-  MeasuredHobData = NULL;
+  MeasuredHobData          = NULL;
+  ExcludedHobData          = NULL;
+  MeasurementExcludedFvPpi = NULL;
 
   PERF_CALLBACK_BEGIN (&gEfiEndOfPeiSignalPpiGuid);
 
@@ -245,6 +254,75 @@ EndofPeiSignalNotifyCallBack (
     CopyMem (&MeasuredHobData->MeasuredFvBuf[mMeasuredBaseFvIndex], mMeasuredChildFvInfo, sizeof (EFI_PLATFORM_FIRMWARE_BLOB) * (mMeasuredChildFvIndex));
   }
 
+  //
+  // Create a guid hob to save all excluded FVs for DXE
+  //
+
+  //
+  // Get count
+  //
+  Instance = 0;
+  Count    = 0;
+  do {
+    Status = PeiServicesLocatePpi (
+               &gEfiPeiFirmwareVolumeInfoMeasurementExcludedPpiGuid,
+               Instance,
+               NULL,
+               (VOID **)&MeasurementExcludedFvPpi
+               );
+    if (!EFI_ERROR (Status)) {
+      Count += MeasurementExcludedFvPpi->Count;
+      Instance++;
+    }
+  } while (!EFI_ERROR (Status));
+
+  // Check if there are any excluded FVs
+  if (Count > 0) {
+    DEBUG ((DEBUG_INFO, "Found %d FVs excluded.  Publishing hob\n", Count));
+    ExcludedHobData = BuildGuidHob (
+                        &gExcludedFvHobGuid,
+                        sizeof (EXCLUDED_HOB_DATA) + (sizeof (EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV) * Count)
+                        );
+
+    // Make sure allocation was successful
+    if (ExcludedHobData != NULL) {
+      ExcludedHobData->Num = Count;
+      //
+      // Copy FV info to hob data
+      //
+      Instance = 0;
+      HobIndex = 0;
+      do {
+        Status = PeiServicesLocatePpi (
+                   &gEfiPeiFirmwareVolumeInfoMeasurementExcludedPpiGuid,
+                   Instance,
+                   NULL,
+                   (VOID **)&MeasurementExcludedFvPpi
+                   );
+        if (!EFI_ERROR (Status)) {
+          if (MeasurementExcludedFvPpi->Count <= 0) {
+            DEBUG ((DEBUG_ERROR, "ExcludedFvPpi has invalid count %d", MeasurementExcludedFvPpi->Count));
+            ASSERT (MeasurementExcludedFvPpi->Count > 0);
+          } else if ( HobIndex + MeasurementExcludedFvPpi->Count > Count) {
+            DEBUG ((DEBUG_ERROR, "Found more ExcludedFvPpi fvs than when calculated buffer size.  BufferSizeCount (0x%x)  NewCount (0x%x)", Count, (HobIndex + MeasurementExcludedFvPpi->Count)));
+            ASSERT (HobIndex + MeasurementExcludedFvPpi->Count <= Count);
+          } else {
+            CopyMem (&ExcludedHobData->ExcludedFvs[HobIndex], &MeasurementExcludedFvPpi->Fv[0], sizeof (EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV) * MeasurementExcludedFvPpi->Count);
+            HobIndex += MeasurementExcludedFvPpi->Count;
+          }
+
+          Instance++;
+        }
+      } while (!EFI_ERROR (Status));
+    } else {
+      DEBUG ((
+        DEBUG_ERROR,
+        "Failed to allocate 0x%x byte of memory for ExcludedFvHob",
+        sizeof (EXCLUDED_HOB_DATA) + (sizeof (EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV) * Count)
+        ));
+    }
+  }  // Done with Excluded Fv Hob
+
   PERF_CALLBACK_END (&gEfiEndOfPeiSignalPpiGuid);
 
   return EFI_SUCCESS;
@@ -263,7 +341,6 @@ SyncPcrAllocationsAndPcrMask (
   EFI_TCG2_EVENT_ALGORITHM_BITMAP  TpmHashAlgorithmBitmap;
   EFI_TCG2_EVENT_ALGORITHM_BITMAP  BiosHashAlgorithmBitmap;
   UINT32                           TpmActivePcrBanks;
-  UINT32                           NewTpmActivePcrBanks;
   UINT32                           Tpm2PcrMask;
   UINT32                           NewTpm2PcrMask;
 
@@ -302,39 +379,6 @@ SyncPcrAllocationsAndPcrMask (
   BiosHashAlgorithmBitmap = PcdGet32 (PcdTcg2HashAlgorithmBitmap);
   DEBUG ((DEBUG_INFO, "Tcg2HashAlgorithmBitmap: 0x%08x\n", BiosHashAlgorithmBitmap));
 
-  if (((TpmActivePcrBanks & Tpm2PcrMask) != TpmActivePcrBanks) ||
-      ((TpmActivePcrBanks & BiosHashAlgorithmBitmap) != TpmActivePcrBanks))
-  {
-    DEBUG ((DEBUG_INFO, "TpmActivePcrBanks & Tpm2PcrMask = 0x%08x\n", (TpmActivePcrBanks & Tpm2PcrMask)));
-    DEBUG ((DEBUG_INFO, "TpmActivePcrBanks & BiosHashAlgorithmBitmap = 0x%08x\n", (TpmActivePcrBanks & BiosHashAlgorithmBitmap)));
-    NewTpmActivePcrBanks  = TpmActivePcrBanks;
-    NewTpmActivePcrBanks &= Tpm2PcrMask;
-    NewTpmActivePcrBanks &= BiosHashAlgorithmBitmap;
-    DEBUG ((DEBUG_INFO, "NewTpmActivePcrBanks 0x%08x\n", NewTpmActivePcrBanks));
-
-    DEBUG ((DEBUG_INFO, "%a - Reallocating PCR banks from 0x%X to 0x%X.\n", __func__, TpmActivePcrBanks, NewTpmActivePcrBanks));
-
-    if (NewTpmActivePcrBanks == 0) {
-      DEBUG ((DEBUG_ERROR, "%a - No viable PCRs active! Please set a less restrictive value for PcdTpm2HashMask!\n", __func__));
-      ASSERT (FALSE);
-    } else {
-      DEBUG ((DEBUG_ERROR, "Tpm2PcrAllocateBanks (TpmHashAlgorithmBitmap: 0x%08x, NewTpmActivePcrBanks: 0x%08x)\n", TpmHashAlgorithmBitmap, NewTpmActivePcrBanks));
-      Status = Tpm2PcrAllocateBanks (NULL, (UINT32)TpmHashAlgorithmBitmap, NewTpmActivePcrBanks);
-      if (EFI_ERROR (Status)) {
-        //
-        // We can't do much here, but we hope that this doesn't happen.
-        //
-        DEBUG ((DEBUG_ERROR, "%a - Failed to reallocate PCRs!\n", __func__));
-        ASSERT_EFI_ERROR (Status);
-      }
-
-      //
-      // Need reset system, since we just called Tpm2PcrAllocateBanks().
-      //
-      ResetCold ();
-    }
-  }
-
   //
   // If there are any PCRs that claim support in the Platform mask that are
   // not supported by the TPM, update the mask.
@@ -351,6 +395,29 @@ SyncPcrAllocationsAndPcrMask (
     Status = PcdSet32S (PcdTpm2HashMask, NewTpm2PcrMask);
     DEBUG ((DEBUG_ERROR, "Set PcdTpm2Hash Mask to 0x%08x\n", NewTpm2PcrMask));
     ASSERT_EFI_ERROR (Status);
+    Tpm2PcrMask = NewTpm2PcrMask;
+  }
+
+  //
+  // If there are active PCR banks that are not supported by the Platform mask,
+  // update the TPM allocations and reboot the machine.
+  // Update PCR order, add PCD, enable deallocate *and* allocate.
+  //
+  if ((Tpm2PcrMask != TpmActivePcrBanks) && FixedPcdGetBool (PcdForceReallocatePcrBanks)) {
+    DEBUG ((DEBUG_INFO, "%a - Reallocating PCR banks from 0x%X to 0x%X.\n", __func__, TpmActivePcrBanks, Tpm2PcrMask));
+    Status = Tpm2PcrAllocateBanks (NULL, (UINT32)TpmHashAlgorithmBitmap, Tpm2PcrMask);
+    if (EFI_ERROR (Status)) {
+      //
+      // We can't do much here, but we hope that this doesn't happen.
+      //
+      DEBUG ((DEBUG_ERROR, "%a - Failed to reallocate PCRs!\n", __func__));
+      ASSERT_EFI_ERROR (Status);
+    }
+
+    //
+    // Need reset system, since we just called Tpm2PcrAllocateBanks().
+    //
+    ResetCold ();
   }
 }
 
@@ -835,7 +902,7 @@ MeasureMainBios (
   EFI_FV_INFO                  VolumeInfo;
   EFI_PEI_FIRMWARE_VOLUME_PPI  *FvPpi;
 
-  PERF_START_EX (mFileHandle, "EventRec", "Tcg2Pei", 0, PERF_ID_TCG2_PEI);
+  PERF_FUNCTION_BEGIN ();
 
   //
   // Only measure BFV at the very beginning. Other parts of Static Core Root of
@@ -866,7 +933,7 @@ MeasureMainBios (
 
   Status = MeasureFvImage ((EFI_PHYSICAL_ADDRESS)(UINTN)VolumeInfo.FvStart, VolumeInfo.FvSize);
 
-  PERF_END_EX (mFileHandle, "EventRec", "Tcg2Pei", 0, PERF_ID_TCG2_PEI + 1);
+  PERF_FUNCTION_END ();
 
   return Status;
 }