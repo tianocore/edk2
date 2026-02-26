tianocore/edk2

commit 0b867b65a2c1b30a822e09572323495478778e81

BaseTools/Plugin/WindowsVsToolChain: CLANGPDB add IA32 DLLs to Path
Add %VCToolsInstallDir%\bin\Hostx64\x86 to PATH to add DLLs
required for IA32 host-based unit tests to execute.

Some Visual Studio environments add the IA32 DLL path to PATH
and some do not. The failure was observed in GitHub Action
windows-2025 agents where the X64 DLL path was added, but the
IA32 DLL path was missing.

Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>

BaseTools/Plugin/WindowsVsToolChain/WindowsVsToolChain.py
@@ -403,6 +403,15 @@ class WindowsVsToolChain(IUefiBuildPlugin):
                 for (k, v) in vs_vars.items():
                     shell_env.set_shell_var(k, v)
 
+                # Add path to IA32 toolchain DLLs for unit test execution
+                vc_tools_install_dir = shell_env.get_shell_var("VCToolsInstallDir")
+                if vc_tools_install_dir:
+                    ia32_dll_path = os.path.join(vc_tools_install_dir, 'bin', 'Hostx64', 'x86')
+                    if os.path.exists (ia32_dll_path):
+                        shell_env.append_path(os.path.join (vc_tools_install_dir, 'bin', 'Hostx64', 'x86'))
+                    else:
+                        self.Logger.warning("IA32 toolchain DLLs not found in expected path %s. IA32 unit tests may fail to run." % ia32_dll_path)
+
                 ##
                 # If environment already has CLANG_HOST_BIN set then user has already
                 # set the path to the mingw32 make utility

commit 5caee63d2d32fa37ab8b3208902c5e62f603dce7

BaseTools/WindowsVsToolChain.py: Minor code style updates
Fixes: 0b867b65a2c1b30a822e09572323495478778e81

Signed-off-by: Mike Beaton <mjsbeaton@gmail.com>

BaseTools/Plugin/WindowsVsToolChain/WindowsVsToolChain.py
@@ -407,8 +407,8 @@ class WindowsVsToolChain(IUefiBuildPlugin):
                 vc_tools_install_dir = shell_env.get_shell_var("VCToolsInstallDir")
                 if vc_tools_install_dir:
                     ia32_dll_path = os.path.join(vc_tools_install_dir, 'bin', 'Hostx64', 'x86')
-                    if os.path.exists (ia32_dll_path):
-                        shell_env.append_path(os.path.join (vc_tools_install_dir, 'bin', 'Hostx64', 'x86'))
+                    if os.path.exists(ia32_dll_path):
+                        shell_env.append_path(ia32_dll_path)
                     else:
                         self.Logger.warning("IA32 toolchain DLLs not found in expected path %s. IA32 unit tests may fail to run." % ia32_dll_path)
 