os-d/mu_basecore

MdeModulePkg: BaseMemoryBinLib: Add EFI_MEMORY_TYPE_STATISTICS_HEADER
This commit adds a new header structure for an array of
EFI_MEMORY_TYPE_STATISTICS structs. This will be used to
publish the array as a config table.

In addition, this commit updates the fields of the
EFI_MEMORY_TYPE_STATISTICS struct to track pages allocated
inside and outside of the bin and whether this is the default
bin or not.

Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

MdeModulePkg/Include/Guid/MemoryTypeInformation.h
@@ -44,11 +44,22 @@ typedef struct {
 typedef struct {
   EFI_PHYSICAL_ADDRESS    BaseAddress;
   EFI_PHYSICAL_ADDRESS    MaximumAddress;
-  UINT64                  CurrentNumberOfPages;
-  UINT64                  NumberOfPages;
+  UINT64                  CurrentNumberOfPagesInBin;
+  UINT64                  CurrentNumberOfPagesOutOfBin;
+  UINT64                  BinNumberOfPages;
   UINTN                   InformationIndex;
   BOOLEAN                 Special;
   BOOLEAN                 Runtime;
+  BOOLEAN                 DefaultBin;
+  EFI_MEMORY_TYPE         Type;
 } EFI_MEMORY_TYPE_STATISTICS;
+
+typedef struct {
+  UINT32                        Version;
+  UINT32                        NumEntries;
+  EFI_MEMORY_TYPE_STATISTICS    Statistics[EfiMaxMemoryType + 1];
+} EFI_MEMORY_TYPE_STATISTICS_HEADER;
+
+#define CURRENT_MEMORY_TYPE_STATISTICS_VERSION  1
 
 #endif

tianocore/edk2

MdeModulePkg/Include/Guid/MemoryTypeInformation.h
@@ -36,9 +36,19 @@ typedef struct {
 typedef struct {
   EFI_PHYSICAL_ADDRESS    BaseAddress;
   EFI_PHYSICAL_ADDRESS    MaximumAddress;
-  UINT64                  CurrentNumberOfPages;
-  UINT64                  NumberOfPages;
+  UINT32                  CurrentNumberOfPagesInBin;
+  UINT32                  CurrentNumberOfPagesOutOfBin;
+  UINT32                  BinNumberOfPages;
   UINTN                   InformationIndex;
   BOOLEAN                 Special;
   BOOLEAN                 Runtime;
+  BOOLEAN                 DefaultBin;
+  EFI_MEMORY_TYPE         Type;
 } EFI_MEMORY_TYPE_STATISTICS;
+
+typedef struct {
+  UINT32                        Version;
+  EFI_MEMORY_TYPE_STATISTICS    Statistics[EfiMaxMemoryType + 1];
+} EFI_MEMORY_TYPE_STATISTICS_HEADER;
+
+#define CURRENT_MEMORY_TYPE_STATISTICS_VERSION  1