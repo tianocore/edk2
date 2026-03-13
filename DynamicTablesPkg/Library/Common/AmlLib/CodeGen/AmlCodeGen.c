tianocore/edk2

commit 1774a34cc518b0d337a0a7f4707d944dc78cb95d

DynamicTablesPkg/AmlLib: Refactor AmlCodeGenInvokeMethod API
Refactor AmlCodeGenInvokeMethod() to construct a proper
AML_METHOD_INVOC_OP wrapper node instead of attaching the method
name and argument nodes directly as siblings to the parent. This
aligns the implementation with AmlCodeGenReturnInvokeMethod().

The method invocation is now built as a structured node with:
- FixedArg[0]: method name string (NameString data node)
- FixedArg[1]: argument count (UINT8 data node)
- VarList: parameter nodes (integer, string, ArgObj, LocalObj)

Also add an optional OUT NewObjectNode parameter so callers can
retrieve the created MethodInvocation node, and make ParentNode
optional, matching the convention used by other AmlLib CodeGen APIs.

Signed-off-by: Abdul Lateef Attar <AbdulLateef.Attar@amd.com>

DynamicTablesPkg/Library/Common/AmlLib/CodeGen/AmlCodeGen.c
@@ -3946,7 +3946,7 @@ AmlAddIntegerToNamedPackage (
   Added integer, string, ArgObj and LocalObj support.
 
   Example 1:
-    AmlCodeGenInvokeMethod ("MET0", 0, NULL, ParentNode);
+    AmlCodeGenInvokeMethod ("MET0", 0, NULL, ParentNode, &NewObjectNode);
     is equivalent to the following ASL code:
       MET0 ();
 
@@ -3960,7 +3960,7 @@ AmlAddIntegerToNamedPackage (
     Param[2].Type = AmlMethodParamTypeArg;
     Param[3].Data.Local = 2;
     Param[3].Type = AmlMethodParamTypeLocal;
-    AmlCodeGenInvokeMethod ("MET0", 4, Param, ParentNode);
+    AmlCodeGenInvokeMethod ("MET0", 4, Param, ParentNode, &NewObjectNode);
 
     is equivalent to the following ASL code:
       MET0 (0x100, "TEST", Arg0, Local2);
@@ -3973,7 +3973,7 @@ AmlAddIntegerToNamedPackage (
     Param[1].Type = AmlMethodParamTypeInteger;
     AmlCodeGenMethodRetNameString ("MET2", NULL, 2, TRUE, 0,
       ParentNode, &MethodNode);
-    AmlCodeGenInvokeMethod ("MET3", 2, Param, MethodNode);
+    AmlCodeGenInvokeMethod ("MET3", 2, Param, MethodNode, &NewObjectNode);
 
     is equivalent to the following ASL code:
     Method (MET2, 2, Serialized)
@@ -3981,12 +3981,13 @@ AmlAddIntegerToNamedPackage (
       MET3 (Arg0, 0x0100)
     }
 
-  @param [in] MethodNameString  The method name to be called or invoked.
-  @param [in] NumArgs           Number of arguments to be passed,
-                                0 to 7 are permissible values.
-  @param [in] Parameters        Contains the parameter data.
-  @param [in] ParentNode        The parent node to which the method invocation
-                                nodes are attached.
+  @param [in]  MethodNameString  The method name to be called or invoked.
+  @param [in]  NumArgs           Number of arguments to be passed,
+                                 0 to 7 are permissible values.
+  @param [in]  Parameters        Contains the parameter data.
+  @param [in]  ParentNode        The parent node to which the method invocation
+                                 nodes are attached.
+  @param [out] NewObjectNode     If success, contains the created node.
 
   @retval EFI_SUCCESS             Success.
   @retval EFI_INVALID_PARAMETER   Invalid parameter.
@@ -3995,10 +3996,11 @@ AmlAddIntegerToNamedPackage (
 EFI_STATUS
 EFIAPI
 AmlCodeGenInvokeMethod (
-  IN  CONST CHAR8             *MethodNameString,
-  IN        UINT8             NumArgs,
-  IN        AML_METHOD_PARAM  *Parameters   OPTIONAL,
-  IN        AML_NODE_HANDLE   ParentNode
+  IN  CONST CHAR8                   *MethodNameString,
+  IN        UINT8                   NumArgs,
+  IN        AML_METHOD_PARAM        *Parameters     OPTIONAL,
+  IN        AML_NODE_HANDLE         ParentNode      OPTIONAL,
+  OUT       AML_OBJECT_NODE_HANDLE  *NewObjectNode  OPTIONAL
   )
 {
   EFI_STATUS       Status;
@@ -4008,27 +4010,31 @@ AmlCodeGenInvokeMethod (
   AML_DATA_NODE    *DataNode;
   AML_OBJECT_NODE  *ObjectNode;
   AML_NODE_HANDLE  *NodeStream;
+  AML_OBJECT_NODE  *MethodInvocationNode;
+  AML_DATA_NODE    *ArgCountNode;
 
-  if ((MethodNameString == NULL) || (ParentNode == NULL)) {
-    ASSERT (0);
+  if ((MethodNameString == NULL) ||
+      ((ParentNode == NULL) && (NewObjectNode == NULL)))
+  {
+    ASSERT_EFI_ERROR (EFI_INVALID_PARAMETER);
     return EFI_INVALID_PARAMETER;
   }
 
-  if ((NumArgs > 7) ||
+  if ((NumArgs > AML_METHOD_MAX_NUM_ARGS) ||
       ((Parameters == NULL) && (NumArgs > 0)))
   {
-    ASSERT (0);
+    ASSERT_EFI_ERROR (EFI_INVALID_PARAMETER);
     return EFI_INVALID_PARAMETER;
   }
 
-  /// Allocate space to store methodname, object, data node pointers
+  MethodInvocationNode = NULL;
+
   NodeStream = AllocateZeroPool (sizeof (AML_NODE_HANDLE) * (NumArgs + 1));
   if (NodeStream == NULL) {
-    ASSERT (0);
+    ASSERT_EFI_ERROR (EFI_OUT_OF_RESOURCES);
     return EFI_OUT_OF_RESOURCES;
   }
 
-  /// Create a called or invoked method name string.
   Status = ConvertAslNameToAmlName (MethodNameString, &AmlNameString);
   if (EFI_ERROR (Status)) {
     ASSERT_EFI_ERROR (Status);
@@ -4058,7 +4064,6 @@ AmlCodeGenInvokeMethod (
   NodeStream[0] = (AML_NODE_HANDLE)DataNode;
 
   if (Parameters != NULL) {
-    /// Validate and convert the Parameters to the stream of nodes.
     for (Index = 0; Index < NumArgs; Index++) {
       ObjectNode = NULL;
       switch (Parameters[Index].Type) {
@@ -4136,38 +4141,89 @@ AmlCodeGenInvokeMethod (
           ASSERT_EFI_ERROR (EFI_INVALID_PARAMETER);
           Status = EFI_INVALID_PARAMETER;
           goto exit_handler;
-      } // switch
+      }  // switch
 
-      // Link the Object Node in the Node Stream.
       NodeStream[Index + 1] = (AML_NODE_HANDLE)ObjectNode;
-    } // for
+    }  // for
   }
 
-  /// Index <= NumArgs, because an additional method name was added.
-  for (Index = 0; Index <= NumArgs; Index++) {
+  Status = AmlCreateObjectNode (
+             AmlGetByteEncodingByOpCode (AML_METHOD_INVOC_OP, 0),
+             0,
+             &MethodInvocationNode
+             );
+  if (EFI_ERROR (Status)) {
+    ASSERT_EFI_ERROR (Status);
+    goto exit_handler;
+  }
+
+  Status = AmlSetFixedArgument (
+             MethodInvocationNode,
+             EAmlParseIndexTerm0,
+             (AML_NODE_HEADER *)NodeStream[0]
+             );
+  if (EFI_ERROR (Status)) {
+    ASSERT_EFI_ERROR (Status);
+    goto exit_handler;
+  }
+
+  ArgCountNode = NULL;
+  Status       = AmlCreateDataNode (
+                   EAmlNodeDataTypeUInt,
+                   &NumArgs,
+                   sizeof (UINT8),
+                   &ArgCountNode
+                   );
+  if (EFI_ERROR (Status)) {
+    ASSERT_EFI_ERROR (Status);
+    goto exit_handler;
+  }
+
+  Status = AmlSetFixedArgument (
+             MethodInvocationNode,
+             EAmlParseIndexTerm1,
+             (AML_NODE_HEADER *)ArgCountNode
+             );
+  if (EFI_ERROR (Status)) {
+    ASSERT_EFI_ERROR (Status);
+    goto exit_handler;
+  }
+
+  ArgCountNode = NULL;
+
+  for (Index = 1; Index <= NumArgs; Index++) {
     Status = AmlVarListAddTail (
-               (AML_NODE_HANDLE)ParentNode,
+               (AML_NODE_HANDLE)MethodInvocationNode,
                (AML_NODE_HANDLE)NodeStream[Index]
                );
     if (EFI_ERROR (Status)) {
       ASSERT_EFI_ERROR (Status);
-      goto exit_handler_detach;
+      goto exit_handler;
     }
+  }
+
+  Status = LinkNode (
+             MethodInvocationNode,
+             ParentNode,
+             NewObjectNode
+             );
+  if (EFI_ERROR (Status)) {
+    ASSERT_EFI_ERROR (Status);
+    goto exit_handler;
   }
 
   FreePool (NodeStream);
   return Status;
 
-exit_handler_detach:
-  /// The index contains the last successful node attached.
-  for ( ; Index > 0; Index--) {
-    /// Index contains the node number that is failed for AmlVarListAddTail().
-    /// Hence, start detaching from the last successful
-    AmlDetachNode (NodeStream[Index-1]);
+exit_handler:
+  if (MethodInvocationNode != NULL) {
+    AmlDeleteTree ((AML_NODE_HANDLE)MethodInvocationNode);
   }
 
-exit_handler:
-  /// Index <= NumArgs, because an additional method name was added.
+  if (ArgCountNode != NULL) {
+    AmlDeleteTree ((AML_NODE_HANDLE)ArgCountNode);
+  }
+
   for (Index = 0; Index <= NumArgs; Index++) {
     if (NodeStream[Index] != 0) {
       AmlDeleteTree (NodeStream[Index]);

DynamicTablesPkg/Include/Library/AmlLib/AmlLib.h
@@ -1842,7 +1842,7 @@ AmlAddIntegerToNamedPackage (
   Added integer, string, ArgObj and LocalObj support.
 
   Example 1:
-    AmlCodeGenInvokeMethod ("MET0", 0, NULL, ParentNode);
+    AmlCodeGenInvokeMethod ("MET0", 0, NULL, ParentNode, &NewObjectNode);
     is equivalent to the following ASL code:
       MET0 ();
 
@@ -1856,7 +1856,7 @@ AmlAddIntegerToNamedPackage (
     Param[2].Type = AmlMethodParamTypeArg;
     Param[3].Data.Local = 2;
     Param[3].Type = AmlMethodParamTypeLocal;
-    AmlCodeGenInvokeMethod ("MET0", 4, Param, ParentNode);
+    AmlCodeGenInvokeMethod ("MET0", 4, Param, ParentNode, &NewObjectNode);
 
     is equivalent to the following ASL code:
       MET0 (0x100, "TEST", Arg0, Local2);
@@ -1868,7 +1868,7 @@ AmlAddIntegerToNamedPackage (
     Param[1].Data.Integer = 0x100;
     Param[1].Type = AmlMethodParamTypeInteger;
     AmlCodeGenMethodRetNameString ("MET2", NULL, 2, TRUE, 0, ParentNode, &MethodNode);
-    AmlCodeGenInvokeMethod ("MET3", 2, Param, MethodNode);
+    AmlCodeGenInvokeMethod ("MET3", 2, Param, MethodNode, &NewObjectNode);
 
     is equivalent to the following ASL code:
     Method (MET2, 2, Serialized)
@@ -1876,12 +1876,13 @@ AmlAddIntegerToNamedPackage (
       MET3 (Arg0, 0x0100)
     }
 
-  @param [in] MethodNameString  The method name to be called or invoked.
-  @param [in] NumArgs           Number of arguments to be passed,
-                                0 to 7 are permissible values.
-  @param [in] Parameters        Contains the parameter data.
-  @param [in] ParentNode        The parent node to which the method invocation
-                                nodes are attached.
+  @param [in]  MethodNameString  The method name to be called or invoked.
+  @param [in]  NumArgs           Number of arguments to be passed,
+                                 0 to 7 are permissible values.
+  @param [in]  Parameters        Contains the parameter data.
+  @param [in]  ParentNode        The parent node to which the method invocation
+                                 nodes are attached.
+  @param [out] NewObjectNode     If success, contains the created node.
 
   @retval EFI_SUCCESS             Success.
   @retval EFI_INVALID_PARAMETER   Invalid parameter.
@@ -1890,10 +1891,11 @@ AmlAddIntegerToNamedPackage (
 EFI_STATUS
 EFIAPI
 AmlCodeGenInvokeMethod (
-  IN  CONST CHAR8             *MethodNameString,
-  IN        UINT8             NumArgs,
-  IN        AML_METHOD_PARAM  *Parameters   OPTIONAL,
-  IN        AML_NODE_HANDLE   ParentNode
+  IN  CONST CHAR8                   *MethodNameString,
+  IN        UINT8                   NumArgs,
+  IN        AML_METHOD_PARAM        *Parameters     OPTIONAL,
+  IN        AML_NODE_HANDLE         ParentNode      OPTIONAL,
+  OUT       AML_OBJECT_NODE_HANDLE  *NewObjectNode  OPTIONAL
   );
 
 /** Create a _PSD node.

commit 7185cc856102ef12d9876799790a8d7f317505d3

DynamicTablesPkg/AmlLib: Fix refactor AmlCodeGenInvokeMethod API
CLANGPDB CI shows that ArgCountNode can be used uninitialized. This is
a genuine error, it should be NULL-ed before any `goto exit_handler`.

Fixes: 1774a34cc518b0d337a0a7f4707d944dc78cb95d

Signed-off-by: Mike Beaton <mjsbeaton@gmail.com>

DynamicTablesPkg/Library/Common/AmlLib/CodeGen/AmlCodeGen.c
@@ -4028,6 +4028,7 @@ AmlCodeGenInvokeMethod (
   }
 
   MethodInvocationNode = NULL;
+  ArgCountNode         = NULL;
 
   NodeStream = AllocateZeroPool (sizeof (AML_NODE_HANDLE) * (NumArgs + 1));
   if (NodeStream == NULL) {
@@ -4167,7 +4168,6 @@ AmlCodeGenInvokeMethod (
     goto exit_handler;
   }
 
-  ArgCountNode = NULL;
   Status       = AmlCreateDataNode (
                    EAmlNodeDataTypeUInt,
                    &NumArgs,