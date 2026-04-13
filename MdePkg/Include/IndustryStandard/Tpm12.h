929115a0ef06ed81c22c69f162410217e0a09308
Oliver Smith-Denny <osde@microsoft.com>
2026-04-13 09:53:38 -0700

    MdePkg,SecurityPkg: Fix Spelling Errors in TCG/TPM Definitions
    Fix spelling errors in definitions in
    TcgPhysicalPresence.h, TcgStorageOpal.h, Tpm12.h,
    and Tpm2Acpi.h. Update consumer in SecurityPkg.
    
    Temporary backward-compatible aliases are provided
    for the old misspelled macro and enum names.
    
    Continuous-integration-options: PatchCheck.ignore-multi-package
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

--- MdePkg/Include/IndustryStandard/Tpm12.h
+++ MdePkg/Include/IndustryStandard/Tpm12.h
@@ -1728,19 +1728,22 @@ typedef struct tdTPM_DELEGATIONS {
 //
 // Part 2, section 20.2.1: Owner Permission Settings
 //
-#define TPM_DELEGATE_SetOrdinalAuditStatus           (BIT30)
-#define TPM_DELEGATE_DirWriteAuth                    (BIT29)
-#define TPM_DELEGATE_CMK_ApproveMA                   (BIT28)
-#define TPM_DELEGATE_NV_WriteValue                   (BIT27)
-#define TPM_DELEGATE_CMK_CreateTicket                (BIT26)
-#define TPM_DELEGATE_NV_ReadValue                    (BIT25)
-#define TPM_DELEGATE_Delegate_LoadOwnerDelegation    (BIT24)
-#define TPM_DELEGATE_DAA_Join                        (BIT23)
-#define TPM_DELEGATE_AuthorizeMigrationKey           (BIT22)
-#define TPM_DELEGATE_CreateMaintenanceArchive        (BIT21)
-#define TPM_DELEGATE_LoadMaintenanceArchive          (BIT20)
-#define TPM_DELEGATE_KillMaintenanceFeature          (BIT19)
-#define TPM_DELEGATE_OwnerReadInteralPub             (BIT18)
+#define TPM_DELEGATE_SetOrdinalAuditStatus         (BIT30)
+#define TPM_DELEGATE_DirWriteAuth                  (BIT29)
+#define TPM_DELEGATE_CMK_ApproveMA                 (BIT28)
+#define TPM_DELEGATE_NV_WriteValue                 (BIT27)
+#define TPM_DELEGATE_CMK_CreateTicket              (BIT26)
+#define TPM_DELEGATE_NV_ReadValue                  (BIT25)
+#define TPM_DELEGATE_Delegate_LoadOwnerDelegation  (BIT24)
+#define TPM_DELEGATE_DAA_Join                      (BIT23)
+#define TPM_DELEGATE_AuthorizeMigrationKey         (BIT22)
+#define TPM_DELEGATE_CreateMaintenanceArchive      (BIT21)
+#define TPM_DELEGATE_LoadMaintenanceArchive        (BIT20)
+#define TPM_DELEGATE_KillMaintenanceFeature        (BIT19)
+#define TPM_DELEGATE_OwnerReadInternalPub          (BIT18)
+// This misspelling is kept temporarily for backwards compatibility and will
+// be removed in a future PR. Consumers must migrate to the new definition
+#define TPM_DELEGATE_OwnerReadInteralPub             TPM_DELEGATE_OwnerReadInternalPub
 #define TPM_DELEGATE_ResetLockValue                  (BIT17)
 #define TPM_DELEGATE_OwnerClear                      (BIT16)
 #define TPM_DELEGATE_DisableOwnerClear               (BIT15)
