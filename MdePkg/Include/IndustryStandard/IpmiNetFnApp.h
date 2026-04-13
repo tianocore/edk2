f0380c724140a568fab870bf32e2d10c91bcfb09
Oliver Smith-Denny <osde@microsoft.com>
2026-04-13 11:14:51 -0700

    MdePkg: Fix Spelling Errors in IPMI Definitions
    Fix spelling errors in definitions in
    IpmiNetFnApp.h.
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

--- MdePkg/Include/IndustryStandard/IpmiNetFnApp.h
+++ MdePkg/Include/IndustryStandard/IpmiNetFnApp.h
@@ -526,7 +526,10 @@ typedef struct {
 //
 //  Definitions for Set Session Privelege Level command
 //
-#define IPMI_APP_SET_SESSION_PRIVELEGE_LEVEL  0x3B
+#define IPMI_APP_SET_SESSION_PRIVILEGE_LEVEL  0x3B
+// This misspelling is kept temporarily for backwards compatibility and will
+// be removed in a future PR. Consumers must migrate to the new definition
+#define IPMI_APP_SET_SESSION_PRIVELEGE_LEVEL  IPMI_APP_SET_SESSION_PRIVILEGE_LEVEL
 
 //
 //  Constants and Structure definitions for "Set Session Privelege Level" command to follow here
