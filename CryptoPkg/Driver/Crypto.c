fca48d2103a208282c88c947d7ab2b2d94de4332
Doug Flick <dougflick@microsoft.com>
2026-04-29 11:50:53 -0700

    CryptoPkg: Expose Pkcs7Encrypt via EDKII_CRYPTO_PROTOCOL
    Add EDKII_CRYPTO_PKCS7_ENCRYPT typedef and Pkcs7Encrypt member to
    the EDKII_CRYPTO_PROTOCOL structure. Bump protocol version to 25.
    Add CryptoServicePkcs7Encrypt wrapper in the Crypto driver and
    Pkcs7Encrypt wrapper in BaseCryptLibOnProtocolPpi. Add Pkcs7Encrypt
    PCD bit to PcdCryptoServiceFamilyEnable for independent service
    control. Update Readme.md feature table.
    
    Signed-off-by: Doug Flick <dougflick@microsoft.com>

--- CryptoPkg/Driver/Crypto.c
+++ CryptoPkg/Driver/Crypto.c
@@ -4028,6 +4028,51 @@ CryptoServicePkcs7Sign (
 }
 
 /**
+  Creates a DER-encoded PKCS#7 ContentInfo containing an envelopedData structure
+  that wraps content encrypted for secure transmission to one or more recipients.
+
+  If this interface is not supported, return FALSE.
+
+  @param[in]  X509Stack        Pointer to a stack of X.509 certificates for the
+                               intended recipients of this message, created using
+                               X509ConstructCertificateStack or similar. Each
+                               certificate must provide an RSA public key. Any of the
+                               corresponding private keys will be able to decrypt the
+                               content of the returned ContentInfo.
+  @param[in]  InData           Pointer to the content to be encrypted.
+  @param[in]  InDataSize       Size of the content to be encrypted in bytes.
+  @param[in]  CipherNid        NID of the symmetric cipher to use for encryption.
+                               Supported values are CRYPTO_NID_AES128CBC,
+                               CRYPTO_NID_AES192CBC, and CRYPTO_NID_AES256CBC.
+  @param[in]  Flags            Flags for the encryption operation. Currently only
+                               CRYPTO_PKCS7_DEFAULT is supported, which indicates that
+                               the input data is treated as binary data.
+  @param[out] ContentInfo      Receives a pointer to the output, which is a PKCS#7
+                               DER-encoded ContentInfo that wraps an envelopedData. The
+                               caller must free the returned buffer with FreePool().
+  @param[out] ContentInfoSize  Receives the size of the output in bytes.
+
+  @retval     TRUE             PKCS#7 data encryption succeeded.
+  @retval     FALSE            PKCS#7 data encryption failed.
+  @retval     FALSE            This interface is not supported.
+
+**/
+BOOLEAN
+EFIAPI
+CryptoServicePkcs7Encrypt (
+  IN   UINT8   *X509Stack,
+  IN   UINT8   *InData,
+  IN   UINTN   InDataSize,
+  IN   UINT32  CipherNid,
+  IN   UINT32  Flags,
+  OUT  UINT8   **ContentInfo,
+  OUT  UINTN   *ContentInfoSize
+  )
+{
+  return CALL_BASECRYPTLIB (Pkcs.Services.Pkcs7Encrypt, Pkcs7Encrypt, (X509Stack, InData, InDataSize, CipherNid, Flags, ContentInfo, ContentInfoSize), FALSE);
+}
+
+/**
   Verifies the validity of a PKCS#7 signed data as described in "PKCS #7:
   Cryptographic Message Syntax Standard". The input signed data could be wrapped
   in a ContentInfo structure.
@@ -7642,4 +7687,6 @@ const EDKII_CRYPTO_PROTOCOL  mEdkiiCrypto = {
   /// RSA PSS (Continued)
   CryptoServiceRsaPssSignDigest,
   CryptoServiceRsaPssVerifyDigest,
+  /// Pkcs (Continued)
+  CryptoServicePkcs7Encrypt,
 };
