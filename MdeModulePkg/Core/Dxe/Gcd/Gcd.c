e12808567d106ce05e2b5f94ebebccb644a5be8e
Oliver Smith-Denny <osde@microsoft.com>
2026-05-12 14:39:41 -0700

    MdeModulePkg: Dxe Core: Use CalculateTotalMemoryBinSizeNeeded()
    CoreSetMemoryTypeInformationRange() currently calculates the bin
    size needed independently from the CalculateTotalMemoryBinSizeNeeded()
    function. This commit updates to use that function and remove the
    duplication.
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

--- MdeModulePkg/Core/Dxe/Gcd/Gcd.c
+++ MdeModulePkg/Core/Dxe/Gcd/Gcd.c
@@ -135,25 +135,6 @@ GLOBAL_REMOVE_IF_UNREFERENCED CONST CHAR8  *mGcdAllocationTypeNames[] = {
 };
 
 /**
-  Calculate total memory bin size needed.
-
-  @param BinTop                The top address of the memory bins. This is an optional parameter.
-                               When NULL, the returned size meets the alignment requirements as long as
-                               the base address selected also meets the alignment requirements. When
-                               non-NULL, then the returned BinTop value and the returned size both meet
-                               the alignment requirements. When non-NULL, this will be updated on
-                               output to the new top address of the memory bins that must be used to
-                               satisfy alignment requirements.
-
-  @return The total memory bin size needed.
-
-**/
-UINT64
-CalculateTotalMemoryBinSizeNeeded (
-  IN OUT OPTIONAL EFI_PHYSICAL_ADDRESS  *BinTop
-  );
-
-/**
   Dump the entire contents if the GCD Memory Space Map using DEBUG() macros when
   PcdDebugPrintErrorLevel has the DEBUG_GCD bit set.
 
