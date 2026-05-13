3129e4b83c232cd5673cc23ce1be45d66f1dc836
Shenghu Liu <shenghul@qti.qualcomm.com>
2026-05-13 14:38:14 -0700

    MdeModulePkg/ArmFfaLib: Fix VM ID handling for RX release and RXTX unmap
    According to the FFA specification, when FFA_RX_RELEASE or
    FFA_RXTX_UNMAP is invoked from UEFI as a non-secure virtual instance,
    the w1 register must be zero (MBZ). Supplying a non-zero value causes the
    SPMC to return FFA_INVALID_PARAMETER.
    
    When these interfaces are invoked from UEFI as a non-secure physical
    instance, FFA_ID_GET always returns a VM ID of zero. Therefore,
    providing a non-zero VM ID in this context is unnecessary and may be
    invalid.
    
    Update ArmFfaLib to always set w1 (VM ID / partition ID) to zero when
    invoking FFA_RX_RELEASE and FFA_RXTX_UNMAP, ensuring compliance with
    the FFA specification.
    
    Signed-off-by: Shenghu Liu <shenghul@qti.qualcomm.com>

--- MdeModulePkg/Library/ArmFfaLib/ArmFfaStandaloneMmRxTxMap.c
+++ MdeModulePkg/Library/ArmFfaLib/ArmFfaStandaloneMmRxTxMap.c
@@ -2,6 +2,8 @@
   Arm Ffa library common code.
 
   Copyright (c) 2024-2025, Arm Limited. All rights reserved.<BR>
+  Copyright (c) Qualcomm Technologies, Inc. All rights reserved.<BR>
+
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
    @par Glossary:
@@ -216,8 +218,6 @@ ArmFfaLibRxTxUnmap (
   VOID          *Buffers;
   UINT16        SourceId;
 
-  ArmFfaLibGetPartId (&SourceId);
-
   if (mArmFfaRxTxBufferStmmInfoHandle == NULL) {
     // This means that the agent tried to unmap the buffers before even know them.
     // Let's be a nice player...
@@ -227,6 +227,13 @@ ArmFfaLibRxTxUnmap (
   ZeroMem (&FfaArgs, sizeof (ARM_FFA_ARGS));
 
   FfaArgs.Arg0 = ARM_FID_FFA_RXTX_UNMAP;
+
+  /*
+   * Per Table 13.30: FFA_RXTX_UNMAP function syntax in
+   * DEN0077A_Firmware_Framework_Arm_A-profile_1.3_ALP1.pdf
+   * SourceId should be set to 0 in ARM_FID_FFA_RXTX_UNMAP.
+   */
+  SourceId     = 0;
   FfaArgs.Arg1 = (SourceId << ARM_FFA_SOURCE_EP_SHIFT);
 
   ArmCallFfa (&FfaArgs);
