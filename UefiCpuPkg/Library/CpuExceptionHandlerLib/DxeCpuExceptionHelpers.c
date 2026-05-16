f062b9655402ed7ce968286b1ef114d96942e21b
Vishal Oliyil Kunnil <vishalo@qti.qualcomm.com>
2026-05-16 09:45:23 -0700

    UefiCpuPkg/CpuExceptionHandlerLib: Add doxygen header
    Add missing doxygen header for CpuExceptionHandlerLib.
    
    Signed-off-by: Vishal Oliyil Kunnil <vishalo@qti.qualcomm.com>

--- UefiCpuPkg/Library/CpuExceptionHandlerLib/DxeCpuExceptionHelpers.c
+++ UefiCpuPkg/Library/CpuExceptionHandlerLib/DxeCpuExceptionHelpers.c
@@ -1,7 +1,8 @@
 /** @file
+  Helper functions for DXE exception handling.
 
   Copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
-  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries. All rights reserved. <BR>
+  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
