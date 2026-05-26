a0a37ad4a94d65e88b3233a55a6189fe7811b334
Yeoreum Yun <yeoreum.yun@arm.com>
2026-05-26 16:53:27 +0100

    FmpDevicePkg: support FmpDxeRuntime driver
    This patch is a preparatory change to make a FmpSmm driver which
    update firmware with the StandaloneMm to support runtime capsule update.
    
    To support runtime capsule update, add support FmpDxeRuntime.
    For this, change dynamic allocation for get variable to use stack value
    and add runtime supports.
    
    Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>

--- FmpDevicePkg/FmpDxe/FmpDxe.c
+++ FmpDevicePkg/FmpDxe/FmpDxe.c
@@ -5,6 +5,7 @@
 
   Copyright (c) Microsoft Corporation.<BR>
   Copyright (c) 2018 - 2020, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2026, Arm Limited. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -12,6 +13,8 @@
 
 #include "FmpDxe.h"
 #include "VariableSupport.h"
+
+#include <Protocol/LoadedImage.h>
 
 ///
 /// FILE_GUID from FmpDxe.inf.  When FmpDxe.inf is used in a platform, the
@@ -28,6 +31,11 @@ const EFI_GUID  mDefaultModuleFileGuid = {
 /// TRUE if FmpDeviceLib manages a single firmware storage device.
 ///
 BOOLEAN  mFmpSingleInstance = FALSE;
+
+///
+/// TRUE if FmpDxe Instance is RUNTIME_DRIVER.
+///
+BOOLEAN  mFmpRuntimeDxe = FALSE;
 
 ///
 /// Firmware Management Protocol instance that is initialized in the entry
@@ -75,6 +83,7 @@ const FIRMWARE_MANAGEMENT_PRIVATE_DATA  mFirmwareManagementPrivateDataTemplate =
   NULL,            // VersionName
   TRUE,            // RuntimeVersionSupported
   NULL,            // FmpDeviceLockEvent
+  NULL,            // FmpDeviceVirtualAddressChangeEvent
   FALSE,           // FmpDeviceLocked
   NULL,            // FmpDeviceContext
   NULL,            // VersionVariableName
@@ -99,6 +108,44 @@ EFI_FIRMWARE_MANAGEMENT_UPDATE_IMAGE_PROGRESS  mProgressFunc = NULL;
 /// Null-terminated Unicode string retrieved from PcdFmpDeviceImageIdName.
 ///
 CHAR16  *mImageIdName = NULL;
+
+/**
+  Callback function for VirtualAddressChangeEvent.
+  @param[in] Event      Event
+  @param[in] Context    Firmware management private data
+**/
+STATIC
+VOID
+EFIAPI
+FmpVirtualAddressChangeEvent (
+  IN  EFI_EVENT  Event,
+  IN  VOID       *Context
+  )
+{
+  FIRMWARE_MANAGEMENT_PRIVATE_DATA  *Private;
+
+  Private = Context;
+  if (Private == NULL) {
+    return;
+  }
+
+  gRT->ConvertPointer (0, (VOID **)&mImageIdName);
+  gRT->ConvertPointer (0, (VOID **)&mVarRT);
+  gRT->ConvertPointer (0, (VOID **)&Private->Fmp.GetImageInfo);
+  gRT->ConvertPointer (0, (VOID **)&Private->Fmp.GetImage);
+  gRT->ConvertPointer (0, (VOID **)&Private->Fmp.SetImage);
+  gRT->ConvertPointer (0, (VOID **)&Private->Fmp.CheckImage);
+  gRT->ConvertPointer (0, (VOID **)&Private->Fmp.GetPackageInfo);
+  gRT->ConvertPointer (0, (VOID **)&Private->Fmp.SetPackageInfo);
+  gRT->ConvertPointer (0, (VOID **)&Private->ImageIdName);
+  gRT->ConvertPointer (0, (VOID **)&Private->Descriptor.VersionName);
+  gRT->ConvertPointer (0, (VOID **)&Private->FmpDeviceContext);
+  gRT->ConvertPointer (0, (VOID **)&Private->VersionVariableName);
+  gRT->ConvertPointer (0, (VOID **)&Private->LsvVariableName);
+  gRT->ConvertPointer (0, (VOID **)&Private->LastAttemptStatusVariableName);
+  gRT->ConvertPointer (0, (VOID **)&Private->LastAttemptVersionVariableName);
+  gRT->ConvertPointer (0, (VOID **)&Private->FmpStateVariableName);
+}
 
 /**
   Callback function to report the process of the firmware updating.
@@ -286,6 +333,8 @@ PopulateDescriptor (
   EFI_STATUS            Status;
   UINT32                DependenciesSize;
   FMP_CONTROLLER_STATE  *FmpControllerState;
+  FMP_CONTROLLER_STATE  State;
+  CHAR16                *VersionName;
 
   if (Private == NULL) {
     DEBUG ((DEBUG_ERROR, "FmpDxe(%s): PopulateDescriptor() - Private is NULL.\n", mImageIdName));
@@ -315,7 +364,12 @@ PopulateDescriptor (
   //
   GenerateFmpVariableNames (Private);
 
-  FmpControllerState = GetFmpControllerState (Private);
+  Status = GetFmpControllerState (Private, &State);
+  if (EFI_ERROR (Status)) {
+    FmpControllerState = NULL;
+  } else {
+    FmpControllerState = &State;
+  }
 
   //
   // Get the version.  Some devices don't support getting the firmware version
@@ -337,8 +391,9 @@ PopulateDescriptor (
   //
   // Free the current version name.  Shouldn't really happen but this populate
   // function could be called multiple times (to refresh).
+  // For FmpDxeRuntime driver, don't free VersionName.
   //
-  if (Private->Descriptor.VersionName != NULL) {
+  if (!mFmpRuntimeDxe && (Private->Descriptor.VersionName != NULL)) {
     FreePool (Private->Descriptor.VersionName);
     Private->Descriptor.VersionName = NULL;
   }
@@ -346,19 +401,37 @@ PopulateDescriptor (
   //
   // Attempt to get the version string from the FmpDeviceLib
   //
-  Status = FmpDeviceGetVersionString (&Private->Descriptor.VersionName);
-  if (Status == EFI_UNSUPPORTED) {
-    DEBUG ((DEBUG_INFO, "FmpDxe(%s): GetVersionString() unsupported in FmpDeviceLib.\n", mImageIdName));
-    Private->Descriptor.VersionName = AllocateCopyPool (
-                                        sizeof (VERSION_STRING_NOT_SUPPORTED),
-                                        VERSION_STRING_NOT_SUPPORTED
-                                        );
-  } else if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_INFO, "FmpDxe(%s): GetVersionString() not available in FmpDeviceLib.\n", mImageIdName));
-    Private->Descriptor.VersionName = AllocateCopyPool (
-                                        sizeof (VERSION_STRING_NOT_AVAILABLE),
-                                        VERSION_STRING_NOT_AVAILABLE
-                                        );
+  if (Private->Descriptor.VersionName == NULL) {
+    Status = FmpDeviceGetVersionString (&Private->Descriptor.VersionName);
+    if (Status == EFI_UNSUPPORTED) {
+      DEBUG ((DEBUG_INFO, "FmpDxe(%s): GetVersionString() unsupported in FmpDeviceLib.\n", mImageIdName));
+      Private->Descriptor.VersionName = AllocateCopyPool (
+                                          sizeof (VERSION_STRING_NOT_SUPPORTED),
+                                          VERSION_STRING_NOT_SUPPORTED
+                                          );
+    } else if (EFI_ERROR (Status)) {
+      DEBUG ((DEBUG_INFO, "FmpDxe(%s): GetVersionString() not available in FmpDeviceLib.\n", mImageIdName));
+      Private->Descriptor.VersionName = AllocateCopyPool (
+                                          sizeof (VERSION_STRING_NOT_AVAILABLE),
+                                          VERSION_STRING_NOT_AVAILABLE
+                                          );
+    }
+
+    // Replace VersionName with the Runtime memory.
+    if (mFmpRuntimeDxe) {
+      VersionName = Private->Descriptor.VersionName;
+      if (VersionName != NULL) {
+        Private->Descriptor.VersionName = AllocateRuntimeCopyPool (
+                                            ((StrLen (VersionName) + 1) * sizeof (CHAR16)),
+                                            VersionName
+                                            );
+        if (Private->Descriptor.VersionName == NULL) {
+          DEBUG ((DEBUG_WARN, "FmpDxe(%s): Failed to reallocate VersionName.\n", mImageIdName));
+        }
+
+        FreePool (VersionName);
+      }
+    }
   }
 
   Private->Descriptor.LowestSupportedImageVersion = GetLowestSupportedVersion (FmpControllerState);
@@ -396,10 +469,6 @@ PopulateDescriptor (
   Private->Descriptor.LastAttemptVersion = GetLastAttemptVersionFromFmpControllerState (FmpControllerState);
   Private->Descriptor.LastAttemptStatus  = GetLastAttemptStatusFromFmpControllerState (FmpControllerState);
 
-  if (FmpControllerState != NULL) {
-    FreePool (FmpControllerState);
-  }
-
   //
   // Get the dependency from the FmpDependencyDeviceLib.
   //
@@ -409,7 +478,11 @@ PopulateDescriptor (
   // Check the attribute IMAGE_ATTRIBUTE_DEPENDENCY
   //
   if (Private->Descriptor.AttributesSetting & IMAGE_ATTRIBUTE_DEPENDENCY) {
-    Private->Descriptor.Dependencies = GetFmpDependency (&DependenciesSize);
+    if (mFmpRuntimeDxe) {
+      DEBUG ((DEBUG_ERROR, "FmpDxe(%s): Runtime FmpDxe doesn't support IMAGE_ATTRIBUTE_DEPENDENCY attribute.\n", mImageIdName));
+    } else {
+      Private->Descriptor.Dependencies = GetFmpDependency (&DependenciesSize);
+    }
   }
 
   Private->DescriptorPopulated = TRUE;
@@ -982,6 +1055,13 @@ CheckTheImageInternal (
     goto cleanup;
   }
 
+  if (mFmpRuntimeDxe && (Dependencies != NULL)) {
+    DEBUG ((DEBUG_ERROR, "FmpDxe(%s): CheckImage() - Runtime Fmp doesn't support Dependencies.\n", mImageIdName));
+    Status             = EFI_UNSUPPORTED;
+    *LastAttemptStatus = LAST_ATTEMPT_STATUS_DRIVER_ERROR_IMAGE_NOT_UPDATABLE;
+    goto cleanup;
+  }
+
   //
   // Check the FmpPayloadHeader
   //
@@ -1020,19 +1100,21 @@ CheckTheImageInternal (
   //
   // Evaluate dependency expression
   //
-  Private->DependenciesSatisfied =  CheckFmpDependency (
-                                      Private->Descriptor.ImageTypeId,
-                                      Version,
-                                      Dependencies,
-                                      DependenciesSize,
-                                      &LocalLastAttemptStatus
-                                      );
-  if (!Private->DependenciesSatisfied) {
-    DEBUG ((DEBUG_ERROR, "FmpDxe(%s): CheckTheImage() - Dependency check failed.\n", mImageIdName));
-    *ImageUpdatable    = IMAGE_UPDATABLE_INVALID;
-    Status             = EFI_SUCCESS;
-    *LastAttemptStatus = LocalLastAttemptStatus;
-    goto cleanup;
+  if (!mFmpRuntimeDxe) {
+    Private->DependenciesSatisfied =  CheckFmpDependency (
+                                        Private->Descriptor.ImageTypeId,
+                                        Version,
+                                        Dependencies,
+                                        DependenciesSize,
+                                        &LocalLastAttemptStatus
+                                        );
+    if (!Private->DependenciesSatisfied) {
+      DEBUG ((DEBUG_ERROR, "FmpDxe(%s): CheckTheImage() - Dependency check failed.\n", mImageIdName));
+      *ImageUpdatable    = IMAGE_UPDATABLE_INVALID;
+      Status             = EFI_SUCCESS;
+      *LastAttemptStatus = LocalLastAttemptStatus;
+      goto cleanup;
+    }
   }
 
   //
@@ -1273,6 +1355,12 @@ SetTheImage (
   // Get the dependency from Image.
   //
   Dependencies = GetImageDependency ((EFI_FIRMWARE_IMAGE_AUTHENTICATION *)Image, ImageSize, &DependenciesSize, &LastAttemptStatus);
+  if (mFmpRuntimeDxe && (Dependencies != NULL)) {
+    DEBUG ((DEBUG_ERROR, "FmpDxe(%s): SetTheImage() - Runtime Fmp doesn't support Dependencies.\n", mImageIdName));
+    LastAttemptStatus = LAST_ATTEMPT_STATUS_DRIVER_ERROR_IMAGE_NOT_UPDATABLE;
+    Status            = EFI_UNSUPPORTED;
+    goto cleanup;
+  }
 
   //
   // No functional error in CheckTheImage.  Attempt to get the Version to
@@ -1697,10 +1785,18 @@ InstallFmpInstance (
   //
   // Allocate FMP Protocol instance
   //
-  Private = AllocateCopyPool (
-              sizeof (mFirmwareManagementPrivateDataTemplate),
-              &mFirmwareManagementPrivateDataTemplate
-              );
+  if (mFmpRuntimeDxe) {
+    Private = AllocateRuntimeCopyPool (
+                sizeof (mFirmwareManagementPrivateDataTemplate),
+                &mFirmwareManagementPrivateDataTemplate
+                );
+  } else {
+    Private = AllocateCopyPool (
+                sizeof (mFirmwareManagementPrivateDataTemplate),
+                &mFirmwareManagementPrivateDataTemplate
+                );
+  }
+
   if (Private == NULL) {
     DEBUG ((DEBUG_ERROR, "FmpDxe(%s): Failed to allocate memory for private structure.\n", mImageIdName));
     Status = EFI_OUT_OF_RESOURCES;
@@ -1724,35 +1820,52 @@ InstallFmpInstance (
   //
   PopulateDescriptor (Private);
 
-  if (IsLockFmpDeviceAtLockEventGuidRequired ()) {
-    //
-    // Register all UEFI Variables used by this module to be locked.
-    //
-    Status = LockAllFmpVariables (Private);
-    if (EFI_ERROR (Status)) {
-      DEBUG ((DEBUG_ERROR, "FmpDxe(%s): Failed to register variables to lock.  Status = %r.\n", mImageIdName, Status));
-    } else {
-      DEBUG ((DEBUG_INFO, "FmpDxe(%s): All variables registered to lock\n", mImageIdName));
-    }
+  if (!mFmpRuntimeDxe) {
+    if (IsLockFmpDeviceAtLockEventGuidRequired ()) {
+      //
+      // Register all UEFI Variables used by this module to be locked.
+      //
+      Status = LockAllFmpVariables (Private);
+      if (EFI_ERROR (Status)) {
+        DEBUG ((DEBUG_ERROR, "FmpDxe(%s): Failed to register variables to lock.  Status = %r.\n", mImageIdName, Status));
+      } else {
+        DEBUG ((DEBUG_INFO, "FmpDxe(%s): All variables registered to lock\n", mImageIdName));
+      }
 
+      //
+      // Create and register notify function to lock the FMP device.
+      //
+      Status = gBS->CreateEventEx (
+                      EVT_NOTIFY_SIGNAL,
+                      TPL_CALLBACK,
+                      FmpDxeLockEventNotify,
+                      Private,
+                      mLockGuid,
+                      &Private->FmpDeviceLockEvent
+                      );
+      if (EFI_ERROR (Status)) {
+        DEBUG ((DEBUG_ERROR, "FmpDxe(%s): Failed to register notification.  Status = %r\n", mImageIdName, Status));
+      }
+
+      ASSERT_EFI_ERROR (Status);
+    } else {
+      DEBUG ((DEBUG_VERBOSE, "FmpDxe(%s): Not registering notification to call FmpDeviceLock() because mfg mode\n", mImageIdName));
+    }
+  } else {
     //
-    // Create and register notify function to lock the FMP device.
+    // When FmpDxe runs as RUNTIME driver, It shouldn't lock the device.
     //
-    Status = gBS->CreateEventEx (
-                    EVT_NOTIFY_SIGNAL,
-                    TPL_CALLBACK,
-                    FmpDxeLockEventNotify,
-                    Private,
-                    mLockGuid,
-                    &Private->FmpDeviceLockEvent
+    Status = gBS->CreateEvent (
+                    EVT_SIGNAL_VIRTUAL_ADDRESS_CHANGE,
+                    TPL_NOTIFY,
+                    FmpVirtualAddressChangeEvent,
+                    (VOID *)Private,
+                    &Private->FmpDeviceVirtualAddressChangeEvent
                     );
     if (EFI_ERROR (Status)) {
       DEBUG ((DEBUG_ERROR, "FmpDxe(%s): Failed to register notification.  Status = %r\n", mImageIdName, Status));
+      goto cleanup;
     }
-
-    ASSERT_EFI_ERROR (Status);
-  } else {
-    DEBUG ((DEBUG_VERBOSE, "FmpDxe(%s): Not registering notification to call FmpDeviceLock() because mfg mode\n", mImageIdName));
   }
 
   //
@@ -1775,12 +1888,17 @@ cleanup:
 
   if (EFI_ERROR (Status)) {
     if (Private != NULL) {
+      if (Private->FmpDeviceVirtualAddressChangeEvent != NULL) {
+        gBS->CloseEvent (Private->FmpDeviceVirtualAddressChangeEvent);
+      }
+
       if (Private->FmpDeviceLockEvent != NULL) {
         gBS->CloseEvent (Private->FmpDeviceLockEvent);
       }
 
       if (Private->Descriptor.VersionName != NULL) {
         FreePool (Private->Descriptor.VersionName);
+        Private->Descriptor.VersionName = NULL;
       }
 
       if (Private->FmpDeviceContext != NULL) {
@@ -1789,22 +1907,27 @@ cleanup:
 
       if (Private->VersionVariableName != NULL) {
         FreePool (Private->VersionVariableName);
+        Private->VersionVariableName = NULL;
       }
 
       if (Private->LsvVariableName != NULL) {
         FreePool (Private->LsvVariableName);
+        Private->LsvVariableName = NULL;
       }
 
       if (Private->LastAttemptStatusVariableName != NULL) {
         FreePool (Private->LastAttemptStatusVariableName);
+        Private->LastAttemptStatusVariableName = NULL;
       }
 
       if (Private->LastAttemptVersionVariableName != NULL) {
         FreePool (Private->LastAttemptVersionVariableName);
+        Private->LastAttemptVersionVariableName = NULL;
       }
 
       if (Private->FmpStateVariableName != NULL) {
         FreePool (Private->FmpStateVariableName);
+        Private->FmpStateVariableName = NULL;
       }
 
       FreePool (Private);
@@ -1850,6 +1973,10 @@ UninstallFmpInstance (
   Private = FIRMWARE_MANAGEMENT_PRIVATE_DATA_FROM_THIS (Fmp);
   FmpDeviceSetContext (Private->Handle, &Private->FmpDeviceContext);
 
+  if (Private->FmpDeviceVirtualAddressChangeEvent != NULL) {
+    gBS->CloseEvent (Private->FmpDeviceVirtualAddressChangeEvent);
+  }
+
   if (Private->FmpDeviceLockEvent != NULL) {
     gBS->CloseEvent (Private->FmpDeviceLockEvent);
   }
@@ -1869,6 +1996,7 @@ UninstallFmpInstance (
 
   if (Private->Descriptor.VersionName != NULL) {
     FreePool (Private->Descriptor.VersionName);
+    Private->Descriptor.VersionName = NULL;
   }
 
   if (Private->FmpDeviceContext != NULL) {
@@ -1877,22 +2005,27 @@ UninstallFmpInstance (
 
   if (Private->VersionVariableName != NULL) {
     FreePool (Private->VersionVariableName);
+    Private->VersionVariableName = NULL;
   }
 
   if (Private->LsvVariableName != NULL) {
     FreePool (Private->LsvVariableName);
+    Private->LsvVariableName = NULL;
   }
 
   if (Private->LastAttemptStatusVariableName != NULL) {
     FreePool (Private->LastAttemptStatusVariableName);
+    Private->LastAttemptStatusVariableName = NULL;
   }
 
   if (Private->LastAttemptVersionVariableName != NULL) {
     FreePool (Private->LastAttemptVersionVariableName);
+    Private->LastAttemptVersionVariableName = NULL;
   }
 
   if (Private->FmpStateVariableName != NULL) {
     FreePool (Private->FmpStateVariableName);
+    Private->FmpStateVariableName = NULL;
   }
 
   FreePool (Private);
@@ -1937,7 +2070,10 @@ FmpDxeEntryPoint (
   IN EFI_SYSTEM_TABLE  *SystemTable
   )
 {
-  EFI_STATUS  Status;
+  EFI_STATUS                 Status;
+  EFI_LOADED_IMAGE_PROTOCOL  *LoadedImage;
+
+  mVarRT = gRT;
 
   //
   // Verify that a new FILE_GUID value has been provided in the <Defines>
@@ -1948,6 +2084,19 @@ FmpDxeEntryPoint (
     DEBUG ((DEBUG_ERROR, "FmpDxe: Use of default FILE_GUID detected.  FILE_GUID must be set to a unique value.\n"));
     ASSERT (FALSE);
     return EFI_UNSUPPORTED;
+  }
+
+  //
+  // Check FmpDxe is RUNTIME driver.
+  //
+  Status = gBS->HandleProtocol (
+                  ImageHandle,
+                  &gEfiLoadedImageProtocolGuid,
+                  (VOID **)&LoadedImage
+                  );
+  ASSERT_EFI_ERROR (Status);
+  if (LoadedImage->ImageCodeType == EfiRuntimeServicesCode) {
+    mFmpRuntimeDxe = TRUE;
   }
 
   //
