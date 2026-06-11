221a98a9ae287284c24326e40b2c8c272582d968
Dongyan Qian <qiandongyan@loongson.cn>
2026-06-11 19:01:53 +0800

    DynamicTablesPkg: Enable LoongArch64 common table generation
    Add a LoongArch64 DynamicTableManager implementation and wire
    LOONGARCH64 into the DynamicTablesPkg package build.
    
    The initial LoongArch64 enablement registers common ACPI generators used by
    the current Loongson platform table set: AcpiRawLib, AcpiMcfgLib,
    AcpiPcctLib, AcpiPpttLib and AcpiSlitLib. Tables that still require
    LoongArch64-specific generation can use AcpiRawLib initially and move to
    dedicated generators in follow-up patches.
    
    This patch also enables the existing SMBIOS Type 0/4/7/16/17/19 generators
    for LOONGARCH64.
    
    The LoongArch64 manager follows the existing arch-specific ACPI
    presence-check model and requires FADT, MADT and DSDT.
    
    Signed-off-by: Dongyan Qian <qiandongyan@loongson.cn>

---
+++ DynamicTablesPkg/Drivers/DynamicTableManagerDxe/LoongArch64/LoongArch64DynamicTableManagerDxe.c
@@ -0,0 +1,60 @@
+/** @file
+  LoongArch64 Dynamic Table Manager Dxe
+
+  Copyright (c) 2026, Loongson Technology Corporation Limited. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <IndustryStandard/Acpi.h>
+#include <Library/DebugLib.h>
+#include <Library/PcdLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Protocol/AcpiSystemDescriptionTable.h>
+#include <Protocol/AcpiTable.h>
+
+// Module specific include files.
+#include <AcpiTableGenerator.h>
+#include <ConfigurationManagerObject.h>
+#include <ConfigurationManagerHelper.h>
+#include <DeviceTreeTableGenerator.h>
+#include <Library/TableHelperLib.h>
+#include <Protocol/ConfigurationManagerProtocol.h>
+#include <Protocol/DynamicTableFactoryProtocol.h>
+#include "DynamicTableManagerDxe.h"
+
+///
+/// Array containing the ACPI tables to check.
+/// We require the FADT, MADT and DSDT tables to boot.
+/// The FADT table must be placed at index 0.
+///
+STATIC ACPI_TABLE_PRESENCE_INFO  mAcpiVerifyTables[] = {
+  { EStdAcpiTableIdFadt, EFI_ACPI_6_2_FIXED_ACPI_DESCRIPTION_TABLE_SIGNATURE,            "FADT", TRUE, 0 },
+  { EStdAcpiTableIdMadt, EFI_ACPI_6_2_MULTIPLE_APIC_DESCRIPTION_TABLE_SIGNATURE,         "MADT", TRUE, 0 },
+  { EStdAcpiTableIdDsdt, EFI_ACPI_6_2_DIFFERENTIATED_SYSTEM_DESCRIPTION_TABLE_SIGNATURE, "DSDT", TRUE, 0 },
+};
+
+/** Get the arch specific ACPI table presence information.
+
+  @param [out] PresenceArray      Array containing the ACPI tables to check.
+  @param [out] PresenceArrayCount Count of elements in the PresenceArray.
+  @param [out] FadtIndex          Index of the FADT table in the PresenceArray.
+                                  -1 if absent.
+
+  @retval EFI_SUCCESS           Success.
+**/
+EFI_STATUS
+EFIAPI
+GetAcpiTablePresenceInfo (
+  OUT ACPI_TABLE_PRESENCE_INFO  **PresenceArray,
+  OUT UINT32                    *PresenceArrayCount,
+  OUT INT32                     *FadtIndex
+  )
+{
+  *PresenceArray      = mAcpiVerifyTables;
+  *PresenceArrayCount = ARRAY_SIZE (mAcpiVerifyTables);
+  *FadtIndex          = ACPI_TABLE_VERIFY_FADT;
+
+  return EFI_SUCCESS;
+}
