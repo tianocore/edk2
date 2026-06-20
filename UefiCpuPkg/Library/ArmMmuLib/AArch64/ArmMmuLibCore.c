3c90fb1ffe504ad2b50fa3f028f9a92123668fb8
Sami Mujawar <sami.mujawar@arm.com>
2026-06-20 11:45:41 +0100

    UefiCpuPkg/ArmMmuLib: Drop IPA width in ArmCcaSetMemoryProtectionAttribute
    ArmCcaSetMemoryProtectionAttribute() now derives the Realm CCA
    protection attribute internally using ArmCcaGetProtectionAttributeMask(),
    so callers no longer need to pass the Realm IPA width.
    
    Drop the IPA width parameter from the API and update callers accordingly.
    
    Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>

--- UefiCpuPkg/Library/ArmMmuLib/AArch64/ArmMmuLibCore.c
+++ UefiCpuPkg/Library/ArmMmuLib/AArch64/ArmMmuLibCore.c
@@ -1069,7 +1069,6 @@ ArmCcaGetMemoryProtectionAttribute (
 
     @param [in]  BaseAddress  Base address of the memory region.
     @param [in]  Length       Length of the memory region.
-    @param [in]  IpaWidth     IPA width of the Realm.
     @param [in]  Share        If TRUE, set the most significant
                               bit of the IPA to configure the memory
                               region as Unprotected IPA.
@@ -1086,7 +1085,6 @@ EFIAPI
 ArmCcaSetMemoryProtectionAttribute (
   IN  EFI_PHYSICAL_ADDRESS  BaseAddress,
   IN  UINT64                Length,
-  IN  UINT64                IpaWidth,
   IN  BOOLEAN               Share
   )
 {
@@ -1095,7 +1093,7 @@ ArmCcaSetMemoryProtectionAttribute (
   UINT64      Mask;
   UINT64      CcaProtectionAttribute;
 
-  if ((Length == 0) || (IpaWidth == 0) ||
+  if ((Length == 0) ||
       !IS_ALIGNED (Length, EFI_PAGE_SIZE) ||
       !IS_ALIGNED (BaseAddress, EFI_PAGE_SIZE))
   {
