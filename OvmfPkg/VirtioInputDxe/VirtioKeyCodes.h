efdbf3c355c9e329bf57fec75744b68f12c1ee81
Jiaqing Zhao <Zhao.Jiaqing@amd.com>
2026-06-25 16:16:39 +0800

    OvmfPkg/VirtioInputDxe: Add virtio mouse support
    Virtio mouse devices report relative movements (EV_REL) and button
    inputs. Add VirtioMouse.c implementing mouse capability probing,
    EFI_SIMPLE_POINTER_PROTOCOL implementation and event handler that
    converts VIRTIO_INPUT_EVENT into EFI_SIMPLE_POINTER_STATE.
    
    The shared event dispatcher in VirtioInput.c now routes EV_REL events
    and EV_KEY codes above MAX_KEYBOARD_CODE to the mouse handler, while
    keycodes in [0, MAX_KEYBOARD_CODE] still go to the keyboard. A device
    is now accepted when it provides a keyboard or a mouse, and the
    EFI_SIMPLE_POINTER_PROTOCOL is installed on mouse-capable devices.
    
    Signed-off-by: Jiaqing Zhao <Zhao.Jiaqing@amd.com>

--- OvmfPkg/VirtioInputDxe/VirtioKeyCodes.h
+++ OvmfPkg/VirtioInputDxe/VirtioKeyCodes.h
@@ -18,6 +18,7 @@
  */
 #define EV_SYN  0x00
 #define EV_KEY  0x01
+#define EV_REL  0x02
 
 /*
  * Keys and buttons
@@ -289,3 +290,16 @@
 #define KEY_MICMUTE  248                /* Mute / unmute the microphone */
 
 /* Code 255 is reserved for special needs of AT keyboard driver */
+
+#define BTN_MISC  0x100
+
+#define BTN_LEFT   0x110
+#define BTN_RIGHT  0x111
+
+#define KEY_OK  0x160
+
+/*
+ * Relative axes
+ */
+#define REL_X  0x00
+#define REL_Y  0x01
