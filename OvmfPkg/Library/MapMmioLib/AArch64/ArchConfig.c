ed3dc61734d28196de11b6ee2ff96c8f4662be28
Sami Mujawar <sami.mujawar@arm.com>
2026-06-26 16:46:17 +0100

    MdeModulePkg/MemoryMapLib: Add arch hook for memory attributes
    Add an architecture-specific hook that runs after GCD MMIO mappings are
    created or updated. Use it on AArch64 to configure Realm CCA protection
    attributes, and provide a common no-op implementation for other
    architectures.
    
    Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>

---
+++ OvmfPkg/Library/MapMmioLib/AArch64/ArchConfig.c
@@ -0,0 +1,63 @@
+/** @file
+  Arch specific configuration interfaces.
+
+  Copyright (c) 2026, Arm Ltd. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#include <Base.h>
+#include <Library/ArmCcaLib.h>
+#include <Library/ArmMmuLib.h>
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+
+/**
+  Configure architecture-specific attributes for a memory range.
+
+  On architectures that require additional memory attribute configuration,
+  this function applies those attributes after the range has been added to
+  the GCD memory map and configured with GCD attributes.
+
+  On AArch64, when running as an Arm CCA Realm, this function marks the
+  range as shared so the Realm can access host-provided MMIO regions.
+
+  @param[in] RegionBase  The base address of the memory range.
+  @param[in] RegionSize  The size of the memory range, in bytes.
+
+  @retval EFI_SUCCESS            The memory range was configured successfully,
+                                  or no architecture-specific configuration was
+                                  required.
+  @retval EFI_INVALID_PARAMETER  A parameter is invalid.
+  @retval EFI_UNSUPPORTED        The requested configuration is not supported.
+  @retval Others                 An error occurred while configuring the memory
+                                  range.
+**/
+EFI_STATUS
+EFIAPI
+ArchConfigureAttributes (
+  IN EFI_PHYSICAL_ADDRESS  RegionBase,
+  IN UINT64                RegionSize
+  )
+{
+  EFI_STATUS  Status;
+
+  if (!ArmCcaIsRealm ()) {
+    // Nothing to do.
+    return EFI_SUCCESS;
+  }
+
+  Status = ArmCcaSetMemoryProtectionAttribute (
+             RegionBase,
+             RegionSize,
+             TRUE
+             );
+  if (EFI_ERROR (Status)) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "Failed to set Arm CCA memory Protection Attribute. Status = %r\n",
+      Status
+      ));
+  }
+
+  return Status;
+}
---
+++ OvmfPkg/Library/MapMmioLib/Common/ArchConfig.c
@@ -0,0 +1,38 @@
+/** @file
+  Arch specific configuration interfaces.
+
+  Copyright (c) 2026, Arm Ltd. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#include <Base.h>
+#include <Uefi.h>
+
+/**
+  Configure architecture-specific attributes for a memory range.
+
+  On architectures that require additional memory attribute configuration,
+  this function applies those attributes after the range has been added to
+  the GCD memory map and configured with GCD attributes.
+
+  @param[in] RegionBase  The base address of the memory range.
+  @param[in] RegionSize  The size of the memory range, in bytes.
+
+  @retval EFI_SUCCESS            The memory range was configured successfully,
+                                  or no architecture-specific configuration was
+                                  required.
+  @retval EFI_INVALID_PARAMETER  A parameter is invalid.
+  @retval EFI_UNSUPPORTED        The requested configuration is not supported.
+  @retval Others                 An error occurred while configuring the memory
+                                  range.
+**/
+EFI_STATUS
+EFIAPI
+ArchConfigureAttributes (
+  IN EFI_PHYSICAL_ADDRESS  RegionBase,
+  IN UINT64                RegionSize
+  )
+{
+  // Nothing to do.
+  return EFI_SUCCESS;
+}
