tianocore/edk2

commit 9dd47eeea171c4073798b7afb095a5c9cb1d3a69

OvmfPkg: add new VirtMmCommunicationDxe driver
EFI driver implementing the MmCommunication2Protocol,
but instead of trapping to MM mode (SMM on x86, el3 on arm)
trap to the host, using a virtual device.

Moving the efi variable management to the host allows to have
persistent efi variables without flash storage, and it also
allows to have secure boot support without requiring MM mode
to protect variable storage in flash.

On x86 etc/hardware-info is used for device discovery, on arm
the FDT will be consulted.

Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

OvmfPkg/VirtMmCommunicationDxe/QemuFdt.c
@@ -1,0 +1,186 @@
+/** @file
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <IndustryStandard/QemuUefiVars.h>
+
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/DxeServicesTableLib.h>
+#include <Library/IoLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiRuntimeServicesTableLib.h>
+
+#include <Protocol/FdtClient.h>
+
+#include "VirtMmCommunication.h"
+
+/*
+  Qemu hooks up the uefi vars communication device like this:
+
+        platform-bus@c000000 {
+                interrupt-parent = <0x8002>;
+                ranges = <0x00 0x00 0xc000000 0x2000000>;
+                #address-cells = <0x01>;
+                #size-cells = <0x01>;
+                compatible = "qemu,platform", "simple-bus";
+
+                qemu-uefi-vars@0 {
+                        reg = <0x00 0x10>;
+                        compatible = "qemu,uefi-vars";
+                };
+        };
+
+  So we have to lookup both platform bus and our device to figure where it
+  actually is mapped in mmio space.
+
+  The code assumes the uefi-vars device is connected to the platform bus without
+  checking that this is actually the case due to FdtClientDxe API limitations.
+*/
+STATIC
+UINT64
+VirtMmGetValue (
+  CONST VOID  *Data,
+  UINT32      Size,
+  UINT32      *Pos,
+  UINT32      Cells
+  )
+{
+  UINT32  *Ptr32;
+  UINT64  *Ptr64;
+  UINT64  Value;
+
+  switch (Cells) {
+    case 1:
+      ASSERT (*Pos + 4 <= Size);
+      Ptr32 = (UINT32 *)(Data + *Pos);
+      *Pos += 4;
+      Value = SwapBytes32 (*Ptr32);
+      break;
+    case 2:
+      ASSERT (*Pos + 8 <= Size);
+      Ptr64 = (UINT64 *)(Data + *Pos);
+      *Pos += 8;
+      Value = SwapBytes64 (ReadUnaligned64 (Ptr64));
+      break;
+    default:
+      ASSERT (!"unsupported cell size");
+      Value = 0;
+  }
+
+  return Value;
+}
+
+STATIC
+EFI_STATUS
+EFIAPI
+VirtMmGetProp (
+  IN  FDT_CLIENT_PROTOCOL  *FdtClient,
+  IN  CHAR8                *Compatible,
+  IN  CHAR8                *Property,
+  OUT CONST VOID           **Values,
+  OUT UINT32               *ValSize
+  )
+{
+  EFI_STATUS  Status;
+
+  Status = FdtClient->FindCompatibleNodeProperty (
+                        FdtClient,
+                        Compatible,
+                        Property,
+                        Values,
+                        ValSize
+                        );
+  if (EFI_ERROR (Status)) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "%a: node compatible=\"%a/%a\" not found (%r)\n",
+      __func__,
+      Compatible,
+      Property,
+      Status
+      ));
+    return EFI_NOT_FOUND;
+  }
+
+  DEBUG ((DEBUG_VERBOSE, "%a: %a/%a: ok\n", __func__, Compatible, Property));
+  return EFI_SUCCESS;
+}
+
+EFI_STATUS
+EFIAPI
+VirtMmHwFind (
+  VOID
+  )
+{
+  FDT_CLIENT_PROTOCOL  *FdtClient;
+  EFI_STATUS           Status;
+  UINT32               Pos;
+  CONST VOID           *Ranges;
+  UINT32               RangesSize;
+  CONST VOID           *Reg;
+  UINT32               RegSize;
+
+  UINT64  DevAddr;
+  UINT64  DevSize;
+  UINT64  BusChildAddr;
+  UINT64  BusParentAddr;
+  UINT64  BusSize;
+
+  Status = gBS->LocateProtocol (
+                  &gFdtClientProtocolGuid,
+                  NULL,
+                  (VOID **)&FdtClient
+                  );
+  ASSERT_EFI_ERROR (Status);
+
+  VirtMmGetProp (
+    FdtClient,
+    "qemu,platform",
+    "ranges",
+    &Ranges,
+    &RangesSize
+    );
+  if (EFI_ERROR (Status)) {
+    return EFI_NOT_FOUND;
+  }
+
+  VirtMmGetProp (
+    FdtClient,
+    UEFI_VARS_FDT_COMPAT,
+    "reg",
+    &Reg,
+    &RegSize
+    );
+  if (EFI_ERROR (Status)) {
+    return EFI_NOT_FOUND;
+  }
+
+  if (RegSize == 2 * sizeof (UINT32)) {
+    ASSERT (RangesSize == RegSize + sizeof (UINT64));
+    Pos           = 0;
+    BusChildAddr  = VirtMmGetValue (Ranges, RangesSize, &Pos, 1);
+    BusParentAddr = VirtMmGetValue (Ranges, RangesSize, &Pos, 2);
+    BusSize       = VirtMmGetValue (Ranges, RangesSize, &Pos, 1);
+    Pos           = 0;
+    DevAddr       = VirtMmGetValue (Reg, RegSize, &Pos, 1);
+    DevSize       = VirtMmGetValue (Reg, RegSize, &Pos, 1);
+  } else {
+    DEBUG ((DEBUG_ERROR, "%a: unexpected regsize\n", __func__));
+    return RETURN_UNSUPPORTED;
+  }
+
+  DEBUG ((DEBUG_VERBOSE, "%a: %a\n", __func__, "qemu,platform"));
+  DEBUG ((DEBUG_VERBOSE, "%a:   bus child  %8lx\n", __func__, BusChildAddr));
+  DEBUG ((DEBUG_VERBOSE, "%a:   bus parent %8lx\n", __func__, BusParentAddr));
+  DEBUG ((DEBUG_VERBOSE, "%a:   bus size   %8lx\n", __func__, BusSize));
+  DEBUG ((DEBUG_VERBOSE, "%a: %a\n", __func__, UEFI_VARS_FDT_COMPAT));
+  DEBUG ((DEBUG_VERBOSE, "%a:   dev addr   %8lx\n", __func__, DevAddr));
+  DEBUG ((DEBUG_VERBOSE, "%a:   dev size   %8lx\n", __func__, DevSize));
+
+  mUefiVarsAddr = DevAddr - BusChildAddr + BusParentAddr;
+  DEBUG ((DEBUG_VERBOSE, "%a:   -> mmio at %8lx\n", __func__, mUefiVarsAddr));
+  return RETURN_SUCCESS;
+}

OvmfPkg/VirtMmCommunicationDxe/QemuHwInfo.c
@@ -1,0 +1,78 @@
+/** @file
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+#include <IndustryStandard/QemuFwCfg.h>
+
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/DxeServicesTableLib.h>
+#include <Library/HardwareInfoLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/QemuFwCfgLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiRuntimeServicesTableLib.h>
+
+#include "VirtMmCommunication.h"
+
+EFI_STATUS
+EFIAPI
+VirtMmHwFind (
+  VOID
+  )
+{
+  FIRMWARE_CONFIG_ITEM  FwCfgItem;
+  UINTN                 FwCfgSize;
+  VOID                  *HardwareInfoBlob;
+  LIST_ENTRY            HwInfoList;
+  LIST_ENTRY            *HwLink;
+  HARDWARE_INFO         *HwInfo;
+  UINTN                 Count;
+  EFI_STATUS            Status;
+
+  Status = QemuFwCfgFindFile ("etc/hardware-info", &FwCfgItem, &FwCfgSize);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  DEBUG ((DEBUG_INFO, "%a: etc/hardware-info: exists\n", __func__));
+  HardwareInfoBlob = AllocatePool (FwCfgSize);
+  if (HardwareInfoBlob == NULL) {
+    return RETURN_OUT_OF_RESOURCES;
+  }
+
+  QemuFwCfgSelectItem (FwCfgItem);
+  QemuFwCfgReadBytes (FwCfgSize, HardwareInfoBlob);
+
+  InitializeListHead (&HwInfoList);
+  Status = CreateHardwareInfoList (
+             HardwareInfoBlob,
+             FwCfgSize,
+             HardwareInfoTypeQemuUefiVars,
+             &HwInfoList
+             );
+  Count = GetHardwareInfoCountByType (
+            &HwInfoList,
+            HardwareInfoTypeQemuUefiVars,
+            sizeof (SIMPLE_INFO)
+            );
+  if (Count != 1) {
+    FreeHardwareInfoList (&HwInfoList);
+    FreePool (HardwareInfoBlob);
+    return RETURN_NOT_FOUND;
+  }
+
+  HwLink = GetFirstHardwareInfoByType (
+             &HwInfoList,
+             HardwareInfoTypeQemuUefiVars,
+             sizeof (SIMPLE_INFO)
+             );
+  HwInfo        = HARDWARE_INFO_FROM_LINK (HwLink);
+  mUefiVarsAddr = HwInfo->Data.SimpleDevice->MmioAddress;
+  DEBUG ((DEBUG_INFO, "%a: etc/hardware-info: uefi vars mmio @ 0x%lx\n", __func__, mUefiVarsAddr));
+
+  FreeHardwareInfoList (&HwInfoList);
+  FreePool (HardwareInfoBlob);
+  return RETURN_SUCCESS;
+}

OvmfPkg/VirtMmCommunicationDxe/QemuMmio.c
@@ -1,0 +1,207 @@
+/** @file
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <IndustryStandard/QemuUefiVars.h>
+
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/DxeServicesTableLib.h>
+#include <Library/IoLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiRuntimeServicesTableLib.h>
+
+#include "VirtMmCommunication.h"
+
+UINT64  mUefiVarsAddr;
+
+STATIC
+EFI_STATUS
+VirtMmHwMemAttr (
+  )
+{
+  EFI_STATUS  Status;
+
+  Status = gDS->AddMemorySpace (
+                  EfiGcdMemoryTypeMemoryMappedIo,
+                  mUefiVarsAddr,
+                  EFI_PAGE_SIZE,
+                  EFI_MEMORY_UC | EFI_MEMORY_RUNTIME
+                  );
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: AddMemorySpace failed: %r\n", __func__, Status));
+    return RETURN_UNSUPPORTED;
+  }
+
+  Status = gDS->SetMemorySpaceAttributes (
+                  mUefiVarsAddr,
+                  EFI_PAGE_SIZE,
+                  EFI_MEMORY_UC | EFI_MEMORY_RUNTIME
+                  );
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: SetMemorySpaceAttributes failed: %r\n", __func__, Status));
+    return RETURN_UNSUPPORTED;
+  }
+
+  return EFI_SUCCESS;
+}
+
+STATIC
+EFI_STATUS
+EFIAPI
+VirtMmHwCommand (
+  UINT32  Cmd
+  )
+{
+  UINT32  Count;
+  UINT32  Sts;
+
+  MmioWrite16 (mUefiVarsAddr + UEFI_VARS_REG_CMD_STS, Cmd);
+  for (Count = 0; Count < 100; Count++) {
+    Sts = MmioRead16 (mUefiVarsAddr + UEFI_VARS_REG_CMD_STS);
+    DEBUG ((DEBUG_VERBOSE, "%a: Sts: 0x%x\n", __func__, Sts));
+    switch (Sts) {
+      case UEFI_VARS_STS_SUCCESS:
+        return RETURN_SUCCESS;
+      case UEFI_VARS_STS_BUSY:
+        CpuPause ();
+        break;
+      case UEFI_VARS_STS_ERR_NOT_SUPPORTED:
+        return RETURN_UNSUPPORTED;
+      case UEFI_VARS_STS_ERR_BAD_BUFFER_SIZE:
+        return RETURN_BAD_BUFFER_SIZE;
+      default:
+        return RETURN_DEVICE_ERROR;
+    }
+  }
+
+  return RETURN_TIMEOUT;
+}
+
+EFI_STATUS
+EFIAPI
+VirtMmHwInit (
+  VOID
+  )
+{
+  UINT32      Magic, AddrLo, AddrHi, Flags;
+  EFI_STATUS  Status;
+
+  Status = VirtMmHwFind ();
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: VirtMmHwFind() failed: %d\n", __func__, Status));
+    return Status;
+  }
+
+  VirtMmHwMemAttr ();
+
+  Magic = MmioRead16 (mUefiVarsAddr + UEFI_VARS_REG_MAGIC);
+  if (Magic != UEFI_VARS_MAGIC_VALUE) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "%a: Magic value mismatch (0x%x != 0x%x)\n",
+      __func__,
+      Magic,
+      UEFI_VARS_MAGIC_VALUE
+      ));
+    return RETURN_DEVICE_ERROR;
+  }
+
+  DEBUG ((DEBUG_INFO, "%a: Magic 0x%x, good\n", __func__, Magic));
+
+  Status = VirtMmHwCommand (UEFI_VARS_CMD_RESET);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Reset failed: %d\n", __func__, Status));
+    return Status;
+  }
+
+  MmioWrite32 (mUefiVarsAddr + UEFI_VARS_REG_BUFFER_SIZE, MAX_BUFFER_SIZE);
+
+  Flags = MmioRead32 (mUefiVarsAddr + UEFI_VARS_REG_FLAGS);
+  if (Flags & UEFI_VARS_FLAG_USE_PIO) {
+    mUsePioTransfer = TRUE;
+    DEBUG ((DEBUG_INFO, "%a: using pio transfer mode\n", __func__));
+  } else {
+    AddrLo = (UINT32)mCommunicateBufferPhys;
+    AddrHi = (UINT32)RShiftU64 (mCommunicateBufferPhys, 32);
+    MmioWrite32 (mUefiVarsAddr + UEFI_VARS_REG_DMA_BUFFER_ADDR_LO, AddrLo);
+    MmioWrite32 (mUefiVarsAddr + UEFI_VARS_REG_DMA_BUFFER_ADDR_HI, AddrHi);
+    DEBUG ((DEBUG_INFO, "%a: using dma transfer mode\n", __func__));
+  }
+
+  return RETURN_SUCCESS;
+}
+
+EFI_STATUS
+EFIAPI
+VirtMmHwPioTransfer (
+  VOID     *Buffer,
+  UINT32   BufferSize,
+  BOOLEAN  ToDevice
+  )
+{
+  UINT32      *Ptr  = Buffer;
+  UINT32      Bytes = 0;
+  UINT32      Crc1;
+  UINT32      Crc2;
+  EFI_STATUS  Status;
+
+  Status = VirtMmHwCommand (UEFI_VARS_CMD_PIO_ZERO_OFFSET);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: zero offset failed: %d\n", __func__, Status));
+    return Status;
+  }
+
+  while (Bytes < BufferSize) {
+    if (ToDevice) {
+      MmioWrite32 (mUefiVarsAddr + UEFI_VARS_REG_PIO_BUFFER_TRANSFER, *Ptr);
+    } else {
+      *Ptr = MmioRead32 (mUefiVarsAddr + UEFI_VARS_REG_PIO_BUFFER_TRANSFER);
+    }
+
+    Bytes += sizeof (*Ptr);
+    Ptr++;
+  }
+
+  Crc1 = CalculateCrc32c (Buffer, Bytes, 0);
+  Crc2 = MmioRead32 (mUefiVarsAddr + UEFI_VARS_REG_PIO_BUFFER_CRC32C);
+  if (Crc1 != Crc2) {
+    DEBUG ((DEBUG_ERROR, "%a: crc32c mismatch (0x%08x,0x%08x)\n", __func__, Crc1, Crc2));
+    return RETURN_DEVICE_ERROR;
+  }
+
+  return RETURN_SUCCESS;
+}
+
+EFI_STATUS
+EFIAPI
+VirtMmHwVirtMap (
+  VOID
+  )
+{
+  EFI_STATUS  Status;
+
+  DEBUG ((DEBUG_VERBOSE, "%a: << %lx\n", __func__, mUefiVarsAddr));
+  Status = gRT->ConvertPointer (EFI_OPTIONAL_PTR, (VOID **)&mUefiVarsAddr);
+  DEBUG ((DEBUG_VERBOSE, "%a: >> %lx\n", __func__, mUefiVarsAddr));
+
+  return Status;
+}
+
+EFI_STATUS
+EFIAPI
+VirtMmHwComm (
+  VOID
+  )
+{
+  EFI_STATUS  Status;
+  UINT32      Cmd;
+
+  Cmd    = mUsePioTransfer ? UEFI_VARS_CMD_PIO_MM : UEFI_VARS_CMD_DMA_MM;
+  Status = VirtMmHwCommand (Cmd);
+  DEBUG ((DEBUG_VERBOSE, "%a: Status: %r\n", __func__, Status));
+
+  return Status;
+}

OvmfPkg/VirtMmCommunicationDxe/QemuX64.c
@@ -1,0 +1,149 @@
+/** @file
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <IndustryStandard/QemuUefiVars.h>
+
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/IoLib.h>
+
+#include "VirtMmCommunication.h"
+
+STATIC
+EFI_STATUS
+EFIAPI
+VirtMmHwCommand (
+  UINT32  Cmd
+  )
+{
+  UINT32  Count;
+  UINT32  Sts;
+
+  IoWrite16 (UEFI_VARS_IO_BASE + UEFI_VARS_REG_CMD_STS, Cmd);
+  for (Count = 0; Count < 100; Count++) {
+    Sts = IoRead16 (UEFI_VARS_IO_BASE + UEFI_VARS_REG_CMD_STS);
+    DEBUG ((DEBUG_VERBOSE, "%a: Sts: 0x%x\n", __func__, Sts));
+    switch (Sts) {
+      case UEFI_VARS_STS_SUCCESS:
+        return RETURN_SUCCESS;
+      case UEFI_VARS_STS_BUSY:
+        CpuPause ();
+        break;
+      case UEFI_VARS_STS_ERR_NOT_SUPPORTED:
+        return RETURN_UNSUPPORTED;
+      case UEFI_VARS_STS_ERR_BAD_BUFFER_SIZE:
+        return RETURN_BAD_BUFFER_SIZE;
+      default:
+        return RETURN_DEVICE_ERROR;
+    }
+  }
+
+  return RETURN_TIMEOUT;
+}
+
+EFI_STATUS
+EFIAPI
+VirtMmHwInit (
+  VOID
+  )
+{
+  UINT32      Magic, AddrLo, AddrHi;
+  EFI_STATUS  Status;
+
+  Magic = IoRead16 (UEFI_VARS_IO_BASE + UEFI_VARS_REG_MAGIC);
+  if (Magic != UEFI_VARS_MAGIC_VALUE) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "%a: Magic value mismatch (0x%x != 0x%x)\n",
+      __func__,
+      Magic,
+      UEFI_VARS_MAGIC_VALUE
+      ));
+    return RETURN_DEVICE_ERROR;
+  }
+
+  DEBUG ((DEBUG_INFO, "%a: Magic 0x%x, good\n", __func__, Magic));
+
+  Status = VirtMmHwCommand (UEFI_VARS_CMD_RESET);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Reset failed: %d\n", __func__, Status));
+    return Status;
+  }
+
+  AddrLo = (UINT32)mCommunicateBufferPhys;
+  AddrHi = (UINT32)RShiftU64 (mCommunicateBufferPhys, 32);
+  IoWrite32 (UEFI_VARS_IO_BASE + UEFI_VARS_REG_DMA_BUFFER_ADDR_LO, AddrLo);
+  IoWrite32 (UEFI_VARS_IO_BASE + UEFI_VARS_REG_DMA_BUFFER_ADDR_HI, AddrHi);
+  IoWrite32 (UEFI_VARS_IO_BASE + UEFI_VARS_REG_BUFFER_SIZE, MAX_BUFFER_SIZE);
+
+  return RETURN_SUCCESS;
+}
+
+EFI_STATUS
+EFIAPI
+VirtMmHwPioTransfer (
+  VOID     *Buffer,
+  UINT32   BufferSize,
+  BOOLEAN  ToDevice
+  )
+{
+  UINT32      *Ptr  = Buffer;
+  UINT32      Bytes = 0;
+  UINT32      Crc1;
+  UINT32      Crc2;
+  EFI_STATUS  Status;
+
+  Status = VirtMmHwCommand (UEFI_VARS_CMD_PIO_ZERO_OFFSET);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: zero offset failed: %d\n", __func__, Status));
+    return Status;
+  }
+
+  while (Bytes < BufferSize) {
+    if (ToDevice) {
+      IoWrite32 (UEFI_VARS_IO_BASE + UEFI_VARS_REG_PIO_BUFFER_TRANSFER, *Ptr);
+    } else {
+      *Ptr = IoRead32 (UEFI_VARS_IO_BASE + UEFI_VARS_REG_PIO_BUFFER_TRANSFER);
+    }
+
+    Bytes += sizeof (*Ptr);
+    Ptr++;
+  }
+
+  Crc1 = CalculateCrc32c (Buffer, Bytes, 0);
+  Crc2 = IoRead32 (UEFI_VARS_IO_BASE + UEFI_VARS_REG_PIO_BUFFER_CRC32C);
+  if (Crc1 != Crc2) {
+    DEBUG ((DEBUG_ERROR, "%a: crc32c mismatch (0x%08x,0x%08x)\n", __func__, Crc1, Crc2));
+    return RETURN_DEVICE_ERROR;
+  }
+
+  return RETURN_SUCCESS;
+}
+
+EFI_STATUS
+EFIAPI
+VirtMmHwVirtMap (
+  VOID
+  )
+{
+  return RETURN_SUCCESS;
+}
+
+EFI_STATUS
+EFIAPI
+VirtMmHwComm (
+  VOID
+  )
+{
+  EFI_STATUS  Status;
+  UINT32      Cmd;
+
+  Cmd    = mUsePioTransfer ? UEFI_VARS_CMD_PIO_MM : UEFI_VARS_CMD_DMA_MM;
+  Status = VirtMmHwCommand (Cmd);
+  DEBUG ((DEBUG_VERBOSE, "%a: Status: %r\n", __func__, Status));
+
+  return Status;
+}

commit e49500b8296064954feac62db35e7cd8c3b4e3f2

OvmfPkg/VirtMmCommunicationDxe: Fix unchecked VirtMmGetProp return values
VirtMmHwFind() calls VirtMmGetProp() twice but does not assign the
return value to Status.  The subsequent EFI_ERROR(Status) checks test
the stale Status from LocateProtocol(), which always succeeds at that
point, so FDT lookup failures are silently ignored and the function
proceeds with uninitialised Ranges/Reg pointers.

Assign both VirtMmGetProp() return values to Status so the error
checks work as intended.

Fixes: 9dd47eeea1 ("OvmfPkg: add new VirtMmCommunicationDxe driver")
Signed-off-by: Luigi Leonardi <leonardi@redhat.com>

OvmfPkg/VirtMmCommunicationDxe/QemuFdt.c
@@ -136,24 +136,24 @@ VirtMmHwFind (
                   );
   ASSERT_EFI_ERROR (Status);
 
-  VirtMmGetProp (
-    FdtClient,
-    "qemu,platform",
-    "ranges",
-    &Ranges,
-    &RangesSize
-    );
+  Status = VirtMmGetProp (
+             FdtClient,
+             "qemu,platform",
+             "ranges",
+             &Ranges,
+             &RangesSize
+             );
   if (EFI_ERROR (Status)) {
     return EFI_NOT_FOUND;
   }
 
-  VirtMmGetProp (
-    FdtClient,
-    UEFI_VARS_FDT_COMPAT,
-    "reg",
-    &Reg,
-    &RegSize
-    );
+  Status = VirtMmGetProp (
+             FdtClient,
+             UEFI_VARS_FDT_COMPAT,
+             "reg",
+             &Reg,
+             &RegSize
+             );
   if (EFI_ERROR (Status)) {
     return EFI_NOT_FOUND;
   }