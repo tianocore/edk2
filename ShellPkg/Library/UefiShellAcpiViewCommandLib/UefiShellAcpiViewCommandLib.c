e3384fd3082fbd2c793f030ebcc20c17208f8964
Amrathesh <amrathesh@arm.com>
2026-09-04 08:18:06 +0000

    ShellPkg/AcpiView: Add parser for BERT ACPI table
    The Boot Error Record Table (BERT) identifies a Boot Error Region
    containing platform error records produced during a previous boot.
    
    Extend AcpiView to display and validate BERT table fields. Validate
    the Boot Error Region length and address, and that the entire region
    is reported as EfiReservedMemoryType in the UEFI memory map.
    
    Signed-off-by: Amrathesh <amrathesh@arm.com>

--- ShellPkg/Library/UefiShellAcpiViewCommandLib/UefiShellAcpiViewCommandLib.c
+++ ShellPkg/Library/UefiShellAcpiViewCommandLib/UefiShellAcpiViewCommandLib.c
@@ -54,6 +54,7 @@ ACPI_TABLE_PARSER  ParserList[] = {
   { EFI_ACPI_6_3_ARM_ERROR_SOURCE_TABLE_SIGNATURE,                                                       ParseAcpiAest },
   { EFI_ACPI_ARM_AGDI_TABLE_SIGNATURE,                                                                   ParseAcpiAgdi },
   { EFI_ACPI_6_4_ARM_PERFORMANCE_MONITORING_UNIT_TABLE_SIGNATURE,                                        ParseAcpiApmt },
+  { EFI_ACPI_6_6_BOOT_ERROR_RECORD_TABLE_SIGNATURE,                                                      ParseAcpiBert },
   { EFI_ACPI_6_2_BOOT_GRAPHICS_RESOURCE_TABLE_SIGNATURE,                                                 ParseAcpiBgrt },
   { EFI_ACPI_6_5_CONFIDENTIAL_COMPUTING_EVENT_LOG_TABLE_SIGNATURE,                                       ParseAcpiCcel },
   { EFI_ACPI_6_2_DEBUG_PORT_2_TABLE_SIGNATURE,                                                           ParseAcpiDbg2 },
