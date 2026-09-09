8c6f252c627f8cafaea39c13af709ab4950ee844
Aaron Pop <aaronpop@microsoft.com>
2026-09-09 13:37:54 -0700

    NetworkPkg/IScsiDxe: Check bounds before root path access
    CodeQL static analysis reports out-of-bounds reads in the iSCSI DHCP
    root path parsing loops. The loop conditions read TmpStr[Index] before
    checking Index against Length.
    
    When a delimiter is absent before the end of the root path, Index
    reaches Length and the old conditions perform a one-past-end read before
    terminating. DHCP root paths are externally supplied, so the parser must
    validate the index before accessing the buffer.
    
    Reorder the conditions in the IPv4 and IPv6 parsing paths so the bounds
    check short-circuits the buffer access. This prevents the out-of-bounds
    reads and resolves the CodeQL static scanning failures.
    
    Signed-off-by: Aaron Pop <aaronpop@microsoft.com>

--- NetworkPkg/IScsiDxe/IScsiDhcp6.c
+++ NetworkPkg/IScsiDxe/IScsiDhcp6.c
@@ -88,7 +88,7 @@ IScsiDhcp6ExtractRootPath (
   Fields[RP_FIELD_IDX_SERVERNAME].Str = &TmpStr[Index];
 
   if (!ConfigNvData->DnsMode) {
-    while ((TmpStr[Index] != ISCSI_ROOT_PATH_ADDR_END_DELIMITER) && (Index < Length)) {
+    while ((Index < Length) && (TmpStr[Index] != ISCSI_ROOT_PATH_ADDR_END_DELIMITER)) {
       Index++;
     }
 
@@ -98,7 +98,7 @@ IScsiDhcp6ExtractRootPath (
     TmpStr[Index] = '\0';
     Index        += 2;
   } else {
-    while ((TmpStr[Index] != ISCSI_ROOT_PATH_FIELD_DELIMITER) && (Index < Length)) {
+    while ((Index < Length) && (TmpStr[Index] != ISCSI_ROOT_PATH_FIELD_DELIMITER)) {
       Index++;
     }
 
@@ -119,7 +119,7 @@ IScsiDhcp6ExtractRootPath (
       Fields[FieldIndex].Str = &TmpStr[Index];
     }
 
-    while ((TmpStr[Index] != ISCSI_ROOT_PATH_FIELD_DELIMITER) && (Index < Length)) {
+    while ((Index < Length) && (TmpStr[Index] != ISCSI_ROOT_PATH_FIELD_DELIMITER)) {
       Index++;
     }
 
