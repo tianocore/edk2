tianocore/edk2

commit b377584b344a47f878124c52cfba5904cd886587

DynamicTablesPkg/HwInfoParser: Add FdtResolveInterrupt()
The Devicetree Specification [1] defines the concept of interrupt
mapping, interrupt domain, interrupt nexus and interrupt controller.
An interrupt property might need to be translated through an
interrupt nexus to obtain the interrupt type, id, flags.

Add a FdtResolveInterrupt() function, resolving an interrupt property
through interrupt nexuses until reaching the interrupt-controller
node.

Make use of FdtResolveInterrupt() wherever relevant.
This allows to handle the presence of interrupt nexus
by default.

[1] Spec. v0.4, s2.4 Interrupts and Interrupt Mapping

Signed-off-by: Pierre Gondois <pierre.gondois@arm.com>

DynamicTablesPkg/Library/FdtHwInfoParserLib/FdtUtility.c
@@ -1180,6 +1180,230 @@ FdtGetInterruptMap (
   return EFI_NOT_FOUND;
 }
 
+/** Resolve a child interrupt specifier through interrupt nexus nodes.
+
+  This helper supports nexus nodes whose interrupt-map matching does not depend
+  on child unit-address cells. If an interrupt-map entry targets another nexus,
+  resolution continues recursively until a real interrupt-controller is found.
+
+  @param [in]  Fdt             Pointer to a Flattened Device Tree (Fdt).
+  @param [in]  NexusNode       Offset of the interrupt nexus or controller
+                               node.
+  @param [in]  InterruptData   Pointer to the child interrupt specifier.
+  @param [in]  InputInterruptCells Number of cells in InterruptData.
+  @param [out] Interrupt       If success, points at the resolved interrupt
+                               specifier for the final controller.
+  @param [out] InterruptCells  If success, number of cells in Interrupt.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_ABORTED             An error occurred.
+  @retval EFI_INVALID_PARAMETER   Invalid parameter.
+  @retval EFI_NOT_FOUND           No matching interrupt-map entry found.
+  @retval EFI_UNSUPPORTED         Unsupported interrupt-map format.
+**/
+STATIC
+EFI_STATUS
+EFIAPI
+FdtResolveInterruptInternal (
+  IN  CONST VOID    *Fdt,
+  IN        INT32   NexusNode,
+  IN  CONST UINT32  *InterruptData,
+  IN        INT32   InputInterruptCells,
+  OUT CONST UINT32  **Interrupt,
+  OUT       INT32   *InterruptCells
+  )
+{
+  EFI_STATUS                Status;
+  INTERRUPT_MAP_ENTRY_INFO  MapEntry;
+  CONST UINT32              *MapMask;
+  INT32                     ChildAddressCells;
+  INT32                     MapMaskSize;
+  UINT32                    ChildCells;
+  UINT32                    EntryIndex;
+  UINT32                    Index;
+
+  if ((Fdt == NULL) ||
+      (InterruptData == NULL) ||
+      (Interrupt == NULL) ||
+      (InterruptCells == NULL))
+  {
+    ASSERT (FALSE);
+    return EFI_INVALID_PARAMETER;
+  }
+
+  if (FdtNodeHasProperty (Fdt, NexusNode, "interrupt-controller")) {
+    *Interrupt      = InterruptData;
+    *InterruptCells = InputInterruptCells;
+    return EFI_SUCCESS;
+  }
+
+  ChildAddressCells = FdtAddressCells (Fdt, NexusNode);
+  if (ChildAddressCells < 0) {
+    ASSERT (FALSE);
+    return EFI_ABORTED;
+  }
+
+  //
+  // An "interrupt-map" child-side key is encoded as:
+  // <child-unit-address child-interrupt-specifier>.
+  //
+  ChildCells = (UINT32)ChildAddressCells + (UINT32)InputInterruptCells;
+  if ((UINT32)ChildAddressCells > FDT_MAX_NCELLS) {
+    ASSERT (FALSE);
+    return EFI_ABORTED;
+  }
+
+  //
+  // The "interrupt-map-mask" is used to mask children ids:
+  // <child-unit-address child-interrupt-specifier>.
+  //
+  MapMask = FdtGetProp (Fdt, NexusNode, "interrupt-map-mask", &MapMaskSize);
+  if ((MapMask == NULL) || (MapMaskSize != (INT32)(ChildCells * sizeof (UINT32)))) {
+    ASSERT (FALSE);
+    return EFI_ABORTED;
+  }
+
+  //
+  // Child unit-address translation across intermediate buses is not handled
+  // here. Support the common case where the interrupt-map ignores it.
+  //
+  for (Index = 0; Index < (UINT32)ChildAddressCells; Index++) {
+    if (Fdt32ToCpu (MapMask[Index]) != 0) {
+      return EFI_UNSUPPORTED;
+    }
+  }
+
+  for (EntryIndex = 0; ; EntryIndex++) {
+    INT32  ParentNode;
+
+    Status = FdtGetInterruptMap (Fdt, NexusNode, EntryIndex, TRUE, &MapEntry);
+    if (Status == EFI_NOT_FOUND) {
+      break;
+    } else if (EFI_ERROR (Status)) {
+      ASSERT_EFI_ERROR (FALSE);
+      return Status;
+    }
+
+    if ((MapEntry.ChildAddressCells != ChildAddressCells) ||
+        (MapEntry.ChildInterruptCells != InputInterruptCells))
+    {
+      ASSERT (FALSE);
+      return EFI_ABORTED;
+    }
+
+    for (Index = 0; Index < (UINT32)InputInterruptCells; Index++) {
+      if (MapEntry.ChildInterrupt[Index] !=
+          (Fdt32ToCpu (InterruptData[Index]) &
+           Fdt32ToCpu (MapMask[(UINT32)ChildAddressCells + Index])))
+      {
+        //
+        // This helper only supports nexus mappings that ignore the child
+        // unit-address, so only the interrupt specifier cells come from
+        // InterruptData.
+        //
+        break;
+      }
+    }
+
+    if (Index == (UINT32)InputInterruptCells) {
+      ParentNode = FdtNodeOffsetByPhandle (Fdt, Fdt32ToCpu (*MapEntry.InterruptParent));
+      if (ParentNode < 0) {
+        ASSERT (FALSE);
+        return EFI_ABORTED;
+      }
+
+      return FdtResolveInterruptInternal (
+               Fdt,
+               ParentNode,
+               MapEntry.ParentInterrupt,
+               MapEntry.ParentInterruptCells,
+               Interrupt,
+               InterruptCells
+               );
+    }
+  }
+
+  return EFI_NOT_FOUND;
+}
+
+/** Resolve an interrupt specifier for a node through interrupt nexus nodes.
+
+  If the interrupt parent domain of Node is already an interrupt-controller,
+  the requested interrupt specifier is returned directly from the node
+  "interrupts" property. Otherwise, the interrupt specifier is resolved
+  recursively through one or more parent nexus "interrupt-map" properties
+  until a final interrupt-controller is reached.
+
+  @param [in]  Fdt        Pointer to a Flattened Device Tree (Fdt).
+  @param [in]  Node       Node to get the interrupt from.
+  @param [in]  Index      Index of the interrupt to get.
+  @param [out] Interrupt  If success, contains the resolved interrupt
+                          specifier.
+  @param [out] InterruptCells If success, number of cells in Interrupt.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_ABORTED             An error occurred.
+  @retval EFI_INVALID_PARAMETER   Invalid parameter.
+  @retval EFI_NOT_FOUND           The requested interrupt was not found.
+  @retval EFI_UNSUPPORTED         Unsupported interrupt-map format.
+**/
+EFI_STATUS
+EFIAPI
+FdtResolveInterrupt (
+  IN  CONST VOID    *Fdt,
+  IN        INT32   Node,
+  IN        UINT32  Index,
+  OUT CONST UINT32  **Interrupt,
+  OUT       INT32   *InterruptCells
+  )
+{
+  EFI_STATUS    Status;
+  INT32         IntcNode;
+  INT32         IntCells;
+  CONST UINT32  *InterruptData;
+  INT32         DataSize;
+  UINTN         EntryOffset;
+
+  if ((Fdt == NULL) || (Interrupt == NULL) || (InterruptCells == NULL)) {
+    ASSERT (FALSE);
+    return EFI_INVALID_PARAMETER;
+  }
+
+  Status = FdtGetIntDomainNode (Fdt, Node, &IntcNode);
+  if (EFI_ERROR (Status)) {
+    ASSERT (FALSE);
+    return Status;
+  }
+
+  Status = FdtGetInterruptCellsInfo (Fdt, IntcNode, FALSE, &IntCells);
+  if (EFI_ERROR (Status)) {
+    ASSERT_EFI_ERROR (Status);
+    return Status;
+  }
+
+  InterruptData = FdtGetProp (Fdt, Node, "interrupts", &DataSize);
+  if ((InterruptData == NULL) || (DataSize <= 0)) {
+    ASSERT (FALSE);
+    return EFI_ABORTED;
+  }
+
+  EntryOffset = (UINTN)Index * (UINTN)IntCells;
+  if ((EntryOffset + (UINTN)IntCells) > ((UINTN)DataSize / sizeof (UINT32))) {
+    return EFI_NOT_FOUND;
+  }
+
+  InterruptData += EntryOffset;
+
+  return FdtResolveInterruptInternal (
+           Fdt,
+           IntcNode,
+           InterruptData,
+           IntCells,
+           Interrupt,
+           InterruptCells
+           );
+}
+
 /** Get the "#address-cells" and/or "#size-cells" property of the node.
 
   According to the Device Tree specification, s2.3.5 "#address-cells and

DynamicTablesPkg/Library/FdtHwInfoParserLib/Arm/GenericTimer/ArmGenericTimerParser.c
@@ -52,11 +52,9 @@ TimerNodeParser (
 {
   EFI_STATUS    Status;
   CONST UINT32  *Data;
-  INT32         IntcNode;
-  UINT32        GicVersion;
+  CONST UINT32  *DecodedInterruptData;
+  INT32         DecodedInterruptCells;
   INT32         DataSize;
-  INT32         IntCells;
-  INT32         IntCount;
   UINT32        AlwaysOnTimerFlag;
 
   if ((Fdt == NULL) ||
@@ -73,92 +71,94 @@ TimerNodeParser (
     AlwaysOnTimerFlag = BIT2;
   }
 
-  // Get the associated interrupt-controller.
-  Status = FdtGetIntControllerNode (Fdt, TimerNode, &IntcNode);
-  if (EFI_ERROR (Status)) {
+  Status = FdtResolveInterrupt (
+             Fdt,
+             TimerNode,
+             FdtSecureTimerIrq,
+             &DecodedInterruptData,
+             &DecodedInterruptCells
+             );
+  if (EFI_ERROR (Status) && (Status != EFI_NOT_FOUND)) {
     ASSERT (0);
     return Status;
-  }
-
-  // Check that the interrupt-controller node is a Gic.
-  Status = GetGicVersion (Fdt, IntcNode, &GicVersion);
-  if (EFI_ERROR (Status)) {
-    ASSERT (0);
-    return Status;
-  }
-
-  // Get the number of cells used to encode an interrupt.
-  Status = FdtGetInterruptCellsInfo (Fdt, IntcNode, FALSE, &IntCells);
-  if (EFI_ERROR (Status) || (IntCells == 0)) {
-    ASSERT (0);
-    ASSERT (IntCells != 0);
-    if (Status == EFI_NOT_FOUND) {
-      // Should have found the node.
-      Status = EFI_ABORTED;
-    }
-
-    return Status;
-  }
-
-  Data     = FdtGetProp (Fdt, TimerNode, "interrupts", &DataSize);
-  IntCount = DataSize / IntCells / sizeof (UINT32);
-  if ((Data == NULL) ||
-      (IntCount > FdtMaxTimerItem))
-  {
-    // If error or not FdtMaxTimerItem interrupts.
-    ASSERT (0);
-    return EFI_ABORTED;
-  }
-
-  if ((IntCount > FdtSecureTimerIrq)) {
-    GenericTimerInfo->SecurePL1TimerGSIV =
-      FdtGetInterruptId (&Data[FdtSecureTimerIrq * IntCells], DataSize);
-    GenericTimerInfo->SecurePL1TimerFlags =
-      FdtGetInterruptFlags (&Data[FdtSecureTimerIrq * IntCells], DataSize) | AlwaysOnTimerFlag;
+  } else if (Status == EFI_NOT_FOUND) {
+    GenericTimerInfo->SecurePL1TimerGSIV  = 0;
+    GenericTimerInfo->SecurePL1TimerFlags = 0;
   } else {
-    // No timer, no luck
-    ASSERT (0);
-    return EFI_ABORTED;
+    GenericTimerInfo->SecurePL1TimerGSIV  = FdtGetInterruptId (DecodedInterruptData, DecodedInterruptCells);
+    GenericTimerInfo->SecurePL1TimerFlags = FdtGetInterruptFlags (DecodedInterruptData, DecodedInterruptCells) | AlwaysOnTimerFlag;
   }
 
-  if ((IntCount > FdtNonSecureTimerIrq)) {
-    GenericTimerInfo->NonSecurePL1TimerGSIV =
-      FdtGetInterruptId (&Data[FdtNonSecureTimerIrq * IntCells], DataSize);
-    GenericTimerInfo->NonSecurePL1TimerFlags =
-      FdtGetInterruptFlags (&Data[FdtNonSecureTimerIrq * IntCells], DataSize) | AlwaysOnTimerFlag;
-  } else {
+  Status = FdtResolveInterrupt (
+             Fdt,
+             TimerNode,
+             FdtNonSecureTimerIrq,
+             &DecodedInterruptData,
+             &DecodedInterruptCells
+             );
+  if (EFI_ERROR (Status) && (Status != EFI_NOT_FOUND)) {
+    ASSERT (0);
+    return Status;
+  } else if (Status == EFI_NOT_FOUND) {
     GenericTimerInfo->NonSecurePL1TimerGSIV  = 0;
     GenericTimerInfo->NonSecurePL1TimerFlags = 0;
+  } else {
+    GenericTimerInfo->NonSecurePL1TimerGSIV  = FdtGetInterruptId (DecodedInterruptData, DecodedInterruptCells);
+    GenericTimerInfo->NonSecurePL1TimerFlags = FdtGetInterruptFlags (DecodedInterruptData, DecodedInterruptCells) | AlwaysOnTimerFlag;
   }
 
-  if ((IntCount > FdtVirtualTimerIrq)) {
-    GenericTimerInfo->VirtualTimerGSIV =
-      FdtGetInterruptId (&Data[FdtVirtualTimerIrq * IntCells], DataSize);
-    GenericTimerInfo->VirtualTimerFlags =
-      FdtGetInterruptFlags (&Data[FdtVirtualTimerIrq * IntCells], DataSize) | AlwaysOnTimerFlag;
-  } else {
+  Status = FdtResolveInterrupt (
+             Fdt,
+             TimerNode,
+             FdtVirtualTimerIrq,
+             &DecodedInterruptData,
+             &DecodedInterruptCells
+             );
+  if (EFI_ERROR (Status) && (Status != EFI_NOT_FOUND)) {
+    ASSERT (FALSE);
+    return Status;
+  } else if (Status == EFI_NOT_FOUND) {
     GenericTimerInfo->VirtualTimerGSIV  = 0;
     GenericTimerInfo->VirtualTimerFlags = 0;
+  } else {
+    GenericTimerInfo->VirtualTimerGSIV  = FdtGetInterruptId (DecodedInterruptData, DecodedInterruptCells);
+    GenericTimerInfo->VirtualTimerFlags = FdtGetInterruptFlags (DecodedInterruptData, DecodedInterruptCells) | AlwaysOnTimerFlag;
   }
 
-  if ((IntCount > FdtHypervisorTimerIrq)) {
-    GenericTimerInfo->NonSecurePL2TimerGSIV =
-      FdtGetInterruptId (&Data[FdtHypervisorTimerIrq * IntCells], DataSize);
-    GenericTimerInfo->NonSecurePL2TimerFlags =
-      FdtGetInterruptFlags (&Data[FdtHypervisorTimerIrq * IntCells], DataSize) | AlwaysOnTimerFlag;
-  } else {
+  Status = FdtResolveInterrupt (
+             Fdt,
+             TimerNode,
+             FdtHypervisorTimerIrq,
+             &DecodedInterruptData,
+             &DecodedInterruptCells
+             );
+  if (EFI_ERROR (Status) && (Status != EFI_NOT_FOUND)) {
+    ASSERT (0);
+    return Status;
+  } else if (Status == EFI_NOT_FOUND) {
     GenericTimerInfo->NonSecurePL2TimerGSIV  = 0;
     GenericTimerInfo->NonSecurePL2TimerFlags = 0;
+  } else {
+    GenericTimerInfo->NonSecurePL2TimerGSIV  = FdtGetInterruptId (DecodedInterruptData, DecodedInterruptCells);
+    GenericTimerInfo->NonSecurePL2TimerFlags = FdtGetInterruptFlags (DecodedInterruptData, DecodedInterruptCells) | AlwaysOnTimerFlag;
   }
 
-  if ((IntCount > FdtHypervisorVTimerIrq)) {
-    GenericTimerInfo->VirtualPL2TimerGSIV =
-      FdtGetInterruptId (&Data[FdtHypervisorVTimerIrq * IntCells], DataSize);
-    GenericTimerInfo->VirtualPL2TimerFlags =
-      FdtGetInterruptFlags (&Data[FdtHypervisorVTimerIrq * IntCells], DataSize) | AlwaysOnTimerFlag;
-  } else {
+  Status = FdtResolveInterrupt (
+             Fdt,
+             TimerNode,
+             FdtHypervisorVTimerIrq,
+             &DecodedInterruptData,
+             &DecodedInterruptCells
+             );
+  if (EFI_ERROR (Status) && (Status != EFI_NOT_FOUND)) {
+    ASSERT (0);
+    return Status;
+  } else if (Status == EFI_NOT_FOUND) {
     GenericTimerInfo->VirtualPL2TimerGSIV  = 0;
     GenericTimerInfo->VirtualPL2TimerFlags = 0;
+  } else {
+    GenericTimerInfo->VirtualPL2TimerGSIV  = FdtGetInterruptId (DecodedInterruptData, DecodedInterruptCells);
+    GenericTimerInfo->VirtualPL2TimerFlags = FdtGetInterruptFlags (DecodedInterruptData, DecodedInterruptCells) | AlwaysOnTimerFlag;
   }
 
   // Setup default values

DynamicTablesPkg/Library/FdtHwInfoParserLib/Arm/Gic/ArmGicCParser.c
@@ -289,52 +289,49 @@ GicCIntcNodeParser (
   )
 {
   EFI_STATUS        Status;
-  INT32             IntCells;
   CM_ARM_GICC_INFO  *GicCInfo;
 
-  CONST UINT8  *Data;
-  INT32        DataSize;
-  UINT32       MaintenanceInterrupt;
-  UINT32       Flags;
+  CONST UINT32  *DecodedInterruptData;
+  INT32         DecodedInterruptCells;
+  UINT32        MaintenanceInterrupt;
+  UINT32        Flags;
 
   if (GicCCmObjDesc == NULL) {
     ASSERT (0);
     return EFI_INVALID_PARAMETER;
   }
 
-  // Get the number of cells used to encode an interrupt.
-  Status = FdtGetInterruptCellsInfo (Fdt, GicIntcNode, FALSE, &IntCells);
-  if (EFI_ERROR (Status)) {
-    ASSERT (0);
-    return Status;
-  }
-
   // Get the GSIV maintenance interrupt.
   // According to the DT bindings, this could be the:
   // "Interrupt source of the parent interrupt controller on secondary GICs"
   // but it is assumed that only one Gic is available.
-  Data = FdtGetProp (Fdt, GicIntcNode, "interrupts", &DataSize);
-  if ((Data != NULL) && (DataSize == (IntCells * sizeof (UINT32)))) {
-    MaintenanceInterrupt = FdtGetInterruptId ((CONST UINT32 *)Data, DataSize);
-    Flags                = DT_IRQ_IS_EDGE_TRIGGERED (
-                             Fdt32ToCpu (((UINT32 *)Data)[IRQ_FLAGS_OFFSET])
-                             ) ?
-                           EFI_ACPI_6_3_VGIC_MAINTENANCE_INTERRUPT_MODE_FLAGS :
-                           0;
-    for (GicCInfo = (CM_ARM_GICC_INFO *)GicCCmObjDesc->Data; CpuCount--; GicCInfo++) {
-      GicCInfo->VGICMaintenanceInterrupt = MaintenanceInterrupt;
-      GicCInfo->Flags                   |= Flags;
-    }
-
-    return Status;
-  } else if (DataSize < 0) {
+  Status = FdtResolveInterrupt (
+             Fdt,
+             GicIntcNode,
+             0,
+             &DecodedInterruptData,
+             &DecodedInterruptCells
+             );
+  if (Status == EFI_NOT_FOUND) {
     // This property is optional and was not found. Just return.
+    return EFI_SUCCESS;
+  } else if (EFI_ERROR (Status)) {
+    ASSERT (FALSE);
     return Status;
   }
 
-  // The property exists and its size doesn't match for one interrupt.
-  ASSERT (0);
-  return EFI_ABORTED;
+  MaintenanceInterrupt = FdtGetInterruptId (DecodedInterruptData, DecodedInterruptCells);
+  Flags                = DT_IRQ_IS_EDGE_TRIGGERED (
+                           Fdt32ToCpu (DecodedInterruptData[IRQ_FLAGS_OFFSET])
+                           ) ?
+                         EFI_ACPI_6_3_VGIC_MAINTENANCE_INTERRUPT_MODE_FLAGS :
+                         0;
+  for (GicCInfo = (CM_ARM_GICC_INFO *)GicCCmObjDesc->Data; CpuCount--; GicCInfo++) {
+    GicCInfo->VGICMaintenanceInterrupt = MaintenanceInterrupt;
+    GicCInfo->Flags                   |= Flags;
+  }
+
+  return EFI_SUCCESS;
 }
 
 /** Parse a Gic compatible interrupt-controller node,
@@ -692,14 +689,13 @@ GicCPmuNodeParser (
   )
 {
   EFI_STATUS        Status;
-  INT32             IntCells;
   INT32             PmuNode;
   UINT32            PmuNodeCount;
   UINT32            PmuIrq;
   UINT32            Index;
   CM_ARM_GICC_INFO  *GicCInfo;
-  CONST UINT8       *Data;
-  INT32             DataSize;
+  CONST UINT32      *DecodedInterruptData;
+  INT32             DecodedInterruptCells;
 
   if (GicCCmObjDesc == NULL) {
     ASSERT (GicCCmObjDesc != NULL);
@@ -739,44 +735,88 @@ GicCPmuNodeParser (
     }
   }
 
-  // Get the number of cells used to encode an interrupt.
-  Status = FdtGetInterruptCellsInfo (Fdt, GicIntcNode, FALSE, &IntCells);
+  //
+  // There must be either:
+  // - 1 interrupt per core
+  // - 1 common interrupt
+  //
+
+  Status = FdtResolveInterrupt (
+             Fdt,
+             PmuNode,
+             0,
+             &DecodedInterruptData,
+             &DecodedInterruptCells
+             );
   if (EFI_ERROR (Status)) {
     ASSERT_EFI_ERROR (Status);
     return Status;
   }
 
-  Data = FdtGetProp (Fdt, PmuNode, "interrupts", &DataSize);
-  if ((Data == NULL) || ((DataSize != (GicCCmObjDesc->Count * IntCells * sizeof (UINT32))) && (DataSize != (IntCells * sizeof (UINT32))))) {
-    // If error, not 1 interrupt or not 1 interrupt per core.
-    ASSERT (Data != NULL);
-    ASSERT (DataSize == (IntCells * sizeof (UINT32)) || DataSize == (GicCCmObjDesc->Count * IntCells * sizeof (UINT32)));
-    return EFI_ABORTED;
-  }
+  PmuIrq = FdtGetInterruptId ((CONST UINT32 *)DecodedInterruptData, DecodedInterruptCells);
 
-  if (DataSize == (IntCells * sizeof (UINT32))) {
+  Status = FdtResolveInterrupt (
+             Fdt,
+             PmuNode,
+             1,
+             &DecodedInterruptData,
+             &DecodedInterruptCells
+             );
+  if (Status == EFI_NOT_FOUND) {
     // One shared PMU IRQ
-    PmuIrq = FdtGetInterruptId ((CONST UINT32 *)Data, DataSize);
+    Status = FdtResolveInterrupt (
+               Fdt,
+               PmuNode,
+               0,
+               &DecodedInterruptData,
+               &DecodedInterruptCells
+               );
+    if (EFI_ERROR (Status)) {
+      ASSERT_EFI_ERROR (Status);
+      return Status;
+    }
 
+    //
     // Only supports PPI 23 for now.
     // According to BSA 1.0 s3.6 PPI assignments, PMU IRQ ID is 23. A non BSA
     // compliant system may assign a different IRQ for the PMU, however this
     // is not implemented for now.
+    //
     if (PmuIrq != BSA_PMU_IRQ) {
       ASSERT (PmuIrq == BSA_PMU_IRQ);
       return EFI_ABORTED;
     }
 
+    //
+    // One common interrupt.
+    //
     for (Index = 0; Index < GicCCmObjDesc->Count; Index++) {
       GicCInfo[Index].PerformanceInterruptGsiv = PmuIrq;
     }
-  } else {
-    // One PMU IRQ per core
-    for (Index = 0; Index < GicCCmObjDesc->Count; Index++) {
-      GicCInfo[Index].PerformanceInterruptGsiv = FdtGetInterruptId ((CONST UINT32 *)Data, DataSize);
 
-      Data += IntCells * sizeof (UINT32);
+    return EFI_SUCCESS;
+  } else if (EFI_ERROR (Status)) {
+    ASSERT_EFI_ERROR (Status);
+    return Status;
+  }
+
+  //
+  // One PMU IRQ per core.
+  //
+  for (Index = 0; Index < GicCCmObjDesc->Count; Index++) {
+    Status = FdtResolveInterrupt (
+               Fdt,
+               PmuNode,
+               Index,
+               &DecodedInterruptData,
+               &DecodedInterruptCells
+               );
+    if (EFI_ERROR (Status)) {
+      ASSERT_EFI_ERROR (Status);
+      return EFI_ABORTED;
     }
+
+    GicCInfo[Index].PerformanceInterruptGsiv = FdtGetInterruptId ((CONST UINT32 *)DecodedInterruptData, DecodedInterruptCells);
   }
 
   return EFI_SUCCESS;

DynamicTablesPkg/Library/FdtHwInfoParserLib/FdtUtility.h
@@ -502,6 +502,38 @@ FdtGetInterruptMap (
   OUT       INTERRUPT_MAP_ENTRY_INFO  *Entry
   );
 
+/** Resolve an interrupt specifier for a node through interrupt nexus nodes.
+
+  If the interrupt parent domain of Node is already an interrupt-controller,
+  the requested interrupt specifier is returned directly from the node
+  "interrupts" property. Otherwise, the interrupt specifier is resolved
+  recursively through one or more parent nexus "interrupt-map" properties
+  until a final interrupt-controller is reached.
+
+  @param [in]  Fdt             Pointer to a Flattened Device Tree (Fdt).
+  @param [in]  Node            Node to get the interrupt from.
+  @param [in]  Index           Index of the interrupt to get.
+  @param [out] Interrupt       If success, contains the resolved interrupt
+                               specifier.
+  @param [out] InterruptCells  If success, contains the "#interrupt-cells"
+                               value of the final interrupt-controller.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_ABORTED             An error occurred.
+  @retval EFI_INVALID_PARAMETER   Invalid parameter.
+  @retval EFI_NOT_FOUND           The requested interrupt was not found.
+  @retval EFI_UNSUPPORTED         Unsupported interrupt-map format.
+**/
+EFI_STATUS
+EFIAPI
+FdtResolveInterrupt (
+  IN  CONST VOID    *Fdt,
+  IN        INT32   Node,
+  IN        UINT32  Index,
+  OUT CONST UINT32  **Interrupt,
+  OUT       INT32   *InterruptCells
+  );
+
 /** Get the "#address-cells" and/or "#size-cells" property of the node.
 
   According to the Device Tree specification, s2.3.5 "#address-cells and

DynamicTablesPkg/Library/FdtHwInfoParserLib/Serial/SerialPortParser.c
@@ -90,13 +90,13 @@ SerialPortNodeParser (
   IN  CM_ARCH_COMMON_SERIAL_PORT_INFO  *SerialPortInfo
   )
 {
-  EFI_STATUS   Status;
-  INT32        IntcNode;
-  CONST UINT8  *SizeValue;
-
-  INT32  AddressCells;
-  INT32  SizeCells;
-  INT32  IntCells;
+  EFI_STATUS    Status;
+  INT32         IntcNode;
+  CONST UINT8   *SizeValue;
+  CONST UINT32  *DecodedInterruptData;
+  INT32         DecodedInterruptCells;
+  INT32         AddressCells;
+  INT32         SizeCells;
 
   CONST UINT8  *Data;
   INT32        DataSize;
@@ -166,21 +166,19 @@ SerialPortNodeParser (
     return Status;
   }
 
-  // Get the number of cells used to encode an interrupt.
-  Status = FdtGetInterruptCellsInfo (Fdt, IntcNode, FALSE, &IntCells);
+  Status = FdtResolveInterrupt (
+             Fdt,
+             SerialPortNode,
+             0,
+             &DecodedInterruptData,
+             &DecodedInterruptCells
+             );
   if (EFI_ERROR (Status)) {
     ASSERT (0);
     return Status;
   }
 
-  Data = FdtGetProp (Fdt, SerialPortNode, "interrupts", &DataSize);
-  if ((Data == NULL) || (DataSize != (IntCells * sizeof (UINT32)))) {
-    // If error or not 1 interrupt.
-    ASSERT (0);
-    return EFI_ABORTED;
-  }
-
-  SerialPortInfo->Interrupt = FdtGetInterruptId ((CONST UINT32 *)Data, DataSize);
+  SerialPortInfo->Interrupt = FdtGetInterruptId (DecodedInterruptData, DecodedInterruptCells);
 
   /*
    * In RISC-V, GSI space can be divided among multiple APLIC/PLICs.

commit 7e331a13e1212d7858994ad2e536839ca65be9e6

DynamicTablesPkg/HwInfoParser: Fix FdtResolveInterrupt() on absent property
FdtResolveInterrupt() might be called  on a node with an absent
"interrupts" property. In such case, the function should:
- not assert
- return EFI_NOT_FOUND

This ASSERT is triggered when running ArmVirtKvmtool,
when searching for a GSIV maintenance interrupt.

Fixes: b377584b344a ("DynamicTablesPkg/HwInfoParser:
Add FdtResolveInterrupt()")

Signed-off-by: Pierre Gondois <pierre.gondois@arm.com>

DynamicTablesPkg/Library/FdtHwInfoParserLib/FdtUtility.c
@@ -1383,8 +1383,7 @@ FdtResolveInterrupt (
 
   InterruptData = FdtGetProp (Fdt, Node, "interrupts", &DataSize);
   if ((InterruptData == NULL) || (DataSize <= 0)) {
-    ASSERT (FALSE);
-    return EFI_ABORTED;
+    return EFI_NOT_FOUND;
   }
 
   EntryOffset = (UINTN)Index * (UINTN)IntCells;